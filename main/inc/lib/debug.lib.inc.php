d436cc87a8a9de18ee872db4fd860da94d7c5a3d
Patrick Cool <patrick.cool@ugent.be>
2006-11-21 21:41:02 +0100

    [svn r10084] removed unused functions and added some new debug functions

--- main/inc/lib/debug.lib.inc.php
+++ main/inc/lib/debug.lib.inc.php
@@ -33,16 +33,68 @@
 
 /**
  * This function displays the contend of a variable, array or object in a nicely formatted way.
- *
  * @param $variable a variable, array or object
- * 
+ * @return html code;
  * @author Patrick Cool <patrick.cool@UGent.be>, Ghent University
+ * @version November 2006
  */
 function debug($variable)
 {
 	echo '<pre>';
 	print_r($variable);
 	echo '</pre>';
+}
+
+/**
+ * This function displays all the information of the dokeos $_course array
+ * This array stores all the information of the current course if the user is in a course.
+ * This is why this array is used to check weither the user is currently is in the course.
+ * @author Patrick Cool <patrick.cool@UGent.be>, Ghent University
+ * @version November 2006
+ */
+function debug_course()
+{
+	global $_course;
+	debug($_course);
+}
+
+/**
+ * This function displays all the information of the dokeos $_user array
+ * This array stores all the information of the current user.
+ * @author Patrick Cool <patrick.cool@UGent.be>, Ghent University
+ * @version November 2006
+ */
+function debug_user()
+{
+	global $_user;
+	debug($_user);
+}
+
+/**
+ * This function displays an overview of the different path constants that can be used with the api_get_path function
+ * @author Patrick Cool <patrick.cool@UGent.be>, Ghent University
+ * @version November 2006
+ */
+function debug_paths()
+{
+	echo 'WEB_PATH :'.api_get_path(WEB_PATH).'<br />';
+	echo 'SYS_PATH :'.api_get_path(SYS_PATH).'<br />';
+	echo 'REL_PATH :'.api_get_path(REL_PATH).'<br />';
+	echo 'WEB_COURSE_PATH :'.api_get_path(WEB_COURSE_PATH).'<br />';
+	echo 'SYS_COURSE_PATH :'.api_get_path(SYS_COURSE_PATH).'<br />';
+	echo 'REL_COURSE_PATH :'.api_get_path(REL_COURSE_PATH).'<br />';
+	echo 'REL_CLARO_PATH :'.api_get_path(REL_CLARO_PATH).'<br />';
+	echo 'WEB_CODE_PATH :'.api_get_path(WEB_CODE_PATH).'<br />';
+	echo 'SYS_CODE_PATH :'.api_get_path(SYS_CODE_PATH).'<br />';
+	echo 'SYS_LANG_PATH :'.api_get_path(SYS_LANG_PATH).'<br />';
+	echo 'WEB_IMG_PATH :'.api_get_path(WEB_IMG_PATH).'<br />';
+	echo 'GARBAGE_PATH :'.api_get_path(GARBAGE_PATH).'<br />';
+	echo 'PLUGIN_PATH :'.api_get_path(PLUGIN_PATH).'<br />';
+	echo 'SYS_ARCHIVE_PATH :'.api_get_path(SYS_ARCHIVE_PATH).'<br />';
+	echo 'INCLUDE_PATH :'.api_get_path(INCLUDE_PATH).'<br />';
+	echo 'LIBRARY_PATH :'.api_get_path(LIBRARY_PATH).'<br />';
+	echo 'CONFIGURATION_PATH :'.api_get_path(CONFIGURATION_PATH).'<br />';
+	
 }
 
 

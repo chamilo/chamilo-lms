a46224b95dd6f62d493b59e6ee77f2b6b27209ca
Evie Embrechts <eembrech@vub.ac.be>
2006-11-23 13:53:41 +0100

    [svn r10167] expanded list of possible display locations for messages plugin (see also date plugin for the changes)

--- plugin/messages/plugin.php
+++ plugin/messages/plugin.php
@@ -13,9 +13,9 @@ $plugin_info['title']='Messages';
 //the comments that go with the plugin
 $plugin_info['comment']="Private messages plugin";
 //the locations where this plugin can be shown
-$plugin_info['location']=array("loginpage_menu", "campushomepage_menu","banner");
+$plugin_info['location']=array('loginpage_main', 'loginpage_menu', 'campushomepage_main', 'campushomepage_menu', 'mycourses_main', 'mycourses_menu', 'header', 'footer');
 //the plugin version
-$plugin_info['version']='1.0';
+$plugin_info['version']='1.1';
 //the plugin author
 $plugin_info['author']='Facultad de Matematicas, UADY (Mexico)';
 ?>

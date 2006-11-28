a0202f4dee1a931ca47c9ae8283d84e9d9b1d4c8
Eric Marguin <e.marguin@elixir-interactive.com>
2006-11-28 16:33:44 +0100

    [svn r10242] add audiorecorder course table

--- main/inc/lib/add_course.lib.inc.php
+++ main/inc/lib/add_course.lib.inc.php
@@ -358,6 +358,9 @@ function update_Db_course($courseDbName)
 	$TABLETOOLSURVEYGROUP = $courseDbName . "survey_group";
 	$TABLETOOLSURVEYQUESTION = $courseDbName . "questions";
 	$TABLETOOLSURVEYREPORT = $courseDbName . "survey_report";
+	
+	// audiorecorder
+	$TABLEAUDIORECORDER = $courseDbName.'audiorecorder';
 
 	// Course settings
 	$TABLESETTING = $courseDbName . "course_setting";
@@ -559,6 +562,27 @@ api_sql_query($sql, __FILE__, __LINE__);
 		) TYPE=MyISAM";
 
 	api_sql_query($sql, __FILE__, __LINE__);
+	
+	
+	/*
+    -----------------------------------------------------------
+        Audirecorder tool
+    ----------------------------------------------------------- 
+    */
+    
+    $sql = "CREATE TABLE `".$TABLEAUDIORECORDER."` (
+                  id int(11) NOT NULL auto_increment,
+                  lp_item int(11) NOT NULL default '0',
+                  name varchar(255) NOT NULL default '', 
+                  inserted bigint(20) NOT NULL default '0',
+                  insertedby int(11) NOT NULL default '0',
+                  `order` tinyint(4) NOT NULL default '0',
+                  deleted int(11) NOT NULL default '0', 
+                  PRIMARY KEY  (`id`)
+                )";
+
+    api_sql_query($sql,__FILE__,__LINE__);
+	
 
 	/*
 	-----------------------------------------------------------

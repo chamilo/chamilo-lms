f15ebc2fe9e105b8df1dd3598cc78589ea686030
Luk Vanlanduyt <luk@develop-it.be>
2006-11-30 14:36:45 +0100

    [svn r10273] hotspot files

---
+++ main/exercice/hotspot.inc.php
@@ -0,0 +1,17 @@
+&select=Select
+&square=Rectangle
+&circle=Ellipse
+&poly=Polygon
+&status1=Draw a hotspot.
+&status2_poly=Use right-click to close the polygon.
+&status2_other=Release the mousebutton to save the hotspot.
+&status3=Hotspot saved.
+&exercise_status_1=Status: Question not yet terminated
+&exercise_status_2=Click to submit your answers to the question
+&exercise_status_3=Status: Question terminated
+&showUserPoints=Show/Hide userclicks
+&showHotspots=Show/Hide hotspots
+&labelPolyMenu=Close polygon
+&triesleft=Tries left
+&exeFinished=All answers defined: rearrange spots or click to submit
+&nextAnswer=Now click on: &done=done
---
+++ main/exercice/hotspot_lang_conversion.php
@@ -0,0 +1,41 @@
+<?php
+include_once('../inc/global.inc.php');
+
+$hotspot_lang_file = api_get_path(SYS_LANG_PATH);
+
+if(isset($_GET['lang']))
+{
+	if(file_exists($hotspot_lang_file . $_GET['lang'] . '/hotspot.inc.php'))
+		$hotspot_lang_file .= $_GET['lang'] . '/hotspot.inc.php';
+	else
+		$hotspot_lang_file .= 'english/hotspot.inc.php';
+}
+else
+	$hotspot_lang_file .= 'english/hotspot.inc.php';
+
+$file = file($hotspot_lang_file);
+
+$temp = array();
+
+foreach($file as $value)
+{
+	$explode = explode('=', $value);
+	
+	if(count($explode) > 1)
+	{
+		$explode[0] = trim($explode[0]);
+		$explode[0] = '&' . substr($explode[0], 1, strlen($explode[0]));
+		
+		$explode[1] = trim($explode[1]);
+		$explode[1] = substr($explode[1], 0, strlen($explode[1]) - 1);
+		$explode[1] = ereg_replace('"', '', $explode[1]);
+		
+		$temp[] = $explode[0] . '=' . $explode[1];
+	}
+}
+
+foreach($temp as $value)
+{
+	echo $value . ' ';
+}
+?>

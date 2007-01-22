6edca8cc0c802cc8f8a3c0a91c943376fb481215
Yannick Warnier <yannick.warnier@beeznest.com>
2007-01-23 00:45:54 +0100

    [svn r10831] Added missing drop column queries

--- main/install/migrate-db-1.6.x-1.8.0-post.sql
+++ main/install/migrate-db-1.6.x-1.8.0-post.sql
@@ -28,9 +28,8 @@ DROP TABLE scorm_main;
 DROP TABLE scorm_sco_data;
 
 -- xxCOURSExx
-ALTER TABLE group_category DROP COLUMN forum_state;
-ALTER TABLE group_category DROP COLUMN self_reg_allowed_temp;
-ALTER TABLE group_category DROP COLUMN self_unreg_allowed_temp;
+ALTER TABLE quiz DROP COLUMN active_temp;
+ALTER TABLE tool DROP COLUMN added_tool_temp;
 
 ALTER TABLE group_info DROP COLUMN tutor_id;
 ALTER TABLE group_info DROP COLUMN forum_state;
@@ -38,6 +37,10 @@ ALTER TABLE group_info DROP COLUMN forum_id;
 ALTER TABLE group_info DROP COLUMN self_registration_allowed_temp;
 ALTER TABLE group_info DROP COLUMN self_unregistration_allowed_temp;
 ALTER TABLE group_info DROP COLUMN doc_state_temp;
+
+ALTER TABLE group_category DROP COLUMN forum_state;
+ALTER TABLE group_category DROP COLUMN self_reg_allowed_temp;
+ALTER TABLE group_category DROP COLUMN self_unreg_allowed_temp;
 
 DROP TABLE bb_access;
 DROP TABLE bb_banlist;
@@ -58,3 +61,6 @@ DROP TABLE bb_topics;
 DROP TABLE bb_users;
 DROP TABLE bb_whosonline;
 DROP TABLE bb_words;
+
+-- ?
+DROP TABLE stud_pub_rel_user;

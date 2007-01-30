60b078888b7b3e9581d6c2d993aa9e3c9f543a2f
Yannick Warnier <yannick.warnier@beeznest.com>
2007-01-30 15:55:56 +0100

    [svn r11034] Fixed problem on install caused by config files name change

--- main/install/install_files.inc.php
+++ main/install/install_files.inc.php
@@ -1,4 +1,4 @@
-<?php // $Id: install_files.inc.php 10060 2006-11-20 19:18:00Z pcool $
+<?php // $Id: install_files.inc.php 11034 2007-01-30 14:55:56Z yannoo $
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
@@ -44,6 +44,11 @@ if(defined('DOKEOS_INSTALL'))
 	write_dokeos_config_file('../inc/conf/configuration.dist.php');
 	// Write a .htaccess file in the course repository
 	write_courses_htaccess_file($urlAppendPath);
+	//copy distribution files into right names for Dokeos install
+	copy('../inc/conf/add_course.conf.dist.php','../inc/conf/add_course.conf.php');
+	copy('../inc/conf/course_info.conf.dist.php','../inc/conf/course_info.conf.php');
+	copy('..inc/conf/mail.conf.dist.php','../inc/conf/mail.conf.php');
+	copy('../inc/conf/profile.conf.dist.php','../inc/conf/profile.conf.inc.php');
 }
 else
 {

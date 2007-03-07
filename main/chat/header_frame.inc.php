66ea1968e0b95edd1eb109b83d5b6d02655090c8
Yannick Warnier <yannick.warnier@beeznest.com>
2007-03-07 01:43:27 +0100

    [svn r11458] Fixed bug in charset

--- main/chat/header_frame.inc.php
+++ main/chat/header_frame.inc.php
@@ -47,12 +47,12 @@ elseif(FRAME == 'message')
 	$bodyXtra='onload="javascript:eventMessage();"';
 }
 
-?>
+header('Content-Type: text/html; charset='. $charset);
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset;?>">
 <title>Chat</title>
 
 <link rel="stylesheet" type="text/css" href="../css/<?php if(api_get_setting('stylesheets')=="") echo 'default'; else echo api_get_setting('stylesheets'); ?>/default.css">

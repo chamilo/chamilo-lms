2cfc698a6dc1de314d95330b158d38d8a9295bc4
Julian Prud'homme <j.prudhomme@elixir-interactive.com>
2007-04-20 10:12:32 +0200

    [svn r12068] DLTT import

---
+++ main/lang/dutch/hotspot.inc.php
@@ -0,0 +1,21 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$select = "Kies";
+$square = "vierkant";
+$circle = "Ellips";
+$poly = "Poligoon";
+$status1 = "Teken en hotspot.";
+$status2_poly = "Gebruik de rechtermuisknop om de poligoon te sluiten.";
+$status2_other = "Laat de muisknop los om de hotspot op te slaan";
+$status3 = "Hotspot is bewaard";
+$exercise_status_1 = "Status: de vraag is nog niet beëindigd";
+$exercise_status_2 = "Klik om je antwoorden in te dienen";
+$exercise_status_3 = "Status: vraag beëindigd";
+$showUserPoints = "Klikken van de gebruikers weergeven / verbergen";
+$showHotspots = "Hotspots weergeven / verbergen";
+$labelPolyMenu = "Poligoon afsluiten";
+$triesleft = "Resterende beurten";
+$exeFinished = "Alle antwoorden zijn gedefinieerd. U kunt nu de hotspots herschikken of klikken om in te dienen.";
+$nextAnswer = "Klik nu op: &done=done";
+?>

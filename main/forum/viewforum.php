d06a18fa6d4d04af2067527b358edba3000ab215
Patrick Cool <patrick.cool@ugent.be>
2007-05-07 09:12:38 +0200

    [svn r12310] bugfix: Forum: locked forum show link to add thread http://www.dokeos.com/forum/viewtopic.php?t=9031

--- main/forum/viewforum.php
+++ main/forum/viewforum.php
@@ -194,7 +194,14 @@ if (isset($message))
 // 3. a visitor is here and new threads AND allowed AND  anonymous posts are allowed
 if (api_is_allowed_to_edit() OR ($current_forum['allow_new_threads']==1 AND isset($_user['user_id'])) OR ($current_forum['allow_new_threads']==1 AND !isset($_user['user_id']) AND $current_forum['allow_anonymous']==1))
 {
+	if ($current_forum['locked'] <> 1 AND $current_forum['locked'] <> 1)
+	{
 	echo '<a href="newthread.php?'.api_get_cidreq().'&forum='.$_GET['forum'].$origin_string.'">'.Display::return_icon('forumthread_new.gif').' '.get_lang('NewTopic').'</a>';
+	}
+	else
+	{
+		echo get_lang('ForumLocked');
+	}
 }
 
 /*

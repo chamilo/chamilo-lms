196940202593d3c2a06e54a736e5cab975553303
Yannick Warnier <yannick.warnier@beeznest.com>
2007-06-14 05:00:28 +0200

    [svn r12592] Added variables for videoconference openlaszlo scripts setup

--- main/webrooms/checksession.php
+++ main/webrooms/checksession.php
@@ -1,9 +1,8 @@
 <?php
-/*
+/**
  * Created on 08.11.2006
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
+ * This script gives information to the videoconference scripts (in OpenLaszlo)
+ * to use the right URL and ports for the videoconference.
  */
 include("../../main/inc/global.inc.php");
 api_block_anonymous_users();
@@ -19,6 +18,13 @@ printf('</courseobject>');
 printf('<userobject>');
 foreach ($_SESSION['_user'] as $key => $val)	printf('<%s>%s</%s>',$key,utf8_encode($val),$key);
 printf('</userobject>');
+
+printf('<library>');
+printf('<attribute name="rmpthostlocal" value="'.api_get_setting('service_visio','visio_rtmp_host_local').'" type="string" />');
+printf('<attribute name="iswebrtmp" value="'.api_get_setting('service_visio','visio_is_web_rtmp').'" type="boolean" />');
+printf('<attribute name="rmptport" value="'.api_get_setting('service_visio','visio_rtmp_port').'" type="string" />');
+printf('<attribute name="rmptTunnelport" value="'.api_get_setting('service_visio','visio_rtmp_tunnel_port').'" type="string" />');
+printf('</library>');
 
 printf('</dokeosobject>');
 ?>

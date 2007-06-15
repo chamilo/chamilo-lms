3820ed278fc4845fca713afc7a2c37e5580fa52d
Yannick Warnier <yannick.warnier@beeznest.com>
2007-06-15 22:00:55 +0200

    [svn r12619] Commented deprecated switch used to differenciate Dokeos 1.6 and 1.8 installs

--- main/newscorm/back_compat.inc.php
+++ main/newscorm/back_compat.inc.php
@@ -1,14 +1,16 @@
 <?php //$id: $
 /**
- * This script allows compatibility between the New SCORM tool and both version 1.6.3
- * and 1.8 of Dokeos by loading libraries in a different way
+ * This script used to allow compatibility between the New SCORM tool and both version 1.6.3
+ * and 1.8 of Dokeos by loading libraries in a different way.
+ * The switch is now deprecated and this file will be renamed later on to
+ * something like lp_includes.inc.php
  */
 /**
  * Have a switch looking for the version. If 1.6, load first bit, if 1.8, load second
  */
 //$my_version = $_GLOBALS['dokeosVersion'];
-$my_version = '1.8';
-switch($my_version){
+//$my_version = '1.8';
+/*switch($my_version){
 	case '1.6.3':
 	case '1.6.4':
 	case '1.6.5':
@@ -23,14 +25,17 @@ switch($my_version){
 		break;
 	case '1.8':
 	default:
-		require('../inc/global.inc.php');
-		require_once(api_get_path(LIBRARY_PATH) . 'events.lib.inc.php');
-		require_once(api_get_path(LIBRARY_PATH) . 'database.lib.php');
-		require_once(api_get_path(LIBRARY_PATH) . 'document.lib.php');
-		require_once(api_get_path(LIBRARY_PATH) . 'fileDisplay.lib.php');
-		require_once(api_get_path(LIBRARY_PATH) . 'fileUpload.lib.php'); //replace_dangerous_char()
-		require_once(api_get_path(LIBRARY_PATH) . 'fileManage.lib.php'); //check_name_exists()
-		include_once (api_get_path(LIBRARY_PATH) . 'pclzip/pclzip.lib.php');
+*/
+require('../inc/global.inc.php');
+require_once(api_get_path(LIBRARY_PATH) . 'events.lib.inc.php');
+require_once(api_get_path(LIBRARY_PATH) . 'database.lib.php');
+require_once(api_get_path(LIBRARY_PATH) . 'document.lib.php');
+require_once(api_get_path(LIBRARY_PATH) . 'fileDisplay.lib.php');
+require_once(api_get_path(LIBRARY_PATH) . 'fileUpload.lib.php'); //replace_dangerous_char()
+require_once(api_get_path(LIBRARY_PATH) . 'fileManage.lib.php'); //check_name_exists()
+include_once (api_get_path(LIBRARY_PATH) . 'pclzip/pclzip.lib.php');
+/*
 		break;
 }
+*/
 ?>

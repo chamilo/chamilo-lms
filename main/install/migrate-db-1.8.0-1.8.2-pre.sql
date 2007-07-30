b0057ae03755a946d3e43cc3d4f45f4a311149f1
Yannick Warnier <yannick.warnier@beeznest.com>
2007-07-30 20:50:19 +0200

    [svn r12797] Minor - Fixed version name in file comments

--- main/install/migrate-db-1.8.0-1.8.2-pre.sql
+++ main/install/migrate-db-1.8.0-1.8.2-pre.sql
@@ -1,5 +1,5 @@
 -- This script updates the databases structure before migrating the data from
--- version 1.8.0 to version 1.8.1
+-- version 1.8.0 to version 1.8.2
 -- it is intended as a standalone script, however, because of the multiple
 -- databases related difficulties, it should be parsed by a PHP script in
 -- order to connect to and update the right databases.

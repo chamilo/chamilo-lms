fa561517ae13d9654f7ac44fa5361762be0b9e25
Arnaud Ligot <arnaud@cblue.be>
2007-08-01 15:52:50 +0200

    [svn r12825] new version, without openlaszlo

--- main/newscorm/audiorecorder.inc.php
+++ main/newscorm/audiorecorder.inc.php
@@ -4,12 +4,12 @@
  * Include the lazlo file necessary to use the audiorecorder  
  * @author Sebastian Wagner <seba.wagner@gmail.com>
  * @author Eric Marguin <e.marguin@elixir-interactive.com>
+ * @author Arnaud Ligot <arnaud@cblue.be>
  */
  
  
 
 
-$params = "?lzt=swf&lzr=swf8&document_id=".$audio_recorder_item_id."&dbName=".$_SESSION["_course"]["dbName"]."&user_id=".$_SESSION["_user"]["user_id"].'&studentview='.$audio_recorder_studentview ;     
 
 list($width, $height) = $audio_recorder_studentview =='true' ? array(220, 28) : array(220,140);
 
@@ -17,23 +17,13 @@ $path_to_lzx = api_get_setting('service_ppt2lp','path_to_lzx');
 
 
 if(!empty($path_to_lzx)){
-
-	$path_to_lzx .= $params;
-	printf ("<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000'
-				codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0'
-			 WIDTH='".$width."px' HEIGHT='".$height."px'>
-			<param name='movie' VALUE='%s'>
-			 <param name='quality' VALUE='high'>
-			 <param name='scale' VALUE='scale'>
-			 <param name='swliveconnect' value='true'>
-			 <param name='FlashVars' value='document_id=%s&dbName=%s&user_id=%s&studentview=%s' />
-			 <param name='salign' value='lt' />
-			 <param name='bgcolor' VALUE='#ffffff'> 
-			 <embed src='%s' quality='high' scale='noscale' salign='lb' 
-			 	bgcolor='#ffffff'  WIDTH='".$width."px' HEIGHT='".$height."px' ALIGN='center' TYPE='application/x-shockwave-flash' 
-			 	PLUGINSPAGE='http://www.macromedia.com/go/getflashplayer'>
-			 </embed>
-			 </object>", $path_to_lzx,$audio_recorder_item_id,$_SESSION["_course"]["dbName"],$_SESSION["_user"]["user_id"],$audio_recorder_studentview,$path_to_lzx);
+	printf("<object type=\"application/x-shockwave-flash\" data=\"%s\" 
+			width='$width' height='$height'>
+	         <param name=\"movie\" value=\"%s\">
+		 <param name=\"quality\" value=\"high\">
+		 <param name=\"scale\" value=\"noscale\">
+		 <param name=\"salign\" value=\"LT\">
+		 <param name=\"menu\" value=\"false\"></object>",$path_to_lzx,$path_to_lzx);
 }
 
 ?>

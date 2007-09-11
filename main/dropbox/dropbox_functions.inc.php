3524577ef3fd0014f02166aa301e59f09b54e06f
Eric Marguin <e.marguin@elixir-interactive.com>
2007-09-11 11:42:08 +0200

    [svn r12992] fix a missing langvar

--- main/dropbox/dropbox_functions.inc.php
+++ main/dropbox/dropbox_functions.inc.php
@@ -1067,7 +1067,7 @@ function feedback_form()
 	$number_users_who_see_file=mysql_num_rows($result);
 	if ($number_users_who_see_file>1)
 	{
-		$return .= '<textarea name="feedback" style="width: 80%; height: 80px;"></textarea><br /><input type="submit" name="store_feedback" value="'.get_lang('Submit').'">';
+		$return .= '<textarea name="feedback" style="width: 80%; height: 80px;"></textarea><br /><input type="submit" name="store_feedback" value="'.get_lang('Ok').'">';
 	}
 	else
 	{

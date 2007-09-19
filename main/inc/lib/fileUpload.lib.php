32edddf14d78839f61a464c02d574977fbb17410
Eric Marguin <e.marguin@elixir-interactive.com>
2007-09-19 09:49:43 +0200

    [svn r13077] oups :) I forgot to remove some debug statement in last commit

--- main/inc/lib/fileUpload.lib.php
+++ main/inc/lib/fileUpload.lib.php
@@ -1394,7 +1394,7 @@ function create_unexisting_directory($_course,$user_id,$to_group_id,$to_user_id,
 	}
 	$perm = api_get_setting('permissions_for_new_directories');
 	
-	$perm = octdec(!empty($perm))?$perm:'0770';echo $perm;exit;
+	$perm = octdec(!empty($perm))?$perm:'0770';
 	if ( mkdir($base_work_dir.$desired_dir_name.$nb, $perm))
 	{
 		$document_id = add_document($_course, $desired_dir_name.$nb,'folder',0,$title);

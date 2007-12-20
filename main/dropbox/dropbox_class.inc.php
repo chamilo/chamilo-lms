7801c5cb39dcba43c20d9f4f4c5aed009759a272
Yannick Warnier <yannick.warnier@beeznest.com>
2007-12-20 05:02:13 +0100

    [svn r14040] Minor - Removed duplicate licensing terms

--- main/dropbox/dropbox_class.inc.php
+++ main/dropbox/dropbox_class.inc.php
@@ -60,32 +60,9 @@
  * @version 1.30
  * @copyright 2004
  * @author Jan Bols <jan@ivpv.UGent.be>
- * with contributions by René Haentjens <rene.haentjens@UGent.be> (see RH)
+ * with contributions by Renï¿½ Haentjens <rene.haentjens@UGent.be> (see RH)
   *	@package dokeos.dropbox
  **/
-/*
-  +----------------------------------------------------------------------
-  |   This program is free software; you can redistribute it and/or      
-  |   modify it under the terms of the GNU General Public License        
-  |   as published by the Free Software Foundation; either version 2     
-  |   of the License, or (at your option) any later version.             
-  |                                                                      
-  |   This program is distributed in the hope that it will be useful,    
-  |   but WITHOUT ANY WARRANTY; without even the implied warranty of     
-  |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      
-  |   GNU General Public License for more details.                       
-  |                                                                      
-  |   You should have received a copy of the GNU General Public License  
-  |   along with this program; if not, write to the Free Software        
-  |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          
-  |   02111-1307, USA. The GNU GPL license is also available through     
-  |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         
-  +----------------------------------------------------------------------
-  | Authors: Jan Bols          <jan@ivpv.UGent.be>                       |
-  +----------------------------------------------------------------------
-*/
-
-
 class Dropbox_Work {
 	var $id;
 	var $uploader_id;

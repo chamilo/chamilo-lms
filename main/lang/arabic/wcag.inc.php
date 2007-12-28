fdfc98d94ca030697ebc517208dd6fea082e2b11
Yannick Warnier <yannick.warnier@beeznest.com>
2007-12-28 15:52:18 +0100

    [svn r14078] DLTT import - added files

---
+++ main/lang/arabic/wcag.inc.php
@@ -0,0 +1,9 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langWCAGContent = "$langWCAGContent";
+$langWCAGImage = "$langWCAGImage";
+$langWCAGImageLabel = "$langWCAGImageLabel";
+$langWCAGLink = "$langWCAGLink";
+$langWCAGLinkLabel = "$langWCAGLinkLabel";
+?>
---
+++ main/lang/lithuanian/wcag.inc.php
@@ -0,0 +1,9 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langWCAGContent = "tekstas";
+$langWCAGImage = "paveikslėlis";
+$langWCAGImageLabel = "paveikslėlio pavadinimas";
+$langWCAGLink = "nuoroda";
+$langWCAGLinkLabel = "link label";
+?>

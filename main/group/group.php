5880c5ac8f6a0ab5401e29a9a1f51ded3208e770
Eric Marguin <e.marguin@elixir-interactive.com>
2008-02-14 15:30:37 +0100

    [svn r14294] order by group title by default

--- main/group/group.php
+++ main/group/group.php
@@ -343,7 +343,7 @@ foreach ($group_cats as $index => $category)
 		{
 			$paging_options = array ();
 		}
-		$table = new SortableTableFromArray($group_data);
+		$table = new SortableTableFromArray($group_data, 0);
 		$table->set_additional_parameters(array('category'=>$_GET['category']));
 		$column = 0;
 		if (api_is_allowed_to_edit() and count($group_list) > 1)

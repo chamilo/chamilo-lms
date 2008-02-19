a2fd4df28ae5d947ebf4f65cb67ddbb59addbdfb
Yannick Warnier <yannick.warnier@beeznest.com>
2008-02-19 17:25:31 +0100

    [svn r14318] Added call to replace_accents() in handle_uploaded_documents()

--- main/inc/lib/fileUpload.lib.php
+++ main/inc/lib/fileUpload.lib.php
@@ -325,6 +325,7 @@ function handle_uploaded_document($_course,$uploaded_file,$base_work_dir,$upload
 	//clean up the name and prevent dangerous files
 	//remove strange characters
 	$clean_name = replace_dangerous_char($uploaded_file['name']);
+	$clean_name = replace_accents($clean_name);
 	//no "dangerous" files
 	$clean_name = disable_dangerous_file($clean_name);
 	if(!filter_extension($clean_name))

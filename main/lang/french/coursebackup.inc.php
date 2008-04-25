5023ab3a07a3d75782173c743c236fb731a804b6
Yannick Warnier <yannick.warnier@beeznest.com>
2008-04-26 00:29:42 +0200

    [svn r15128] Last DLTT import before beta

--- main/lang/french/coursebackup.inc.php
+++ main/lang/french/coursebackup.inc.php
@@ -44,4 +44,5 @@ $langCreateBackupInfo = "Créer un fichier de sauvegarde de cet espace. Vous pouv
 $ToolIntro = "Introduction";
 $UploadError = "l\'import n\'a pas fonctionné, veuillez vérifier les limites de taille de fichiers autorisée et les permissions de vos dossiers.";
 $DocumentsWillBeAddedToo = "Les documents seront également ajoutés";
+$ToExportLearnpathWithQuizYouHaveToSelectQuiz = "Si vous désirez exporter un parcours d\'apprentissage contenant des exercices, il est nécessaire d\'inclure ces exercices dans l\'export en les sélectionnant dans la liste des exercices.";
 ?>

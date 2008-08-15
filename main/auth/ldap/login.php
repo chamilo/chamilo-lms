1bb7d2995cf7b7e1467de8904280d0d7f26be3ea
Yannick Warnier <yannick.warnier@beeznest.com>
2008-08-15 16:46:02 +0200

    [svn r16001] Fix to add login event in LDAP authentication method - submitted by Jand - see http://www.dokeos.com/forum/viewtopic.php?p=44642#44642

--- main/auth/ldap/login.php
+++ main/auth/ldap/login.php
@@ -1,4 +1,4 @@
-<?php // $Id: login.php 14965 2008-04-20 23:01:17Z yannoo $
+<?php // $Id: login.php 16001 2008-08-15 14:46:02Z yannoo $
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
@@ -60,6 +60,11 @@ if ($loginLdapSucces)
 	$uidReset = true;
 	$_user['user_id'] = $uData['user_id'];
 	api_session_register('_uid');
+    // Jand: copied from event_login in events.lib.php to enable login statistics:
+    if(!function_exists('event_login')){
+        include(api_get_path(LIBRARY_PATH)."events.lib.inc.php");
+    }
+    event_login(); 
 }
 else
 {

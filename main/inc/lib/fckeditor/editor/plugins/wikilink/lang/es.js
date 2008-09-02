2ed5c13a4ebdee46851268978628f464adc9f50c
Juan Carlos Raña <juancarlos@innovasoc.com>
2008-09-02 21:54:37 +0200

    [svn r16226] to add a wiki button into fckeditor

---
+++ main/inc/lib/fckeditor/editor/plugins/wikilink/lang/es.js
@@ -0,0 +1,31 @@
+﻿/*
+ * FCKeditor - The text editor for Internet - http://www.fckeditor.net
+ * Copyright (C) 2003-2008 Frederico Caldeira Knabben
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * Author: Juan Carlos Raña Trabado
+ * Wikilink Spanish language file.
+ */
+
+
+FCKLang.WikilinkBtn			    = 'Insertar/Editar Enlace wiki' ;
+FCKLang.WikilinkDlgTitle	    = 'Enlace wiki' ;
+FCKLang.WikilinkDlgName		    = 'Nombre del enlace wiki' ;
+FCKLang.WikilinkErrNoName	    = 'Introduzca el nombre del enlace wiki que desea insertar' ;
+FCKLang.PlaceholderErrNameInUse	= ' ';
+FCKLang.WikilinkHelp            = 'SUGERENCIA: también puede crear enlaces wiki sin usar este botón. Para crear una nueva página o para crear un enlace a una existente empleando enlaces wiki, edite una página y añada a su contenido [[título de la página]] o [[texto del enlace | título de la página]] .';

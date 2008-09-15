658347bb444d7591e077335460e811e859d417ec
Juan Carlos Raña <juancarlos@innovasoc.com>
2008-09-15 22:54:12 +0200

    [svn r16370] wiki is active when is created a course

--- main/install/migrate-db-1.8.5-1.8.6-pre.sql
+++ main/install/migrate-db-1.8.5-1.8.6-pre.sql
@@ -68,3 +68,4 @@ ALTER TABLE survey ADD session_id SMALLINT UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE survey ADD INDEX ( session_id ) ;
 CREATE TABLE wiki_discuss (id int NOT NULL auto_increment, publication_id int NOT NULL default 0, userc_id int NOT NULL default 0, comment text NOT NULL, p_score varchar(255) default NULL, timestamp timestamp(14) NOT NULL, PRIMARY KEY  (id) );
 CREATE TABLE wiki_mailcue (id int NOT NULL, user_id int NOT NULL, type text NOT NULL, group_id int DEFAULT NULL, KEY  (id) );
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url, access_url_changeable) VALUES ('course_create_active_tools', 'wiki', 'checkbox', 'Tools', 'true', 'CourseCreateActiveToolsTitle', 'CourseCreateActiveToolsComment', NULL, 'Wiki', 1, 0);

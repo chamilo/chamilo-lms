de0ae9ca8832d7df1164c563fb6ab5354aa7cf2a
Juan Carlos Raña <juancarlos@innovasoc.com>
2008-09-19 17:01:13 +0200

    [svn r16399] update and bug fix mimetex plugin for fckeditor 2.6.2

---
+++ main/inc/lib/fckeditor/editor/plugins/mimetex/lang/fr.js
@@ -0,0 +1,22 @@
+﻿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2006 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * "Support Open Source software. What about a donation today?"
+ * 
+ * File Name: fr.js
+ * 	Placholder French language file.
+ * 
+ * File Authors:
+ * 		Yannick Warnier (yannick.warnier@dokeos.com)
+ */
+FCKLang.mimetexBtn			= 'Insertion/Edition d\'un tag' ;
+FCKLang.mimetexDlgTitle		= 'Propriétés du tag' ;
+FCKLang.mimetexDlgName		= 'Nom du tag' ;
+FCKLang.DlgMimeTeX			= 'mimeTeX Editor' ;

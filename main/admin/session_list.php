e2b9a92ca042ac8fd25cdd38fce071aa20532157
Julian Prud'homme <j.prudhomme@elixir-interactive.com>
2008-09-30 16:13:09 +0200

    [svn r16425] FS#2722 : add an advanced search with 2 filters : active and inactive sessions

--- main/admin/session_list.php
+++ main/admin/session_list.php
@@ -4,6 +4,7 @@ $language_file='admin';
 $cidReset=true;
 
 include('../inc/global.inc.php');
+require_once (api_get_path(LIBRARY_PATH).'formvalidator/FormValidator.class.php');
 
 api_protect_admin_script(true);
 
@@ -52,208 +53,241 @@ if($action == 'delete')
 	exit();
 }
 
-$limit=20;
-$from=$page * $limit;
-
-//if user is crfp admin only list its sessions
-if(!api_is_platform_admin())
-{
-	$where = 'WHERE session_admin_id='.intval($_user['user_id']);
-	$where .= (empty($_REQUEST['keyword']) ? " " : " AND name LIKE '%".addslashes($_REQUEST['keyword'])."%'");
-}
-else{
-	$where .= (empty($_REQUEST['keyword']) ? " " : " WHERE name LIKE '%".addslashes($_REQUEST['keyword'])."%'");
-}
-
-if(trim($where) == ''){
-	$and=" WHERE id_coach=user_id";
-}
-else{
-	$and=" AND id_coach=user_id";
-}
-
-$result=api_sql_query("SELECT id,name,nbr_courses,date_start,date_end, firstname, lastname 
-						FROM $tbl_session, $tbl_user 
-						$where
-						$and
-						ORDER BY $sort 
-						LIMIT $from,".($limit+1),__FILE__,__LINE__);
-
-$Sessions=api_store_result($result);
-
-$nbr_results=sizeof($Sessions);
-
-$tool_name = get_lang('SessionList');
-
 $interbreadcrumb[]=array("url" => "index.php","name" => get_lang('AdministrationTools'));
 
-Display::display_header($tool_name);
-
-api_display_tool_title($tool_name);
-?>
-
-<div id="main">
-
-<?php
-
-if(isset($_GET['action'])){
-	Display::display_normal_message(Security::remove_XSS($_GET['message']), false);
-}
-
-?>
-<form method="POST" action="session_list.php">
-	<input type="text" name="keyword" value="<?php echo Security::remove_XSS($_GET['keyword']); ?>"/>
-	<input type="submit" value="<?php echo get_lang('Search'); ?>"/>
-	</form>
-<form method="post" action="<?php echo api_get_self(); ?>?action=delete&sort=<?php echo $sort; ?>" onsubmit="javascript:if(!confirm('<?php echo get_lang('ConfirmYourChoice'); ?>')) return false;">
-
-<div align="left">
-
-<?php
-
-if(count($Sessions)==0 && isset($_POST['keyword']))
+if (isset ($_GET['search']) && $_GET['search'] == 'advanced')
 {
-	echo get_lang('NoSearchResults');
+	
+	$interbreadcrumb[] = array ("url" => 'session_list.php', "name" => get_lang('SessionList'));
+	$tool_name = get_lang('SearchASession');
+	Display :: display_header($tool_name);
+	
+	$form = new FormValidator('advanced_search','get');
+	
+	$active_group = array();
+	$active_group[] = $form->createElement('checkbox','active','',get_lang('Active'));
+	$active_group[] = $form->createElement('checkbox','inactive','',get_lang('Inactive'));
+	$form->addGroup($active_group,'',get_lang('ActiveSession'),'<br/>',false);
+	
+	$form->addElement('submit','submit',get_lang('Ok'));
+	$defaults['active'] = 1;
+	$defaults['inactive'] = 1;
+	$form->setDefaults($defaults);
+	$form->display();
+	
 }
-else
-{
-	if($page)
+else{
+	
+	$limit=20;
+	$from=$page * $limit;
+	
+	//if user is crfp admin only list its sessions
+	if(!api_is_platform_admin())
 	{
-	?>
-
-	<a href="<?php echo api_get_self(); ?>?page=<?php echo $page-1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Previous'); ?></a>
-
-	<?php
+		$where = 'WHERE session_admin_id='.intval($_user['user_id']);
+		$where .= (empty($_REQUEST['keyword']) ? " " : " AND name LIKE '%".addslashes($_REQUEST['keyword'])."%'");
 	}
-	else
-	{
-		echo get_lang('Previous');
+	else{
+		$where .= (empty($_REQUEST['keyword']) ? " " : " WHERE name LIKE '%".addslashes($_REQUEST['keyword'])."%'");
 	}
-	?>
-
-	|
-
-	<?php
-	if($nbr_results > $limit)
-	{
-	?>
-
-	<a href="<?php echo api_get_self(); ?>?page=<?php echo $page+1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Next'); ?></a>
-
-	<?php
+	
+	if(trim($where) == ''){
+		$and=" WHERE id_coach=user_id";
 	}
-	else
-	{
-		echo get_lang('Next');
+	else{
+		$and=" AND id_coach=user_id";
 	}
-	?>
-
-	</div>
-
-	<br>
-
-	<table class="data_table" width="100%">
-	<tr>
-	  <th>&nbsp;</th>
-	  <th><a href="<?php echo api_get_self(); ?>?sort=name"><?php echo get_lang('NameOfTheSession'); ?></a></th>
-	  <th><a href="<?php echo api_get_self(); ?>?sort=nbr_courses"><?php echo get_lang('NumberOfCourses'); ?></a></th>
-	  <th><a href="<?php echo api_get_self(); ?>?sort=date_start"><?php echo get_lang('StartDate'); ?></a></th>
-	  <th><a href="<?php echo api_get_self(); ?>?sort=date_end"><?php echo get_lang('EndDate'); ?></a></th>
-	  <th><a href="<?php echo api_get_self(); ?>?sort=coach_name"><?php echo get_lang('Coach'); ?></a></th>
-	  <th><?php echo get_lang('Actions'); ?></th>
-	</tr>
-
-	<?php
-	$i=0;
-
-	foreach($Sessions as $key=>$enreg)
-	{
-		if($key == $limit)
-		{
-			break;
-		}
-		$sql = 'SELECT COUNT(course_code) FROM '.$tbl_session_rel_course.' WHERE id_session='.intval($enreg['id']);
-
-	  	$rs = api_sql_query($sql, __FILE__, __LINE__);
-	  	list($nb_courses) = Database::fetch_array($rs);
-
-	?>
-
-	<tr class="<?php echo $i?'row_odd':'row_even'; ?>">
-	  <td><input type="checkbox" name="idChecked[]" value="<?php echo $enreg['id']; ?>"></td>
-	  <td><a href="resume_session.php?id_session=<?php echo $enreg['id']; ?>"><?php echo htmlentities($enreg['name'],ENT_QUOTES,$charset); ?></a></td>
-	  <td><a href="session_course_list.php?id_session=<?php echo $enreg['id']; ?>"><?php echo $nb_courses; ?> cours</a></td>
-	  <td><?php echo htmlentities($enreg['date_start'],ENT_QUOTES,$charset); ?></td>
-	  <td><?php echo htmlentities($enreg['date_end'],ENT_QUOTES,$charset); ?></td>
-	  <td><?php echo htmlentities($enreg['firstname'],ENT_QUOTES,$charset).' '.htmlentities($enreg['lastname'],ENT_QUOTES,$charset); ?></td>
-	  <td>
-		<a href="add_users_to_session.php?page=session_list.php&id_session=<?php echo $enreg['id']; ?>"><img src="../img/add_user_big.gif" border="0" align="absmiddle" title="<?php echo get_lang('SubscribeUsersToSession'); ?>"></a>
-		<a href="add_courses_to_session.php?page=session_list.php&id_session=<?php echo $enreg['id']; ?>"><img src="../img/synthese_view.gif" border="0" align="absmiddle" title="<?php echo get_lang('SubscribeCoursesToSession'); ?>"></a>
-		<a href="session_edit.php?page=session_list.php&id=<?php echo $enreg['id']; ?>"><img src="../img/edit.gif" border="0" align="absmiddle" title="<?php echo get_lang('Edit'); ?>"></a>
-		<a href="<?php echo api_get_self(); ?>?sort=<?php echo $sort; ?>&action=delete&idChecked=<?php echo $enreg['id']; ?>" onclick="javascript:if(!confirm('<?php echo get_lang('ConfirmYourChoice'); ?>')) return false;"><img src="../img/delete.gif" border="0" align="absmiddle" title="<?php echo get_lang('Delete'); ?>" alt="<?php echo get_lang('Delete'); ?>"></a>
-	  </td>
-	</tr>
-
-	<?php
-		$i=$i ? 0 : 1;
+	
+	if(isset($_REQUEST['active']) && !isset($_REQUEST['inactive']) ){
+		$and .= ' AND ( (session.date_start <= CURDATE() AND session.date_end >= CURDATE()) OR session.date_start="0000-00-00" ) ';
 	}
-
-	unset($Sessions);
-
+	if(!isset($_REQUEST['active']) && isset($_REQUEST['inactive']) ){
+		$and .= ' AND ( (session.date_start > CURDATE() OR session.date_end < CURDATE()) AND session.date_start<>"0000-00-00" ) ';
+	}
+	
+	$result=api_sql_query("SELECT id,name,nbr_courses,date_start,date_end, firstname, lastname 
+							FROM $tbl_session, $tbl_user 
+							$where
+							$and
+							ORDER BY $sort 
+							LIMIT $from,".($limit+1),__FILE__,__LINE__);
+	
+	$Sessions=api_store_result($result);
+	
+	$nbr_results=sizeof($Sessions);
+	
+	$tool_name = get_lang('SessionList');
+	
+	Display::display_header($tool_name);
+	
+	api_display_tool_title($tool_name);
 	?>
-
-	</table>
-
-	<br>
-
+	
+	<div id="main">
+	
+	<?php
+	
+	if(isset($_GET['action'])){
+		Display::display_normal_message(Security::remove_XSS($_GET['message']), false);
+	}
+	
+	?>
+	<form method="POST" action="session_list.php">
+		<input type="text" name="keyword" value="<?php echo Security::remove_XSS($_GET['keyword']); ?>"/>
+		<input type="submit" value="<?php echo get_lang('Search'); ?>"/>
+		<a href="session_list.php?search=advanced"><?php echo get_lang('AdvancedSearch'); ?></a>
+		</form>
+	<form method="post" action="<?php echo api_get_self(); ?>?action=delete&sort=<?php echo $sort; ?>" onsubmit="javascript:if(!confirm('<?php echo get_lang('ConfirmYourChoice'); ?>')) return false;">
+	
 	<div align="left">
-
+	
 	<?php
-	if($page)
+	
+	if(count($Sessions)==0 && isset($_POST['keyword']))
 	{
-	?>
-
-	<a href="<?php echo api_get_self(); ?>?page=<?php echo $page-1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Previous'); ?></a>
-
-	<?php
+		echo get_lang('NoSearchResults');
 	}
 	else
 	{
-		echo get_lang('Previous');
-	}
-	?>
-
-	|
-
-	<?php
-	if($nbr_results > $limit)
-	{
-	?>
-
-	<a href="<?php echo api_get_self(); ?>?page=<?php echo $page+1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Next'); ?></a>
-
-	<?php
-	}
-	else
-	{
-		echo get_lang('Next');
-	}
-	?>
-
+		if($page)
+		{
+		?>
+	
+		<a href="<?php echo api_get_self(); ?>?page=<?php echo $page-1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Previous'); ?></a>
+	
+		<?php
+		}
+		else
+		{
+			echo get_lang('Previous');
+		}
+		?>
+	
+		|
+	
+		<?php
+		if($nbr_results > $limit)
+		{
+		?>
+	
+		<a href="<?php echo api_get_self(); ?>?page=<?php echo $page+1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Next'); ?></a>
+	
+		<?php
+		}
+		else
+		{
+			echo get_lang('Next');
+		}
+		?>
+	
+		</div>
+	
+		<br>
+	
+		<table class="data_table" width="100%">
+		<tr>
+		  <th>&nbsp;</th>
+		  <th><a href="<?php echo api_get_self(); ?>?sort=name"><?php echo get_lang('NameOfTheSession'); ?></a></th>
+		  <th><a href="<?php echo api_get_self(); ?>?sort=nbr_courses"><?php echo get_lang('NumberOfCourses'); ?></a></th>
+		  <th><a href="<?php echo api_get_self(); ?>?sort=date_start"><?php echo get_lang('StartDate'); ?></a></th>
+		  <th><a href="<?php echo api_get_self(); ?>?sort=date_end"><?php echo get_lang('EndDate'); ?></a></th>
+		  <th><a href="<?php echo api_get_self(); ?>?sort=coach_name"><?php echo get_lang('Coach'); ?></a></th>
+		  <th><?php echo get_lang('Actions'); ?></th>
+		</tr>
+	
+		<?php
+		$i=0;
+	
+		foreach($Sessions as $key=>$enreg)
+		{
+			if($key == $limit)
+			{
+				break;
+			}
+			$sql = 'SELECT COUNT(course_code) FROM '.$tbl_session_rel_course.' WHERE id_session='.intval($enreg['id']);
+	
+		  	$rs = api_sql_query($sql, __FILE__, __LINE__);
+		  	list($nb_courses) = Database::fetch_array($rs);
+	
+		?>
+	
+		<tr class="<?php echo $i?'row_odd':'row_even'; ?>">
+		  <td><input type="checkbox" name="idChecked[]" value="<?php echo $enreg['id']; ?>"></td>
+		  <td><a href="resume_session.php?id_session=<?php echo $enreg['id']; ?>"><?php echo htmlentities($enreg['name'],ENT_QUOTES,$charset); ?></a></td>
+		  <td><a href="session_course_list.php?id_session=<?php echo $enreg['id']; ?>"><?php echo $nb_courses; ?> cours</a></td>
+		  <td><?php echo htmlentities($enreg['date_start'],ENT_QUOTES,$charset); ?></td>
+		  <td><?php echo htmlentities($enreg['date_end'],ENT_QUOTES,$charset); ?></td>
+		  <td><?php echo htmlentities($enreg['firstname'],ENT_QUOTES,$charset).' '.htmlentities($enreg['lastname'],ENT_QUOTES,$charset); ?></td>
+		  <td>
+			<a href="add_users_to_session.php?page=session_list.php&id_session=<?php echo $enreg['id']; ?>"><img src="../img/add_user_big.gif" border="0" align="absmiddle" title="<?php echo get_lang('SubscribeUsersToSession'); ?>"></a>
+			<a href="add_courses_to_session.php?page=session_list.php&id_session=<?php echo $enreg['id']; ?>"><img src="../img/synthese_view.gif" border="0" align="absmiddle" title="<?php echo get_lang('SubscribeCoursesToSession'); ?>"></a>
+			<a href="session_edit.php?page=session_list.php&id=<?php echo $enreg['id']; ?>"><img src="../img/edit.gif" border="0" align="absmiddle" title="<?php echo get_lang('Edit'); ?>"></a>
+			<a href="<?php echo api_get_self(); ?>?sort=<?php echo $sort; ?>&action=delete&idChecked=<?php echo $enreg['id']; ?>" onclick="javascript:if(!confirm('<?php echo get_lang('ConfirmYourChoice'); ?>')) return false;"><img src="../img/delete.gif" border="0" align="absmiddle" title="<?php echo get_lang('Delete'); ?>" alt="<?php echo get_lang('Delete'); ?>"></a>
+		  </td>
+		</tr>
+	
+		<?php
+			$i=$i ? 0 : 1;
+		}
+	
+		unset($Sessions);
+	
+		?>
+	
+		</table>
+	
+		<br>
+	
+		<div align="left">
+	
+		<?php
+		if($page)
+		{
+		?>
+	
+		<a href="<?php echo api_get_self(); ?>?page=<?php echo $page-1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Previous'); ?></a>
+	
+		<?php
+		}
+		else
+		{
+			echo get_lang('Previous');
+		}
+		?>
+	
+		|
+	
+		<?php
+		if($nbr_results > $limit)
+		{
+		?>
+	
+		<a href="<?php echo api_get_self(); ?>?page=<?php echo $page+1; ?>&sort=<?php echo $sort; ?>&keyword=<?php echo $_REQUEST['keyword']; ?>"><?php echo get_lang('Next'); ?></a>
+	
+		<?php
+		}
+		else
+		{
+			echo get_lang('Next');
+		}
+		?>
+	
+		</div>
+	
+		<br>
+	
+		<select name="action">
+		<option value="delete"><?php echo get_lang('DeleteSelectedSessions'); ?></option>
+		</select>
+		<input type="submit" value="<?php echo get_lang('Ok'); ?>">
+		<?php } ?>
+	</table>
+	
 	</div>
 
-	<br>
-
-	<select name="action">
-	<option value="delete"><?php echo get_lang('DeleteSelectedSessions'); ?></option>
-	</select>
-	<input type="submit" value="<?php echo get_lang('Ok'); ?>">
-	<?php } ?>
-</table>
-
-</div>
-
 <?php
+
+}
 
 Display::display_footer();
 ?>

35d1e1bd9f4357edf23c28e69334c725df6b7216
Yannick Warnier <yannick.warnier@beeznest.com>
2008-11-26 21:27:15 +0100

    [svn r16961] Fix bug where broken image tag appears when showing forums list (see FS#3226)

--- main/forum/index.php
+++ main/forum/index.php
@@ -311,8 +311,10 @@ if ($_GET['action']!='add' && $_GET['action']!='edit' ) {
 						echo "\t<tr class=\"forum\">\n";
 						
 						// Showing the image
-						echo "\t\t<td width=\"50\">"; 
-						echo '<img src="'.api_get_path(WEB_COURSE_PATH).api_get_course_path().'/upload/forum/images/'.$forum['forum_image'].'">';
+						echo "\t\t<td width=\"50\">";
+						if(!empty($forum['forum_image'])) {
+							echo '<img src="'.api_get_path(WEB_COURSE_PATH).api_get_course_path().'/upload/forum/images/'.$forum['forum_image'].'">';
+						}
 						echo "</td>\n";
 					
 					

9a861ab188750db200734616476042bf0d0327d5
Isaac Flores <isaac.flores@beeznest.com>
2008-12-16 15:59:24 +0100

    [svn r17321] minor - logic changes - it show qualification of exercises in gradebook tool - (partial FS#3249)

--- main/gradebook/lib/be/exerciselink.class.php
+++ main/gradebook/lib/be/exerciselink.class.php
@@ -115,25 +115,21 @@ class ExerciseLink extends AbstractLink
     	$tbl_stats = Database::get_statistic_table(TABLE_STATISTIC_TRACK_E_EXERCICES);
     	$tbl_stats_e_attempt_recording = Database::get_statistic_table(TABLE_STATISTIC_TRACK_E_ATTEMPT_RECORDING);  
 		// for 1 student
-    	if (isset($stud_id))
-    	{    			
+    	if (isset($stud_id)) {    			
     		$sql = "SELECT DISTINCT  e.exe_id, exe_result, exe_weighting, exe_user_id
 					FROM $tbl_stats  as e INNER JOIN 
 					$tbl_stats_e_attempt_recording as r
 					ON (e.exe_id =r.exe_id)
 					WHERE e.exe_cours_id = '".$this->get_course_code()."'AND					
-					e.exe_id = '".$stud_id."'  AND author != ''  AND exe_user_id = '$stud_id' ORDER BY  e.exe_id DESC ";
+					author != ''  AND exe_user_id = '$stud_id' ORDER BY  e.exe_id DESC ";
 			$scores = api_sql_query($sql, __FILE__, __LINE__);
-    		if ($data=mysql_fetch_array($scores))
-    		{	
+    		if ($data=Database::fetch_array($scores)) {	
     			return array ($data['exe_result'], $data['exe_weighting']);
+       		} else {
+       			 return null;
        		}
-    		else
-    			return null;
-    	}    	
-    	// all students -> get average
-    	else
-    	{
+
+    	} else {// all students -> get average
     		// normal way of getting the info
 		
 			$sql = "SELECT DISTINCT  e.exe_id, exe_result, exe_weighting, exe_user_id
@@ -150,12 +146,9 @@ class ExerciseLink extends AbstractLink
 			$rescount = 0;
 			$sum = 0;
 
-			while ($data=mysql_fetch_array($scores))
-			{
-				if (!(array_key_exists($data['exe_user_id'],$students)))
-				{
-					if ($data['exe_weighting'] != 0)
-					{
+			while ($data=Database::fetch_array($scores)) {
+				if (!(array_key_exists($data['exe_user_id'],$students))) {
+					if ($data['exe_weighting'] != 0) {
 						$students[$data['exe_user_id']] = $data['exe_result'];
 						$rescount++;
 						$sum += ($data['exe_result'] / $data['exe_weighting']);
@@ -163,10 +156,12 @@ class ExerciseLink extends AbstractLink
 				}
 			}
 
-			if ($rescount == 0)
+			if ($rescount == 0) {
 				return null;
-			else
+			} else {
 				return array ($sum , $rescount);
+			}
+				
     	}
     }
     

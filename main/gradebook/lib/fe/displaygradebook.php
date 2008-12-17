90fdccd4cf638dab7d815890687e774b0e7c5461
Jan Derriks <j.derriks@hva.nl>
2008-12-17 17:25:10 +0100

    [svn r17357] FS#3365: The green "blackboard" image /img/tutorial.gif appeared right through the message text. One-line-style-fix

--- main/gradebook/lib/fe/displaygradebook.php
+++ main/gradebook/lib/fe/displaygradebook.php
@@ -78,8 +78,8 @@ class DisplayGradebook
 			$evalinfo .= '<br /><i>' . get_lang('NoResultsInEvaluation') . '</i>';
 		elseif ($scoredisplay->is_custom() && api_get_self() != '/dokeos/main/gradebook/gradebook_statistics.php')
 			$evalinfo .= '<br /><br /><a href="gradebook_statistics.php?selecteval='.Security::remove_XSS($_GET['selecteval']).'"> '. get_lang('ViewStatistics') . '</a>';
-		$evalinfo .= '</td><td align="right"><img src="../img/tutorial.gif"></img></td></table>';
-		Display :: display_normal_message($evalinfo,false);
+		$evalinfo .= '</td><td><img style="left:0;position:relative" src="../img/tutorial.gif"></img></td></table>';
+        Display :: display_normal_message($evalinfo,false);
 		echo $header;
 
 	}

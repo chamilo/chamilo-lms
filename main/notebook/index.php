79112aa61745af26d0de33012cae618e5957a157
Cristian Fasanando <cristian.fasanando@beeznest.com>
2008-12-23 22:57:15 +0100

    [svn r17444] minor - style change - I fixed a litle bug when you put your mouse on the icon new note didn't say New note see FS#3387

--- main/notebook/index.php
+++ main/notebook/index.php
@@ -33,8 +33,7 @@ $icon_delete ='delete.gif';
 
 //---------------------------------------------------------
 
-echo '<a href="index.php?action=addnotebook">'.Display::return_icon($icon_add,get_lang(' $tool = TOOL_NOTEBOOK;
- Display::display_header(get_lang(ucfirst($tool)));')).get_lang('NewNotebook').'</a>';
+echo '<a href="index.php?action=addnotebook">'.Display::return_icon($icon_add,get_lang('NewNotebook')).get_lang('NewNotebook').'</a>';
 
 if (isset($_REQUEST['action']) && $_REQUEST['action']=='addnotebook') {
 	echo '<div class="notebook-add-form" id="notebook-add">';			
@@ -42,7 +41,7 @@ if (isset($_REQUEST['action']) && $_REQUEST['action']=='addnotebook') {
 	echo '<input type="hidden" name="sec_token" value="'.$stok.'" />';
 	echo '<input type="hidden" name="action" value="addnotebook">';
 	echo '<div class="add-desc-notebook"><textarea class="style-add-textarea" rows="4" cols="95" name="description" maxlength="255" onfocus="this.value=\'\';document.getElementById(\'msg_add_error\').style.display=\'none\';"><<'.get_lang("WriteHereYourNote").'>></textarea></div>';
-	echo '<div class="action_notebook"><input type="button" value="Grabar" onclick="return add_notebook()"><input type="button" value="Cancelar" onclick="document.getElementById(\'notebook-add\').style.display = \'none\';document.getElementById(\'msg_add_error\').style.display=\'none\';"></div>';
+	echo '<div class="action_notebook"><input type="button" value="'.get_lang('Ok').'" onclick="return add_notebook()"><input type="button" value="'.get_lang('Cancel').'" onclick="document.getElementById(\'notebook-add\').style.display = \'none\';document.getElementById(\'msg_add_error\').style.display=\'none\';"></div>';
 	echo '<span class="msg_error" id="msg_add_error"></span>';
 	echo '</form>';				
 	echo '</div>';
@@ -111,7 +110,7 @@ while ($row_notebook_list=Database::fetch_array($notebook_list)){
 		echo '<form name="frm_edit_notebook" action="index.php" method="post"><input type="hidden" name="upd_notebook_id" value="'.$notebook_id.'"';
 		echo '<input type="hidden" name="sec_token" value="'.$stok.'" />';				
 		echo '<div class="upd-desc-notebook"><textarea class="style-edit-textarea" rows="4" cols="120"  name="upd_description" maxlength="255" onfocus="this.select()">'.$row_notebook_list['description'].'</textarea></div>';
-		echo '<div class="action_notebook"><input type="button" value="Grabar" onclick="edit_notebook()"><input type="button" value="Cancelar" onclick="edit_cancel_notebook()"></div>';
+		echo '<div class="action_notebook"><input type="button" value="'.get_lang('Ok').'" onclick="edit_notebook()"><input type="button" value="'.get_lang('Cancel').'" onclick="edit_cancel_notebook()"></div>';
 		echo '<span class="msg_error" id="msg_edit_error"></span>';
 		echo '</form></div>';	
 	} else {				
@@ -119,8 +118,7 @@ while ($row_notebook_list=Database::fetch_array($notebook_list)){
 		echo '<div class="notebook-term-action-links">';
 		echo '<span><a href="index.php?action=edit_notebook&notebook_id='.$row_notebook_list['notebook_id'].'#note-'.$row_notebook_list['notebook_id'].'" >'.Display::return_icon($icon_edit,get_lang('Edit')).'</a>&nbsp;';
 		echo '<a href="index.php?action=delete_notebook&notebook_id='.$row_notebook_list['notebook_id'].'" onclick="return confirmation(\''.$title.'\');">'.Display::return_icon($icon_delete,get_lang('Edit')).'</a></span>';
-		if ( $row_notebook_list['status']==1 ) {
-			
+		if ( $row_notebook_list['status']==1 ) {			
 			echo '&nbsp;&nbsp;<span class="date_information">'.get_lang('EndDate').'&nbsp;:&nbsp;'.$row_notebook_list['end_date'].'</span>';	
 		}			
 		echo '</div>';		

625271d4f75b30a752a8ed32ed4aafff3e1ee8cb
Juan Carlos Raña <juancarlos@innovasoc.com>
2009-01-02 21:50:07 +0100

    [svn r17507] previous commit plugin unchanged for later better identify the changes

---
+++ main/inc/lib/fckeditor/editor/plugins/ajaxfilemanager/inc/data.php
@@ -0,0 +1,8 @@
+<pre>Array
+(
+    [currentFolderPath] => ../uploaded/3/New Folder/
+    [new_folder] => sdfds
+)
+</pre>
+
+28/Aug/2008 22:57:37
---
+++ main/inc/lib/fckeditor/editor/plugins/ajaxfilemanager/session/gc_log.ajax.php
@@ -0,0 +1,2 @@
+<?php die(); ?>
+gc start at 31/Aug/2008 16:05:40

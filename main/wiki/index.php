29718cb7c64c687407ef294dc4ddc73850cca515
Juan Carlos Raña <juancarlos@innovasoc.com>
2009-01-16 14:36:49 +0100

    [svn r17772] restores removing features Wiki destroyed by 17769

--- main/wiki/index.php
+++ main/wiki/index.php
@@ -176,7 +176,7 @@ if(!api_is_allowed_to_edit())
 
 $fck_attribute['Width'] = '100%';
 $fck_attribute['Height'] = '400';
-$fck_attribute['ToolbarSet'] = 'Full';
+
 
 $fck_attribute['Config']['InDocument'] = false;
 $fck_attribute['Config']['CreateDocumentWebDir'] = api_get_path('WEB_COURSE_PATH').api_get_course_path().'/document/';

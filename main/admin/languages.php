48f4494bb45e53164eacf0364d4669dc6df231bb
Carlos Vargas <carlos.vargas@beeznest.com>
2009-01-19 18:50:59 +0100

    [svn r17830] minor correctly lang see FS#3458

--- main/admin/languages.php
+++ main/admin/languages.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: languages.php 17825 2009-01-19 16:38:37Z cvargas1 $
+// $Id: languages.php 17830 2009-01-19 17:50:59Z cvargas1 $
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
@@ -183,9 +183,9 @@ while ($row = Database::fetch_array($result_select)) {
 	$row_td[] = $row['dokeos_folder'];
 	
 	if ($row['english_name'] == $row_lang['selected_value']){
-		$setplatformlanguage = Display::return_icon('links.gif', get_lang('CurrentLanguage'));
+		$setplatformlanguage = Display::return_icon('links.gif', get_lang('CurrentLanguagesPortal'));
 	} else {		
-		$setplatformlanguage = "<a href=\"javascript:if (confirm('".get_lang('SetThisLanguage')."')) { location.href='".api_get_self()."?action=setplatformlanguage&id=".$row['id']."'; }\">".Display::return_icon('link_na.gif', get_lang('CurrentLanguage'))."</a>";
+		$setplatformlanguage = "<a href=\"javascript:if (confirm('".get_lang('AreYouSureYouWantToSetThisLanguageAsThePortalDefault')."')) { location.href='".api_get_self()."?action=setplatformlanguage&id=".$row['id']."'; }\">".Display::return_icon('link_na.gif')."</a>";
 	}		
 	if ($row['available'] == 1) {
 		$row_td[] = "<a href='".api_get_self()."?action=makeunavailable&id=".$row['id']."'>".Display::return_icon('visible.gif')."</a> <a href='".api_get_self()."?action=edit&id=".$row['id']."#value'>".Display::return_icon('edit.gif', get_lang('Edit'))."</a>&nbsp;".$setplatformlanguage;

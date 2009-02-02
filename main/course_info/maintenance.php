fc0f333ebd09b673e235faf971495ff19be65817
Juan Carlos Raña <juancarlos@innovasoc.com>
2009-02-02 16:52:42 +0100

    [svn r18148] fix several texts

--- main/course_info/maintenance.php
+++ main/course_info/maintenance.php
@@ -48,10 +48,10 @@ api_display_tool_title($nameTools);
 ?>
 
 
-<div class="sectiontitle"><?php Display::display_icon('tool_delete.gif'); ?>&nbsp;&nbsp;<a href="../course_info/delete_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("DelCourse");?></a></div>
+<div class="sectiontitle"><?php Display::display_icon('tool_delete.gif',get_lang("DelCourse")); ?>&nbsp;&nbsp;<a href="../course_info/delete_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("DelCourse");?></a></div>
 <div class="sectioncomment"><?php echo get_lang("DescriptionDeleteCourse");	?></div>	
 
-<div class="sectiontitle"><?php Display::display_icon('save_import.gif'); ?>&nbsp;&nbsp;<a href="../coursecopy/backup.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("backup");?></a></div>
+<div class="sectiontitle"><?php Display::display_icon('save_import.gif', get_lang("backup")); ?>&nbsp;&nbsp;<a href="../coursecopy/backup.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("backup");?></a></div>
 <div class="sectioncomment">
 			<ul>
 			    <li><a href="../coursecopy/create_backup.php?<?php echo api_get_cidreq();?>"><?php echo get_lang('CreateBackup')  ?></a><br/>
@@ -63,10 +63,10 @@ api_display_tool_title($nameTools);
 		    </ul>
 </div>
 			
-<div class="sectiontitle"><?php Display::display_icon('empty.gif'); ?>&nbsp;&nbsp;<a href="../coursecopy/recycle_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("recycle_course");?></a></div>
+<div class="sectiontitle"><?php Display::display_icon('empty.gif', get_lang("recycle_course")); ?>&nbsp;&nbsp;<a href="../coursecopy/recycle_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("recycle_course");?></a></div>
 <div class="sectioncomment"><?php echo get_lang("DescriptionRecycleCourse");?></div>
 
-<div class="sectiontitle"><?php Display::display_icon('copy.gif'); ?>&nbsp;&nbsp;<a href="../coursecopy/copy_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("CopyCourse");?></a></div>
+<div class="sectiontitle"><?php Display::display_icon('copy.gif', get_lang("CopyCourse")); ?>&nbsp;&nbsp;<a href="../coursecopy/copy_course.php?<?php echo api_get_cidreq();?>"><?php echo get_lang("CopyCourse");?></a></div>
 <div class="sectioncomment"><?php echo get_lang("DescriptionCopyCourse"); ?></div>
 
 <?php

2090efca2be3920dda79285205205f131540c49d
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-02-03 23:43:23 +0100

    [svn r18216] FS#2867 - FCKEditor, audio plugin: Adding ' (mp3)' suffix to the title, so people to know what type of files the plugin deals with.

--- main/inc/lib/fckeditor/editor/plugins/audio/fckplugin.js
+++ main/inc/lib/fckeditor/editor/plugins/audio/fckplugin.js
@@ -1,10 +1,10 @@
 // Register the related commands.
 var dialogPath = FCKConfig.PluginsPath + 'audio/fck_audio.html' ;
-var flashDialogCmd = new FCKDialogCommand( FCKLang["DlgAudioTitle"], FCKLang["DlgAudioTitle"], dialogPath, 600, 300 ) ;
+var flashDialogCmd = new FCKDialogCommand( FCKLang["DlgAudioTitle"], FCKLang["DlgAudioTitle"] + ' (mp3)', dialogPath, 600, 300 ) ;
 FCKCommands.RegisterCommand( 'MP3', flashDialogCmd ) ;
 
 // Create the Audio toolbar button.
-var oFlashItem		= new FCKToolbarButton( 'MP3', FCKLang["DlgAudioTitle"]) ;
+var oFlashItem		= new FCKToolbarButton( 'MP3', FCKLang["DlgAudioTitle"] + ' (mp3)') ;
 oFlashItem.IconPath	= FCKConfig.PluginsPath + 'audio/audio.gif' ;
 
 FCKToolbarItems.RegisterItem( 'MP3', oFlashItem ) ;

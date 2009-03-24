7794184f767f59c9b7b07e10fc925204017511f6
Arthur Portugal <yukiportu@msn.com>
2009-03-24 21:05:28 +0100

    [svn r19245] Changed the name variable Gradebook_edit to ModifyEvaluation and added AddEvaluation

--- main/gradebook/lib/fe/evalform.class.php
+++ main/gradebook/lib/fe/evalform.class.php
@@ -283,7 +283,7 @@ class EvalForm extends FormValidator
 		} else {
 			$this->addElement('checkbox', 'addresult', get_lang('AddResult'));
 		}
-		$this->addElement('submit', null, get_lang('Add'));
+		$this->addElement('style_submit_button', 'submit', get_lang('AddEvaluation'),'class="add"');
 	}
 	/**
 	 * Builds a form to edit an evaluation
@@ -294,7 +294,7 @@ class EvalForm extends FormValidator
 		$id_current=isset($this->id)?$this->id :null;
 		$this->addElement('hidden', 'hid_id',$id_current);
 		$this->build_basic_form(1);
-		$this->addElement('style_submit_button', 'submit', get_lang('Gradebook_edit'),'class="save"');
+		$this->addElement('style_submit_button', 'submit', get_lang('ModifyEvaluation'),'class="save"');
 	}
 	/**
 	 * Builds a basic form that is used in add and edit

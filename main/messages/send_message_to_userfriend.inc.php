a5ed48491684229ba81c8203a48acc5f07812047
Yannick Warnier <yannick.warnier@beeznest.com>
2009-04-13 01:50:35 +0200

    [svn r19723] Fixed CSS class name Added comments Removed deprecated code

--- main/messages/send_message_to_userfriend.inc.php
+++ main/messages/send_message_to_userfriend.inc.php
@@ -1,28 +1,5 @@
-<?php
-/*
-==============================================================================
-	Dokeos - elearning and course management software
-
-	Copyright (c) 2009 Dokeos SPRL
-	Copyright (c) 2009 Julio Montoya Armas <gugli100@gmail.com>
-	Copyright (c) Facultad de Matematicas, UADY (México)
-	Copyright (c) Evie, Free University of Brussels (Belgium)	
-	Copyright (c) 2009 Isaac Flores Paz <isaac.flores@dokeos.com>	
-
-	For a full list of contributors, see "credits.txt".
-	The full license can be read in "license.txt".
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	See the GNU General Public License for more details.
-
-    Contact address: Dokeos, rue du Corbeau, 108, B-1030 Brussels, Belgium
-    Mail: info@dokeos.com
-==============================================================================
-*/
+<?php //$id: $
+/* For licensing terms, see /dokeos_license.txt */
 $language_file = array('registration','messages','userInfo','admin');
 $cidReset=true;
 include_once ('../inc/global.inc.php');
@@ -60,17 +37,13 @@ if ( isset($_REQUEST['user_friend']) ) {
 			}
 			?>
 		</div>
-<div class="message-conten-body-right">
+<div class="message-content-body-right">
 <div id="id_content_panel_init">
 			<dl>
 <?php 
-		/*if ($panel == 1) {
-		<dd><a href="javascript:void(0)" onclick="change_panel('2','<?php echo $userfriend_id; ?>')"><?php echo mb_convert_encoding(get_lang('SendInviteMessage'),'UTF-8',$charset);?></a></dd>' .
-		}
-		<input type="button" value="<?php echo mb_convert_encoding(get_lang('NewMessage'),'UTF-8',$charset); ?>" onclick="hide_display_message()" />&nbsp;&nbsp;&nbsp;
-		**/		
 		if (api_get_setting('allow_message_tool')=='true') {
 			if ($panel == 1) {
+                //normal message
 		   		 $user_info=api_get_user_info($userfriend_id);
 		  		 echo mb_convert_encoding(get_lang('To'),'UTF-8',$charset); ?> :&nbsp;&nbsp;&nbsp;&nbsp;<?php echo mb_convert_encoding($user_info['firstName'].' '.$user_info['lastName'],'UTF-8',$charset); ?>
 		  		 <br/>
@@ -79,17 +52,15 @@ if ( isset($_REQUEST['user_friend']) ) {
 		   		 <br /><br />		    	  
 		   		 <input type="button" value="<?php echo get_lang('SendMessage'); ?>" onclick="action_database_panel('5','<?php echo $userfriend_id;?>')" />
 <?php
-			} else {				
+			} else {
+                // friend invitation message
 				echo mb_convert_encoding(get_lang('AddPersonalMessage'),'UTF-8',$charset);  ?> :<br/><br/>
 				<textarea id="txt_area_invite" rows="5" cols="41"></textarea><br /><br />
 						    
 		    	<input type="button" value="<?php echo mb_convert_encoding(get_lang('SocialAddToFriends'),'UTF-8',$charset); ?>" onclick="action_database_panel('4','<?php echo $userfriend_id;?>')" />
 <?php					
 				}
-			}
-			if (!isset($_REQUEST['view'])) {
-				//<dd><a href="main/social/index.php#remote-tab-5"> echo get_lang('SocialSeeContacts'); </a></dd>
-			}
+			}            
 ?>
 			</dl>			
 </div>

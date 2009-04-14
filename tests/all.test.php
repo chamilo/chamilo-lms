aefbbb671c78323ca63c1cad4c663c362ddbfda6
Yannick Warnier <yannick.warnier@beeznest.com>
2009-04-14 20:35:49 +0200

    [svn r19764] Triggering UserManager test instead of dummy tests

--- tests/all.test.php
+++ tests/all.test.php
@@ -1,11 +1,11 @@
 <?php //$id$
-//require_once('simpletest/autorun.php');
+require_once('../main/inc/global.inc.php');
+require_once('simpletest/autorun.php');
 
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests');
-        $this->addTestFile(dirname(__FILE__).'/dummy.test.php');
-        $this->addTestFile(dirname(__FILE__).'/dummy2.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
     }
 }
 ?>

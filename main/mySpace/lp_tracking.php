962337453d44244c0c857a77652b82c65daeff61
Julio Montoya <gugli100@gmail.com>
2009-04-30 00:37:19 +0200

    [svn r20203] Adding security::remove_XSS and escape_string functions

--- main/mySpace/lp_tracking.php
+++ main/mySpace/lp_tracking.php
@@ -3,7 +3,7 @@
 ==============================================================================
 	Dokeos - elearning and course management software
 
-	Copyright (c) 2008 Dokeos SPRL
+	Copyright (c) 2008-2009 Dokeos SPRL
 
 	For a full list of contributors, see "credits.txt".
 	The full license can be read in "license.txt".
@@ -48,9 +48,7 @@ $user_id = intval($_GET['student_id']);
 $user_infos = UserManager :: get_user_info_by_id($user_id);
 $name = $user_infos['firstname'].' '.$user_infos['lastname'];
 
-
-if(!api_is_platform_admin(true) && !CourseManager :: is_course_teacher($_user['user_id'], $_GET['course']) && !Tracking :: is_allowed_to_coach_student($_user['user_id'],$_GET['student_id']) && $user_infos['hr_dept_id']!==$_user['user_id'])
-{
+if(!api_is_platform_admin(true) && !CourseManager :: is_course_teacher($_user['user_id'], $_GET['course']) && !Tracking :: is_allowed_to_coach_student($_user['user_id'],$_GET['student_id']) && $user_infos['hr_dept_id']!==$_user['user_id']) {
 	Display::display_header('');
 	api_not_allowed();
 	Display::display_footer();
@@ -58,28 +56,22 @@ if(!api_is_platform_admin(true) && !CourseManager :: is_course_teacher($_user['u
 
 $_course = CourseManager :: get_course_information($_GET['course']);
 $_course['dbNameGlu'] = $_configuration['table_prefix'] . $_course['db_name'] . $_configuration['db_glue'];
-$cidReq = $_GET['course'];
+$cidReq = Security::remove_XSS($_GET['course']);
 
-if(!empty($_GET['origin']) && $_GET['origin'] == 'user_course')
-{
+if(!empty($_GET['origin']) && $_GET['origin'] == 'user_course') {
 	$interbreadcrumb[] = array ("url" => api_get_path(WEB_COURSE_PATH).$_course['directory'], 'name' => $_course['title']);
-	$interbreadcrumb[] = array ("url" => "../user/user.php?cidReq=".$_GET['course'], "name" => get_lang("Users"));
-}
-else if(!empty($_GET['origin']) && $_GET['origin'] == 'tracking_course')
-{
+	$interbreadcrumb[] = array ("url" => "../user/user.php?cidReq=".Security::remove_XSS($_GET['course']), "name" => get_lang("Users"));
+} else if(!empty($_GET['origin']) && $_GET['origin'] == 'tracking_course') {
 	$interbreadcrumb[] = array ("url" => api_get_path(WEB_COURSE_PATH).$_course['directory'], 'name' => $_course['title']);
-	$interbreadcrumb[] = array ("url" => "../tracking/courseLog.php?cidReq=".$_GET['course'].'&studentlist=true&id_session='.$_SESSION['id_session'], "name" => get_lang("Tracking"));
-}
-else
-{
+	$interbreadcrumb[] = array ("url" => "../tracking/courseLog.php?cidReq=".Security::remove_XSS($_GET['course']).'&studentlist=true&id_session='.$_SESSION['id_session'], "name" => get_lang("Tracking"));
+} else {
 	$interbreadcrumb[] = array ("url" => "index.php", "name" => get_lang('MySpace'));
 	$interbreadcrumb[] = array ("url" => "student.php", "name" => get_lang("MyStudents"));
- 	$interbreadcrumb[] = array ("url" => "myStudents.php?student=".$_GET['student_id'], "name" => get_lang("StudentDetails"));
+ 	$interbreadcrumb[] = array ("url" => "myStudents.php?student=".Security::remove_XSS($_GET['student_id']), "name" => get_lang("StudentDetails"));
  	$nameTools=get_lang("DetailsStudentInCourse");
 }
 
-$interbreadcrumb[] = array("url" => "myStudents.php?student=".$_GET['student_id']."&course=".$_GET['course']."&details=true&origin=".$_GET['origin'] , "name" => get_lang("DetailsStudentInCourse"));
-
+$interbreadcrumb[] = array("url" => "myStudents.php?student=".Security::remove_XSS($_GET['student_id'])."&course=".Security::remove_XSS($_GET['course'])."&details=true&origin=".Security::remove_XSS($_GET['origin']) , "name" => get_lang("DetailsStudentInCourse"));
 $nameTools = get_lang('LearningPathDetails');
 
 $htmlHeadXtra[] = '
@@ -148,36 +140,29 @@ $lp_id = intval($_GET['lp_id']);
 
 $sql = 'SELECT name 
 		FROM '.Database::get_course_table(TABLE_LP_MAIN, $_course['db_name']).'
-		WHERE id='.$lp_id;
+		WHERE id='.Database::escape_string($lp_id);
 $rs = api_sql_query($sql, __FILE__, __LINE__);
-$lp_title = Database::result($rs, 0, 0);
-	
+$lp_title = Database::result($rs, 0, 0);	
 
 echo '<div class ="actions"><div align="left" style="float:left;margin-top:2px;" ><strong>'.$_course['title'].' - '.$lp_title.' - '.$name.'</strong></div>
 	  <div  align="right">
 			<a href="#" onclick="window.print()"><img align="absbottom" src="../img/printmgr.gif">&nbsp;'.get_lang('Print').'</a>
-			<a href="'.api_get_self().'?export=csv&'.$_SERVER['QUERY_STRING'].'"><img align="absbottom" src="../img/excel.gif">&nbsp;'.get_lang('ExportAsCSV').'</a>
+			<a href="'.api_get_self().'?export=csv&'.Security::remove_XSS($_SERVER['QUERY_STRING']).'"><img align="absbottom" src="../img/excel.gif">&nbsp;'.get_lang('ExportAsCSV').'</a>
 		 </div></div>
 	<div class="clear"></div>';
 
 $list = learnpath :: get_flat_ordered_items_list($lp_id);
-
 $origin = 'tracking';
 
-
-if($export_csv)
-{	
+if($export_csv) {	
 	include_once('../newscorm/lp_stats.php');	
 	//Export :: export_table_csv($csv_content, 'reporting_student');
-}
-else
-{
+} else {
 	ob_start();
 	include_once('../newscorm/lp_stats.php');
 	$tracking_content = ob_get_contents();
 	ob_end_clean();
-	echo utf8_decode($tracking_content);   
-	
+	echo utf8_decode($tracking_content);
 }
 Display :: display_footer();
 ?>

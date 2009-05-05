28bf1e09eb93cad4444278b0c9b15e456bd97011
Julio Montoya <gugli100@gmail.com>
2009-05-05 16:13:56 +0200

    [svn r20327] Minor - UI improvement

--- main/survey/survey.lib.php
+++ main/survey/survey.lib.php
@@ -24,7 +24,7 @@
 *	@package dokeos.survey
 * 	@author Patrick Cool <patrick.cool@UGent.be>, Ghent University: cleanup, refactoring and rewriting large parts (if not all) of the code
 	@author Julio Montoya Armas <gugli100@gmail.com>, Dokeos: Personality Test modification and rewriting large parts of the code
-* 	@version $Id: survey.lib.php 20306 2009-05-04 23:22:40Z iflorespaz $
+* 	@version $Id: survey.lib.php 20327 2009-05-05 14:13:56Z juliomontoya $
 *
 * 	@todo move this file to inc/lib
 * 	@todo use consistent naming for the functions (save vs store for instance)
@@ -4394,10 +4394,10 @@ class SurveyUtil {
 		$table->set_header(7, get_lang('AvailableUntill'));
 		$table->set_header(8, get_lang('Invite'));
 		$table->set_header(9, get_lang('Anonymous'));
-		$table->set_header(10, get_lang('Modify'), false,'width="130"');
+		$table->set_header(10, get_lang('Modify'), false,'width="150"');
 		$table->set_column_filter(9, 'anonymous_filter');
 		$table->set_column_filter(10, 'modify_filter');
-			$table->set_form_actions(array ('delete' => get_lang('DeleteSurvey')));
+		$table->set_form_actions(array ('delete' => get_lang('DeleteSurvey')));
 		$table->display();	
 	}
 	

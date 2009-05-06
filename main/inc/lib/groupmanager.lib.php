e37bfb5afaad383505198510f9067b553704419b
Noël Dieschburg <noel@cblue.be>
2009-05-06 19:54:50 +0200

    [svn r20370] Suppression of the mysql error when using student view

--- main/inc/lib/groupmanager.lib.php
+++ main/inc/lib/groupmanager.lib.php
@@ -99,6 +99,7 @@ class GroupManager
 	function get_group_list($category = null, $course_code = null)
 	{
 		global $_user;
+		$isStudentView  = $_REQUEST['isStudentView'];
 		$course_db = '';
 		$my_user_id=api_get_user_id();
 		$my_status_of_user_in_course='';
@@ -142,7 +143,7 @@ class GroupManager
 					LEFT JOIN ".$table_group_user." `ug2`
 					ON `ug2`.`group_id` = `g`.`id`";
 					
-		} elseif ($my_status_of_user_in_course==STUDENT || $is_student_in_session===true) {
+		} elseif ($my_status_of_user_in_course==STUDENT || $is_student_in_session===true || $isStudentView == 'true') {
 						$sql = "SELECT  g.id ,
 						g.name ,
 						g.description ,

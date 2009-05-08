2dad6c08b06a597abfca90ec1867bd3601b57017
Juan Carlos Raña <juancarlos@innovasoc.com>
2009-05-08 22:45:38 +0200

    [svn r20425] fix icon text

--- main/user/userInfo.php
+++ main/user/userInfo.php
@@ -1,4 +1,4 @@
-<?php // $Id: userInfo.php 20261 2009-05-04 03:10:45Z iflorespaz $
+<?php // $Id: userInfo.php 20425 2009-05-08 20:45:38Z herodoto $
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
@@ -281,16 +281,16 @@ if ($allowedToEditContent)
 */
 // Back button for each display mode (Top)
 echo '<div class="actions">';
-echo '<a href="user.php?'.api_get_cidreq().'&amp;origin='.$origin.'">'.Display::return_icon('members.gif').get_lang('BackUser').'</a>';
+echo '<a href="user.php?'.api_get_cidreq().'&amp;origin='.$origin.'">'.Display::return_icon('members.gif',get_lang('BackUser')).get_lang('BackUser').'</a>';
 if (!is_numeric($_GET['editMainUserInfo']))
 {
-	echo '<a href="userInfo.php?'.api_get_cidreq().'&amp;origin='.$origin.'&amp;editMainUserInfo='.$userIdViewed.'">'.Display::return_icon('edit.gif').get_lang('EditUser').'</a>';
+	echo '<a href="userInfo.php?'.api_get_cidreq().'&amp;origin='.$origin.'&amp;editMainUserInfo='.$userIdViewed.'">'.Display::return_icon('edit.gif',get_lang('EditUser')).get_lang('EditUser').'</a>';
 }
 else 
 {
-	echo '<a href="userInfo.php?'.api_get_cidreq().'&amp;origin='.$origin.'&amp;uInfo='.$userIdViewed.'">'.Display::return_icon('edit.gif').get_lang('ViewUser').'</a>';
+	echo '<a href="userInfo.php?'.api_get_cidreq().'&amp;origin='.$origin.'&amp;uInfo='.$userIdViewed.'">'.Display::return_icon('edit.gif',get_lang('ViewUser')).get_lang('ViewUser').'</a>';
 }
-echo '<a href="../mySpace/myStudents.php?'.api_get_cidreq().'&amp;origin=user_course&amp;student='.$userIdViewed.'&amp;details=true&amp;course='.$_course['id'].'">'.Display::return_icon('statistics.gif').get_lang('UserStatistics').'</a>';
+echo '<a href="../mySpace/myStudents.php?'.api_get_cidreq().'&amp;origin=user_course&amp;student='.$userIdViewed.'&amp;details=true&amp;course='.$_course['id'].'">'.Display::return_icon('statistics.gif',get_lang('UserStatistics')).get_lang('UserStatistics').'</a>';
 echo '</div>';
 
 

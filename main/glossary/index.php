be4d4264af3a10dbe2136551b1d416a895a4c1a7
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-05-10 14:33:46 +0200

    [svn r20453] FS#306 - The Glossary tool: A modification for better encoding support.

--- main/glossary/index.php
+++ main/glossary/index.php
@@ -35,7 +35,7 @@ event_access_tool(TOOL_GLOSSARY);
 
 if ( isset($_GET['action']) && ($_GET['action'] == 'addglossary' || $_GET['action'] == 'edit_glossary')) {
 $tool=get_lang('GlossaryManagement');
-$interbreadcrumb[] = array ("url"=>"index.php", "name"=> ucfirst(get_lang(TOOL_GLOSSARY)));
+$interbreadcrumb[] = array ("url"=>"index.php", "name"=> api_ucfirst(get_lang(TOOL_GLOSSARY)));
 }
 
 Display::display_header(get_lang(ucfirst($tool)));

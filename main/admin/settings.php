b338b7ef8ffc9a39ef2cf40964df39ea1991a35b
Julio Montoya <gugli100@gmail.com>
2009-05-12 00:20:34 +0200

    [svn r20509] Load CSS stylesheet name from the language variable

--- main/admin/settings.php
+++ main/admin/settings.php
@@ -1,4 +1,4 @@
-<?php // $Id: settings.php 20508 2009-05-11 22:14:38Z juliomontoya $
+<?php // $Id: settings.php 20509 2009-05-11 22:20:34Z juliomontoya $
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
@@ -642,7 +642,7 @@ function handle_stylesheets()
 					{
 						$selected = '';
 					}
-					$show_name=ucwords(str_replace('_',' ', $style_dir));
+					$show_name=get_lang(str_replace(' ','', ucwords(str_replace('_',' ', $style_dir))));
 					
 					if ($is_style_changeable)
 					{					

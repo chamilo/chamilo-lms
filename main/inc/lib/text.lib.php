93dff3d1ee868fefbfa652010c873cbd7b9db259
Cristian Fasanando <cristian.fasanando@beeznest.com>
2009-05-13 02:06:48 +0200

    [svn r20574] Remove part of license text (shortcut to common license)

--- main/inc/lib/text.lib.php
+++ main/inc/lib/text.lib.php
@@ -1,26 +1,5 @@
-<?php // $Id: text.lib.php 20573 2009-05-13 00:05:49Z cfasanando $
-/*
-==============================================================================
-	Dokeos - elearning and course management software
-
-	Copyright (c) 2004-2009 Dokeos S.A.
-	Copyright (c) 2003 Ghent University (UGent)
-	Copyright (c) 2001 Universite catholique de Louvain (UCL)
-	Copyright (c) various contributors
-
-	For a full list of contributors, see "credits.txt".
-	The full license can be read in "license.txt".
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	See the GNU General Public License for more details.
-
-	Contact: Dokeos, 181 rue Royale, B-1000 Brussels, Belgium, info@dokeos.com
-==============================================================================
-*/
+<?php // $Id: text.lib.php 20574 2009-05-13 00:06:48Z cfasanando $
+/* For licensing terms, see /dokeos_license.txt */
 /**
 ==============================================================================
 *	This is the text library for Dokeos.

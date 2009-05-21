8bab29ce8e721d1ebcaf0999d6594f8cb5f0927f
Yannick Warnier <yannick.warnier@beeznest.com>
2009-05-21 21:36:08 +0200

    [svn r20934] Minor - base for main_api simple-testing

---
+++ tests/main/inc/lib/main_api.lib.test.php
@@ -0,0 +1,27 @@
+<?php
+require_once(api_get_path(LIBRARY_PATH).'main_api.lib.php');
+
+class TestMainApi extends UnitTestCase {
+	
+	function TestMainApi() {
+        $this->UnitTestCase('Main API tests');
+    }
+
+// todo function testApiProtectCourseScriptReturnsFalse()
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+// todo function test_
+	
+//	function testApiGetSettingReturnsRightValue() {
+	 	//$this->assertPattern('/\d/',$res);
+//	}
+}

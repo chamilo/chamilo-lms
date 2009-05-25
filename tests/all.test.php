f0e74ff55189e40c061de15d86bf6dd2bcbbe48a
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2009-05-25 22:17:21 +0200

    [svn r20978] Minor - editing all.test.php  added $test =$new Alltests()

--- tests/all.test.php
+++ tests/all.test.php
@@ -1,13 +1,17 @@
 <?php //$id$
 require_once('../main/inc/global.inc.php');
 $_SESSION['_user']['user_id'] = 1;
-require('../main/inc/global.inc.php');
+require_once('../main/inc/global.inc.php');
 require_once('simpletest/autorun.php');
 
 class AllTests extends TestSuite {
     function AllTests() {
         $this->TestSuite('All tests');
-        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
+        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
+      
     }
 }
+$test = &new AllTests();
+//$test-> run( new HtmlReporter());
 ?>

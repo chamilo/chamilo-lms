746b937469abb160199dc8d788e87fa0cf8c5a4c
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2009-05-26 17:41:41 +0200

    [svn r20992] Minor - modified all.test.php  add $test =&new AllTetst();

--- tests/all.test.php
+++ tests/all.test.php
@@ -1,4 +1,4 @@
-<?php //$id$
+<?php
 require_once('../main/inc/global.inc.php');
 $_SESSION['_user']['user_id'] = 1;
 require_once('../main/inc/global.inc.php');
@@ -9,7 +9,7 @@ class AllTests extends TestSuite {
         $this->TestSuite('All tests');
         //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
         $this->addTestFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
-      
+       // $this->assertTrue(file_exists('/temp/test.log'));
     }
 }
 $test = &new AllTests();

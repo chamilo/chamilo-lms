e0a027377e89a25d4c97e87fe22a2f0275f8f959
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2009-05-28 01:21:56 +0200

    [svn r21027] Minor - added new require_once(api_get_path(SYS_CODE_PATH) . 'permissions/permissions_functions.inc.php and require_once(api_get_path(LIBRARY_PATH) . "/groupmanager.lib.php)

--- tests/all.test.php
+++ tests/all.test.php
@@ -1,15 +1,20 @@
 <?php
+require_once('simpletest/unit_tester.php');
 require_once('../main/inc/global.inc.php');
 $_SESSION['_user']['user_id'] = 1;
-require_once('../main/inc/global.inc.php');
+require('../main/inc/global.inc.php');
+require_once('main/inc/lib/main_api.lib.test.php'); 
+require_once(api_get_path(SYS_CODE_PATH) . 'permissions/permissions_functions.inc.php');
+require_once(api_get_path(LIBRARY_PATH) . "/groupmanager.lib.php");
 require_once('simpletest/autorun.php');
 
 class AllTests extends TestSuite {
+    
     function AllTests() {
         $this->TestSuite('All tests');
         //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
-        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
-       // $this->assertTrue(file_exists('/temp/test.log'));
+        $this->addFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
+        //$this->assertTrue(file_exists('/temp/test.log'));
     }
 }
 $test = &new AllTests();

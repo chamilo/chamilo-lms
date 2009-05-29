e986b0aef9e8a3e6a251729d82dc487deaafa674
Carlos Vargas <carlos.vargas@beeznest.com>
2009-05-29 21:11:13 +0200

    [svn r21089] show only one icon table or list in glossary see FS#4232

--- main/glossary/index.php
+++ main/glossary/index.php
@@ -326,8 +326,11 @@ function display_glossary()
 	{
 		echo '<a href="index.php?'.api_get_cidreq().'&action=addglossary&msg=add">'.Display::return_icon('filenew.gif',get_lang('TermAddNew')).get_lang('TermAddNew').'</a>';
 	}
-	echo '<a href="index.php?'.api_get_cidreq().'&action=changeview&view=list">'.Display::return_icon('view_list.gif',get_lang('ListView')).get_lang('ListView').'</a>';
-	echo '<a href="index.php?'.api_get_cidreq().'&action=changeview&view=table">'.Display::return_icon('view_table.gif',get_lang('TableView')).get_lang('TableView').'</a>';
+	if ($_GET['view']=='table'){
+		echo '<a href="index.php?'.api_get_cidreq().'&action=changeview&view=list">'.Display::return_icon('view_list.gif',get_lang('ListView')).get_lang('ListView').'</a>';
+	} else {
+		echo '<a href="index.php?'.api_get_cidreq().'&action=changeview&view=table">'.Display::return_icon('view_table.gif',get_lang('TableView')).get_lang('TableView').'</a>';
+	}
 	echo '</div>';	
 	
 	if (!$_SESSION['glossary_view'] OR $_SESSION['glossary_view'] == 'table')

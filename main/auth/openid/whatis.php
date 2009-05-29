96325a89df2f7fe3502c70a5f447320487e00fd7
Isaac Flores <isaac.flores@beeznest.com>
2009-05-29 22:23:45 +0200

    [svn r21092] Minor - logic changes - added remove XSS in file whatis.php - (partial FS#4261)

--- main/auth/openid/whatis.php
+++ main/auth/openid/whatis.php
@@ -8,7 +8,11 @@ Display::display_header('OpenID', NULL);
 <?php echo get_lang('OpenIDDescription');?>
 </p>
 <p>
-<a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Return to the previous page</a>
+<a href="<?php
+ if (strpos($_SERVER['HTTP_REFERER'],$_SERVER['HTTP_HOST'])) {
+ 	 echo Security::remove_XSS($_SERVER['HTTP_REFERER']); 
+ } 
+ ?>">Return to the previous page</a>
 </p>
 <?php
 Display::display_footer();

5a95e98b16c0069bfbea3277f2580840a69b4c1c
Yannick Warnier <yannick.warnier@beeznest.com>
2009-06-01 03:51:43 +0200

    [svn r21153] Fixed syntax error due to missing space after opening php tag

--- main/gradebook/gradebook_add_user.php
+++ main/gradebook/gradebook_add_user.php
@@ -1,29 +1,6 @@
-<?php// $Id: $
-/*
-==============================================================================
-	Dokeos - elearning and course management software
-
-	Copyright (c) 2008 Dokeos Latinoamerica SAC
-	Copyright (c) 2006 Dokeos SPRL
-	Copyright (c) 2006 Ghent University (UGent)
-	Copyright (c) various contributors
-
-	For a full list of contributors, see "credits.txt".
-	The full license can be read in "license.txt".
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	See the GNU General Public License for more details.
-
-	Contact address: Dokeos, rue du Corbeau, 108, B-1030 Brussels, Belgium
-	Mail: info@dokeos.com
-==============================================================================
-*/
+<?php //$Id: gradebook_add_user.php 21153 2009-06-01 01:51:43Z yannoo $
+/* For licensing terms, see /dokeos_license.txt */
 $language_file = 'gradebook';
-//$cidReset = true;
 require_once '../inc/global.inc.php';
 $this_section = SECTION_MYGRADEBOOK;
 require_once ('lib/be.inc.php');

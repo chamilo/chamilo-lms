23bb5f5edfd615b7c0a9e349f2c3ee13c1b32904
Arthur Portugal <yukiportu@msn.com>
2009-06-02 02:02:06 +0200

    [svn r21180] Minor - remove float cause was problem view student icon "search" FS#4232

--- main/forum/index.php
+++ main/forum/index.php
@@ -241,7 +241,7 @@ if (isset($_GET['action']) && $_GET['action'] == 'notify' AND isset($_GET['conte
 		$session_id=isset($_SESSION['id_session']) ? $_SESSION['id_session'] : false;
 		//if (api_is_allowed_to_edit() and !$_GET['action'])
 		echo '<div class="actions">';
-		echo '<span style="float:right;">'.search_link().'</span>';
+		echo '<span>'.search_link().'</span>';
 		if (api_is_allowed_to_edit(false,true)) {
 			echo '<a href="'.api_get_self().'?'.api_get_cidreq().'&gradebook='.$gradebook.'&action=add&amp;content=forumcategory"> '.Display::return_icon('forum_category_new.gif', get_lang('AddForumCategory')).' '.get_lang('AddForumCategory').'</a>';
 			if (is_array($forum_categories_list) and !empty($forum_categories_list)) {

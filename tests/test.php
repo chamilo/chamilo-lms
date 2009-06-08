00e6e766a0d92d0e02042bfc81d001dca8c19516
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2009-06-08 19:09:10 +0200

    [svn r21294] Minor - created file for make tests to functions.

---
+++ tests/test.php
@@ -0,0 +1,19 @@
+<?php
+	require_once('simpletest/unit_tester.php');
+	require_once('../main/inc/global.inc.php');
+	$_SESSION['_user']['user_id'] = 1;
+	require('../main/inc/global.inc.php');
+	require_once('main/inc/lib/main_api.lib.test.php'); 
+	require_once(api_get_path(SYS_CODE_PATH) . 'permissions/permissions_functions.inc.php');
+	require_once('simpletest/autorun.php');
+		
+	class Testing extends TestSuite {
+        function Testing() {
+            $this->TestSuite('Testing');
+            $this->addTestFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
+        }
+     
+    }
+      $test = &new Testing();
+   // $test->run(new HtmlReporter());
+?>

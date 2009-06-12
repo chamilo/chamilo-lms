7126e9c662b751885760d2e62ba44f84ad55c197
Arthur Portugal <yukiportu@msn.com>
2009-06-12 18:15:13 +0200

    [svn r21399] Added all functions

--- tests/main/inc/lib/debug.lib.inc.test.php
+++ tests/main/inc/lib/debug.lib.inc.test.php
@@ -1,36 +1,59 @@
 <?php
 require_once(api_get_path(LIBRARY_PATH).'debug.lib.inc.php');
 
+
 class TestDebug extends UnitTestCase {
 	
-	function testDebug() {
-		$variable='';
-		$res= debug($variable);
-		$this->assertTrue($res);
-		
+	function testDebugFunction() {
+		$variable='br';
+		ob_start();
+		debug($variable);
+		$res=ob_get_contents();
+		ob_end_clean();
+		//print_r($res);
+		$this->assertTrue(is_string($res));	
+		$this->assertTrue(is_scalar($res));
 	}
 	
-	function debug_course() {
-		
-		
+	function testDebugCourse() {
+		global $_course;
+		ob_start();
+		debug_course($_course);
+		$res=ob_get_contents();
+		ob_end_clean();
+		//print_r($res);
+		$this->assertTrue(is_array($_course));
 	}
 	
-	function debug_paths() {
-		
-		
+	function testDebugPaths() {
+		ob_start();
+		debug_paths();
+		$res=ob_get_contents();
+		ob_end_clean();
+		//print_r($res);
+		$this->assertTrue(is_string($res));
+		$this->assertTrue(is_scalar($res));
 	}
 	
-	function debug_user() {
-		
-		
+	function testDebugUser() {
+		global $_user;
+		ob_start();
+		debug_user($_user);
+		$res=ob_get_contents();
+		ob_end_clean();
+		//print_r($res);
+		$this->assertTrue(is_array($_user));		
 	}
 	
-	function printVar() {
-		
-		
+	function testPrintVar() {
+		$var='';
+		GLOBAL $DEBUG;
+		ob_start();
+		printVar($var, $varName = "@");
+		$res=ob_get_contents();
+		ob_end_clean();
+		//print_r($res);
+		$this->assertTrue(is_string($res));
 	}
-		
-	
-	
 } 
 ?>

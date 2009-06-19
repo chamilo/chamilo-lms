fad95e940fa12e305ed8c3ede0ea837b76433358
Isaac Flores <isaac.flores@beeznest.com>
2009-06-19 22:56:24 +0200

    [svn r21521] logic changes - add message,when xapian module not is instaled - (partial FS#4346)

--- main/search/index.php
+++ main/search/index.php
@@ -3,16 +3,24 @@
  * This file includes lp_list_search to avoid duplication of code, it
  * bootstraps dokeos api enough to make lp_list_search work.
  */
-
-include_once ('../inc/global.inc.php');
+$language_file = array('admin');
+require_once ('../inc/global.inc.php');
  //var_dump(api_get_path(LIBRARY_PATH));
-include_once (api_get_path(LIBRARY_PATH).'course.lib.php');
-include_once (api_get_path(LIBRARY_PATH).'debug.lib.inc.php');
-include_once (api_get_path(LIBRARY_PATH).'system_announcements.lib.php');
-include_once (api_get_path(LIBRARY_PATH).'groupmanager.lib.php');
-include_once (api_get_path(LIBRARY_PATH).'usermanager.lib.php');
-include_once (api_get_path(LIBRARY_PATH).'events.lib.inc.php');
-//api_block_anonymous_users(); // only users who are logged in can proceed
 
-require '../newscorm/lp_list_search.php';
+if (extension_loaded('xapian')) {
+	include_once (api_get_path(LIBRARY_PATH).'course.lib.php');
+	include_once (api_get_path(LIBRARY_PATH).'debug.lib.inc.php');
+	include_once (api_get_path(LIBRARY_PATH).'system_announcements.lib.php');
+	include_once (api_get_path(LIBRARY_PATH).'groupmanager.lib.php');
+	include_once (api_get_path(LIBRARY_PATH).'usermanager.lib.php');
+	include_once (api_get_path(LIBRARY_PATH).'events.lib.inc.php');
+	//api_block_anonymous_users(); // only users who are logged in can proceed
+	require '../newscorm/lp_list_search.php';
+} else {
+	Display::display_header(get_lang('Search'));
+	Display::display_error_message(get_lang('SearchXapianModuleNotInstaled'));
+	Display::display_footer();
+	exit;
+}
+
 ?>

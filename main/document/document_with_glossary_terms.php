8478c6a9aa550d6bef8d1d4c5c1c6d44f90c8776
Isaac Flores <isaac.flores@beeznest.com>
2009-06-19 12:10:11 +0200

    [svn r21506] style changes - add content div - (partial FS#4337)

--- main/document/document_with_glossary_terms.php
+++ main/document/document_with_glossary_terms.php
@@ -18,21 +18,22 @@ $content_html=file_get_contents($file_url_sys);
 $new_file=str_replace('<head>','<head><script src="'.api_get_path(WEB_LIBRARY_PATH).'javascript/jquery.js" type="text/javascript" language="javascript"></script><script type="text/javascript">
  $(document).ready(function() {
 	$("body a").toggle(function(){
-	  $(this).append("<div id=\"div_show_id\" >&nbsp;</div>");
+	  $(this).append("<div id=\"div_show_id\" ><div id=\"div_content_id\">&nbsp;</div></div>");
    	  $("div#div_show_id").attr("style","display:inline;float:left;position:absolute;background-color:#F5F6CE;border-bottom: 1px dashed #dddddd;border-right: 1px dashed #dddddd;border-left: 1px dashed #dddddd;border-top: 1px dashed #dddddd;color:#305582;margin-left:5px;margin-right:5px;");
-  	  	notebook_id=$(this).attr("name");
+   	  $("div#div_content_id").attr("style","background-color:#F5F6CE;color:#305582;margin-left:8px;margin-right:8px;margin-top:5px;margin-bottom:5px;");
+ 		notebook_id=$(this).attr("name");
   	  	data_notebook=notebook_id.split("link");
   	  	my_glossary_id=data_notebook[1];
 
   	  	$.ajax({
 			contentType: "application/x-www-form-urlencoded",
 			beforeSend: function(objeto) {
-			$("div#div_show_id").html("<img src=\'../inc/lib/javascript/indicator.gif\' />"); },
+			$("div#div_content_id").html("<img src=\'../inc/lib/javascript/indicator.gif\' />"); },
 			type: "POST",
 			url: "../glossary/glossary_ajax_request.php",
 			data: "glossary_id="+my_glossary_id,
 			success: function(datos) {
-			 	$("div#div_show_id").html(datos);
+			 	$("div#div_content_id").html(datos);
 			}
 	 	});  	
    	  		

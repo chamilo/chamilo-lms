57ec2da85e6ee0dd20efd63a2f4f0127db73b72e
Arthur Portugal <yukiportu@msn.com>
2009-07-06 21:40:00 +0200

    [svn r21824] Added memory to the tests

--- tests/all.test.php
+++ tests/all.test.php
@@ -1,28 +1,29 @@
 <?php
+ini_set('memory_limit','128M');
 require_once('simpletest/unit_tester.php');
 require_once('../main/inc/global.inc.php');
-$_SESSION['_user']['user_id'] = 1;
-//require('../main/inc/global.inc.php');
-//require_once('main/inc/lib/main_api.lib.test.php'); 
-//require_once(api_get_path(SYS_CODE_PATH) . 'permissions/permissions_functions.inc.php');
-//require_once(api_get_path(LIBRARY_PATH) . "/groupmanager.lib.php");
+require_once('simpletest/web_tester.php');
+require_once('simpletest/mock_objects.php');
 require_once('simpletest/autorun.php');
+$_SESSION['_user']['user_id'] = 1;
 
 class AllTests extends TestSuite {
     
     function AllTests() {
         $this->TestSuite('All tests');
-        //$this->addFile(dirname(__FILE__).'/main/inc/lib/add_course.lib.inc.test.php');
-        //$this->addFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/blog.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/classmanager.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/database.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/debug.lib.inc.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/display.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/document.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/events.lib.inc.test.php'); 
-        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/export.lib.inc.test.php');   
+        $this->addFile(dirname(__FILE__).'/main/inc/lib/add_course.lib.inc.test.php');
+        $this->addFile(dirname(__FILE__).'/main/inc/lib/main_api.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/blog.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/classmanager.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/database.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/debug.lib.inc.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/display.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/usermanager.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/document.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/main/inc/lib/events.lib.inc.test.php'); 
+        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/export.lib.inc.test.php');
+        //$this->addTestFile(dirname(__FILE__).'/main/inc/lib/fileDisplay.lib.test.php');
+        $this->addTestFile(dirname(__FILE__).'/run_me.php');   
         //$this->assertTrue(file_exists('/temp/test.log'));
     }
 }

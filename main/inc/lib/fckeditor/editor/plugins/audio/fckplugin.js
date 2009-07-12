656e34963ecf586cea6282cbe596740a939ef589
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-07-12 10:55:16 +0200

    [svn r22006] FS#2867 - The online editor, the audion plugin: Fixing a small visual problem.

--- main/inc/lib/fckeditor/editor/plugins/audio/fckplugin.js
+++ main/inc/lib/fckeditor/editor/plugins/audio/fckplugin.js
@@ -1,7 +1,7 @@
 // Register the command.
 FCKCommands.RegisterCommand( 'MP3',
 	new FCKDialogCommand( FCKLang['DlgAudioTitle'], FCKLang['DlgAudioTitle'] + ' (mp3)',
-	FCKConfig.PluginsPath + 'audio/fck_audio.html', 600, 300 )
+	FCKConfig.PluginsPath + 'audio/fck_audio.html', 600, 320 )
 ) ;
 
 // Create and register the Audio toolbar button.

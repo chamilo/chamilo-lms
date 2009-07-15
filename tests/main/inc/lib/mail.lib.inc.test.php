1966d1c647f34c3c5fc4c8d15bd219ebd241e4dd
Arthur Portugal <yukiportu@msn.com>
2009-07-15 21:48:48 +0200

    [svn r22117] Update two functions

--- tests/main/inc/lib/mail.lib.inc.test.php
+++ tests/main/inc/lib/mail.lib.inc.test.php
@@ -1,43 +1,34 @@
 <?php
 require_once(api_get_path(LIBRARY_PATH).'mail.lib.inc.php');
 
-
 Mock::generate('PHPMailer');
-
 class TestMail extends UnitTestCase {
 
 	function testApiMail() {
-		
+		$mensajee = new MockPHPMailer();
 		global $regexp;
  		global $platform_email;
- 		$mail = new MockPHPMailer();
  		$recipient_name='';
  		$recipient_email='';
  		$subject='';
  		$message='';
  		$res=api_mail($recipient_name, $recipient_email, $subject, $message);
- 		$mail->expectOnce($recipient_name, $recipient_email, $subject, $message, $sender_name="", $sender_email="", $extra_headers="");
-		$this->assertTrue(is_object($mail));
-		//var_dump($mail);
+ 		$mensajee->expectOnce($mail);
+ 		$this->assertTrue(is_numeric($res));
+		//var_dump($res);
 	}
-	/*
-	function testApiMail() {
+	
+	function testApiMailHtml() {
+		$mensajee = new MockPHPMailer();
 		global $regexp;
  		global $platform_email;
  		$recipient_name='';
  		$recipient_email='';
  		$subject='';
- 		$message='';
- 		$mail='';
- 		$res=api_mail($recipient_name, $recipient_email, $subject, $message);
+ 		$res=api_mail_html($recipient_name, $recipient_email, $subject);
+ 		$mensajee->expectOnce($mail);
 		$this->assertTrue(is_numeric($res));
-		var_dump($mail);
-	}*/
-
-	
-
-
-
-	
+		//var_dump($res);
+	}
 }
 ?>

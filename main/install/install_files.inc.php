37106adac3092710d5d3ef965275b9cdf5962a06
Yannick Warnier <yannick.warnier@beeznest.com>
2009-08-03 06:31:24 +0200

    [svn r22577] Remove garbage_dir definition where it still existed Updated license to use global file Minor code style changes

--- main/install/install_files.inc.php
+++ main/install/install_files.inc.php
@@ -1,26 +1,5 @@
-<?php // $Id: install_files.inc.php 11139 2007-02-17 15:43:14Z yannoo $
-/*
-==============================================================================
-	Dokeos - elearning and course management software
-
-	Copyright (c) 2004 Dokeos S.A.
-	Copyright (c) 2003 Ghent University (UGent)
-	Copyright (c) 2001 Universite catholique de Louvain (UCL)
-
-	For a full list of contributors, see "credits.txt".
-	The full license can be read in "license.txt".
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	See the GNU General Public License for more details.
-
-	Contact address: Dokeos, 44 rue des palais, B-1030 Brussels, Belgium
-	Mail: info@dokeos.com
-==============================================================================
-*/
+<?php // $Id: install_files.inc.php 22577 2009-08-03 04:31:24Z yannoo $
+/* For licensing terms, see /dokeos_license.txt */
 /**
 ==============================================================================
 * Install the Dokeos files

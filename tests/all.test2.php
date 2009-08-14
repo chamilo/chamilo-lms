8dcaa9af8e5ac5cd34963fb6c5c1686a9ae5184e
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2009-08-14 18:09:20 -0500

    Addes new files test

--- tests/all.test2.php
+++ tests/all.test2.php
@@ -10,6 +10,7 @@ require_once(api_get_path(SYS_CODE_PATH).'admin/calendar.lib.php');
 require_once(api_get_path(SYS_CODE_PATH).'admin/statistics/statistics.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'usermanager.lib.php');
 require_once(api_get_path(SYS_CODE_PATH).'/survey/survey.lib.php');
+require_once(api_get_path(SYS_CODE_PATH).'/install/install_upgrade.lib.php');
 
 
 $_SESSION['_user']['user_id'] = 1;
@@ -26,18 +27,13 @@ class AllTests2 extends TestSuite {
         //$this->addTestFile(dirname(__FILE__).'/main/auth/openid/openid.lib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/chat/chat_functions.lib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/conference/get_translation.lib.test.php');
-        //$this->addTestFile(dirname(__FILE__).'/main/install/install_upgrade_.lib.test.php');
+        //$this->addTestFile(dirname(__FILE__).'/main/install/install_upgrade.lib.test.php');
        	  $this->addTestFile(dirname(__FILE__).'/main/survey/survey.lib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/exercice/hotpotatoes.lib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/newscorm/scorm.lib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/user/userInfoLib.test.php');
         //$this->addTestFile(dirname(__FILE__).'/main/webservices/user_import/import.lib.test.php');
-        
-        
-        
-       
-
-    }
+   }
 }
 $test = &new AllTests2();
 //$test-> run( new HtmlReporter());

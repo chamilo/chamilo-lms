15c205e78ae85c6793af799c6315a2d5a2c11a72
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-08-27 17:34:11 +0300

    Bug #1370 - The database library: Adding a new constant TABLE_FORUM_NOTIFICATION. Doing some cosmetics for the source code.

--- main/inc/lib/database.lib.php
+++ main/inc/lib/database.lib.php
@@ -14,12 +14,14 @@
 * 		  This is because of the analogy with the tool constants TOOL_
 ==============================================================================
 */
+
 /*
 ==============================================================================
 		CONSTANTS
 ==============================================================================
 */
-//main database tables
+
+// Main database tables
 define('TABLE_MAIN_COURSE', 'course');
 define('TABLE_MAIN_USER', 'user');
 define('TABLE_MAIN_CLASS', 'class');
@@ -43,53 +45,61 @@ define('TABLE_MAIN_SHARED_SURVEY_QUESTION', 'shared_survey_question');
 define('TABLE_MAIN_SHARED_SURVEY_QUESTION_OPTION', 'shared_survey_question_option');
 define('TABLE_MAIN_TEMPLATES', 'templates');
 define('TABLE_MAIN_SYSTEM_TEMPLATE', 'system_template');
-define('TABLE_MAIN_OPENID_ASSOCIATION','openid_association');
-//Gradebook
+define('TABLE_MAIN_OPENID_ASSOCIATION', 'openid_association');
+
+// Gradebook
 define('TABLE_MAIN_GRADEBOOK_CATEGORY', 	'gradebook_category');
 define('TABLE_MAIN_GRADEBOOK_EVALUATION', 	'gradebook_evaluation');
-define('TABLE_MAIN_GRADEBOOK_LINKEVAL_LOG', 	'gradebook_linkeval_log');
+define('TABLE_MAIN_GRADEBOOK_LINKEVAL_LOG', 'gradebook_linkeval_log');
 define('TABLE_MAIN_GRADEBOOK_RESULT', 		'gradebook_result');
-define('TABLE_MAIN_GRADEBOOK_RESULT_LOG', 		'gradebook_result_log');
+define('TABLE_MAIN_GRADEBOOK_RESULT_LOG', 	'gradebook_result_log');
 define('TABLE_MAIN_GRADEBOOK_LINK', 		'gradebook_link');
 define('TABLE_MAIN_GRADEBOOK_SCORE_DISPLAY','gradebook_score_display');
-//Profiling
+
+// Profiling
 define('TABLE_MAIN_USER_FIELD',			'user_field');
 define('TABLE_MAIN_USER_FIELD_OPTIONS',	'user_field_options');
 define('TABLE_MAIN_USER_FIELD_VALUES',	'user_field_values');
-//Search engine
+
+// Search engine
 define('TABLE_MAIN_SPECIFIC_FIELD',			'specific_field');
 define('TABLE_MAIN_SPECIFIC_FIELD_VALUES',	'specific_field_values');
 define('TABLE_MAIN_SEARCH_ENGINE_REF',		'search_engine_ref');
-//Access URLS
+
+// Access URLs
 define('TABLE_MAIN_ACCESS_URL', 'access_url');
 define('TABLE_MAIN_ACCESS_URL_REL_USER',	'access_url_rel_user');
 define('TABLE_MAIN_ACCESS_URL_REL_COURSE', 	'access_url_rel_course');
 define('TABLE_MAIN_ACCESS_URL_REL_SESSION', 'access_url_rel_session');
-//Global calendar
+
+// Global calendar
 define('TABLE_MAIN_SYSTEM_CALENDAR', 'sys_calendar');
-//Reservation System
+
+// Reservation System
 define('TABLE_MAIN_RESERVATION_ITEM', 'reservation_item');
 define('TABLE_MAIN_RESERVATION_RESERVATION', 'reservation_main');
 define('TABLE_MAIN_RESERVATION_SUBSCRIBTION', 'reservation_subscription');
 define('TABLE_MAIN_RESERVATION_CATEGORY', 'reservation_category');
 define('TABLE_MAIN_RESERVATION_ITEM_RIGHTS', 'reservation_item_rights');
-//Social networking
+
+// Social networking
 define('TABLE_MAIN_USER_FRIEND','user_friend');
-define('TABLE_MAIN_USER_FRIEND_RELATION_TYPE','user_friend_relation_type');
-//Web services
+define('TABLE_MAIN_USER_FRIEND_RELATION_TYPE', 'user_friend_relation_type');
+
+// Web services
 define('TABLE_MAIN_USER_API_KEY','user_api_key');
 define('TABLE_MAIN_COURSE_FIELD',			'course_field');
 define('TABLE_MAIN_COURSE_FIELD_VALUES',	'course_field_values');
 define('TABLE_MAIN_SESSION_FIELD',			'session_field');
 define('TABLE_MAIN_SESSION_FIELD_VALUES',	'session_field_values');
 
-//Message
+// Message
 define('TABLE_MAIN_MESSAGE','message');
 
-//Term and conditions
+// Term and conditions
 define('TABLE_MAIN_LEGAL','legal');
 
-//statistic database tables
+// Statistic database tables
 define('TABLE_STATISTIC_TRACK_E_LASTACCESS', 'track_e_lastaccess');
 define('TABLE_STATISTIC_TRACK_E_ACCESS', 'track_e_access');
 define('TABLE_STATISTIC_TRACK_E_LOGIN', 'track_e_login');
@@ -102,14 +112,14 @@ define('TABLE_STATISTIC_TRACK_E_EXERCICES', 'track_e_exercices');
 define('TABLE_STATISTIC_TRACK_E_ATTEMPT', 'track_e_attempt');
 define('TABLE_STATISTIC_TRACK_E_ATTEMPT_RECORDING', 'track_e_attempt_recording');
 define('TABLE_STATISTIC_TRACK_E_DEFAULT', 'track_e_default');
-define('TABLE_STATISTIC_TRACK_E_UPLOADS','track_e_uploads');
-define('TABLE_STATISTIC_TRACK_E_HOTSPOT','track_e_hotspot');
+define('TABLE_STATISTIC_TRACK_E_UPLOADS', 'track_e_uploads');
+define('TABLE_STATISTIC_TRACK_E_HOTSPOT', 'track_e_hotspot');
 
-//scorm database tables
+// SCORM database tables
 define('TABLE_SCORM_MAIN', 'scorm_main');
 define('TABLE_SCORM_SCO_DATA', 'scorm_sco_data');
 
-//course tables
+// Course tables
 define('TABLE_AGENDA', 'calendar_event');
 define('TABLE_AGENDA_REPEAT', 'calendar_event_repeat');
 define('TABLE_AGENDA_REPEAT_NOT', 'calendar_event_repeat_not');
@@ -128,51 +138,58 @@ define('TABLE_STUDENT_PUBLICATION', 'student_publication');
 define('TABLE_STUDENT_PUBLICATION_ASSIGNMENT', 'student_publication_assignment');
 define('CHAT_CONNECTED_TABLE', 'chat_connected');
 
-//course forum tables
-define('TABLE_FORUM_CATEGORY','forum_category');
-define('TABLE_FORUM','forum_forum');
-define('TABLE_FORUM_THREAD','forum_thread');
-define('TABLE_FORUM_POST','forum_post');
-define('TABLE_FORUM_ATTACHMENT','forum_attachment');
-define('TABLE_FORUM_MAIL_QUEUE','forum_mailcue');
-define('TABLE_FORUM_THREAD_QUALIFY','forum_thread_qualify');
-define('TABLE_FORUM_THREAD_QUALIFY_LOG','forum_thread_qualify_log');
-//course group tables
+// Course forum tables
+define('TABLE_FORUM_CATEGORY', 'forum_category');
+define('TABLE_FORUM', 'forum_forum');
+define('TABLE_FORUM_THREAD', 'forum_thread');
+define('TABLE_FORUM_POST', 'forum_post');
+define('TABLE_FORUM_ATTACHMENT', 'forum_attachment');
+define('TABLE_FORUM_MAIL_QUEUE', 'forum_mailcue');
+define('TABLE_FORUM_THREAD_QUALIFY', 'forum_thread_qualify');
+define('TABLE_FORUM_THREAD_QUALIFY_LOG', 'forum_thread_qualify_log');
+define('TABLE_FORUM_NOTIFICATION', 'forum_notification');
+
+// Course group tables
 define('TABLE_GROUP', 'group_info');
 define('TABLE_GROUP_USER', 'group_rel_user');
 define('TABLE_GROUP_TUTOR', 'group_rel_tutor');
 define('TABLE_GROUP_CATEGORY', 'group_category');
-//course dropbox tables
-define('TABLE_DROPBOX_CATEGORY','dropbox_category');
-define('TABLE_DROPBOX_FEEDBACK','dropbox_feedback');
-define('TABLE_DROPBOX_POST','dropbox_post');
-define('TABLE_DROPBOX_FILE','dropbox_file');
-define('TABLE_DROPBOX_PERSON','dropbox_person');
-//course quiz tables
+
+// Course dropbox tables
+define('TABLE_DROPBOX_CATEGORY', 'dropbox_category');
+define('TABLE_DROPBOX_FEEDBACK', 'dropbox_feedback');
+define('TABLE_DROPBOX_POST', 'dropbox_post');
+define('TABLE_DROPBOX_FILE', 'dropbox_file');
+define('TABLE_DROPBOX_PERSON', 'dropbox_person');
+
+// Course quiz (or test, or exercice) tables
 define('TABLE_QUIZ_QUESTION', 'quiz_question');
 define('TABLE_QUIZ_TEST', 'quiz');
 define('TABLE_QUIZ_ANSWER', 'quiz_answer');
 define('TABLE_QUIZ_TEST_QUESTION', 'quiz_rel_question');
-//linked resource table
+
+// Linked resource table
 define('TABLE_LINKED_RESOURCES', 'resource');
-//new scorm tables
+
+// New SCORM tables
 define('TABLE_LP_MAIN', 'lp');
 define('TABLE_LP_ITEM', 'lp_item');
 define('TABLE_LP_VIEW', 'lp_view');
 define('TABLE_LP_ITEM_VIEW', 'lp_item_view');
 define('TABLE_LP_IV_INTERACTION', 'lp_iv_interaction'); // IV = Item View
 define('TABLE_LP_IV_OBJECTIVE', 'lp_iv_objective'); // IV = Item View
+
 // Smartblogs (Kevin Van Den Haute::kevin@develop-it.be)
-// permission tables
+// Permission tables
 define('TABLE_PERMISSION_USER', 'permission_user');
 define('TABLE_PERMISSION_TASK', 'permission_task');
 define('TABLE_PERMISSION_GROUP', 'permission_group');
-// roles tables
+// Role tables
 define('TABLE_ROLE', 'role');
 define('TABLE_ROLE_PERMISSION', 'role_permissions');
 define('TABLE_ROLE_USER', 'role_user');
 define('TABLE_ROLE_GROUP', 'role_group');
-// blogs tables
+// Blog tables
 define('TABLE_BLOGS', 'blog');
 define('TABLE_BLOGS_POSTS', 'blog_post');
 define('TABLE_BLOGS_COMMENTS', 'blog_comment');
@@ -183,25 +200,30 @@ define('TABLE_BLOGS_RATING', 'blog_rating');
 define('TABLE_BLOGS_ATTACHMENT', 'blog_attachment');
 define('TABLE_BLOGS_TASKS_PERMISSIONS', 'permission_task');
 //end of Smartblogs
-// user information tables
+
+// User information tables
 define('TABLE_USER_INFO', 'userinfo_def');
 define('TABLE_USER_INFO_CONTENT', 'userinfo_content');
-// course settings table
+
+// Course settings table
 define('TABLE_COURSE_SETTING', 'course_setting');
-// course online tables
+
+// Course online tables
 define('TABLE_ONLINE_LINK', 'online_link');
 define('TABLE_ONLINE_CONNECTED', 'online_connected');
-// dokeos_user database
+
+// Dokeos_user database
 define('TABLE_PERSONAL_AGENDA', 'personal_agenda');
 define('TABLE_PERSONAL_AGENDA_REPEAT', 'personal_agenda_repeat');
 define('TABLE_PERSONAL_AGENDA_REPEAT_NOT', 'personal_agenda_repeat_not');
 define('TABLE_USER_COURSE_CATEGORY', 'user_course_category');
-//Survey
+
+// Survey
 // @todo: are these MAIN tables or course tables ?
 define('TABLE_MAIN_SURVEY', 'survey');
 define('TABLE_MAIN_GROUP', 'survey_group');
 define('TABLE_MAIN_SURVEYQUESTION', 'questions');
-// SURVEY
+// Survey
 define('TABLE_SURVEY', 'survey');
 define('TABLE_SURVEY_QUESTION', 'survey_question');
 define('TABLE_SURVEY_QUESTION_OPTION', 'survey_question_option');
@@ -209,19 +231,19 @@ define('TABLE_SURVEY_INVITATION', 'survey_invitation');
 define('TABLE_SURVEY_ANSWER', 'survey_answer');
 define('TABLE_SURVEY_QUESTION_GROUP','survey_group');
 
-// wiki tables
+// Wiki tables
 define('TABLE_WIKI', 'wiki');
 define('TABLE_WIKI_CONF', 'wiki_conf');
 define('TABLE_WIKI_DISCUSS', 'wiki_discuss');
 define('TABLE_WIKI_MAILCUE', 'wiki_mailcue');
 
-// GLOSSARY
+// Glossary
 define('TABLE_GLOSSARY', 'glossary');
 
-// GLOSSARY
+// Notebook
 define('TABLE_NOTEBOOK', 'notebook');
 
-// MESSAGE
+// Message
 define('TABLE_MESSAGE', 'message'); 
 
 
@@ -231,6 +253,7 @@ define('TABLE_MESSAGE', 'message');
 		the class and its functions
 ==============================================================================
 */
+
 /**
  *	@package dokeos.library
  */

82ec14ab9c9c1f2a3dcf1642bb0d3212c2940380
Yannick Warnier <yannick.warnier@beeznest.com>
2009-09-01 00:53:50 -0500

    Fixed warning in learning path caused by erroneous table name being used

--- main/newscorm/learnpathItem.class.php
+++ main/newscorm/learnpathItem.class.php
@@ -431,12 +431,13 @@ class learnpathItem{
     	if($this->debug>1){error_log('New LP - In learnpathItem::get_interactions_count()',0);}
     	$return = 0;
         if ($checkdb) {
-            $tbl = Database::get_course_table(TABLE_LP_VIEW);
+            $tbl = Database::get_course_table(TABLE_LP_ITEM_VIEW);
             $sql = "SELECT id FROM $tbl " .
                     "WHERE lp_item_id = ".$this->db_id." " .
                     "AND   lp_view_id = ".$this->view_id." " .
                     "AND   view_count = ".$this->attempt_id;
             $res = api_sql_query($sql,__FILE__,__LINE__);
+		error_log(mysql_error());
             if (Database::num_rows($res)>0) {
                 $row = Database::fetch_array($res);
                 $lp_iv_id = $row[0];

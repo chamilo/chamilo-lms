c0b931e6ffb10c46222d2435fa025e3dc8e4de17
Juan Carlos Raña <juancarlos@innovasoc.com>
2009-09-18 21:18:20 +0200

    translate to spanish some files

--- main/inc/lib/fckeditor/editor/plugins/youtube/lang/es.js
+++ main/inc/lib/fckeditor/editor/plugins/youtube/lang/es.js
@@ -4,17 +4,17 @@
  * Spanish language file.
  */
 
-FCKLang['YouTubeTip']			= 'Insert/Edit YouTube' ;
-FCKLang['DlgYouTubeTitle']		= 'YouTube Property' ;
-FCKLang['DlgYouTubeCode']		= '"Please insert the URL of YouTube videos."' ;
-FCKLang['DlgYouTubeSecurity']	= 'Invalid URL.' ;
+FCKLang['YouTubeTip']			= 'Insertar/Editar Videos de YouTube' ;
+FCKLang['DlgYouTubeTitle']		= 'Propiedades YouTube' ;
+FCKLang['DlgYouTubeCode']		= '"Por favor, inserte la URL de sus videos de YouTube."' ;
+FCKLang['DlgYouTubeSecurity']	= 'URL no v&amp;alida.' ;
 FCKLang['DlgYouTubeURL']	    = 'URL' ;
-FCKLang['DlgYouTubeWidth']	    = 'Width' ;
-FCKLang['DlgYouTubeHeight']	    = 'Height' ;
-FCKLang['DlgYouTubeQuality']    = 'Quality' ;
-FCKLang['DlgYouTubeLow']	    = 'Low' ;
-FCKLang['DlgYouTubeHigh']	    = 'High (If available)' ;
-FCKLang['DlgYouTubeURLTip']		= 'Tip:' ;
-FCKLang['DlgYouTubeURLTipContent1']  = 'Go to %s. Navigate within the site to find your video.' ;
-FCKLang['DlgYouTubeURLTipContent2']  = 'Copy the URL of the video from your browser\'s address box and paste it in this dialog, the "URL" box.' ;
-FCKLang['DlgYouTubeURLTipContent3']  = 'The copied URL should look like %s.' ;
+FCKLang['DlgYouTubeWidth']	    = 'Ancho' ;
+FCKLang['DlgYouTubeHeight']	    = 'Alto' ;
+FCKLang['DlgYouTubeQuality']    = 'Calidad' ;
+FCKLang['DlgYouTubeLow']	    = 'Baja' ;
+FCKLang['DlgYouTubeHigh']	    = 'Alta (Si est&aacute; disponible)' ;
+FCKLang['DlgYouTubeURLTip']		= 'Ayuda:' ;
+FCKLang['DlgYouTubeURLTipContent1']  = 'Vaya a %s. Navegue por este sitio para encontrar su video.' ;
+FCKLang['DlgYouTubeURLTipContent2']  = 'Copie la direcci&oacute;n URL del video que aparece en su navegador y p&eacute;guela aqu&iacute;.' ;
+FCKLang['DlgYouTubeURLTipContent3']  = 'La URL copiada deber&iacute;a tener un aspecto semejante a %s.' ;

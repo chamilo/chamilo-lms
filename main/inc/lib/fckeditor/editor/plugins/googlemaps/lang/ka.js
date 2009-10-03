063ee7ba2e17b44de938f42aa3ed9119112f98d0
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-10-03 21:58:32 +0300

    Feature #306 - Online editor -> googlmaps plugin -> language files: Adding missing files, line endings are LF.

--- main/inc/lib/fckeditor/editor/plugins/googlemaps/lang/ka.js
+++ main/inc/lib/fckeditor/editor/plugins/googlemaps/lang/ka.js
@@ -1,22 +1,6 @@
-﻿﻿/*
- * == BEGIN LICENSE ==
- *
- * Licensed under the terms of any of the following licenses at your
- * choice:
- *
- *  - GNU General Public License Version 2 or later (the "GPL")
- *    http://www.gnu.org/licenses/gpl.html
- *
- *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
- *    http://www.gnu.org/licenses/lgpl.html
- *
- *  - Mozilla Public License Version 1.1 or later (the "MPL")
- *    http://www.mozilla.org/MPL/MPL-1.1.html
- *
- * == END LICENSE ==
- *
- * Google Maps for FCKeditor.
- * Latvian language file.
+/*
+ * Georgian language file.
+ * Please, use UTF-8 encoding.
  */
 
 FCKLang.DlgGMapsTitle = 'Google Maps properties' ;

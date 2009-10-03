d21649853d67ff13a6a816ac506332afd0d334e8
Ivan Tcholakov <ivantcholakov@gmail.com>
2009-10-03 22:11:19 +0300

    Feature #306 - Online editor -> mimrtex plugin -> language files: Adding missing files, line endings are LF.

--- main/inc/lib/fckeditor/editor/plugins/mimetex/lang/oc.js
+++ main/inc/lib/fckeditor/editor/plugins/mimetex/lang/oc.js
@@ -1,23 +1,9 @@
-﻿/*
- * FCKeditor - The text editor for internet
- * Copyright (C) 2003-2006 Frederico Caldeira Knabben
- *
- * Licensed under the terms of the GNU Lesser General Public License:
- * 		http://www.opensource.org/licenses/lgpl-license.php
- *
- * For further information visit:
- * 		http://www.fckeditor.net/
- *
- * "Support Open Source software. What about a donation today?"
- *
- * File Name: es.js
- * 	Spanish language file.
- *
- * File Authors:
- * 		Juan Carlos Raña Trabado
+/*
+ * Occitan language file.
+ * Please, use UTF-8 encoding.
  */
 
-FCKLang["DlgMimeTeX"]             = 'Editor mimeTeX' ;
+FCKLang["DlgMimeTeX"]             = 'mimeTeX Editor' ;
 FCKLang["DlgMimeTeXManual"]       = "Manual" ;
 FCKLang["DlgMimeTeXFormula"]      = "Formula:" ;
 FCKLang["DlgMimeTeXPreview"]      = "Preview:" ;

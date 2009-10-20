993a072d3ef015e210f321562309a0cde82c9f75
Julio Montoya <gugli100@gmail.com>
2009-10-20 17:25:07 -0500

    Adding the user image in the user_portal.php see DT#5490

--- user_portal.php
+++ user_portal.php
@@ -1196,6 +1196,20 @@ if (isset($toolsList) and is_array($toolsList) and isset($digest)) {
 if ($show_menu) {
 	echo '<div class="menusection">';
 	echo '<span class="menusectioncaption">'.get_lang('MenuUser').'</span>';
+	
+	//user image	
+	$img_array= UserManager::get_user_picture_path_by_id(api_get_user_id(),'web',true,true);
+	$img_array = UserManager::get_picture_user(api_get_user_id(), $img_array['file'], 92, 'medium_', ' width="90" height="90" ');
+	
+	echo '<div id="picture" style="">';
+		echo '<a href="/main/auth/profile.php"><img src="'.$img_array['file'].'" '.$img_array['style'].' border="1"></a>';
+	echo '</div><br />';
+	
+	//@todo add the Inbox, pending invitations, etc...
+	//echo get_lang('Inbox');
+	
+	
+	
 	echo '<ul class="menulist">';
 	if ($show_create_link) {
 		display_create_course_link();

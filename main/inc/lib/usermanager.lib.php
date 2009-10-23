82f856bf345e3fb48ee13bad3b53a4aa59e5c9f5
Patrick Cool <patrick.cool@ugent.be>
2009-10-23 16:30:25 +0200

    bugfix: sorting as defined in course management is not respected https://support.dokeos.com/issues/5446

--- main/inc/lib/usermanager.lib.php
+++ main/inc/lib/usermanager.lib.php
@@ -1475,7 +1475,7 @@ class UserManager {
 										ON course_rel_user.user_course_cat = user_course_category.id
 										$join_access_url
 										WHERE  course_rel_user.user_id = '".$user_id."'  $where_access_url
-										ORDER BY user_course_category.sort,course.title,course_rel_user.sort ASC";
+										ORDER BY user_course_category.sort, course_rel_user.sort, course.title ASC";
 		
 		$course_list_sql_result = api_sql_query($personal_course_list_sql, __FILE__, __LINE__);
 		//var_dump($course_list_sql_result); exit;

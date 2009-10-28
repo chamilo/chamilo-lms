34deedcf0835589c543465bfe17163ca310ef684
Arthur Portugal <yukiportu@msn.com>
2009-10-28 10:32:19 -0500

    Changed old button arrow into survey DT#5496

--- main/inc/lib/pear/HTML/QuickForm/advmultiselect.php
+++ main/inc/lib/pear/HTML/QuickForm/advmultiselect.php
@@ -330,7 +330,6 @@ class HTML_QuickForm_advmultiselect extends HTML_QuickForm_select
             case 'add':
                 if (is_null($attributes)) {
                     $this->_addButtonAttributes = array('name'  => 'add',
-                                                        'value' => ' >> ',
                                                         'type'  => 'button'
                                                        );
                 } else {
@@ -342,7 +341,6 @@ class HTML_QuickForm_advmultiselect extends HTML_QuickForm_select
             case 'remove':
                 if (is_null($attributes)) {
                     $this->_removeButtonAttributes = array('name'  => 'remove',
-                                                           'value' => ' << ',
                                                            'type'  => 'button'
                                                           );
                 } else {

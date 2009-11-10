a22c77e4204ac6a9bebf78da314c5717a931739d
iflores <iflores@dokeos-desktop>
2009-11-10 17:46:21 -0500

    improvements of work tool with gradebook

--- main/work/work.lib.php
+++ main/work/work.lib.php
@@ -564,6 +564,16 @@ function display_student_publications_list($work_dir,$sub_course_dir,$currentCou
 						Database::query('UPDATE '.$work_table.' SET description = '."'".Database::escape_string(Security::remove_XSS($_POST['description']))."'".', qualification = '."'".Database::escape_string($_POST['qualification']['qualification'])."'".',weight = '."'".Database::escape_string($_POST['weight']['weight'])."'".' WHERE id = '."'".$row['id']."'",__FILE__,__LINE__);
 						Database::query('UPDATE '.Database :: get_main_table(TABLE_MAIN_GRADEBOOK_LINK).' SET weight = '."'".Database::escape_string($_POST['weight']['weight'])."'".' WHERE course_code = '."'".api_get_course_id()."'".' AND ref_id = '."'".$row['id']."'".'',__FILE__,__LINE__);
 
+				    //we are changing the current work and we want add them into gradebook
+					if(isset($_POST['make_calification']) && $_POST['make_calification']==1) {
+						require_once('../gradebook/lib/be/gradebookitem.class.php');
+						require_once('../gradebook/lib/be/evaluation.class.php');
+						require_once('../gradebook/lib/be/abstractlink.class.php');
+						require_once('../gradebook/lib/gradebook_functions.inc.php');
+				
+						$resource_name = Security::remove_XSS($_POST['dir_name']);
+						add_resource_to_course_gradebook(api_get_course_id(), 3, $row['id'], Database::escape_string($resource_name),(float)$_POST['weight']['weight'], (float)$_POST['qualification']['qualification'], Database::escape_string($_POST['description']),time(), 1,api_get_session_id());
+					}                    
 					Display::display_confirmation_message(get_lang('FolderEdited'));
 
 					$values = $form_folder -> exportValues();

cef78c260ad12548c15724da858ddc1208ee5147
Isaac Flores Paz <florespaz@bidsoftperu.com>
2009-11-11 10:06:12 -0500

    test

--- tests/dummy2.test.php
+++ tests/dummy2.test.php
@@ -5,7 +5,7 @@ class TestDummy2 extends UnitTestCase
 {
     function testDummy2Values()
     {
-    	$this->assertFalse(false);
+    	$this->assertFalse(false); 
         $this->assertTrue(false);
     }
 }

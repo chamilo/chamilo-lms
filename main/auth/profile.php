27f8dc6dda2d730bec20d1c43af98306a6bce34f
Carlos Vargas <carlos.vargas@beeznest.com>
2009-11-23 14:54:30 -0500

    minor one message the email and password change see #5403

--- main/auth/profile.php
+++ main/auth/profile.php
@@ -534,30 +534,19 @@ $msg_is_not_password = false;
 if (!empty($_SESSION['change_email'])) {
 	$msg_fail_changue_email= ($_SESSION['change_email'] == 'success');
 	unset($_SESSION['change_email']);
-}
-
-if (!empty($_SESSION['is_not_password'])) {
+} elseif (!empty($_SESSION['is_not_password'])) {
 	$msg_is_not_password = ($_SESSION['is_not_password'] == 'success');
 	unset($_SESSION['is_not_password']);
-}
-
-if (!empty($_SESSION['profile_update'])) {
+} elseif (!empty($_SESSION['profile_update'])) {
 	$update_success = ($_SESSION['profile_update'] == 'success');
 	unset($_SESSION['profile_update']);
-}
-
-if (!empty($_SESSION['image_uploaded'])) {
+} elseif (!empty($_SESSION['image_uploaded'])) {
 	$upload_picture_success = ($_SESSION['image_uploaded'] == 'success');
 	unset($_SESSION['image_uploaded']);
-}
-
-if (!empty($_SESSION['production_uploaded'])) {
-
+} elseif (!empty($_SESSION['production_uploaded'])) {
 	$upload_production_success = ($_SESSION['production_uploaded'] == 'success');
 	unset($_SESSION['production_uploaded']);
-
 } elseif (isset($_POST['remove_production'])) {
-
 	foreach (array_keys($_POST['remove_production']) as $production) {
 		UserManager::remove_user_production($_user['user_id'], urldecode($production));
 	}
@@ -566,7 +555,6 @@ if (!empty($_SESSION['production_uploaded'])) {
 	}
 	$form->removeElement('productions_list');
 	$file_deleted = true;
-
 } elseif ($form->validate()) {
 
 	$wrong_current_password = false;

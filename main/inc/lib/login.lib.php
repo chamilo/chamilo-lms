ced85b88ca9699e7f8729070f2b17d8a904df46a
Julio Montoya <gugli100@gmail.com>
2009-12-24 15:58:30 -0500

    Adding login.lib.php nothing there for the moment ...

---
+++ main/inc/lib/login.lib.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * @todo Here I should move all functions located in dokeos/index.php, dokeos/user_portal.php 
+ * and all functions that means login, logout, show courses after login (maybe)
+ *
+ * Julio Montoya
+ * 
+ *  
+ */
+?>

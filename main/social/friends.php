496edf123e72c3d5a989245e8f11869c45fc613f
Carlos Vargas <carlos.vargas@beeznest.com>
2010-01-11 15:09:25 -0500

    improve the image or user CT#462

--- main/social/friends.php
+++ main/social/friends.php
@@ -172,7 +172,7 @@ if (count($friends) == 0 ) {
 									$user_name = api_xml_http_response_encode($friend['firstName'].' '.$friend['lastName']);
 									$friends_profile = SocialManager::get_picture_user($friend['friend_user_id'], $friend['image'], 92);
 									$friend_html.='<div onMouseover="show_icon_delete(this)" onMouseout="hide_icon_delete(this)" class="image-social-content" id=div_'.$friends[$j]['friend_user_id'].'>';
-									$friend_html.='<span><a href="profile.php?u='.$friend['friend_user_id'].'"><center><img src="'.$friends_profile['file'].'" style="width:60px;height:60px;border:3pt solid #eee" id="imgfriend_'.$friend['friend_user_id'].'" title="'.$user_name.'" /></center></a></span>';
+									$friend_html.='<span><a href="profile.php?u='.$friend['friend_user_id'].'"><center><img src="'.$friends_profile['file'].'" style="height:60px;border:3pt solid #eee" id="imgfriend_'.$friend['friend_user_id'].'" title="'.$user_name.'" /></center></a></span>';
 									$friend_html.='<img onclick="delete_friend (this)" id=img_'.$friend['friend_user_id'].' src="../img/blank.gif" alt="" title=""  class="image-delete" /> <center class="friend">'.$user_name.'</center></div>';				
 								}
 								$j++;

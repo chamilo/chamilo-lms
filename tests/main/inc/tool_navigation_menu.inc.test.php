dc86a6a301f4ac4ef9636b6249c53403339096ba
Arthur Portugal <yukiportu@msn.com>
2010-01-21 15:51:21 -0500

    Added a ob_start to hide the objects CT#191

--- tests/main/inc/tool_navigation_menu.inc.test.php
+++ tests/main/inc/tool_navigation_menu.inc.test.php
@@ -7,7 +7,7 @@ class TestTNMenu extends UnitTestCase{
 		$this->UnitTestCase('Navigation menu display code function tests');
 
 	}
-	public function testGetNavigationItems(){
+	public function testGetNavigationItems() {
 		global $is_courseMember;
 		global $_user;
 		global $_course;
@@ -16,7 +16,7 @@ class TestTNMenu extends UnitTestCase{
 		$this->assertTrue(is_array($res));
 	}
 
-	public function testShowNavigationMenu(){
+	public function testShowNavigationMenu() {
 		ob_start();
 		$navigation_items = get_navigation_items(true);
 		$course_id = api_get_course_id();
@@ -25,10 +25,12 @@ class TestTNMenu extends UnitTestCase{
 		ob_end_clean();
 	}
 
-	public function testShowNavigationToolShortcuts(){
+	public function testShowNavigationToolShortcuts() {
+		ob_start();
 		$orientation = SHORTCUTS_HORIZONTAL;
 		$res = show_navigation_tool_shortcuts($orientation);
 		$this->assertTrue(is_null($res));
+		ob_end_clean();
 	}
 
 

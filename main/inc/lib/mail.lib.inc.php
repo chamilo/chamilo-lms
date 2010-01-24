ff0f6f267ffe3af2c345aade590acf0adaab45c4
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-01-24 12:43:32 +0200

    Feature #272 - Code-formatting, file mail.lib.inc.php.

--- main/inc/lib/mail.lib.inc.php
+++ main/inc/lib/mail.lib.inc.php
@@ -1,11 +1,11 @@
 <?php //$id: $
-require(api_get_path(INCLUDE_PATH).'lib/phpmailer/class.phpmailer.php');
+require_once(api_get_path(INCLUDE_PATH).'lib/phpmailer/class.phpmailer.php');
 require_once(api_get_path(INCLUDE_PATH).'/conf/mail.conf.dist.php');
 
- //regular expression to test for valid email address
- // this should actually be revised to use the complete RFC3696 description
- // http://tools.ietf.org/html/rfc3696#section-3
- $regexp = "^[0-9a-z_\.+-]+@(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z][0-9a-z-]*[0-9a-z]\.)+[a-z]{2,3})$";
+//regular expression to test for valid email address
+// this should actually be revised to use the complete RFC3696 description
+// http://tools.ietf.org/html/rfc3696#section-3
+$regexp = "^[0-9a-z_\.+-]+@(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z][0-9a-z-]*[0-9a-z]\.)+[a-z]{2,3})$";
 
 /**
  * Sends email using the phpmailer class
@@ -23,77 +23,76 @@ require_once(api_get_path(INCLUDE_PATH).'/conf/mail.conf.dist.php');
  */
 function api_mail($recipient_name, $recipient_email, $subject, $message, $sender_name="", $sender_email="", $extra_headers="") {
 
-   global $regexp;
-   global $platform_email;
+	global $regexp;
+	global $platform_email;
 
-   $mail = new PHPMailer();
-   $mail->Mailer  = $platform_email['SMTP_MAILER'];
-   $mail->Host    = $platform_email['SMTP_HOST'];
-   $mail->Port    = $platform_email['SMTP_PORT'];
-   $mail->CharSet = $platform_email['SMTP_CHARSET'];
-   $mail->WordWrap = 200; // stay far below SMTP protocol 980 chars limit
+	$mail = new PHPMailer();
+	$mail->Mailer  = $platform_email['SMTP_MAILER'];
+	$mail->Host    = $platform_email['SMTP_HOST'];
+	$mail->Port    = $platform_email['SMTP_PORT'];
+	$mail->CharSet = $platform_email['SMTP_CHARSET'];
+	$mail->WordWrap = 200; // stay far below SMTP protocol 980 chars limit
 
-   if($platform_email['SMTP_AUTH'])
-   {
-      $mail->SMTPAuth = 1;
-      $mail->Username = $platform_email['SMTP_USER'];
-      $mail->Password = $platform_email['SMTP_PASS'];
-   }
+	if($platform_email['SMTP_AUTH'])
+	{
+		$mail->SMTPAuth = 1;
+		$mail->Username = $platform_email['SMTP_USER'];
+		$mail->Password = $platform_email['SMTP_PASS'];
+	}
 
-   $mail->Priority = 3; // 5=low, 1=high
-   $mail->AddCustomHeader("Errors-To: ".$platform_email['SMTP_FROM_EMAIL']."");
-   $mail->IsHTML(0);
-   $mail->SMTPKeepAlive = true;
+	$mail->Priority = 3; // 5=low, 1=high
+	$mail->AddCustomHeader("Errors-To: ".$platform_email['SMTP_FROM_EMAIL']."");
+	$mail->IsHTML(0);
+	$mail->SMTPKeepAlive = true;
 
+	// attachments
+	// $mail->AddAttachment($path);
+	// $mail->AddAttachment($path,$filename);
 
-   // attachments
-   // $mail->AddAttachment($path);
-   // $mail->AddAttachment($path,$filename);
+	if ($sender_email!="")
+	{
+		$mail->From         = $sender_email;
+		$mail->Sender       = $sender_email;
+		//$mail->ConfirmReadingTo = $sender_email; //Disposition-Notification
+	}
+	else
+	{
+		$mail->From 		= $platform_email['SMTP_FROM_EMAIL'];
+		$mail->Sender 	= $platform_email['SMTP_FROM_EMAIL'];
+		//$mail->ConfirmReadingTo = $platform_email['SMTP_FROM_EMAIL']; //Disposition-Notification
+	}
 
-
-   if ($sender_email!="")
-   {
-      $mail->From 		  = $sender_email;
-      $mail->Sender       = $sender_email;
-      //$mail->ConfirmReadingTo = $sender_email; //Disposition-Notification
-   }
-   else
-    {
-      $mail->From 		= $platform_email['SMTP_FROM_EMAIL'];
-      $mail->Sender 	= $platform_email['SMTP_FROM_EMAIL'];
-      //$mail->ConfirmReadingTo = $platform_email['SMTP_FROM_EMAIL']; //Disposition-Notification
-    }
-
-   if ($sender_name!="")
-   {
-      $mail->FromName = $sender_name;
-   }
-   else
-   {
-      $mail->FromName = $platform_email['SMTP_FROM_NAME'];
-   }
-      $mail->Subject = $subject;
-      $mail->Body    = $message;
-      //only valid address
-      if(eregi( $regexp, $recipient_email ))
-      {
-     	 $mail->AddAddress($recipient_email, $recipient_name);
-      }
+	if ($sender_name!="")
+	{
+		$mail->FromName = $sender_name;
+	}
+	else
+	{
+		$mail->FromName = $platform_email['SMTP_FROM_NAME'];
+	}
+	$mail->Subject = $subject;
+	$mail->Body    = $message;
+	//only valid address
+	if(eregi( $regexp, $recipient_email ))
+	{
+		$mail->AddAddress($recipient_email, $recipient_name);
+	}
 
 	if ($extra_headers != ""){
 		$mail->AddCustomHeader($extra_headers);
 	}
-   //send mail
-   if (!$mail->Send())
-   {
-        //echo "ERROR: mail not sent to ".$recipient_name." (".$recipient_email.") because of ".$mail->ErrorInfo."<br>";
-        return 0;
-   }
 
-   // Clear all addresses
-   $mail->ClearAddresses();
-   return 1;
- }
+	//send mail
+	if (!$mail->Send())
+	{
+		//echo "ERROR: mail not sent to ".$recipient_name." (".$recipient_email.") because of ".$mail->ErrorInfo."<br>";
+		return 0;
+	}
+
+	// Clear all addresses
+	$mail->ClearAddresses();
+	return 1;
+}
 
 /**
  * Sends an HTML email using the phpmailer class (and multipart/alternative to downgrade gracefully)
@@ -115,79 +114,78 @@ function api_mail($recipient_name, $recipient_email, $subject, $message, $sender
  * @see                     class.phpmailer.php
  */
 function api_mail_html($recipient_name, $recipient_email, $subject, $message, $sender_name = "", $sender_email = "", $extra_headers = null, $data_file = array()) {
-	
-   global $regexp;
-   global $platform_email;
-   
-   $mail = new PHPMailer();
-   $mail->Mailer  = $platform_email['SMTP_MAILER'];
-   $mail->Host    = $platform_email['SMTP_HOST'];
-   $mail->Port    = $platform_email['SMTP_PORT'];
-   $mail->WordWrap = 200; // stay far below SMTP protocol 980 chars limit
 
-   if($platform_email['SMTP_AUTH'])
-   {
-      $mail->SMTPAuth = 1;
-      $mail->Username = $platform_email['SMTP_USER'];
-      $mail->Password = $platform_email['SMTP_PASS'];
-   }
+	global $regexp;
+	global $platform_email;
 
-   $mail->Priority = 3; // 5=low, 1=high
-   $mail->AddCustomHeader("Errors-To: ".$platform_email['SMTP_FROM_EMAIL']."");
-   $mail->IsHTML(0);
-   $mail->SMTPKeepAlive = true;
+	$mail = new PHPMailer();
+	$mail->Mailer  = $platform_email['SMTP_MAILER'];
+	$mail->Host    = $platform_email['SMTP_HOST'];
+	$mail->Port    = $platform_email['SMTP_PORT'];
+	$mail->CharSet = $platform_email['SMTP_CHARSET'];
+	$mail->WordWrap = 200; // stay far below SMTP protocol 980 chars limit
 
+	if($platform_email['SMTP_AUTH'])
+	{
+		$mail->SMTPAuth = 1;
+		$mail->Username = $platform_email['SMTP_USER'];
+		$mail->Password = $platform_email['SMTP_PASS'];
+	}
 
-   // attachments
-   // $mail->AddAttachment($path);
-   // $mail->AddAttachment($path,$filename);
+	$mail->Priority = 3; // 5=low, 1=high
+	$mail->AddCustomHeader("Errors-To: ".$platform_email['SMTP_FROM_EMAIL']."");
+	$mail->IsHTML(0);
+	$mail->SMTPKeepAlive = true;
 
+	// attachments
+	// $mail->AddAttachment($path);
+	// $mail->AddAttachment($path,$filename);
 
-   if ($sender_email!="")
-   {
-      $mail->From 		  = $sender_email;
-      $mail->Sender       = $sender_email;
-      //$mail->ConfirmReadingTo = $sender_email; //Disposition-Notification
-   }
-   else
-    {
-      $mail->From 		= $platform_email['SMTP_FROM_EMAIL'];
-      $mail->Sender 	= $platform_email['SMTP_FROM_EMAIL'];
-      //$mail->ConfirmReadingTo = $platform_email['SMTP_FROM_EMAIL']; //Disposition-Notification
-    }
+	if ($sender_email!="")
+	{
+		$mail->From         = $sender_email;
+		$mail->Sender       = $sender_email;
+		//$mail->ConfirmReadingTo = $sender_email; //Disposition-Notification
+	}
+	else
+	{
+		$mail->From 		= $platform_email['SMTP_FROM_EMAIL'];
+		$mail->Sender 	= $platform_email['SMTP_FROM_EMAIL'];
+		//$mail->ConfirmReadingTo = $platform_email['SMTP_FROM_EMAIL']; //Disposition-Notification
+	}
 
-   if ($sender_name!="")
-   {
-      $mail->FromName = $sender_name;
-   }
-   else
-   {
-      $mail->FromName = $platform_email['SMTP_FROM_NAME'];
-   }
-      $mail->Subject = $subject;
-      
-      $mail->AltBody = strip_tags(str_replace('<br />',"\n", api_html_entity_decode($message)));
-      $mail->Body = '<html><head></head><body>'.$message.'</body></html>';
-      
-      // attachment ...
-      if (!empty($data_file)) {
-      	$mail->AddAttachment($data_file['path'], $data_file['filename']);
-      }
-    
-      // only valid address
-      if(is_array($recipient_email)) {
+	if ($sender_name!="")
+	{
+		$mail->FromName = $sender_name;
+	}
+	else
+	{
+		$mail->FromName = $platform_email['SMTP_FROM_NAME'];
+	}
+	$mail->Subject = $subject;
+
+	$mail->AltBody = strip_tags(str_replace('<br />',"\n", api_html_entity_decode($message)));
+	$mail->Body = '<html><head></head><body>'.$message.'</body></html>';
+
+	// attachment ...
+	if (!empty($data_file)) {
+		$mail->AddAttachment($data_file['path'], $data_file['filename']);
+	}
+
+	// only valid address
+	if(is_array($recipient_email)) {
 		foreach($recipient_email as $dest) {
-	      if(eregi( $regexp, $dest )) {	
-	     	 $mail->AddAddress($dest, $recipient_name);
-	     	 //$mail->AddAddress($dest, ($i>1?'':$recipient_name));
-	      }
+			if(eregi( $regexp, $dest )) {
+				$mail->AddAddress($dest, $recipient_name);
+				//$mail->AddAddress($dest, ($i>1?'':$recipient_name));
+			}
 		}
-      } else {
-	      if(eregi( $regexp, $recipient_email ))
-	      {
-	     	 $mail->AddAddress($recipient_email, $recipient_name);
-	      }
-      }
+	} else {
+		if(eregi( $regexp, $recipient_email ))
+		{
+			$mail->AddAddress($recipient_email, $recipient_name);
+		}
+	}
 
 	if (is_array($extra_headers) && count($extra_headers)>0){
 		foreach($extra_headers as $key=>$value)
@@ -210,18 +208,20 @@ function api_mail_html($recipient_name, $recipient_email, $subject, $message, $s
 			}
 		}
 	}
-    // WordWrap the html body (phpMailer only fixes AltBody) FS#2988
-    $mail->Body = $mail->WrapText($mail->Body, $mail->WordWrap);
-   //send mail
-   if (!$mail->Send())
-   {
-        //echo "ERROR: mail not sent to ".$recipient_name." (".$recipient_email.") because of ".$mail->ErrorInfo."<br>";
-        return 0;
-   }
 
-   // Clear all addresses
-   $mail->ClearAddresses();
-   return 1;
- }
+	// WordWrap the html body (phpMailer only fixes AltBody) FS#2988
+	$mail->Body = $mail->WrapText($mail->Body, $mail->WordWrap);
+
+	//send mail
+	if (!$mail->Send())
+	{
+		//echo "ERROR: mail not sent to ".$recipient_name." (".$recipient_email.") because of ".$mail->ErrorInfo."<br>";
+		return 0;
+	}
+
+	// Clear all addresses
+	$mail->ClearAddresses();
+	return 1;
+}
 
 ?>

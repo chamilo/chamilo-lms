ab6e8660ab757ef0ea1dda1eead77ae4afedebad
Cristian Fasanando <cristian.fasanando@beeznest.com>
2010-01-26 12:03:06 -0500

    added changes to migrate-db-1.8.6.2-1.8.7 about thematic advance, history and new field id for item_property and course tables - partial CT#579

--- main/install/migrate-db-1.8.6.2-1.8.7-pre.sql
+++ main/install/migrate-db-1.8.6.2-1.8.7-pre.sql
@@ -11,16 +11,23 @@
 -- will allow us to deal with the unique-database-type installations
 --
 -- This first part is for the main database
+
 -- xxMAINxx
 INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url, access_url_changeable) VALUES ('course_create_active_tools','notebook','checkbox','Tools','true','CourseCreateActiveToolsTitle','CourseCreateActiveToolsComment',NULL,'Notebook',1,0);
 INSERT INTO course_module (name, link, image, `row`, `column`, position) VALUES ('notebook','notebook/index.php','notebook.gif',2,1,'basic');
-
 UPDATE settings_current SET selected_value = '1.8.7.10004' WHERE variable = 'dokeos_database_version';
+ALTER TABLE course DROP PRIMARY KEY , ADD UNIQUE KEY code (code); 
+ALTER TABLE course ADD id int NOT NULL auto_increment PRIMARY KEY FIRST;
 
 -- xxSTATSxx
+CREATE TABLE track_e_item_property(id int NOT NULL auto_increment PRIMARY KEY, course_id int NOT NULL, item_property_id int NOT NULL, title varchar(255), content text, lastedit_date datetime NOT NULL default '0000-00-00 00:00:00', lastedit_user_id int  NOT NULL, session_id int NOT NULL default 0);
+ALTER TABLE track_e_item_property ADD INDEX (course_id, item_property_id, session_id);
 
 -- xxUSERxx
 
 -- xxCOURSExx
-
 INSERT INTO tool(name,link,image,visibility,admin,address,added_tool,target,category) VALUES ('notebook','notebook/index.php','notebook.gif',0,'0','squaregrey.gif',0,'_self','interaction');
+ALTER TABLE course_description ADD COLUMN progress INT  NOT NULL DEFAULT 0 AFTER description_type;
+ALTER TABLE item_property ADD id int NOT NULL auto_increment PRIMARY KEY FIRST;
+UPDATE course_description SET description_type = (SELECT IF(description_type>7,description_type+1,description_type)); -- update description_type field for using thematic advance with description_type = 8
+

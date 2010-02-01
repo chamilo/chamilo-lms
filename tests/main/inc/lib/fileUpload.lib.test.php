41a63cea3578609b89233d4adfe9a6e12c816821
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2010-02-01 17:57:01 -0500

    Modified the test fileUpload fixed the exceptions

--- tests/main/inc/lib/fileUpload.lib.test.php
+++ tests/main/inc/lib/fileUpload.lib.test.php
@@ -2,34 +2,25 @@
 /** To can run this test you need comment "die() This code is in the line 278,1377,1386,1389,1542,1547,1571 file fileUpload.lib.php
  *  @author aportugal arthur.portugal@dokeos.com
  */
+
 require_once(api_get_path(LIBRARY_PATH).'fileUpload.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'document.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'/pclzip/pclzip.lib.php');
-Mock::generate('DocumentManager');
-Mock::generate('Display');
-Mock::generate('Database');
 
 class TestFileUpload extends UnitTestCase {
 
 		function testAddAllDocumentsInFolderToDatabase() {
-		  //$docman = new MockDocumentManager();
-			$_course='';
-			$user_id='';
-			$base_work_dir='';
-		  //$path = $base_work_dir;
-		  //$handle=opendir($path);
-		  //$file=readdir($handle);
-		  //$safe_file=replace_dangerous_char($file);
-			$res=add_all_documents_in_folder_to_database($_course,$user_id,$base_work_dir,$current_path='',$to_group_id=0);
-          //$docman->expectOnce('DocumentManager::get_document_id',array($_course, $current_path.'/'.$safe_file));
-	      //$this->assertTrue(is_object($docman));
+			global $_course, $user_id;
+			$base_work_dir= api_get_path(SYS_PATH).'';
+			$current_path = 'courses/';
+			$to_group_id =0;
+			$res=add_all_documents_in_folder_to_database($_course,$user_id,$base_work_dir,$current_path,$to_group_id);
 	        $this->assertTrue(is_null($res));
 	        //var_dump($res);
 		}
 
 		function testAddDocument() {
-			global $charset;
-			$_course['dbName']='';
+			global $_course;
 			$path='';
 			$filetype='';
 			$filesize='';
@@ -50,9 +41,8 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testApiReplaceLinksInHtml() {
-			$upload_path='';
-			$doc_url = $_GET['file'];
-			$full_file_name = api_get_path(SYS_COURSE_PATH).api_get_course_path().'/upload/blog/'.$doc_url;
+			$upload_path='/var/www/chamilo1/courses/COURSEX/upload/blog';
+			$full_file_name = 'doc.php';
 			$res=api_replace_links_in_html($upload_path,$full_file_name);
 			$this->assertTrue(is_null($res));
 			//var_dump($res);
@@ -86,14 +76,10 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testBuildMissingFilesForm() {
-
-			$_course['path']='';
-			$courseDir   = $_course['path']."/document";
-			$sys_course_path = api_get_path(SYS_COURSE_PATH);
-			$base_work_dir = $sys_course_path.$courseDir;
-			$missing_files = check_for_missing_files($base_work_dir.$new_path);
-			$upload_path='';
-			$file_name = '';
+			global	$_course;
+			$missing_files = array();
+			$upload_path=api_get_path(SYS_COURSE_PATH).'/COURSEX/document/image';
+			$file_name = 'board.jpg';
 			$res=build_missing_files_form($missing_files,$upload_path,$file_name);
 			$this->assertTrue(is_string($res));
 			//var_dump($res);
@@ -132,8 +118,8 @@ class TestFileUpload extends UnitTestCase {
 
 		function testCreateLinkFile() {
 			
-			$filePath='/var/tmp/archivo123.txt';
-			$url='http://www.dokeos.com';
+			$filePath = api_get_path(SYS_PATH).'documentation/readme.html';
+			$url = 'http://www.chamilo.com';
 			$res= create_link_file($filePath, $url);
 			
 			if (!is_numeric($res)) {
@@ -146,27 +132,34 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testCreateUnexistingDirectory()  {
-			$_course='';
-			$user_id='';
-			$to_group_id='';
-			$to_user_id='';
-			$base_work_dir='';
-			$desired_dir_name='';
+			global $_course, $user_id;
+			$to_group_id = 1;
+			$to_user_id = 1;
+			$base_work_dir = api_get_path(SYS_COURSE_PATH).'COURSEX/document';
+			$desired_dir_name = 'images';
 			$res= create_unexisting_directory($_course,$user_id,$to_group_id,$to_user_id,$base_work_dir,$desired_dir_name);
-			$this->assertTrue(is_bool($res));
-			//var_dump($res);
-		}
+			
+			if(is_string($res)){
+				$this->assertTrue(is_string($res));
+				$this->assertTrue($res);	
+			} else {
+				$this->assertTrue(is_bool($res));
+				$this->assertFalse(is_string($res));
+			} 
+ 			//var_dump($res);
+			}
+
 
 		function testDirTotalSpace() {
-			$dirPath='/var/www/path';
+			$dirPath= api_get_path(SYS_COURSE_PATH).'COURSEX/document/';
 			$res= dir_total_space($dirPath);
 			$this->assertTrue(is_numeric($res));
+			$this->assertTrue($res);
 			//var_dump($res);
 		}
 
-		function testDisableDangerousFile($filename) {
-			$filename = php2phps($filename);
-			$filename = htaccess2txt($filename);
+		function testDisableDangerousFile() {
+			$filename = 'index.php';
 			$res= disable_dangerous_file($filename);
 			$this->assertTrue(is_string($res));
 			//var_dump($res);
@@ -200,7 +193,7 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testfilter_extension() {
-			$filename='';
+			$filename='index.php';
 			$res= filter_extension($filename);
 			$this->assertTrue(is_numeric($res));			
 		}
@@ -213,15 +206,14 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testget_levels() {
-			$filename='';
+			$filename='readme.txt';
 			$res= get_levels($filename);
 			$this->assertTrue(is_numeric($res));
 			//var_dump($res);
 		}
 
 		function testhandle_uploaded_document() {
-			$docman = new MockDisplay();
-			$_course='';
+			global $_course;
 			$uploaded_file='';
 			$base_work_dir='';
 			$upload_path='';
@@ -240,43 +232,28 @@ class TestFileUpload extends UnitTestCase {
 			$file_path = $upload_path.$clean_name;
 			ob_start();
 			$res= handle_uploaded_document($_course,$uploaded_file,$base_work_dir,$upload_path,$user_id,$to_group_id=0,$to_user_id=NULL,$maxFilledSpace='',$unzip=0,$what_if_file_exists='',$output=true);
-			$docman->expectOnce(Display::display_error_message(get_lang('UplNotEnoughSpace')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplNotAZip')." ".get_lang('PleaseTryAgain')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplUnableToSaveFileFilteredExtension')));
-			$docman->expectOnce(Display::display_error_message(get_lang('DestDirectoryDoesntExist').' ('.$upload_path.')'));
-			$docman->expectOnce(DocumentManager::get_document_id($_course,$file_path));
-			$docman->expectOnce(Display::display_confirmation_message(get_lang('UplUploadSucceeded')."<br/>".$file_path .' '. get_lang('UplFileOverwritten'),false));
-			$docman->expectOnce(Display::display_confirmation_message(get_lang('UplUploadSucceeded')."<br/>".$file_path,false));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplUnableToSaveFile')));
-			$docman->expectOnce(Display::display_confirmation_message(get_lang('UplUploadSucceeded'). "<br>" .get_lang('UplFileSavedAs') . $new_file_path,false));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplUnableToSaveFile')));
-			$docman->expectOnce(Display::display_error_message($clean_name.' '.get_lang('UplAlreadyExists')));
-			$docman->expectOnce(Display::display_confirmation_message(get_lang('UplUploadSucceeded')."<br/>".$file_path,false));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplUnableToSaveFile')));
 			$this->assertTrue(is_null($res));
 			ob_end_clean();
 			//var_dump($res);
 		}
 
 		function testhtaccess2txt() {
-			$filename = str_replace('.htaccess', 'htaccess.txt', $filename);
-			$filename = str_replace('.HTACCESS', 'htaccess.txt', $filename);
+			$filename = 'readme.txt';
 			$res= htaccess2txt($filename);
 			$this->assertTrue(is_string($res));
 			//var_dump($res);
 		}
 
 		function testitem_property_update_on_folder() {
-			$path='/main/document/document.php';
-			$_course='';
-			$user_id='';
+			global $_course, $user_id;
+			$path=api_get_path(SYS_COURSE_PATH).'COURSEX/document/license.txt';
 			$res= item_property_update_on_folder($_course,$path,$user_id);
 			$this->assertTrue(is_null($res));
 			//var_dump($res);
 		}
 
 		function testmove_uploaded_file_collection_into_directory() {
-			$_course=array();
+			global $_course;
 			$uploaded_file_collection=array();
 			$base_work_dir='';
 			$missing_files_dir='';
@@ -290,26 +267,19 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testphp2phps () {
-			$fileName = preg_replace('/\.(php.?|phtml.?)(\.){0,1}.*$/i', '.phps', $fileName);
+			$fileName = 'index.php';
 			$res= php2phps($fileName);
 			$this->assertTrue(is_string($res));
 			//var_dump($res);
 		}
 
 		function testprocess_uploaded_file() {
-			$docman = new MockDisplay();
 			$uploaded_file='';
 			ob_start();
 			$res= process_uploaded_file($uploaded_file);
-			$docman->expectOnce(Display::display_error_message(get_lang('UplExceedMaxServerUpload'). ini_get('upload_max_filesize')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplExceedMaxPostSize'). round($_POST['MAX_FILE_SIZE']/1024) ." KB"));
-			$docman->expectOnce(Display::display_error_message(get_lang('$UplPartialUpload')." ".get_lang('PleaseTryAgain')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplNoFileUploaded')." ". get_lang('UplSelectFileFirst')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplNoFileUploaded')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplFileTooBig')));
-			$docman->expectOnce(Display::display_error_message(get_lang('UplUploadFailed')));
-			$this->assertTrue(is_bool($res));
 			ob_end_clean();
+			$this->assertTrue(is_bool($res));
+			
 		}
 
 		function testreplace_img_path_in_html_file() {
@@ -328,23 +298,24 @@ class TestFileUpload extends UnitTestCase {
 			$res= search_img_from_html($htmlFile);
 			if(is_array($res)){
 			$this->assertTrue(is_array($res));
-			}else {
+			} else {
 			$this->assertTrue(is_null($res));
 			}
 			//var_dump($res);
 		}
 
 		function testset_default_settings() {
-			global $dbTable,$_configuration;
-			global $default_visibility;
-			$upload_path=str_replace('\\','/',$upload_path);
-			$upload_path=str_replace("//","/",$upload_path);
-			$filename=substr($filename,0,-1);
-			$res= set_default_settings($upload_path,$filename,$filetype="file");
+			global $_course, $_configuration, $cidReq;
+			global $default_visibility, $dbTable;
+			$dbTable = Database::get_course_table(TABLE_DOCUMENT);
+			$upload_path = api_get_path(SYS_COURSE_PATH).'COURSEX/document';
+			$filetype="file";
+			$filename = substr($filename,0,-1);
+			$res= set_default_settings($upload_path,$filename,$filetype);
 			$this->assertTrue(is_null($res));
 			//var_dump($res);
 		}
-
+		
 		function testtreat_uploaded_file() {
 			$uploadedFile['name']=stripslashes($uploadedFile['name']);
 			$uploadedFile='';
@@ -366,8 +337,7 @@ class TestFileUpload extends UnitTestCase {
 		}
 
 		function testunzip_uploaded_document() {
-			$docman = new MockDisplay();
-			require_once(api_get_path(LIBRARY_PATH).'/pclzip/pclzip.lib.php');
+			//require_once(api_get_path(LIBRARY_PATH).'/pclzip/pclzip.lib.php');
 			global $_course;
 			global $_user;
 			global $to_user_id;
@@ -378,31 +348,36 @@ class TestFileUpload extends UnitTestCase {
 			$max_filled_space='';
 			ob_start();
 			$res= unzip_uploaded_document($uploaded_file, $upload_path, $base_work_dir, $max_filled_space, $output = true, $to_group_id=0);
-			$docman->expectOnce(Display::display_error_message(get_lang('UplNotEnoughSpace')));
-			$this->assertTrue(is_bool($res));
 			ob_end_clean();
+			$this->assertTrue(is_bool($res));
 			//var_dump($res);
 		}
 
 		function testunzip_uploaded_file() {
-			$uploadedFile='';
-			$uploadPath='';
-			$baseWorkDir='';
-			$maxFilledSpace='';
+			global $_course;	
+			$uploadedFile   = 'README.txt';
+			$uploadPath     = api_get_path(SYS_PATH).$_course.'/document';
+			$baseWorkDir    = api_get_path(SYS_PATH);
+			$maxFilledSpace = 10;
 			$res= unzip_uploaded_file($uploadedFile, $uploadPath, $baseWorkDir, $maxFilledSpace);
 			$this->assertTrue(is_bool($res));
 			//var_dump($res);
 		}
 
 		function testupdate_existing_document() {
-			$docman = new MockDatabase();
 			$_course='';
 			$document_id='';
 			$filesize='';
 			$res= update_existing_document($_course,$document_id,$filesize,$readonly=0);
-			$docman->expectOnce(Database::get_course_table(TABLE_DOCUMENT,$_course['dbName']));
 			$this->assertTrue(is_bool($res));
 			//var_dump($res);
-		} 
+		}
+		
+		public function testdeleteCourse(){
+		$this->dmanager = null;
+	 	$code = 'COURSEX';				
+		$res = CourseManager::delete_course($code);					
+		}
+
 }
 ?>

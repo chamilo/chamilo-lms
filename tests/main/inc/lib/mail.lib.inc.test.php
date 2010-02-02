1eb1d8a2b13729fe5348fb7ebe851cf96f510fdc
Arthur Portugal <yukiportu@msn.com>
2010-02-02 13:55:39 -0500

    Fixed the file mail.lib.inc.test.php, adding ob_start() CT#191

--- tests/main/inc/lib/mail.lib.inc.test.php
+++ tests/main/inc/lib/mail.lib.inc.test.php
@@ -1,32 +1,27 @@
 <?php
 require_once(api_get_path(LIBRARY_PATH).'mail.lib.inc.php');
 
-Mock::generate('PHPMailer');
 class TestMail extends UnitTestCase {
 
 	function testApiMail() {
-		$mensajee = new MockPHPMailer();
-		global $regexp;
- 		global $platform_email;
+		ob_start();
  		$recipient_name='';
  		$recipient_email='';
  		$subject='';
  		$message='';
  		$res=@api_mail($recipient_name, $recipient_email, $subject, $message);
- 		$mensajee->expectOnce($mail);
+ 		ob_end_clean();
  		$this->assertTrue(is_numeric($res));
 		//var_dump($res);
 	}
 
 	function testApiMailHtml() {
-		$mensajee = new MockPHPMailer();
-		global $regexp;
- 		global $platform_email;
+		ob_start();
  		$recipient_name='';
  		$recipient_email='';
  		$subject='';
  		$res=@api_mail_html($recipient_name, $recipient_email, $subject);
- 		$mensajee->expectOnce($mail);
+ 		ob_end_clean();
 		$this->assertTrue(is_numeric($res));
 		//var_dump($res);
 	}

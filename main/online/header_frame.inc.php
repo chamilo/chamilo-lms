983998a6e5bbe89ef6a3eed69fc0815ac4746a3e
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-02-10 07:16:32 +0200

    Feature #272 - The old online conferention tool: Minor corrections in a html header for declaring language, encoding and text direction.

--- main/online/header_frame.inc.php
+++ main/online/header_frame.inc.php
@@ -59,13 +59,17 @@ elseif(FRAME == 'message')
 {
 	$bodyXtra='onload="javascript:eventMessage();"';
 }
+
+$document_language = api_get_language_isocode();
+
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $document_language; ?>" lang="<?php echo $document_language; ?>">
 <head>
-<?php /* Encoding is directly set here, to be fixed. */ ?>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo api_get_system_encoding(); ?>">
 <title>Online Conference</title>
 
 <?php
@@ -148,4 +152,4 @@ function eventMessage()
 </script>
 
 </head>
-<body <?php echo $bodyXtra; ?> >
+<body dir="<?php echo api_get_text_direction(); ?>" <?php echo $bodyXtra; ?> >

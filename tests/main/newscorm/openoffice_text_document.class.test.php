eaa8546ee28601d010e9411d605337c776973992
Carlos Vargas <carlos.vargas@beeznest.com>
2010-02-18 17:01:13 -0500

    create test oppenoffice_document oppenoffice_presentation, oppenoffice_text oppenoffice_text_document CT#191

---
+++ tests/main/newscorm/openoffice_text_document.class.test.php
@@ -0,0 +1,47 @@
+<?php
+//require_once(api_get_path(SYS_CODE_PATH).'newscorm/openoffice_text_document.class.php');
+
+class TestOpenOfficeTextDocument extends UnitTestCase {
+/*
+	public function testMakeLp() {
+		//ob_start();
+		$obj = new OpenofficeTextDocument($split_steps=false, $course_code=null, $resource_id=null,$user_id=null); 		
+		$res = $obj->make_lp($files=array());
+		$this->assertTrue(is_bool($res)); 
+		//ob_end_clean();
+		//var_dump($res);
+	}
+
+	public function testDealPerChapter() {
+		//ob_start();
+		$res = OpenOfficeTextDocument::dealPerChapter($header = 'Header', $content = 'Content');
+		$this->assertTrue(is_null($res)); 
+		//ob_end_clean();
+		//var_dump($res);
+	}
+
+	public function testDealPerPage() {
+		//ob_start();
+		$res = OpenOfficeTextDocument::dealPerPage($header = 'Header',$body = 'Content');
+		$this->assertTrue(is_null($res)); 
+		//ob_end_clean();
+		//var_dump($res);
+	}
+
+	public function testAddCommandParameters() {
+		//ob_start();
+		$res = OpenOfficeTextDocument::add_command_parameters();
+		$this->assertTrue(is_string($res)); 
+		//ob_end_clean();
+		//var_dump($res);
+	}
+
+	public function testFormatPageContent() {
+		//ob_start();
+		$res = OpenOfficeTextDocument::format_page_content($header = 'Header', $content = 'Content');
+		$this->assertTrue(is_string($res)); 
+		//ob_end_clean();
+		//var_dump($res);
+	}	*/
+}
+?>

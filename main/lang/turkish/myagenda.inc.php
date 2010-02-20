d0547c695d1667c26246f983aab40353bc655801
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-02-20 17:46:02 +0200

    Feature #272 - Removing new language folders that didn't have the same names as the old folders have.

--- main/lang/turkish/myagenda.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-for more information: see languages.txt in the lang folder.
-*/
-$langMyAgenda = "Ajandam";
-$langToday = "Bugün";
-?>
--- main/lang/turkish/scormbuilder.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-for more information: see languages.txt in the lang folder.
-*/
-$langScormBuilder = "Yol yap&#305;c&#305; - Scorm biçem ders yap&#305;c&#305;";
-?>

dfd55dc62b7a7f75367ef903fa0ec8c8ce5be1a8
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-03-03 22:09:56 +0200

    Task #272 - Installation scripts, upgrading: Forsing inclusion of the old configuration file. This is needed to be done after the blocking feature has been introduced.

--- main/install/index.php
+++ main/install/index.php
@@ -218,7 +218,7 @@ if ($_POST['step2_install'] || $_POST['step2_update_8'] || $_POST['step2_update_
 
 if ($installType == 'update' && in_array($my_old_version, $update_from_version_8)) {
 	// This is the main configuration file of the system before the upgrade.
-	include_once api_get_path(CONFIGURATION_PATH).'configuration.php';
+	include api_get_path(CONFIGURATION_PATH).'configuration.php'; // Don't change to include_once
 }
 
 if (!isset($_GET['running'])) {

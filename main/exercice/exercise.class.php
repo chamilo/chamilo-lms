a31076805519c277797ea195b16cd30b476b9c97
Julio Montoya <gugli100@gmail.com>
2010-03-15 17:19:59 -0500

    Fixing copy_course

--- main/exercice/exercise.class.php
+++ main/exercice/exercise.class.php
@@ -1378,7 +1378,7 @@ class Exercise
 		$table_track_e_attempt   = Database::get_statistic_table(TABLE_STATISTIC_TRACK_E_ATTEMPT);	
 		
 		$sql_select = "SELECT exe_id FROM $table_track_e_exercises
-						WHERE exe_cours_id = '".api_get_course_id()."' AND exe_exo_id = ".$this->id." AND orig_lp_id = 0 AND orig_lp_item_id = 0 AND session_id = ".api_get_session_id()."";
+					   WHERE exe_cours_id = '".api_get_course_id()."' AND exe_exo_id = ".$this->id." AND orig_lp_id = 0 AND orig_lp_item_id = 0 AND session_id = ".api_get_session_id()."";
 						
 		$result   = Database::query($sql_select);
 		$exe_list = Database::store_result($result);
@@ -1404,7 +1404,7 @@ class Exercise
 	 * Copies an exercise (duplicate all questions and answers)
 	*/
 	
-	function copy_exercise() {
+	public function copy_exercise() {
 		$exercise_obj= new Exercise();
 		$exercise_obj = $this;
 		
@@ -1424,8 +1424,11 @@ class Exercise
 	
 		//Question creation 		
 		foreach ($question_list as $old_question_id) {			
-			$new_question_obj = Question::read($old_question_id);
-			$new_id = $new_question_obj->duplicate();
+			$old_question_obj = Question::read($old_question_id);
+			$new_id = $old_question_obj->duplicate();
+			
+			$new_question_obj = Question::read($new_id);
+			
 			$new_question_obj->addToList($new_exercise_id);		
 			// This should be moved to the duplicate function				
 			$new_answer_obj = new Answer($old_question_id);

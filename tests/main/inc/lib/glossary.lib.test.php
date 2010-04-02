2991b92efe92a94cb09363c4c20a3e56e6d2c248
Yannick Warnier <yannick.warnier@beeznest.com>
2010-04-02 17:35:22 -0500

    Added TestManager include and creation of course to have the right context for the glossary

--- tests/main/inc/lib/glossary.lib.test.php
+++ tests/main/inc/lib/glossary.lib.test.php
@@ -3,141 +3,127 @@ require_once(api_get_path(LIBRARY_PATH).'glossary.lib.php');
 
 class TestGlossary extends UnitTestCase {
 	
+    public function __construct() {
+        $this->UnitTestCase('Glossary library tests');
+        //build a course
+        require_once api_get_path(SYS_TEST_PATH).'test_manager.inc.php';
+        TestManager::create_test_course();
+    }
+    
+    public function __destruct() {
+    	TestManager::delete_test_course();
+    }
+    
 	function testGetGlossaryTerms() {
-		//ob_start();
 		$res = GlossaryManager::get_glossary_terms();
-		$this->assertTrue(is_array($res)); 
-		//ob_end_clean();
-		//var_dump($res);
+		$this->assertTrue(is_array($res));
 	}
 	
 	function testSaveGlossary() {
-		ob_start();
-		$values = 'algo';
+		$values = 'stuffing';
+        ob_start();
 		$res = GlossaryManager::save_glossary($values);
-		$this->assertTrue(is_null($res)); 
-		ob_end_clean();
-		//var_dump($res);
+        ob_end_clean();
+		$this->assertFalse($res);
+		$values = array('glossary_title'=>'stuffing','glossary_description'=>'something to fill');
+        ob_start();
+        $res = GlossaryManager::save_glossary($values);
+        ob_end_clean();
+        $this->assertTrue(is_numeric($res));
+        $this->assertTrue($res > 0);
+        //clean
+        ob_start();
+        GlossaryManager::delete_glossary($res);
+        ob_end_clean();
 	}
 		
 	function testGetGlossaryTermByGlossaryId() {
-		//ob_start();
 		$glossary_id = 1;
 		$res = GlossaryManager::get_glossary_term_by_glossary_id($glossary_id);
-		$this->assertTrue(is_null($res)); 
-		//ob_end_clean();
-		//var_dump($res);
+		$this->assertTrue(is_string($res)); 
+        $glossary_id = 1000000;
+        $res = GlossaryManager::get_glossary_term_by_glossary_id($glossary_id);
+        $this->assertEqual($res,''); 
 	}
 	
 	function testGetGlossaryTermByGlossaryName() {
-		//ob_start();
 		$glossary_name = '';
 		$res = GlossaryManager::get_glossary_term_by_glossary_name($glossary_name);
 		$this->assertTrue(is_string($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 
-	
 	function testUpdateGlossary() {
 		ob_start();
 		$values = '';
 		$res = GlossaryManager::update_glossary($values);
-		$this->assertTrue(is_null($res)); 
-		ob_end_clean();
-		//var_dump($res);
+        ob_end_clean();
+		$this->assertFalse($res); 
 	}
 	
 	function testGetMaxGlossaryItem() {
-		//ob_start();
 		$res = GlossaryManager::get_max_glossary_item();
 		$this->assertTrue(is_numeric($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	
 	function testGlossaryExists() {
-		//ob_start();
 		$term = '';
 		$not_id = 1;
 		$res = GlossaryManager::glossary_exists($term,$not_id);
 		$this->assertTrue(is_bool($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testGetGlossaryInformation() {
-		//ob_start();
 		$glossary_id = 1;
 		$res = GlossaryManager::get_glossary_information($glossary_id);
 		$this->assertTrue(is_bool($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 
 	function testDisplayGlossary() {
 		ob_start();
 		$res = GlossaryManager::display_glossary();
+        ob_end_clean();
 		$this->assertTrue(is_null($res)); 
-		ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testDisplayGlossaryList() {
 		ob_start();
 		$res = GlossaryManager::display_glossary_list();
-		$this->assertTrue(is_null($res)); 
 		ob_end_clean();
-		//var_dump($res);
+        $this->assertTrue(is_null($res)); 
 	}
 	
 	function testGetNumberGlossaryTerms() {
-		//ob_start();
 		$res = GlossaryManager::get_number_glossary_terms();
 		$this->assertTrue(is_numeric($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testGetGlossaryData() {
-		//ob_start();
 		$from = 1;
 		$number_of_items = 2;
 		$column = 1;
 		$direction = 'ASC';
 		$res = GlossaryManager::get_glossary_data($from, $number_of_items, $column, $direction);
 		$this->assertTrue(is_array($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testActionsFilter() {
-		//ob_start();
 		$glossary_id = 1;
 		$url_params = '';
 		$row = '';
 		$res = GlossaryManager::actions_filter($glossary_id,$url_params,$row);
 		$this->assertTrue(is_string($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testJavascriptGlossary() {
-		//ob_start();
 		$res = GlossaryManager::javascript_glossary();
 		$this->assertTrue(is_string($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testReorderGlossary() {
-		//ob_start();
 		$res = GlossaryManager::reorder_glossary();
 		$this->assertTrue(is_null($res)); 
-		//ob_end_clean();
-		//var_dump($res);
 	}	
 	
 	function testMoveGlossary() {
@@ -145,17 +131,15 @@ class TestGlossary extends UnitTestCase {
 		$direction = '';
 		$glossary_id = '';
 		$res = GlossaryManager::move_glossary($direction, $glossary_id);
+        ob_end_clean();
 		$this->assertTrue(is_null($res)); 
-		ob_end_clean();
-		//var_dump($res);
 	}
 	
 	function testDeleteGlossary() {
 		ob_start();
 		$glossary_id = 1;
 		$res = GlossaryManager::delete_glossary($glossary_id);
+        ob_end_clean();
 		$this->assertTrue(is_null($res)); 
-		ob_end_clean();
-		//var_dump($res);
 	}	
 }

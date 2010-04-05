92328138876882c079f644df8de84a6b4f9b9908
Ricardo Rodriguez <ricardo.rodriguez@beeznest.com>
2010-04-05 12:50:05 -0500

    Modified - modifying the test of function get_language_file_as_xml.

--- tests/main/conference/get_translation.lib.test.php
+++ tests/main/conference/get_translation.lib.test.php
@@ -4,8 +4,14 @@ require_once(api_get_path(SYS_CODE_PATH).'conference/get_translation.lib.php');
 class TestGetTranslation extends UnitTestCase {
 
 	function testget_language_file_as_xml(){
+		ob_start();
 		$res=get_language_file_as_xml($language='english');
-		$this->assertTrue(($res));
+		ob_end_clean();
+		if(is_string($res)) {
+			$this->assertFalse($res);
+		} else {
+		$this->assertTrue($res);
+		}
 		//var_dump($res);
 	}
 }

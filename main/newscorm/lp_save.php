1de5604a9e59925c956573b3d1b0a0428fa1332e
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-04-14 17:55:49 +0300

    Task #272 - Adding html-metadata (encoding, language, text direction) in the file lp-save.php.

--- main/newscorm/lp_save.php
+++ main/newscorm/lp_save.php
@@ -1,33 +1,37 @@
-<?php //$id: $
+<?php
+/* For licensing terms, see /license.txt */
+
 /**
  * Script that handles the saving of item status
  * @package dokeos.learnpath
  * @author Yannick Warnier <ywarnier@beeznest.org>
  */
+
 /**
  * Init
  */
 //done by lp_controller.php
+
 /**
  * Switching within the field to update
  */
 $msg = $_SESSION['oLP']->get_message();
-$charset = 'ISO-8859-15'; //not taken into account here as we don't include a header
-error_log('New LP - Loaded lp_save : '.$_SERVER['REQUEST_URI'].' from '.$_SERVER['HTTP_REFERER'],0);
+
+error_log('New LP - Loaded lp_save : '.$_SERVER['REQUEST_URI'].' from '.$_SERVER['HTTP_REFERER'], 0);
 ?>
 <!DOCTYPE html
      PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo api_get_language_isocode(); ?>" lang="<?php echo api_get_language_isocode(); ?>">
 <head>
-
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo api_get_system_encoding(); ?>" />
 <script language='javascript'>
 <?php
-if($_SESSION['oLP']->mode != 'fullscreen'){
+if ($_SESSION['oLP']->mode != 'fullscreen'){
 }
 ?>
 </script>
 
 </head>
-<body>
+<body dir="<?php echo api_get_text_direction(); ?>">
 </body></html>

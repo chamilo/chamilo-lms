084ca9d079a288419f19b12797b3ea91ef4da906
Cristian Fasanando <cristian.fasanando@beeznest.com>
2010-04-22 19:45:09 -0500

    added category id field inside gradebook_score_display for filtering by course - private BT#942

--- main/install/db_main.sql
+++ main/install/db_main.sql
@@ -763,7 +763,7 @@ VALUES
 ('timezone_value', 'timezones', 'select', 'Timezones', '', 'TimezoneValueTitle','TimezoneValueComment',NULL,'Timezones', 1),
 ('allow_user_course_subscription_by_course_admin', NULL, 'radio', 'Security', 'true', 'AllowUserCourseSubscriptionByCourseAdminTitle', 'AllowUserCourseSubscriptionByCourseAdminComment', NULL, NULL, 1),
 ('show_link_bug_notification', NULL, 'radio', 'Platform', 'true', 'ShowLinkBugNotificationTitle', 'ShowLinkBugNotificationComment', NULL, NULL, 0),
-('dokeos_database_version', NULL, 'textfield', NULL,'1.8.7.11515','DokeosDatabaseVersion','',NULL,NULL,0);
+('dokeos_database_version', NULL, 'textfield', NULL,'1.8.7.11569','DokeosDatabaseVersion','',NULL,NULL,0);
 
 
 UNLOCK TABLES;
@@ -1171,8 +1171,11 @@ CREATE TABLE gradebook_score_display (
   id int NOT NULL auto_increment,
   score float unsigned NOT NULL,
   display varchar(40) NOT NULL,
+  category_id int NOT NULL default 0,
   PRIMARY KEY (id)
 );
+ALTER TABLE gradebook_score_display ADD INDEX(category_id);
+
 DROP TABLE IF EXISTS user_field;
 CREATE TABLE user_field (
 	id	INT NOT NULL auto_increment,

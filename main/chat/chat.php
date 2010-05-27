621d59578a157b516c57d5ffe4ef53c6f27729b4
Juan Carlos Raña <juancarlos@innovasoc.com>
2010-05-27 13:03:21 +0200

    improve chat presentation and kill frameset

--- main/chat/chat.php
+++ main/chat/chat.php
@@ -1,17 +1,11 @@
 <?php
 /* For licensing terms, see /license.txt */
 
-/**
- *	Frameset of the Chat tool
- *
- *	@author Olivier Brouckaert
- *	@package chamilo.chat
- */
-
 $language_file = array('chat');
 
 require_once '../inc/global.inc.php';
-
+require_once api_get_path(LIBRARY_PATH).'groupmanager.lib.php';
+$this_section = SECTION_COURSES;
 $nameTools = get_lang('ToolChat');
 
 if ($_GET["origin"] != 'whoisonline') {
@@ -54,38 +48,6 @@ if (!empty($mycourseid) && $mycourseid != -1) {
 	}
 	$open_chat_window = api_get_course_setting('allow_open_chat_window');
 }
-if (api_get_setting('show_navigation_menu') != 'false') {
-	$footer_size = 20;
-} else {	
-	$top_size = 135;
-	switch($my_style) {	
-		case 'chamilo_red' :
-		case 'chamilo_green' :	
-		case 'chamilo' :
-			$top_size = 185;
-			$footer_size = 48;
-		break;
-		case 'dokeos_classic' :
-		case 'chamilo_classic' :
-			$footer_size = 48;
-			break;
-		case 'academica' :
-			$footer_size = 140;
-			break;
-		case 'silver_line' :
-			$footer_size = 60;
-			break;
-		case 'baby_orange' :
-			$footer_size = 120;
-			break;
-		case 'public_admin' :
-			$footer_size =90;
-			break;
-		default :
-			$footer_size = 48;
-			break;
-	}
-}
 
 $cidreq = Security::remove_XSS($_GET['cidReq']);
 
@@ -99,29 +61,52 @@ $cidreq = Security::remove_XSS($_GET['cidReq']);
 <?php
 echo'<title>'.get_lang('Chat').' - '.$mycourseid.' - '.api_get_setting('siteName').'</title>';
 
-if (empty($open_chat_window)) {
-	echo'<frameset rows="'.$top_size.',*,'.$footer_size.'" border="0" frameborder="0" framespacing="1">';
-	echo '<frame src="chat_banner.php?cidReq='.$cidreq.'" name="chat_banner" scrolling="no">';
-}
+// If it is a group chat then the breadcrumbs.
+if ($_SESSION['_gid'] OR $_GET['group_id']) {
 
-if (api_get_setting('show_navigation_menu') == 'false' || !empty($open_chat_window)) {
-	echo '<frameset cols="165,*,0" border="1" frameborder="1" framespacing="1">';
-} else {
-	echo '<frameset cols="165,*,200" border="1" frameborder="1" framespacing="1">';
-}
-echo '<frame src="chat_whoisonline.php?cidReq='.$cidreq.'" name="chat_whoisonline" scrolling="auto">';
-echo'<frameset rows="25,15" border="1" frameborder="1" framespacing="1">';
-echo '<frame src="chat_chat.php?origin='.Security::remove_XSS($_GET['origin']).'&target='.Security::remove_XSS($_GET['target']).'&amp;cidReq='.$cidreq.'" name="chat_chat" scrolling="auto">';
-echo '<frame src="chat_message.php?cidReq='.$cidreq.'" name="chat_message" scrolling="no">';
-echo '</frameset>';
-echo '<frame src="chat_hidden.php?cidReq='.$cidreq.'" name="chat_hidden" >';
-echo'</frameset>';
-
-if (api_get_setting('show_navigation_menu') == 'false') {
-	if (empty($open_chat_window)) {
-		echo '<frame src="chat_footer.php?cidReq='.$cidreq.'" name="chat_footer" scrolling="no">';
-		echo '</frameset>';
+	if (isset($_SESSION['_gid'])) {
+		$_clean['group_id'] = (int)$_SESSION['_gid'];
 	}
+	if (isset($_GET['group_id'])) {
+		$_clean['group_id'] = (int)Database::escape_string($_GET['group_id']);
+	}
+
+	$group_properties  = GroupManager :: get_group_properties($_clean['group_id']);
+	$interbreadcrumb[] = array('url' => '../group/group.php', 'name' => get_lang('Groups'));
+	$interbreadcrumb[] = array('url' => '../group/group_space.php?gidReq='.$_SESSION['_gid'], 'name' => get_lang('GroupSpace').' ('.$group_properties['name'].')');
+	$noPHP_SELF = true;
+	$shortBanner = false;
+	$add_group_to_title = ' ('.$group_properties['name'].')';
+	$groupfilter = 'group_id="'.$_clean['group_id'].'"';
+
+	// Ensure this tool in groups whe it's private or deactivated
+	/*if ($group_properties['chat_state'] == 0) {
+		echo api_not_allowed();
+	} elseif ($group_properties['chat_state'] == 2) {
+ 		if (!api_is_allowed_to_edit(false,true) and !GroupManager :: is_user_in_group($_user['user_id'], $_SESSION['_gid'])) {
+			echo api_not_allowed();
+		}
+	}*/
+
+} else {
+	$groupfilter = 'group_id=0';
 }
-echo'<noframes></noframes>';
+
+//$is_allowed_to_edit = api_is_allowed_to_edit(false, true);
+
+
+if (empty($open_chat_window)) {
+Display::display_header($tool_name, 'Chat');
+}
+	
+
+echo '<iframe src="chat_whoisonline.php?cidReq='.$cidreq.'" name="chat_whoisonline" scrolling="auto" style="height:320px; width:19%; border: 0px none; float:left"></iframe>';
+echo '<iframe src="chat_chat.php?origin='.Security::remove_XSS($_GET['origin']).'&target='.Security::remove_XSS($_GET['target']).'&amp;cidReq='.$cidreq.'" name="chat_chat" scrolling="auto" height="240" style="width:80%; border: 0px none; float:right"></iframe>';
+echo '<iframe src="chat_message.php?cidReq='.$cidreq.'" name="chat_message" scrolling="no" height="80" style="width:80%; border: 0px none; float:right"></iframe>';
+echo '<iframe src="chat_hidden.php?cidReq='.$cidreq.'" name="chat_hidden" height="0" style="border: 0px none"></iframe>';
+
+if (empty($open_chat_window)) {
+Display::display_footer();
+}
+
 echo '</html>';

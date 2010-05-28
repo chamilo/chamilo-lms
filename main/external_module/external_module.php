dd1bda67bd66b85900e11f2ecb290a135b7e6081
Julio Montoya <gugli100@gmail.com>
2010-05-28 15:53:43 +0200

    Changing headers

--- main/external_module/external_module.php
+++ main/external_module/external_module.php
@@ -1,26 +1,5 @@
 <?php
-/*
-==============================================================================
-	Dokeos - elearning and course management software
-
-	Copyright (c) 2004 Dokeos S.A.
-	Copyright (c) 2003 Ghent University (UGent)
-	Copyright (c) 2001 Universite catholique de Louvain (UCL)
-	Copyright (c) various contributors
-
-	For a full list of contributors, see "credits.txt".
-	The full license can be read in "license.txt".
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	See the GNU General Public License for more details.
-
-	Contact: Dokeos, 181 rue Royale, B-1000 Brussels, Belgium, info@dokeos.com
-==============================================================================
-*/
+/* For licensing terms, see /license.txt */
 /**
 ==============================================================================
 *	This script is used for adding hyperlinks to a course homepage.
@@ -32,7 +11,8 @@
 *	so the external module - html file include has to be refactored to
 *	reuse documents tool code.
 *
-*	@package dokeos.external_module
+*	@package chamilo.external_module
+*	@todo this code is useless? 
 ==============================================================================
 */
 

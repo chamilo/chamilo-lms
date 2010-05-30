71967f64edbecb35297edbde3f6e3cb5caedb990
Yannick Warnier <yannick.warnier@beeznest.com>
2010-05-30 13:10:30 -0500

    Added gradebook lang file loading to fix missing terms

--- main/document/upload.php
+++ main/document/upload.php
@@ -34,7 +34,7 @@
  */
 
 // Name of the language file that needs to be included
-$language_file = 'document';
+$language_file[] = array('document','gradebook');
 
 // Including the global initialization file
 require_once '../inc/global.inc.php';

22dac83fa84c92498c2049afd6b4e4db82924221
Yannick Warnier <yannick.warnier@beeznest.com>
2010-07-19 16:12:27 -0500

    fixed test failure and removed useless test

--- tests/main/conference/get_translation.lib.test.php
+++ tests/main/conference/get_translation.lib.test.php
@@ -3,16 +3,15 @@ require_once(api_get_path(SYS_CODE_PATH).'conference/get_translation.lib.php');
 
 class TestGetTranslation extends UnitTestCase {
 
+        public function __construct() {
+            $this->UnitTestCase('Conference translation getter library - main/conference/get_translation.lib.test.php');
+        }
+
 	function testget_language_file_as_xml(){
 		ob_start();
 		$res=get_language_file_as_xml($language='english');
 		ob_end_clean();
-		if(is_string($res)) {
-			$this->assertFalse($res);
-		} else {
-		$this->assertTrue($res);
-		}
-		//var_dump($res);
+		$this->assertTrue(is_string($res));
 	}
 }
 ?>

65e237109c063eeea4b94ba5d1b74a56446cf531
Yannick Warnier <yannick.warnier@beeznest.com>
2010-08-02 01:45:09 -0500

    added default cronnable cleanup script (cleans the archive director after a week)

---
+++ main/cron/cleanup.php
@@ -0,0 +1,32 @@
+<?php
+if (php_sapi_name() != 'cli') { exit; } //do not run from browser
+$dir = dirname(__FILE__);
+$a_dir = realpath($dir.'/../../archive/');
+$list = scandir($a_dir);
+$t = time()-(86400*7);
+foreach($list as $item) {
+	if (substr($item,0,1) == '.') { continue; }
+	$stat = @stat($a_dir.'/'.$item);
+	if ($stat === false) { error_log('Cron task cannot stat '.$a_dir.'/'.$item); continue; }
+	if ($stat['mtime'] > $t) { //if the file is older than one week, delete
+		recursiveDelete($a_dir.'/'.$item);
+	}
+}
+
+/**
+ * Delete a file or recursively delete a directory
+ *
+ * @param string $str Path to file or directory
+ */
+function recursiveDelete($str){
+	if(is_file($str)){
+		return @unlink($str);
+	}
+	elseif(is_dir($str)){
+		$scan = glob(rtrim($str,'/').'/*');
+		foreach($scan as $index=>$path){
+			recursiveDelete($path);
+		}
+		return @rmdir($str);
+	}
+}

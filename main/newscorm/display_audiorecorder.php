d902106f3f4bb3849daac1b676d779d2967ba31e
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-09-24 16:51:07 +0300

    Task #1765 - Code cleaning - newscorm/audiorecorder.inc.php, newscorm/display_audiorecorder.php.

--- main/newscorm/display_audiorecorder.php
+++ main/newscorm/display_audiorecorder.php
@@ -1,42 +1,42 @@
 <?php
 /* For licensing terms, see /license.txt */
+
 /**
  * Script opened in an iframe and containing the learning path's table of contents
  * @package chamilo.learnpath
  * @author Yannick Warnier <ywarnier@beeznest.org>
  * @license	GNU/GPL
  */
-//flag to allow for anonymous user - needs to be set before global.inc.php
+
+// Flag to allow for anonymous user - needs to be set before global.inc.php
 $use_anonymous = true;
 require_once 'back_compat.inc.php';
 require_once 'learnpath.class.php';
 require_once 'scorm.class.php';
 require_once 'aicc.class.php';
 
-if(isset($_SESSION['lpobject']))
-{
+if (isset($_SESSION['lpobject'])) {
 	$oLP = unserialize($_SESSION['lpobject']);
-	if(is_object($oLP)){
+	if (is_object($oLP)) {
 		$_SESSION['oLP'] = $oLP;
-	}else{
-		die('Could not instanciate lp object');
+	} else {
+		die('Could not instanciate lp object.');
 	}
 }
 
-$lp_theme_css=$_SESSION['oLP']->get_theme();
-$scorm_css_header=true;
+$lp_theme_css = $_SESSION['oLP']->get_theme();
+$scorm_css_header = true;
 include_once '../inc/reduced_header.inc.php';
-echo '<html>
-		<body>';
+
+echo '<body dir="'.api_get_text_direction().'">';
 
 		echo '<div id="audiorecorder">	';
 		$audio_recorder_studentview = 'true';
 		$audio_recorder_item_id = $_SESSION['oLP']->current;
-		if(api_get_setting('service_visio','active')=='true'){
+		if (api_get_setting('service_visio', 'active') == 'true') {
 			include 'audiorecorder.inc.php';
 		}
 		echo '</div>';
 		// end of audiorecorder include
 
 echo '</body></html>';
-?>

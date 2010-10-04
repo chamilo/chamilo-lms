45a459c7b70f54ed702fae67900889ea85aaeb37
Ivan Tcholakov <ivantcholakov@gmail.com>
2010-10-04 09:49:13 +0300

    Feature #2099 - A new library file "course_request.lib.php" has been added.

---
+++ main/inc/lib/course_request.lib.php
@@ -0,0 +1,21 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ * Course request manager
+ * @package chamilo.library
+ *
+ * @author José Manuel Abuin Mosquera <chema@cesga.es>, 2010
+ * @author Bruno Rubio Gayo <brubio@cesga.es>, 2010
+ * Centro de Supercomputacion de Galicia (CESGA)
+ *
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com> (technical adaptation for Chamilo 1.8.8), 2010
+ */
+
+define(COURSE_REQUEST_PENDING,  0);
+define(COURSE_REQUEST_ACCEPTED, 1);
+define(COURSE_REQUEST_REJECTED, 2);
+
+class CourseRequestManager {
+
+}

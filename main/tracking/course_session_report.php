51612f4daed284b984c7ec30de292b0dffaae86e
Julio Montoya <gugli100@gmail.com>
2010-10-14 17:42:25 +0200

    Fixing reports to get exercise from lp results

--- main/tracking/course_session_report.php
+++ main/tracking/course_session_report.php
@@ -140,7 +140,7 @@ foreach($course_list  as $current_course ) {
 		$exercise_list = get_all_exercises_from_lp($lp_id, $current_course['db_name']);		
 		//Looping Chamilo Exercises in LP
 		foreach ($exercise_list as $exercise) {
-			$exercise_stats = get_all_exercise_event($exercise['path'],$course_info['id'], $session_id);
+			$exercise_stats = get_all_exercise_event_from_lp($exercise['path'],$course_info['id'], $session_id);
 			//Looping Exercise Attempts
 			foreach($exercise_stats as $stats) {
 				$attempt_result[$stats['exe_user_id']]['result'] += $stats['exe_result'] / $stats['exe_weighting'];		

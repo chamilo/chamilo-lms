e6538e44011d4ede1a715b1441fce62994a9a626
Julio Montoya <gugli100@gmail.com>
2010-10-21 18:48:08 +0200

    Minor - Changing column order

--- main/exercice/exercice_history.php
+++ main/exercice/exercice_history.php
@@ -10,27 +10,22 @@
 * 	@version $Id:exercice.php 12269 2007-05-03 14:17:37Z elixir_julian $
 */
 
-
 // name of the language file that needs to be included
 $language_file='exercice';
 
-require_once('../inc/global.inc.php');
+require_once '../inc/global.inc.php';
 $this_section=SECTION_COURSES;
 api_protect_course_script(true);
 
 $show=(isset($_GET['show']) && $_GET['show'] == 'result')?'result':'test'; // moved down to fix bug: http://www.dokeos.com/forum/viewtopic.php?p=18609#18609
 
-/*
-	Libraries
-*/
+/*	Libraries */
 
 require_once api_get_path(LIBRARY_PATH).'document.lib.php';
 //include(api_get_path(LIBRARY_PATH).'mail.lib.inc.php');
 require_once api_get_path(LIBRARY_PATH).'usermanager.lib.php';
 
-/*
-	Constants and variables
-*/
+/* 	Constants and variables */
 $is_allowedToEdit = api_is_allowed_to_edit(null,true);
 $is_tutor = api_is_allowed_to_edit(true);
 
@@ -77,24 +72,18 @@ Display::display_header($nameTools,"Exercise");
 		 <tr class="row_odd">
 		  <th><?php echo get_lang('Question'); ?></th>
 		  <th width="50px"><?php echo get_lang('Value'); ?></th>
-		  <th><?php echo get_lang('Feedback'); ?></th>
-		  <th width="150px"><?php echo get_lang('Date'); ?></th>
+		  <th><?php echo get_lang('Feedback'); ?></th>		  
 		  <th ><?php echo get_lang('Author'); ?></th>
+          <th width="150px"><?php echo get_lang('Date'); ?></th>
 		 </tr>
 <?php
 
-//Display::display_introduction_section(TOOL_QUIZ);
-/*
-$sql = 'SELECT * FROM '.$TBL_EXERCICES;
-$query = Database::query($sql);
-*/
 $sql = "SELECT *, quiz_question.question, firstname, lastname FROM $TBL_TRACK_ATTEMPT_RECORDING t,$TBL_USER,$TBL_EXERCICES_QUESTION quiz_question WHERE quiz_question.id = question_id AND user_id = author AND exe_id = '".(int)$_GET['exe_id']."' ORDER BY t.insert_date desc,question ASC";
 $query = Database::query($sql);
 while($row = Database::fetch_array($query)){
 	echo '<tr';
 	if($i%2==0) echo 'class="row_odd"'; else echo 'class="row_even"';
 	echo '>';
-
 	echo '<td>'.$row['question'].'</td>';
 	echo '<td>'.$row['marks'].'</td>';
 	if(!empty($row['teacher_comment'])){
@@ -102,10 +91,9 @@ while($row = Database::fetch_array($query)){
 	} else {
 		echo '<td>'.get_lang('WithoutComment').'</td>';
 	}
+    echo '<td>'.(empty($row['firstname']) && empty($row['lastname']) ? '<i>'.get_lang('OriginalValue').'</i>' : api_get_person_name($row['firstname'], $row['lastname'])).'</td>';
 	echo '<td>'.api_get_local_time($row['insert_date']).'</td>';
-	echo '<td>'.(empty($row['firstname']) && empty($row['lastname']) ? '<i>'.get_lang('OriginalValue').'</i>' : api_get_person_name($row['firstname'], $row['lastname'])).'</td>';
-
-	echo '</tr>';
+    echo '</tr>';
 }
 echo '</table>';
 Display::display_footer();

549ab51f310e9828ba14d17fa8627ea4721ccda2
Yannick Warnier <yannick.warnier@beeznest.com>
2010-11-16 10:51:02 -0500

    Improvements to BBB plugin installation procedure

--- plugin/bbb/install.php
+++ plugin/bbb/install.php
@@ -38,7 +38,8 @@ while ($row = Database::fetch_assoc($res)) {
     $r = Database::query($sql_course);
     $sql_course = "INSERT INTO $t_course (variable,value,category) VALUES ('big_blue_button_welcome_message','','plugins')";
     $r = Database::query($sql_course);
-    $t_tool = Database::get_course_table(TABLE_TOOL_LIST,$cdb);
-    $sql_course = "INSERT INTO $t_tool VALUES ((NULL, 'videoconference','../../plugin/bbb/start.php','visio.gif','".string2binary(api_get_setting('course_create_active_tools', 'videoconference'))."','0','squaregrey.gif','NO','_self','plugin','0'))";
+    $t_tool = Database::get_course_table(TABLE_TOOL_LIST,$row['db_name']);
+    $sql_course = "INSERT INTO $t_tool VALUES (NULL, 'videoconference','../../plugin/bbb/start.php','visio.gif','".string2binary(api_get_setting('course_create_active_tools', 'videoconference'))."','0','squaregrey.gif','NO','_self','plugin','0')";
+    error_log($sql_course);
     $r = Database::query($sql_course);
 }

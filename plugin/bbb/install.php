a5749278ac9420abdca36ce645c09d694d182fba
Yannick Warnier <yannick.warnier@beeznest.com>
2010-11-16 11:47:59 -0500

    Minor - removed error_log from BBB install script

--- plugin/bbb/install.php
+++ plugin/bbb/install.php
@@ -53,6 +53,5 @@ while ($row = Database::fetch_assoc($res)) {
     $r = Database::query($sql_course);
     $t_tool = Database::get_course_table(TABLE_TOOL_LIST,$row['db_name']);
     $sql_course = "INSERT INTO $t_tool VALUES (NULL, 'videoconference','../../plugin/bbb/start.php','visio.gif','".string2binary(api_get_setting('course_create_active_tools', 'videoconference'))."','0','squaregrey.gif','NO','_blank','plugin','0')";
-    error_log($sql_course);
     $r = Database::query($sql_course);
 }

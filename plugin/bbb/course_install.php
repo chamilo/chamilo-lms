1d0038884748d2d28f11c570fca126f8f781e710
Yannick Warnier <yannick.warnier@beeznest.com>
2010-11-16 01:04:38 -0500

    Added tool entry for plugin

--- plugin/bbb/course_install.php
+++ plugin/bbb/course_install.php
@@ -12,3 +12,6 @@ $sql_course = "INSERT INTO $t_course (variable,value,category) VALUES ('big_blue
 $r = Database::query($sql_course);
 $sql_course = "INSERT INTO $t_course (variable,value,category) VALUES ('big_blue_button_welcome_message','','plugins')";
 $r = Database::query($sql_course);
+$t_tool = Database::get_course_table(TABLE_TOOL_LIST,$cdb);
+$sql_course = "INSERT INTO $t_tool VALUES ((NULL, 'videoconference','../../plugin/bbb/start.php','visio.gif','".string2binary(api_get_setting('course_create_active_tools', 'videoconference'))."','0','squaregrey.gif','NO','_self','plugin','0'))";
+$r = Database::query($sql_course);

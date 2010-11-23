0d3404c3bdbf1e108f94b6e623776e8d80f7bd42
Christian Fasanando <christian.fasanando@beeznest.com>
2010-11-23 12:07:03 -0500

    updating ppt convert for office 3 and posibility to convert it with webservices

--- main/newscorm/openoffice_document.class.php
+++ main/newscorm/openoffice_document.class.php
@@ -53,79 +53,133 @@ abstract class OpenofficeDocument extends learnpath {
 
         $this->created_dir = create_unexisting_directory($_course, $_user['user_id'], 0, 0, $this->base_work_dir, $dir_name);
 
-        move_uploaded_file($file['tmp_name'], $this->base_work_dir.'/'.$this->file_path);
+        $ppt2lp_host = api_get_setting('service_ppt2lp', 'host');
 
-        $perm = api_get_setting('permissions_for_new_files');
+        if ($ppt2lp_host == 'localhost') {
+          
+          move_uploaded_file($file['tmp_name'], $this->base_work_dir.'/'.$this->file_path);
+          $perm = api_get_setting('permissions_for_new_files');
+          if (IS_WINDOWS_OS) { // IS_WINDOWS_OS has been defined in main_api.lib.php
+              $converter_path = str_replace('/', '\\', api_get_path(SYS_PATH).'main/inc/lib/ppt2png');
+              $class_path = $converter_path.';'.$converter_path.'/jodconverter-2.2.2.jar;'.$converter_path.'/jodconverter-cli-2.2.2.jar';
+              //$cmd = 'java -cp "'.$class_path.'" DokeosConverter';
+              $cmd = 'java -Dfile.encoding=UTF-8 -cp "'.$class_path.'" DokeosConverter';
+          } else {
+              $converter_path = api_get_path(SYS_PATH).'main/inc/lib/ppt2png';
+              //$class_path = '-cp .:jodconverter-2.2.1.jar:jodconverter-cli-2.2.1.jar';
+              $class_path = ' -Dfile.encoding=UTF-8 -cp .:jodconverter-2.2.2.jar:jodconverter-cli-2.2.2.jar';
+              $cmd = 'cd '.$converter_path.' && java '.$class_path.' DokeosConverter';
+          }
+          $cmd .=  ' -p '.api_get_setting('service_ppt2lp', 'port');
+          // Call to the function implemented by child.
+          $cmd .= $this -> add_command_parameters();
+          // To allow openoffice to manipulate docs.
+          @chmod($this->base_work_dir.$this->created_dir, 0777);
+          @chmod($this->base_work_dir.$this->file_path, 0777);
 
-        /*
-        $classpath = '-cp .:jodconverter-2.2.1.jar:jodconverter-cli-2.2.1.jar';
-        if (isset($_ENV['OS']) && strpos($_ENV['OS'], 'Windows') !== false) {
-            $classpath = str_replace(':', ';', $classpath);
-        }
-        if (isset($_ENV['OS']) && strpos($_ENV['OS'], 'Windows') !== false) {
-            $cmd = 'cd '.str_replace('/', '\\', api_get_path(SYS_PATH).'main/inc/lib/ppt2png ').$classpath.' DokeosConverter';
+          $locale = $this->original_locale; // TODO: Improve it because we're not sure this locale is present everywhere.
+          putenv('LC_ALL='.$locale);
+
+          $files = array(); $return = 0;
+          $shell = exec($cmd, $files, $return);
+
+          if ($return != 0) { // If the java application returns an error code.
+              switch ($return) {
+                  // Can't connect to openoffice.
+                  case 1: $this->error = get_lang('CannotConnectToOpenOffice'); break;
+                  // Conversion failed in openoffice.
+                  case 2: $this->error = get_lang('OogieConversionFailed'); break;
+                  // Conversion can't be launch because command failed.
+                  case 255: $this->error = get_lang('OogieUnknownError'); break;
+              }
+              DocumentManager::delete_document($_course, $dir_name, $this->base_work_dir);
+              return false;
+          }
+
         } else {
-            $cmd = 'cd '.api_get_path(SYS_PATH).'main/inc/lib/ppt2png && java '.$classpath.' DokeosConverter';
-        }
-        $cmd .=  ' -p '.api_get_setting('service_ppt2lp','port');
-        */
-        if (IS_WINDOWS_OS) { // IS_WINDOWS_OS has been defined in main_api.lib.php
-            $converter_path = str_replace('/', '\\', api_get_path(SYS_PATH).'main/inc/lib/ppt2png');
-            $class_path = $converter_path.';'.$converter_path.'/jodconverter-2.2.1.jar;'.$converter_path.'/jodconverter-cli-2.2.1.jar';
 
-            //$cmd = 'java -cp "'.$class_path.'" DokeosConverter';
-            $cmd = 'java -Dfile.encoding=UTF-8 -cp "'.$class_path.'" DokeosConverter';
-        } else {
-            $converter_path = api_get_path(SYS_PATH).'main/inc/lib/ppt2png';
+          // get result from webservices
+          $result = $this->_get_remote_ppt2lp_files($file);
+          $result = unserialize(base64_decode($result));
 
-            //$class_path = '-cp .:jodconverter-2.2.1.jar:jodconverter-cli-2.2.1.jar';
-            $class_path = ' -Dfile.encoding=UTF-8 -cp .:jodconverter-2.2.1.jar:jodconverter-cli-2.2.1.jar';
-
-            $cmd = 'cd '.$converter_path.' && java '.$class_path.' DokeosConverter';
-        }
-        $cmd .=  ' -p '.api_get_setting('service_ppt2lp', 'port');
-
-        // Call to the function implemented by child.
-        $cmd .= $this -> add_command_parameters();
-
-        // To allow openoffice to manipulate docs.
-        @chmod($this->base_work_dir.$this->created_dir, 0777);
-        @chmod($this->base_work_dir.$this->file_path, 0777);
-
-        $locale = $this->original_locale; // TODO: Improve it because we're not sure this locale is present everywhere.
-        putenv('LC_ALL='.$locale);
-        $files = array(); $return = 0;
-        $shell = exec($cmd, $files, $return);
-        if ($return != 0) { // If the java application returns an error code.
-            switch ($return) {
-                // Can't connect to openoffice.
-                case 1: $this->error = get_lang('CannotConnectToOpenOffice'); break;
-
-                // Conversion failed in openoffice.
-                case 2: $this->error = get_lang('OogieConversionFailed'); break;
-
-                // Conversion can't be launch because command failed.
-                case 255: $this->error = get_lang('OogieUnknownError'); break;
+          // save remote images to server
+          chmod ($this->base_work_dir.$this->created_dir, api_get_permissions_for_new_directories());
+          if (!empty($result['images'])) {
+            foreach ($result['images'] as $image => $img_data) {
+              $image_path = $this->base_work_dir.$this->created_dir;
+              @file_put_contents($image_path.'/'.$image, base64_decode($img_data));
+              @chmod($image_path.'/'.$image, 0777);
             }
+          }
 
-            DocumentManager::delete_document($_course, $dir_name, $this->base_work_dir);
-            return false;
+          // files info
+          $files = $result['files'];
+
         }
 
-        // Create lp.
-        $this->lp_id = learnpath::add_lp($_course['id'], ucfirst(pathinfo($file['name'], PATHINFO_FILENAME)), '', 'guess', 'manual');
+        if (!empty($files)) {
+          // Create lp.
+          $this->lp_id = learnpath::add_lp($_course['id'], ucfirst(pathinfo($file['name'], PATHINFO_FILENAME)), '', 'guess', 'manual');
 
-        // Call to the function implemented by child following action_after_conversion parameter.
-        switch ($action_after_conversion) {
-            case 'make_lp': $this -> make_lp($files);
-                break;
-            case 'add_docs_to_visio': $this -> add_docs_to_visio($files);
-                break;
+          // Call to the function implemented by child following action_after_conversion parameter.
+          switch ($action_after_conversion) {
+              case 'make_lp': $this -> make_lp($files);
+                  break;
+              case 'add_docs_to_visio': $this -> add_docs_to_visio($files);
+                  break;
+          }
+
+          chmod ($this->base_work_dir.$this->created_dir, api_get_permissions_for_new_directories());
         }
-
-        chmod ($this->base_work_dir.$this->created_dir, api_get_permissions_for_new_directories());
         return $this->first_item;
     }
+
+    /**
+     * Get images files from remote host (with webservices)
+     * @param   array current ppt file
+     * @return  array images files
+     */
+    private function _get_remote_ppt2lp_files($file) {
+
+      require_once api_get_path(LIBRARY_PATH).'nusoap/nusoap.php';
+      
+      // host
+      $ppt2lp_host = api_get_setting('service_ppt2lp', 'host');
+
+
+      // secret key
+      $secret_key = sha1(api_get_setting('service_ppt2lp', 'ftp_password'));
+      //$secret_key = sha1($_SERVER['REMOTE_ADDR'].api_get_setting('service_ppt2lp', 'ftp_password'));
+      //error_log('IP:'.$_SERVER['REMOTE_ADDR']);
+      
+      // client
+      $client = new nusoap_client($ppt2lp_host, true);
+
+      $result = '';
+      $err = $client->getError();
+      if (!$err) {
+
+        $file_data  = base64_encode(file_get_contents($file['tmp_name']));
+        $file_name  = $file['name'];
+        $service_ppt2lp_size = api_get_setting('service_ppt2lp', 'size');
+
+        $params = array(
+                      'secret_key'  => $secret_key,
+                      'file_data'   => $file_data,
+                      'file_name'   => $file_name,
+                      'service_ppt2lp_size' => $service_ppt2lp_size,
+                  );
+        
+        $result = $client->call('ws_convert_ppt', array('convert_ppt' => $params));
+
+      } else {
+        return false;
+      }
+
+      return $result;
+
+    }
+
 
     abstract function make_lp();
     abstract function add_docs_to_visio();

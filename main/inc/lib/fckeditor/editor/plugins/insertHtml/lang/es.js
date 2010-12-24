814d650f983f534392ccc1f4a43b094b60f61775
Juan Carlos Raña <juancarlos@innovasoc.com>
2010-12-24 01:56:08 +0100

    Feature #2479 some adjustments of insertHTML plugin for Chamilo

---
+++ main/inc/lib/fckeditor/editor/plugins/insertHtml/lang/es.js
@@ -0,0 +1,6 @@
+
+
+	FCKLang.inserHTML_buttonTooltip					= 'Insertar HTML';
+	FCKLang.insertHtml_dialogTitle						= 'Insertar HTML';
+	FCKLang.inserHtml_help								= 'Introduzca cualquier código HTML debajo y haga clic \'ok\' para insertarlo en la posición en la que se encuentra el cursos en el editor.';
+
---
+++ main/inc/lib/fckeditor/editor/plugins/insertHtml/lang/nl.js
@@ -0,0 +1,5 @@
+
+
+	FCKLang.inserHTML_buttonTooltip					= 'HTML invoegen';
+	FCKLang.insertHtml_dialogTitle						= 'HTML invoegen';
+	FCKLang.inserHtml_help								= 'Geef HTML code in en klik op \'ok\' om de code in te voegen op de locatie van de cursor in de editor.';

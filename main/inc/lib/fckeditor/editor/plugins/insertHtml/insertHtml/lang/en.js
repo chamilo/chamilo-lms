3931c3865fb48312eb7316e6f0f8fbcbdb522a01
Juan Carlos Raña <juancarlos@innovasoc.com>
2010-12-24 01:57:07 +0100

    Feature #2479 delete ancient files

--- main/inc/lib/fckeditor/editor/plugins/insertHtml/insertHtml/lang/en.js
+++
@@ -1,6 +0,0 @@
-
-
-	FCKLang.inserHTML_buttonTooltip					= 'Insert HTML';
-	FCKLang.insertHtml_dialogTitle						= 'Insert HTML';
-	FCKLang.inserHtml_help								= 'Enter any HTML below and click \'ok\' to insert it at the location of the cursor in the editor.';
-

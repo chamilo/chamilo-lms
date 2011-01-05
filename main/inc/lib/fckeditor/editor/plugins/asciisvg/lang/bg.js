147b6ad887424991c49cced5f0fa1cced7912cc2
Ivan Tcholakov <ivantcholakov@gmail.com>
2011-01-05 14:55:27 +0200

    Feature #2132 - "asciisvg" plug-in: Opening the language files.

---
+++ main/inc/lib/fckeditor/editor/plugins/asciisvg/lang/bg.js
@@ -0,0 +1,50 @@
+/*
+ * File Name: bg.js
+ * asciisvg plugin English language file.
+ * Encoding: UTF-8.
+ *
+ * Author: Ivan Tcholakov (ivantcholakov@gmail.com), 2011
+ */
+
+FCKLang["DlgAsciiSvg"] = 'Математическа графика' ;
+FCKLang["DlgAsciiSvgGraphEditor"] = 'Редактор на математически графики' ;
+FCKLang["DlgAsciiSvgEquationType"] = 'Вид графика:' ;
+FCKLang["DlgAsciiSvgFrom"] = 'от' ;
+FCKLang["DlgAsciiSvgTo"] = 'до' ;
+FCKLang["DlgAsciiSvgEvery"] = 'за всеки' ;
+FCKLang["DlgAsciiSvgDlgAsciiSvgColor"] = 'Цвят:' ;
+FCKLang["DlgAsciiSvgBlack"] = 'черен' ;
+FCKLang["DlgAsciiSvgRed"] = 'червен' ;
+FCKLang["DlgAsciiSvgOrange"] = 'оранжев' ;
+FCKLang["DlgAsciiSvgYellow"] = 'жълт' ;
+FCKLang["DlgAsciiSvgGreen"] = 'зелен' ;
+FCKLang["DlgAsciiSvgBlue"] = 'син' ;
+FCKLang["DlgAsciiSvgPurple"] = 'виолетов' ;
+FCKLang["DlgAsciiSvgWidth"] = 'Широчина:' ;
+FCKLang["DlgAsciiSvgLine"] = 'Тип линия:' ;
+FCKLang["DlgAsciiSvgSolid"] = 'плътна' ;
+FCKLang["DlgAsciiSvgDotted"] = 'точки' ;
+FCKLang["DlgAsciiSvgDashed"] = 'тирета' ;
+FCKLang["DlgAsciiSvgTightDash"] = 'тирета, плътно' ;
+FCKLang["DlgAsciiSvgDashDot"] = 'точки-тирета' ;
+FCKLang["DlgAsciiSvgStartWith"] = 'Започва с:' ;
+FCKLang["DlgAsciiSvgStartWith"] = 'Завършва с:' ;
+FCKLang["DlgAsciiSvgNothing"] = 'нищо' ;
+FCKLang["DlgAsciiSvgArrow"] = 'стрелка' ;
+FCKLang["DlgAsciiSvgOpenDot"] = 'кръгче' ;
+FCKLang["DlgAsciiSvgDot"] = 'точка' ;
+FCKLang["DlgAsciiSvgShowAxisLabels"] = 'Надписи по осите:' ;
+FCKLang["DlgAsciiSvgShowXYGrid"] = 'XY мрежа:' ;
+FCKLang["DlgAsciiSvgResizeTo"] = 'Размер:' ;
+FCKLang["DlgAsciiSvgBy"] = 'X' ;
+FCKLang["DlgAsciiSvgUpdate"] = 'Обновяване' ;
+FCKLang["DlgAsciiSvgAddGraph"] = 'Добаване на графика' ;
+FCKLang["DlgAsciiSvgGraphs"] = 'Графики:' ;
+FCKLang["DlgAsciiSvgReplaceGraph"] = 'Замяна на избраната графика' ;
+FCKLang["DlgAsciiSvgRemoveGraph"] = 'Премахване на графиката' ;
+FCKLang["DlgAsciiSvgAlignment"] = 'Подравняване' ;
+FCKLang["DlgAsciiSvgTop"] = 'горе' ;
+FCKLang["DlgAsciiSvgMiddle"] = 'в средата' ;
+FCKLang["DlgAsciiSvgBottom"] = 'долу' ;
+FCKLang["DlgAsciiSvgFloatLeft"] = 'наляво' ;
+FCKLang["DlgAsciiSvgFloatRight"] = 'надясно' ;

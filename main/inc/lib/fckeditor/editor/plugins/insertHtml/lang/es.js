f82505f4c25fbc6548825326ee0a79ba74e0ce73
Ivan Tcholakov <ivantcholakov@gmail.com>
2011-02-08 18:26:36 +0200

    Task #2764 - Encoding change to UTF-8 of a Spanish language file.

--- main/inc/lib/fckeditor/editor/plugins/insertHtml/lang/es.js
+++ main/inc/lib/fckeditor/editor/plugins/insertHtml/lang/es.js
@@ -1,6 +1,6 @@
 
 
-	FCKLang.inserHTML_buttonTooltip					= 'Insertar Widget';
-	FCKLang.insertHtml_dialogTitle						= 'Insertar Widget';
-	FCKLang.inserHtml_help								= 'Introduzca cualquier código HTML debajo y haga clic \'ok\' para insertarlo en la posición en la que se encuentra el cursor en el editor.';
+    FCKLang.inserHTML_buttonTooltip					= 'Insertar Widget';
+    FCKLang.insertHtml_dialogTitle						= 'Insertar Widget';
+    FCKLang.inserHtml_help								= 'Introduzca cualquier cÃ³digo HTML debajo y haga clic \'ok\' para insertarlo en la posiciÃ³n en la que se encuentra el cursor en el editor.';
 

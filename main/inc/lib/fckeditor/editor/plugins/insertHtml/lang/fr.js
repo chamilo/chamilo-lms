6dc85116c4d9b744adfbb4347d12743bb056174b
ywarnier <ywarnier@beeznest.org>
2011-02-10 00:46:11 -0500

    Added French translation to insertHTML plugin

---
+++ main/inc/lib/fckeditor/editor/plugins/insertHtml/lang/fr.js
@@ -0,0 +1,6 @@
+
+
+    FCKLang.inserHTML_buttonTooltip					= 'Insérer Widget';
+    FCKLang.insertHtml_dialogTitle						= 'Insérer Widget';
+    FCKLang.inserHtml_help								= 'Introduidez n\'importe quel code HTML ci-dessous et cliquez \'ok\' pour l\'insérer à la position à laquelle le curseur se trouve dans l\'éditeur.';
+

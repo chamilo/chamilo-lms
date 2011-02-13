8ae140d450210acb083c4e7ac38a14dc74c2e096
Yannick Warnier <yannick.warnier@beeznest.com>
2011-02-13 15:24:08 -0500

    Fixed warning in nusoap WSDL generation

--- main/inc/lib/nusoap/nusoap.php
+++ main/inc/lib/nusoap/nusoap.php
@@ -6230,12 +6230,14 @@ class wsdl extends nusoap_base {
 	    	foreach($elements as $n => $e){
 	            // expand each element
 	            $ee = array();
-	            foreach ($e as $k => $v) {
+                    if (is_array($e) && count($e)>0) {
+	                foreach ($e as $k => $v) {
 		            $k = strpos($k,':') ? $this->expandQname($k) : $k;
 		            $v = strpos($v,':') ? $this->expandQname($v) : $v;
 		            $ee[$k] = $v;
 		    	}
 	    		$eElements[$n] = $ee;
+                    }
 	    	}
 	    	$elements = $eElements;
 		}

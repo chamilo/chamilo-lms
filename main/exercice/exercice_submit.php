16359e6544402a94255c2f0fbaac2b560d776610
Julio Montoya <gugli100@gmail.com>
2011-02-17 18:19:45 +0100

    Fixing icon label see #2773

--- main/exercice/exercice_submit.php
+++ main/exercice/exercice_submit.php
@@ -469,7 +469,7 @@ if (isset($exerciseId) && !empty($exerciseId)) {
 // I'm in a preview mode
 if (api_is_course_admin() && $origin != 'learnpath') {
     echo '<div class="actions">';
-    echo '<a href="exercice.php?show=test">' . Display :: return_icon('back.png', get_lang('GoBackToQuestionList'),'','32').'</a>';
+    echo '<a href="exercice.php?show=test">' . Display :: return_icon('back.png', get_lang('BackToExercisesList'),'','32').'</a>';
     if ($show_quiz_edition) {
     	echo '<a href="exercise_admin.php?' . api_get_cidreq() . '&modifyExercise=yes&exerciseId=' . $objExercise->id . '">'.Display :: return_icon('settings.png', get_lang('ModifyExercise'),'','32').'</a>';
         //echo Display :: return_icon('wizard.gif', get_lang('QuestionList')) . '<a href="exercice/admin.php?' . api_get_cidreq() . '&exerciseId=' . $objExercise->id . '">' . get_lang('QuestionList') . '</a>';

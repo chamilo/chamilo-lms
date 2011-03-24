b15cd0a5312ba6abc319425c917142ce21143fda
Noël Dieschburg <noel@cblue.be>
2011-03-25 00:48:01 +0100

    Send event notification : traduction correction.

--- main/lang/french/events.inc.php
+++ main/lang/french/events.inc.php
@@ -5,15 +5,15 @@ $events_userListTile = 'Utilisateurs de la plate-forme';
 $events_userSubListTile = 'Liste des utilisateurs à prévenir';
 
 $events_btnMod = 'Modifier l\'évenement';
-$course_deleted = 'Supprimer une formation';
-$course_created = 'Créer une formation';
-$user_deleted = 'Supprimer un utilisateur';
-$user_created = 'Créer un utilisateur';
-$session_created = 'Créer une session';
-$session_deleted ='Supprimer une session';
-$session_category_created = 'Créer une catégorie de session';
-$session_category_deleted = 'Supprimer une catégorie de session';
-$settings_changed = 'Changer un paramètre de configuration';
-$user_subscribed = 'S\'inscrire';
-$user_unsubscribed = 'Se désinscrire';
+$course_deleted = 'Suppression d\'une formation';
+$course_created = 'Création d\'une formation';
+$user_deleted = 'Suppression d\'un utilisateur';
+$user_created = 'Création d\'un utilisateur';
+$session_created = 'Création d\'une session';
+$session_deleted ='Suppression d\'une session';
+$session_category_created = 'Création d\'une catégorie de session';
+$session_category_deleted = 'Suppression d\'une catégorie de session';
+$settings_changed = 'Changement d\'un paramètre de configuration';
+$user_subscribed = 'Inscription d\'un utilisateur';
+$user_unsubscribed = 'Désinscription d\'un utilisateur';
 ?>

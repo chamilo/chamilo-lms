0bb4adb83b163b33c1aa9419f28da0a346c07605
Julio Montoya <gugli100@gmail.com>
2011-08-15 14:36:56 +0200

    Setting the cache and log path of the PHP code QR lib #3631

--- main/inc/lib/phpqrcode/qrconfig.php
+++ main/inc/lib/phpqrcode/qrconfig.php
@@ -4,10 +4,12 @@
  *
  * Config file, feel free to modify
  */
-     
+
     define('QR_CACHEABLE', true);                                                               // use cache - more disk reads but less CPU power, masks and format templates are stored there
-    define('QR_CACHE_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR);  // used when QR_CACHEABLE === true
-    define('QR_LOG_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);                                // default error logs dir   
+//    define('QR_CACHE_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR);  // used when QR_CACHEABLE === true
+    define('QR_CACHE_DIR', api_get_path(SYS_ARCHIVE_PATH));
+//    define('QR_LOG_DIR', dirname(__FILE__).DIRECTORY_SEPARATOR);                                // default error logs dir   
+    define('QR_LOG_DIR', api_get_path(SYS_ARCHIVE_PATH));
     
     define('QR_FIND_BEST_MASK', true);                                                          // if true, estimates best mask (spec. default, but extremally slow; set to false to significant performance boost but (propably) worst quality code
     define('QR_FIND_FROM_RANDOM', false);                                                       // if false, checks all masks available, otherwise value tells count of masks need to be checked, mask id are got randomly

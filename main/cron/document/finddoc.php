8f56b84358f5de34a3643cd238abec84c063468c
ywarnier <ywarnier@beeznest.org>
2011-09-06 19:05:31 -0500

    Minor - Updated script inclusion

--- main/cron/document/finddoc.php
+++ main/cron/document/finddoc.php
@@ -6,7 +6,7 @@
  * Code init - comment die() call to enable
  */
 die();
-require '../inc/global.inc.php';
+require '../../inc/global.inc.php';
 require_once '../inc/lib/course.lib.php';
 if (empty($_GET['doc'])) {
   echo "To add a document name to search, add ?doc=abc to the URL";

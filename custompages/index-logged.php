2c1608d12d8e6fb78ad3b35efad40b5cbc07bce9
Noel Dieschburg <noel@cblue.be>
2011-09-20 14:32:38 +0200

    Custom pages : removes debug in index-logged

--- custompages/index-logged.php
+++ custompages/index-logged.php
@@ -1,8 +1,6 @@
 <?php 
-echo "toto";
 require_once('main/inc/global.inc.php');
 //require_once api_get_path(LIBRARY_PATH).'usermanager.lib.php';
-echo "toto";
 
 header("Location: /user_portal.php");
 ?>

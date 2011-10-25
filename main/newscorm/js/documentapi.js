205946b2ab7324703afb2491cdec6b009de41556
Arnaud Ligot <arnaud@cblue.be>
2011-10-25 16:44:14 +0200

    documentAPI + gestion leaderboard in storage API

---
+++ main/newscorm/js/documentapi.js
@@ -0,0 +1,28 @@
+// JS interface enabling scrom content to use main/document/remote.php easily
+// CBlue SPRL, Arnaud Ligot <arnaud@cblue.be>
+
+
+lms_documents_list = function(path) {
+	var result;
+	$.ajax({
+		async: false,
+		type: "POST",
+		datatype: "json",
+		url: "../document/remote.php",
+		data: {
+			action: "list",
+			cwd: path,
+			cidReq: chamilo_courseCode,
+		},
+		success: function(data) {
+			result = eval("("+data+")");
+		}
+	});
+	return result;
+}
+
+// Accessor object
+function DOCUMENTAPIobject() {
+	this.list = lms_documents_list;
+}
+var DOCUMENTAPI = new DOCUMENTAPIobject();

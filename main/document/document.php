1df33f3fd0704b76cd621eca36155e62c39f64c1
Hubert Borderiou <baelmyhu@gmail.com>
2011-12-12 14:14:45 +0100

    Correction a mistake I did in Revision: ab3aca9ed88d . File document shouldn't have been commited.

--- main/document/document.php
+++ main/document/document.php
@@ -1175,9 +1175,7 @@ $default_column = $is_allowed_to_edit ? 2 : 1;
 $tablename = $is_allowed_to_edit ? 'teacher_table' : 'student_table';
 
 //var_dump($column_show,$column_order);
-echo "HUBC<pre>"; print_r($column_order); echo "</pre>";
-//$table = new SortableTableFromArrayConfig($sortable_data, $default_column, 20, $tablename, $column_show, $column_order, 'ASC', true);
-$table = new SortableTableFromArrayConfig($sortable_data, $default_column, 20, $tablename, '', $column_order, 'ASC', true);
+$table = new SortableTableFromArrayConfig($sortable_data, $default_column, 20, $tablename, $column_show, $column_order, 'ASC', true);
 
 if(isset($_GET['keyword'])) {
     $query_vars['keyword'] = Security::remove_XSS($_GET['keyword']);

002bad5846af532889affc1cc9c197b70b5dc574
Yannick Warnier <yannick.warnier@beeznest.com>
2012-02-18 23:52:29 -0500

    Cleanup

--- main/admin/cli.php
+++ main/admin/cli.php
@@ -1,65 +1,46 @@
 <?php
-/* For licensing terms, see /dokeos_license.txt */
+/* For licensing terms, see /license.txt */
 /**
-==============================================================================
 * Used for external support of chamilo's users
 *
 * @author Arnaud Ligot, CBlue SPRL
-* @package dokeos.admin
-==============================================================================
+* @package chamilo.admin.cli
 */
-/*
-==============================================================================
-	   INIT SECTION
-==============================================================================
-*/
+/**
+ * Init section 
+ */
 // name of the language file that needs to be included
 $language_file = 'admin';
-
 // we are in the admin area so we do not need a course id
 $cidReset = true;
-
 // include global script
 require_once '../inc/global.inc.php';
 $this_section = SECTION_PLATFORM_ADMIN;
-
+// make sure only logged-in admins can execute this
 api_protect_admin_script();
-
-
-/*
-==============================================================================
-		MAIN CODE
-==============================================================================
-*/
+/**
+ * Main code 
+ */
 // setting the name of the tool
 $tool_name = get_lang('CLI');
-
 // setting breadcrumbs
 $interbreadcrumb[] = array ('url' => 'index.php', 'name' => get_lang('PlatformAdmin'));
-
 // including the header file (which includes the banner itself)
 Display :: display_header($tool_name);
-
-
-
 switch ($_GET["cmd"]) {
-	case "clear_stapi":
-		echo "Are you sure you are willing to erease all storage api data (no backup)? <a href='cli.php?cmd=clear_stapi_confirm' >Yes</a>";
-		break;
-	case "clear_stapi_confirm":
-		Database::query("delete from ".Database::get_main_table(TABLE_MAIN_STORED_VALUES));
-		Database::query("delete from ".Database::get_main_table(TABLE_MAIN_STORED_STACK));
-		echo "Done";
-		break;
-	default:
-		echo "UNKNOWN COMMAND";
-		break;
+    case "clear_stapi":
+      echo "Are you sure you are willing to erease all storage api data (no backup)? <a href='cli.php?cmd=clear_stapi_confirm' >Yes</a>";
+      break;
+    case "clear_stapi_confirm":
+      Database::query("delete from ".Database::get_main_table(TABLE_MAIN_STORED_VALUES));
+      Database::query("delete from ".Database::get_main_table(TABLE_MAIN_STORED_STACK));
+      echo "Done";
+      break;
+    default:
+      echo "UNKNOWN COMMAND";
+      break;
 }
-
-/*
-==============================================================================
-		FOOTER
-==============================================================================
-*/
+/**
+ * Footer 
+ */
 Display :: display_footer();
-?>

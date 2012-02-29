969710366fa8f7c2b7e14c90594023c1a907df02
Julio Montoya <gugli100@gmail.com>
2012-02-29 17:46:28 +0100

    Deleting unused header/footers scripts

--- main/inc/footer.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* For licensing terms, see /license.txt */
-
-echo $global_tpl->show_footer();
--- main/inc/header.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* For licensing terms, see /license.txt */
-
-/**
- *  This script displays the Chamilo header.
- *  @package chamilo.include
- */
-

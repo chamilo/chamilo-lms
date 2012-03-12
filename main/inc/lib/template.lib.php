7f26778d8d7c8adc6116143b909bc5540e070b3f
Julio Montoya <gugli100@gmail.com>
2012-03-13 00:53:34 +0100

    Updating bootstrap to version 2.0.2

--- main/inc/lib/template.lib.php
+++ main/inc/lib/template.lib.php
@@ -269,7 +269,7 @@ class Template extends Smarty {
 			$style_html .= 'img, div { behavior: url('.api_get_path(WEB_LIBRARY_PATH).'javascript/iepngfix/iepngfix.htc) } '."\n";
 		}
         
-        $style_html .= '@import "'.api_get_path(WEB_CSS_PATH).'bootstrap-responsive.css";'."\n";        
+        $style_html .= '@import "'.api_get_path(WEB_CSS_PATH).'bootstrap-responsive.min.css";'."\n";        
         $style_html .= '@import "'.api_get_path(WEB_CSS_PATH).'responsive.css";'."\n";   
 		
 		$this->assign('css_style', $style_html);

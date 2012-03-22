7f70b8e1b4d8c344565b6d43dc4b3d6b6d4379bf
Julio Montoya <gugli100@gmail.com>
2012-03-22 09:24:53 +0100

    Minor - adding comments

--- plugin/follow_buttons/index.php
+++ plugin/follow_buttons/index.php
@@ -4,16 +4,16 @@
  * @package chamilo.plugin.sharethis
  */
 
-
 //Plugin settings
 $_template = null;
-//$_template['message']       = 'Follow us';
-$_template['facebook']      = 'chamilolms';
-$_template['twitter']       = 'chamilo_news';
-$_template['linkedin']      = '';
-$_template['googleplus']    = '';
-$_template['youtube']       = '';
-$_template['flickr']        = '';
-$_template['vimeo']         = '';
-$_template['rss']           = 'http://www.chamilo.org/';
 
+//$_template['message']       = 'Follow us';
+
+$_template['facebook']      = 'chamilolms'; //http://www.facebook.com/your-profile
+$_template['twitter']       = 'chamilo_news'; //http://twitter.com/your-profile 
+$_template['linkedin']      = ''; //http://www.linkedin.com/in/your-profile
+$_template['googleplus']    = ''; //https://plus.google.com/your-profile
+$_template['youtube']       = ''; //http://www.youtube.com/user/your-profile 
+$_template['flickr']        = ''; //http://www.flickr.com/photos/your-profile 
+$_template['vimeo']         = ''; //http://www.vimeo.com/your-profile 
+$_template['rss']           = 'http://www.chamilo.org/';

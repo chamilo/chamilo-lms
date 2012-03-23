a5cf64e7ab115e585d2b1cc7adcccd2694029672
Julio Montoya <gugli100@gmail.com>
2012-03-23 14:58:22 +0100

    Should fix bug when resizing text see #4524

--- main/inc/lib/javascript/fontresize.js
+++ main/inc/lib/javascript/fontresize.js
@@ -2,8 +2,22 @@ $(document).ready(function(){
     // Reset Font Size
     var originalFontSize = $('body').css('font-size');
     
-    $(".reset_font").click(function(){
+    var original_head1 = $('h1').css('font-size');
+    var original_head2 = $('h2').css('font-size');
+    var original_head3 = $('h3').css('font-size');
+    var original_head4 = $('h4').css('font-size');
+    var original_head5 = $('h5').css('font-size');
+    var original_head6 = $('h9').css('font-size');
+    
+    
+    $(".reset_font").click(function() {
         $('body').css('font-size', originalFontSize);
+        $('h1').css('font-size', original_head1);
+        $('h2').css('font-size', original_head2);
+        $('h3').css('font-size', original_head3);
+        $('h4').css('font-size', original_head4);
+        $('h5').css('font-size', original_head5);
+        $('h6').css('font-size', original_head6);        
     });
     
     // Increase Font Size
@@ -12,6 +26,7 @@ $(document).ready(function(){
         var currentFontSizeNum = parseFloat(currentFontSize, 10);
         var newFontSize = currentFontSizeNum*1.2;
         $('body').css('font-size', newFontSize);
+        $('h1, h2, h3, h4, h5, h6').css('font-size', newFontSize);
         return false;
     });
     
@@ -21,6 +36,7 @@ $(document).ready(function(){
         var currentFontSizeNum = parseFloat(currentFontSize, 10);
         var newFontSize = currentFontSizeNum*0.8;
         $('body').css('font-size', newFontSize);
-    return false;
+        $('h1, h2, h3, h4, h5, h6').css('font-size', newFontSize);
+        return false;
     });
 });

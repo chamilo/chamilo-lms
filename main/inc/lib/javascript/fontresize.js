8fb360a07f98766831cc27eb041cf5c296859220
Julio Montoya <gugli100@gmail.com>
2012-03-23 14:09:20 +0100

    Minor - cosmetic changes

--- main/inc/lib/javascript/fontresize.js
+++ main/inc/lib/javascript/fontresize.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){
- // Reset Font Size
- var originalFontSize = $('body').css('font-size');
- $(".reset_font").click(function(){
-  $('body').css('font-size', originalFontSize);
- });
- // Increase Font Size
- $(".increase_font").click(function(){
-  var currentFontSize = $('body').css('font-size');
-  var currentFontSizeNum = parseFloat(currentFontSize, 10);
-  var newFontSize = currentFontSizeNum*1.2;
-  $('body').css('font-size', newFontSize);
-  return false;
- });
- // Decrease Font Size
- $(".decrease_font").click(function(){
-  var currentFontSize = $('body').css('font-size');
-  var currentFontSizeNum = parseFloat(currentFontSize, 10);
-  var newFontSize = currentFontSizeNum*0.8;
-  $('body').css('font-size', newFontSize);
-  return false;
- });
+    // Reset Font Size
+    var originalFontSize = $('body').css('font-size');
+    
+    $(".reset_font").click(function(){
+        $('body').css('font-size', originalFontSize);
+    });
+    
+    // Increase Font Size
+    $(".increase_font").click(function(){
+        var currentFontSize = $('body').css('font-size');
+        var currentFontSizeNum = parseFloat(currentFontSize, 10);
+        var newFontSize = currentFontSizeNum*1.2;
+        $('body').css('font-size', newFontSize);
+        return false;
+    });
+    
+    // Decrease Font Size
+    $(".decrease_font").click(function(){
+        var currentFontSize = $('body').css('font-size');
+        var currentFontSizeNum = parseFloat(currentFontSize, 10);
+        var newFontSize = currentFontSizeNum*0.8;
+        $('body').css('font-size', newFontSize);
+    return false;
+    });
 });

ec0f90125da03d850f9f07c4ff7540029241a1e4
Juan Carlos Raña <juancarlos@innovasoc.com>
2012-04-04 13:09:13 +0200

    implement wami record in documents tool

--- main/inc/lib/wami-recorder/example/server/php/record.php
+++
@@ -1,9 +0,0 @@
-<?php
-# Save the audio to a URL-accessible directory for playback.
-parse_str($_SERVER['QUERY_STRING'], $params);
-$name = isset($params['name']) ? $params['name'] : 'output.wav';
-$content = file_get_contents('php://input');
-$fh = fopen($name, 'w') or die("can't open file");
-fwrite($fh, $content);
-fclose($fh);
-?>

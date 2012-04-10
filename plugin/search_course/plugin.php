061d27b8c463e95ea7bb3d7a88f68c0383877c6c
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-04-10 08:49:53 +0200

    plugin search course: #4597

---
+++ plugin/search_course/plugin.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @copyright (c) 2011 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht
+ */
+
+require_once api_get_path(LIBRARY_PATH) . '/plugin.class.php';
+require_once dirname(__FILE__) . '/lib/search_course_plugin.class.php';
+
+$plugin_info = SearchCoursePlugin::create()->get_info();

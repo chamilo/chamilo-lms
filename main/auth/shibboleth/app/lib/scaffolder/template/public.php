7440840f53402e36535a43f6a8c0902a0008286e
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-04-10 08:55:30 +0200

    shibboleth authentication module #4554, add new templates for code generation

---
+++ main/auth/shibboleth/app/lib/scaffolder/template/public.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+
+echo '<?php';
+?>
+
+/**
+ *
+ * Model for table <?php echo $table_name ?> 
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+class <?php echo $class_name ?>
+
+{
+
+    
+}
+
+/**
+ * Store for <?php echo $class_name ?> objects. Interact with the database.
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+class <?php echo $class_name ?>Store extends Store
+{
+
+}

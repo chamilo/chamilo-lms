eada0adfd94566b4b81d0ca3a7815866f1e6329e
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-04-23 11:06:01 +0200

    api key authentication

---
+++ main/auth/key/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Display nothing. This ensure Apache doesn't display the list of files and folders
+ * when it is not propertly configured.
+ *
+ * @license see /license.txt
+ * @author Laurent Opprecht <laurent@opprecht.info>, Nicolas Rod for the University of Geneva
+ */

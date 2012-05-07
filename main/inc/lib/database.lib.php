4137c340b4ac6df77fafc9d790962ce62e7dd71f
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-05-07 12:49:31 +0200

    #4694 add feedback during installation, remove warnings

--- main/inc/lib/database.lib.php
+++ main/inc/lib/database.lib.php
@@ -418,9 +418,16 @@ class Database {
         if (!isset($parameters['client_flags'])) {
             $parameters['client_flags'] = 0;
         }
-        return $parameters['persistent']
-            ? mysql_pconnect($parameters['server'], $parameters['username'], $parameters['password'], $parameters['client_flags'])
-            : mysql_connect($parameters['server'], $parameters['username'], $parameters['password'], $parameters['new_link'], $parameters['client_flags']);
+        
+        $persistent = isset($parameters['persistent']) ? $parameters['persistent'] : null;
+        $server = isset($parameters['server']) ? $parameters['server'] : null;
+        $username = isset($parameters['username']) ? $parameters['username'] : null;
+        $password = isset($parameters['password']) ? $parameters['password'] : null;
+        $client_flag = isset($parameters['client_flags']) ? $parameters['client_flags'] : null;
+        $new_link = isset($parameters['new_link']) ? $parameters['new_link'] : null;
+        return $persistent
+            ? mysql_pconnect($server, $username, $password, $client_flags)
+            : mysql_connect($server, $username, $password, $new_link, $client_flags);
     }
 
     /**

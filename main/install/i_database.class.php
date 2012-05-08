8dbc17a6765cc1caac67e65948f211d27f0a64a9
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-05-08 17:11:45 +0200

    #4694 add feedback during installation

---
+++ main/install/i_database.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Install database. With logging.
+ *
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ */
+class iDatabase extends Database
+{
+
+    static function select_db($database_name, $connection = null)
+    {
+        Log::notice(__FUNCTION__ . ' ' . $database_name, Log::get_frame(1));
+        parent::select_db($database_name, $connection);
+    }
+
+    static function query($query, $connection = null, $file = null, $line = null)
+    {
+        Log::notice(__FUNCTION__ . ' ' . $database_name, Log::get_frame(1));
+
+        parent::query($query, $connection, $file, $line);
+    }
+
+}

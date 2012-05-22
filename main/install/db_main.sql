92d6440ae71b64dbb69507eaae522ef85e1fdec1
Julio Montoya <gugli100@gmail.com>
2012-05-22 15:52:18 +0200

    Adding gradebook_enable_grade_model setting see BT#4080

--- main/install/db_main.sql
+++ main/install/db_main.sql
@@ -859,8 +859,9 @@ VALUES
 ('shibboleth_description', NULL, 'radio', 'Shibboleth', 'false', 'ShibbolethMainActivateTitle', 'ShibbolethMainActivateComment', NULL, NULL, 0),
 ('facebook_description', NULL, 'radio', 'Facebook', 'false', 'FacebookMainActivateTitle', 'FacebookMainActivateComment', NULL, NULL, 0),
 ('gradebook_locking_enabled', NULL, 'radio', 'Gradebook', 'false', 'GradebookEnableLockingTitle', 'GradebookEnableLockingComment', NULL, NULL, 0),
-('gradebook_default_grade_model_id', NULL, 'select', 'Gradebook', '', 'GradebookDefaultGradeModelTitle', 'GradebookDefaultGradeModelComment', NULL, NULL, 1)
-('chamilo_database_version',NULL,'textfield',NULL, '1.9.0.17972','DatabaseVersion','', NULL, NULL, 0);
+('gradebook_default_grade_model_id', NULL, 'select', 'Gradebook', '', 'GradebookDefaultGradeModelTitle', 'GradebookDefaultGradeModelComment', NULL, NULL, 1),
+('gradebook_enable_grade_model', NULL, 'radio', 'Gradebook', 'false', 'GradebookEnableGradeModelTitle', 'GradebookEnableGradeModelComment', NULL, NULL, 1),
+('chamilo_database_version',NULL,'textfield',NULL, '1.9.0.17976','DatabaseVersion','', NULL, NULL, 0);
 
 /*
 ('show_tabs', 'custom_tab_1', 'checkbox', 'Platform', 'true', 'ShowTabsTitle', 'ShowTabsComment', NULL, 'TabsCustom1', 1),
@@ -1200,7 +1201,10 @@ VALUES
 ('teachers_can_change_grade_model_settings', 'true', 'Yes'),
 ('teachers_can_change_grade_model_settings', 'false', 'No'),
 ('gradebook_locking_enabled', 'true', 'Yes'),
-('gradebook_locking_enabled', 'false', 'No');
+('gradebook_locking_enabled', 'false', 'No'),
+('gradebook_enable_grade_model', 'true', 'Yes'),
+('gradebook_enable_grade_model', 'false', 'No');
+
 UNLOCK TABLES;
 /*
 ('activate_send_event_by_mail', 'true', 'Yes'),

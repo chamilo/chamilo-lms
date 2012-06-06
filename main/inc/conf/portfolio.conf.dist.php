b722580c2c48cebcec7d0f15e44dc118ce5462ea
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-06-06 17:03:36 +0200

    #2851 portfolio integration

---
+++ main/inc/conf/portfolio.conf.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file contains the portfolios configuration.
+ * 
+ * Portfolios are external applicatoins used to display and share files. The
+ * portfolio configuration set up where files can be sent.
+ * 
+ * @see \Portfolio
+ */
+
+$portfolios = array();
+
+//$_mahara_portfolio = new Portfolio\Mahara('http(s)://...');
+//$portfolios[] = $_mahara_portfolio;
+
+$download_portfolio = new Portfolio\Download();
+$download_portfolio->set_title(get_lang('download'));
+$portfolios[] = $_download_portfolio;

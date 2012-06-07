30adb21db7218512bede6ff7a068a3d23d0f90d5
Julio Montoya <gugli100@gmail.com>
2012-06-07 18:19:55 +0200

    Minor fixes see #4525

--- main/inc/ajax/events.ajax.php
+++ main/inc/ajax/events.ajax.php
@@ -1,35 +1,25 @@
 <?php
-	error_reporting(E_ERROR);
-	
-	ob_start();
-	
-	require_once '../global.inc.php';
-	require_once '../lib/usermanager.lib.php';
-	
-	$id = isset($_REQUEST['id'])?$_REQUEST['id']:null;
-	$action = isset($_REQUEST['action'])?$_REQUEST['action']:null;
-	$actionEventName = isset($_REQUEST['eventName'])?$_REQUEST['eventName']:null;
-	
-	api_protect_admin_script();
-	
-	ob_end_clean();
-	
-	header('content-type: text/json');
-	
-	if($action == 'getEventTypes') {
-		$events = get_all_event_types();
-		
-		print json_encode($events);
-	}
-	elseif($action == 'getUsers') {
-		$users = UserManager::get_user_list();
-		
-		print json_encode($users);
-	}
-	elseif($action == 'get_event_users') {
-		$users = get_event_users($actionEventName);
-		
-		print json_encode($users);
-	}
-	
-	
+
+require_once '../global.inc.php';
+
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : null;
+$actionEventName = isset($_REQUEST['eventName']) ? $_REQUEST['eventName'] : null;
+
+api_protect_admin_script();
+
+switch ($action) {
+	case 'getEventTypes':
+        $events = get_all_event_types();
+        print json_encode($events);
+        break;
+    case 'getUsers':
+        $users = UserManager::get_user_list();
+        print json_encode($users);
+        break;
+    case 'get_event_users' :
+        $users = get_event_users($actionEventName);
+        print json_encode($users);
+        break;
+}
+exit;

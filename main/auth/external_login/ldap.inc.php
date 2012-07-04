6fe6026d6e0a14d2ad604f1b9357e6ca33f19dad
Hubert Borderiou <baelmyhu@gmail.com>
2012-07-04 09:31:56 +0200

    Minor add a missing / in a path

--- main/auth/external_login/ldap.inc.php
+++ main/auth/external_login/ldap.inc.php
@@ -6,7 +6,7 @@
 
 //Includes the configuration file
 require_once dirname(__FILE__).'/../../inc/global.inc.php';
-require_once dirname(__FILE__).'../../inc/conf/auth.conf.php';
+require_once dirname(__FILE__).'/../../inc/conf/auth.conf.php';
 
 /**
  * Returns a transcoded and trimmed string

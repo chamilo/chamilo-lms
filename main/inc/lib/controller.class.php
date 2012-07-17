42da41f04360684d0405579716a675c23a9ad42f
Laurent Opprecht <laurent.opprecht@unige.ch>
2012-07-17 08:52:19 +0200

    minor: add base controller

---
+++ main/inc/lib/controller.class.php
@@ -0,0 +1,170 @@
+<?php
+
+/**
+ * Controller
+ * 
+ * 
+ * 
+ * @author Laurent Opprecht <laurent@opprecht.info> for the Univesity of Genevas
+ * @license see /license.txt
+ */
+class Controller // extends Controller {	
+{
+
+    const PARAM_ACTION = 'action';
+
+    /**
+     * List of actions accepted by the controller.
+     * 
+     * @return array
+     */
+    public function get_actions()
+    {
+        $reflector = new ReflectionClass($this);
+        $constants = $reflector->getConstants();
+        $result = array();
+        foreach ($constants as $key => $value) {
+            if (strpos($key, 'ACTION') !== false) {
+                $result[$key] = $value;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Action to perform. 
+     * Returns the request parameter.
+     * 
+     * @return string
+     */
+    public function get_action()
+    {
+        $result = Request::get(self::PARAM_ACTION);
+        $actions = $this->get_actions();
+        $result = in_array($result, $actions) ? $result : '';
+        return $result;
+    }
+
+    /**
+     * Set up the environment. Set up breadcrumps, raise tracking event, etc.
+     */
+    protected function prolog()
+    {
+        
+    }
+
+    /**
+     * Whether the call is authorized or not.
+     * 
+     * @return boolean 
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function javascript()
+    {
+        return '';
+    }
+
+    public function check_token()
+    {
+        return (bool) Security::check_token('get');
+    }
+
+    /**
+     * Run the controller. Dispatch action and execute requested tasks.
+     */
+    public function run()
+    {
+        if (!$this->authorize()) {
+            $this->forbidden();
+            return false;
+        }
+
+        $this->prolog();
+        $action = $this->get_action();
+        if (empty($action)) {
+            $this->unknown();
+            return;
+        }
+        $f = array($this, $action);
+        if (is_callable($f)) {
+            call_user_func($f);
+        } else {
+            $this->missing();
+        }
+    }
+
+    /**
+     * Unknown action. I.e. the action has not been registered. 
+     * Possibly missing action declaration:
+     * 
+     *      const ACTION_XXX = 'XXX';
+     * 
+     * @return boolean 
+     */
+    public function unknown()
+    {
+        return false;
+    }
+
+    /**
+     * 
+     * @return boolean 
+     */
+    public function forbidden()
+    {
+        api_not_allowed();
+        return false;
+    }
+
+    /**
+     * Action exists but implementation is missing. 
+     */
+    public function missing()
+    {
+        echo 'No implementation';
+        return false;
+    }
+
+    /**
+     * Render a template using data. Adds a few common parameters to the data array.
+     * 
+     * @see /main/template/default/course_description/
+     * @param string $template
+     * @param array $data 
+     */
+    protected function render($template_name, $data)
+    {
+        $data = (object)$data;
+        $data->www = \Chamilo::url();
+        $data->messages = isset($data->messages) ? $data->messages : array();
+        $javascript = $this->javascript();
+        if ($javascript) {
+            $data->javascript = $javascript;
+        }
+
+        $tpl = new Template();
+        foreach ($data as $key => $value) {
+            $tpl->assign($key, $value);
+        }
+        $template = $tpl->get_template($template_name);
+        $content = $tpl->fetch($template);
+        $tpl->assign('content', $content);
+        $tpl->display_one_col_template();
+    }
+
+    /**
+     * Render data as JSON
+     * 
+     * @param any $data 
+     */
+    protected function render_json($data)
+    {
+        Header::content_type_json();
+        echo json_encode($data);
+    }
+
+}

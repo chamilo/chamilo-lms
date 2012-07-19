0a255edf5804ded995519e8fe0a7adfe819bd432
Julio Montoya <gugli100@gmail.com>
2012-07-19 18:23:39 +0200

    Should fix bug in the condition class. Swapping the return values of the function, returns true means that the user can pass, false otherwise see #4619

--- main/inc/lib/conditional_login.class.php
+++ main/inc/lib/conditional_login.class.php
@@ -7,16 +7,21 @@
  */
 
 class ConditionalLogin {
-
+    
+    /**
+     * Check conditions based in the $login_conditions see conditional_login.php file
+     * @param type $user
+     */
     public static function check_conditions($user) {
         if (file_exists(api_get_path(SYS_PATH).'main/auth/conditional_login/conditional_login.php')) {
             include_once api_get_path(SYS_PATH).'main/auth/conditional_login/conditional_login.php';
-            if (isset($dc_conditions)) {
-                foreach ($dc_conditions as $dc_condition) { 
-                    if (isset($dc_condition['conditional_function']) && $dc_condition['conditional_function']($user)) {
-                        $_SESSION['conditional_login']['uid'] = $user['user_id'];
+            if (isset($login_conditions)) {
+                foreach ($login_conditions as $condition) {
+                    //If condition fails we redirect to the URL defined by the condition
+                    if (isset($condition['conditional_function']) && $condition['conditional_function']($user) == false) {
+                        $_SESSION['conditional_login']['uid']       = $user['user_id'];
                         $_SESSION['conditional_login']['can_login'] = false;
-                        header("Location:". $dc_condition['url']);
+                        header("Location:". $condition['url']);
                         exit();
                     }
                 }

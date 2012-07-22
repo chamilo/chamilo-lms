98e60471196d7a3d943b09dc033f5adb199167be
Yannick Warnier <yannick.warnier@beeznest.com>
2012-07-22 00:10:07 -0500

    Applied removal of gradebook options when gradebook is disabled, to assessment tool - refs #5034

--- main/attendance/attendance_edit.php
+++ main/attendance/attendance_edit.php
@@ -38,29 +38,30 @@ $form->add_html_editor('description', get_lang('Description'), false, false, arr
 
 // Adavanced Parameters
 
-if (!empty($attendance_qualify_title) || !empty($attendance_weight)) {
-    $advanced = '<a href="javascript://" class="advanced_parameters"><span id="img_plus_and_minus">&nbsp;'.Display::return_icon('div_hide.gif',get_lang('Show'),array('style'=>'vertical-align:middle')).' '.get_lang('AdvancedParameters').'</span></a>';
-    $form -> addElement('advanced_settings',$advanced);    
+if (Gradebook::is_active()) {
+    if (!empty($attendance_qualify_title) || !empty($attendance_weight)) {
+        $advanced = '<a href="javascript://" class="advanced_parameters"><span id="img_plus_and_minus">&nbsp;'.Display::return_icon('div_hide.gif',get_lang('Show'),array('style'=>'vertical-align:middle')).' '.get_lang('AdvancedParameters').'</span></a>';
+        $form->addElement('advanced_settings',$advanced);    
     
 	$form->addElement('html','<div id="id_qualify" style="display:block">');    
 	$form->addElement('checkbox', 'attendance_qualify_gradebook', '', get_lang('QualifyAttendanceGradebook'),array('checked'=>'true','onclick'=>'javascript: if(this.checked){document.getElementById(\'options_field\').style.display = \'block\';}else{document.getElementById(\'options_field\').style.display = \'none\';}'));
-	$form -> addElement('html','<div id="options_field" style="display:block">');
-} else {
+	$form->addElement('html','<div id="options_field" style="display:block">');
+    } else {
 	$advanced = '<a href="javascript://" class="advanced_parameters"><span id="img_plus_and_minus">&nbsp;'.Display::return_icon('div_show.gif',get_lang('Show'),array('style'=>'vertical-align:middle')).' '.get_lang('AdvancedParameters').'</span></a>';
-    $form -> addElement('advanced_settings',$advanced);
+        $form->addElement('advanced_settings',$advanced);
 	$form->addElement('html','<div id="id_qualify" style="display:none">');    
 	$form->addElement('checkbox', 'attendance_qualify_gradebook', '', get_lang('QualifyAttendanceGradebook'),'onclick="javascript: if(this.checked){document.getElementById(\'options_field\').style.display = \'block\';}else{document.getElementById(\'options_field\').style.display = \'none\';}"');
-	$form -> addElement('html','<div id="options_field" style="display:none">');
+	$form->addElement('html','<div id="options_field" style="display:none">');
+    }
+    load_gradebook_select_in_tool($form);
+    $form->addElement('text', 'attendance_qualify_title', get_lang('TitleColumnGradebook'));
+    $form->applyFilter('attendance_qualify_title', 'html_filter');
+    $form->addElement('text', 'attendance_weight', get_lang('QualifyWeight'),'value="0.00" Style="width:40px" onfocus="javascript: this.select();"');
+    $form->applyFilter('attendance_weight', 'html_filter');
+    $form->addElement('html','</div>');
+
+    $form->addElement('html','</div>');
 }
-load_gradebook_select_in_tool($form);
-$form->addElement('text', 'attendance_qualify_title', get_lang('TitleColumnGradebook'));
-$form->applyFilter('attendance_qualify_title', 'html_filter');
-$form->addElement('text', 'attendance_weight', get_lang('QualifyWeight'),'value="0.00" Style="width:40px" onfocus="javascript: this.select();"');
-$form->applyFilter('attendance_weight', 'html_filter');
-$form->addElement('html','</div>');
-
-$form->addElement('html','</div>');
-
 $form->addElement('style_submit_button', null, get_lang('Save'), 'class="save"');
 
 // set default values

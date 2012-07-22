c1bc14ec81c48c784b276e4fa47f3d741e9f338d
Yannick Warnier <yannick.warnier@beeznest.com>
2012-07-21 23:02:55 -0500

    More changes to build file

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 php5 test_suite.php > logs/results.xml
-xsltproc simpletest_to_junit.xsl logs/results.xml > logs/results-1.xml
+xsltproc simpletest_to_junit.xsl logs/results.xml > logs/junit.xml

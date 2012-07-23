3c5626689a71d5bb345e9d2c7ab91d0d9d49839a
Yannick Warnier <yannick.warnier@beeznest.com>
2012-07-23 12:10:09 -0500

    Minor - fix variable name - refs #4776

--- main/link/ajax_controller.class.php
+++ main/link/ajax_controller.class.php
@@ -249,7 +249,7 @@ class AjaxController extends \Controller
 
     function forbidden()
     {
-        $this->response(false, get_lang('YourAreNotAuthorized'));
+        $this->response(false, get_lang('YouAreNotAuthorized'));
     }
 
     public function unknown()

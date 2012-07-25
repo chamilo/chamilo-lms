6d3826957f85bb246de1cb3d6f69659002a41cca
Julio Montoya <gugli100@gmail.com>
2012-07-25 17:48:43 +0200

    Minor - Adding "static" to functions

--- main/inc/lib/pear/PEAR.php
+++ main/inc/lib/pear/PEAR.php
@@ -253,7 +253,7 @@ class PEAR
         $GLOBALS['_PEAR_shutdown_funcs'][] = array($func, $args);
     }
 
-    // }}}
+    // }}} 
     // {{{ isError()
 
     /**
@@ -267,7 +267,7 @@ class PEAR
      * @access  public
      * @return  bool    true if parameter is an error
      */
-    function isError($data, $code = null)
+    static function isError($data, $code = null)
     {
         if (!is_a($data, 'PEAR_Error')) {
             return false;

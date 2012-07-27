688a07e7688a654ef2d96819e8094d1dd2353c4e
Julio Montoya <gugli100@gmail.com>
2012-07-27 15:17:30 +0200

    Should fix bug when sending announcements see #4715

--- main/inc/lib/main_api.lib.php
+++ main/inc/lib/main_api.lib.php
@@ -5752,13 +5752,18 @@ function api_get_course_url($course_code = null, $session_id = null) {
         $course_info = api_get_course_info($course_code);
     }
     if (empty($session_id)) {
-        if (api_get_session_id() != 0)
         $session_url = '?id_session='.api_get_session_id();
     } else {
         $session_url = '?id_session='.intval($session_id);
     }
+    /*
+    if (empty($group_id)) {
+        $group_url = '&gidReq='.api_get_group_id();
+    } else {
+        $group_url = '&gidReq='.intval($group_id);
+    }*/
     if (!empty($course_info['path'])) {
-        return api_get_path(WEB_COURSE_PATH).$course_info['path'].'/'.$session_url;
+        return api_get_path(WEB_COURSE_PATH).$course_info['path'].'/index.php'.$session_url;
     }
     return null;
 }

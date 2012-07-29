ec7d6f3a529046505456a1a6767872a8f7ab6224
Yannick Warnier <yannick.warnier@beeznest.com>
2012-07-29 18:56:08 -0500

    Added Spanish version of media browsers' dialog texts

---
+++ main/inc/lib/fckeditor/editor/plugins/media/lang/es.js
@@ -0,0 +1,20 @@
+/*
+ * For FCKeditor 2.3
+ *
+ *
+ */
+
+FCKLang['MediaTip']			 = 'Insertar/Editar medios' ;
+FCKLang['DlgMediaTitle']		= 'Propiedades del medio' ;
+FCKLang['DlgMediaCode']		= '"Por favor insertar URL de vídeos."' ;
+FCKLang['DlgMediaSecurity']	= 'URL inválida.' ;
+FCKLang['DlgMediaURL']	    = 'URL' ;
+FCKLang['DlgMediaWidth']	    = 'Ancho' ;
+FCKLang['DlgMediaHeight']	    = 'Alto' ;
+FCKLang['DlgMediaQuality']    = 'Calidad' ;
+FCKLang['DlgMediaLow']	    = 'Baja' ;
+FCKLang['DlgMediaHigh']	    = 'Alta (si disponible)' ;
+FCKLang['DlgMediaURLTip']		= 'Truco:' ;
+FCKLang['DlgMediaURLTipContent1']  = 'Vaya a %s. Navega en el sitio para encontrar su vídeo.' ;
+FCKLang['DlgMediaURLTipContent2']  = 'Copie la URL del vídeo desde la barra de dirección de su navegador y copie esta en esta caja "URL".' ;
+FCKLang['DlgMediaURLTipContent3']  = 'La URL copiada debería verse como la siguiente: %s.' ;

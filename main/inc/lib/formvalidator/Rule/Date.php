a0b4371e615b5eee2e22acdfe2eb271ca44b94c6
Julio Montoya <gugli100@gmail.com>
2012-08-13 14:42:03 +0200

    Fixing PHP warning in surveys see #4923

--- main/inc/lib/formvalidator/Rule/Date.php
+++ main/inc/lib/formvalidator/Rule/Date.php
@@ -33,9 +33,8 @@ class HTML_QuickForm_Rule_Date extends HTML_QuickForm_Rule
 	 * @param array $date An array with keys F (month), d (day) and Y (year)
 	 * @return boolean True if date is valid
 	 */
-	function validate($date)
+	function validate($date, $options)
 	{
 		return checkdate($date['F'], $date['d'], $date['Y']);
 	}
 }
-?>

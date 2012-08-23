8bef42696ca7bb7f9d164627b31cb5f2e93e17e1
Yannick Warnier <yannick.warnier@beeznest.com>
2012-08-23 09:57:33 -0500

    Fixed wrong database creation test in install procedure

--- main/install/install.lib.php
+++ main/install/install.lib.php
@@ -1790,9 +1790,9 @@ function display_database_settings_form($installType, $dbHostForm, $dbUsernameFo
             } else {
                  //Try to create the database
                 $user_can_create_databases = false;            
-                $multipleDbCheck = @Database::query("CREATE DATABASE test_chamilo_connection");            
+                $multipleDbCheck = @Database::query("CREATE DATABASE ".mysql_real_escape_string($dbNameForm));
                 if ($multipleDbCheck !== false) {
-                    $multipleDbCheck = @Database::query("DROP DATABASE IF EXISTS test_chamilo_connection");                
+                    $multipleDbCheck = @Database::query("DROP DATABASE IF EXISTS ".mysql_real_escape_string($dbNameForm));                
                     $user_can_create_databases = true;
                 }             
 

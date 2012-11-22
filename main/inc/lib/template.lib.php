9c43b25890d90721ab035d831390ce99cf099da0
Julio Montoya <gugli100@gmail.com>
2012-11-22 10:41:01 +0100

    Fixing name username in tabs when login as user #5692

--- main/inc/lib/template.lib.php
+++ main/inc/lib/template.lib.php
@@ -314,7 +314,7 @@ class Template {
         $user_info['logged'] = 0;
         $this->user_is_logged_in = false;
         if (api_user_is_login()) {
-            $user_info = api_get_user_info();
+            $user_info = api_get_user_info(api_get_user_id());
             $user_info['logged'] = 1;
 
             $user_info['is_admin'] = 0;

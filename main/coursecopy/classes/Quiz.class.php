273ca23abb836a3f5f034a6ea1197f1816531261
Julio Montoya <gugli100@gmail.com>
2012-12-07 12:02:59 +0100

    Minor - cleaning code

--- main/coursecopy/classes/Quiz.class.php
+++ main/coursecopy/classes/Quiz.class.php
@@ -25,29 +25,11 @@ class Quiz extends Resource
      * @param int $type
      * @param int $active
      */
-//    function Quiz($id, $title, $description, $random, $type, $active, $media, $attempts = 0, $results_disabled = 0, $access_condition = null, $start_time = '0000-00-00 00:00:00', $end_time = '0000-00-00 00:00:00', $feedback_type = 0, $random_answers = 0, $expired_time = 0, $session_id = 0) {
+    public $obj; //question
+
     function Quiz($obj) {
         parent::Resource($obj->id, RESOURCE_QUIZ);
         $this->obj = $obj;
-
-        /*
-        $this->title = $title;
-        $this->description = $description;
-        $this->random = $random;
-        $this->quiz_type = $type;
-        $this->active = $active;
-        $this->media = $media;
-        $this->attempts = $attempts;
-        $this->question_ids = array();
-        $this->question_orders = array();
-        $this->results_disabled = $results_disabled;
-        $this->access_condition = $access_condition;
-        $this->start_time = $start_time;
-        $this->end_time = $end_time;
-        $this->feedback_type = $feedback_type;
-        $this->random_answers = $random_answers;
-        $this->expired_time = $expired_time;
-        $this->session_id = $session_id;*/
     }
 
     /**

ca9b8f1d82fb0c5cb3a19d235df8b3b7eba0db6d
Hubert Borderiou <baelmyhu@gmail.com>
2012-12-12 10:51:58 +0100

    Add css class label for span6 when is a user_course_category

--- main/inc/lib/course.lib.php
+++ main/inc/lib/course.lib.php
@@ -2855,7 +2855,7 @@ class CourseManager {
                     $html .= '</div>';
                     $notifications = isset($params['notifications']) ? $params['notifications'] : null;
 
-                    $html .= '<div class="span6">';
+                    $html .= '<div class="span6 '.$params['class'].'">';
                         $html .='<h3>'.$params['title'].$notifications.'</h3> ';
 
                         if (!empty($params['subtitle'])) {
@@ -3010,6 +3010,7 @@ class CourseManager {
             // We simply display the title of the category.
             $params['icon'] = Display::return_icon('folder_yellow.png', $row['title'], array(), ICON_SIZE_LARGE);
             $params['title'] = $row['title'];
+            $params['class'] = 'table_user_course_category';
             $html .= self::course_item_parent(self::course_item_html($params, true), self :: display_courses_in_category($row['id'], $load_dirs));
         }
 

802e111f1877236fe0c027140b2e831b2e56b4fe
Julio Montoya <gugli100@gmail.com>
2013-02-25 17:28:53 +0100

    Minor removing unused code

--- main/newscorm/lp_edit_item_prereq.php
+++ main/newscorm/lp_edit_item_prereq.php
@@ -15,10 +15,9 @@ api_protect_course_script();
 
 /* Libraries */
 
-include 'learnpath_functions.inc.php';
-//include '../resourcelinker/resourcelinker.inc.php';
-include 'resourcelinker.inc.php';
-// Rewrite the language file, sadly overwritten by resourcelinker.inc.php.
+require_once 'learnpath_functions.inc.php';
+require_once 'resourcelinker.inc.php';
+
 // Name of the language file that needs to be included.
 $language_file = 'learnpath';
 
@@ -30,19 +29,18 @@ $tbl_lp = Database::get_course_table(TABLE_LP_MAIN);
 $tbl_lp_item = Database::get_course_table(TABLE_LP_ITEM);
 $tbl_lp_view = Database::get_course_table(TABLE_LP_VIEW);
 
-$isStudentView = (int) $_REQUEST['isStudentView'];
-$learnpath_id = (int) $_REQUEST['lp_id'];
-$submit = $_POST['submit_button'];
+$isStudentView = (int)$_REQUEST['isStudentView'];
+$learnpath_id = (int)$_REQUEST['lp_id'];
 
 /* MAIN CODE */
 
 // Using the resource linker as a tool for adding resources to the learning path.
 if ($action == 'add' and $type == 'learnpathitem') {
-    $htmlHeadXtra[] = "<script language='JavaScript' type='text/javascript'> window.location=\"../resourcelinker/resourcelinker.php?source_id=5&action=$action&learnpath_id=$learnpath_id&chapter_id=$chapter_id&originalresource=no\"; </script>";
+    $htmlHeadXtra[] = "<script type='text/javascript'> window.location=\"../resourcelinker/resourcelinker.php?source_id=5&action=$action&learnpath_id=$learnpath_id&chapter_id=$chapter_id&originalresource=no\"; </script>";
 }
 if ((!$is_allowed_to_edit) || ($isStudentView)) {
     error_log('New LP - User not authorized in lp_edit_item_prereq.php');
-    header('location:lp_controller.php?action=view&lp_id=' . $learnpath_id);
+    header('location:lp_controller.php?action=view&lp_id='.$learnpath_id);
 }
 $course_id = api_get_course_int_id();
 
@@ -59,14 +57,20 @@ if (isset($_SESSION['gradebook'])) {
 
 if (!empty($gradebook) && $gradebook == 'view') {
     $interbreadcrumb[] = array(
-        'url' => '../gradebook/' . $_SESSION['gradebook_dest'],
+        'url' => '../gradebook/'.$_SESSION['gradebook_dest'],
         'name' => get_lang('ToolGradebook')
     );
 }
 
 $interbreadcrumb[] = array('url' => 'lp_controller.php?action=list', 'name' => get_lang('LearningPaths'));
-$interbreadcrumb[] = array('url' => api_get_self() . "?action=build&lp_id=$learnpath_id", 'name' => stripslashes("{$therow['name']}"));
-$interbreadcrumb[] = array('url' => api_get_self() . "?action=add_item&type=step&lp_id=$learnpath_id", 'name' => get_lang('NewStep'));
+$interbreadcrumb[] = array(
+    'url' => api_get_self()."?action=build&lp_id=$learnpath_id",
+    'name' => stripslashes("{$therow['name']}")
+);
+$interbreadcrumb[] = array(
+    'url' => api_get_self()."?action=add_item&type=step&lp_id=$learnpath_id",
+    'name' => get_lang('NewStep')
+);
 
 // Theme calls.
 $show_learn_path = true;
@@ -79,14 +83,14 @@ $suredel = trim(get_lang('AreYouSureToDelete'));
 <script>
     /* <![CDATA[ */
     function stripslashes(str) {
-        str=str.replace(/\\'/g,'\'');
-        str=str.replace(/\\"/g,'"');
-        str=str.replace(/\\\\/g,'\\');
-        str=str.replace(/\\0/g,'\0');
+        str = str.replace(/\\'/g, '\'');
+        str = str.replace(/\\"/g, '"');
+        str = str.replace(/\\\\/g, '\\');
+        str = str.replace(/\\0/g, '\0');
         return str;
     }
     function confirmation(name) {
-        name=stripslashes(name);
+        name = stripslashes(name);
         if (confirm("<?php echo $suredel; ?> " + name + " ?")) {
             return true;
         } else {
@@ -105,13 +109,8 @@ echo '<div class="span3">';
 echo $_SESSION['oLP']->return_new_tree();
 echo '</div>';
 echo '<div class="span9">';
-if (isset($is_success) && $is_success == true) {
-    echo $_SESSION['oLP']->display_manipulate($_GET['id'], null);
-    echo Display::return_message(get_lang("PrerequisitesAdded"));
-} else {
-    echo $_SESSION['oLP']->display_manipulate($_GET['id'], null);
-    echo $_SESSION['oLP']->display_item_prerequisites_form($_GET['id']);
-}
+//echo Display::return_message(get_lang("PrerequisitesAdded"));
+echo $_SESSION['oLP']->display_manipulate($_GET['id'], null);
+echo $_SESSION['oLP']->display_item_prerequisites_form($_GET['id']);
 echo '</div>';
-
 Display::display_footer();

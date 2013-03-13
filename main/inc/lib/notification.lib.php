2f7a3432174318869e5568f9c32671fa8a20eea5
Hubert Borderiou <baelmyhu@gmail.com>
2013-03-13 15:11:58 +0100

    Sender of annoucement is teacher instead of admin - ref #5738

--- main/inc/lib/notification.lib.php
+++ main/inc/lib/notification.lib.php
@@ -154,9 +154,9 @@ class Notification extends Model
                                 $extra_headers = array();
                                 $extra_headers['reply_to']['mail'] = $sender_info['email'];
                                 $extra_headers['reply_to']['name'] = $sender_info['complete_name'];
-                                api_mail_html($name, $user_info['mail'], Security::filter_terms($title), Security::filter_terms($content), $this->admin_name, $this->admin_email, $extra_headers);
+                                api_mail_html($name, $user_info['mail'], Security::filter_terms($title), Security::filter_terms($content), $sender_info['complete_name'], $sender_info['email'], $extra_headers);
                             } else {
-                                api_mail_html($name, $user_info['mail'], Security::filter_terms($title), Security::filter_terms($content), $this->admin_name, $this->admin_email);
+                                api_mail_html($name, $user_info['mail'], Security::filter_terms($title), Security::filter_terms($content), $sender_info['complete_name'], $sender_info['email']);
                             }
                         }
                         $params['sent_at'] = api_get_utc_datetime();

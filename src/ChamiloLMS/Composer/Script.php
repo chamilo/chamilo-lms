f1f7499a25a9e7fa3556b6f8c721f0caa10b2949
Julio Montoya <gugli100@gmail.com>
2013-04-02 10:49:26 +0200

    Adding composer-chamilo script

---
+++ src/ChamiloLMS/Composer/Script.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ChamiloLMS\Composer;
+
+class Script
+{
+    public static function install()
+    {
+        /*chmod('resources/cache', 0777);
+        chmod('resources/log', 0777);
+        chmod('web/assets', 0777);
+        chmod('console', 0500);
+        exec('php console assetic:dump');*/
+        //echo 'Execting ChamiloLMS\Composer\Script::install';
+    }
+}

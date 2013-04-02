49e6ca456d3911ac421db0cb96de82950d7bcce0
Julio Montoya <gugli100@gmail.com>
2013-04-02 12:40:47 +0200

    Fixing fatal error during merge

--- main/template/default/auth/courses_categories.php
+++ main/template/default/auth/courses_categories.php
@@ -33,22 +33,22 @@ $stok = Security::get_token();
     <div class="span3">
         <div id="course_category_well" class="well">
             <ul class="nav nav-list">
-<?php if (intval($_GET['hidden_links']) != 1) { ?>
+<?php if (isset($_GET['hidden_links']) && intval($_GET['hidden_links']) != 1) { ?>
 
 <div class="actions">
     <form class="form-search" method="post" action="<?php echo api_get_self(); ?>?action=subscribe&amp;hidden_links=0">
-                    <fieldset>
+    <fieldset>
         <input type="hidden" name="sec_token" value="<?php echo $stok; ?>">
         <input type="hidden" name="search_course" value="1" />
-                        <div class="control-group">
-                            <div class="controls">
-                                <div class="input-append">
-                                    <input class="span2" type="text" name="search_term" value="<?php echo (empty($_POST['search_term']) ? '' : api_htmlentities(Security::remove_XSS($_POST['search_term']))); ?>" />
-                                    <button class="btn" type="submit"><?php echo get_lang('Search'); ?></button>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
+        <div class="control-group">
+            <div class="controls">
+                <div class="input-append">
+                    <input class="span2" type="text" name="search_term" value="<?php echo (empty($_POST['search_term']) ? '' : api_htmlentities(Security::remove_XSS($_POST['search_term']))); ?>" />
+                    <button class="btn" type="submit"><?php echo get_lang('Search'); ?></button>
+                </div>
+            </div>
+        </div>
+    </fieldset>
     </form>
 </div>
 <?php
@@ -56,84 +56,77 @@ $stok = Security::get_token();
 } else {
     $hidden_links = 1;
 }
-?>
-<div class="row">
-    <div class="span3">
-        <div id="course_category_well" class="well">
-            <ul class="nav nav-list">
-            <?php
 
-            if (!empty($browse_course_categories)) {
-                echo '<a class="btn" href="'.api_get_self().'?action=display_random_courses">'.get_lang('RandomPick').'</a><br /><br />';
+        if (!empty($browse_course_categories)) {
+            echo '<a class="btn" href="'.api_get_self().'?action=display_random_courses">'.get_lang('RandomPick').'</a><br /><br />';
 
-                echo '<li class="nav-header">'.get_lang('CourseCategories').'</li>';
+            echo '<li class="nav-header">'.get_lang('CourseCategories').'</li>';
 
-                // level 1
-                foreach ($browse_course_categories[0] as $category) {
-                    $category_name = $category['name'];
-                    $category_code = $category['code'];
-                    $count_courses_lv1 = $category['count_courses'];
+            // level 1
+            foreach ($browse_course_categories[0] as $category) {
+                $category_name = $category['name'];
+                $category_code = $category['code'];
+                $count_courses_lv1 = $category['count_courses'];
 
-                    if ($code == $category_code) {
-                        $category_link = '<strong>'.$category_name.' ('.$count_courses_lv1.')</strong>';
+                if ($code == $category_code) {
+                    $category_link = '<strong>'.$category_name.' ('.$count_courses_lv1.')</strong>';
+                } else {
+                    if (!empty($count_courses_lv1)) {
+                        $category_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$category_code.'&amp;hidden_links='.$hidden_links.'">'.$category_name.' ('.$count_courses_lv1.') </a>';
                     } else {
-                        if (!empty($count_courses_lv1)) {
-                            $category_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$category_code.'&amp;hidden_links='.$hidden_links.'">'.$category_name.' ('.$count_courses_lv1.') </a>';
-                        } else {
-                            $category_link = ''.$category_name.' ('.$count_courses_lv1.')';
-                        }
+                        $category_link = ''.$category_name.' ('.$count_courses_lv1.')';
                     }
-                    echo '<li>'.$category_link.'</li>';
+                }
+                echo '<li>'.$category_link.'</li>';
 
 
-                    // level 2
-                    if (!empty($browse_course_categories[$category_code])) {
-                        foreach ($browse_course_categories[$category_code] as $subcategory1) {
-                            $subcategory1_name = $subcategory1['name'];
-                            $subcategory1_code = $subcategory1['code'];
-                            $count_courses_lv2 = $subcategory1['count_courses'];
-                            if ($code == $subcategory1_code) {
-                                $subcategory1_link = '<strong>'.$subcategory1_name.' ('.$count_courses_lv2.')</strong>';
-                            } else {
-                                $subcategory1_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory1_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory1_name.' ('.$count_courses_lv2.') </a> ';
-                            }
-                            echo '<li style="margin-left:20px;">'.$subcategory1_link.'</li>';
+                // level 2
+                if (!empty($browse_course_categories[$category_code])) {
+                    foreach ($browse_course_categories[$category_code] as $subcategory1) {
+                        $subcategory1_name = $subcategory1['name'];
+                        $subcategory1_code = $subcategory1['code'];
+                        $count_courses_lv2 = $subcategory1['count_courses'];
+                        if ($code == $subcategory1_code) {
+                            $subcategory1_link = '<strong>'.$subcategory1_name.' ('.$count_courses_lv2.')</strong>';
+                        } else {
+                            $subcategory1_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory1_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory1_name.' ('.$count_courses_lv2.') </a> ';
                         }
+                        echo '<li style="margin-left:20px;">'.$subcategory1_link.'</li>';
+                    }
 
-                        // level 3
-                        if (!empty($browse_course_categories[$subcategory1_code])) {
-                            foreach ($browse_course_categories[$subcategory1_code] as $subcategory2) {
-                                $subcategory2_name = $subcategory2['name'];
-                                $subcategory2_code = $subcategory2['code'];
-                                $count_courses_lv3 = $subcategory2['count_courses'];
-                                if ($code == $subcategory2_code) {
-                                    $subcategory2_link = '<strong>'.$subcategory2_name.' ('.$count_courses_lv3.')</strong>';
-                                } else {
-                                    $subcategory2_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory2_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory2_name.'</a> ('.$count_courses_lv3.')';
-                                }
-                                echo '<li style="margin-left:40px;">'.$subcategory2_link.'</li>';
+                    // level 3
+                    if (!empty($browse_course_categories[$subcategory1_code])) {
+                        foreach ($browse_course_categories[$subcategory1_code] as $subcategory2) {
+                            $subcategory2_name = $subcategory2['name'];
+                            $subcategory2_code = $subcategory2['code'];
+                            $count_courses_lv3 = $subcategory2['count_courses'];
+                            if ($code == $subcategory2_code) {
+                                $subcategory2_link = '<strong>'.$subcategory2_name.' ('.$count_courses_lv3.')</strong>';
+                            } else {
+                                $subcategory2_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory2_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory2_name.'</a> ('.$count_courses_lv3.')';
+                            }
+                            echo '<li style="margin-left:40px;">'.$subcategory2_link.'</li>';
 
-                                // level 4
-                                if (!empty($browse_course_categories[$subcategory2_code])) {
-                                    foreach ($browse_course_categories[$subcategory2_code] as $subcategory3) {
-                                        $subcategory3_name = $subcategory3['name'];
-                                        $subcategory3_code = $subcategory3['code'];
-                                        $count_courses_lv4 = $subcategory3['count_courses'];
-                                        if ($code == $subcategory3_code) {
-                                            $subcategory3_link = '<strong>'.$subcategory3_name.' ('.$count_courses_lv4.') </strong>';
-                                        } else {
-                                            $subcategory3_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory3_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory3_name.' ('.$count_courses_lv4.') </a>';
-                                        }
-                                        echo '<li style="margin-left:60px;">'.$subcategory3_link.'</li>';
+                            // level 4
+                            if (!empty($browse_course_categories[$subcategory2_code])) {
+                                foreach ($browse_course_categories[$subcategory2_code] as $subcategory3) {
+                                    $subcategory3_name = $subcategory3['name'];
+                                    $subcategory3_code = $subcategory3['code'];
+                                    $count_courses_lv4 = $subcategory3['count_courses'];
+                                    if ($code == $subcategory3_code) {
+                                        $subcategory3_link = '<strong>'.$subcategory3_name.' ('.$count_courses_lv4.') </strong>';
+                                    } else {
+                                        $subcategory3_link = '<a href="'. api_get_self().'?action=display_courses&amp;category_code='.$subcategory3_code.'&amp;hidden_links='.$hidden_links.'">'.$subcategory3_name.' ('.$count_courses_lv4.') </a>';
                                     }
+                                    echo '<li style="margin-left:60px;">'.$subcategory3_link.'</li>';
                                 }
                             }
                         }
                     }
                 }
             }
-            }
-            ?>
+        }
+        ?>
         </div>
     </div>
 

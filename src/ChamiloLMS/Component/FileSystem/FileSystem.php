fe6ab41e394ae57c1308fbbb17e56857e16922c2
Julio Montoya <gugli100@gmail.com>
2013-04-15 14:39:31 +0200

    Updating composer files

--- src/ChamiloLMS/Component/FileSystem/FileSystem.php
+++ src/ChamiloLMS/Component/FileSystem/FileSystem.php
@@ -1,8 +1,3 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: jmontoya
- * Date: 4/11/13
- * Time: 2:15 PM
- * To change this template use File | Settings | File Templates.
- */
+
+namespace Symfony\Component\Filesystem;
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit6045f5f5c2161ad0fbc749276d508654::getLoader();
+return ComposerAutoloaderInit142b0d726e3437b29f0c0f0d7047c1f2::getLoader();

821fa0b3973a54f217a1aad391942177727ae922
Julio Montoya <gugli100@gmail.com>
2013-04-18 11:20:34 +0200

    Minor adding comments, adding folders in robots.txt

--- certificates/index.php
+++ certificates/index.php
@@ -1,6 +1,8 @@
 <?php
 /* For licensing terms, see /license.txt */
 
+//Redirects to web/certificates/X
+
 require_once '../main/inc/global.inc.php';
 $id = isset($_GET['id']) ? intval($_GET['id']) : null;
 $url = Certificate::getCertificatePublicURL($id);
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 /* For licensing terms, see /license.txt */
-
-/**
- * @package chamilo.main
- */
-
-//Temporal hack to redirect calls to the new web/index.php
+// Redirect calls to web/index
 header('Location: web/index');
 exit;

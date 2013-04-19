2dbf1c8b78ec1a0dc08e2cb8d7cf2e8d5211fa8b
Julio Montoya <gugli100@gmail.com>
2013-04-19 14:19:57 +0200

    Adding redirects in htaccess and removing certificates folder, news_list.php and user.php

--- news_list.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* For licensing terms, see /license.txt */
-
-//Temporal hack to redirect calls to the new web/index.php
-require_once 'main/inc/global.inc.php';
-$id = isset($_GET['id']) ? intval($_GET['id']) : null;
-$path = api_get_path(WEB_PUBLIC_PATH);
-header('Location: '.$path.'news/'.$id);
-exit;

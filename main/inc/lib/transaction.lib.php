a547d5c52905febbe50786eefe482823450fa745
Marco Villegas <git@marvil07.net>
2013-04-30 00:44:53 -0500

    Several improvements to TransactionLog class
    - Converts the class into abstract for better self-documentation: use a
      new class for each transaction type (action) added.
    - Defines a couple of constants to represent a local chamilo on a
      branch_id, TransactionLog::BRANCH_LOCAL, and on transaction_id,
      TransactionLog::TRANSACTION_LOCAL.
    - Improves loading methods to use more Database APIs and return objects
      instead of simple arrays.
    - Adds a method to retrive transaction log related settings from
      settings_current.
    - Starts ExerciseTestTransactionLog class to handle exercises attemps.

--- main/inc/lib/transaction.lib.php
+++ main/inc/lib/transaction.lib.php
@@ -9,18 +9,27 @@
  * @see Migration
  */
 
-class TransactionLog {
-  protected $table;
-  protected $branch_id;
+/**
+ * Base transaction log class.
+ */
+abstract class TransactionLog {
+  const BRANCH_LOCAL = 0;
+  const TRANSACTION_LOCAL = 0;
+  protected static $table;
 
   public function __construct($data) {
     $this->table = Database::get_main_table(TABLE_BRANCH_TRANSACTION);
-    $this->branch_id = Database::get_main_table(TABLE_BRANCH_TRANSACTION);
     // time_insert and time_update are handled manually.
-    $fields = array('id', 'transaction_id', 'branch_id', 'action', 'item_id', 'orig_id', 'dest_id', 'info', 'status_id');
+    $fields = array('id', 'action', 'branch_id', 'transaction_id', 'item_id', 'orig_id', 'dest_id', 'info', 'status_id');
     foreach ($fields as $field) {
       if (isset($data[$field])) {
         $this->$field = $data[$field];
+      }
+      elseif ($field == 'branch_id') {
+        $this->branch_id = TransactionLog::BRANCH_LOCAL;
+      }
+      elseif ($field == 'transaction_id') {
+        $this->transaction_id = TransactionLog::TRANSACTION_LOCAL;
       }
     }
   }
@@ -53,10 +62,74 @@ class TransactionLog {
   }
 
   /**
-   * Load by id.
+   * General load method.
+   */
+  public static function load($db_fields) {
+    foreach ($db_fields as $db_field => $db_value) {
+      $conditions[] = $db_field;
+      $values[] = $db_value;
+    }
+    $results = Database::select('*', self::$table, array('where' => array(implode(' = ? AND ', $conditions) => $values)));
+    $objects = array();
+    foreach ($results as $result) {
+      $objects[] = self::createInstance($transaction);
+    }
+    return $objects;
+  }
+
+  /**
+   * Loads by id.
    */
   public static function load_by_id($id) {
-    $transaction = Database::select('*', $this->table, array('where' => array('id = ?' => $id)));
-    return new TransactionLog($transaction);
+    $transactions = self::load(array('id' => $id));
+    if (empty($transactions)) {
+      return FALSE;
+    }
+    return array_shift($transactions);
+  }
+
+  /**
+   * Load by branch and transaction.
+   */
+  public static function load_by_branch_and_transaction($branch_id, $transaction_id) {
+    $transactions = $this->load(array('branch_id' => $branch_id, 'transaction_id' => $transaction_id));
+    if (empty($transactions)) {
+      return FALSE;
+    }
+    return array_shift($transactions);
+  }
+
+  public static function getTransactionSettings($reset = FALSE) {
+    static $settings;
+    if (isset($settings) && !$reset) {
+      return $settings;
+    }
+    $settings = array();
+    $log_transactions_settings = api_get_settings('LogTransactions');
+    foreach ($log_transactions_settings as $setting) {
+      $settings[$settings['subkey']] = $setting;
+    }
+    return $settings;
+  }
+
+  /**
+   * Creates an object of the current class.
+   */
+  public static abstract function createInstance($transaction);
+}
+
+class ExerciseTestTransactionLog extends TransactionLog {
+  public static function createInstance($transaction) {
+    $transaction['action'] = 'exercise_test';
+    return new ExerciseTestTransactionLog($transaction);
+  }
+
+  public static function load_exercise_test($exercise_id, $attempt_id, $branch_id = BRANCH_LOCAL) {
+    $exercise_test_id = sprintf('%s:%s', $exercise_id, $attempt_id);
+    $transactions = $this->load(array('branch_id' => $branch_id, 'item_id' => $exercise_test_id));
+    if (empty($transactions)) {
+      return FALSE;
+    }
+    return array_shift($transactions);
   }
 }

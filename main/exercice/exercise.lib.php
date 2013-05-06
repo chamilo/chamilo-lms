00458c8e8645a1ad29c21a628cc7c8a08c2c5c80
Marco Villegas <git@marvil07.net>
2013-05-06 16:34:51 -0500

    Add transaction saving after the other update_event_exercise() call.

--- main/exercice/exercise.lib.php
+++ main/exercice/exercise.lib.php
@@ -2485,6 +2485,23 @@ function display_question_list_by_attempt($objExercise, $exe_id, $save_user_resu
 
         if (api_is_allowed_to_session_edit()) {
             update_event_exercise($exercise_stat_info['exe_id'], $objExercise->selectId(), $total_score, $total_weight, api_get_session_id(), $learnpath_id, $learnpath_item_id, $learnpath_item_view_id, $exercise_stat_info['exe_duration'], '', array());
+            require_once api_get_path(LIBRARY_PATH).'transaction.lib.php';
+            $log_transactions_settings = TransactionLog::getTransactionSettings();
+            if (isset($log_transactions_settings['exercise_test'])) {
+              $exercise_test_id = sprintf('%s:%s', $objExercise->selectId(), $exe_id);
+              $transaction_controller = new ExerciseTestTransactionLogController();
+              $transaction = $transaction_controller->load_exercise_test($objExercise->selectId(), $exe_id);
+              if (!$transaction) {
+                $transaction_data = array(
+                  'item_id' => $exercise_test_id,
+                  'data' => array(
+                    'question_order' => implode(',', $objExercise->selectQuestionList()),
+                  ),
+                );
+                $transaction = new ExerciseTestTransactionLog($transaction_data);
+              }
+              $transaction->save();
+            }
         }
 
         // Send notification ..

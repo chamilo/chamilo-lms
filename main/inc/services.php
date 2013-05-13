c268388fefed54a5bc822a1e01ca692a022e19d4
Julio Montoya <gugli100@gmail.com>
2013-05-13 16:02:06 +0200

    Moving web profiler after the twig service

--- main/inc/services.php
+++ main/inc/services.php
@@ -10,6 +10,24 @@
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+// Monolog
+if (is_writable($app['sys_temp_path'])) {
+
+    /** Adding Monolog service provider Monolog  use examples
+    $app['monolog']->addDebug('Testing the Monolog logging.');
+    $app['monolog']->addInfo('Testing the Monolog logging.');
+    $app['monolog']->addError('Testing the Monolog logging.');
+     */
+
+    $app->register(
+        new Silex\Provider\MonologServiceProvider(),
+        array(
+            'monolog.logfile' => $app['chamilo.log'],
+            'monolog.name' => 'chamilo',
+        )
+    );
+}
+
 //Setting HttpCacheService provider in order to use do: $app['http_cache']->run();
 /*
 $app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
@@ -85,36 +103,6 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
 ));*/
 
 
-// Developer tools
-if (is_writable($app['sys_temp_path'])) {
-    if ($app['debug']) {
-        // Adding symfony2 web profiler (memory, time, logs, etc)
-        if (api_get_setting('allow_web_profiler') == 'true') {
-            $app->register($p = new Silex\Provider\WebProfilerServiceProvider(), array(
-                'profiler.cache_dir' => $app['profiler.cache_dir'],
-            ));
-            $app->mount('/_profiler', $p);
-        }
-
-        //$app->register(new Whoops\Provider\Silex\WhoopsServiceProvider);
-        //}
-    }
-
-    /** Adding Monolog service provider Monolog  use examples
-    $app['monolog']->addDebug('Testing the Monolog logging.');
-    $app['monolog']->addInfo('Testing the Monolog logging.');
-    $app['monolog']->addError('Testing the Monolog logging.');
-     */
-
-    $app->register(
-        new Silex\Provider\MonologServiceProvider(),
-        array(
-            'monolog.logfile' => $app['chamilo.log'],
-            'monolog.name' => 'chamilo',
-        )
-    );
-
-}
 // Setting Controllers as services provider
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
 
@@ -307,6 +295,22 @@ $app['twig'] = $app->share(
         return $twig;
     })
 );
+
+
+// Developer tools
+if (is_writable($app['sys_temp_path'])) {
+    if ($app['debug']) {
+        // Adding symfony2 web profiler (memory, time, logs, etc)
+        if (api_get_setting('allow_web_profiler') == 'true') {
+            $app->register($p = new Silex\Provider\WebProfilerServiceProvider(), array(
+                'profiler.cache_dir' => $app['profiler.cache_dir'],
+            ));
+            $app->mount('/_profiler', $p);
+        }
+        //$app->register(new Whoops\Provider\Silex\WhoopsServiceProvider);
+        //}
+    }
+}
 
 // Registering Menu service provider (too gently creating menus with the URLgenerator provider)
 $app->register(new \Knp\Menu\Silex\KnpMenuServiceProvider());

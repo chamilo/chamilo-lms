23604911e17284fcb02406c2fc06e89df54aac2f
Julio Montoya <gugli100@gmail.com>
2013-05-21 13:07:14 +0200

    Fixing URL link, reducing size of columns

--- main/exercice/question_list_pagination_admin.inc.php
+++ main/exercice/question_list_pagination_admin.inc.php
@@ -56,21 +56,21 @@ $column_model = array(
     array(
         'name'     => 'level',
         'index'    => 'level',
-        'width'    => '100',
+        'width'    => '50',
         'align'    => 'left',
         'sortable' => 'false'
     ),
     array(
         'name'     => 'score',
         'index'    => 'score',
-        'width'    => '100',
+        'width'    => '50',
         'align'    => 'left',
         'sortable' => 'false'
     ),
     array(
         'name'      => 'actions',
         'index'     => 'actions',
-        'width'     => '100',
+        'width'     => '50',
         'align'     => 'left',
         'formatter' => 'action_formatter',
         'sortable'  => 'false'
@@ -91,7 +91,7 @@ if ($objExercise->edit_exercise_in_lp == true) {
 }
 //With this function we can add actions to the jgrid (edit, delete, etc)
 $action_links = 'function action_formatter(cellvalue, options, rowObject) {
-    return \'<a href="?fromExercise='.$exerciseId.'&myid=1&cidReq='.$courseCode.'&editQuestion=\'+options.rowId+\'">'.Display::return_icon('edit.png', get_lang('Edit'), '', ICON_SIZE_SMALL).'</a>'.
+    return \'<a href="?exerciseId='.$exerciseId.'&myid=1&cidReq='.$courseCode.'&editQuestion=\'+options.rowId+\'">'.Display::return_icon('edit.png', get_lang('Edit'), '', ICON_SIZE_SMALL).'</a>'.
     '&nbsp;<a onclick="javascript:if(!confirm('."\'".addslashes(api_htmlentities(get_lang("ConfirmYourChoice"), ENT_QUOTES))."\'".')) return false;" href="?cidReq='.$courseCode.'&sec_token='.$token.'&clone_question=\'+options.rowId+\'">'.Display::return_icon('cd.gif',get_lang('Copy'), '',ICON_SIZE_SMALL).'</a>'.
     $delete_link.'\';
 }';

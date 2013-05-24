ad5b66254e194b4f3cb7cb7ce07452ac8fd0d6b4
Yannick Warnier <ywarnier@beeznest.org>
2013-05-24 13:27:47 -0500

    Updated version script for 1.9.6RC and moving to RC2 (because of that forgotten change)

--- main/install/version.php
+++ main/install/version.php
@@ -13,8 +13,8 @@
  * Variables used from the main/install/index.php
  */
 $new_version            = '1.9.6';
-$new_version_status     = 'dev';
-$new_version_last_id	= 0;
+$new_version_status     = 'RC';
+$new_version_last_id	= 1;
 $new_version_stable 	= false;
 $new_version_major      = false;
 $software_name          = 'Chamilo';

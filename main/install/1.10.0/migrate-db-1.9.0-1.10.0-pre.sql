69a9a3a8ca332c8aa93ecbe9bc92149223fa8b9c
Yannick Warnier <ywarnier@beeznest.org>
2013-05-26 18:14:01 -0500

    Minor - update SQL update file's doc

--- main/install/1.10.0/migrate-db-1.9.0-1.10.0-pre.sql
+++ main/install/1.10.0/migrate-db-1.9.0-1.10.0-pre.sql
@@ -1,14 +1,15 @@
 -- This script updates the databases structure before migrating the data from
--- version 1.9.0 (or version 1.9.2, 1.9.4) to version 1.10.0
--- it is intended as a standalone script, however, because of the multiple
--- databases related difficulties, it should be parsed by a PHP script in
--- order to connect to and update the right databases.
--- There is one line per query, allowing the PHP function file() to read
+-- version 1.9.0 (or version 1.9.2, .4, .6 or .8) to version 1.10.0
+-- It is intended as a standalone script. Because of the relatively recent
+-- migration to a single-database structure, it is still recommended to let
+-- it be parsed by Chamilo installation scripts. We will modify this message
+-- once we have ensured it works properly through direct SQL execution.
+-- There is one line per query now, allowing the PHP function file() to read
 -- all lines separately into an array. The xxMAINxx-type markers are there
 -- to tell the PHP script which database we're talking about.
--- By always using the keyword "TABLE" in the queries, we should be able
--- to retrieve and modify the table name from the PHP script if needed, which
--- will allow us to deal with the unique-database-type installations
+-- For version tracking easiness, we recommend you define all new queries
+-- at the end of the corresponding section. We will re-order them previous
+-- to the stable release in order to optimize the migration.
 
 -- xxMAINxx
 

23e1b191bd3fe623e31c6b7b6ba803b5ad5fcd13
Yoselyn Castillo <ycastillo06@gmail.com>
2013-05-28 14:29:28 -0400

    Removing message bottom -refs #5970

--- main/work/work.php
+++ main/work/work.php
@@ -715,8 +715,7 @@ switch ($action) {
                     $emailbody .= get_lang('DateSent')." : ".api_format_date(api_get_local_time())."\n";
                     $emailbody .= get_lang('WorkName')." : ".$title."\n\n".get_lang('DownloadLink')."\n";
                     $url = api_get_path(WEB_CODE_PATH)."work/work.php?".api_get_cidreq()."&amp;id=".$work_id;
-                    $emailbody .= Display::url($url, $url)." \n\n" . api_get_setting('administratorName') . " " . api_get_setting('administratorSurname') . "\n" . get_lang('Manager') . " " . api_get_setting('siteName') . "\n" . get_lang('Email') . " : " . api_get_setting('emailAdministrator');
-
+                    
                     MessageManager::send_message_simple($user_id, $emailsubject, $emailbody);
                 }
             }

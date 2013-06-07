f6e903b888960ca34264f9a4160048be6afdd0b3
Julio Montoya <gugli100@gmail.com>
2013-06-07 11:55:45 +0200

    Do not use constants, functions or anything chamilo related in config files.

--- main/inc/conf/mail.conf.dist.php
+++ main/inc/conf/mail.conf.dist.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
- *	This file holds the configuration settings
- *	for phpmailer Class.
- *
+ *	This file holds the email configuration settings
  *	@package chamilo.configuration
  */
 
@@ -11,7 +9,7 @@ $platform_email['SMTP_FROM_EMAIL']   = (isset($administrator['email'])?$administ
 $platform_email['SMTP_FROM_NAME']    = (isset($administrator['name'])?$administrator['name']:'Admin');
 $platform_email['SMTP_HOST']         = 'localhost';
 $platform_email['SMTP_PORT']         = 25;
-$platform_email['SMTP_MAILER']       = IS_WINDOWS_OS ? 'smtp' : 'mail'; // mail, sendmail or smtp
+$platform_email['SMTP_MAILER']       = 'mail'; // mail, sendmail or smtp
 $platform_email['SMTP_AUTH']         = 0;
 $platform_email['SMTP_USER']         = '';
 $platform_email['SMTP_PASS']         = '';

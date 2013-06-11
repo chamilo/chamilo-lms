386f118ca39c04942de76d48ea89d448daf567d3
Yoselyn Castillo <ycastillo06@gmail.com>
2013-06-11 13:04:50 -0400

    Removing unnecessary variables declaration -refs #6092

--- main/wiki/index.php
+++ main/wiki/index.php
@@ -291,8 +291,7 @@ if ($_GET['view']) {
                         Display::display_normal_message($is_being_edited, false);
 
                     } else {
-                         $escaped_content = mysql_real_escape_string($current_row['content']);
-                         Display::display_confirmation_message(restore_wikipage($current_row['page_id'], $current_row['reflink'], $current_row['title'], $escaped_content, $current_row['group_id'], $current_row['assignment'], $current_row['progress'], $current_row['version'], $last_row['version'], $current_row['linksto']).': <a href="index.php?cidReq='.$_course['id'].'&action=showpage&amp;title='.api_htmlentities(urlencode($last_row['reflink'])).'&session_id='.$last_row['session_id'].'&group_id='.$last_row['group_id'].'">'.api_htmlentities($last_row['title']).'</a>',false);
+                         Display::display_confirmation_message(restore_wikipage($current_row['page_id'], $current_row['reflink'], mysql_real_escape_string($current_row['title']), mysql_real_escape_string($current_row['content']), $current_row['group_id'], $current_row['assignment'], $current_row['progress'], $current_row['version'], $last_row['version'], $current_row['linksto']).': <a href="index.php?cidReq='.$_course['id'].'&action=showpage&amp;title='.api_htmlentities(urlencode($last_row['reflink'])).'&session_id='.$last_row['session_id'].'&group_id='.$last_row['group_id'].'">'.api_htmlentities($last_row['title']).'</a>',false);
                     }
                 }
             }

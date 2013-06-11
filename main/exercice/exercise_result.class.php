744aeaf3d7b729873687d94f810ae5c998789d2a
Julio Montoya <gugli100@gmail.com>
2013-06-11 15:06:55 +0200

    Fixing query (addin iid instead of id)

--- main/exercice/exercise_result.class.php
+++ main/exercice/exercise_result.class.php
@@ -115,7 +115,7 @@ class ExerciseResult
                         steps_counter as exstep,
                         exe_user_id as excruid,
                         te.exe_duration as duration
-                FROM $TBL_EXERCISES  AS ce INNER JOIN $TBL_TRACK_EXERCISES AS te ON (te.exe_exo_id = ce.id) INNER JOIN
+                FROM $TBL_EXERCISES  AS ce INNER JOIN $TBL_TRACK_EXERCISES AS te ON (te.exe_exo_id = ce.iid) INNER JOIN
                      $TBL_USER  AS user ON (user.user_id = exe_user_id)
                 WHERE   ce.c_id = $course_id AND
                         te.status != 'incomplete' AND
@@ -150,7 +150,7 @@ class ExerciseResult
                     exe_user_id as excruid,
                     te.exe_duration as duration,
                     ce.results_disabled as exdisabled
-                        FROM $TBL_EXERCISES  AS ce INNER JOIN $TBL_TRACK_EXERCISES AS te ON (te.exe_exo_id = ce.id)
+                        FROM $TBL_EXERCISES  AS ce INNER JOIN $TBL_TRACK_EXERCISES AS te ON (te.exe_exo_id = ce.iid)
                         INNER JOIN $TBL_USER  AS user ON (user.user_id = exe_user_id)
                         WHERE   ce.c_id = $course_id AND
                                 te.status != 'incomplete' AND

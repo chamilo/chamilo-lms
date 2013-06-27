2b4bc04a662c01efc569aafcd1ec1884f6092b6c
Julio Montoya <gugli100@gmail.com>
2013-06-27 12:11:05 +0200

    Restoring removed function.

--- main/inc/lib/internationalization_internal.lib.php
+++ main/inc/lib/internationalization_internal.lib.php
@@ -1158,3 +1158,28 @@ if (MBSTRING_INSTALLED && !function_exists('mb_strstr')) {
         return mb_substr($haystack, $pos, mb_strlen($haystack, $encoding), $encoding);
     }
 }
+
+/**
+ * Returns an array of translated week days and months, short and normal names.
+ * @param string $language (optional)	Language indentificator. If it is omited, the current interface language is assumed.
+ * @return array						Returns a multidimensional array with translated week days and months.
+ */
+function &_api_get_day_month_names($language = null) {
+    static $date_parts = array();
+    if (empty($language)) {
+        $language = api_get_interface_language();
+    }
+    if (!isset($date_parts[$language])) {
+        $week_day = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        $month = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        for ($i = 0; $i < 7; $i++) {
+            $date_parts[$language]['days_short'][] = get_lang($week_day[$i].'Short', '', $language);
+            $date_parts[$language]['days_long'][] = get_lang($week_day[$i].'Long', '', $language);
+        }
+        for ($i = 0; $i < 12; $i++) {
+            $date_parts[$language]['months_short'][] = get_lang($month[$i].'Short', '', $language);
+            $date_parts[$language]['months_long'][] = get_lang($month[$i].'Long', '', $language);
+        }
+    }
+    return $date_parts[$language];
+}

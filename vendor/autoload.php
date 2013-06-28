3a484b8d25020310592378ac723eb68c6780acea
Julio Montoya <gugli100@gmail.com>
2013-06-28 12:25:53 +0200

    Updating vendors

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit8811d572e3015a10a8864726603bac3a::getLoader();
+return ComposerAutoloaderInit6849b8aa9cb29cb82589890fae069cb3::getLoader();

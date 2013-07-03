baaf50345dea3beec15f4b327939817224e97b40
jmontoya <gugli100@gmail.com>
2013-07-03 17:33:08 +0200

    Adding toolbar for LP documents.

---
+++ src/ChamiloLMS/Component/Editor/Toolbar/LearningPathDocuments.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ChamiloLMS\Component\Editor\Toolbar;
+
+class LearningPathDocuments extends Basic
+{
+
+    public function getConfig()
+    {
+        $config['toolbarGroups'] = array(
+            array('name' => 'document',  'groups' =>array('mode', 'document', 'doctools')),
+            array('name' => 'clipboard',    'groups' =>array('clipboard', 'undo', )),
+            array('name' => 'editing',    'groups' =>array('clipboard', 'undo', )),
+            array('name' => 'forms',    'groups' =>array('clipboard', 'undo', )),
+            '/',
+            array('name' => 'basicstyles',    'groups' =>array('basicstyles', 'cleanup', )),
+            array('name' => 'paragraph',    'groups' =>array('list', 'indent', 'blocks', 'align' )),
+            array('name' => 'links'),
+            array('name' => 'insert'),
+            '/',
+            array('name' => 'styles'),
+            array('name' => 'colors'),
+            array('name' => 'tools'),
+            array('name' => 'others')
+        );
+
+        $config['fullPage'] = 'true';
+
+        return $config;
+    }
+}
+
+

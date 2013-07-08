26c3b09efa19067b842efc04df34b87188f5405a
Yoselyn Castillo <ycastillo06@gmail.com>
2013-07-07 22:38:58 -0400

    Showing active sessions with nb_days_access_after_end > 0 and nb_days_access_before_beginning -refs #6311

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -241,13 +241,15 @@ class SessionManager {
         $today = date('Y-m-d', $today);
 
 		$select = "SELECT * FROM (SELECT
-                IF (
-                    (s.date_start <= '$today' AND '$today' < s.date_end) OR
-                    (DATEDIFF(s.date_start,'".$today."' ".") <= s.nb_days_access_before_beginning) OR                     
+                IF ( 
+					(s.date_start <= '$today' AND '$today' < s.date_end) OR
+                    (s.nb_days_access_before_beginning > 0 AND DATEDIFF(s.date_start,'".$today."' ".") <= s.nb_days_access_before_beginning) OR
+                    (s.nb_days_access_after_end > 0 AND DATEDIFF('".$today."',s.date_end) <= s.nb_days_access_after_end) OR
                     (s.date_start  = '0000-00-00' AND s.date_end  = '0000-00-00' ) OR
-                    (s.date_start <= '$today' AND '0000-00-00' = s.date_end) OR
-                    ('$today' < s.date_end AND '0000-00-00' = s.date_start)
-                , 1, 0)
+					(s.date_start <= '$today' AND '0000-00-00' = s.date_end) OR
+					('$today' < s.date_end AND '0000-00-00' = s.date_start)
+                    
+				, 1, 0) 
 				as session_active,
 				s.name,
                 nbr_courses,

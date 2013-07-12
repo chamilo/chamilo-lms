b5ba0faeb664b97d2d6095cb9f6af8f0a00e25cc
Julio Montoya <gugli100@gmail.com>
2013-07-12 19:54:45 +0200

    AAdding session manager role

--- main/inc/services.php
+++ main/inc/services.php
@@ -96,6 +96,7 @@ $app['security.role_hierarchy'] = array(
     'ROLE_TEACHER' => array('ROLE_STUDENT'),
     'ROLE_RRHH' => array('ROLE_TEACHER'),
     'ROLE_QUESTION_MANAGER' => array('ROLE_QUESTION_MANAGER'),
+    'ROLE_SESSION_MANAGER' => array('ROLE_SESSION_MANAGER'),
     'ROLE_STUDENT' => array('ROLE_STUDENT'),
     'ROLE_ANONYMOUS' => array('ROLE_ANONYMOUS')
 );

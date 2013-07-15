10f3aea237d1e3fce7f73d4bfb39f72aea596637
Julio Montoya <gugli100@gmail.com>
2013-07-15 14:30:40 +0200

    Updating api.lib.php

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -2186,7 +2186,7 @@ function api_get_session_condition($session_id, $and = true, $with_base_content 
  * @author Bart Mollet
  */
 function api_get_setting($variable, $key = null) {
-    global $_setting;
+    $_setting = Session::read('_setting');
     if ($variable == 'header_extra_content') {
         $filename = api_get_path(SYS_PATH).api_get_home_path().'header_extra_content.txt';
         if (file_exists($filename)) {
@@ -2246,8 +2246,11 @@ function api_delete_settings_params($params) {
  * @return string   Escaped version of $_SERVER['PHP_SELF']
  */
 function api_get_self() {
-    $urlInfo = parse_url($_SERVER['REQUEST_URI']);
-    return $urlInfo['path'];
+    if (isset($_SERVER['REQUEST_URI'])) {
+        $urlInfo = parse_url($_SERVER['REQUEST_URI']);
+        return $urlInfo['path'];
+    }
+    return null;
     //return $_SERVER['REQUEST_URI'];
     //return htmlentities($_SERVER['PHP_SELF']);
 }
@@ -2261,30 +2264,76 @@ function api_get_self() {
  * false otherwise.
  * @see usermanager::is_admin(user_id) for a user-id specific function
  */
-function api_is_platform_admin($allow_sessions_admins = false) {
+function api_is_platform_admin($allow_sessions_admins = false)
+{
     global $app;
-    $isAdmin = Session::read('is_platformAdmin');
-    if ($isAdmin) {
+    if ($app['security']->isGranted('ROLE_ADMIN')) {
         return true;
     }
-    //$_user = api_get_user_info();
 
     if ($app['security']->isGranted('ROLE_SESSION_MANAGER')) {
         return true;
     }
     return false;
-    //  isset($_user['status']) && $_user['status'] == SESSIONADMIN;
-
 }
 
-function api_is_question_manager() {
+function api_is_question_manager()
+{
     global $app;
     if ($app['security']->isGranted('ROLE_QUESTION_MANAGER')) {
         return true;
     }
     return false;
-    /*$_user = api_get_user_info();
-    return isset($_user['status']) && $_user['status'] == QUESTION_MANAGER;*/
+}
+
+
+/**
+ * Checks whether the current user is a session administrator
+ * @return boolean True if current user is a course administrator
+ */
+function api_is_session_admin()
+{
+    global $app;
+    if ($app['security']->isGranted('ROLE_SESSION_MANAGER')) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks whether the current user is a human resources manager
+ * @return boolean True if current user is a human resources manager
+ */
+function api_is_drh() {
+    global $app;
+    if ($app['security']->isGranted('ROLE_RRHH')) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks whether the current user is a student
+ * @return boolean True if current user is a human resources manager
+ */
+function api_is_student() {
+    global $app;
+    if ($app['security']->isGranted('ROLE_STUDENT')) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * Checks whether the current user is a teacher
+ * @return boolean True if current user is a human resources manager
+ */
+function api_is_teacher() {
+    global $app;
+    if ($app['security']->isGranted('ROLE_TEACHER')) {
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -2313,6 +2362,7 @@ function api_is_platform_admin_by_id($user_id = null, $url = null) {
     $is_on_url = Database::num_rows($res) === 1;
     return $is_on_url;
 }
+
 /**
  * Returns the user's numeric status ID from the users table
  * @param int User ID. If none provided, will use current user
@@ -2532,41 +2582,6 @@ function api_is_coach($session_id = 0, $courseId = null) {
     return $result;
 }
 
-/**
- * Checks whether the current user is a session administrator
- * @return boolean True if current user is a course administrator
- */
-function api_is_session_admin() {
-    global $_user;
-    return isset($_user['status']) && $_user['status'] == SESSIONADMIN;
-}
-
-/**
- * Checks whether the current user is a human resources manager
- * @return boolean True if current user is a human resources manager
- */
-function api_is_drh() {
-    global $_user;
-    return isset($_user['status']) && $_user['status'] == DRH;
-}
-
-/**
- * Checks whether the current user is a student
- * @return boolean True if current user is a human resources manager
- */
-function api_is_student() {
-    global $_user;
-    return isset($_user['status']) && $_user['status'] == STUDENT;
-
-}
-/**
- * Checks whether the current user is a teacher
- * @return boolean True if current user is a human resources manager
- */
-function api_is_teacher() {
-    global $_user;
-    return isset($_user['status']) && $_user['status'] == COURSEMANAGER;
-}
 
 /**
  * This function checks whether a session is assigned into a category
@@ -2881,7 +2896,7 @@ function api_is_allowed_to_session_edit($tutor = false, $coach = false) {
 */
 function api_is_allowed($tool, $action, $task_id = 0) {
     $_course = api_get_course_info();
-    global $_user;
+    $_user = api_get_user_info();
 
     if (api_is_course_admin()) {
         return true;
@@ -3865,8 +3880,9 @@ function api_string_2_boolean($string) {
 /**
  * Determines the number of plugins installed for a given location
  */
-function api_number_of_plugins($location) {
-    global $_plugins;
+function api_number_of_plugins($location)
+{
+    $_plugins = Session::read('_plugins');
     return isset($_plugins[$location]) && is_array($_plugins[$location]) ? count($_plugins[$location]) : 0;
 }
 
@@ -3875,8 +3891,9 @@ function api_number_of_plugins($location) {
  * @author Patrick Cool <patrick.cool@UGent.be>, Ghent University
  * @deprecated use AppPlugin::get_all_plugin_contents_by_region function
  */
-function api_plugin($location) {
-    global $_plugins;
+function api_plugin($location)
+{
+    $_plugins = Session::read('_plugins');
     if (isset($_plugins[$location]) && is_array($_plugins[$location])) {
         foreach ($_plugins[$location] as $this_plugin) {
             include api_get_path(SYS_PLUGIN_PATH)."$this_plugin/index.php";
@@ -4289,7 +4306,6 @@ function api_get_version() {
  * @return string
  */
 function api_get_software_name() {
-    //return 'Chamilo';
     return 'Desarrollo docente';
     /*global $_configuration;
     if (isset($_configuration['software_name']) && !empty($_configuration['software_name'])) {
@@ -4393,13 +4409,19 @@ function api_delete_setting_option($id) {
  * @param int       The access_url for which this parameter is valid
  */
 function api_set_setting($var, $value, $subvar = null, $cat = null, $access_url = 1) {
-    if (empty($var)) { return false; }
+    if (empty($var)) {
+        return false;
+    }
     $t_settings = Database::get_main_table(TABLE_MAIN_SETTINGS_CURRENT);
     $var = Database::escape_string($var);
     $value = Database::escape_string($value);
     $access_url = (int)$access_url;
-    if (empty($access_url)) { $access_url = 1; }
+
+    if (empty($access_url)) {
+        $access_url = 1;
+    }
     $select = "SELECT id FROM $t_settings WHERE variable = '$var' ";
+
     if (!empty($subvar)) {
         $subvar = Database::escape_string($subvar);
         $select .= " AND subkey = '$subvar'";
@@ -4415,6 +4437,7 @@ function api_set_setting($var, $value, $subvar = null, $cat = null, $access_url 
     }
 
     $res = Database::query($select);
+
     if (Database::num_rows($res) > 0) {
         // Found item for this access_url.
         $row = Database::fetch_array($res);
@@ -4814,7 +4837,7 @@ function api_is_course_visible_for_user($userid = null, $cid = null) {
     $cid = Database::escape_string($cid);
     $courseInfo = api_get_course_info($cid);
     $courseId = $courseInfo['real_id'];
-    global $is_platformAdmin;
+    $is_platformAdmin = api_is_platform_admin();
 
     $course_table = Database::get_main_table(TABLE_MAIN_COURSE);
     $course_cat_table = Database::get_main_table(TABLE_MAIN_CATEGORY);
@@ -6176,8 +6199,10 @@ function api_is_global_chat_enabled(){
  * @param int The integer course ID, in case we cannot get it from the context
  * @todo Fix tool_visible_by_default_at_creation labels
  */
-function api_set_default_visibility($courseInfo, $item_id, $tool_id, $group_id = null) {
+function api_set_default_visibility($courseInfo, $item_id, $tool_id, $group_id = null)
+{
     $original_tool_id = $tool_id;
+
     switch ($tool_id) {
         case TOOL_LINK:
             $tool_id = 'links';
@@ -6342,7 +6367,6 @@ function api_get_js_simple($file)
 {
     return '<script type="text/javascript" src="'.$file.'"></script>'."\n";
 }
-
 
 function api_set_settings_and_plugins()
 {

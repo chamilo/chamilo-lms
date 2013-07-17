0fb20f3c5a9894a483c3caff2ba1085c90b8a974
Julio Montoya <gugli100@gmail.com>
2013-07-17 19:07:23 +0200

    Adding wordcount ckeditor plugin.

---
+++ main/inc/lib/javascript/ckeditor/plugins/wordcount/lang/es.js
@@ -0,0 +1,10 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang('wordcount', 'es', {
+    WordCount: 'Palabras:',
+    CharCount: 'Caràcteres:',
+    limit: 'Límite:',
+    title: 'Estadísticas'
+});

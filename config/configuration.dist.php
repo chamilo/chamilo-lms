fe6a9a8c978d82e7eb86667e7786bc4242a753c1
Yannick Warnier <ywarnier@beeznest.org>
2013-07-23 16:01:37 -0500

    Added comment about root_url to enable multiple urls

--- config/configuration.dist.php
+++ config/configuration.dist.php
@@ -38,6 +38,7 @@ $_configuration['main_database'] = '{DATABASE_MAIN}';
 
 /** Directory settings */
 // URL to the root of your Chamilo installation, e.g.: http://www.mychamilo.com/
+// Comment this line if you want to enable multiple URLs
 $_configuration['root_web']    = '{ROOT_WEB}';
 
 /** Chamilo will automatically manage the following paths */

fbc7620d8e8ed41dfe2da24c4c6509aa93b4724f
Yannick Warnier <ywarnier@beeznest.org>
2013-07-23 15:39:15 -0500

    Fix require path in access url scripts

--- main/admin/access_url_edit_users_to_url.php
+++ main/admin/access_url_edit_users_to_url.php
@@ -13,7 +13,7 @@ $language_file='admin';
 $cidReset = true;
 
 require_once '../inc/global.inc.php';
-require_once '../inc/lib/xajax/xajax.inc.php';
+require_once __DIR__.'/../inc/lib/xajax/xajax.inc.php';
 $xajax = new xajax();
 
 $xajax -> registerFunction (array('search_users', 'Accessurledituserstourl', 'search_users'));

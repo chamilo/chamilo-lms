c77d1adfaca7738c03b2f02aef892c2f00b2a92c
Yannick Warnier <ywarnier@beeznest.org>
2013-08-18 16:34:56 -0500

    Fixed missing virtual keyboard on password field - refs BT#5674

--- src/ChamiloLMS/Controller/IndexController.php
+++ src/ChamiloLMS/Controller/IndexController.php
@@ -298,7 +298,14 @@ class IndexController extends CommonController
                 'autofocus' => 'autofocus'
             )
         );
-        $form->addElement('password', 'password', get_lang('Pass'), array('class' => 'input-medium '));
+        $form->addElement(
+            'password', 
+            'password', 
+            get_lang('Pass'), 
+            array(
+                'class' => 'input-medium virtualkey'
+            )
+        );
         $form->addElement('style_submit_button', 'submitAuth', get_lang('LoginEnter'), array('class' => 'btn'));
         $html = $form->return_form();
         /*if (api_get_setting('openid_authentication') == 'true') {

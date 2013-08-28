dc3cc1d7e17f5957916dc031b0d3f41572a589e9
Julio Montoya <gugli100@gmail.com>
2013-08-28 13:08:16 +0200

    Minor - removing comments

--- console.php
+++ console.php
@@ -71,10 +71,7 @@ $cli->addCommands(
         new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand(),
 
         // Chamilo commands.
-        /*new ChamiloLMS\Command\Database\UpgradeCommand(),
-        new ChamiloLMS\Command\Database\InstallCommand(),
-        new ChamiloLMS\Command\Database\StatusCommand(),
-        new ChamiloLMS\Command\Database\SetupCommand(),*/
+
         new ChamiloLMS\Command\Template\AsseticDumpCommand(),
         new ChamiloLMS\Command\Translation\ExportLanguagesCommand(),
 

dbc103b9ed13cd8436310cc9536edd62771cd028
Julio Montoya <gugli100@gmail.com>
2013-08-30 11:31:40 +0200

    Updating chash

--- vendor/chamilo/chash/src/Chash/Resources/Database/1.10.0/db_course.sql
+++ vendor/chamilo/chash/src/Chash/Resources/Database/1.10.0/db_course.sql
@@ -1324,6 +1324,7 @@ CREATE TABLE c_quiz (
   question_selection_type INT default 1,
   hide_question_title INT default 0,
   score_type_model INT default 0,
+  global_category_id INT default NULL,
   PRIMARY KEY (iid)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;

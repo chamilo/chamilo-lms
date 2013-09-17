887b40c337a8c3a1f7ea83b729f3adaaa614d3e0
Julio Montoya <gugli100@gmail.com>
2013-09-17 11:16:15 +0200

    Updating work from 1.9.x

--- main/work/view.php
+++ main/work/view.php
@@ -1,5 +1,7 @@
 <?php
 
+/* For licensing terms, see /license.txt */
+
 $language_file = array('exercice', 'work', 'document', 'admin');
 
 require_once '../inc/global.inc.php';
@@ -7,30 +9,32 @@ $current_course_tool  = TOOL_STUDENTPUBLICATION;
 
 require_once 'work.lib.php';
 
-$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : null;
+$id = isset($_GET['id']) ? intval($_GET['id']) : null;
 $work = get_work_data_by_id($id);
 
 if (empty($id) || empty($work)) {
-	api_not_allowed();
+    api_not_allowed();
 }
 $interbreadcrumb[] = array ('url' => 'work.php', 'name' => get_lang('StudentPublications'));
 
 $my_folder_data = get_work_data_by_id($work['parent_id']);
 $course_info = api_get_course_info();
 
+allowOnlySubscribedUser(api_get_user_id(), $work['parent_id'], $course_info['real_id']);
+
 if (user_is_author($id) || $course_info['show_score'] == 0 && $work['active'] == 1 && $work['accepted'] == 1) {
     $url_dir = 'work.php?&id=' . $my_folder_data['id'];
-    $interbreadcrumb[] = array ('url' => $url_dir,'name' =>  $my_folder_data['title']);	
-    $interbreadcrumb[] = array ('url' => '#','name' =>  $work['title']);	
+    $interbreadcrumb[] = array ('url' => $url_dir,'name' =>  $my_folder_data['title']);
+    $interbreadcrumb[] = array ('url' => '#','name' =>  $work['title']);
 
     if (($course_info['show_score'] == 0 && $work['active'] == 1 && $work['accepted'] == 1) || api_is_allowed_to_edit() || ($work['user_id'] == api_get_user_id() && $work['active'] == 1 && $work['accepted'] == 1)) {
-        $tpl = $app['template'];
+        $tpl = new Template();
         $tpl->assign('work', $work);
-        $template = $tpl->get_template('work/view.tpl');	
+        $template = $tpl->get_template('work/view.tpl');
         $content  = $tpl->fetch($template);
         $tpl->assign('content', $content);
-        $tpl->display_one_col_template();	
+        $tpl->display_one_col_template();
     } else {
-        api_not_allowed();	
+        api_not_allowed();
     }
 }

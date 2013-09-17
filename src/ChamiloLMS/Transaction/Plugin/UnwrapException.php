c169eedaf403c842b395550308108c5d5427ba91
Marco Villegas <git@marvil07.net>
2013-09-17 18:34:45 -0500

    tx-io: Initial wrapper plugin interface and exceptions.

---
+++ src/ChamiloLMS/Transaction/Plugin/UnwrapException.php
@@ -0,0 +1,11 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace ChamiloLMS\Transaction\Plugin;
+
+/**
+ * A custom exception for envelope unwrapping.
+ */
+class UnwrapException extends \Exception
+{
+}
---
+++ src/ChamiloLMS/Transaction/Plugin/WrapperPluginInterface.php
@@ -0,0 +1,41 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace ChamiloLMS\Transaction\Plugin;
+
+/**
+ * Defines how a transaction is handled before going out of the system.
+ */
+interface WrapperPluginInterface
+{
+    /**
+     * Wraps a list of transactions.
+     *
+     * @param array $transactions
+     *   A list of \ChamiloLMS\Transaction\TransactionLog objects to wrap
+     *   together. The transactions passed should be in export format, so this
+     *   function does not require to call transaction export() method.
+     *
+     * @return string
+     *   The wrapped envelope blob.
+     *
+     * @throws WrapException
+     *   When there is an error on the wrapping process.
+     */
+    public function wrap($transactions);
+
+    /**
+     * Wraps a list of transactions.
+     *
+     * @param string $envelope_blob
+     *   The transactions wrapped with the corresponding wrap() method.
+     *
+     * @return array
+     *   A set of \ChamiloLMS\Transaction\TransactionLog objects retrieved from
+     *   the envelope blob.
+     *
+     * @throws UnwrapException
+     *   When there is an error on the unwrapping process.
+     */
+    public function unwrap($envelope_blob);
+}

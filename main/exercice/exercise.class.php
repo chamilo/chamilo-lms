be2ef68b7adac7094a9661067c8e7230f75508d9
Julio Montoya <gugli100@gmail.com>
2013-09-24 13:18:38 +0200

    Minor - cleaning code

--- main/exercice/exercise.class.php
+++ main/exercice/exercise.class.php
@@ -579,7 +579,6 @@ class Exercise
     {
         if (!empty($this->id)) {
             $category_list = Testcategory::getListOfCategoriesNameForTest($this->id, false);
-            //$category_list = Testcategory::getListOfCategoriesIDForTestObject($this);
 
             $TBL_EXERCICE_QUESTION = Database::get_course_table(TABLE_QUIZ_TEST_QUESTION);
             $TBL_QUESTIONS = Database::get_course_table(TABLE_QUIZ_QUESTION);
@@ -6042,6 +6041,13 @@ class Exercise
         return $categoriesWithQuestion;
     }
 
+    /**
+     * @param array $questionList
+     * @param int $currentQuestion
+     * @param array $conditions
+     * @param string $link
+     * @return string
+     */
     public function progressExercisePaginationBar($questionList, $currentQuestion, $conditions, $link)
     {
         $mediaQuestions = $this->getMediaList();
@@ -6074,6 +6080,7 @@ class Exercise
                     true,
                     true
                 );
+
                 $counter += count($mediaQuestions[$questionId]) - 1 ;
                 $before = count($questionList);
                 $wasMedia = true;
@@ -6100,8 +6107,12 @@ class Exercise
      * @param string $link
      * @return string
      */
-    public function progressExercisePaginationBarWithCategories($categories, $current, $conditions = array(), $link = null)
-    {
+    public function progressExercisePaginationBarWithCategories(
+        $categories,
+        $current,
+        $conditions = array(),
+        $link = null
+    ) {
         $html = null;
         $counterNoMedias = 0;
         $nextValue = 0;
@@ -6133,12 +6144,6 @@ class Exercise
                 // The new categories list starts empty
                 $newCategoryList = array();
                 foreach ($categories as $category) {
-                    /*if ($this->categoryMinusOne) {
-                        $rootElement = $category['id'];
-                    } else {
-
-                    }*/
-
                     $rootElement = $category['root'];
 
                     if (isset($category['parent_info'])) {
@@ -6162,55 +6167,6 @@ class Exercise
                 // Now use the newly built categories list, with only parents
                 $categories = $newCategoryList;
             }
-            /*
-            $showCategoriesGrouped = true;
-            if (in_array(
-                $selectionType,
-                array(
-                    EX_Q_SELECTION_CATEGORIES_RANDOM_QUESTIONS_ORDERED_NO_GROUPED,
-                    EX_Q_SELECTION_CATEGORIES_RANDOM_QUESTIONS_RANDOM_NO_GROUPED
-                )
-            )) {
-                $showCategoriesGrouped = false;
-
-                if ($showCategoriesGrouped) {
-
-                } else {
-                    $globalQuestionList = array();
-                    // @todo test with medias
-                    foreach ($categories as $category) {
-                        // Check if in this category there questions added in a media
-                        $mediaQuestionId = $category['media_question'];
-                        $isMedia = false;
-                        $fixedValue = null;
-
-                        // Media exists!
-                        if ($mediaQuestionId != 999) {
-                            $isMedia = true;
-                            $fixedValue = $counterNoMedias;
-                        }
-                        $globalQuestionList = array_merge($globalQuestionList, $category['question_list']);
-                    }
-
-                    $html .= '<div class="row">';
-                    $html .= '<div class="span8">';
-
-                    $html .= Display::progressPaginationBar(
-                        $nextValue,
-                        $globalQuestionList,
-                        $current,
-                        $fixedValue,
-                        $conditions,
-                        $link,
-                        $isMedia,
-                        true
-                    );
-                    $html .= '</div>';
-                    $html .= '</div>';
-                    return $html;
-
-                }
-            }*/
 
             foreach ($categories as $category) {
                 $questionList = $category['question_list'];

58bab7a00f7a7b27553841064dd6512af8b68fbc
Julio Montoya <gugli100@gmail.com>
2013-09-27 16:11:30 +0200

    Adding new db changes see BT#6718

--- main/install/1.10.0/update.sql
+++ main/install/1.10.0/update.sql
@@ -4,9 +4,8 @@ INSERT INTO roles (name, role) VALUES('Jury substitute', 'ROLE_JURY_SUBSTITUTE')
 INSERT INTO roles (name, role) VALUES('Director', 'ROLE_DIRECTOR');
 
 -- Add new configuration setting for action related transaction settings.
-INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES
-('log_transactions','exercise_attempt','checkbox','LogTransactions','false','LogTransactionsForExerciseAttempts','LogTransactionsForExerciseAttemptsComment',NULL,'LogTransactionsForExerciseAttemptsText', 1),
-('transaction_action_map','exercise_attempt','text','TransactionMapping','a:0:{}','TransactionMapForExerciseAttempts','TransactionMapForExerciseAttemptsComment',NULL,'TransactionMapForExerciseAttemptsText', 1);
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('log_transactions','exercise_attempt','checkbox','LogTransactions','false','LogTransactionsForExerciseAttempts','LogTransactionsForExerciseAttemptsComment',NULL,'LogTransactionsForExerciseAttemptsText', 1);
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('transaction_action_map','exercise_attempt','text','TransactionMapping','a:0:{}','TransactionMapForExerciseAttempts','TransactionMapForExerciseAttemptsComment',NULL,'TransactionMapForExerciseAttemptsText', 1);
 
 CREATE TABLE jury (
   id int NOT NULL AUTO_INCREMENT,
@@ -55,3 +54,34 @@ RENAME TABLE branch_sync_log TO branch_transaction_log;
 
 UPDATE settings_current SET selected_value = 'minedu' WHERE variable = 'template';
 UPDATE settings_current SET selected_value = 'digedd' WHERE variable = 'stylesheets';
+
+
+CREATE TABLE c_quiz_distribution (
+  id int unsigned not null primary key AUTO_INCREMENT,
+  exercise_id int unsigned not null,
+  -- the list of questions id that the student will have to go through for this form, split by ","
+  -- (as in track_e_exercices - this will avoid 60 more queries to the next table once the exam is taking place)
+  data_tracking text not null default '',
+  active tinyint not null default 1,
+  author_user_id int unsigned not null,
+  last_generation_date datetime default null
+);
+
+CREATE TABLE c_quiz_distribution_questions (
+  id int unsigned not null primary key AUTO_INCREMENT,
+  quiz_distribution_id int unsigned not null, -- the id of the quiz distribution
+  category_id int unsigned, -- the (global) category ID of the question that has been selected
+  question_id int unsigned -- the (global) question ID
+);
+
+CREATE TABLE c_quiz_distribution_rel_session (
+  id int unsigned not null primary key AUTO_INCREMENT,
+  session_id int unsigned not null, -- the session id
+  c_id int unsigned not null, -- the course id (in case more than one course per session)
+  exercise_id int unsigned not null, -- the quiz global id
+  quiz_distribution_id int unsigned not null -- one of the valid distributions for this turn
+);
+
+--store the distribution ID that was assigned to this user (SUPER IMPORTANT TRACKING INFO, DO NOT MISS THIS)
+UPDATE TABLE track_e_exercices ADD COLUMN quiz_distribution_id int unsigned default null;
+

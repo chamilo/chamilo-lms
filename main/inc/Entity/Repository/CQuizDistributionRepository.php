1a7f15cb4ff47e7489235f252a7d1c004ab0114b
Julio Montoya <gugli100@gmail.com>
2013-10-08 17:07:26 +0200

    Fixing php warnings.

--- main/inc/Entity/Repository/CQuizDistributionRepository.php
+++ main/inc/Entity/Repository/CQuizDistributionRepository.php
@@ -51,11 +51,14 @@ class CQuizDistributionRepository extends EntityRepository
 
         // 5. Getting questions from each category
         $questionsPerCategoryInExercise = array();
-        foreach ($categoriesInExercise as $categoryInfo) {
-            $categoryId = $categoryInfo['category_id'];
 
-            $questions = \Testcategory::getQuestionsByCategory($categoryId);
-            $questionsPerCategoryInExercise[$categoryId] = $questions;
+        if (!empty($categoriesInExercise)) {
+            foreach ($categoriesInExercise as $categoryInfo) {
+                $categoryId = $categoryInfo['category_id'];
+
+                $questions = \Testcategory::getQuestionsByCategory($categoryId);
+                $questionsPerCategoryInExercise[$categoryId] = $questions;
+            }
         }
 
         // 6. Checking if the quiz distribution exists
@@ -78,73 +81,76 @@ class CQuizDistributionRepository extends EntityRepository
         }
 
         $questionsPerCategoryInDistribution = array();
-        /** @var \Entity\CQuizDistributionQuestions $question */
-        foreach ($currentDistributionQuestions as $question) {
-            $questionsPerCategoryInDistribution[$question->getCategoryId()][] = $question->getQuestionId();
-        }
 
-        /** @var \Entity\CQuizDistributionQuestions $question */
-        foreach ($currentDistributionQuestions as $question) {
-            $result = array();
+        if (!empty($currentDistributionQuestions)) {
+            /** @var \Entity\CQuizDistributionQuestions $question */
+            foreach ($currentDistributionQuestions as $question) {
+                $questionsPerCategoryInDistribution[$question->getCategoryId()][] = $question->getQuestionId();
+            }
 
-            if (!empty($distributionIdList)) {
+            /** @var \Entity\CQuizDistributionQuestions $question */
+            foreach ($currentDistributionQuestions as $question) {
+                $result = array();
 
-                // Checking if there are questions from this category that are not added yet
-                $qb = $em->getRepository('Entity\CQuizDistributionQuestions')->createQueryBuilder('e');
-                $qb->where('e.categoryId = :categoryId')
-                    ->andWhere('e.questionId = :questionId')
-                    ->andWhere($qb->expr()->in('e.quizDistributionId', $distributionIdList))
-                    ->setParameters(
-                        array(
-                            'categoryId' => $question->getCategoryId(),
-                            'questionId' => $question->getQuestionId()
+                if (!empty($distributionIdList)) {
+
+                    // Checking if there are questions from this category that are not added yet
+                    $qb = $em->getRepository('Entity\CQuizDistributionQuestions')->createQueryBuilder('e');
+                    $qb->where('e.categoryId = :categoryId')
+                        ->andWhere('e.questionId = :questionId')
+                        ->andWhere($qb->expr()->in('e.quizDistributionId', $distributionIdList))
+                        ->setParameters(
+                            array(
+                                'categoryId' => $question->getCategoryId(),
+                                'questionId' => $question->getQuestionId()
+                            )
                         )
-                    )
-                ;
-                $result = $qb->getQuery()->getArrayResult();
-            }
-
-            // Doubles found
-            if (count($result) > 0) {
-                $questionListFromDistribution = $questionsPerCategoryInDistribution[$question->getCategoryId()];
-                $questionListFromExercise = $questionsPerCategoryInExercise[$question->getCategoryId()];
-
-                $diff = array_diff($questionListFromExercise, $questionListFromDistribution);
-
-                // Found some questions. Great! now we can select one question.
-                if (!empty($diff)) {
-                    shuffle($diff);
-                    $selectedQuestionId = current($diff);
-                } else {
-                    // Nothing found take one random question from the question list in the exercise
-                    shuffle($questionListFromExercise);
-                    $selectedQuestionId = current($questionListFromExercise);
+                    ;
+                    $result = $qb->getQuery()->getArrayResult();
                 }
 
-                // $selectedQuestionId contains the new question id
-                if (!empty($selectedQuestionId)) {
-                    $questionsPerCategoryInDistribution[$question->getCategoryId()][] = $selectedQuestionId;
-                    // Update the relationship
-                    $question->setQuestionId($selectedQuestionId);
-                    $em->persist($question);
-                    $em->flush();
+                // Doubles found
+                if (count($result) > 0) {
+                    $questionListFromDistribution = $questionsPerCategoryInDistribution[$question->getCategoryId()];
+                    $questionListFromExercise = $questionsPerCategoryInExercise[$question->getCategoryId()];
+
+                    $diff = array_diff($questionListFromExercise, $questionListFromDistribution);
+
+                    // Found some questions. Great! now we can select one question.
+                    if (!empty($diff)) {
+                        shuffle($diff);
+                        $selectedQuestionId = current($diff);
+                    } else {
+                        // Nothing found take one random question from the question list in the exercise
+                        shuffle($questionListFromExercise);
+                        $selectedQuestionId = current($questionListFromExercise);
+                    }
+
+                    // $selectedQuestionId contains the new question id
+                    if (!empty($selectedQuestionId)) {
+                        $questionsPerCategoryInDistribution[$question->getCategoryId()][] = $selectedQuestionId;
+                        // Update the relationship
+                        $question->setQuestionId($selectedQuestionId);
+                        $em->persist($question);
+                        $em->flush();
+                    }
                 }
             }
+
+            $criteria = array('quizDistributionId' => $distribution->getId());
+            $currentDistributionQuestions = $em
+                ->getRepository('Entity\CQuizDistributionQuestions')
+                ->findBy($criteria);
+
+            $questionList = array();
+            foreach ($currentDistributionQuestions as $question) {
+                $questionList[] = $question->getQuestionId();
+            }
+
+            // Rebuild question list
+            $distribution->setDataTracking(implode(',', $questionList));
+            $em->persist($distribution);
+            $em->flush();
         }
-
-        $criteria = array('quizDistributionId' => $distribution->getId());
-        $currentDistributionQuestions = $em
-            ->getRepository('Entity\CQuizDistributionQuestions')
-            ->findBy($criteria);
-
-        $questionList = array();
-        foreach ($currentDistributionQuestions as $question) {
-            $questionList[] = $question->getQuestionId();
-        }
-
-        // Rebuild question list
-        $distribution->setDataTracking(implode(',', $questionList));
-        $em->persist($distribution);
-        $em->flush();
     }
 }

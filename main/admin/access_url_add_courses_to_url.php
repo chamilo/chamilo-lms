ac7f651af0f8718c7c37e5d7cfbb115d9d03036d
Julio Montoya <gugli100@gmail.com>
2013-10-24 12:20:27 +0200

    Minor - Formatting code.

--- main/admin/access_url_add_courses_to_url.php
+++ main/admin/access_url_add_courses_to_url.php
@@ -12,18 +12,15 @@
 $language_file = 'admin';
 $cidReset = true;
 require_once '../inc/global.inc.php';
+require_once api_get_path(LIBRARY_PATH).'urlmanager.lib.php';
+
 $this_section=SECTION_PLATFORM_ADMIN;
 
-require_once (api_get_path(LIBRARY_PATH).'urlmanager.lib.php');
 api_protect_global_admin_script();
 if (!api_get_multiple_access_url()) {
-	header('Location: index.php');
-	exit;
+    header('Location: index.php');
+    exit;
 }
-
-/*
-	Global constants and variables
-*/
 
 $form_sent = 0;
 $first_letter_course = '';
@@ -41,50 +38,47 @@ $tool_name = get_lang('AddCoursesToURL');
 $interbreadcrumb[] = array ('url' => 'index.php', 'name' => get_lang('PlatformAdmin'));
 $interbreadcrumb[] = array ('url' => 'access_urls.php', 'name' => get_lang('MultipleAccessURLs'));
 
-/*		MAIN CODE   */
-
 Display :: display_header($tool_name);
 
 echo '<div class="actions">';
-echo Display::url(Display::return_icon('edit.gif',get_lang('EditCoursesToURL'),''), api_get_path(WEB_CODE_PATH).'admin/access_url_edit_courses_to_url.php"'); 
+echo Display::url(
+    Display::return_icon('edit.png', get_lang('EditCoursesToURL'), ''),
+    api_get_path(WEB_CODE_PATH).'admin/access_url_edit_courses_to_url.php'
+);
 echo '</div>';
 
 api_display_tool_title($tool_name);
 
-if ($_POST['form_sent']) {
-	$form_sent = $_POST['form_sent'];
-	$courses = is_array($_POST['course_list']) ? $_POST['course_list'] : array() ;
-	$url_list = is_array($_POST['url_list']) ? $_POST['url_list'] : array() ;
-	$first_letter_course = $_POST['first_letter_course'];
+if (isset($_POST['form_sent']) && $_POST['form_sent']) {
+    $form_sent = $_POST['form_sent'];
+    $courses = is_array($_POST['course_list']) ? $_POST['course_list'] : array() ;
+    $url_list = is_array($_POST['url_list']) ? $_POST['url_list'] : array() ;
+    $first_letter_course = $_POST['first_letter_course'];
 
-	foreach($users as $key => $value) {
-		$users[$key] = intval($value);
-	}
+    foreach ($users as $key => $value) {
+        $users[$key] = intval($value);
+    }
 
-	if ($form_sent == 1) {
-		if ( count($courses) == 0 || count($url_list) == 0) {
-			Display :: display_error_message(get_lang('AtLeastOneCourseAndOneURL'));
-		} else {
-			UrlManager::add_courses_to_urls($courses,$url_list);
-			Display :: display_confirmation_message(get_lang('CourseBelongURL'));
-		}
-	}
+    if ($form_sent == 1) {
+        if (count($courses) == 0 || count($url_list) == 0) {
+            Display :: display_error_message(get_lang('AtLeastOneCourseAndOneURL'));
+        } else {
+            UrlManager::add_courses_to_urls($courses, $url_list);
+            Display :: display_confirmation_message(get_lang('CourseBelongURL'));
+        }
+    }
 }
 
-
-
-/*	Display GUI */
-
-if(empty($first_letter_user)) {
-	$sql = "SELECT count(*) as num_courses FROM $tbl_course";
-	$result = Database::query($sql);
-	$num_row = Database::fetch_array($result);
-	if($num_row['num_courses']>1000)
-	{//if there are too much num_courses to gracefully handle with the HTML select list,
-	 // assign a default filter on users names
-		$first_letter_user = 'A';
-	}
-	unset($result);
+if (empty($first_letter_user)) {
+    $sql = "SELECT count(*) as num_courses FROM $tbl_course";
+    $result = Database::query($sql);
+    $num_row = Database::fetch_array($result);
+    if ($num_row['num_courses']>1000) {
+        //if there are too much num_courses to gracefully handle with the HTML select list,
+        // assign a default filter on users names
+        $first_letter_user = 'A';
+    }
+    unset($result);
 }
 
 $first_letter_course = Database::escape_string($first_letter_course);
@@ -126,35 +120,26 @@ unset($result);
    <tr>
     <td width="40%" align="center">
      <select name="course_list[]" multiple="multiple" size="20" style="width:400px;">
-		<?php
-		foreach ($db_courses as $course) {
-			?>
-			<option value="<?php echo $course['code']; ?>" <?php if(in_array($course['code'],$courses)) echo 'selected="selected"'; ?>><?php echo $course['title'].' ('.$course['code'].')'; ?></option>
-			<?php
-		}
-		?>
+		<?php foreach ($db_courses as $course) { ?>
+			<option value="<?php echo $course['code']; ?>" <?php if(in_array($course['code'],$courses)) echo 'selected="selected"'; ?>><?php echo $course['title'].' ('.$course['code'].')'; ?>
+            </option>
+        <?php } ?>
     </select>
    </td>
-   <td width="20%" valign="middle" align="center">    
+   <td width="20%" valign="middle" align="center">
     <button type="submit" class="add"> <?php echo get_lang('AddCoursesToThatURL'); ?> </button>
    </td>
    <td width="40%" align="center">
     <select name="url_list[]" multiple="multiple" size="20" style="width:300px;">
-		<?php
-		foreach ($db_urls as $url_obj) {
-			?>
-			<option value="<?php echo $url_obj['id']; ?>" <?php if(in_array($url_obj['id'],$url_list)) echo 'selected="selected"'; ?>><?php echo $url_obj['url']; ?></option>
-			<?php
-		}
-		?>
+		<?php foreach ($db_urls as $url_obj) { ?>
+        <option value="<?php echo $url_obj['id']; ?>" <?php if(in_array($url_obj['id'],$url_list)) echo 'selected="selected"'; ?>><?php echo $url_obj['url']; ?>
+        </option>
+		<?php } ?>
     </select>
    </td>
   </tr>
  </table>
 </form>
 <?php
-/*
-		FOOTER
-*/
+
 Display :: display_footer();
-?>

f9a9089a1833324081f5e778db326fc8a2743766
Marco Villegas <git@marvil07.net>
2013-10-25 10:52:02 -0500

    tx:send command: Let choose the transaction subset.

--- src/ChamiloLMS/Command/Transaction/SendCommand.php
+++ src/ChamiloLMS/Command/Transaction/SendCommand.php
@@ -4,6 +4,7 @@ namespace ChamiloLMS\Command\Transaction;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use ChamiloLMS\Transaction\Envelope;
 use ChamiloLMS\Transaction\TransactionLogController;
@@ -13,33 +14,60 @@ use ChamiloLMS\Transaction\TransactionLog;
  * Selects a subset of transactions, wraps and send them.
  *
  * It will use local branch settings.
- *
- * @fixme Let choose the transactions subset.
  */
 class SendCommand extends Command
 {
+    protected $local_branch = null;
+
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+        // @todo Is there a clean way/no-global to access app from here?
+        global $app;
+        $this->local_branch = $app['orm.em']->getRepository('Entity\BranchSync')->getLocalBranch();
+    }
+
+    /**
+     * @todo Let accept multiple values on options. Notice Database::select()
+     * does not allow multiple values.
+     */
     protected function configure()
     {
         $this
             ->setName('tx:send')
-            ->setDescription('Selects a subset of transactions, wraps and send them using local branch configuration.');
-            //->addOption('temp-folder', null, InputOption::VALUE_OPTIONAL, 'The temp folder.', '/tmp');
+            ->setDescription('Selects a subset of transactions, wraps and send them using local branch configuration.')
+            ->addOption('course', null, InputOption::VALUE_OPTIONAL, 'A course ID to select transactions.', '')
+            ->addOption('session', null, InputOption::VALUE_OPTIONAL, 'A session ID to select transactions.', '');
+    }
+
+    /**
+     * Convert input options into TransactionLogController::load() conditions.
+     *
+     * @todo Validate arguments?
+     */
+    protected function getSelectConditions(InputInterface $input)
+    {
+        $conditions = array(
+            'branch_id' => $this->local_branch->getId(),
+            'status_id' => TransactionLog::STATUS_LOCAL,
+        );
+        // Course.
+        $course_string = $input->getOption('course');
+        if (strlen($course_string) > 0) {
+            $conditions['c_id'] = $course_string;
+        }
+        // Session.
+        $session_string = $input->getOption('session');
+        if (strlen($session_string) > 0) {
+            $conditions['session_id'] = $session_string;
+        }
+        return $conditions;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // @todo Is there a clean way/no-global to access app from here?
-        global $app;
-        $local_branch = $app['orm.em']->getRepository('Entity\BranchSync')->getLocalBranch();
         $tc = new TransactionLogController();
-        // @fixme Do not hardcode conditions.
-        $conditions = array(
-            'c_id' => 1,
-            'session_id' => 0,
-            'branch_id' => $local_branch->getId(),
-            'status_id' => TransactionLog::STATUS_LOCAL,
-        );
-        $transactions = $tc->load($conditions);
+        $transactions = $tc->load($this->getSelectConditions($input));
         if (empty($transactions)) {
             $output->writeln('No transactions to be exported');
             return;
@@ -55,8 +83,8 @@ class SendCommand extends Command
         }
 
         // Create an evelope and wrap it.
-        $wrapper = TransactionLogController::createPlugin('wrapper', $local_branch->getPluginEnvelope(), $local_branch->getPluginData('wrapper'));
-        $envelope_data = array('transactions' => $transactions, 'origin_branch_id' => $local_branch->getId());
+        $wrapper = TransactionLogController::createPlugin('wrapper', $this->local_branch->getPluginEnvelope(), $this->local_branch->getPluginData('wrapper'));
+        $envelope_data = array('transactions' => $transactions, 'origin_branch_id' => $this->local_branch->getId());
         $envelope = new Envelope($wrapper, $envelope_data);
         try {
             $envelope->wrap();

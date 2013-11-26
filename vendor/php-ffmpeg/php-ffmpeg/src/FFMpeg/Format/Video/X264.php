ff8c8ee7a41e2c0d5b633c8a9aa08085ae61ec1c
Julio Montoya <gugli100@gmail.com>
2013-11-26 17:20:56 +0100

    Adding lib no git submodule BT#6449

---
+++ vendor/php-ffmpeg/php-ffmpeg/src/FFMpeg/Format/Video/X264.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of PHP-FFmpeg.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FFMpeg\Format\Video;
+
+/**
+ * The X264 video format
+ */
+class X264 extends DefaultVideo
+{
+    public function __construct($audioCodec = 'libfaac', $videoCodec = 'libx264')
+    {
+        $this
+            ->setAudioCodec($audioCodec)
+            ->setVideoCodec($videoCodec);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supportBFrames()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAvailableAudioCodecs()
+    {
+        return array('libvo_aacenc', 'libfaac', 'libmp3lame');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAvailableVideoCodecs()
+    {
+        return array('libx264');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPasses()
+    {
+        return 2;
+    }
+
+    public function getModulus()
+    {
+        return 2;
+    }
+}

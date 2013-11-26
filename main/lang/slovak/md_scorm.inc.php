1e4b69166043d666ca17948676e3432c16b3959e
Julio Montoya <gugli100@gmail.com>
2013-11-26 17:43:57 +0100

    Minor - updating slovak variables.

--- main/lang/slovak/md_scorm.inc.php
+++ main/lang/slovak/md_scorm.inc.php
@@ -2,8 +2,13 @@
 /*
 for more information: see languages.txt in the lang folder.
 */
+$nameTools = "zastaralá jazyková premenná";
 $langMdCallingTool = "Dokumenty";
+$langTool = "Metadáta dokumentu";
 $langNotInDB = "žiadna taká kategória odkazov";
+$langManifestSyntax = "(syntaxová chyba v manifest súbore...)";
+$langEmptyManifest = "(prázdny manifest súbor...)";
+$langNoManifest = "(žiadny manifest súbor...)";
 $langNotFolder = "nie je možné, ie je to priečinok...";
 $langUploadHtt = "Načítať HTT súbor";
 $langHttFileNotFound = "Nový HTT súbor sa nedá otvoriť (napr. je prázdny, veľmi veľký)";
@@ -12,11 +17,22 @@ $langHttNotOk = "Načítanie HTT súboru bolo neúspešné";
 $langRemoveHtt = "Odstrániť HTT súbor";
 $langHttRmvOk = "HTT súbor bol odstránený";
 $langHttRmvNotOk = "Odstránenie HTT súboru nebolo úspešné";
+$langImport = "Importovať";
 $langRemove = "Odstrániť MDEs";
 $langAllRemovedFor = "Všetky záznamy z kategórie odstránené";
+$langIndex = "Index slová";
 $langTotalMDEs = "Celkový počet MD záznamov:";
 $langMainMD = "Otvoriť hlavné MDE";
 $langLines = "riadky";
+$langPlay = "Pustiť index.php";
+$langNonePossible = "Nie sú možné žiadne MD operácie";
 $langOrElse = "Vybrať kategóriu odkazov";
+$langWorkWith = "Pracovať s SCORM adresárom";
+$langSDI = "... SCORM adresár s SD-id (a rozdeliť manifest - alebo nechať prázdne)";
 $langRoot = "koreň";
+$langSplitData = "Rozdeliť manifesty, a #MDe, ak existuje:";
+$langMffNotOk = "Náhrada manifest súboru zlyhala";
+$langMffOk = "Manifest súbor bol nahradený";
+$langMffFileNotFound = "Nový manifest súbor sa nedá otvoriť (napr. je prázdny, príliš veľký)";
+$langUploadMff = "Nahradiť manifest súbor";
 ?>

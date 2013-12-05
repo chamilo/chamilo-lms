24c8a7474f7e36c09013f4b2e8943141cc096c44
Kevin LEVRON <kevin.levron@univ-pau.fr>
2013-12-05 14:49:31 +0100

    Minor : remove extra space

--- main/inc/lib/fckeditor/editor/plugins/youtube/youtube.js
+++ main/inc/lib/fckeditor/editor/plugins/youtube/youtube.js
@@ -17,7 +17,7 @@ var REG_AUTH = new RegExp( "document\.cookie|Microsoft\.XMLHTTP", "i" ) ;
 // TEXTAREA
 var REG_NEWLINE = new RegExp( "\x0d|\x0a", "i" ) ;
 
-var YoutubeSite = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//www.youtube.com/v/' ;
+var YoutubeSite = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//www.youtube.com/v/';
 var HighQualityString = '%26hl=en%26fs=1%26rel=0%26ap=%2526fmt=18' ;
 var LowQualityString = '%26hl=en%26fs=1%26rel=0' ;
 

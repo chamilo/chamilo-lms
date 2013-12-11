2d518c626f71e325c5a5834c8e0e57b4383c58e6
Yannick Warnier <ywarnier@beeznest.org>
2013-12-11 18:25:49 -0500

    Add hidden config parameters that will be moved to settings_current in future versions (disabled for now) - Add new param

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -184,3 +184,5 @@ $_configuration['system_stable']     = NEW_VERSION_STABLE;
 //$_configuration['captcha_number_mistakes_to_block_account'] = 5;
 // Prevent account from logging in for the specified number of minutes
 //$_configuration['captcha_time_to_block'] = 5;//minutes
+// Allow DRH role to access all content and users from the sessions he follows
+//$_configuration['drh_can_access_all_session_content'] = true;

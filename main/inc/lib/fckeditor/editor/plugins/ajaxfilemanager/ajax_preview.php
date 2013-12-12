cbae5fba116ff97c685dafd05eebd2bedcc479ea
Julio Montoya <gugli100@gmail.com>
2013-12-12 10:29:19 +0100

    Updating license.

--- main/inc/lib/fckeditor/editor/plugins/ajaxfilemanager/ajax_preview.php
+++ main/inc/lib/fckeditor/editor/plugins/ajaxfilemanager/ajax_preview.php
@@ -1,6 +1,11 @@
 <?php
 /* For licensing terms, see /license.txt */
-
+/**
+ * @author Logan Cai (cailongqun [at] yahoo [dot] com [dot] cn)
+ * @link www.phpletter.com
+ * @since 22/April/2007
+ *
+ */
 require_once '../../../../../../inc/global.inc.php';
 require_once api_get_path(LIBRARY_PATH).'fckeditor/editor/plugins/ajaxfilemanager/inc/config.php';
 

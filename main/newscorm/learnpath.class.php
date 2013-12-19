0a5cb7d0781e1a51c6b42eca4463eae07bd2be43
Daniel Barreto <9leinad0@gmail.com>
2013-12-19 10:08:10 -0500

    Fixing bug when adding LP docs whit dots (.) -refs #6696

--- main/newscorm/learnpath.class.php
+++ main/newscorm/learnpath.class.php
@@ -6811,16 +6811,7 @@ class learnpath
 
             $result = Database::query($sql_doc);
             $row 	= Database::fetch_array($result);
-
-            $explode = explode('.', $row['title']);
-
-            if (count($explode) > 1) {
-                for ($i = 0; $i < count($explode) - 1; $i++)
-                    $item_title .= $explode[$i];
-            } else {
-                $item_title = $row['title'];
-            }
-
+            $item_title = $row['title'];
             $item_title = str_replace('_', ' ', $item_title);
             if (empty ($item_title)) {
                 $path_parts = pathinfo($row['path']);

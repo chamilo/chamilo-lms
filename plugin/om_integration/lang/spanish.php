5aa83325b07e294a11f3f0a56f51c34ce1c7956d
Yannick Warnier <ywarnier@beeznest.org>
2013-12-26 08:06:14 -0500

    Updated OpenMeetings plugin translations - refs BT#7046

--- plugin/om_integration/lang/spanish.php
+++ plugin/om_integration/lang/spanish.php
@@ -1,6 +1,6 @@
 <?php /* License: see /license.txt */
 //Needed in order to show the plugin title
-$strings['plugin_title']        = "Videoconferencias 'OpenMeetings'";
+$strings['plugin_title']        = "OpenMeetings";
 $strings['plugin_comment']      = "Añade una sala de videoconferencia en los cursos de Chamilo con OpenMeetings (om)";
 
 $strings['Videoconference']     = "VideoconferenciaOM";
@@ -38,13 +38,12 @@ $strings['pass'] = 'Clave OpenMeetings';
 $strings['pass_help'] = 'Esta es la llave de seguridad de su servidor OpenMeetings (llamada "salt" en inglés), que permitirá a su servidor de autentifica la instalación de Chamilo (como autorizada). Refiérese a la documentación de OpenMeetings para ubicarla, o use el comando "bbb-conf --salt" si tiene acceso al servidor en línea de comando.';
 
 $strings['tool_enable'] = 'Herramienta de videoconferencia OpenMeetings activada';
-$strings['tool_enable_help'] = "Escoja si desea activar la herramienta de videoconferencia OpenMeetings.
-    Una vez activada, se mostrará como una herramienta adicional en todas las páginas principales de cursos, y los profesores podrán iniciar una conferencia en cualquier momento. Los estudiantes no podrían lanzar una conferencia, solo juntarse a una existente. Si no tiene un servidor de videoconferencia OpenMeetings, por favor <a target=\"_blank\" href=\"http://bigbluebutton.org/\">configure uno</a> antes de seguir, o pida una cotización a uno de los proveedores oficiales de Chamilo. OpenMeetings es una herramienta de software libre (y gratuita), pero su instalación requiere de competencias que quizás no sean inmediatamente disponibles para todos. Puede instalarla usted mismo o buscar ayuda profesional. Esta ayuda podría no obstante generar algunos costos (por lo menos el tiempo de la persona quien lo ayude). En el puro espíritu del software libre, le ofrecemos las herramientas para hacer su trabajo más simple, en la medida de nuestras posibilidades, y le recomendamos profesionales (los proveedores oficiales de Chamilo) para ayudarlo en caso esto fuera demasiado complicado.<br />";
+$strings['tool_enable_help'] = 'Escoja si desea activar la herramienta de videoconferencia OpenMeetings. Una vez activada, se mostrará como una herramienta adicional en todas las páginas principales de cursos, y los profesores podrán iniciar una conferencia en cualquier momento. Los estudiantes no podrían lanzar una conferencia, solo juntarse a una existente. Si no tiene un servidor de videoconferencia OpenMeetings, por favor <a target=\"_blank\" href=\"http://openmeetings.apache.org/\">configure uno</a> antes de seguir, o pida una cotización a uno de los proveedores oficiales de Chamilo. OpenMeetings es una herramienta de software libre (y gratuita), pero su instalación requiere de competencias que quizás no sean inmediatamente disponibles para todos. Puede instalarla usted mismo o buscar ayuda profesional. Esta ayuda podría no obstante generar algunos costos (por lo menos el tiempo de la persona quien lo ayude). En el puro espíritu del software libre, le ofrecemos las herramientas para hacer su trabajo más simple, en la medida de nuestras posibilidades, y le recomendamos profesionales (los proveedores oficiales de Chamilo) para ayudarlo en caso esto fuera demasiado complicado.<br />';
 
-$strings['big_blue_button_welcome_message'] = 'Mensaje de bienvenida de ';
-$strings['big_blue_button_record_and_store'] = 'Grabar las sesiones de videoconferencia';
+$strings['openmeetings_welcome_message'] = 'Mensaje de bienvenida de ';
+$strings['openmeetings_record_and_store'] = 'Grabar las sesiones de videoconferencia';
 
-$strings['plugin_tool_bbb'] = 'Video';
+$strings['plugin_tool_openmeetings'] = 'Video';
 
 $strings['ThereAreNotRecordingsForTheMeetings'] = 'No hay grabaciones de sesiones de videoconferencia';
 $strings['NoRecording'] = 'No hay grabación';

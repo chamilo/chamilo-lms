a23c2f6f6eec4bcf59ad6f2402f91717a7b83940
Julio Montoya <gugli100@gmail.com>
2014-02-21 15:50:11 +0100

    Fixing PHP warning + reformat code.

--- main/exercice/showinframes.php
+++ main/exercice/showinframes.php
@@ -5,16 +5,13 @@
  *	@package chamilo.exercise
  * 	@author Istvan Mandak
  */
-/**
- * Included libraries
- */
-require '../inc/global.inc.php';
+require_once '../inc/global.inc.php';
 require_once api_get_path(LIBRARY_PATH).'fileManage.lib.php';
 require_once api_get_path(SYS_CODE_PATH).'exercice/hotpotatoes.lib.php';
 
 $time = time();
 $doc_url = str_replace(array('../', '\\', '\\0', '..'), array('', '', '', ''), urldecode($_GET['file']));
-$cid                = api_get_course_id();
+$cid = api_get_course_id();
 $document_path      = api_get_path(SYS_COURSE_PATH).$_course['path'].'/document';
 $document_web_path  = api_get_path(WEB_COURSE_PATH).$_course['path'].'/document';
 $origin             = isset($_REQUEST['origin']) ? $_REQUEST['origin'] : null;
@@ -29,62 +26,53 @@ $content = ReadFileCont($full_file_path.$user_id.'.t.html');
 
 if ($content == '') {
 	$content = ReadFileCont($full_file_path);
+    // Do not move this like:
 	$mit = "function Finish(){";
 	$js_content = "
-        //Code added - start
-        var SaveScoreVariable = 0;
-        function mySaveScore() {
-            if (SaveScoreVariable==0) {
-                SaveScoreVariable = 1;
-                    if (C.ie) {
-                        document.location.href = '".api_get_path(WEB_PATH)."main/exercice/savescores.php?origin=$origin&learnpath_id=$learnpath_id&learnpath_item_id=$learnpath_item_id&time=".Security::remove_XSS($time)."&test=".$doc_url."&uid=".$user_id."&cid=".$cid."&score='+Score;
-						//window.alert(Score);
-                    } else {
-                        window.location.href = '".api_get_path(WEB_PATH)."main/exercice/savescores.php?origin=$origin&learnpath_id=$learnpath_id&learnpath_item_id=$learnpath_item_id&time=".Security::remove_XSS($time)."&test=".$doc_url."&uid=".$user_id."&cid=".$cid."&score='+Score;
-                    }
-						}
-				}
-				function Finish(){
-                    mySaveScore();
-        //Code added - end
+    // Code added - start
+    var SaveScoreVariable = 0;
+    function mySaveScore() {
+        if (SaveScoreVariable==0) {
+            SaveScoreVariable = 1;
+            if (C.ie) {
+                document.location.href = '".api_get_path(WEB_PATH)."main/exercice/savescores.php?origin=$origin&learnpath_id=$learnpath_id&learnpath_item_id=$learnpath_item_id&time=".Security::remove_XSS($time)."&test=".$doc_url."&uid=".$user_id."&cid=".$cid."&score='+Score;
+                //window.alert(Score);
+            } else {
+                window.location.href = '".api_get_path(WEB_PATH)."main/exercice/savescores.php?origin=$origin&learnpath_id=$learnpath_id&learnpath_item_id=$learnpath_item_id&time=".Security::remove_XSS($time)."&test=".$doc_url."&uid=".$user_id."&cid=".$cid."&score='+Score;
+            }
+        }
+    }
+    function Finish() {
+        mySaveScore();
+    // Code added - end
     ";
 
-	$newcontent = str_replace($mit, $js_content, $content);
-	$prehref = "<!-- BeginTopNavButtons -->";
-	$posthref = "<!-- BeginTopNavButtons -->";
-	$newcontent = str_replace($prehref, $posthref, $newcontent);
+    $newcontent = str_replace($mit, $js_content, $content);
+    $prehref = "<!-- BeginTopNavButtons -->";
+    $posthref = "<!-- BeginTopNavButtons -->";
+    $newcontent = str_replace($prehref, $posthref, $newcontent);
 
-	if (CheckSubFolder($full_file_path.$user_id.'.t.html') == 0) {
-		$newcontent = ReplaceImgTag($newcontent);
-	}
+    if (CheckSubFolder($full_file_path.$user_id.'.t.html') == 0) {
+        $newcontent = ReplaceImgTag($newcontent);
+    }
 } else {
-	$newcontent = $content;
+    $newcontent = $content;
 }
-
 
 WriteFileCont($full_file_path.$user_id.'.t.html', $newcontent);
 $doc_url = GetFolderPath($doc_url).urlencode(basename($doc_url));
 
 $documentPath= api_get_path(SYS_COURSE_PATH).$_course['path']."/document";
 $my_file = Security::remove_XSS($_GET['file']);
-$my_file = str_replace(array('../','\\..','\\0','..\\'),array('','','',''),urldecode($my_file));
+$my_file = str_replace(array('../', '\\..', '\\0', '..\\'), array('', '', '', ''),urldecode($my_file));
 
-$title = GetQuizName($my_file,$documentPath);
+$title = GetQuizName($my_file, $documentPath);
 if ($title =='') {
-	$title = basename($my_file);
+    $title = basename($my_file);
 }
 $nameTools = $title;
-$noPHP_SELF=true;
-if (isset($_SESSION['gradebook'])){
-	$gradebook=	$_SESSION['gradebook'];
-}
+$noPHP_SELF = true;
 
-if (!empty($gradebook) && $gradebook=='view') {
-	$interbreadcrumb[]= array (
-        'url'  => '../gradebook/'.$_SESSION['gradebook_dest'],
-        'name' => get_lang('ToolGradebook')
-    );
-}
 $htmlHeadXtra[] = '
 <script>
     $(document).ready( function(){

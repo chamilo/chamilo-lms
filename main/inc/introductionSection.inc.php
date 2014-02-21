d71d47c0a61e0a82eeb18883690aeef9f7aeabdc
Alex Aragon <aragonc@gmail.com>
2014-02-21 16:00:00 -0500

    responsive css home course tablet 600

--- main/inc/introductionSection.inc.php
+++ main/inc/introductionSection.inc.php
@@ -235,11 +235,15 @@ $introduction_section .= '<div class="row course-tools-intro"><div class="span12
 $introduction_section .=  $thematic_description_html;
 $introduction_section .=  '</div>';
 
-$introduction_section .=  '<div class="home-course-intro span12"><div class="page-course-intro">';
+$introduction_section .=  '<div class="home-course-intro span12"><div class="page-course">';
 if ($intro_dispDefault) {
+	
 	$intro_content = $intro_content;
+
 	if (!empty($intro_content))	{
+		$introduction_section.='<div class="page-course-intro">';
 		$introduction_section .=  $intro_content;
+		$introduction_section.='</div>';
 	}
 }
 $introduction_section .=  '</div></div>';

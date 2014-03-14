4a74df7cb6068fa39146ed1b65917a59ef4edd11
Julio Montoya <gugli100@gmail.com>
2014-03-14 14:58:16 +0100

    Moving code inside agenda.lib.php see BT#7772

--- main/calendar/agenda_js.php
+++ main/calendar/agenda_js.php
@@ -133,9 +133,11 @@ $export_icon_high = '../img/export_high_fade.png';
 $tpl->assign('export_ical_confidential_icon', Display::return_icon($export_icon_high, get_lang('ExportiCalConfidential')));
 
 $actions = null;
+$filter = null;
 
 if (api_is_allowed_to_edit(false, true) OR
-    (api_get_course_setting('allow_user_edit_agenda') && !api_is_anonymous()) && api_is_allowed_to_session_edit(false, true) OR
+    (api_get_course_setting('allow_user_edit_agenda') && !api_is_anonymous()) &&
+    api_is_allowed_to_session_edit(false, true) OR
     $is_group_tutor
 ) {
     if ($type == 'course') {

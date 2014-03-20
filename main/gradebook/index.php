aec96e383b1ea5a5c57cdc7f5c894fabf3024a6f
Julio Montoya <gugli100@gmail.com>
2014-03-20 19:22:33 +0100

    Fixes #6974 (change provided by aigualada)

--- main/gradebook/index.php
+++ main/gradebook/index.php
@@ -820,11 +820,11 @@ if (isset($first_time) && $first_time==1 && api_is_allowed_to_edit(null,true)) {
         }
 
 		$i = 0;
-
-		foreach ($cats as $cat) {
+        /** @var Category $cat **/
+        foreach ($cats as $cat) {
 			$allcat  = $cat->get_subcategories($stud_id, $course_code, $session_id);
 			$alleval = $cat->get_evaluations($stud_id);
-			$alllink = $cat->get_links($stud_id,true);
+			$alllink = $cat->get_links($stud_id);
 
 			if ($cat->get_parent_id() != 0) {
 				$i++;

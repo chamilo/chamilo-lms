1e8ce24d5c1a2c109140ffbcd4904dd5dc4650df
unknown <ycastillo06@gmail.com>
2014-03-24 10:37:53 -0400

    Fixing bug in including captcha -refs #6373

--- main/inc/lib/pear/Text/CAPTCHA/Exception.php
+++ main/inc/lib/pear/Text/CAPTCHA/Exception.php
@@ -12,7 +12,7 @@
  * @license  http://www.opensource.org/licenses/bsd-license.php BSD License
  * @link     http://pear.php.net/package/Text_CAPTCHA
  */
- $lib_path = api_get_path(LIBRARY_PATH);
+$lib_path = api_get_path(LIBRARY_PATH);
 require_once $lib_path.'/pear/Exception.php';
 /**
  * Exception for Text_CAPTCHA

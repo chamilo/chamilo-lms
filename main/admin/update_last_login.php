b31b9dc6f59e815810ecff528f9aa9c9ca728b0d
Julio Montoya <gugli100@gmail.com>
2014-03-31 16:24:46 +0200

    If empty then alter the table. see BT#7297

--- main/admin/update_last_login.php
+++ main/admin/update_last_login.php
@@ -5,14 +5,8 @@ api_protect_admin_script();
 exit;
 if (isset($_configuration['save_user_last_login'])) {
     $tableUser = Database::get_main_table(TABLE_MAIN_USER);
-
     $userInfo = api_get_user_info(api_get_user_id());
     if (!empty($userInfo['last_login'])) {
-        echo "<br />Script was already executed";
-        exit;
-    }
-
-    if (!isset($userInfo['last_login'])) {
         $sql = "SELECT login_user_id, MAX(login_date) login_date from track_e_login group by login_user_id";
         echo "Executing: <br />$sql<br /> Updating <br />";
         $result = Database::query($sql);

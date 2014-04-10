chamilo/chamilo-lms

commit 086a8e1c2cad86b6dc7b809ce9e697cb9a59cc8f

merged changes by aragonc

main/css/chamilo/scorm.css
@@ -187,7 +187,7 @@
 }
 
 .inner_lp_toc .scorm_item a {
-    font-weight: bold;
+    font-weight: normal;
     font-size: 14px;
     margin-right: 1px;
     padding-bottom: 2px;
@@ -201,17 +201,10 @@
     margin-right: 10px;
 }
 .inner_lp_toc .scorm_item_highlight {   
-    border:1px solid #00516e;
-    background:#4b88b6;
-    font-weight:bold;
-    
-    text-shadow:0 -1px 1px #014d68;
-    background-image:-webkit-gradient(linear,left top,left bottom,from(#72b0d4),to(#4b88b6));
-    background-image:-webkit-linear-gradient(top,#72b0d4,#4b88b6);
-    background-image:-moz-linear-gradient(top,#72b0d4,#4b88b6);
-    background-image:-ms-linear-gradient(top,#72b0d4,#4b88b6);
-    background-image:-o-linear-gradient(top,#72b0d4,#4b88b6);
-    background-image:linear-gradient(top,#72b0d4,#4b88b6);    
+    border:1px solid #00aad4;
+    background:#00aad4;
+    font-weight:normal;
+    background-image:none;
     margin-right: 0px;
     padding: 10px 0px 10px 0px;
     text-decoration: none;  
@@ -221,13 +214,13 @@
     color:#fff;
     margin-right: 1px;
     text-decoration: none;
-    font-weight: bold;
+    font-weight: normal;
 }
 
 .inner_lp_toc .scorm_item_section {
     border:1px solid #222;
     background:#333;
-    font-weight:bold;
+    font-weight:normal;
     color:#fff;
     text-shadow:0 -1px 1px #000;
     background-image:-webkit-gradient(linear,left top,left bottom,from(#555),to(#333));
@@ -236,7 +229,6 @@
     background-image:-ms-linear-gradient(top,#555,#333);
     background-image:-o-linear-gradient(top,#555,#333);
     background-image:linear-gradient(top,#555,#333);
-    
     margin-right: 1px;
     padding: 10px 0px 10px 0px;
     text-decoration: none;  
@@ -257,12 +249,7 @@
     font-weight:bold;
     color:#444;
     text-shadow:0 1px 1px #f6f6f6;
-    background-image:-webkit-gradient(linear,left top,left bottom,from(#fdfdfd),to(#eee));
-    background-image:-webkit-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-moz-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-ms-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-o-linear-gradient(top,#fdfdfd,#eee);
-    background-image:linear-gradient(top,#fdfdfd,#eee);
+    background-image:none;
     
     margin-right: 1px;
     padding: 10px 0px 10px 0px;
@@ -272,16 +259,10 @@
 }
 
 .inner_lp_toc .scorm_item_2 {    
-    background-image:-webkit-gradient(linear,left top,left bottom,from(#fdfdfd),to(#eee));
-    background-image:-webkit-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-moz-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-ms-linear-gradient(top,#fdfdfd,#eee);
-    background-image:-o-linear-gradient(top,#fdfdfd,#eee);
-    background-image:linear-gradient(top,#fdfdfd,#eee);    
+    background-image:none;
     border-bottom: 1px solid #CCCCCC;
     color: #444444;
-    font-weight: bold;
-    text-shadow: 0 1px 1px #F6F6F6;
+    font-weight: normal;
     margin-right: 1px;  
     padding: 10px 0px 10px 0px;
     text-decoration: none;
@@ -365,15 +346,97 @@
     padding-left: 17px;
     padding-top: 4px;
 }
-#scorm_title {
-    font-weight: normal;
-    background-color:#000;
-    font-size: 18px;
+
+
+
+#learning_path_breadcrumb_zone .breadcrumb{
     background-image: none;
+    background-color: #2e75a3;
+    color: #fff;
+    text-transform: capitalize;
+}
+#learning_path_breadcrumb_zone .breadcrumb a{
+    color: #FFF;
+}
+#learning_path_breadcrumb_zone .breadcrumb a:hover{
+    text-decoration: underline;
+}
+#scorm_title {
+    font-weight: bold;
+    background:none;
+    font-size: 20px;
+    background-image: none;
+    color: #0088aa;
+    padding-top: 5px;
+    padding-bottom: 15px;
 }
 #learning_path_main #control {
   opacity: 1;
 }
 .buttons a img:hover{
 opacity: 0.7;
+}
+#author_image{
+    background-color: #d7eef4;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+    border: 1px solid  #b4cbff;
+    margin-top: 10px;
+}
+#author_icon img{
+    background-color: #fff;
+    padding: 5px;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+}
+#author_image .progresstext{
+    font-size: 20px;
+    color: #0088aa;
+    font-weight: bold;
+}
+#learning_path_left_zone #header {
+  font-size: 14px;
+  margin-top: 10px;
+}
+#learning_path_left_zone .no-border{
+    background: none;
+    color: #2e75a3;
+    font-weight: normal;
+}
+.progress {
+  height: 18px;
+  margin-bottom: 18px;
+  overflow: hidden;
+  background-color: #f7f7f7;
+  background-image: -moz-linear-gradient(top, #f5f5f5, #f9f9f9);
+  background-image: -ms-linear-gradient(top, #f5f5f5, #f9f9f9);
+  background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#f5f5f5), to(#f9f9f9));
+  background-image: -webkit-linear-gradient(top, #f5f5f5, #f9f9f9);
+  background-image: -o-linear-gradient(top, #f5f5f5, #f9f9f9);
+  background-image: linear-gradient(top, #f5f5f5, #f9f9f9);
+  background-repeat: repeat-x;
+  -webkit-border-radius: 4px;
+     -moz-border-radius: 4px;
+          border-radius: 4px;
+  filter: progid:dximagetransform.microsoft.gradient(startColorstr='#f5f5f5', endColorstr='#f9f9f9', GradientType=0);
+  -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
+     -moz-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
+          box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
+}
+.progress-striped .bar {
+  background-color: #149BDF;
+  background-size: 40px 40px;
+  background-image: none;
+}
+
+/*STYLES PROCESS LESSION*/
+
+.inner_lp_toc .scorm_item_highlight {
+    background-color: #00aad4;
+    background-image: url("images/white_arrow.png") !important;
+    background-position: 250px center !important;
+    background-repeat: no-repeat;
+    border: 1px solid #00aad4;
 }

main/newscorm/learnpath.class.php
@@ -2918,6 +2918,18 @@ class learnpath
                 'browsed'       => '../img/completed.png',
             );
 
+            // Style Status
+
+            $class_name = array (
+                'not attempted' => 'scrom_not_attempted',
+                'incomplete'    => 'scrom_incomplete',
+                'failed'        => 'scrom_failed',
+                'completed'     => 'scrom_completed',
+                'passed'        => 'scrom_passed',
+                'succeeded'     => 'scrom_succeeded',
+                'browsed'       => 'scrom_completed',
+            );
+
             $style = 'scorm_item';
             $scorm_color_background = 'scorm_item';
             $style_item = 'scorm_item';
@@ -2933,12 +2945,12 @@ class learnpath
                     $scorm_color_background = 'scorm_item_2';
                 }
                 if ($item['type'] == 'dokeos_module' || $item['type'] == 'dokeos_chapter') {
-                	$scorm_color_background =' scorm_item_section ';
+                    $scorm_color_background =' scorm_item_section ';
                 }
             }
 
             if ($scorm_color_background != '') {
-                $html .= '<div id="toc_' . $item['id'] . '" class="' . $scorm_color_background . '">';
+                $html .= '<div id="toc_' . $item['id'] . '" class="' . $scorm_color_background . ' '.$class_name[$item['status']].' ">';
             }
 
             // Learning path title
@@ -2979,7 +2991,8 @@ class learnpath
                             status <> 'incomplete'";
             $result = Database::query($sql);
             $count = Database :: num_rows($result);*/
-            if ($item['type'] == 'quiz') {
+            
+            /*if ($item['type'] == 'quiz') {
                 if ($item['status'] == 'completed') {
                     $html .= "&nbsp;<img id='toc_img_" . $item['id'] . "' src='" . $icon_name[$item['status']] . "' alt='" . substr($item['status'], 0, 1) . "' width='14' />";
                 } else {
@@ -2990,6 +3003,7 @@ class learnpath
                     $html .= "&nbsp;<img id='toc_img_" . $item['id'] . "' src='" . $icon_name[$item['status']] . "' alt='" . substr($item['status'], 0, 1) . "' width='14' />";
                 }
             }
+            */
 
             $html .= "</div>";
 
@@ -3002,7 +3016,6 @@ class learnpath
         $html .= "</div>";
         return $html;
     }
-
     /**
      * Gets the learnpath maker name - generally the editor's name
      * @return	string	Learnpath maker name

main/css/chamilo/default.css
@@ -103,7 +103,6 @@ a:active {
 .breadcrumb li{
     font-size: 14px;
     text-shadow: none;
-    text-transform: uppercase;
 }
 .breadcrumb a.btn-mini{
     font-size: 12px;

main/newscorm/lp_view.php
@@ -332,7 +332,7 @@ if ($is_allowed_to_edit) {
                 <tr>
                     <td>';
                         echo '<a href="lp_controller.php?action=return_to_course_homepage&'.api_get_cidreq().'" target="_self" onclick="javascript: window.parent.API.save_asset();">
-                            <img src="../img/lp_arrow.gif" />
+                            <img src="../img/btn_home.png" />
                         </a>
                     </td>
                     <td>';
@@ -378,7 +378,7 @@ if ($is_allowed_to_edit) {
         <!-- end image preview Layout -->
 
         <div id="author_name">
-            <?php echo $_SESSION['oLP']->get_author(); ?>
+            <?php echo $_SESSION['oLP']->get_author(); ?> 
         </div>
 
         <!-- media player layout -->

commit 66ba7e6137cf2ae8c09730126e8dec877c689da0

Fix SCORM class naming issue introduced in 086a8e1c2 #minor

main/css/academica/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/baby_orange/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/blue_lagoon/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo_electric_blue/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo_green/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo_orange/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo_red/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/chamilo_sport_red/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/cool_blue/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/corporate/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/cosmic_campus/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/css/kiddy/scorm.css
@@ -432,7 +432,7 @@ opacity: 0.7;
 }
 
 /*STYLES PROCESS LESSION*/
-.inner_lp_toc .scrom_completed {
+.inner_lp_toc .scorm_completed {
     
     background-image: url("images/blue_arrow.png") !important;
     background-position: 250px center !important;

main/newscorm/js/documentapi.js
@@ -1,4 +1,4 @@
-// JS interface enabling scrom content to use main/document/remote.php easily
+// JS interface enabling scorm content to use main/document/remote.php easily
 // CBlue SPRL, Arnaud Ligot <arnaud@cblue.be>
 
 

main/newscorm/learnpath.class.php
@@ -2921,13 +2921,13 @@ class learnpath
             // Style Status
 
             $class_name = array (
-                'not attempted' => 'scrom_not_attempted',
-                'incomplete'    => 'scrom_incomplete',
-                'failed'        => 'scrom_failed',
-                'completed'     => 'scrom_completed',
-                'passed'        => 'scrom_passed',
-                'succeeded'     => 'scrom_succeeded',
-                'browsed'       => 'scrom_completed',
+                'not attempted' => 'scorm_not_attempted',
+                'incomplete'    => 'scorm_incomplete',
+                'failed'        => 'scorm_failed',
+                'completed'     => 'scorm_completed',
+                'passed'        => 'scorm_passed',
+                'succeeded'     => 'scorm_succeeded',
+                'browsed'       => 'scorm_completed',
             );
 
             $style = 'scorm_item';
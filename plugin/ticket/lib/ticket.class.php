111c09f08018ce1503195fd375f55d3b172db5bd
Francis Gonzales <francis.gonzales@beeznest.com>
2014-04-11 19:00:11 -0500

    Erase comment

--- plugin/ticket/lib/ticket.class.php
+++ plugin/ticket/lib/ticket.class.php
@@ -26,34 +26,6 @@ class Ticket {
 
         // initialize video server settings from global settings
         $plugin = TicketPlugin::create();
-/*
-        $bbb_plugin = $plugin->get('tool_enable');
-        $bbb_host   = $plugin->get('host');
-        $bbb_salt   = $plugin->get('salt');
-
-        //$course_code = api_get_course_id();
-
-        $this->logout_url = api_get_path(WEB_PLUGIN_PATH).'bbb/listing.php';
-        $this->table = Database::get_main_table('plugin_bbb_meeting');
-
-        if ($bbb_plugin == true) {
-            $user_info = api_get_user_info();
-            $this->user_complete_name = $user_info['complete_name'];
-            $this->salt = $bbb_salt;
-            $info = parse_url($bbb_host);
-            $this->url = $bbb_host.'/bigbluebutton/';
-            if (isset($info['scheme'])) {
-                $this->protocol = $info['scheme'].'://';
-                $this->url = str_replace($this->protocol, '', $this->url);
-            }
-
-            // Setting BBB api
-            define('CONFIG_SECURITY_SALT', $this->salt);
-            define('CONFIG_SERVER_BASE_URL', $this->url);
-
-            $this->api = new TckBlueButtonBN();
-            $this->plugin_enabled = true;
-        }*/
     }
     /**
      * Checks whether a user is teacher in the current course

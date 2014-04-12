9342fc3c1edaa79c316e8cc949323842b0bae010
Francis Gonzales <francis.gonzales@beeznest.com>
2014-04-11 19:04:37 -0500

    Rename Variables and avoid header tab in file

--- plugin/ticket/lib/ticket_plugin.class.php
+++ plugin/ticket/lib/ticket_plugin.class.php
@@ -23,76 +23,82 @@ class TicketPlugin extends Plugin
         require_once api_get_path(SYS_PLUGIN_PATH).PLUGIN_NAME.'/database.php';
 
         // Create link tab
-        $homep = api_get_path(SYS_PATH).'home/'; //homep for Home Path
-        $menutabs = 'home_tabs'; //menutabs for tabs Menu
-        $menuf = $menutabs;
-        $ext = '.html'; //ext for HTML Extension - when used frequently, variables are faster than hardcoded strings
-        $lang = ''; //el for "Edit Language"
-        if (!empty($_SESSION['user_language_choice'])) {
-            $lang = $_SESSION['user_language_choice'];
-        } elseif (!empty($_SESSION['_user']['language'])) {
-            $lang = $_SESSION['_user']['language'];
-        } else {
-            $lang = api_get_setting('platformLanguage');
-        }
-        $link_url = api_get_path(WEB_PLUGIN_PATH).'ticket/s/myticket.php';
-
-        $home_menu = '<li class="show_menu"><a href="'.$link_url.'" target="_self"><span>Ticket</span></a></li>';
-
-        // Write
-        if (file_exists($homep.$menuf.'_'.$lang.$ext)) {
-            if (is_writable($homep.$menuf.'_'.$lang.$ext)) {
-                $fp = fopen($homep.$menuf.'_'.$lang.$ext, 'w');
-                fputs($fp, $home_menu);
-                fclose($fp);
-                if (file_exists($homep.$menuf.$ext)) {
-                    if (is_writable($homep.$menuf.$ext)) {
-                        $fpo = fopen($homep.$menuf.$ext, 'w');
-                        fputs($fpo, $home_menu);
-                        fclose($fpo);
-                    }
-                }
-            } else {
-                $errorMsg = get_lang('HomePageFilesNotWritable');
-            }
-        } else {
-            //File does not exist
-            $fp = fopen($homep.$menuf.'_'.$lang.$ext, 'w');
-            fputs($fp, $home_menu);
-            fclose($fp);
-        }
+//        $homep = api_get_path(SYS_PATH).'home/'; //homep for Home Path
+//        $menutabs = 'home_tabs'; //menutabs for tabs Menu
+//        $menuf = $menutabs;
+//        $ext = '.html'; //ext for HTML Extension - when used frequently, variables are faster than hardcoded strings
+//        $lang = ''; //el for "Edit Language"
+//        if (!empty($_SESSION['user_language_choice'])) {
+//            $lang = $_SESSION['user_language_choice'];
+//        } elseif (!empty($_SESSION['_user']['language'])) {
+//            $lang = $_SESSION['_user']['language'];
+//        } else {
+//            $lang = api_get_setting('platformLanguage');
+//        }
+//        $link_url = api_get_path(WEB_PLUGIN_PATH).'ticket/s/myticket.php';
+//
+//        $home_menu = '<li class="show_menu"><a href="'.$link_url.'" target="_self"><span>Ticket</span></a></li>';
+//
+//        // Write
+//        if (file_exists($homep.$menuf.'_'.$lang.$ext)) {
+//            if (is_writable($homep.$menuf.'_'.$lang.$ext)) {
+//                $fp = fopen($homep.$menuf.'_'.$lang.$ext, 'w');
+//                fputs($fp, $home_menu);
+//                fclose($fp);
+//                if (file_exists($homep.$menuf.$ext)) {
+//                    if (is_writable($homep.$menuf.$ext)) {
+//                        $fpo = fopen($homep.$menuf.$ext, 'w');
+//                        fputs($fpo, $home_menu);
+//                        fclose($fpo);
+//                    }
+//                }
+//            } else {
+//                $errorMsg = get_lang('HomePageFilesNotWritable');
+//            }
+//        } else {
+//            //File does not exist
+//            $fp = fopen($homep.$menuf.'_'.$lang.$ext, 'w');
+//            fputs($fp, $home_menu);
+//            fclose($fp);
+//        }
 
     }
 
     function uninstall() {
-        $t_settings = Database::get_main_table(TABLE_MAIN_SETTINGS_CURRENT);
+        $tblSettings = Database::get_main_table(TABLE_MAIN_SETTINGS_CURRENT);
         $t_options = Database::get_main_table(TABLE_MAIN_SETTINGS_OPTIONS);
         $t_tool = Database::get_course_table(TABLE_TOOL_LIST);
+        $tblTicketTicket = Database::get_main_table(TABLE_SUPPORT_TICKET);
+        $tblTicketStatus = Database::get_main_table(TABLE_SUPPORT_STATUS);
+        $tblTicketProject = Database::get_main_table(TABLE_SUPPORT_PROJECT);
+        $tblTicketPriority = Database::get_main_table(TABLE_SUPPORT_PRIORITY);
+        $tblTicketMesAttch = Database::get_main_table(TABLE_SUPPORT_MESSAGE_ATTACHMENTS);
+        $tblTicketMessage = Database::get_main_table(TABLE_SUPPORT_MESSAGE);
+        $tblTicketCategory = Database::get_main_table(TABLE_SUPPORT_CATEGORY);
+        $tblTicketAssgLog = Database::get_main_table(TABLE_SUPPORT_ASSIGNED_LOG);
 
         //Delete settings
-        $sql = "DELETE FROM $t_settings WHERE variable = 'ticket_tool_enable'";
+        $sql = "DELETE FROM $tblSettings WHERE variable = 'ticket_tool_enable'";
         Database::query($sql);
-
-        $sql = "DROP TABLE IF EXISTS ticket_ticket";
+        
+        
+        $sql = "DROP TABLE IF EXISTS $tblTicketTicket";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_status";
+        $sql = "DROP TABLE IF EXISTS $tblTicketStatus";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_project";
+        $sql = "DROP TABLE IF EXISTS $tblTicketProject";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_priority";
+        $sql = "DROP TABLE IF EXISTS $tblTicketPriority";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_message_attch";
+        $sql = "DROP TABLE IF EXISTS $tblTicketMesAttch";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_message";
+        $sql = "DROP TABLE IF EXISTS $tblTicketMessage";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_category";
+        $sql = "DROP TABLE IF EXISTS $tblTicketCategory";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_assigned_log";
+        $sql = "DROP TABLE IF EXISTS $tblTicketAssgLog";
         Database::query($sql);
-        $sql = "DROP TABLE IF EXISTS ticket_ticket";
+        $sql = "DROP TABLE IF EXISTS $tblTicketTicket";
         Database::query($sql);
-
-        //Deleting course settings
-        $this->uninstall_course_fields_in_all_courses();
     }
 }

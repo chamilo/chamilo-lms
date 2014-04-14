42ea64d381da6dd07abdb302fcc3607a3148f0b3
Yannick Warnier <ywarnier@beeznest.org>
2014-04-14 03:30:04 -0500

    Code cleanup for ticket plugin (3) - refs #6715

--- plugin/ticket/lang/french.php
+++ plugin/ticket/lang/french.php
@@ -1,11 +1,13 @@
 <?php /* License: see /license.txt */
 //Needed in order to show the plugin title
-$strings['plugin_title']        = "Tickets";
+$strings['plugin_title']        = "Tickets de support";
 $strings['plugin_comment']      = "Plugin de gestion des tickets de support.";
+$strings['tool_enable']         = "Activer le plugin de tickets";
+$strings['tool_enable_help']    = "Activer l'outil de tickets activera un nouvel onglet dans le même menu horizontal. Cet onglet apparaîtra pour tous les utilisateurs et les mènera au système de gestion de tickets où ils pourront vérifier l'état de leurs tickets.";
+$strings['TabsTickets']         = "Onglet tickets";
 
 $strings['TicketNum']     = "Ticket #";
 $strings['Date']          = "Date";
-$strings['DateLastEdition']   = "Dernière modification";
 $strings['Category']      = "Catégorie";
 $strings['User']          = "Utilisateur";
 $strings['Program']       = "Programme";

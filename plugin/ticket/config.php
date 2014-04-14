e6ac6258e6deaf8d97a2c50c16f89f77f8b7a634
Yannick Warnier <ywarnier@beeznest.org>
2014-04-14 00:54:23 -0500

    Rename directory "s" to "src" - refs #6715

--- plugin/ticket/config.php
+++ plugin/ticket/config.php
@@ -42,4 +42,4 @@ require_once api_get_path(LIBRARY_PATH).'export.lib.inc.php';
 
 require_once api_get_path(PLUGIN_PATH).PLUGIN_NAME.'/lib/ticket.class.php';
 require_once api_get_path(PLUGIN_PATH).PLUGIN_NAME.'/lib/ticket_plugin.class.php';
-require_once api_get_path(PLUGIN_PATH).PLUGIN_NAME.'/s/ticket.class.php';
+require_once api_get_path(PLUGIN_PATH).PLUGIN_NAME.'/src/ticket.class.php';

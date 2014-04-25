333a71ac05d6cea9e95b33df7e9956b6feab2883
Julio Montoya <gugli100@gmail.com>
2014-04-25 18:30:55 +0200

    Adding jcapture plugin see #5101

---
+++ plugin/jcapture/plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Screencast widget
+ */
+$plugin_info = array(
+    'title' => 'Jcapture Screencast',
+    'comment' => "Screencast widget",
+    'version' => '1.0',
+    'author' => 'Julio Montoya'
+);

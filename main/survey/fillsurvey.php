ecb34c3d1ae821087c8695caad0032d88e323c96
unknown <ycastillo06@gmail.com>
2014-05-01 23:19:39 -0400

    Changing button text on first survey page -refs #7103

--- main/survey/fillsurvey.php
+++ main/survey/fillsurvey.php
@@ -1127,9 +1127,12 @@ if ($survey_data['survey_type'] === '0') {
     if ($survey_data['show_form_profile'] == 0) {
         // The normal survey as always
         if (($show < $numberofpages) || !$_GET['show']) { //$show = $_GET['show'] + 1
-            echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('NextQuestion').'</button>';
+            if ($show == 0) {
+                echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('StartSurvey').'</button>';    
+            } else {                   
+                echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('Next').'</button>';
+            }
         }
-
         if ($show >= $numberofpages && $_GET['show']) {
             echo '<button type="submit" name="finish_survey" class="next">'.get_lang('FinishSurvey').'</button>';
         }
@@ -1138,7 +1141,11 @@ if ($survey_data['survey_type'] === '0') {
         if (isset($_GET['show'])) {
             $numberofpages = count($paged_questions);
             if (($show < $numberofpages) || !$_GET['show']) { //$show = $_GET['show'] + 1
-                echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('Next').'</button>';
+                if ($show == 0) {
+                    echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('StartSurvey').'</button>';    
+                } else {                   
+                    echo '<button type="submit" name="next_survey_page" class="next">'.get_lang('Next').'</button>';
+                }
             }
 
             if ($show >= $numberofpages && $_GET['show']) {

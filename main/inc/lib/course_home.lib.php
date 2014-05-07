ba76cedbb8e6b041d636586d4d2dc37ceb53ba74
Yannick Warnier <ywarnier@beeznest.org>
2014-05-06 20:20:26 -0500

    Remove white space in course homepage when eye is not visible - student role

--- main/inc/lib/course_home.lib.php
+++ main/inc/lib/course_home.lib.php
@@ -694,7 +694,7 @@ class CourseHome
                         }
                     }
                 } else {
-                    $item['visibility'] .= '&nbsp;&nbsp;&nbsp;&nbsp;';
+                    $item['visibility'] .= '';
                 }
 
                 // NOTE : Table contains only the image file name, not full path

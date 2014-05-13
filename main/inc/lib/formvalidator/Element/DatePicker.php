9ae4629782542024b0807958f715a7006f3f2054
Julio Montoya <gugli100@gmail.com>
2014-05-13 11:54:33 +0200

    Minor format code.

--- main/inc/lib/formvalidator/Element/DatePicker.php
+++ main/inc/lib/formvalidator/Element/DatePicker.php
@@ -1,36 +1,47 @@
 <?php
 /* For licensing terms, see /license.txt */
+
 require_once 'HTML/QuickForm/date.php';
 
 /**
  * Form element to select a date and hour (with popup datepicker)
+ *
+ * Class DatePicker
  */
 class DatePicker extends HTML_QuickForm_text
 {
-	/**
-	 * Constructor
-	 */
-	public function DatePicker($elementName = null, $elementLabel = null, $attributes = null)
-	{
+    /**
+     * @param string $elementName
+     * @param string $elementLabel
+     * @param array  $attributes
+     */
+    public function DatePicker($elementName = null, $elementLabel = null, $attributes = null)
+    {
         if (!isset($attributes['id'])) {
             $attributes['id'] = $elementName;
         }
 
-		HTML_QuickForm_element::HTML_QuickForm_element($elementName, $elementLabel, $attributes);
-		$this->_appendName = true;
-		$this->_type = 'date_picker';
-	}
+        HTML_QuickForm_element::HTML_QuickForm_element($elementName, $elementLabel, $attributes);
+        $this->_appendName = true;
+        $this->_type = 'date_picker';
+    }
 
-	/**
-	 * HTML code to display this datepicker
-	 */
-	public function toHtml()
-	{
-		$js = $this->getElementJS();
-		return $js.parent::toHtml();
-	}
+    /**
+    * HTML code to display this datepicker
+     *
+     * @return string
+    */
+    public function toHtml()
+    {
+        $js = $this->getElementJS();
 
-    function setValue($value)
+        return $js.parent::toHtml();
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
     {
         $value = substr($value, 0, 16);
         $this->updateAttributes(
@@ -40,11 +51,12 @@ class DatePicker extends HTML_QuickForm_text
         );
     }
 
-	/**
-	 * Get the necessary javascript for this datepicker
-	 */
-	private function getElementJS()
-	{
+    /**
+     * Get the necessary javascript for this datepicker
+     * @return string
+     */
+    private function getElementJS()
+    {
         $js = null;
         $id = $this->getAttribute('id');
 
@@ -56,6 +68,7 @@ class DatePicker extends HTML_QuickForm_text
             });
         </script>";
 
-		return $js;
-	}
+        return $js;
+    }
+
 }

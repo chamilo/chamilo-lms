dc024f5a0a6e4744e411da081c73025408a0c1b1
Julio Montoya <gugli100@gmail.com>
2014-05-14 15:05:18 +0200

    Fixing require_once

--- console.php
+++ console.php
@@ -9,7 +9,7 @@ if (PHP_SAPI != 'cli') {
 }
 require_once __DIR__.'/vendor/autoload.php';
 
-$app = require_once dirname(__FILE__).'/main/inc/global.inc.php';
+$app = require_once dirname(__FILE__).'/src/ChamiloLMS/app.php';
 
 use Knp\Provider\ConsoleServiceProvider;
 

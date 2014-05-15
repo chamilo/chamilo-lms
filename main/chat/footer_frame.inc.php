d6670a738bab0bea0d806deaa26b37d96e59dd8b
aragonc <aragcar@gmail.com>
2014-05-15 15:26:04 -0500

    updating and tidy code, as well free notification sound #7147

--- main/chat/footer_frame.inc.php
+++ main/chat/footer_frame.inc.php
@@ -9,9 +9,9 @@
  */
 ?>
 <audio id="audio">
-	<source src="sound/sonido_notificacion.wav" type="audio/wav"></source>
-	<source src="sound/sonido_notificacion.ogg" type="audio/ogg"></source>
-	<source src="sound/sonido_notificacion.mp3" type="audio/mpeg"></source>
+	<source src="<?php echo api_get_path(WEB_PATH);?>main/chat/sound/notification.wav" type="audio/wav"></source>
+	<source src="<?php echo api_get_path(WEB_PATH);?>main/chat/sound/notification.ogg" type="audio/ogg"></source>
+	<source src="<?php echo api_get_path(WEB_PATH);?>main/chat/sound/notification.mp3" type="audio/mpeg"></source>
 </audio>
 </body>
 </html>

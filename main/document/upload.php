b663c4b407675a95887ecbf734de0e1275f71c1d
Yannick Warnier <ywarnier@beeznest.org>
2014-05-30 15:13:40 -0500

    Fix issue loosing ref of the course - refs BT#8186

--- main/document/upload.php
+++ main/document/upload.php
@@ -273,7 +273,7 @@ $form->setDefaults($defaults);
 
 $simple_form = $form->return_form();
 
-$url = api_get_path(WEB_AJAX_PATH).'document.ajax.php?a=upload_file';
+$url = api_get_path(WEB_AJAX_PATH).'document.ajax.php?'.api_get_cidreq().'&a=upload_file';
 $multiple_form =  get_lang('ClickToSelectOrDragAndDropMultipleFilesOnTheUploadField').'<br />';
 //Adding icon replace the  <div>'.get_lang('UploadFiles').'</div> with this:
 //<div style="width:50%;margin:0 auto;"> '.Display::div(Display::return_icon('folder_document.png', '', array(), 64), array('style'=>'float:left')).' '.get_lang('UploadFiles').'</div>

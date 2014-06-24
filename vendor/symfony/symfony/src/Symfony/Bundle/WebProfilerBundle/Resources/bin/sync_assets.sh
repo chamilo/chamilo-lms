2a4d0017a0477054a318de1ced43295bbc58bded
Julio Montoya <gugli100@gmail.com>
2014-06-24 20:20:44 +0200

    Adding missing files.

--- vendor/symfony/symfony/src/Symfony/Bundle/WebProfilerBundle/Resources/bin/sync_assets.sh
+++ vendor/symfony/symfony/src/Symfony/Bundle/WebProfilerBundle/Resources/bin/sync_assets.sh
@@ -4,3 +4,4 @@ DIR=`php -r "echo realpath(dirname('$0'));"`
 
 cp $DIR/../../../FrameworkBundle/Resources/public/css/body.css $DIR/../views/Profiler/body.css.twig
 cp $DIR/../../../FrameworkBundle/Resources/public/css/exception.css $DIR/../views/Collector/exception.css.twig
+

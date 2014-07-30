2582408cedfb0a81afc2d520adc3f4f1e6af9656
Daniel Barreto <daniel.barreto@beeznest.com>
2014-07-30 18:22:56 -0500

    Add ViewerJS  -refs BT#8274

--- main/inc/lib/document.lib.php
+++ main/inc/lib/document.lib.php
@@ -3761,7 +3761,7 @@ class DocumentManager
      */
     public static function get_web_odf_extension_list()
     {
-        return array('ods', 'odt');
+        return array('ods', 'odt', 'odp');
     }
 
     /**

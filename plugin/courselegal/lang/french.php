d35c2e9f0dbc98c3082a04a49347b842bbfb1a92
Yannick Warnier <ywarnier@beeznest.org>
2014-10-19 21:35:10 -0500

    Fix language terms and add default icon in course settings page - refs #7307

---
+++ plugin/courselegal/lang/french.php
@@ -0,0 +1,27 @@
+<?php
+
+$strings['plugin_title'] = "Termes légaux d'utilisation de cours";
+$strings['plugin_comment'] = "Ajouter des termes d'utilisation par cours (fonctionne également avec les cours dans des sessions), avec possibilité de demander confirmation par mail.";
+$strings['tool_enable'] = 'Activer le plugin';
+$strings['tool_enable_help'] = 'Une fois activé, il vous faudra configurer le plugin dans chaque cours (outil de configuration du cours), puis depuis la page d\'accueil du cours (un bouton apparaîtra au professeur uniquement)';
+
+$strings['CourseLegal'] = 'Termes d\'utilisation du cours';
+$strings['WebAgreement'] = 'Accord web';
+$strings['MailAgreement'] = 'Accord par mail';
+$strings['MailAgreementWasSentWithClickX'] = 'Cliquez sur le lien suivant pour accepter les termes d\'accès au cours %s: <br /> %s';
+$strings['AgreementUpdated'] = 'Termes mis à jour';
+$strings['AgreementWasUpdatedClickHere'] = 'Les termes d\'utilisation ont été mis à jour. Cliquez ici: %s';
+$strings['YouNeedToConfirmYourAgreementCheckYourEmail'] = 'Veuillez confirmer votre accord. Vérifiez votre e-mail.';
+$strings['DeleteFile'] = 'Éliminez la pièce jointe';
+$strings['RemoveAllUserAgreements'] = 'Supprimer tous les accords utilisateurs antérieurs';
+$strings['WarnAllUsersByEmail'] = 'Avertir tous les utilisateurs par mail';
+$strings['WarnAllUsersByEmailAndSendAttachment'] = 'Avertir tous les utilisateurs par mail avec la pièce jointe.';
+$strings['ReSendMailAgreementLink'] = 'Envoyer le lien d\'accord par mail de nouveau';
+$strings['SendOnlyWarning'] = 'Envoyer seulement l\'avertissement';
+$strings['SendAgreementFile'] = 'Envoyer à nouveau l\'avertissement avec la pièce jointe';
+$strings['NoSendWarning'] = 'Ne pas envoyer l\'avertissement';
+$strings['TheAgreementIsAttachedInThisEmail'] = 'Une copie du document d\'accord est joint à cet e-mail';
+
+
+
+

0333b1ba4a8f9f2b5301a715a81e788d8523850c
aragonc <aragcar@gmail.com>
2014-10-21 20:43:02 -0500

    Fix style of social network - refs #7304

--- main/inc/lib/social.lib.php
+++ main/inc/lib/social.lib.php
@@ -622,7 +622,7 @@ class SocialManager extends UserManager
         $total_invitations = (!empty($total_invitations) ? Display::badge($total_invitations) : '');
         $showUserImage = user_is_online($user_id) || api_is_platform_admin();
 
-        $html = '<div class="social-menu">';
+        $html = '<div>';
         if (in_array($show, $show_groups) && !empty($group_id)) {
             //--- Group image
             $group_info = GroupPortalManager::get_group_data($group_id);
@@ -661,8 +661,8 @@ class SocialManager extends UserManager
                 $html .= '<a href="'.api_get_path(WEB_PATH).'main/auth/profile.php">'.get_lang('EditProfile').'</a></div>';
             }
             $html .= '</div>';
-            $html .= '</div>';
         }
+        $html .= '</div>';
         return $html;
     }
 

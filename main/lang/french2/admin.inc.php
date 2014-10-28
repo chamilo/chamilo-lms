2d605260523b150405126331b7833104f20844bd
Yannick Warnier <ywarnier@beeznest.org>
2014-10-28 12:17:21 -0500

    Remove french2 erroneous upload

--- main/lang/french2/admin.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$ShowAdministratorDataTitle="Toon gegevens<br />a";
-$emailAdministratorTitle="Platformbeheerder:<br />e-mail";
-$ShowLinkBugNotificationTitle="Toon link<br />\nom bug te rapporteren";

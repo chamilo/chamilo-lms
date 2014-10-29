beeznest/chamilo-lms-icpna

Update document scripts to fix convertion issues and PHP warnings - refs BT#8274

main/inc/lib/document.lib.php
@@ -3320,7 +3320,14 @@ class DocumentManager {
         if (api_get_setting('show_chat_folder') == 'false') {
             $folders_to_avoid[] = '/chat_files';
         }
-        return in_array($path, $folders_to_avoid);
+
+        if (is_array($folders_to_avoid)) {
+
+            return in_array($path, $folders_to_avoid);
+        } else {
+
+            return false;
+        }
     }
 
     static function get_system_folders() {

chamilo/chamilo-lms

main/inc/lib/document.lib.php
@@ -4042,7 +4042,14 @@ class DocumentManager
         if (api_get_setting('show_chat_folder') == 'false') {
             $foldersToAvoid[] = '/chat_files';
         }
-        return in_array($path, $foldersToAvoid);
+
+        if (is_array($foldersToAvoid)) {
+
+            return in_array($path, $foldersToAvoid);
+        } else {
+
+            return false;
+        }
     }
 
     /**
543dc56eae6e524b585496356e6d74e8ec679236
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-12-04 11:26:21 -0500

    Change session edit form and update session extra field - refs BT#9036

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -1350,7 +1350,8 @@ class SessionManager
         $end_limit = true,
         $description = null,
         $showDescription = null,
-        $duration = null
+        $duration = null,
+        $extraFields = array()
     ) {
         $name = trim(stripslashes($name));
         $year_start = intval($year_start);
@@ -1460,6 +1461,12 @@ class SessionManager
 					  WHERE id='$id'";
 
                 Database::query($sql);
+
+                $extraFields['session_id'] = $id;
+
+                $sessionFieldValue = new SessionFieldValue();
+                $sessionFieldValue->save_field_values($extraFields);
+
                 return $id;
             }
         }

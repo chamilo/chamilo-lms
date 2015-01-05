29199eee64e3353ad35eaf41188959dad4bebd94
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-01-05 16:19:02 -0500

    Don't generate certificate to invited user - refs BT#9070

--- main/gradebook/gradebook_display_certificate.php
+++ main/gradebook/gradebook_display_certificate.php
@@ -70,6 +70,10 @@ switch ($action) {
         $user_list = CourseManager::get_user_list_from_course_code(api_get_course_id(), api_get_session_id());
         if (!empty($user_list)) {
             foreach ($user_list as $user_info) {
+                if ($user_info['status'] == ROLE_INVITED) {
+                    continue;
+                }
+
                 Category::register_user_certificate($cat_id, $user_info['user_id']);
             }
         }

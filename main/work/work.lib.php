900d92f5a2cbe0e15143ad60c713b73a370064e2
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-01-06 15:03:45 -0500

    Don't allow send works to invtited users - refs BT#9070

--- main/work/work.lib.php
+++ main/work/work.lib.php
@@ -2260,6 +2260,7 @@ function get_work_user_list(
         $sql = " $select
                 FROM $work_condition  $user_condition
                 WHERE $extra_conditions $where_condition $condition_session
+                    AND u.status != " . ROLE_INVITED . "
                 ORDER BY $column $direction";
 
         if (!empty($start) && !empty($limit)) {

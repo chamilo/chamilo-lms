e5454a2d5c859a2ce6ab6e58c9d292d43819fb4b
Julio Montoya <gugli100@gmail.com>
2015-02-03 13:42:28 +0100

    Updating .gitattributes

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9eafe859ee8be4d5a438d1eca7997a07::getLoader();
+return ComposerAutoloaderInit14e9c6cb5b03d993912f65da12a9cc3e::getLoader();

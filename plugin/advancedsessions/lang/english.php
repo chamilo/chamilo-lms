3d1a955c28bf479e4925563836710d7124fa6d47
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-02-05 16:39:27 -0500

    Update plugin lang variable - refs BT#9049

--- plugin/advancedsessions/lang/english.php
+++ plugin/advancedsessions/lang/english.php
@@ -1,3 +1,3 @@
 <?php
 $strings['plugin_title'] = "Advanced Sessions";
-$strings['plugin_comment'] = "Add a session extra field for set a session description";
+$strings['plugin_comment'] = "Add a session extra field for to set a session description";

847cf7cb3519b0a505030077c33838a788e083c9
Yannick Warnier <ywarnier@beeznest.org>
2015-02-13 16:21:42 -0500

    Little fixes to language terms in Create Drupal User plugin - BT#9083

--- plugin/createdrupaluser/lang/english.php
+++ plugin/createdrupaluser/lang/english.php
@@ -5,8 +5,8 @@
  * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
  * @package chamilo.plugin.createDrupalUser
  */
-$strings['plugin_title'] = 'Create Drupal user';
-$strings['plugin_comment'] = 'This plugin create a user in Drupal website when a user is registered in Chamilo LMS';
+$strings['plugin_title'] = 'Create Drupal users';
+$strings['plugin_comment'] = 'This plugin creates users in an associated Drupal website when a user is created in Chamilo LMS. This requires the "chamilo" module to be installed and configured on the Drupal side.';
 
 $strings['drupal_domain'] = 'Drupal website URL';
 $strings['drupal_domain_help'] = 'The server domain name should be written with a trailing slash and with the protocol, e.g. http://www.example.com/';
--- plugin/createdrupaluser/lang/spanish.php
+++ plugin/createdrupaluser/lang/spanish.php
@@ -6,7 +6,7 @@
  * @package chamilo.plugin.createDrupalUser
  */
 $strings['plugin_title'] = 'Crear usuario Drupal';
-$strings['plugin_comment'] = 'Este plugin permite crear un usuario en un sitio web Drupal cuando un usuario es registrado en Chamilo LMS';
+$strings['plugin_comment'] = 'Este plugin permite crear usuarios en un sitio web Drupal cuando un usuario es creado en Chamilo LMS. Este plugin requiere que el módulo "chamilo" de Drupal esté instalado y configurado en el sitio Drupal.';
 
 $strings['drupal_domain'] = 'URL del sitio web Drupal';
 $strings['drupal_domain_help'] = 'La dirección del servidor debe escribirse con el protocolo al comienzo y con la barra al final, por ejemplo http://www.example.com/';

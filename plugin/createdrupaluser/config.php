40f012b0fac484febc3be0f6cca244ea605a07ad
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-02-13 15:02:38 -0500

    Create plugin to crete a Drupal user - refs BT#9083

---
+++ plugin/createdrupaluser/config.php
@@ -0,0 +1,12 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * Config the plugin
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ * @package chamilo.plugin.createDrupalUser
+ */
+
+require_once api_get_path(SYS_PATH) . 'main/inc/global.inc.php';
+require_once api_get_path(LIBRARY_PATH) . 'plugin.class.php';
+require_once api_get_path(PLUGIN_PATH) . 'createdrupaluser/src/HookCreateDrupalUser.php';
+require_once api_get_path(PLUGIN_PATH) . 'createdrupaluser/src/CreateDrupalUser.php';

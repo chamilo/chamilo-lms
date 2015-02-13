fb528d4e663385df463354eff6579abbe6f41efe
Julio Montoya <gugli100@gmail.com>
2015-02-13 15:26:31 +0100

    Fixes introduction tool name

--- src/Chamilo/CoreBundle/Component/Editor/CkEditor/Toolbar/IntroductionTool.php
+++ src/Chamilo/CoreBundle/Component/Editor/CkEditor/Toolbar/IntroductionTool.php
@@ -20,8 +20,14 @@ class IntroductionTool extends Basic
             //array('name' => 'editing',    'groups' =>array('clipboard', 'undo', )),
             //array('name' => 'forms',    'groups' =>array('clipboard', 'undo', )),
             '/',
-            array('name' => 'basicstyles',    'groups' =>array('basicstyles', 'cleanup', )),
-            array('name' => 'paragraph',    'groups' =>array('list', 'indent', 'blocks', 'align' )),
+            array(
+                'name' => 'basicstyles',
+                'groups' => array('basicstyles', 'cleanup',)
+            ),
+            array(
+                'name' => 'paragraph',
+                'groups' => array('list', 'indent', 'blocks', 'align')
+            ),
             array('name' => 'links'),
             array('name' => 'insert'),
             '/',

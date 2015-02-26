4374442bf93d6c4eac07fe7b3bd6f4f1b9aa0c53
Yannick Warnier <ywarnier@beeznest.org>
2015-02-26 12:17:04 -0500

    Add condition to avoid session ID to be overwritten by weird extra field 'id' - refs BT#9092

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -5518,10 +5518,13 @@ class SessionManager
                     if ($sessionFieldValue['session_id'] == $id) {
                         // Check if session field value is set in session field list
                         if (isset($fields[$sessionFieldValue['field_id']])) {
-                            $var = $fields[$sessionFieldValue['field_id']];
-                            $val = $sessionFieldValue['field_value'];
-                            // Assign session field value to session
-                            $session[$var] = $val;
+                            // Avoid overwriting the session's ID field
+                            if ($fields[$sessionFieldValue['field_id']] != 'id') {
+                                $var = $fields[$sessionFieldValue['field_id']];
+                                $val = $sessionFieldValue['field_value'];
+                                // Assign session field value to session
+                                $session[$var] = $val;
+                            }
                         }
                     }
                 }

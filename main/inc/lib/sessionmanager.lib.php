4a57cef0eb6b2f2094b34c1739e75de2b960c33b
Yannick Warnier <ywarnier@beeznest.org>
2015-02-26 15:23:32 -0500

    Improve efficiency in searchSession() by avoiding to query extra fields if none requested - refs BT#9418

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -5740,19 +5740,26 @@ class SessionManager
 
         $term = Database::escape_string($term);
 
-        $resultData = Database::select('*', $sTable, array(
-            'where' => array(
-                "name LIKE %?% " => $term,
-                "OR description LIKE %?% " => $term,
-                "OR id IN (
+        if (is_array($extraFieldsToInclude) && count($extraFieldsToInclude) > 0) {
+            $resultData = Database::select('*', $sTable, array(
+                'where' => array(
+                    "name LIKE '%?%' " => $term,
+                    "OR description LIKE '%?%' " => $term,
+                    "OR id IN (
                     SELECT session_id
                     FROM $sfvTable
-                    WHERE field_value LIKE %?%
+                    WHERE field_value LIKE '%?%'
                 ) " => $term
-            )
-        ));
+                )
+            ));
+        } else {
+            $resultData = Database::select('*', $sTable, array(
+                'where' => array(
+                    "name LIKE '%?%' " => $term,
+                    "OR description LIKE '%?%' " => $term
+                )
+            ));
 
-        if (empty($extraFieldsToInclude)) {
             return $resultData;
         }
 

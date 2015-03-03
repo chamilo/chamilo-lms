71519811fee9d8b229e06f268fbcd4b92a742c53
Julio Montoya <gugli100@gmail.com>
2015-03-03 10:01:08 +0100

    Update lang vars.

--- main/lang/french/import.inc.php
+++ main/lang/french/import.inc.php
@@ -2,8 +2,6 @@
 /*
 for more information: see languages.txt in the lang folder.
 */
-$langExplanation = "Une fois que vous aurez cliqué sur OK, un cours contenant Tests, Documents, Parcours d'Apprentissage SCORM... sera créé. Grâce à votre identifiant de responsable du cours vous pourrez en modifier le contenu";
-$langTooBig = "Vous n'avez choisi aucun fichier à envoyer, ou celui-ci est trop gros";
 $langCouldNot = "Le fichier ne peut être envoyé";
 $langAddPageToSite = "Ajouter une page au site";
 $langCouldNotSendPage = "Ce fichier n'est pas au format HTML et n'a pu être envoyé. Si vous voulez envoyer vers le serveur des documents non HTML (PDF, Word, Power Point, Vidéo, etc.) utilisez <a href=../document/document.php>Documents</a>";

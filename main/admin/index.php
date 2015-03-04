17132f999772adb71b2dfa0b5232ac18b210ab1f
Yannick Warnier <ywarnier@beeznest.org>
2015-03-03 23:52:20 -0500

    Remove social-tool requirement to use users social groups: groups can be used for other purposes as well - refs BT#9431

--- main/admin/index.php
+++ main/admin/index.php
@@ -115,11 +115,9 @@ if (api_is_platform_admin()) {
         array('url'=>'user_import.php', 'label' => get_lang('ImportUserListXMLCSV')),
         array('url'=>'user_update_import.php', 'label' => get_lang('EditUserListCSV')),
     );
+    $items[] = array('url'=>'group_add.php', 	'label' => get_lang('AddGroups'));
+    $items[] = array('url'=>'group_list.php', 	'label' => get_lang('GroupList'));
 
-    if (api_get_setting('allow_social_tool') == 'true') {
-        $items[] = array('url'=>'group_add.php', 	'label' => get_lang('AddGroups'));
-        $items[] = array('url'=>'group_list.php', 	'label' => get_lang('GroupList'));
-    }
     if (isset($extAuthSource) && isset($extAuthSource['extldap']) && count($extAuthSource['extldap']) > 0) {
         $items[] = array('url'=>'ldap_users_list.php', 	'label' => get_lang('ImportLDAPUsersIntoPlatform'));
     }

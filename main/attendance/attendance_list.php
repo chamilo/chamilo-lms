aff299574546d66d8d16a163dfcda455214ba6e5
Julio Montoya <gugli100@gmail.com>
2015-03-09 11:34:12 +0100

    Minor - format code.

--- main/attendance/attendance_list.php
+++ main/attendance/attendance_list.php
@@ -18,10 +18,6 @@ if (api_is_allowed_to_edit(null, true)) {
     echo '<div class="actions">';
     echo '<a href="index.php?'.api_get_cidreq().$param_gradebook.'&action=attendance_add">'.
         Display::return_icon('new_attendance_list.png',get_lang('CreateANewAttendance'),'',ICON_SIZE_MEDIUM).'</a>';
-
-    /*echo '<a href="index.php?'.api_get_cidreq().$param_gradebook.'&action=calendar_logins">'.
-        Display::return_icon('attendance_list.png',get_lang('Logins'),'',ICON_SIZE_MEDIUM).'</a>';*/
-
     echo '</div>';
 }
 $attendance = new Attendance();

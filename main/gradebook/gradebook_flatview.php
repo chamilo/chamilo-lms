4235e46f20d4ce4e4bd223ac30cd6d0c0d09b971
Julio Montoya <gugli100@gmail.com>
2015-03-17 15:32:23 +0100

    Minor - Format code.

--- main/gradebook/gradebook_flatview.php
+++ main/gradebook/gradebook_flatview.php
@@ -280,15 +280,11 @@ if (isset($_GET['isStudentView']) && $_GET['isStudentView'] == 'false') {
         $simple_search_form
     );
 
-    // main graph
+    // Table
     $flatviewtable->display();
-    // @todo this needs a fix
-    //$image_file = $flatviewtable->display_graph();
+
     //@todo load images with jquery
     echo '<div id="contentArea" style="text-align: center;" >';
-    if (!empty($image_file)) {
-        echo '<img src="'.$image_file.'">';
-    }
     $flatviewtable->display_graph_by_resource();
     echo '</div>';
 }

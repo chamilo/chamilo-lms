c4792482d5967f96de27eee4557828fe872243cd
Julio Montoya <gugli100@gmail.com>
2015-03-23 12:33:01 +0100

    Add setting course_log_hide_columns default columns to hide see BT#9609

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -293,3 +293,5 @@ $_configuration['system_stable']     = NEW_VERSION_STABLE;
 //$_configuration['prevent_session_admins_to_manage_all_users'] = false;
 // Show delete option in attendance
 //$_configuration['allow_delete_attendance'] = false;
+// Course log - Default columns to hide
+//$_configuration['course_log_hide_columns'] = array(1, 9);

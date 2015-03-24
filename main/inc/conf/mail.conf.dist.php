5d3560a8c4e32a6cbd962bae77fedc97cbc8d467
Nicolas Ducoulombier <nicolas@ldd.fr>
2015-03-24 15:58:44 -0500

    Add SMTP unique sender configuration - refs BT#9588

--- main/inc/conf/mail.conf.dist.php
+++ main/inc/conf/mail.conf.dist.php
@@ -16,3 +16,4 @@ $platform_email['SMTP_AUTH']         = 0;
 $platform_email['SMTP_USER']         = '';
 $platform_email['SMTP_PASS']         = '';
 $platform_email['SMTP_CHARSET']      = api_get_system_encoding();
+$platform_email['SMTP_UNIQUE_SENDER']= 0; // to send all mails from the same user

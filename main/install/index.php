1709094e121f666648844402793059029e18aa1f
aragonc <aragcar@gmail.com>
2015-03-26 18:34:54 -0500

    udpate css default

--- main/install/index.php
+++ main/install/index.php
@@ -76,12 +76,12 @@ if (!array_key_exists($install_language, get_language_folder_list())) {
 
 // Loading language files.
 require api_get_path(SYS_LANG_PATH).'english/trad4all.inc.php';
-require api_get_path(SYS_LANG_PATH).'english/admin.inc.php';
+//require api_get_path(SYS_LANG_PATH).'english/admin.inc.php';
 require api_get_path(SYS_LANG_PATH).'english/install.inc.php';
 if ($install_language != 'english') {
 	include_once api_get_path(SYS_LANG_PATH).$install_language.'/trad4all.inc.php';
 	include_once api_get_path(SYS_LANG_PATH).$install_language.'/install.inc.php';
-    include_once api_get_path(SYS_LANG_PATH).$install_language.'/admin.inc.php';
+    //include_once api_get_path(SYS_LANG_PATH).$install_language.'/admin.inc.php';
 }
 
 // These global variables must be set for proper working of the function get_lang(...) during the installation.

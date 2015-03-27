32272842fcb1111ddb2ba098a18eb091e4ebb7df
Imanol Losada <imanol.losada@gmail.com>
2015-03-27 09:03:39 -0500

    Add corrected custom pages - refs BT#8538

--- custompages/registration-feedback-dist.php
+++ custompages/registration-feedback-dist.php
@@ -3,39 +3,50 @@
 /**
  * Quick display for user registration
  * @package chamilo.custompages
- */ 
+ */
 /**
  * HTML output
  */
+require_once('language.php');
 ?>
 <html>
 <head>
-	<title>Registration</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<!--[if !IE 6]><!-->
-	<link rel="stylesheet" type="text/css" href="../../custompages/style.css" />
-	<!--<![endif]-->
-	<!--[if IE 6]>
-	<link rel="stylesheet" type="text/css" href="../../custompages/style-ie6.css" />
-	<![endif]-->
-	<script type="text/javascript" src="../../main/inc/lib/javascript/jquery.min.js"></script>
+    <title><?php echo custompages_get_lang('Registration');?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/custompages/css/vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="/custompages/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+    <script src="js/vendor/html5shiv.js"></script>
+    <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="<?php echo api_get_path('WEB_PATH') ?>/main/inc/lib/javascript/jquery.min.js"></script>
 </head>
 <body>
-	<div id="backgroundimage">
-		<img src="/custompages/images/page-background.png" class="backgroundimage" />
-	</div>
-	<div id="wrapper">
-		<div id="header">
-			<img src="../../custompages/images/header.png" alt="Ambassador logo" />
-		</div> <!-- #header -->
-		<div id="registration-feedback-box">
-      <?php
-      echo $content['info'];
-      ?>
-		</div> <!-- #feedback -->
-		<div id="footer">
-			<img src="../../custompages/images/footer.png" />
-		</div> <!-- #footer -->
-	</div> <!-- #wrapper -->
+<img src="/custompages/images/page-background.png" class="backgroundimage" />
+<section id="registration">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="form-wrap">
+                    <div class="logo">
+                        <img src="/custompages/images/header.png">
+                    </div>
+                    <div id="registration-form-box" class="form-box">
+                        <div class="block-form-login">
+                            <?php   echo $content['info'];  ?>
+                        </div>
+                    </div>
+                    <div id="footer">
+                        <img src="../../custompages/images/footer.png" />
+                    </div> <!-- #footer -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>

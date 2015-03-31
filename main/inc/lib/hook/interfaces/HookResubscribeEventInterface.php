99f462bc102f65a24571d7f72910ce6753fbd1e4
Imanol Losada <imanol.losada@gmail.com>
2015-03-31 15:46:52 -0500

    Add hook files - refs BT#9438

---
+++ main/inc/lib/hook/interfaces/HookResubscribeEventInterface.php
@@ -0,0 +1,21 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * This file contains all Hook interfaces and their relation.
+ * They are used for Hook classes
+ * @package chamilo.library.hook
+ */
+
+/**
+ * Interface HookResubscribeEventInterface
+ */
+interface HookResubscribeEventInterface extends HookEventInterface
+{
+    /**
+     * Update all the observers
+     * @param int $type
+     *
+     * @return int
+     */
+    public function notifyResubscribe($type);
+}
---
+++ main/inc/lib/hook/interfaces/HookResubscribeObserverInterface.php
@@ -0,0 +1,20 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * This file contains all Hook interfaces and their relation.
+ * They are used for Hook classes
+ * @package chamilo.library.hook
+ */
+
+/**
+ * Interface ResubscribeHookInterface
+ */
+interface HookResubscribeObserverInterface extends HookObserverInterface
+{
+    /**
+     * @param HookResubscribeObserverInterface $hook
+     *
+     * @return int
+     */
+    public function hookResubscribe(HookResubscribeEventInterface $hook);
+}

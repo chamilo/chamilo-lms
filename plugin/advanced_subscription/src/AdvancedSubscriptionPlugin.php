e47d18978bd01ea3c809d6e1329b743d08a9867b
Nicolas Ducoulombier <nicolas@ldd.fr>
2015-04-08 15:48:40 -0500

    fix induction query - ref BT9092

--- plugin/advanced_subscription/src/AdvancedSubscriptionPlugin.php
+++ plugin/advanced_subscription/src/AdvancedSubscriptionPlugin.php
@@ -1304,11 +1304,15 @@ class AdvancedSubscriptionPlugin extends Plugin implements HookPluginInterface
      */
     private function getApprovedInductionSessions($userId)
     {
-        $sql = "SELECT s.id FROM session AS s "
-            . "INNER JOIN session_field_values AS sfv ON s.id = sfv.session_id "
-            . "INNER JOIN session_field AS sf ON sfv.field_id = sf.id "
-            . "INNER JOIN session_rel_user AS su ON s.id = su.id_session "
-            . "WHERE sf.field_variable = 'is_induccion_session' "
+        $tSession = Database::get_main_table(TABLE_MAIN_SESSION);
+        $tSessionField = Database::get_main_table(TABLE_MAIN_SESSION_FIELD);
+        $tSessionFieldValues = Database::get_main_table(TABLE_MAIN_SESSION_FIELD_VALUES);
+        $tSessionUser = Database::get_main_table(TABLE_MAIN_SESSION_USER);
+        $sql = "SELECT s.id FROM $tSession AS s "
+            . "INNER JOIN $tSessionFieldValues AS sfv ON s.id = sfv.session_id "
+            . "INNER JOIN $tSessionField AS sf ON sfv.field_id = sf.id "
+            . "INNER JOIN $tSessionUser AS su ON s.id = su.id_session "
+            . "WHERE sf.field_variable = 'is_induction_session' "
             . "AND su.relation_type = 0 "
             . "AND su.id_user = " . intval($userId);
 

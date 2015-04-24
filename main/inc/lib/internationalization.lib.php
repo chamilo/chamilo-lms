9c13c98876794f1dc6583ab6588f8bbb6877fe16
Julio Montoya <gugli100@gmail.com>
2015-04-24 10:18:36 +0200

    Merge files.

--- main/inc/lib/internationalization.lib.php
+++ main/inc/lib/internationalization.lib.php
@@ -1123,9 +1123,6 @@ function api_html_entity_decode($string, $quote_style = ENT_COMPAT, $encoding = 
     if (empty($encoding)) {
         $encoding = _api_mb_internal_encoding();
     }
-    if (_api_html_entity_supports($encoding)) {
-        return html_entity_decode($string, $quote_style, $encoding);
-    }
     if (api_is_encoding_supported($encoding)) {
         if (!api_is_utf8($encoding)) {
             $string = api_utf8_encode($string, $encoding);
@@ -1858,7 +1855,225 @@ function get_plugin_lang($variable, $pluginName) {
 }
 
 /**
- * Functions for internal use behind this API
+ * Returns an array of translated week days and months, short and normal names.
+ * @param string $language (optional)	Language id. If it is ommited, the current interface language is assumed.
+ * @return array						Returns a multidimensional array with translated week days and months.
+ */
+function &_api_get_day_month_names($language = null) {
+    static $date_parts = array();
+    if (empty($language)) {
+        $language = api_get_interface_language();
+    }
+    if (!isset($date_parts[$language])) {
+        $week_day = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        $month = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        for ($i = 0; $i < 7; $i++) {
+            $date_parts[$language]['days_short'][] = get_lang($week_day[$i].'Short', '', $language);
+            $date_parts[$language]['days_long'][] = get_lang($week_day[$i].'Long', '', $language);
+        }
+        for ($i = 0; $i < 12; $i++) {
+            $date_parts[$language]['months_short'][] = get_lang($month[$i].'Short', '', $language);
+            $date_parts[$language]['months_long'][] = get_lang($month[$i].'Long', '', $language);
+        }
+    }
+    return $date_parts[$language];
+}
+
+/**
+ * Returns returns person name convention for a given language.
+ * @param string $language	The input language.
+ * @param string $type		The type of the requested convention.
+ * It may be 'format' for name order convention or 'sort_by' for name sorting convention.
+ * @return mixed Depending of the requested type,
+ * the returned result may be string or boolean; null is returned on error;
+ */
+function _api_get_person_name_convention($language, $type)
+{
+    static $conventions;
+    $language = api_purify_language_id($language);
+    if (!isset($conventions)) {
+        $file = dirname(__FILE__).'/internationalization_database/name_order_conventions.php';
+        if (file_exists($file)) {
+            $conventions = include ($file);
+        } else {
+            $conventions = array(
+                'english' => array(
+                    'format' => 'title first_name last_name',
+                    'sort_by' => 'first_name'
+                )
+            );
+        }
+        // Overwrite classic conventions
+        $customConventions = api_get_configuration_value('name_order_conventions');
+
+        if (!empty($customConventions)) {
+            foreach ($customConventions as $key => $data) {
+                $conventions[$key] = $data;
+            }
+        }
+
+        $search1 = array('FIRST_NAME', 'LAST_NAME', 'TITLE');
+        $replacement1 = array('%F', '%L', '%T');
+        $search2 = array('first_name', 'last_name', 'title');
+        $replacement2 = array('%f', '%l', '%t');
+        foreach (array_keys($conventions) as $key) {
+            $conventions[$key]['format'] = str_replace($search1, $replacement1, $conventions[$key]['format']);
+            $conventions[$key]['format'] = _api_validate_person_name_format(_api_clean_person_name(str_replace('%', ' %', str_ireplace($search2, $replacement2, $conventions[$key]['format']))));
+            $conventions[$key]['sort_by'] = strtolower($conventions[$key]['sort_by']) != 'last_name' ? true : false;
+        }
+    }
+    switch ($type) {
+        case 'format':
+            return is_string($conventions[$language]['format']) ? $conventions[$language]['format'] : '%t %f %l';
+        case 'sort_by':
+            return is_bool($conventions[$language]['sort_by']) ? $conventions[$language]['sort_by'] : true;
+    }
+    return null;
+}
+
+/**
+ * Replaces non-valid formats for person names with the default (English) format.
+ * @param string $format	The input format to be verified.
+ * @return bool				Returns the same format if is is valid, otherwise returns a valid English format.
+ */
+function _api_validate_person_name_format($format) {
+    if (empty($format) || stripos($format, '%f') === false || stripos($format, '%l') === false) {
+        return '%t %f %l';
+    }
+    return $format;
+}
+
+/**
+ * Removes leading, trailing and duplicate whitespace and/or commas in a full person name.
+ * Cleaning is needed for the cases when not all parts of the name are available or when the name is constructed using a "dirty" pattern.
+ * @param string $person_name	The input person name.
+ * @return string				Returns cleaned person name.
+ */
+function _api_clean_person_name($person_name) {
+    return preg_replace(array('/\s+/', '/, ,/', '/,+/', '/^[ ,]/', '/[ ,]$/'), array(' ', ', ', ',', '', ''), $person_name);
+}
+
+/**
+ * Appendix to "Multibyte string conversion functions"
  */
 
-require_once dirname(__FILE__).'/internationalization_internal.lib.php';
+/**
+ * This is a php-implementation of a function that is similar to mb_convert_encoding() from mbstring extension.
+ * The function converts a given string from one to another character encoding.
+ * @param string $string					The string being converted.
+ * @param string $to_encoding				The encoding that $string is being converted to.
+ * @param string $from_encoding				The encoding that $string is being converted from.
+ * @return string							Returns the converted string.
+ */
+function _api_convert_encoding(&$string, $to_encoding, $from_encoding)
+{
+    return mb_convert_encoding($string, $to_encoding, $from_encoding);
+}
+
+/**
+ * This function determines the name of corresponding to a given encoding conversion table.
+ * It is able to deal with some aliases of the encoding.
+ * @param string $encoding		The given encoding identificator, for example 'WINDOWS-1252'.
+ * @return string				Returns the name of the corresponding conversion table, for the same example - 'CP1252'.
+ */
+function _api_get_character_map_name($encoding) {
+    static $character_map_selector;
+    if (!isset($character_map_selector)) {
+        $file = dirname(__FILE__).'/internationalization_database/conversion/character_map_selector.php';
+        if (file_exists($file)) {
+            $character_map_selector = include ($file);
+        } else {
+            $character_map_selector = array();
+        }
+    }
+    return isset($character_map_selector[$encoding]) ? $character_map_selector[$encoding] : '';
+}
+
+/**
+ * Appendix to "String comparison"
+ */
+
+/**
+ * A reverse function from php-core function strnatcmp(), performs string comparison in reverse natural (alpha-numerical) order.
+ * @param string $string1		The first string.
+ * @param string $string2		The second string.
+ * @return int					Returns 0 if $string1 = $string2; >0 if $string1 < $string2; <0 if $string1 > $string2.
+ */
+function _api_strnatrcmp($string1, $string2) {
+    return strnatcmp($string2, $string1);
+}
+
+/**
+ * ICU locales (accessible through intl extension).
+ */
+
+/**
+ * Appendix to "Encoding management functions"
+ */
+
+/**
+ * Sets/Gets internal character encoding of the common string functions within the PHP mbstring extension.
+ * @param string $encoding (optional)	When this parameter is given, the function sets the internal encoding.
+ * @return string						When $encoding parameter is not given, the function returns the internal encoding.
+ * Note: This function is used in the global initialization script for setting the internal encoding to the platform's character set.
+ * @link http://php.net/manual/en/function.mb-internal-encoding
+ */
+function _api_mb_internal_encoding($encoding = null)
+{
+    return mb_internal_encoding($encoding);
+}
+
+/**
+ * Checks whether the specified encoding is supported by the PHP mbstring extension.
+ * @param string $encoding	The specified encoding.
+ * @return bool				Returns TRUE when the specified encoding is supported, FALSE othewise.
+ */
+function _api_mb_supports($encoding) {
+    static $supported = array();
+    if (!isset($supported[$encoding])) {
+        if (MBSTRING_INSTALLED) {
+            $supported[$encoding] = api_equal_encodings($encoding, mb_list_encodings(), true);
+        } else {
+            $supported[$encoding] = false;
+        }
+    }
+    return $supported[$encoding];
+}
+
+/**
+ * Checks whether the specified encoding is supported by the PHP iconv extension.
+ * @param string $encoding	The specified encoding.
+ * @return bool				Returns TRUE when the specified encoding is supported, FALSE othewise.
+ */
+function _api_iconv_supports($encoding) {
+    static $supported = array();
+    if (!isset($supported[$encoding])) {
+        if (ICONV_INSTALLED) {
+            $enc = api_refine_encoding_id($encoding);
+            if ($enc != 'HTML-ENTITIES') {
+                $test_string = '';
+                for ($i = 32; $i < 128; $i++) {
+                    $test_string .= chr($i);
+                }
+                $supported[$encoding] = (@iconv_strlen($test_string, $enc)) ? true : false;
+            } else {
+                $supported[$encoding] = false;
+            }
+        } else {
+            $supported[$encoding] = false;
+        }
+    }
+    return $supported[$encoding];
+}
+
+// This function checks whether the function _api_convert_encoding() (the php-
+// implementation) is able to convert from/to a given encoding.
+function _api_convert_encoding_supports($encoding) {
+    static $supports = array();
+    if (!isset($supports[$encoding])) {
+        $supports[$encoding] = _api_get_character_map_name(api_refine_encoding_id($encoding)) != '';
+    }
+    return $supports[$encoding];
+}
+
+

2fdb8e62a8eb8a9f37fb9fe31f1d16f5788665f1
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-04-28 12:06:49 -0500

    User Bootstrap modal for show exercise results - refs #7611

--- main/exercice/overview.php
+++ main/exercice/overview.php
@@ -163,7 +163,7 @@ if (!empty($attempts) && $visible_return['value'] == true) {
             $attempt_result['exe_result'],
             $attempt_result['exe_weighting']
         );
-        $attempt_url = api_get_path(WEB_CODE_PATH) . 'exercice/result.php?' . api_get_cidreq() . '&id=' . $attempt_result['exe_id'] . '&height=500&width=950' . $url_suffix;
+        $attempt_url = api_get_path(WEB_CODE_PATH) . 'exercice/result.php?' . api_get_cidreq() . '&id=' . $attempt_result['exe_id'] . '&modal_size=lg' . $url_suffix;
         $attempt_link = Display::url(
             get_lang('Show'),
             $attempt_url,

f61a34f7054f80c78bcaf2374c790d017c0e197a
Julio Montoya <gugli100@gmail.com>
2015-05-19 14:05:54 +0200

    Add sequence CRUD see BT#9893

---
+++ src/Chamilo/CoreBundle/Entity/Manager/SequenceManager.php
@@ -0,0 +1,17 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Entity\Manager;
+
+use Sonata\CoreBundle\Model\BaseEntityManager;
+
+/**
+ * Class SequenceManager
+ * CRUD for the course
+ *
+ * @package Chamilo\CoreBundle\Entity\Repository
+ */
+class SequenceManager extends BaseEntityManager
+{
+
+}

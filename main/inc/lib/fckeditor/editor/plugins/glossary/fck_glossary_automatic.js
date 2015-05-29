148399581c470debbc558a58ed8676741411be95
Julio Montoya <gugli100@gmail.com>
2015-05-29 08:56:56 +0200

    Fix glossary ajax URLs see BT#8187

--- main/inc/lib/fckeditor/editor/plugins/glossary/fck_glossary_automatic.js
+++ main/inc/lib/fckeditor/editor/plugins/glossary/fck_glossary_automatic.js
@@ -5,33 +5,35 @@
 *    document/showinframes.php
 *    newscorm/lp_view.php
 *    newscorm/scorm_api.php
-*    
+*
 */
 
 /*$(document).ready(function() {
     $(window).load(function() {
-	*/	
+	*/
     my_protocol = location.protocol;
     my_pathname=location.pathname;
     work_path = my_pathname.substr(0,my_pathname.indexOf('/courses/'));
+    var glossaryAjaxUrl = my_protocol+"//"+location.host+work_path+"/main/glossary/glossary_ajax_request.php";
+    var mainUrl = my_protocol+"//"+location.host+work_path+"/main/";
 
     $.ajax({
         contentType: "application/x-www-form-urlencoded",
         beforeSend: function(content_object) {},
         type: "POST",
-        url: my_protocol+"//"+location.host+work_path+"/main/glossary/glossary_ajax_request.php",
+        url: glossaryAjaxUrl,
         data: "glossary_data=true",
         success: function(datas) {
             if (datas.length==0) {
                 return false;
             }
-            // glossary terms 
+            // glossary terms
             data_terms=datas.split("[|.|_|.|-|.|]");
             var complex_array = new Array();
             var cp_complex_array = new Array();
             for(i=0;i<data_terms.length;i++) {
                 specific_terms=data_terms[i].split("__|__|");
-                var real_term = specific_terms[1]; // glossary term 
+                var real_term = specific_terms[1]; // glossary term
                 var real_code = specific_terms[0]; // glossary id
                 complex_array[real_code] = real_term;
                 cp_complex_array[real_code] = real_term;
@@ -55,8 +57,8 @@
                         }
                     }
                     //alert(n + ' ' + my_index);
-                    $('body').removeHighlight().highlight(n,my_index) 
-                }                   
+                    $('body').removeHighlight().highlight(n,my_index)
+                }
             }
 
             var complex_array = new Array();
@@ -67,19 +69,19 @@
             //mouse on click
             $("body").on("click", ".glossary-ajax", function(e) {
                 random_id = Math.round(Math.random()*100);
-                
+
                 //div_show_id = "div_show_id"+random_id;
                 //div_content_id = "div_content_id"+random_id;
                 div_show_id = "div_show_id";
                 div_content_id = "div_content_id";
-                
+
                 $(this).append("<div id="+div_show_id+"><div id="+div_content_id+">&nbsp;</div></div>");
-                
+
                 //$("div#"+div_show_id).attr("style","z-index:99;display:inline;float:left;position:absolute;background-color:#F2F2F2;border-bottom: 1px solid #2E2E2E;border-right: 1px solid #2E2E2E;border-left: 1px solid #2E2E2E;border-top: 1px solid #2E2E2E;color:#305582;margin-left:5px;margin-right:5px;");
                 //$("div#"+div_content_id).attr("style","z-index:99;background-color:#F2F2F2;color:#0B3861;margin-left:8px;margin-right:8px;margin-top:5px;margin-bottom:5px;");
-                
-                var $target = $(this);                
-                
+
+                var $target = $(this);
+
                 $("#"+div_show_id).dialog("destroy");
                 $("#"+div_show_id).dialog({
                     autoOpen: false,
@@ -91,25 +93,25 @@
                          $("div#"+div_content_id).remove();
                     }
                 });
-                
+
                 notebook_id = $(this).attr("name");
                 data_notebook = notebook_id.split("link");
                 my_glossary_id = data_notebook[1];
-                
+
                 $.ajax({
                     contentType: "application/x-www-form-urlencoded",
                     beforeSend: function(content_object) {
-                        $("div#"+div_content_id).html("<img src='../../../../../../../main/inc/lib/javascript/indicator.gif' />");
+                        $("div#"+div_content_id).html("<img src='"+mainUrl+"inc/lib/javascript/indicator.gif' />");
                     },
                     type: "POST",
-                    url: "../../../../../../../main/glossary/glossary_ajax_request.php",
+                    url: glossaryAjaxUrl,
                     data: "glossary_id="+my_glossary_id,
                     success: function(datas) {
                         $("div#"+div_content_id).html(datas);
                         $("#"+div_show_id).dialog("open");
                     }
                 });
-                
+
             });
 
             //mouse out event
@@ -118,18 +120,18 @@
                 div_show_id=current_element.find("div").attr("id");
                 $("div#"+div_show_id).remove();
             });*/
-            
+
             //mouse click
-            
+
             /*$("body").on("click", ".glossary-ajax", function(){
                 var current_element = $(this);
                 div_show_id=current_element.find("div").attr("id");
                 $("div#"+div_show_id).remove();
             });*/
-            
+
         }
 
     });
-/*			
+/*
         });
 });*/

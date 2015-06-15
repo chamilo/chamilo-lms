7c186aa6b7641f648cd3ed94b035b941fd8ca21a
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-06-15 17:29:21 -0500

    Get course coach using entities - refs BT#9889 #TMI

--- src/Chamilo/CoreBundle/Entity/Repository/SessionRepository.php
+++ src/Chamilo/CoreBundle/Entity/Repository/SessionRepository.php
@@ -5,6 +5,7 @@ namespace Chamilo\CoreBundle\Entity\Repository;
 use Doctrine\ORM\EntityRepository;
 use Chamilo\CoreBundle\Entity\Session;
 use \Doctrine\ORM\Query\Expr\Join;
+use Chamilo\CoreBundle\Entity\Course;
 
 /**
  * SessionRepository
@@ -41,6 +42,40 @@ class SessionRepository extends EntityRepository
                 $queryBuilder->expr()->eq('s.id', $session->getId())
             )
             ->orderBy('src.position');
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * Get the coach for a course in session
+     * @param Session $session The session
+     * @param Course $course The course
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function getCourseCoachesForCoach(Session $session, Course $course)
+    {
+        $queryBuilder = $this->createQueryBuilder('s');
+
+        $queryBuilder->select('u')
+            ->innerJoin('s.courses', 'session_courses')
+            ->innerJoin(
+                'ChamiloCoreBundle:SessionRelCourseRelUser',
+                'scu',
+                Join::WITH,
+                'session_courses.course = scu.course'
+            )
+            ->innerJoin(
+                'ChamiloUserBundle:User',
+                'u',
+                Join::WITH,
+                'scu.user = u.id'
+            )
+            ->where(
+                $queryBuilder->expr()->andX(
+                    $queryBuilder->expr()->eq('s.id', $session->getId()),
+                    $queryBuilder->expr()->eq('scu.course', $course->getId())
+                )
+            );
 
         return $queryBuilder->getQuery()->getResult();
     }

3c7892b19011f232c0762d3f0484e84b89abf039
Julio Montoya <gugli100@gmail.com>
2015-07-07 13:25:54 +0200

    Add text plugin (show a text in a region).

---
+++ plugin/text/plugin.php
@@ -0,0 +1,39 @@
+<?php
+/* See license terms in /license.txt */
+
+/**
+ * This script is a configuration file for the date plugin.
+ * You can use it as a master for other platform plugins (course plugins are slightly different).
+ * These settings will be used in the administration interface for plugins (Chamilo configuration settings->Plugins)
+ * @package chamilo.plugin
+ * @author Julio Montoya <gugli100@gmail.com>
+ */
+
+/* Plugin config */
+
+//the plugin title
+$plugin_info['title'] = 'Text';
+//the comments that go with the plugin
+$plugin_info['comment'] = "Displays a text message";
+//the plugin version
+$plugin_info['version'] = '1.0';
+//the plugin author
+$plugin_info['author'] = 'Julio Montoya';
+
+/* Plugin optional settings */
+
+$form = new FormValidator('text_form');
+
+// A simple select$plugin_info['settings']
+$form->add_textarea('content', get_lang('Content'));
+$form->addElement('style_submit_button', 'submit_button', get_lang('Save'));
+
+$content = '';
+$setting = api_get_setting('text_content');
+if (isset($setting) && is_array($setting) && isset($setting['text'])) {
+    $content = $setting['text'];
+}
+
+$form->setDefaults(array('content' => $content));
+
+$plugin_info['settings_form'] = $form;
---
+++ plugin/text/readme.txt
@@ -0,0 +1,3 @@
+README
+
+Display a text in a region.

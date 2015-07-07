61a6a2805232244bd0fa1f397019de3be457f770
Yannick Warnier <ywarnier@beeznest.org>
2015-07-07 18:40:22 -0500

    Update languages

--- main/lang/english/trad4all.inc.php
+++ main/lang/english/trad4all.inc.php
@@ -7403,4 +7403,5 @@ $ByDates = "By dates";
 $SendAnnouncementCopyToDRH = "Send a copy to HR managers of selected students";
 $PoweredByX = "Powered by %s";
 $AnnouncementErrorNoUserSelected = "Please select at least one user. The announcement has not been saved.";
+$NoDependencies = "No dependencies";
 ?>

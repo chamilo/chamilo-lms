fbed57afb9ffb9ed8f38bfb31948c17f0f9f8fa4
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-07-14 11:49:24 -0500

    Minor - Add PHPDoc - refs BT#10092 #TMI

--- src/Chamilo/UserBundle/Entity/User.php
+++ src/Chamilo/UserBundle/Entity/User.php
@@ -1500,6 +1500,10 @@ class User extends BaseUser //implements ParticipantInterface, ThemeUser
         return null;
     }
 
+    /**
+     * Get sessionCourseSubscription
+     * @return ArrayCollection
+     */
     public function getSessionCourseSubscriptions()
     {
         return $this->sessionCourseSubscriptions;

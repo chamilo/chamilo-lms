b35addf5fa3a0aba7f535431cb91c46f77a9f176
Yannick Warnier <ywarnier@beeznest.org>
2015-08-04 08:36:56 -0500

    Remove config.php

--- tests/migrations/claroline/1.11.10/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Store your Claroline database access settings here
-//$sourceHost = 'localhost';
-//$sourcePort = '3306';
-//$sourceUser = 'claroline';
-//$sourcePass = 'claroline';
-//$sourceDB   = 'claroline';

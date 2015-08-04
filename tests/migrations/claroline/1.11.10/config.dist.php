1b699fd48ee8b21ba8c44d45da84eeea5dc010d5
Yannick Warnier <ywarnier@beeznest.org>
2015-08-04 09:48:11 -0500

    Update source config default file

--- tests/migrations/claroline/1.11.10/config.dist.php
+++ tests/migrations/claroline/1.11.10/config.dist.php
@@ -5,3 +5,5 @@
 //$sourceUser = 'claroline';
 //$sourcePass = 'claroline';
 //$sourceDB   = 'claroline';
+//$sourceSingle = TRUE; //or FALSE. Find it in claroline/platform/conf/claro_main.conf.php as $GLOBALS['singleDbEnabled']
+//$sourcePref = 'cl_'; //tables prefix

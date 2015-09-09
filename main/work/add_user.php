dea1b772ffd2815b399c51fe3a3806a1a2b595f7
aragonc <aragcar@gmail.com>
2015-09-09 14:35:46 -0500

    fix add user works

--- main/work/add_user.php
+++ main/work/add_user.php
@@ -68,16 +68,16 @@ $items = getAllUserToWork($workId, api_get_course_int_id());
 $usersAdded = array();
 if (!empty($items)) {
     echo Display::page_subheader(get_lang('UsersAdded'));
-    echo '<div class="well">';
+    echo '<ul class="list-group">';
     foreach ($items as $data) {
         $myUserId = $data['user_id'];
         $usersAdded[] = $myUserId;
         $userInfo = api_get_user_info($myUserId);
         $url = api_get_path(WEB_CODE_PATH).'work/add_user.php?action=delete&id='.$workId.'&user_id='.$myUserId;
-        $link = Display::url(get_lang('Delete'), $url, array('class' => 'btn btn-danger'));
-        echo $userInfo['complete_name_with_username'].' '.$link.'<br />';
+        $link = Display::url('<i class="fa fa-trash"></i> ' . get_lang('Delete'), $url, array('class' => 'btn btn-danger btn-sm'));
+        echo '<li class="list-group-item">' . $userInfo['complete_name_with_username'] . '<div class="pull-right">' . $link . '</div></li>';
     }
-    echo '</div>';
+    echo '</ul>';
 }
 
 
@@ -104,14 +104,14 @@ foreach ($userList as $user) {
 
 if (!empty($userToAddList)) {
     echo Display::page_subheader(get_lang('UsersToAdd'));
-    echo '<div class="well">';
+    echo '<ul class="list-group">';
     foreach ($userToAddList as $user) {
         $userName = api_get_person_name($user['firstname'], $user['lastname']).' ('.$user['username'].') ';
         $url = api_get_path(WEB_CODE_PATH).'work/add_user.php?action=add&id='.$workId.'&user_id='.$user['user_id'];
-        $link = Display::url(get_lang('Add'), $url, array('class' => 'btn btn-primary'));
-        echo $userName.' '.$link.'<br />';
+        $link = Display::url('<i class="fa fa-plus"></i> ' . get_lang('Add'), $url, array('class' => 'btn btn-primary btn-sm'));
+        echo '<li class="list-group-item">' . $userName . '<div class="pull-right"> ' . $link . '</div></li>';
     }
-    echo '</div>';
+    echo '</ul>';
 } else {
     Display::display_warning_message(get_lang('NoUsersToAdd'));
 }

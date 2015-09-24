fa29e02ac31a3461ed002e32ec04b650b0ba1479
José Loguercio <jloguercio.work@gmail.com>
2015-09-24 10:39:39 -0500

    Remove chat button from elfinder popup file selector for 1.10.x - Refs #7841

--- main/inc/lib/elfinder/filemanager.php
+++ main/inc/lib/elfinder/filemanager.php
@@ -2,6 +2,6 @@
 /* For licensing terms, see /license.txt */
 
 require_once '../../global.inc.php';
-
+Chat::setDisableChat();
 $template = new Template();
 $template->display('default/javascript/editor/ckeditor/elfinder.tpl');

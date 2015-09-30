4d9891bca4565ba52edd9854b1a849de8cd73c21
José Loguercio <jloguercio.work@gmail.com>
2015-09-30 12:27:18 -0500

    Adicional Fix for unsubscribe course (hot courses) - Refs #7850

--- main/auth/courses.php
+++ main/auth/courses.php
@@ -156,8 +156,9 @@ if (isset($_REQUEST['subscribe_course'])) {
 }
 // We are unsubscribing from a course (=Unsubscribe from course).
 if (isset($_GET['unsubscribe'])) {
+    $search_term = isset($_GET['searh_term']) ? intval($_GET['searh_term']) : null;
     if ($ctok == $_GET['sec_token']) {
-        $courses_controller->unsubscribe_user_from_course($_GET['unsubscribe'], $_GET['search_term'], $categoryCode);
+        $courses_controller->unsubscribe_user_from_course($_GET['unsubscribe'], $search_term, $categoryCode);
             //$message = remove_user_from_course($_user['user_id'], $_POST['unsubscribe']);
     }
 }

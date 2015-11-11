63b62b4bd0b00c1fde2a75affd9f689ff824c523
aragonc <aragcar@gmail.com>
2015-11-11 12:08:47 -0500

    fix bug list catalog course CT#7945

--- main/inc/lib/course.lib.php
+++ main/inc/lib/course.lib.php
@@ -3270,7 +3270,6 @@ class CourseManager
             $html .= $params['icon'];
             $html .= '</div>';
         }
-
         $html .= '</div>';
         $notifications = isset($params['notifications']) ? $params['notifications'] : '';
         $param_class = isset($params['class']) ? $params['class'] : '';

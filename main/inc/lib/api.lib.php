a6847125c8c9b3e43136041ed1debb571ec78e5f
aragonc <aragcar@gmail.com>
2015-11-11 14:54:59 -0500

    fix bug

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -1901,7 +1901,7 @@ function api_format_course_array($course_data)
     if (array_key_exists('add_teachers_to_sessions_courses', $course_data)) {
         $_course['add_teachers_to_sessions_courses'] = $course_data['add_teachers_to_sessions_courses'];
     }
-    var_dump($course_data['directory']);
+    
     if (file_exists(api_get_path(SYS_COURSE_PATH).$course_data['directory'].'/course-pic85x85.png')) {
         $url_image = api_get_path(WEB_COURSE_PATH).$course_data['directory'].'/course-pic85x85.png';
     } else {

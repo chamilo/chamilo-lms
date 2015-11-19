92c5d94ce9b27db8e2e3ef8a320031536edf78c1
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2015-11-19 10:27:52 -0500

    Fix edit lp item - refs #7972

--- main/newscorm/lp_edit_item.php
+++ main/newscorm/lp_edit_item.php
@@ -25,7 +25,25 @@ include 'resourcelinker.inc.php';
 /* Header and action code */
 
 $htmlHeadXtra[] = '
-<script>'.$_SESSION['oLP']->get_js_dropdown_array().'
+<script>'.$_SESSION['oLP']->get_js_dropdown_array().
+
+'function load_cbo(id){' ."\n" .
+  'if (!id) {return false;}'.
+  'var cbo = document.getElementById(\'previous\');' .
+  'for(var i = cbo.length - 1; i > 0; i--) {' .
+    'cbo.options[i] = null;' .
+  '}' ."\n" .
+  'var k=0;' .
+  'for(var i = 1; i <= child_name[id].length; i++){' ."\n" .
+  '  cbo.options[i] = new Option(child_name[id][i-1], child_value[id][i-1]);' ."\n" .
+  '  k=i;' ."\n" .
+  '}' ."\n" .
+  //'if( typeof cbo != "undefined" ) {'."\n" .
+  'cbo.options[k].selected = true;'."\n" .
+   //'}'."\n" .
+
+   '$(\'#previous\').selectpicker(\'refresh\');' .
+'}
 
 $(document).on("ready", function() {
     CKEDITOR.on("instanceReady", function (e) {

d9b4c01a8341f888cd04eb88109fadc5789730fd
Yannick Warnier <ywarnier@beeznest.org>
2016-01-07 12:47:23 -0500

    Avoid notice message for reverse proxies with only 1 IP.

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -6797,7 +6797,11 @@ function api_get_real_ip(){
     global $debug;
     $ip = trim($_SERVER['REMOTE_ADDR']);
     if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        list($ip1, $ip2) = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        if (preg_match('/,/', $_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            list($ip1, $ip2) = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        } else {
+            $ip1 = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
         $ip = trim($ip1);
     }
     if (!empty($debug)) error_log('Real IP: '.$ip);

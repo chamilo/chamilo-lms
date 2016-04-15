f1db277f1ac8565266006ceba4309710af1d8d6d
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2016-04-15 12:22:37 -0500

    Fix logout with facebook - refs #8156

--- main/auth/external_login/facebook.init.php
+++ main/auth/external_login/facebook.init.php
@@ -22,5 +22,5 @@
  * See facebook section of the auth.conf.php file
  */
 
-require_once dirname(__FILE__) . '/../../../app/config/auth.conf.php';
+require dirname(__FILE__) . '/../../../app/config/auth.conf.php';
 

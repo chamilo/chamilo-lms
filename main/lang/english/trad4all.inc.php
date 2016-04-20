135fc316a8cdab1cfbe9d04c07a68f89748e8417
Julio <gugli100@gmail.com>
2016-04-20 08:25:45 +0200

    Update lang files

--- main/lang/english/trad4all.inc.php
+++ main/lang/english/trad4all.inc.php
@@ -7638,4 +7638,9 @@ $EnableForumAutoLaunch = "Enable forum auto-launch";
 $NowDownloadYourCertificateClickHere = "You can now download your certificate by clicking here";
 $AdditionallyYouHaveObtainedTheFollowingSkills = "Additionally, you have achieved the following skills";
 $IHaveObtainedSkillXOnY = "I have achieved skill %s on %s";
+$AnotherAttempt = "Another attempt";
+$RemainingXAttempts = "Remaining %d attempts";
+$Map = "Map";
+$MyLocation = "My location";
+$ShowCourseInUserLanguage = "Show course in user's language";
 ?>

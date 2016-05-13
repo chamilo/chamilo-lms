8664e704272aa6ddae277d28d5764cd6821ad0a7
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2016-05-13 16:00:56 -0500

    Remove E_NOTICE on installer when no connection

--- main/install/install.lib.php
+++ main/install/install.lib.php
@@ -1438,26 +1438,19 @@ function display_database_settings_form(
             $database_exists_text = $e->getMessage();
         }
 
-        if ($manager->getConnection()->isConnected()): ?>
-
-            <?php echo $database_exists_text ?>
-            <div id="db_status" class="alert alert-success">
-                Database host: <strong><?php echo $manager->getConnection()->getHost(); ?></strong><br />
-                Database port: <strong><?php echo $manager->getConnection()->getPort(); ?></strong><br />
-                Database driver: <strong><?php echo $manager->getConnection()->getDriver()->getName(); ?></strong><br />
-
-            </div>
-
-        <?php else: ?>
-
-            <?php echo $database_exists_text ?>
-            <div id="db_status" style="float:left;" class="alert alert-danger">
-                <div style="float:left;">
-                    <?php echo get_lang('FailedConectionDatabase'); ?></strong>
-                </div>
-            </div>
-
-        <?php endif; ?>
+    if ($manager && $manager->getConnection()->isConnected()): ?>
+        <?php echo $database_exists_text ?>
+        <div id="db_status" class="alert alert-success">
+            Database host: <strong><?php echo $manager->getConnection()->getHost(); ?></strong><br/>
+            Database port: <strong><?php echo $manager->getConnection()->getPort(); ?></strong><br/>
+            Database driver: <strong><?php echo $manager->getConnection()->getDriver()->getName(); ?></strong><br/>
+        </div>
+    <?php else: ?>
+        <div id="db_status" class="alert alert-danger">
+            <p><?php echo get_lang('FailedConectionDatabase'); ?></strong></p>
+            <code><?php echo $database_exists_text ?></code>
+        </div>
+    <?php endif; ?>
    <div class="form-group">
        <div class="col-sm-6">
            <button type="submit" name="step2" class="btn btn-default pull-right" value="&lt; <?php echo get_lang('Previous'); ?>" >

ee874accf2a186902a8815a5e45244bf381f2b98
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2016-05-20 10:42:05 -0500

    Revert "Fix questions list for calculated answers - refs #8237"
    This reverts commit 4b006900f470727ebc25794bdc36a27fd1ed98d8.

--- main/inc/lib/exercise.lib.php
+++ main/inc/lib/exercise.lib.php
@@ -89,13 +89,6 @@ class ExerciseLib
             $objAnswerTmp = new Answer($questionId);
             $nbrAnswers = $objAnswerTmp->selectNbrAnswers();
 
-            if ($answerType == FREE_ANSWER ||
-                $answerType == ORAL_EXPRESSION ||
-                $answerType == CALCULATED_ANSWER
-            ) {
-                $nbrAnswers = 1;
-            }
-
             $quiz_question_options = Question::readQuestionOption(
                 $questionId,
                 $course_id

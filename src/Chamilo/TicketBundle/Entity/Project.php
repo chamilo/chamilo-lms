008a8a7f19bba6d2a2c11d17c238748a522bbb1b
jmontoyaa <gugli100@gmail.com>
2016-05-31 15:20:19 +0200

    Add setters/getters

--- src/Chamilo/TicketBundle/Entity/Project.php
+++ src/Chamilo/TicketBundle/Entity/Project.php
@@ -59,7 +59,7 @@ class Project
      * @ORM\Column(name="sys_insert_user_id", type="integer", nullable=false, unique=false)
      */
     protected $insertUserId;
-    
+
     /**
      * @var \DateTime
      *
@@ -80,4 +80,185 @@ class Project
      * @ORM\Column(name="sys_lastedit_datetime", type="datetime", nullable=true, unique=false)
      */
     protected $lastEditDateTime;
+
+    /**
+     * Project constructor.
+     */
+    public function __construct()
+    {
+        $this->insertDateTime = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Project
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Project
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Project
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Project
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOtherArea()
+    {
+        return $this->otherArea;
+    }
+
+    /**
+     * @param string $otherArea
+     * @return Project
+     */
+    public function setOtherArea($otherArea)
+    {
+        $this->otherArea = $otherArea;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getInsertUserId()
+    {
+        return $this->insertUserId;
+    }
+
+    /**
+     * @param int $insertUserId
+     * @return Project
+     */
+    public function setInsertUserId($insertUserId)
+    {
+        $this->insertUserId = $insertUserId;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getInsertDateTime()
+    {
+        return $this->insertDateTime;
+    }
+
+    /**
+     * @param \DateTime $insertDateTime
+     * @return Project
+     */
+    public function setInsertDateTime($insertDateTime)
+    {
+        $this->insertDateTime = $insertDateTime;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastEditUserId()
+    {
+        return $this->lastEditUserId;
+    }
+
+    /**
+     * @param int $lastEditUserId
+     * @return Project
+     */
+    public function setLastEditUserId($lastEditUserId)
+    {
+        $this->lastEditUserId = $lastEditUserId;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastEditDateTime()
+    {
+        return $this->lastEditDateTime;
+    }
+
+    /**
+     * @param \DateTime $lastEditDateTime
+     * @return Project
+     */
+    public function setLastEditDateTime($lastEditDateTime)
+    {
+        $this->lastEditDateTime = $lastEditDateTime;
+
+        return $this;
+    }
+
+
 }
--- src/Chamilo/TicketBundle/Entity/Status.php
+++ src/Chamilo/TicketBundle/Entity/Status.php
@@ -45,4 +45,82 @@ class Status
      * @ORM\Column(name="description", type="text", nullable=true)
      */
     protected $description;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Status
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     * @return Status
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Status
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Status
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+    
+    
 }

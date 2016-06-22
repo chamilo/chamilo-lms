c38007690995d0a002ed7143e24e07ee65bf09f2
jmontoyaa <gugli100@gmail.com>
2016-06-22 11:04:36 +0200

    Remove default values

--- main/auth/inscription.php
+++ main/auth/inscription.php
@@ -514,7 +514,7 @@ if ($user_already_registered_show_terms === false) {
 
 
     // EXTRA FIELDS
-    if (in_array('extra_fields', $allowedFields)) {
+    if (array_key_exists('extra_fields', $allowedFields) || in_array('extra_fields', $allowedFields)) {
         $extraField = new ExtraField('user');
         $extraFieldList = is_array($allowedFields['extra_fields']) ? $allowedFields['extra_fields'] : [];
         $returnParams = $extraField->addElements($form);

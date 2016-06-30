7d8d209ad14e7ac55c1ec389b1e197449939207a
Julio <gugli100@gmail.com>
2016-06-30 10:48:17 +0200

    Use "throw new Exception"

--- main/inc/lib/pear/PEAR.php
+++ main/inc/lib/pear/PEAR.php
@@ -508,7 +508,7 @@ class PEAR
      * @see PEAR::setErrorHandling
      * @since PHP 4.0.5
      */
-    function &raiseError($message = null,
+    public static function &raiseError($message = null,
                          $code = null,
                          $mode = null,
                          $options = null,

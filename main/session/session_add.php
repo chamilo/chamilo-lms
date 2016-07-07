7edfb7e87ed8b48af5df2244046d2822452a5bc4
Julio <gugli100@gmail.com>
2016-07-07 15:04:51 +0200

    Format code

--- main/session/session_add.php
+++ main/session/session_add.php
@@ -190,16 +190,18 @@ $(function() {
     '.$result['js'].'
 });
 </script>';
-
+// @todo add an html element
 $form->addHtml('
     <div class="form-group">
-    label for="cropImage" id="labelCropImage" class="col-sm-2 control-label"></label>
+    <label for="cropImage" id="labelCropImage" class="col-sm-2 control-label"></label>
     <div class="col-sm-8">
     <div id="cropImage" class="cropCanvas">
     <img id="previewImage">
     </div>
     <div>
-    <button class="btn btn-primary hidden" name="cropButton" id="cropButton"><em class="fa fa-crop"></em> '.get_lang('CropYourPicture').'</button>
+    <button class="btn btn-primary hidden" name="cropButton" id="cropButton">
+        <em class="fa fa-crop"></em> '.get_lang('CropYourPicture').'
+    </button>
     </div>
     </div>
     </div>

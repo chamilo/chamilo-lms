3d793f4fa30055a96aceb9aba1e6c0f412389b9a
jmontoyaa <gugli100@gmail.com>
2016-07-12 14:25:39 +0200

    Remove ClassManager

--- tests/main/webservices/user_import/import.lib.test.php
+++ tests/main/webservices/user_import/import.lib.test.php
@@ -2,7 +2,6 @@
 require_once(api_get_path(SYS_CODE_PATH).'webservices/user_import/import.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'usermanager.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'import.lib.php');
-require_once(api_get_path(LIBRARY_PATH).'classmanager.lib.php');
 require_once(api_get_path(LIBRARY_PATH).'course.lib.php');
 
 

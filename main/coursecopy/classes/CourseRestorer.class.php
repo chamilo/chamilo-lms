f681741abc7042e0e8fc7d166d338e46d91769f1
jmontoyaa <gugli100@gmail.com>
2016-07-13 11:33:20 +0200

    Fix import see #8326

--- main/coursecopy/classes/CourseRestorer.class.php
+++ main/coursecopy/classes/CourseRestorer.class.php
@@ -1369,7 +1369,7 @@ class CourseRestorer
 				$sql = "DELETE FROM ".$tool_intro_table."
 				        WHERE
 				            c_id = ".$this->destination_course_id." AND
-				            id='".self::DBUTF8escapestring($tool_intro->id)."'";
+				            id = '".self::DBUTF8escapestring($tool_intro->id)."'";
 				Database::query($sql);
 
                 $tool_intro->intro_text = DocumentManager::replace_urls_inside_content_html_from_copy_course(
@@ -1389,8 +1389,9 @@ class CourseRestorer
 
                 $id = Database::insert($tool_intro_table, $params);
                 if ($id) {
-                    $sql = "UPDATE $tool_intro_table SET id = iid WHERE iid = $id";
-                    Database::query($sql);
+                    // id is a varchar not an int
+                    /*$sql = "UPDATE $tool_intro_table SET id = iid WHERE iid = $id";
+                    Database::query($sql);*/
 
                     if (!isset($this->course->resources[RESOURCE_TOOL_INTRO][$id])) {
                         $this->course->resources[RESOURCE_TOOL_INTRO][$id] = new stdClass();

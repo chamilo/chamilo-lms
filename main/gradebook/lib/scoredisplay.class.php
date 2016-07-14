b2a19350f90b39551cc535abb1160d1ef9661473
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2016-07-14 10:38:55 -0500

    Improve gradebook results table - refs #8339

--- main/gradebook/lib/scoredisplay.class.php
+++ main/gradebook/lib/scoredisplay.class.php
@@ -323,7 +323,7 @@ class ScoreDisplay
             $display = $this->display_default($my_score, $type);
         }
 
-        if ($this->coloring_enabled && $no_color == false) {
+        if ($this->coloring_enabled && $no_color != false) {
             $my_score_denom = isset($score[1]) && !empty($score[1]) ? $score[1] : 1;
             $scoreCleaned = isset($score[0]) ? $score[0] : 0;
             if (($scoreCleaned / $my_score_denom) < ($this->color_split_value / 100)) {

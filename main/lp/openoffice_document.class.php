5b1816fa70cd98dc84a5a2139b03dbc03dbee3ab
Yannick Warnier <ywarnier@beeznest.org>
2016-07-21 18:27:06 -0500

    Minor - Add parameters definition in Rapid PPT Converter to avoid warning

--- main/lp/openoffice_document.class.php
+++ main/lp/openoffice_document.class.php
@@ -156,6 +156,7 @@ abstract class OpenofficeDocument extends learnpath
             $this->lp_id = learnpath::add_lp($_course['id'], $this->file_name, '', 'guess', 'manual');
             // make sure we have a course code available for later
             $this->cc = $_course['id'];
+            $this->course_info = $_course;
 
             // Call to the function implemented by child following action_after_conversion parameter.
             switch ($action_after_conversion) {

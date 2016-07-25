c0846a93835c389bb0daae65520744169dce0afe
José Loguercio <jloguercio.work@gmail.com>
2016-07-25 18:06:27 -0500

    Fix prevent multiple simultaneous login (1.10.x) - Refs #8340

--- main/inc/lib/online.inc.php
+++ main/inc/lib/online.inc.php
@@ -57,7 +57,7 @@ function preventMultipleLogin($userId)
     $table = Database::get_main_table(TABLE_STATISTIC_TRACK_E_ONLINE);
     $userId = intval($userId);
 
-    if (api_get_settings('prevent_multiple_simultaneous_login') === 'true') {
+    if (api_get_setting('prevent_multiple_simultaneous_login') === 'true') {
         if (!empty($userId) && !api_is_anonymous()) {
 
             $isFirstLogin = Session::read('first_user_login');

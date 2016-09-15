0fdc16f525c4ece197fef58eaa029062a988b9ed
jmontoyaa <gugli100@gmail.com>
2016-09-15 08:48:31 +0200

    Fix query

--- main/forum/forumfunction.inc.php
+++ main/forum/forumfunction.inc.php
@@ -2103,8 +2103,7 @@ function get_thread_information($thread_id)
             ON (item_properties.ref = threads.thread_id AND threads.c_id = item_properties.c_id)
             WHERE
                 item_properties.tool= '".TOOL_FORUM_THREAD."' AND                
-                threads.id = ".$thread_id."
-            ";
+                threads.iid = $thread_id";
     $result = Database::query($sql);
     $row = Database::fetch_assoc($result);
 

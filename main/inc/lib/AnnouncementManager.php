0e3b1dbca2699c6c22a0e9c750b4a396023b74ad
José Loguercio <jloguercio.work@gmail.com>
2016-10-05 11:19:22 -0500

    Fix announcement attachment when you edit a announcement - Refs #8154

--- main/inc/lib/AnnouncementManager.php
+++ main/inc/lib/AnnouncementManager.php
@@ -1160,7 +1160,7 @@ class AnnouncementManager
             } else {
                 $new_file_name = uniqid('');
                 $new_path = $updir . '/' . $new_file_name;
-                @move_uploaded_file($file['tmp_name'], $new_path);
+                copy($file['tmp_name'], $new_path);
                 $safe_file_comment = Database::escape_string($file_comment);
                 $safe_file_name = Database::escape_string($file_name);
                 $safe_new_file_name = Database::escape_string($new_file_name);

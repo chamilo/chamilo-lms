1131c804432075973bbededbbedc1b4c4fba7725
José Loguercio <jloguercio.work@gmail.com>
2016-10-05 16:49:36 -0500

    Update AnnouncementManager.php

--- main/inc/lib/AnnouncementManager.php
+++ main/inc/lib/AnnouncementManager.php
@@ -1327,7 +1327,9 @@ class AnnouncementManager
             } else {
                 $new_file_name = uniqid('');
                 $new_path = $updir . '/' . $new_file_name;
-                @move_uploaded_file($file['tmp_name'], $new_path);
+		
+                // This file is copy here but its cleaned in api_mail_html in api.lib.php    
+                copy($file['tmp_name'], $new_path);
 
                 $params = [
                     'c_id' => $course_id,
@@ -1381,10 +1383,8 @@ class AnnouncementManager
                 Display :: display_error_message(get_lang('UplUnableToSaveFileFilteredExtension'));
             } else {
                 $new_file_name = uniqid('');
-                $new_path = $updir . '/' . $new_file_name;
-		
-		// This file is copy here but its cleaned in api_mail_html in api.lib.php    
-                copy($file['tmp_name'], $new_path);
+                $new_path = $updir . '/' . $new_file_name;  
+                @move_uploaded_file($file['tmp_name'], $new_path);
                 $safe_file_comment = Database::escape_string($file_comment);
                 $safe_file_name = Database::escape_string($file_name);
                 $safe_new_file_name = Database::escape_string($new_file_name);

e75f5d88881addeb0ddcf2cfa3054f2ffd978f94
jmontoyaa <gugli100@gmail.com>
2016-10-11 14:12:59 +0200

    Minor - format code + add message

--- main/group/group_space.php
+++ main/group/group_space.php
@@ -345,6 +345,7 @@ $my_gidreq = isset($_GET['gidReq']) ? Security::remove_XSS($_GET['gidReq']) : ''
 $parameters = array('cidReq' => $my_cidreq, 'origin'=> $my_origin, 'gidReq' => $my_gidreq);
 $table->set_additional_parameters($parameters);
 $table->set_header(0, '');
+
 if (api_is_western_name_order()) {
     $table->set_header(1, get_lang('FirstName'));
     $table->set_header(2, get_lang('LastName'));

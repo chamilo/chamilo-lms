ff0ff6c89253e04f8aaa0492b76a4b06dc075d48
jmontoyaa <gugli100@gmail.com>
2016-10-12 12:06:41 +0200

    Admin is allow to api_is_allowed_to_create_course()

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -2666,7 +2666,11 @@ function api_get_user_status($user_id = null)
  * @return boolean True if the user has course creation rights,
  * false otherwise.
  */
-function api_is_allowed_to_create_course() {
+function api_is_allowed_to_create_course()
+{
+    if (api_is_platform_admin()) {
+        return true;
+    }
     return Session::read('is_allowedCreateCourse');
 }
 

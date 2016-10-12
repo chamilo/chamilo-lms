4835cb52935e7169d8aef219a8563e0916f44d1c
jmontoyaa <gugli100@gmail.com>
2016-10-12 14:44:05 +0200

    Update ckeditor to version 4.5.11

--- app/Resources/public/assets/ckeditor/plugins/uicolor/lang/fr.js
+++ app/Resources/public/assets/ckeditor/plugins/uicolor/lang/fr.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("uicolor","fr",{title:"UI Sélecteur de couleur",preview:"Aperçu",config:"Collez cette chaîne de caractères dans votre fichier config.js",predefined:"Palettes de couleurs prédéfinies"});
+CKEDITOR.plugins.setLang("uicolor","fr",{title:"Sélecteur de couleur",preview:"Aperçu interactif",config:"Collez ce texte dans votre fichier config.js",predefined:"Palettes de couleurs prédéfinies"});

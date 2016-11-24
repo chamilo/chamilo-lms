a4c63ca5f1f371c8e30b149a83d3d375ed83734c
jmontoyaa <gugli100@gmail.com>
2016-11-24 12:12:35 +0100

    Fix slow query see BT#12023

--- main/exercice/TestCategory.php
+++ main/exercice/TestCategory.php
@@ -521,7 +521,7 @@ class TestCategory
         $sql = "SELECT DISTINCT qrc.question_id, qrc.category_id
                 FROM $TBL_QUESTION_REL_CATEGORY qrc
                 INNER JOIN $TBL_EXERCICE_QUESTION eq
-                ON (eq.question_id = qrc.question_id)
+                ON (eq.question_id = qrc.question_id AND eq.c_id = qrc.c_id)
                 INNER JOIN $categoryTable c
                 ON (c.id = qrc.category_id)
                 INNER JOIN $tableQuestion q

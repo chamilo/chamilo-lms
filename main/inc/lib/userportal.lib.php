038c6dab5e7dda21534d77ef5dbe2d16e4d35ed2
Nicolas Ducoulombier <nicolas@ldd.fr>
2016-11-25 22:11:48 +0100

    Displaying correct text for diagnosis bloc and diagnosis link -refs BT#10849

--- main/inc/lib/userportal.lib.php
+++ main/inc/lib/userportal.lib.php
@@ -1000,12 +1000,12 @@ class IndexManager
         $diagnosis = '';
         if (api_is_drh() || api_is_student_boss()) {
             $diagnosis = Display::url(get_lang('DiagnosisManagement'), api_get_path(WEB_PATH).'load_search.php').'<br />';
-            $diagnosis .= Display::url(get_lang('Diagnostic'), api_get_path(WEB_PATH).'search.php');
+            $diagnosis .= Display::url(get_lang('DiagnosticForm'), api_get_path(WEB_PATH).'search.php');
         } else {
             if (api_is_student()) {
                 if ($diagnosisComplete === false) {
                     $diagnosis = Display::url(
-                        get_lang('Diagnostic'),
+                        get_lang('DiagnosticForm'),
                         api_get_path(WEB_PATH).'search.php'
                     );
                 }

ee8ae89f86fee43778571071687fdaa0db252f4b
José Loguercio <jloguercio.work@gmail.com>
2016-12-13 11:23:46 -0500

    Update google maps plugin lang vars

--- plugin/google_maps/lang/spanish.php
+++ plugin/google_maps/lang/spanish.php
@@ -4,3 +4,5 @@ $strings['plugin_title'] = "Google Maps";
 $strings['plugin_comment'] = "Enable the functionality to show google maps";
 $strings['extra_field_name_help'] = "Ingrese aquí el nombre del campo extra de usuario del cual quiere obtener las localizaciones, siga el siguiente link para ver el mapa : <a href='". api_get_path(WEB_PLUGIN_PATH) . "google_maps/src/map_coordinates.php'>" . api_get_path(WEB_PLUGIN_PATH) . "google_maps/src/map_coordinates.php</a>";
 $strings['enable_api'] = "Activar API";
+$strings['api_key'] = "Api Key";
+$strings['extra_field_name'] = "Nombre del campo extra";

61f39a901e0d8e4764edf4005c80c2555a17a303
Yannick Warnier <ywarnier@beeznest.org>
2016-12-22 09:43:46 -0500

    WIP - Remove MySQL-specific syntax in vChamilo plugin installer. This does NOT match Chamilo's coding conventions ("name" and "value" are reserved keywords). See http://dev.mysql.com/doc/refman/5.7/en/keywords.html and https://support.chamilo.org/projects/chamilo-18/wiki/Coding_conventions#Database-INT-not-INT-11 (and following sections)

--- plugin/vchamilo/install.php
+++ plugin/vchamilo/install.php
@@ -18,26 +18,26 @@ api_protect_admin_script();
 $table = 'vchamilo';
 $tablename = Database::get_main_table($table);
 $sql = "CREATE TABLE IF NOT EXISTS $tablename (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sitename` varchar(80) NOT NULL,
-   slug varchar(255) NOT NULL,
-  `institution` varchar(80) NOT NULL,
-  `root_web` varchar(120),
-  `db_host` varchar(80) NOT NULL,
-  `db_user` varchar(16) DEFAULT 'root',
-  `db_password` varchar(32),  
-  `table_prefix` varchar(16),
-  `db_prefix` varchar(16),
-  `main_database` varchar(60) DEFAULT 'chamilo',
-  `url_append` varchar(32),
-  `course_folder` varchar(80),
-  `visible` int(1),
-  `lastcrongap` int(11),
-  `lastcron` int(11),
-  `croncount` int(11),
-  `template` varchar(255),
-  `password_encryption` varchar(255),  
-  PRIMARY KEY (`id`)
+  id int NOT NULL AUTO_INCREMENT,
+  sitename varchar(80) NOT NULL,
+  slug varchar(255) NOT NULL,
+  institution varchar(80) NOT NULL,
+  root_web varchar(120),
+  db_host varchar(80) NOT NULL,
+  db_user varchar(16) DEFAULT 'root',
+  db_password varchar(32),  
+  table_prefix varchar(16),
+  db_prefix varchar(16),
+  main_database varchar(60) DEFAULT 'chamilo',
+  url_append varchar(32),
+  course_folder varchar(80),
+  visible int,
+  lastcrongap int,
+  lastcron int,
+  croncount int,
+  template varchar(255),
+  password_encryption varchar(255),  
+  PRIMARY KEY (id)
 ) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 ";
 Database::query($sql);
@@ -45,11 +45,11 @@ Database::query($sql);
 $table = 'vchamilo_config';
 $tablename = Database::get_main_table($table);
 $sql = "CREATE TABLE IF NOT EXISTS $tablename (
-    `id` int(11) NOT NULL AUTO_INCREMENT, 
-    `component` int(11) NOT NULL,
+    id int(11) NOT NULL AUTO_INCREMENT, 
+    component int(11) NOT NULL,
     `name` varchar(64) NOT NULL,
     `value` varchar(255) NOT NULL,
-    `longvalue` varchar(255) NOT NULL,
+    longvalue varchar(255) NOT NULL,
     PRIMARY KEY (id)
 )
 ";

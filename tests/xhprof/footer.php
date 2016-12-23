0c3fedf2af3c31ec39f191825d8219704be20ce2
Yannick Warnier <ywarnier@beeznest.org>
2016-12-23 11:49:10 -0500

    Add get_memory_peak_usage() info to profile link

--- tests/xhprof/footer.php
+++ tests/xhprof/footer.php
@@ -15,5 +15,6 @@ if (extension_loaded('tideways')) {
     // url to the XHProf UI libraries (change the host name and path)
     $subDir = substr(__DIR__, strlen(trim($_SERVER['DOCUMENT_ROOT'])));
     $profiler_url = sprintf($subDir.'/xhprof_html/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
-    echo '<a style="z-index:99; position: absolute;" href="'. $profiler_url .'" target="_blank">Profiler output</a>';
+    $mem = round(memory_get_peak_usage()/1024);
+    echo '<a style="z-index:99; position: absolute;" href="'. $profiler_url .'" target="_blank">Profiler output (Memory: '.$mem.'KB)</a>';
 }

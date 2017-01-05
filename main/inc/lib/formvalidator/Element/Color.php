f1111c1079d7bbda6aca3f168208e11800c42de4
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2017-01-05 17:47:33 -0500

    Add input of type color to FormValidator - refs BT#12103

---
+++ main/inc/lib/formvalidator/Element/Color.php
@@ -0,0 +1,52 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ * Input Color element
+ *
+ * Class Color
+ */
+class Color extends HTML_QuickForm_text
+{
+    /**
+     * @param string $elementName
+     * @param string $elementLabel
+     * @param array  $attributes
+     */
+    public function __construct($elementName = null, $elementLabel = null, $attributes = null)
+    {
+        if (!isset($attributes['id'])) {
+            $attributes['id'] = $elementName;
+        }
+
+        $attributes['type'] = 'color';
+        $attributes['class'] = 'form-control';
+        $attributes['cols-size'] = isset($attributes['cols-size']) ? $attributes['cols-size'] : [2, 1, 9];
+
+        parent::__construct($elementName, $elementLabel, $attributes);
+
+        $this->_appendName = true;
+        $this->setType('color');
+    }
+
+    /**
+     * @return string
+     */
+    public function toHtml()
+    {
+        return parent::toHtml() . <<<JS
+            <script>
+                $(document).on('ready', function () {
+                    var txtColor = $('#{$this->getAttribute('id')}'),
+                        lblColor = txtColor.parent().next();
+
+                    lblColor.text(txtColor.val());
+
+                    txtColor.on('change', function () {
+                        lblColor.text(txtColor.val());
+                    })
+                });
+            </script>
+JS;
+    }
+}

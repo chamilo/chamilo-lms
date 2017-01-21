5e7f37bc3b38c82f615b5e67b99283618a4a549d
Yannick Warnier <ywarnier@beeznest.org>
2017-01-21 02:05:49 +0100

    Update language vars for vchamilo plugin

--- plugin/vchamilo/lang/spanish.php
+++ plugin/vchamilo/lang/spanish.php
@@ -1,1 +1,5 @@
 <?php
+$strings['FromVersion'] = 'Versión de origen';
+$strings['CoursePath'] = 'Ruta hacia carpeta courses';
+$strings['HomePath'] = 'Ruta hacia carpeta home';
+$strings['UploadPath'] = 'Ruta hacia carpeta upload';

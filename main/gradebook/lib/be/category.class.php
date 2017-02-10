11b366c022ebe10c02a480b736f9cbb4002c9605
Rafa <teran.gautier@gmail.com>
2017-02-10 12:39:32 +0100

    Fixes #1659
    4 space instead of tabs

--- main/gradebook/lib/be/category.class.php
+++ main/gradebook/lib/be/category.class.php
@@ -947,12 +947,12 @@ class Category implements GradebookItem
                 /** @var EvalLink|ExerciseLink $link */
                 foreach ($links as $link) {
                     $link->setStudentList($this->getStudentList());
-	                
-	                if ($session_id) {
-		                $link->set_session_id($session_id);
-	                }
-	                
-	                $linkres = $link->calc_score($stud_id, null);
+
+                    if ($session_id) {
+                        $link->set_session_id($session_id);
+                    }
+
+                    $linkres = $link->calc_score($stud_id, null);
                     if (!empty($linkres) && $link->get_weight() != 0) {
                         $students[$stud_id] = $linkres[0];
                         $linkweight = $link->get_weight();
@@ -1048,10 +1048,10 @@ class Category implements GradebookItem
                 /** @var EvalLink|ExerciseLink $link */
                 foreach ($links as $link) {
                     $link->setStudentList($this->getStudentList());
-	                
-	                if ($session_id) {
-		                $link->set_session_id($session_id);
-	                }
+
+                    if ($session_id) {
+                        $link->set_session_id($session_id);
+                    }
 	                
                     $linkres = $link->calc_score($stud_id, $type);
                     if (!empty($linkres) && $link->get_weight() != 0) {

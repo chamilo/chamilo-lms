8738b7633f896df458577484a84c96af7b4bed2d
José Loguercio <jloguercio.work@gmail.com>
2017-02-13 19:15:31 -0500

    Added Extra Fields handler to works - Refs BT#12212

--- src/Chamilo/CoreBundle/Entity/ExtraField.php
+++ src/Chamilo/CoreBundle/Entity/ExtraField.php
@@ -24,6 +24,8 @@ class ExtraField extends BaseAttribute
     const CALENDAR_FIELD_TYPE = 5;
     const LP_FIELD_TYPE = 6;
     const LP_ITEM_FIELD_TYPE = 7;
+    const SKILL_FIELD_TYPE = 8;
+    const WORK_FIELD_TYPE = 9;
 
     /**
      * @var integer

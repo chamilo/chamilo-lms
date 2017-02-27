3e78de8ad24da8ee9815be55cf874edfc73d533e
jmontoyaa <gugli100@gmail.com>
2017-02-27 17:21:44 +0100

    Fix constructor.

--- main/inc/lib/pear/Text/Diff/Renderer.php
+++ main/inc/lib/pear/Text/Diff/Renderer.php
@@ -35,7 +35,7 @@ class Text_Diff_Renderer {
     /**
      * Constructor.
      */
-    function Text_Diff_Renderer($params = array())
+    public function __construct($params = array())
     {
         foreach ($params as $param => $value) {
             $v = '_' . $param;

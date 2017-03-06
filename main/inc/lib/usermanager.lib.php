5906a3c1f5932082167e1803ba75c92d125c141e
jmontoyaa <gugli100@gmail.com>
2017-03-06 15:45:37 +0100

    Use api_get_user_info()

--- main/inc/lib/usermanager.lib.php
+++ main/inc/lib/usermanager.lib.php
@@ -1463,7 +1463,7 @@ class UserManager
         $user_table = Database :: get_main_table(TABLE_MAIN_USER);
         $tblAccessUrlRelUser = Database::get_main_table(TABLE_MAIN_ACCESS_URL_REL_USER);
         $return_array = array();
-        $sql_query = "SELECT * FROM $user_table ";
+        $sql_query = "SELECT user.id FROM $user_table user ";
 
         if (api_is_multiple_url_enabled()) {
             $sql_query .= " INNER JOIN $tblAccessUrlRelUser auru ON auru.user_id = user.id ";
@@ -1499,11 +1499,8 @@ class UserManager
 
         $sql_result = Database::query($sql_query);
         while ($result = Database::fetch_array($sql_result)) {
-            $result['complete_name'] = api_get_person_name(
-                $result['firstname'],
-                $result['lastname']
-            );
-            $return_array[] = $result;
+            $userInfo = api_get_user_info($result['id']);
+            $return_array[] = $userInfo;
         }
 
         return $return_array;

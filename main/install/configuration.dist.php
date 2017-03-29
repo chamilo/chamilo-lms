7bed12704e2ebf14aae422372b6d783707faaca9
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2017-03-29 13:58:23 -0500

    Fix documentation in configuration.php variables

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -250,9 +250,6 @@ $_configuration['system_stable'] = NEW_VERSION_STABLE;
 //$_configuration['packager'] = 'chamilo';
 // If true exercises added in LP can be modified.
 //$_configuration['force_edit_exercise_in_lp'] = false;
-// to reflect their packaging method. The default value is 'chamilo'. This will
-// be reflected on the https://version.chamilo.org/stats page in the future.
-//$_configuration['packager'] = 'chamilo';
 // List of driver to plugin in ckeditor
 //$_configuration['editor_driver_list'] = ['PersonalDriver', 'CourseDriver'];
 // Hide send to hrm users options in announcements

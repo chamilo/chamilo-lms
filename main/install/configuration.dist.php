063278adc93cf3558cef097aa3f1178f349f2de9
jmontoyaa <gugli100@gmail.com>
2017-04-13 13:32:42 +0200

    "remove_session_url" false by default see #1835

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -338,4 +338,4 @@ $_configuration['tracking_columns'] = [
 ];
 */
 // Hide session link of course_block on index/userportal
-//$_configuration['remove_session_url']= true ;
+//$_configuration['remove_session_url']= false ;

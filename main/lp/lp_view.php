789a77664495fbe9658624996ffd416b845ad196
Alex Aragon Calixto <aragcar@gmail.com>
2017-05-10 17:06:26 -0500

    New TPL for learning paths (#1908)
    * TPL for scorm list
    
    * TPL for scorm list
    
    * fix #1908
    
    * fix code format #1908

--- main/lp/lp_view.php
+++ main/lp/lp_view.php
@@ -540,10 +540,7 @@ $template->assign(
 $template->assign('lp_author', $_SESSION['oLP']->get_author());
 $template->assign('lp_mode', $_SESSION['oLP']->mode);
 $template->assign('lp_title_scorm', $_SESSION['oLP']->name);
-$template->assign(
-    'lp_html_toc',
-    $_SESSION['oLP']->get_html_toc($get_toc_list)
-);
+$template->assign('data_list', $_SESSION['oLP']->getListArrayToc($get_toc_list));
 $template->assign('lp_id', $_SESSION['oLP']->lp_id);
 $template->assign('lp_current_item_id', $_SESSION['oLP']->get_current_item_id());
 

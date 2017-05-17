ed59aea31dfc58aef0976bc7447370a854321d47
jmontoyaa <gugli100@gmail.com>
2017-05-17 12:04:15 +0200

    If rename document use the new doc title not the old title
    See BT#10887

--- main/inc/lib/document.lib.php
+++ main/inc/lib/document.lib.php
@@ -2982,11 +2982,8 @@ class DocumentManager
                             }
                         }*/
 
-                        // Don't need to rename the title it's already added inside handle_uploaded_document
-                        if ($if_exists !== 'rename') {
-                            if (!empty($title)) {
-                                $params['title'] = $title;
-                            }
+                        if (!empty($title)) {
+                            $params['title'] = $title;
                         }
 
                         if (!empty($comment)) {

59579de69e5726377dc71f38e97ed412a33ab404
jmontoyaa <gugli100@gmail.com>
2017-05-26 10:45:13 +0200

    Fix upgrade see #1983

--- app/Migrations/Schema/V111/Version20170522120000.php
+++ app/Migrations/Schema/V111/Version20170522120000.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Class Version20170522120000
  * Remove track_e_attempt.course_code which is deleted between 1.9 and 1.10
- * but somehow still existed in the 1.10 entity (and it is not deleted from 
+ * but somehow still existed in the 1.10 entity (and it is not deleted from
  * 1.10 to 1.11)
  * @package Application\Migrations\Schema\V111
  */
@@ -21,7 +21,6 @@ class Version20170522120000 extends AbstractMigrationChamilo
     public function up(Schema $schema)
     {
         $trackEAttempt = $schema->getTable('track_e_attempt');
-
         if ($trackEAttempt->hasColumn('course_code')) {
             $this->addSql("ALTER TABLE track_e_attempt DROP COLUMN course_code");
         }

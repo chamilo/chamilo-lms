d6972a104470a17b5cdb475e08f06099a1622b13
Alex Aragon <aragcar@gmail.com>
2017-06-06 10:40:33 -0500

    fix lp list

--- main/inc/lib/mpdf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->fontdata['couriersans-light'] = array(
-    'R' => 'CourierSans-Light.ttf'
-);
-
-$this->fontdata['couriersans-lightextra'] = array(
-    'R' => 'CourierSans-LightExtra.ttf',
-);

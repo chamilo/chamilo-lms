2ff96dbc0974ca113f8fd86fe0803cf5091de007
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2017-07-06 16:22:40 -0500

    Improve InputUser for FormValidator - refs BT#12955

--- main/inc/lib/formvalidator/Element/InputUser.php
+++ main/inc/lib/formvalidator/Element/InputUser.php
@@ -1,52 +1,68 @@
 <?php
 /* For licensing terms, see /license.txt */
 
-use Chamilo\UserBundle\Entity\User as ChUser;
+use Chamilo\UserBundle\Entity\User;
 
 class InputUser extends HTML_QuickForm_input
 {
-    /** @var ChUser */
-    private $user;
-    private $imageSize;
-    private $subTitle;
+    /** @var User */
+    private $user = null;
+    private $imageSize = 'small';
+    private $subTitle = '';
 
-    public function __construct($name, $label, ChUser $user = null, $attributes = [])
+    public function __construct($name, $label, $attributes = [])
     {
-        $this->user = $user;
-        $this->imageSize = 'small';
-        $this->subTitle = null;
-
         if (isset($attributes['image_size'])) {
             $this->imageSize = $attributes['image_size'];
             unset($attributes['image_size']);
-        }
-
-        parent::__construct($name, $label, $attributes);
-
-        $this->setType('hidden');
-
-        if ($this->user) {
-            $this->subTitle = $this->user->getUsername();
-            $this->setValue($this->user->getId());
         }
 
         if (isset($attributes['sub_title'])) {
             $this->subTitle = $attributes['sub_title'];
             unset($attributes['sub_title']);
         }
+
+        parent::__construct($name, $label, $attributes);
+
+        $this->setType('hidden');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setValue($value)
+    {
+        $this->user = !is_a($value, 'Chamilo\UserBundle\Entity\User')
+            ? UserManager::getManager()->find($value)
+            : $value;
+
+        parent::setValue($this->user->getId());
     }
 
     public function toHtml()
     {
+        if (!$this->user) {
+            return '';
+        }
+
+        $userInfo = api_get_user_info($this->user->getId());
+        $userPicture = isset($userInfo["avatar_{$this->imageSize}"])
+            ? $userInfo["avatar_{$this->imageSize}"]
+            : $userInfo["avatar"];
+
+        if (!$this->subTitle) {
+            $this->subTitle = $this->user->getUsername();
+        }
+
         $html = parent::toHtml();
         $html .= '
             <div class="media">
                 <div class="media-left">
-                    <img src="' . $this->user->getUserPicture($this->imageSize) . '" alt="' . $this->user->getCompleteName() . '">
+                    <img src="'.$userPicture.'" alt="'.$this->user->getCompleteName().'">
                 </div>
                 <div class="media-body">
-                    <h4 class="media-heading">' . $this->user->getCompleteName() . '</h4>
-                    ' . $this->subTitle . '
+                    <h4 class="media-heading">'.$this->user->getCompleteName().'</h4>
+                    '.$this->subTitle.'
                 </div>
             </div>
         ';

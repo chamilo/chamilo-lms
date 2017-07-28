f7b633d9f81f17a06537ad1f2aa528896fb8412e
jmontoyaa <gugli100@gmail.com>
2017-07-28 11:32:38 +0200

    Fix cron in order to set URLs correctly.

--- main/cron/scheduled_announcement.php
+++ main/cron/scheduled_announcement.php
@@ -6,6 +6,8 @@ require_once __DIR__.'/../inc/global.inc.php';
 $urlList = UrlManager::get_url_data();
 foreach ($urlList as $url) {
     echo "Portal: # ".$url['id']." - ".$url['url'].PHP_EOL;
+    // Set access_url to set correct paths.
+    $configuration['access_url'] = $url['id'];
     $object = new ScheduledAnnouncement();
     $messagesSent = $object->sendPendingMessages($url['id']);
     echo "Messages sent $messagesSent".PHP_EOL;

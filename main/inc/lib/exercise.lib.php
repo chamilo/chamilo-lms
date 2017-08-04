6b32bc3881806f539f5e777a840422bb6227ce14
Alex Aragon <aragcar@gmail.com>
2017-08-04 14:37:27 -0500

    fix attributes class input radio

--- main/inc/lib/exercise.lib.php
+++ main/inc/lib/exercise.lib.php
@@ -360,12 +360,13 @@ class ExerciseLib
                         );
 
                         $answer_input = null;
-
+                        $attributes['class'] = 'checkradios';
                         if ($answerType == UNIQUE_ANSWER_IMAGE) {
+                            $attributes['class'] = '';
                             $attributes['style'] = 'display: none;';
                             $answer = '<div class="thumbnail">'.$answer.'</div>';
                         }
-                        $attributes['class'] = 'checkradios';
+                        
                         $answer_input .= '<label class="radio '.$hidingClass.'">';
                         $answer_input .= Display::input(
                             'radio',

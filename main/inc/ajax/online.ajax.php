29da5a71002eab5ff13cc75ed829bb0f896a90f3
jmontoyaa <gugli100@gmail.com>
2017-08-14 14:22:44 +0200

    Minor - format code.

--- main/inc/ajax/online.ajax.php
+++ main/inc/ajax/online.ajax.php
@@ -3,7 +3,7 @@
 
 require_once __DIR__.'/../global.inc.php';
 
-$action = $_GET['a'];
+$action = isset($_GET['a']) ? $_GET['a'] : '';
 
 switch ($action) {
     case 'get_users_online':
@@ -13,7 +13,6 @@ switch ($action) {
         $images_to_show = MAX_ONLINE_USERS;
         $page = intval($_REQUEST['online_page_nr']);
         $max_page = ceil(who_is_online_count() / $images_to_show);
-
         $page_rows = ($page - 1) * MAX_ONLINE_USERS;
 
         if (!empty($max_page) && $page <= $max_page) {

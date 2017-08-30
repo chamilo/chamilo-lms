e6b7eb95c3488a593c2b630d9c810b38ad3f479a
jmontoyaa <gugli100@gmail.com>
2017-08-30 14:27:48 +0200

    Update course user session tracking every 5 minutes see BT#13267

--- main/inc/lib/login.lib.php
+++ main/inc/lib/login.lib.php
@@ -448,9 +448,13 @@ class Login
                     }
 
                     if (!isset($_SESSION['login_as'])) {
-                        //Course login
+                        // Course login
                         if (isset($_user['user_id'])) {
-                            Event::event_course_login(api_get_course_int_id(), $_user['user_id'], api_get_session_id());
+                            Event::eventCourseLogin(
+                                api_get_course_int_id(),
+                                $_user['user_id'],
+                                api_get_session_id()
+                            );
                         }
                     }
                 } else {

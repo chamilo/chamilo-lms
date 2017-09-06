33c1114f86b3c050be970da3bc608c320efc8799
jmontoyaa <gugli100@gmail.com>
2017-09-06 08:59:10 +0200

    Add allow_public_course_with_no_terms_conditions setting

--- main/inc/local.inc.php
+++ main/inc/local.inc.php
@@ -1088,6 +1088,7 @@ if ((isset($uidReset) && $uidReset) || (isset($cidReset) && $cidReset)) {
                 $_course['id'],
                 $session_id
             ) || $user_pass_open_course;
+
             if (!$user_is_subscribed) {
                 $url = api_get_path(WEB_CODE_PATH).'course_info/legal.php?course_code='.$_course['code'].'&session_id='.$session_id;
                 header('Location: '.$url);
@@ -1150,9 +1151,20 @@ if ((isset($uidReset) && $uidReset) || (isset($cidReset) && $cidReset)) {
                         );
                     }
                 }
-                $url = api_get_path(WEB_CODE_PATH).'auth/inscription.php';
-                header("Location:".$url);
-                exit;
+
+                $redirect = true;
+                $disable = api_get_configuration_value('allow_public_course_with_no_terms_conditions');
+                if ($disable === true &&
+                    isset($_course['visibility']) &&
+                    $_course['visibility'] == COURSE_VISIBILITY_OPEN_WORLD
+                ) {
+                    $redirect = false;
+                }
+                if ($redirect) {
+                    $url = api_get_path(WEB_CODE_PATH).'auth/inscription.php';
+                    header("Location:".$url);
+                    exit;
+                }
             }
         }
     }

ee596ff4a1425caf5198dc176aa795200a751e42
jmontoyaa <gugli100@gmail.com>
2017-11-30 01:03:03 +0100

    Fix vertical radio buttons in surveys see BT#13709

--- main/survey/ch_yesno.php
+++ main/survey/ch_yesno.php
@@ -54,12 +54,13 @@ class ch_yesno extends survey_question
     {
         if (is_array($questionData['options'])) {
             $class = 'radio-inline';
+            $labelClass = 'radio-inline';
             if ($questionData['display'] == 'vertical') {
-                $class = 'radio';
+                $class = 'radio-vertical';
             }
 
             $name = 'question'.$questionData['question_id'];
-            $radioAttributes = ['radio-class' => $class, 'label-class' => $class];
+            $radioAttributes = ['radio-class' => $class, 'label-class' => $labelClass];
 
             if (!empty($questionData['is_required'])) {
                 $radioAttributes['required'] = 'required';

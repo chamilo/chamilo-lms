f6b5f5257e0cbc4ee5adc49be7fbb5af8d1aa1f9
Yannick Warnier <ywarnier@beeznest.org>
2017-12-12 12:37:38 -0500

    Fix issue preventing install when using Galera DB cluster - refs #2010

---
+++ src/Chamilo/CoreBundle/Entity/Repository/AccessUrlRepository.php
@@ -0,0 +1,26 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class AccessUrlRepository
+ * @package Chamilo\CoreBundle\Entity\Repository
+ */
+class AccessUrlRepository extends EntityRepository
+{
+    /**
+     * Select the first access_url ID in the list as a default setting for
+     * the creation of new users.
+     * @return mixed
+     */
+    public function getFirstId()
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb->select('MIN (a.id)');
+        $q = $qb->getQuery();
+        return $q->execute();
+    }
+}

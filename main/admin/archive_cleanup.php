ff3884becada5f0c40f1377d63faebb9c8bd7f98
jmontoyaa <gugli100@gmail.com>
2017-12-21 12:09:00 +0100

    Allow cleanup to take more time than normal script.

--- main/admin/archive_cleanup.php
+++ main/admin/archive_cleanup.php
@@ -9,6 +9,9 @@ $cidReset = true;
 // including some necessary files
 require_once __DIR__.'/../inc/global.inc.php';
 
+ini_set('memory_limit', -1);
+ini_set('max_execution_time', 0);
+
 // setting the section (for the tabs)
 $this_section = SECTION_PLATFORM_ADMIN;
 

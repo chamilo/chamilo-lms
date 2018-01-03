8c7b08d61f1b34f2a7add7fc92648e3c252351d1
jmontoyaa <gugli100@gmail.com>
2018-01-03 14:43:40 +0100

    Add SkillsBundle file + fix Kernel file

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Chamilo\CoreBundle\Component\Utils\ChamiloApi;
 
 class Kernel extends BaseKernel
 {
@@ -57,5 +58,77 @@ class Kernel extends BaseKernel
             $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
         }
         $routes->import($confDir.'/routes'.self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        if (null === $this->rootDir) {
+            $r = new \ReflectionObject($this);
+            $this->rootDir = str_replace('\\', '/', dirname($r->getFileName()));
+        }
+
+        return $this->rootDir;
+    }
+
+    /**
+     * Returns the real root path
+     * @return string
+     */
+    public function getRealRootDir()
+    {
+        return realpath($this->getRootDir().'/../').'/';
+    }
+
+    /**
+     * Returns the data path
+     * @return string
+     */
+    public function getDataDir()
+    {
+        return $this->getRealRootDir().'data/';
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppDir()
+    {
+        return $this->getRealRootDir().'app/';
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigDir()
+    {
+        return $this->getRealRootDir().'app/config/';
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigurationFile()
+    {
+        return $this->getRealRootDir().'app/config/configuration.php';
+    }
+
+    /**
+     * @param array $configuration
+     */
+    public function setApi(array $configuration)
+    {
+        new ChamiloApi($configuration);
+    }
+
+    /**
+    * Check if system is installed
+    * @return bool
+    */
+    public function isInstalled()
+    {
+        return !empty($this->getContainer()->getParameter('installed'));
     }
 }

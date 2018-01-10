8d9b94c0523f02ce7eb4d5d6af0351559b64f5b4
Julio <gugli100@gmail.com>
2018-01-10 12:32:32 +0100

    Minor - fix php warning (added isset)

--- main/inc/local.inc.php
+++ main/inc/local.inc.php
@@ -365,7 +365,7 @@ if (!empty($_SESSION['_user']['user_id']) && !($login || $logout)) {
                         'update_type'
                     );
 
-                    $update_type = $update_type['update_type'];
+                    $update_type = isset($update_type['update_type']) ? $update_type['update_type'] : '';
                     if (!empty($extAuthSource[$update_type]['updateUser'])
                         && file_exists($extAuthSource[$update_type]['updateUser'])
                     ) {

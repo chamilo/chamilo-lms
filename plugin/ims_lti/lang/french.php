d713cbff9ff1ebceb4c0e90ecb8131bcf04763cd
Yannick Warnier <ywarnier@beeznest.org>
2018-01-19 00:16:08 +0100

    Add LTI plugin translation to French and Spanish

---
+++ plugin/ims_lti/lang/french.php
@@ -0,0 +1,23 @@
+<?php
+/* For license terms, see /license.txt */
+$strings['plugin_title'] = 'IMS/LTI';
+$strings['plugin_comment'] = 'IMS/LTI';
+
+$strings['enabled'] = 'Activé';
+
+$strings['ImsLtiDescription'] = '<p>Learning Tools Interoperability® (LTI®) est une spécification développée par le IMS Global Learning Consortium. L\'objectif principal de LTI est d\'établir une forme standard d\'intégration d\'application riches d\'apprentissage (souvent hébergées de manière distante et fournies au travers de services d\'une tierce partie) dans des plateformes telles que les learning management systems, portails, learning object repositories, ou autres environnements éducatifs.</p>';
+$strings['ManageToolButton'] = '<p>Pour gérer les différents outils, dirigez-vous vers la <a href="%s">liste d\'outils</a></p>';
+$strings['AddExternalTool'] = 'Ajouter un outil externe';
+$strings['ProviderName'] = 'Nom du fournisseur';
+$strings['LaunchUrl'] = 'URL de démarrage (Launch URL)';
+$strings['ConsumerKey'] = 'Clef consommateur (Consumer key)';
+$strings['SharedSecret'] = 'Secret partagé (Shared secret)';
+$strings['CustomParams'] = 'Paramètres personnalisés';
+$strings['ToolName'] = 'Nom de l\'outil';
+$strings['ToolNotAdded'] = 'Outil non ajouté';
+$strings['AvailableTools'] = 'Outils disponibles';
+$strings['ToolSettings'] = 'Paramètres de l\'outil';
+$strings['ToolNotAvailable'] = 'Outil non disponible';
+$strings['IsGlobal'] = 'Est global';
+$strings['EditExternalTool'] = 'Éditer outil externe';
+$strings['ToolDeleted'] = 'Outil supprimé';
---
+++ plugin/ims_lti/lang/spanish.php
@@ -0,0 +1,23 @@
+<?php
+/* For license terms, see /license.txt */
+$strings['plugin_title'] = 'IMS/LTI';
+$strings['plugin_comment'] = 'IMS/LTI';
+
+$strings['enabled'] = 'Activado';
+
+$strings['ImsLtiDescription'] = '<p>Learning Tools Interoperability® (LTI®) es una especificación desarrollada por el IMS Global Learning Consortium. El concepto principal de LTI es establecer una manera estándar de integrar aplicaciones de aprendizaje inteligentes (usualmente alojadas en servidores remotos y proveeidos a través de servicios de terceros) en plataformas como los LMS, portales, repositorios de objetos de aprendizaje, u otros ambientes educativos.</p>';
+$strings['ManageToolButton'] = '<p>Para gestionar las herramientas, ir a la <a href="%s">lista de herramientas</a></p>';
+$strings['AddExternalTool'] = 'Añadir herramienta externa';
+$strings['ProviderName'] = 'Proveedor de la herramienta';
+$strings['LaunchUrl'] = 'URL de arranque (launch URL)';
+$strings['ConsumerKey'] = 'Clave consumidor (consumer key)';
+$strings['SharedSecret'] = 'Secreto compartido (shared secret)';
+$strings['CustomParams'] = 'Parámetros personalizados';
+$strings['ToolName'] = 'Nombre de la herramienta';
+$strings['ToolNotAdded'] = 'Herramienta no añadida';
+$strings['AvailableTools'] = 'Herramientas disponibles';
+$strings['ToolSettings'] = 'Parámetros de la herramienta';
+$strings['ToolNotAvailable'] = 'Herramienta no disponible';
+$strings['IsGlobal'] = 'Es global';
+$strings['EditExternalTool'] = 'Editar herramienta externa';
+$strings['ToolDeleted'] = 'Herramienta eliminada';

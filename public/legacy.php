b91b73b8987370c3c2b6995e10a76ae68f266686
jmontoyaa <gugli100@gmail.com>
2018-01-30 10:24:20 +0100

    Minor - format code

--- public/legacy.php
+++ public/legacy.php
@@ -11,4 +11,3 @@ require_once __DIR__.'/../main/inc/lib/online.inc.php';
 require_once __DIR__.'/../main/inc/lib/fileManage.lib.php';
 require_once __DIR__.'/../main/inc/lib/fileUpload.lib.php';
 require_once __DIR__.'/../main/inc/lib/fileDisplay.lib.php';
-

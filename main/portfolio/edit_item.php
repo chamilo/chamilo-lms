86fb43592be89a44d9fd7a237933826ff3fd418f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-03-13 15:53:42 -0500

    Add Portfolio Tool - refs BT#14056
    Remove code about unused old Portfolio - refs BT#14056
    
    Creating portfolio tool - refs BT#14056
    
    Add portfolio categories - refs BT#14056
    
    Allow show portfolio to others users - refs BT#14056
    
    Disabled portfolio entities - refs BT#14056
    
    Minor - Format code - refs BT#14056
    
    Minor - Cast instead of intval - refs BT#14056
    
    Fix portfolio items view from session - refs BT#14056
    
    Minor - FlintCI fixes - refs BT#14056

---
+++ main/portfolio/edit_item.php
@@ -0,0 +1,53 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$categories = $em
+    ->getRepository('ChamiloCoreBundle:PortfolioCategory')
+    ->findBy([
+        'user' => $user,
+    ]);
+
+$form = new FormValidator('edit_portfolio', 'post', $baseUrl."action=edit_item&id={$item->getId()}");
+$form->addText('title', get_lang('Title'));
+$form->addHtmlEditor('content', get_lang('Content'), true, false, ['ToolbarSet' => 'NotebookStudent']);
+$form->addSelectFromCollection('category', get_lang('Category'), $categories, [], true, '__toString');
+$form->addButtonUpdate(get_lang('Update'));
+$form->setDefaults([
+    'title' => $item->getTitle(),
+    'content' => $item->getContent(),
+    'category' => $item->getCategory() ? $item->getCategory()->getId() : '',
+]);
+
+if ($form->validate()) {
+    $values = $form->exportValues();
+    $currentTime = new DateTime(api_get_utc_datetime(), new DateTimeZone('UTC'));
+
+    $item
+        ->setTitle($values['title'])
+        ->setContent($values['content'])
+        ->setUpdateDate($currentTime)
+        ->setCategory(
+            $em->find('ChamiloCoreBundle:PortfolioCategory', $values['category'])
+        );
+
+    $em->persist($item);
+    $em->flush();
+
+    Display::addFlash(
+        Display::return_message(get_lang('Updated'), 'success')
+    );
+
+    header("Location: $baseUrl");
+    exit;
+}
+
+$toolName = get_lang('EditPortfolioItem');
+$interbreadcrumb[] = [
+    'name' => get_lang('Portfolio'),
+    'url' => $baseUrl,
+];
+$actions[] = Display::url(
+    Display::return_icon('back.png', get_lang('Back'), [], ICON_SIZE_MEDIUM),
+    $baseUrl
+);
+$content = $form->returnForm();

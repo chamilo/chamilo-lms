a4fd52ed9f1b46f363e2c4c5f80703bcda42d2a1
jmontoyaa <gugli100@gmail.com>
2018-03-15 14:51:18 +0100

    Minor - open link in new tab

--- main/inc/lib/ScheduledAnnouncement.php
+++ main/inc/lib/ScheduledAnnouncement.php
@@ -275,7 +275,7 @@ class ScheduledAnnouncement extends Model
         if (!empty($file) && !empty($file['value'])) {
             //$file = api_get_uploaded_web_url('schedule_announcement', $id, basename($file['value']));
             $url = api_get_path(WEB_UPLOAD_PATH).$file['value'];
-            return get_lang('Attachment').': '.Display::url(basename($file['value']), $url);
+            return get_lang('Attachment').': '.Display::url(basename($file['value']), $url, ['target' => '_blank']);
         }
         return '';
     }

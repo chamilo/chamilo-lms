b78dc53b554ca50bfbb5754f69872d05d9007cc3
jmontoyaa <gugli100@gmail.com>
2018-04-11 08:07:39 +0200

    Minor - remove addslashes

--- main/inc/ajax/lp.ajax.php
+++ main/inc/ajax/lp.ajax.php
@@ -323,9 +323,9 @@ switch ($action) {
             echo '1';
         } else {
             if (!empty($lp->error)) {
-                echo addslashes($lp->error);
+                echo $lp->error;
             } else {
-                echo addslashes(get_lang('LearnpathPrereqNotCompleted'));
+                echo get_lang('LearnpathPrereqNotCompleted');
             }
         }
         $lp->error = '';

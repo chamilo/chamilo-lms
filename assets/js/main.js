cb44278c9f219c2d1c74eaada0428d8f02defe5f
jmontoyaa <gugli100@gmail.com>
2018-04-12 10:18:16 +0200

    Add "timeago" lib

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,27 +13,24 @@ require("webpack-jquery-ui/css");
 // no need to set this to a variable, just require it
 require("bootstrap-sass");
 require("chosen-js");
+require("mediaelement");
 // require("font-awesome-webpack"); already added manually in main.scss
 
-
-require("mediaelement");
-
-// full calendar added in lib
-//require("fullcalendar");
 require("qtip2");
 require("image-map-resizer");
-
 require("cropper");
 require("jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon");
 require("jquery.scrollbar");
 require("blueimp-file-upload");
 require("select2");
+require("timeago");
 require("select2/dist/css/select2.css");
 require('bootstrap-select/dist/js/bootstrap-select.js');
 require('bootstrap-select/dist/css/bootstrap-select.css');
 
 // doesn't work with webpack added directly in /public/libs folder
 /*
+require("fullcalendar");
 require("pwstrength-bootstrap");
 require ("readmore-js");
 require("js-cookie");

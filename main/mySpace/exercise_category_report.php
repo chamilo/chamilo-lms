1c1b658b6161c1e21daa274a3013041a65a9495c
jmontoyaa <gugli100@gmail.com>
2018-04-27 13:43:47 +0200

    Minor - add course code in export file see BT#13187

--- main/mySpace/exercise_category_report.php
+++ main/mySpace/exercise_category_report.php
@@ -93,7 +93,7 @@ if ($form->validate() && !empty($courseInfo)) {
     $values = $form->getSubmitValues();
     $exerciseId = isset($values['exercise_id']) ? $values['exercise_id'] : 0;
     $startDate = Security::remove_XSS($values['start_date']);
-    $exportFilename = 'exercise_results_report_'.$exerciseId;
+    $exportFilename = 'exercise_results_report_'.$exerciseId.'_'.$courseInfo['code'];
     $url = api_get_path(WEB_AJAX_PATH).'model.ajax.php?a=get_exercise_results_report&exercise_id='.$exerciseId.'&start_date='.$startDate.'&cidReq='.$courseInfo['code'].'&export_filename='.$exportFilename;
 
     $categoryList = TestCategory::getListOfCategoriesIDForTest($exerciseId, $courseId);

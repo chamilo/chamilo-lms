65215220db552511d93892550be88f1af8c832c3
Nosolored <nosolored@gmail.com>
2018-05-23 12:05:25 +0200

    Change javascript code to jquery code

--- plugin/customcertificate/resources/js/certificate.js
+++ plugin/customcertificate/resources/js/certificate.js
@@ -1,64 +1,36 @@
 function dateCertificateSwitchRadioButton2(){
-    var inputDateStart = document.getElementById("date_start");
-    var inputDateEnd = document.getElementById("date_end");
-    inputDateStart.value = "";
-    inputDateEnd.value = "";
-    inputDateStart.setAttribute("disabled","disabled");
-    inputDateEnd.setAttribute("disabled","disabled");
+    $("#date_start").prop("disabled", true).val('');
+    $("#date_end").prop("disabled", true).val('');
 }
 
 function dateCertificateSwitchRadioButton1(){
-    var inputDateStart = document.getElementById("date_start");
-    var inputDateEnd = document.getElementById("date_end");
-    inputDateStart.removeAttribute("disabled");
-    inputDateEnd.removeAttribute("disabled");
+    $("#date_start").prop("disabled", false);
+    $("#date_end").prop("disabled", false);
 }
 
 function dateCertificateSwitchRadioButton0(){
-    var inputDateStart = document.getElementById("date_start");
-    var inputDateEnd = document.getElementById("date_end");
-    inputDateStart.value = "";
-    inputDateEnd.value = "";
-    inputDateStart.setAttribute("disabled","disabled");
-    inputDateEnd.setAttribute("disabled","disabled");
+    $("#date_start").prop("disabled", true).val('');
+    $("#date_end").prop("disabled", true).val('');
 }
 
 function typeDateExpedictionSwitchRadioButton(){
-    var inputType = document.getElementsByName("type_date_expediction");
-    var type;
-    for (var i=0;i<inputType.length;i++){
-        if ( inputType[i].checked === true) {
-            type = parseInt(inputType[i].value);
+    $('[name="type_date_expediction"]').each(function( index ) {
+        if ( $(this).is(':checked') && $(this).val() == "2") {
+            $("#day, #month, #year").prop("disabled", false);
+        } else {
+            $("#day, #month, #year").prop('disabled', true);
         }
-    }
-    var inputDay = document.getElementById("day");
-    var inputMonth = document.getElementById("month");
-    var inputYear = document.getElementById("year");
-    if (type === 2) {
-        inputDay.removeAttribute("disabled");
-        inputMonth.removeAttribute("disabled");
-        inputYear.removeAttribute("disabled");
-    } else {
-        inputDay.setAttribute("disabled","disabled");
-        inputMonth.setAttribute("disabled","disabled");
-        inputYear.setAttribute("disabled","disabled");
-    }
+    });
 }
 
 function contentsTypeSwitchRadioButton(){
-    var inputType = document.getElementsByName("contents_type");
-    var type;
-    for (var i=0;i<inputType.length;i++){
-        if ( inputType[i].checked === true) {
-          type = parseInt(inputType[i].value);
+    $('[name="contents_type"]').each(function( index ) {
+        if ( $(this).is(':checked') && $(this).val() == "2") {
+            $("#contents").prop("disabled", false);
+        } else {
+            $("#contents").prop('disabled', true);
         }
-    }
-    var inputContents = document.getElementById("contents");
-    if (type === 2) {
-        inputContents.removeAttribute("disabled");
-    } else {
-        inputContents.setAttribute("disabled","disabled");
-    }
+    });
 }
 
 $(document).ready(function() {

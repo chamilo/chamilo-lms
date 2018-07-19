e0bf77a8e5e94b727547cd28c8a89b0cc93b9b5d
Yannick Warnier <ywarnier@beeznest.org>
2018-07-19 11:06:37 -0500

    Minor - Fix notice about legal_accept extra_field in user_information.php

--- main/admin/user_information.php
+++ main/admin/user_information.php
@@ -197,7 +197,7 @@ if (api_get_setting('allow_terms_conditions') === 'true') {
         'legal_accept'
     );
     $icon = Display::return_icon('accept_na.png');
-    if (isset($value['value'])) {
+    if (!empty($value['value'])) {
         list($legalId, $legalLanguageId, $legalTime) = explode(':', $value['value']);
         $icon = Display::return_icon('accept.png').' '.api_get_local_time($legalTime);
         $icon .= ' '.Display::url(

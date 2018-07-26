638f5e6bc86dff2793a76cdef578ba4931d6bf46
Julio Montoya <gugli100@gmail.com>
2018-07-26 09:53:24 +0200

    Fix LP export document paths see BT#14644
    - Fix export of images inside "default_course_document", users/upload,
      and the use of relative paths.
    
    - Fix error when export LP using the directory "document/learning_path"

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -9163,9 +9163,49 @@ function location($url, $exit = true)
  */
 function api_get_web_url()
 {
-    if (api_get_setting('server_type') == 'test') {
+    if (api_get_setting('server_type') === 'test') {
         return api_get_path(WEB_PATH).'web/app_dev.php/';
     } else {
         return api_get_path(WEB_PATH).'web/';
     }
+}
+
+/**
+ * @param string $from
+ * @param string $to
+ *
+ * @return string
+ */
+function api_get_relative_path($from, $to)
+{
+    // some compatibility fixes for Windows paths
+    $from = is_dir($from) ? rtrim($from, '\/').'/' : $from;
+    $to = is_dir($to) ? rtrim($to, '\/').'/' : $to;
+    $from = str_replace('\\', '/', $from);
+    $to = str_replace('\\', '/', $to);
+
+    $from = explode('/', $from);
+    $to = explode('/', $to);
+    $relPath = $to;
+
+    foreach ($from as $depth => $dir) {
+        // find first non-matching dir
+        if ($dir === $to[$depth]) {
+            // ignore this directory
+            array_shift($relPath);
+        } else {
+            // get number of remaining dirs to $from
+            $remaining = count($from) - $depth;
+            if ($remaining > 1) {
+                // add traversals up to first matching dir
+                $padLength = (count($relPath) + $remaining - 1) * -1;
+                $relPath = array_pad($relPath, $padLength, '..');
+                break;
+            } else {
+                $relPath[0] = './'.$relPath[0];
+            }
+        }
+    }
+
+    return implode('/', $relPath);
 }

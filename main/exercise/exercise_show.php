chamilo/chamilo-lms

commit 5a80a12c58e3864cead851c43ad10d0048b7acd6

add tpl exercise_result_alert_body - refs #2549

main/exercise/exercise_show.php
@@ -1049,7 +1049,7 @@ if ($isFeedbackAllowed && $origin != 'learnpath' && $origin != 'student_progress
             'course' => Security::remove_XSS($_GET['cidReq']),
         ]);
 
-        $emailForm = new FormValidator('myform', 'post', $formUrl, '', ['id' => 'myform']);
+        $emailForm = new FormValidator('form-email', 'post', $formUrl, '', ['id' => 'form-email']);
         $emailForm->addHidden('lp_item_id', $learnpath_id);
         $emailForm->addHidden('lp_item_view_id', $lp_item_view_id);
         $emailForm->addHidden('student_id', $student_id);
@@ -1065,11 +1065,11 @@ if ($isFeedbackAllowed && $origin != 'learnpath' && $origin != 'student_progress
         ]);
 
         $emailForm = new FormValidator(
-            'myform',
+            'form-email',
             'post',
             $formUrl,
             '',
-            ['id' => 'myform']
+            ['id' => 'form-email']
         );
     }
 
@@ -1079,13 +1079,13 @@ if ($isFeedbackAllowed && $origin != 'learnpath' && $origin != 'student_progress
         get_lang('SendEmail'),
         ['onclick' => 'openEmailWrapper();']
     );
-    $emailForm->addHtml('<span id="email_content_wrapper" style="display:none">');
+    $emailForm->addHtml('<div id="email_content_wrapper" style="display:none; margin-bottom: 20px;">');
     $emailForm->addHtmlEditor(
         'notification_content',
         get_lang('Content'),
         false
     );
-    $emailForm->addHtml('</span>');
+    $emailForm->addHtml('</div>');
 
     if (empty($track_exercise_info['orig_lp_id']) || empty($track_exercise_info['orig_lp_item_id'])) {
         // Default url

app/Resources/public/css/base.css
@@ -526,7 +526,9 @@ input[type=checkbox], input[type=radio] {
     margin-right: 5px;
     margin-top: 2px;
 }
-
+.checkbox_send_notification{
+    margin-bottom: 10px;
+}
 /* end chamilo forms fixes */
 
 .actions {

main/inc/lib/exercise.lib.php
@@ -4972,16 +4972,16 @@ EOT;
             PERSON_NAME_EMAIL_ADDRESS
         );
 
-        $message = '<p>'.get_lang('DearStudentEmailIntroduction').'</p><p>'.get_lang('AttemptVCC');
-        $message .= '<h3>'.get_lang('CourseName').'</h3><p>'.Security::remove_XSS($course_info['name']).'';
-        $message .= '<h3>'.get_lang('Exercise').'</h3><p>'.Security::remove_XSS($test);
-        $message .= '<p>'.get_lang('ClickLinkToViewComment').' <br /><a href="#url#">#url#</a><br />';
-        $message .= '<p>'.get_lang('Regards').'</p>';
-        $message .= $from_name;
-        $message = str_replace("#test#", Security::remove_XSS($test), $message);
-        $message = str_replace("#url#", $url, $message);
+        $view = new Template('', false, false, false, false, false, false);
+        $view->assign('course_title', Security::remove_XSS($course_info['name']));
+        $view->assign('test_title', Security::remove_XSS($test));
+        $view->assign('url', $url );
+        $view->assign('teacher_name', $from_name);
+        $template = $view->get_template('mail/exercise_result_alert_body.tpl');
 
-        return $message;
+        return $view->fetch($template);
+
+
     }
 
     /**

main/inc/lib/pear/HTML/QuickForm/checkbox.php
@@ -140,13 +140,11 @@ class HTML_QuickForm_checkbox extends HTML_QuickForm_input
         } elseif ($this->_flagFrozen) {
             $label = $this->_text;
         } else {
-            $labelClass = $this->labelClass;
-            $checkboxClass = $this->checkboxClass;
-
-            $label ='
-                <div class="'.$checkboxClass.'">
-                <label class="'.$labelClass.'">' .
-                    HTML_QuickForm_input::toHtml().$this->_text.
+            $className = $this->checkboxClass;
+            $name = $this->_attributes['name'];
+            $label ='<div id="'.$name.'" class="'.$className.'_'.$name.'">
+                <label>' .
+                    HTML_QuickForm_input::toHtml().' '.$this->_text.
                 '</label>
                 </div>
             ';

main/template/default/mail/exercise_result_alert_body.tpl
@@ -1,0 +1,19 @@
+<div id="email-message" style="padding-top:20px; padding-bottom:20px; margin: 10px;">
+    <p><strong>{{ 'DearStudentEmailIntroduction'|get_lang }}</strong> {{ 'AttemptVCC'|get_lang }} </p>
+    <div style="border-color: #bce8f1; padding: 15px; background-color: #d9edf7; margin-bottom: 15px; font-size: 16px; color:#31708f;">
+        <p>
+            <strong>{{ 'CourseName'|get_lang }}: </strong>
+            {{ course_title }}
+        <br>
+            <strong>{{ 'Exercise'|get_lang }}: </strong>
+            {{ test_title }}
+        </p>
+    </div>
+
+    <p>{{ 'ClickLinkToViewComment'|get_lang }}</p>
+    <p><a style="font-weight: bold; color: #2BA6CB;" href="{{ url }}">{{ url }}</a></p>
+    <div style="text-align: right;">
+        <p><strong>{{ 'Regards'|get_lang }}</strong><br>
+        {{ teacher_name }}</p>
+    </div>
+</div>

commit 302ec74db1002edb053ea0f8fc6b9aa0903674c9

Exercises: Fix inter-version issue introduced by 5a80a12c58e3864cead851c43ad10d0048b7acd6 in which the comments and scores in exercises corrections were not taken into account - refs BT#14688

main/exercise/exercise_show.php
@@ -197,7 +197,7 @@ if ($action != 'export') {
         }
 
         function getFCK(vals, marksid) {
-            var f = document.getElementById('myform');
+            var f = document.getElementById('form-email');
 
             var m_id = marksid.split(',');
             for (var i = 0; i < m_id.length; i++) {
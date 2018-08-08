751b381310b42562adc39f303ffa6acf25ce0ebe
Yannick Warnier <ywarnier@beeznest.org>
2018-08-08 01:39:21 -0500

    Update language terms

--- main/lang/english/trad4all.inc.php
+++ main/lang/english/trad4all.inc.php
@@ -8287,4 +8287,5 @@ $InformationRightToBeForgottenLinkX = "You can find more information about the u
 $ExplanationDeleteLegal = "Please tell us why you want to withdraw the rights you previously gave us, to let us make it in the smoothest way possible.";
 $ExplanationDeleteAccount = "Explain in this box why you want your account deleted";
 $WhyYouWantToDeleteYourLegalAgreement = "You can ask below for your legal agreement to be deleted or your account to be deleted.</br>In the case of the legal agreement, once deleted you will have to accept it again on your next login to be able to access the platform and recover your access, because we cannot reasonably at the same time give you a personal environment and not treat your personal data.</br>In the case of an account deletion, your account will be deleted along with all of your course subscriptions and all the information related to your account. Please select the corresponding option with care. In both cases, one of our administrators will review your request before it is effective, to avoid any misunderstanding and definitive loss of your data.";
+$PersonalDataPrivacy = "Personal data protection";
 ?>

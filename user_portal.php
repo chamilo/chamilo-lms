cac83af385e8bad99bf89f7dc188071e9441ff36
Julio Montoya <gugli100@gmail.com>
2018-08-08 09:10:45 +0200

    Minor - partial merge with 1.11.x

--- user_portal.php
+++ user_portal.php
@@ -146,7 +146,6 @@ if (!$myCourseListAsCategory) {
     }
 
     // if teacher, session coach or admin, display the button to change te course view
-
     if ($displayMyCourseViewBySessionLink &&
         (
             api_is_drh() ||
@@ -213,7 +212,7 @@ if (api_get_setting('go_to_course_after_login') == 'true') {
 
             // Session has many courses.
             if (isset($sessionInfo['session_id'])) {
-                $url = api_get_path(WEB_CODE_PATH).'session/?session_id='.$sessionInfo['session_id'];
+                $url = api_get_path(WEB_CODE_PATH).'session/index.php?session_id='.$sessionInfo['session_id'];
 
                 header('Location:'.$url);
                 exit;
@@ -226,10 +225,7 @@ if (api_get_setting('go_to_course_after_login') == 'true') {
         $count_of_sessions == 0 &&
         $count_of_courses_no_sessions == 1
     ) {
-        $courses = CourseManager::get_courses_list_by_user_id(
-            $userId
-        );
-
+        $courses = CourseManager::get_courses_list_by_user_id($userId);
         if (!empty($courses) && isset($courses[0]) && isset($courses[0]['code'])) {
             $courseInfo = api_get_course_info_by_id($courses[0]['real_id']);
             if (!empty($courseInfo)) {
@@ -287,7 +283,7 @@ $controller->tpl->assign('course_block', $controller->return_course_block());
 $controller->tpl->assign('navigation_course_links', $controller->return_navigation_links());
 $controller->tpl->assign('search_block', $controller->return_search_block());
 $controller->tpl->assign('notice_block', $controller->return_notice());
-$controller->tpl->assign('classes_block', $controller->return_classes_block());
+$controller->tpl->assign('classes_block', $controller->returnClassesBlock());
 $controller->tpl->assign('skills_block', $controller->returnSkillLinks());
 
 $historyClass = '';

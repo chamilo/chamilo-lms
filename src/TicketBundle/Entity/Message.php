b6df3825b1c3c9ab5241ed7fdad72799e041281d
Julio Montoya <gugli100@gmail.com>
2018-08-20 15:23:49 +0200

    Minor - adding getters/setters

--- src/TicketBundle/Entity/Message.php
+++ src/TicketBundle/Entity/Message.php
@@ -85,4 +85,204 @@ class Message
      * @ORM\Column(name="sys_lastedit_datetime", type="datetime", nullable=true, unique=false)
      */
     protected $lastEditDateTime;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Message
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     *
+     * @return Message
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return Message
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+
+    /**
+     * @param string $ipAddress
+     *
+     * @return Message
+     */
+    public function setIpAddress($ipAddress)
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+
+    /**
+     * @return Ticket
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
+    /**
+     * @param Ticket $ticket
+     *
+     * @return Message
+     */
+    public function setTicket($ticket)
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getInsertUserId()
+    {
+        return $this->insertUserId;
+    }
+
+    /**
+     * @param int $insertUserId
+     *
+     * @return Message
+     */
+    public function setInsertUserId($insertUserId)
+    {
+        $this->insertUserId = $insertUserId;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getInsertDateTime()
+    {
+        return $this->insertDateTime;
+    }
+
+    /**
+     * @param \DateTime $insertDateTime
+     *
+     * @return Message
+     */
+    public function setInsertDateTime($insertDateTime)
+    {
+        $this->insertDateTime = $insertDateTime;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastEditUserId()
+    {
+        return $this->lastEditUserId;
+    }
+
+    /**
+     * @param int $lastEditUserId
+     *
+     * @return Message
+     */
+    public function setLastEditUserId($lastEditUserId)
+    {
+        $this->lastEditUserId = $lastEditUserId;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastEditDateTime()
+    {
+        return $this->lastEditDateTime;
+    }
+
+    /**
+     * @param \DateTime $lastEditDateTime
+     *
+     * @return Message
+     */
+    public function setLastEditDateTime($lastEditDateTime)
+    {
+        $this->lastEditDateTime = $lastEditDateTime;
+
+        return $this;
+    }
 }

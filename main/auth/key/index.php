fad48411511cd205ac0b8d8af9cad4c59640c715
Julio Montoya <gugli100@gmail.com>
2018-08-24 13:56:08 +0200

    Remove unused login methods #2645

--- main/auth/key/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Display nothing. This ensure Apache doesn't display the list of files and folders
- * when it is not propertly configured.
- *
- * @license see /license.txt
- * @author Laurent Opprecht <laurent@opprecht.info>, Nicolas Rod for the University of Geneva
- */

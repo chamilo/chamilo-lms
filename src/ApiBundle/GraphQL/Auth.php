7bb57001e2845d70892bb9b7d6bbe3cdae85316f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-08-31 18:20:28 -0500

    GraphQL Authenticate user
    Refs #2644

---
+++ src/ApiBundle/GraphQL/Auth.php
@@ -0,0 +1,86 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\ApiBundle\GraphQL;
+
+use Chamilo\CoreBundle\Framework\Container;
+use Chamilo\UserBundle\Entity\User;
+use Firebase\JWT\JWT;
+use Overblog\GraphQLBundle\Error\UserError;
+
+/**
+ * Class Auth
+ *
+ * @package Chamilo\ApiBundle\GraphQL
+ */
+class Auth
+{
+
+    /**
+     * @param string $username
+     * @param string $password
+     *
+     * @return string
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function getUserToken($username, $password): string
+    {
+        $userRepo = Container::getEntityManager()->getRepository('ChamiloUserBundle:User');
+
+        /** @var User $user */
+        $user = $userRepo->findOneByUsername($username);
+
+        if (!$user) {
+            throw new UserError(get_lang('NoUser'));
+        }
+
+        $encoder = Container::$container->get('chamilo_user.security.encoder');
+        $isValid = $encoder->isPasswordValid(
+            $user->getPassword(),
+            $password,
+            $user->getSalt()
+        );
+
+        if (!$isValid) {
+            throw new UserError(get_lang('InvalidId'));
+        }
+
+        return self::generateToken($user->getId());
+    }
+
+    /**
+     * @param int $userId
+     *
+     * @return string
+     */
+    public static function generateToken($userId): string
+    {
+        $secret = api_get_configuration_value('security_key');
+        $time = time();
+
+        $payload = [
+            'iat' => $time,
+            'exp' => $time + (60 * 60 * 24),
+            'data' => [
+                'user' => $userId,
+            ],
+        ];
+
+        return JWT::encode($payload, $secret, 'HS384');
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return array
+     */
+    public static function getTokenData($token): array
+    {
+        $secret = api_get_configuration_value('security_key');
+        $jwt = JWT::decode($token, $secret, ['HS384']);
+
+        $data = (array) $jwt->data;
+
+        return $data;
+    }
+}
--- src/ApiBundle/GraphQL/Mutation/RootMutation.php
+++ src/ApiBundle/GraphQL/Mutation/RootMutation.php
@@ -3,9 +3,12 @@
 
 namespace Chamilo\ApiBundle\GraphQL\Mutation;
 
+use Chamilo\ApiBundle\GraphQL\Auth;
 use Chamilo\CoreBundle\Framework\Container;
+use Overblog\GraphQLBundle\Definition\Argument;
 use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+use Overblog\GraphQLBundle\Error\UserError;
 
 /**
  * Class RootMutation
@@ -24,6 +27,26 @@ class RootMutation implements MutationInterface, AliasedInterface
      */
     public static function getAliases()
     {
-        return [];
+        return [
+            'mutationAuthenticate' => 'authenticate',
+        ];
+    }
+
+    /**
+     * @param Argument $args
+     *
+     * @return array
+     */
+    public function mutationAuthenticate(Argument $args)
+    {
+        try {
+            $token = Auth::getUserToken($args['username'], $args['password']);
+        } catch (\Exception $exception) {
+            throw new UserError(get_lang('NotAllowed'));
+        }
+
+        return [
+            'token' => $token,
+        ];
     }
 }

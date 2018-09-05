1b45923bf3163afcc4347213486e40bc99bc9e2e
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-09-05 15:21:48 -0500

    GraphQL Create datetime type #2644

---
+++ src/ApiBundle/GraphQL/Type/DateTimeType.php
@@ -0,0 +1,44 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\ApiBundle\GraphQL\Type;
+
+use GraphQL\Language\AST\Node;
+
+/**
+ * Class DateTimeType
+ *
+ * @package Chamilo\ApiBundle\GraphQL\Type
+ */
+class DateTimeType
+{
+    /**
+     * @param \DateTime $value
+     *
+     * @return string
+     */
+    public static function serialize(\DateTime $value)
+    {
+        return $value->format(\DateTime::ATOM);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return \DateTime
+     */
+    public static function parseValue(string $value)
+    {
+        return new \DateTime($value, new \DateTimeZone('UTC'));
+    }
+
+    /**
+     * @param Node $valueNode
+     *
+     * @return \DateTime
+     */
+    public static function parseLiteral(Node $valueNode)
+    {
+        return new \DateTime($valueNode->value, new \DateTimeZone('UTC'));
+    }
+}

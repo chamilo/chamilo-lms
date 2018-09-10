520cffe0d8bb004af7dd91f18385713ecbf124c9
Julio Montoya <gugli100@gmail.com>
2018-09-10 15:31:44 +0200

    Minor - remove display_individual_user() move code into a controller
    - user.php file deleted using controller to render user information
    
    public/user/usernameX

---
+++ src/CoreBundle/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Controller;
+
+use Chamilo\CoreBundle\Controller\BaseController;
+use Chamilo\CoreBundle\Entity\CourseRelUser;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController.
+ *
+ * @Route("/user")
+ *
+ * @author Julio Montoya <gugli100@gmail.com>
+ */
+class UserController extends BaseController
+{
+    /**
+     * @Route("/{username}", methods={"GET"})
+     *
+     * @param string $username
+     *
+     * @Template("ChamiloCoreBundle:User:profile.html.twig")
+     *
+     * @return Response
+     */
+    public function profileAction($username): array
+    {
+        $user = $this->container->get('fos_user.user_manager')->findUserByUsername($username);
+
+        return ['user' => $user];
+    }
+}

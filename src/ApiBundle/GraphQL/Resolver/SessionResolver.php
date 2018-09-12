7c5bbe2c06f674747b0d718e70e47447c41a071a
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-09-12 15:19:37 -0500

    GraphQL add query for session courses #2644

--- src/ApiBundle/GraphQL/Resolver/SessionResolver.php
+++ src/ApiBundle/GraphQL/Resolver/SessionResolver.php
@@ -5,6 +5,8 @@ namespace Chamilo\ApiBundle\GraphQL\Resolver;
 
 use Chamilo\ApiBundle\GraphQL\ApiGraphQLTrait;
 use Chamilo\CoreBundle\Entity\Session;
+use Chamilo\CoreBundle\Entity\SessionRelCourse;
+use Chamilo\UserBundle\Entity\User;
 use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
 use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -32,6 +34,7 @@ class SessionResolver implements ResolverInterface, AliasedInterface, ContainerA
             'resolveDescription' => 'session_description',
             'resolveNumberCourses' => 'session_nbrcourses',
             'resolveNumberUsers' => 'session_nbrusers',
+            'resolveCourses' => 'session_courses',
         ];
     }
 
@@ -67,5 +70,41 @@ class SessionResolver implements ResolverInterface, AliasedInterface, ContainerA
     public function resolveNumberUsers(Session $session): int
     {
         return $session->getNbrUsers();
+    }
+
+    /**
+     * @param Session $session
+     *
+     * @return array
+     */
+    public function resolveCourses(Session $session): array
+    {
+        $authChecker = $this->container->get('security.authorization_checker');
+
+        if ($authChecker->isGranted('ROLE_ADMIN')) {
+            $courses = [];
+
+            /** @var SessionRelCourse $sessionCourse */
+            foreach ($session->getCourses() as $sessionCourse) {
+                $courses[] = $sessionCourse->getCourse();
+            }
+
+            return $courses;
+        }
+
+        $token = $this->container->get('security.token_storage')->getToken();
+        /** @var User $user */
+        $user = $token->getUser();
+        $courseList = \UserManager::get_courses_list_by_session($user->getId(), $session->getId());
+        $courseRepo = $this->em->getRepository('ChamiloCoreBundle:Course');
+        $qb = $courseRepo->createQueryBuilder('c');
+        $courses = $qb
+            ->where(
+                $qb->expr()->in('id', array_column($courseList, 'real_id'))
+            )
+            ->getQuery()
+            ->getResult();
+
+        return $courses;
     }
 }

2f4f1350841ebf27da58a65c08edfb67e1117916
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-09-28 09:14:22 -0500

    Filter forums by category in course #2644

--- src/CourseBundle/Repository/CForumForumRepository.php
+++ src/CourseBundle/Repository/CForumForumRepository.php
@@ -5,6 +5,7 @@ namespace Chamilo\CourseBundle\Repository;
 
 use Chamilo\CoreBundle\Entity\Course;
 use Chamilo\CoreBundle\Entity\Session;
+use Chamilo\CourseBundle\Entity\CForumCategory;
 use Chamilo\CourseBundle\Entity\CForumForum;
 use Chamilo\CourseBundle\Entity\CItemProperty;
 use Doctrine\ORM\EntityRepository;
@@ -17,17 +18,19 @@ use Doctrine\ORM\EntityRepository;
 class CForumForumRepository extends EntityRepository
 {
     /**
-     * @param bool         $isAllowedToEdit
-     * @param Course       $course
-     * @param Session|null $session
-     * @param bool         $includeGroupsForums
+     * @param bool           $isAllowedToEdit
+     * @param CForumCategory $category
+     * @param Course         $course
+     * @param Session|null   $session
+     * @param bool           $includeGroupsForums
      *
      * @return array
      *
      * @todo Remove api_get_session_condition
      */
-    public function findAllInCourse(
+    public function findAllInCourseByCategory(
         $isAllowedToEdit,
+        CForumCategory $category,
         Course $course,
         Session $session = null,
         $includeGroupsForums = true
@@ -43,11 +46,12 @@ class CForumForumRepository extends EntityRepository
             ? 'AND (f.forumOfGroup = 0 OR f.forumOfGroup IS NULL)'
             : '';
 
-        $dql = "SELECT ip, f
+        $dql = "SELECT f, ip
             FROM ChamiloCourseBundle:CForumForum AS f
             INNER JOIN ChamiloCourseBundle:CItemProperty AS ip
                 WITH (f.iid = ip.ref AND f.cId = ip.course)
             WHERE
+                f.forumCategory = :category AND
                 ip.tool = :tool AND
                 f.cId = :course
                 $conditionSession AND
@@ -58,16 +62,16 @@ class CForumForumRepository extends EntityRepository
         $result = $this
             ->_em
             ->createQuery($dql)
-            ->setParameters(['course' => $course, 'tool' => TOOL_FORUM])
+            ->setParameters(['category' => $category, 'course' => $course, 'tool' => TOOL_FORUM])
             ->getResult();
 
         $forums = [];
 
         for ($i = 0; $i < count($result); $i += 2) {
-            /** @var CItemProperty $ip */
-            $ip = $result[$i];
             /** @var CForumForum $f */
-            $f = $result[$i + 1];
+            $f = $result[$i];
+            /** @var CItemProperty $ip */
+            $ip = $result[$i + 1];
             $f->setItemProperty($ip);
 
             $forums[] = $f;
@@ -84,12 +88,12 @@ class CForumForumRepository extends EntityRepository
      */
     public function findOneInCourse($id, Course $course)
     {
-        $dql = "SELECT ip, f
+        $dql = "SELECT f, ip
             FROM ChamiloCourseBundle:CForumForum AS f
             INNER JOIN ChamiloCourseBundle:CItemProperty AS ip
                 WITH (f.iid = ip.ref AND f.cId = ip.course)
             WHERE
-                f.iid = :id
+                f.iid = :id AND
                 ip.tool = :tool AND
                 f.cId = :course AND
                 ip.visibility != 2";
@@ -104,10 +108,10 @@ class CForumForumRepository extends EntityRepository
             return null;
         }
 
-        /** @var CItemProperty $ip */
-        $ip = $result[0];
         /** @var CForumForum $f */
-        $f = $result[1];
+        $f = $result[0];
+        /** @var CItemProperty $ip */
+        $ip = $result[1];
         $f->setItemProperty($ip);
 
         return $f;

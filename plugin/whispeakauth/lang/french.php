4707a13176a01258a987dcb4496df2163057319d
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-10-08 13:14:29 -0500

    Whispeak plugin add french lang - refs BT#14921

---
+++ plugin/whispeakauth/lang/french.php
@@ -0,0 +1,15 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = 'Authentification vocale avec Whispeak';
+
+$strings['EnrollmentSampleText'] = 'Le fameux chef-d\'oeuvre Mona Lisa a été peint par Léonardo da Vinci.';
+
+$strings['RepeatThisPhrase'] = 'Autorisez l\'enregistrement audio puis répétez cette phrase trois fois:';
+
+$strings['SpeechAuthentication'] = 'Authentification de voix';
+$strings['EnrollmentFailed'] = 'Échec à l\'inscription.';
+$strings['EnrollmentSuccess'] = 'Inscription réussie.';
+$strings['AuthentifyFailed'] = 'Échec de l\'authentification.';
+$strings['AuthentifySuccess'] = 'Authentification réussie!';
+$strings['TryAgain'] = 'Essayez encore';

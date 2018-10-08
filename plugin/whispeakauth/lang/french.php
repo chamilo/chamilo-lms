f5efac45c0e689a7e0128f977ce324682765eb1b
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-10-08 14:23:26 -0500

    Show reliability when authentifing with whispeak - refs BT#14921

--- plugin/whispeakauth/lang/french.php
+++ plugin/whispeakauth/lang/french.php
@@ -6,7 +6,10 @@ $strings['plugin_title'] = 'Authentification vocale avec Whispeak';
 $strings['EnrollmentSampleText'] = 'Le fameux chef-d\'oeuvre Mona Lisa a été peint par Léonardo da Vinci.';
 
 $strings['RepeatThisPhrase'] = 'Autorisez l\'enregistrement audio puis répétez cette phrase trois fois:';
-
+$strings['EnrollmentSignature0'] = 'Signature non viable, nécessite un nouvel enrôlement';
+$strings['EnrollmentSignature1'] = 'Signature passable, conseil de faire un nouvel enrôlement.';
+$strings['EnrollmentSignature2'] = 'Signature correcte.';
+$strings['EnrollmentSignature3'] = 'Signature bonne.';
 $strings['SpeechAuthentication'] = 'Authentification de voix';
 $strings['EnrollmentFailed'] = 'Échec à l\'inscription.';
 $strings['EnrollmentSuccess'] = 'Inscription réussie.';

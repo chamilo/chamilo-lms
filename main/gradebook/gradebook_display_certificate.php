535356516f1e9aa36f13229b8f622766f988efe2
Julio Montoya <gugli100@gmail.com>
2018-10-18 14:28:16 +0200

    Fix error when replacing urls in a HTML when exporting to a pdf file
    See BT#14981

--- main/gradebook/gradebook_display_certificate.php
+++ main/gradebook/gradebook_display_certificate.php
@@ -131,7 +131,8 @@ switch ($action) {
         if (api_get_plugin_setting('customcertificate', 'enable_plugin_customcertificate') == 'true' &&
             api_get_course_setting('customcertificate_course_enable', $courseCode) == 1
         ) {
-            $url = api_get_path(WEB_PLUGIN_PATH).'customcertificate/src/print_certificate.php?export_all=1';
+            $params = 'course_code='.api_get_course_id().'&session_id='.api_get_session_id().'&'.api_get_cidreq();
+            $url = api_get_path(WEB_PLUGIN_PATH).'customcertificate/src/print_certificate.php?export_all=1&'.$params;
         } else {
             if (api_is_student_boss()) {
                 $userGroup = new UserGroup();

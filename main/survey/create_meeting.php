9bac59dcef174bea7adebcd6af77e49fefd6eaa2
Julio Montoya <gugli100@gmail.com>
2018-11-14 10:20:32 +0100

    Add survey meeting new files see BT#14957

---
+++ main/survey/create_meeting.php
@@ -0,0 +1,183 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+require_once __DIR__.'/../inc/global.inc.php';
+
+$this_section = SECTION_COURSES;
+
+if (!api_is_allowed_to_edit()) {
+    api_not_allowed(true);
+}
+$htmlHeadXtra[] = api_get_css_asset('jt.timepicker/jquery.timepicker.css');
+$htmlHeadXtra[] = api_get_asset('jt.timepicker/jquery.timepicker.js');
+$htmlHeadXtra[] = api_get_asset('datepair.js/dist/datepair.js');
+$htmlHeadXtra[] = api_get_asset('datepair.js/dist/jquery.datepair.js');
+
+$interbreadcrumb[] = [
+    'url' => api_get_path(WEB_CODE_PATH).'survey/survey_list.php?'.api_get_cidreq(),
+    'name' => get_lang('SurveyList'),
+];
+
+$courseInfo = api_get_course_info();
+
+$tool_name = get_lang('CreateMeeting');
+
+$form = new FormValidator(
+    'survey',
+    'post',
+    api_get_self().'?action=add&'.api_get_cidreq()
+);
+
+$form->addElement('header', $tool_name);
+$form->addHidden('anonymous', 0);
+$form->addHidden('survey_language', $courseInfo['language']);
+$form->addHidden('survey_subtitle', '');
+$form->addHidden('survey_thanks', '');
+$form->addHidden('visible_results', '0');
+$form->addHidden('survey_type', 3);
+
+// Setting the form elements
+/*if ($_GET['action'] == 'edit' && isset($survey_id) && is_numeric($survey_id)) {
+    $form->addElement('hidden', 'survey_id');
+}*/
+
+$text = $form->addElement(
+    'text',
+    'survey_title',
+    get_lang('Title'),
+    null,
+    ['ToolbarSet' => 'Survey', 'Width' => '100%', 'Height' => '200']
+);
+
+$form->addDateTimePicker('start_date', get_lang('StartDate'));
+$form->addDateTimePicker('end_date', get_lang('EndDate'));
+
+$form->addRule('start_date', get_lang('InvalidDate'), 'datetime');
+$form->addRule('end_date', get_lang('InvalidDate'), 'datetime');
+$form->addRule(
+    ['start_date', 'end_date'],
+    get_lang('StartDateShouldBeBeforeEndDate'),
+    'date_compare',
+    'lte'
+);
+
+$form->addHtmlEditor('survey_introduction', get_lang('Description'), false);
+$form->setRequired($text);
+
+$hideList = '';
+$maxEvents = 20;
+for ($i = 1; $i <= $maxEvents; $i++) {
+    $name = 'time_'.$i;
+    $form->addDateTimeRangePicker($name, get_lang('Time'));
+    if ($i > 3) {
+        $hideList .= "$('#".$name."_alt').parent().parent().parent().hide();";
+    }
+}
+
+$form->addHtml('<script>
+$(function() {
+    '.$hideList.'
+    var number = 3;    
+    $("#add_button").on("click", function() {
+        number++;
+        $("#time_" + number + "_alt").parent().parent().parent().show();
+    });
+    
+    $("#remove_button").on("click", function() {
+        if (number > 1) {            
+            $("#time_" + number + "_alt").parent().parent().parent().hide();
+            number--;
+        }
+    });
+});
+</script>');
+
+$form->addLabel(
+    '',
+    Display::url(get_lang('Add'), 'javascript:void(0)', ['id' => 'add_button', 'class' => 'btn btn-default'])
+    .' '.
+    Display::url(
+        get_lang('Remove'),
+        'javascript:void(0)',
+        ['id' => 'remove_button', 'class' => 'btn btn-danger']
+    )
+);
+
+$form->addButtonCreate(get_lang('CreateSurvey'), 'submit_survey');
+
+$defaults = [];
+$form->setDefaults($defaults);
+
+// The validation or display
+if ($form->validate()) {
+    // Exporting the values
+    $values = $form->getSubmitValues();
+    $values['survey_code'] = SurveyManager::generateSurveyCode($values['survey_title']);
+    // Storing the survey
+    $surveyData = SurveyManager::store_survey($values);
+
+    $dates = [];
+    for ($i = 1; $i <= $maxEvents; $i++) {
+        $name = 'time_'.$i;
+        if (isset($values[$name]) && !empty($values[$name])) {
+            $date = $values[$name];
+
+            if (empty($date)) {
+                continue;
+            }
+
+            $start = $name.'_time_range_start';
+            $end = $name.'_time_range_end';
+
+            $start = $values[$start];
+            $end = $values[$end];
+
+            $start = api_get_utc_datetime($values[$name].' '.$start, true);
+            $end = api_get_utc_datetime($values[$name].' '.$end, true);
+
+            if (!empty($start) && !empty($start)) {
+                $row = [
+                    'start' => $start,
+                    'end' => $end,
+                ];
+                $dates[] = $row;
+            }
+        }
+    }
+
+    $questionTable = Database::get_course_table(TABLE_SURVEY_QUESTION);
+    $counter = 1;
+    if (!empty($surveyData['id'])) {
+        foreach ($dates as $date) {
+            //SurveyManager::save_question();
+            $params = [
+                'c_id' => api_get_course_int_id(),
+                'survey_id' => $surveyData['id'],
+                'survey_question' => $date['start'].'@@'.$date['end'],
+                'survey_question_comment' => '',
+                'type' => 'doodle',
+                'display' => 'horizontal',
+                'sort' => $counter,
+                'shared_question_id' => '0',
+                'max_value' => 0,
+            ];
+            $questionId = Database::insert($questionTable, $params);
+            if ($questionId) {
+                $sql = "UPDATE $questionTable SET question_id = $questionId
+                        WHERE iid = $questionId";
+                Database::query($sql);
+            }
+            $counter++;
+        }
+    }
+
+    // Redirecting to the survey page (whilst showing the return message)
+    header('Location: '.api_get_path(WEB_CODE_PATH).'survey/survey_list.php?'.api_get_cidreq());
+    exit;
+} else {
+    // Displaying the header
+    Display::display_header($tool_name);
+    $form->display();
+}
+
+Display::display_footer();
---
+++ main/survey/meeting.php
@@ -0,0 +1,264 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+require_once __DIR__.'/../inc/global.inc.php';
+
+api_protect_course_script(true);
+
+$sessionId = api_get_session_id();
+$courseId = api_get_course_int_id();
+$userId = api_get_user_id();
+$courseInfo = api_get_course_info();
+
+$surveyId = isset($_REQUEST['survey_id']) ? (int) $_REQUEST['survey_id'] : 0;
+$invitationcode = isset($_REQUEST['invitationcode']) ? Database::escape_string($_REQUEST['invitationcode']) : 0;
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+if (!api_is_allowed_to_edit() || !empty($invitationcode)) {
+    $table_survey_invitation = Database::get_course_table(TABLE_SURVEY_INVITATION);
+    $table_survey = Database::get_course_table(TABLE_SURVEY);
+
+    $sql = "SELECT * FROM $table_survey_invitation
+            WHERE
+                c_id = $courseId AND
+                invitation_code = '".Database::escape_string($invitationcode)."'";
+    $result = Database::query($sql);
+    if (Database::num_rows($result) < 1) {
+        api_not_allowed(true, get_lang('WrongInvitationCode'));
+    }
+
+    $survey_invitation = Database::fetch_array($result, 'ASSOC');
+    $sql = "SELECT * FROM $table_survey
+            WHERE
+                c_id = $courseId AND
+                code = '".Database::escape_string($survey_invitation['survey_code'])."'";
+    $sql .= api_get_session_condition($sessionId);
+    $result = Database::query($sql);
+    $result = Database::fetch_array($result, 'ASSOC');
+    $surveyId = $result['iid'];
+}
+
+// getting all the students of the course
+/*if (empty($sessionId)) {
+    // Registered students in a course outside session.
+    $students = CourseManager:: get_student_list_from_course_code(
+        api_get_course_id(),
+        false,
+        0,
+        null,
+        null,
+        true
+    );
+} else {
+    // Registered students in session.
+    $students = CourseManager:: get_student_list_from_course_code(
+        api_get_course_id(),
+        true,
+        $sessionId
+    );
+}*/
+
+$surveyData = SurveyManager::get_survey($surveyId);
+
+if (empty($surveyData)) {
+    api_not_allowed(true);
+}
+
+$invitations = SurveyUtil::get_invited_users($surveyData['code']);
+$students = isset($invitations['course_users']) ? $invitations['course_users'] : [];
+
+$content = Display::page_header($surveyData['title']);
+
+$interbreadcrumb[] = [
+    'url' => api_get_path(WEB_CODE_PATH).
+        'survey/survey_list.php?cidReq='.$courseInfo['code'].'&id_session='.$sessionId,
+    'name' => get_lang('SurveyList'),
+];
+
+$questions = SurveyManager::get_questions($surveyData['iid']);
+
+$url = api_get_self().'?survey_id='.$surveyId.'&invitationcode='.$invitationcode.'&'.api_get_cidreq();
+$urlEdit = $url.'&action=edit';
+
+if (isset($_POST) && !empty($_POST)) {
+    $options = isset($_POST['options']) ? array_keys($_POST['options']) : [];
+
+    foreach ($questions as $item) {
+        $questionId = $item['question_id'];
+        SurveyUtil::remove_answer(
+            $userId,
+            $surveyId,
+            $questionId,
+            $courseId
+        );
+    }
+
+    $status = 1;
+    foreach ($options as $selectedQuestionId) {
+        SurveyUtil::store_answer(
+            $userId,
+            $surveyId,
+            $selectedQuestionId,
+            1,
+            $status,
+            $surveyData
+        );
+    }
+    Display::addFlash(Display::return_message(get_lang('Saved')));
+    header('Location: '.$url);
+    exit;
+}
+
+$template = new Template();
+
+$table = new HTML_Table(['class' => 'table']);
+
+$row = 0;
+$column = 1;
+$answerList = [];
+foreach ($questions as $item) {
+    $answers = SurveyUtil::get_answers_of_question_by_user($surveyId, $item['question_id']);
+    foreach ($answers as $tempUserId => &$value) {
+        $value = $value[0];
+        $value = explode('*', $value);
+        $value = isset($value[1]) ? $value[1] : 0;
+    }
+    $answerList[$item['question_id']] = $answers;
+    $parts = explode('@@', $item['question']);
+    $startDateTime = api_get_local_time($parts[0]);
+    $endDateTime = api_get_local_time($parts[1]);
+
+    $date = explode(' ', $startDateTime);
+    $endDate = explode(' ', $endDateTime);
+    $mainDate = $date[0];
+    $startTime = $date[1];
+    $endTime = $endDate[1];
+
+    $mainDate = api_format_date($mainDate, DATE_FORMAT_SHORT);
+    $table->setHeaderContents($row, $column, "<h4>$mainDate</h4> $startTime <br >$endTime");
+    $column++;
+}
+
+$row = 1;
+$column = 0;
+// Total counter
+$table->setHeaderContents(
+    $row,
+    0,
+    get_lang('NumberOfUsers').': '.count($students)
+);
+foreach ($questions as $item) {
+    $count = 0;
+    if (isset($answerList[$item['question_id']])) {
+        $count = '<p style="color:cornflowerblue" >
+            <span class="fa fa-check fa-2x"></span>'.count($answerList[$item['question_id']]).'</p>';
+    }
+    $table->setHeaderContents(
+        $row,
+        ++$column,
+        $count
+    );
+}
+
+$row = 2;
+$column = 0;
+foreach ($students as $studentId) {
+    $userInfo = api_get_user_info($studentId);
+    $name = $userInfo['complete_name'];
+    if ($userId == $studentId) {
+        if ($action !== 'edit') {
+            $name .= Display::url(
+                Display::return_icon('edit.png', get_lang('Edit')),
+                $urlEdit
+            );
+        }
+        $rowColumn = 1;
+        foreach ($questions as $item) {
+            $checked = '';
+            $html = '';
+            if (isset($answerList[$item['question_id']][$studentId])) {
+                $checked = Display::return_icon('check-circle.png');
+                if ($action === 'edit') {
+                    $checked = 'checked';
+                }
+            }
+
+            if ($action === 'edit') {
+                $html = '<div class="alert alert-info"><input 
+                    id="'.$item['question_id'].'" 
+                    name="options['.$item['question_id'].']" 
+                    class="question" '.$checked.' 
+                    type="checkbox" 
+                /></div>';
+            } else {
+                $html = $checked;
+            }
+
+            $table->setHeaderContents(
+                $row,
+                $rowColumn,
+                $html
+            );
+            $rowColumn++;
+        }
+    } else {
+        $rowColumn = 1;
+        foreach ($questions as $item) {
+            $checked = '';
+            if (isset($answerList[$item['question_id']][$studentId])) {
+                $checked = Display::return_icon('check-circle.png');
+            }
+            $table->setHeaderContents(
+                $row,
+                $rowColumn,
+                $checked
+            );
+            $rowColumn++;
+        }
+    }
+    $column = 0;
+    $table->setCellContents($row, $column, $name);
+    $class = 'class="row_odd"';
+    if ($row % 2) {
+        $class = 'class="row_even"';
+    }
+    $row++;
+}
+if ($action === 'edit') {
+    $content .= '<form name="meeting" action="'.$urlEdit.'" method="post">';
+}
+
+$content .= $table->toHtml();
+
+if ($action === 'edit') {
+    $content .= '<div class="pull-right">
+        <button name="submit" type="submit" class="btn btn-primary btn-lg">'.get_lang('Save').'</button></div>';
+    $content .= '</form>';
+}
+
+/*$ajaxUrl = api_get_path(WEB_AJAX_PATH).
+    'survey.ajax.php?a=save_question&'.api_get_cidreq().'&survey_id='.$surveyId.'&question_id=';
+
+$content .= '<script>
+$(function() {
+    $(".question").on("change", function() {
+        var questionId = $(this).attr("id");
+        var status = 0;
+        if ($(this).prop("checked")) {
+            status = 1;
+        }
+        $.ajax({
+            url: "'.$ajaxUrl.'" + questionId + "&status=" + status,
+            success: function (data) {
+                $("#ajax_result").html("<div class=\"alert alert-info\">"+ "'.get_lang('Saved').'" + "</div>");
+                $("#ajax_result").show();
+                return;
+            },
+        });
+    });
+
+});
+</script>';*/
+
+$template->assign('content', $content);
+$template->display_one_col_template();

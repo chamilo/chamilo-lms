2527e87f87a67ae75ae171a674256e3bda2b2f51
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-11-14 14:19:32 -0500

    WIP LTI adding unsupported service response - refs BT#13469

---
+++ plugin/ims_lti/src/ImsLtiServiceUnsupportedRequest.php
@@ -0,0 +1,31 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/**
+ * Class ImsLtiServiceUnsupportedRequest.
+ */
+class ImsLtiServiceUnsupportedRequest extends ImsLtiServiceRequest
+{
+    /**
+     * ImsLtiDeleteServiceRequest constructor.
+     *
+     * @param SimpleXMLElement $xml
+     * @param string           $name
+     */
+    public function __construct(SimpleXMLElement $xml, $name)
+    {
+        parent::__construct($xml);
+
+        $this->responseType = $name;
+    }
+
+    protected function processBody()
+    {
+        $this->statusInfo
+            ->setSeverity(ImsLtiServiceResponseStatus::SEVERITY_STATUS)
+            ->setCodeMajor(ImsLtiServiceResponseStatus::CODEMAJOR_UNSUPPORTED)
+            ->setDescription(
+                $this->responseType.' is not supported'
+            );
+    }
+}

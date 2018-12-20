2b552d9ae15b2546f2791a57ea2300c56b38b4b8
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2018-12-20 11:01:35 -0500

    Add Start button to Reading Comprehension when showing all in one page
    Refs #2082

--- main/inc/lib/exercise.lib.php
+++ main/inc/lib/exercise.lib.php
@@ -446,6 +446,7 @@ class ExerciseLib
 
             $hidingClass = '';
             if ($answerType == READING_COMPREHENSION) {
+                $objQuestionTmp->setExerciseType($exercise->selectType());
                 $objQuestionTmp->processText($objQuestionTmp->selectDescription());
                 $hidingClass = 'hide-reading-answers';
                 $s .= Display::div(

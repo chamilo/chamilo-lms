b12d2f98a88d529bbe454078abf069fed5422180
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-02-19 16:52:51 -0500

    Plugin: Rename to SurveyExportTxt - refs BT#15281

--- plugin/apaexportsurvey/lang/spanish.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* For licensing terms, see /license.txt */
-
-$strings['plugin_title'] = "APA Export Survey";
-$strings['plugin_comment'] = "Exportar encuestas para APA.";
-
-$strings['enabled'] = 'Habilitado';
---
+++ plugin/surveyexporttxt/lang/english.php
@@ -0,0 +1,7 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = "Survey Export TXT";
+$strings['plugin_comment'] = "Export surveys results to TXT file";
+
+$strings['enabled'] = 'Enabled';

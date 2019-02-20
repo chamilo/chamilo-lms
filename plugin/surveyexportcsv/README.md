8c62d78b3d91e29df943047d932c70c554e18479
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-02-20 17:12:12 -0500

    Add SurveyExportCsv plugin - refs BT#15280

---
+++ plugin/surveyexportcsv/README.md
@@ -0,0 +1,10 @@
+# Survey Export CSV
+
+Export surveys to CSV file.
+
+This plugin will add a new action button in survey list allowing export the survey.
+
+**Instructions**
+
+- Install plugin
+- Set enabled in configuration
---
+++ plugin/surveyexportcsv/lang/spanish.php
@@ -0,0 +1,7 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = "Survey Export CSV";
+$strings['plugin_comment'] = "Exportar resultados de encuestas en archivo CSV.";
+
+$strings['enabled'] = 'Habilitado';

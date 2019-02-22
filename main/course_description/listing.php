cbc92b7e4801361cfdeba5d202c8e28ec435a56e
Alex Aragón <aragcar@gmail.com>
2019-02-22 16:27:39 -0500

    fix course description -refs #2681

---
+++ main/course_description/listing.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Template (view in MVC pattern) used for listing course descriptions
+* @author Christian Fasanando <christian1827@gmail.com>
+* @package chamilo.course_description
+*/
+
+// protect a course script
+api_protect_course_script(true);
+
+echo $toolbar;
+echo $descriptions;

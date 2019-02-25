941887eebc08fe32fac356baa800c14e8379256f
Alex Aragón <aragcar@gmail.com>
2019-02-25 10:43:31 -0500

    adding new configuration parameter 'forum_fold_categories' - refs #15317

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -1110,3 +1110,6 @@ VALUES (2, 13, 'session_courses_read_only_mode', 'Lock Course In Session', 1, 1,
 // You need add a new option called "confirmation" to the registration settings
 //INSERT INTO settings_options (variable, value, display_text) VALUES ('allow_registration', 'confirmation', 'MailConfirmation');
 // ------ (End) Custom DB changes
+
+// Visually "fold" forum categories by default
+// $_configuration['forum_fold_categories'] = false;

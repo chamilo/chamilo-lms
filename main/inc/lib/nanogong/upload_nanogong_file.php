2164d36f0b0f61f342dff08d7ef977634e05e876
Julio Montoya <gugli100@gmail.com>
2019-02-26 09:36:09 +0100

    Remove nanogong files (deprecated).

--- main/inc/lib/nanogong/upload_nanogong_file.php
+++ main/inc/lib/nanogong/upload_nanogong_file.php
@@ -1,7 +1,11 @@
 <?php
 /* For licensing terms, see /license.txt */
 
+exit;
+
+
 use ChamiloSession as Session;
+
 
 /**
  *    This file allows creating new svg and png documents with an online editor.

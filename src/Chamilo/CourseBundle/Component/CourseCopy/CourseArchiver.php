c245b03308f8274b93f2a39e5435d5e9e4b6aecf
Julio Montoya <gugli100@gmail.com>
2019-02-26 11:40:57 +0100

    Use "clean_up_files_in_zip" function before extracting content
    Blocks php/htaccess files

--- src/Chamilo/CourseBundle/Component/CourseCopy/CourseArchiver.php
+++ src/Chamilo/CourseBundle/Component/CourseCopy/CourseArchiver.php
@@ -293,7 +293,13 @@ class CourseArchiver
         // unzip the archive
         $zip = new \PclZip($unzip_dir.'/backup.zip');
         @chdir($unzip_dir);
-        $zip->extract(PCLZIP_OPT_TEMP_FILE_ON);
+
+        $zip->extract(
+            PCLZIP_OPT_TEMP_FILE_ON,
+            PCLZIP_CB_PRE_EXTRACT,
+            'clean_up_files_in_zip'
+        );
+
         // remove the archive-file
         if ($delete) {
             @unlink($filePath);

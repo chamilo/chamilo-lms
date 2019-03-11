399d7ce660f5334b3743a819148a837cb519c39d
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-03-11 17:50:09 -0500

    Add questionoptionsevaluation plugin - refs BT#15265
    Allow recalulate the options score in questions

---
+++ plugin/questionoptionsevaluation/evaluation.php
@@ -0,0 +1,139 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+require_once __DIR__.'/../../main/inc/global.inc.php';
+
+api_protect_teacher_script();
+api_protect_course_script();
+
+$exerciseId = isset($_REQUEST['exercise']) ? (int) $_REQUEST['exercise'] : 0;
+
+if (empty($exerciseId)) {
+    echo Display::return_message(get_lang('NotAllowed'), 'error');
+
+    exit;
+}
+
+$exercise = new Exercise();
+
+if (!$exercise->read($exerciseId)) {
+    echo Display::return_message(get_lang('ExerciseNotFound'), 'error');
+
+    exit;
+}
+
+$plugin = QuestionOptionsEvaluationPlugin::create();
+
+if ($plugin->get('enable') !== 'true') {
+    echo Display::return_message(get_lang('NotAllowed'), 'error');
+
+    exit;
+}
+
+$form = new FormValidator('evaluation');
+
+$form->addRadio(
+    'formula',
+    $plugin->get_lang('EvaluationFormula'),
+    [
+        $plugin->get_lang('NoFormula'),
+        $plugin->get_lang('Formula1'),
+        $plugin->get_lang('Formula2'),
+        $plugin->get_lang('Formula3'),
+    ]
+)->setColumnsSize([4, 7, 1]);
+$form->addButtonSave(get_lang('Save'))->setColumnsSize([4, 7, 1]);
+$form->addHidden('exercise', $exerciseId);
+
+if ($form->validate()) {
+    $values = $form->exportValues();
+    $formula = isset($values['formula']) ? (int) $values['formula'] : 0;
+
+    $nbrQuestions = count($exercise->questionList);
+
+    foreach ($exercise->questionList as $questionId) {
+        $question = Question::read($questionId);
+
+        if (!in_array($question->selectType(), [UNIQUE_ANSWER, MULTIPLE_ANSWER])) {
+            continue;
+        }
+
+        $questionAnswers = new Answer($questionId, 0,$exercise);
+        $counts = array_count_values($questionAnswers->correct);
+
+        $weighting = [];
+
+        foreach ($questionAnswers->correct as $i => $correct) {
+            // successes
+            if (1 == $correct) {
+                $weighting[$i] = 10 / $counts[1] / $nbrQuestions;
+
+                continue;
+            }
+
+            // failures
+            switch ($formula) {
+                case 0:
+                default:
+                    $weighting[$i] = isset($questionAnswers->weighting[$i]) ? $questionAnswers->weighting[$i] : 0;
+                    break;
+                case 1:
+                    $weighting[$i] = -10 / $nbrQuestions;
+                    break;
+                case 2:
+                    $weighting[$i] = -10 / 2 / $nbrQuestions;
+                    break;
+                case 3:
+                    $weighting[$i] = -10 / 3 / $nbrQuestions;
+                    break;
+            }
+        }
+
+        $weighting = array_map(
+            function ($weight) {
+                return float_format($weight);
+            },
+            $weighting
+        );
+
+        $questionAnswers->new_nbrAnswers = $questionAnswers->nbrAnswers;
+        $questionAnswers->new_answer = $questionAnswers->answer;
+        $questionAnswers->new_comment = $questionAnswers->comment;
+        $questionAnswers->new_correct = $questionAnswers->correct;
+        $questionAnswers->new_weighting = $weighting;
+        $questionAnswers->new_position = $questionAnswers->position;
+        $questionAnswers->new_destination = $questionAnswers->destination;
+        $questionAnswers->new_hotspot_coordinates = $questionAnswers->hotspot_coordinates;
+        $questionAnswers->new_hotspot_type = $questionAnswers->hotspot_type;
+
+        $allowedWeights = array_filter(
+            $weighting,
+            function ($weight) {
+                return $weight > 0;
+            }
+        );
+
+        $questionAnswers->save();
+        $question->updateWeighting(
+            array_sum($allowedWeights)
+        );
+        $question->save($exercise);
+    }
+
+    Display::addFlash(
+        Display::return_message($plugin->get_lang('QuestionsEvaluated'))
+    );
+
+    header(
+        'Location: '.api_get_path(WEB_CODE_PATH).'exercise/exercise.php?'.api_get_cidreq()."&exerciseId=$exerciseId"
+    );
+
+    exit;
+}
+
+echo Display::return_message(
+    $plugin->get_lang('QuizQuestionsScoreRulesTitleConfirm'),
+    'warning'
+);
+
+$form->display();
---
+++ plugin/questionoptionsevaluation/lang/english.php
@@ -0,0 +1,15 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = 'Question options evaluation';
+$strings['plugin_comment'] = 'Allow recalulate the options score in questions';
+
+$strings['enable'] = 'Enable';
+
+$strings['QuizQuestionsScoreRulesTitleConfirm'] = 'Changing the evaluation formula generates changes for each question in the exercise and prevents undoing this change below. Are you sure you want to proceed?';
+$strings['EvaluationFormula'] = 'Evaluation formula';
+$strings['NoFormula'] = 'No formula';
+$strings['Formula1'] = 'Successes - Failures';
+$strings['Formula2'] = 'Successes - Failures / 2';
+$strings['Formula3'] = 'Successes - Failures / 3';
+$strings['QuestionsEvaluated'] = 'Questions evaluated';

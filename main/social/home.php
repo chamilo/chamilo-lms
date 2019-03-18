2e6cfd9075e4d3cd2c20057bf6b6d21667d24b8c
Alex Aragón <aragcar@gmail.com>
2019-03-18 11:14:42 -0500

    fix social search - refs BT#15312

--- main/social/home.php
+++ main/social/home.php
@@ -233,7 +233,7 @@ $formSearch->addText(
     [
         'aria-label' => get_lang('SearchUsers'),
         'custom' => true,
-        'placeholder' => get_lang('ByName'),
+        'placeholder' => get_lang('Rechercher'),
     ]
 );
 //$formSearch->addButtonSearch(get_lang('Search'));

e76cd54acccec27cc50560ca51c16b9869c66bba
Julio <gugli100@gmail.com>
2019-04-03 09:04:36 +0200

    Add missing twig extension

--- src/CoreBundle/Twig/Extension/ChamiloExtension.php
+++ src/CoreBundle/Twig/Extension/ChamiloExtension.php
@@ -27,6 +27,7 @@ class ChamiloExtension extends AbstractExtension
             new TwigFilter('icon', 'Display::get_icon_path'),
             new TwigFilter('img', 'Display::get_image'),
             new TwigFilter('api_get_local_time', 'api_get_local_time'),
+            new TwigFilter('api_convert_and_format_date', 'api_convert_and_format_date'),
             new TwigFilter('format_date', 'api_format_date'),
             new TwigFilter('date_to_time_ago', 'Display::dateToStringAgoAndLongDate'),
             new TwigFilter('api_get_configuration_value', 'api_get_configuration_value'),

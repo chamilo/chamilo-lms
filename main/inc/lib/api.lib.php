b730c8e823c6b3cba45ff658da54a184b7d9d637
Julio Montoya <gugli100@gmail.com>
2019-04-11 10:17:09 +0200

    Add language filter in course home see BT#15166

--- main/inc/lib/api.lib.php
+++ main/inc/lib/api.lib.php
@@ -9460,3 +9460,91 @@ function api_find_template($template)
 {
     return Template::findTemplateFilePath($template);
 }
+
+/**
+ * @return array
+ */
+function api_get_language_list_for_flag()
+{
+    $table = Database::get_main_table(TABLE_MAIN_LANGUAGE);
+    $sql = "SELECT english_name, isocode FROM $table 
+            ORDER BY original_name ASC";
+    static $languages = [];
+    if (empty($languages)) {
+        $result = Database::query($sql);
+        while ($row = Database::fetch_array($result)) {
+            $languages[$row['english_name']] = $row['isocode'];
+        }
+        $languages['english'] = 'gb';
+    }
+
+    return $languages;
+}
+
+/**
+ *
+ * @return string
+ *
+ */
+function api_get_language_translate_html()
+{
+    $translate = api_get_configuration_value('translate_html');
+
+    if (!$translate) {
+       return '';
+    }
+
+    $languageList = api_get_languages();
+    $hideAll = '';
+    foreach ($languageList['all'] as $language) {
+        $hideAll .=
+            '$("span:lang('.$language['isocode'].')").filter(
+                function() {
+                    // Ignore ckeditor classes
+                    return !this.className.match(/cke(.*)/);
+            }).hide();';
+    }
+
+    $userInfo = api_get_user_info();
+    $languageId = api_get_language_id($userInfo['language']);
+    $languageInfo = api_get_language_info($languageId);
+
+    return '
+            $(function() {
+                '.$hideAll.'                 
+                var defaultLanguageFromUser = "'.$languageInfo['isocode'].'";   
+                                             
+                $("span:lang('.$languageInfo['isocode'].')").filter(
+                    function() {
+                        // Ignore ckeditor classes
+                        return !this.className.match(/cke(.*)/);
+                }).show();
+                
+                var defaultLanguage = "";
+                var langFromUserFound = false;
+                
+                $(this).find("span").filter(
+                    function() {
+                        // Ignore ckeditor classes
+                        return !this.className.match(/cke(.*)/);
+                }).each(function() {
+                    defaultLanguage = $(this).attr("lang");                            
+                    if (defaultLanguage) {
+                        $(this).before().next("br").remove();                
+                        if (defaultLanguageFromUser == defaultLanguage) {
+                            langFromUserFound = true;
+                        }
+                    }
+                });
+                
+                // Show default language
+                if (langFromUserFound == false && defaultLanguage) {
+                    $("span:lang("+defaultLanguage+")").filter(
+                    function() {
+                            // Ignore ckeditor classes
+                            return !this.className.match(/cke(.*)/);
+                    }).show();
+                }
+            });
+    ';
+}

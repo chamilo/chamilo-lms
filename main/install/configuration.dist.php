d9c0ba6166a5394689f6e8aac77fa00272c62034
Yannick Warnier <ywarnier@beeznest.org>
2019-04-22 16:19:09 -0500

    Minor - Change location of new config option

--- main/install/configuration.dist.php
+++ main/install/configuration.dist.php
@@ -1191,6 +1191,8 @@ $_configuration['required_extra_fields_in_profile'] = [
 
 // Block student's access to the course documents when using the ckeditor "Browse server" button
 //$_configuration['block_editor_file_manager_for_students'] = false;
+// Show a language flag next to the user picture in the social network
+//$_configuration['social_show_language_flag_in_profile'] = false;
 
 // KEEP THIS AT THE END
 // -------- Custom DB changes
@@ -1199,5 +1201,3 @@ $_configuration['required_extra_fields_in_profile'] = [
 // You need add a new option called "confirmation" to the registration settings
 //INSERT INTO settings_options (variable, value, display_text) VALUES ('allow_registration', 'confirmation', 'MailConfirmation');
 // ------ (End) Custom DB changes
-// Show a language flag next to the user picture in the social network
-//$_configuration['social_show_language_flag_in_profile'] = false;

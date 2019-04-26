chamilo/chamilo-lms

commit d8fdd1f410cdcc8fcce7580d677f4045475ec267

Minor - format code, fix php docs (from master)

main/exercise/hotspot.class.php
@@ -77,17 +77,15 @@ class HotSpot extends Question
      */
     public function processCreation($form, $exercise)
     {
-        $file_info = $form->getSubmitValue('imageUpload');
+        $fileInfo = $form->getSubmitValue('imageUpload');
         parent::processCreation($form, $exercise);
 
-        if (!empty($file_info['tmp_name'])) {
-            $result = $this->uploadPicture($file_info['tmp_name']);
+        if (!empty($fileInfo['tmp_name'])) {
+            $result = $this->uploadPicture($fileInfo);
             if ($result) {
                 $this->save($exercise);
 
                 return true;
-            } else {
-                return false;
             }
         }
 

main/coursecopy/copy_course_session.php
@@ -9,7 +9,7 @@ use ChamiloSession as Session;
 /**
  * Copy resources from one course in a session to another one.
  *
- * @author Christian Fasanando <christian.fasanando@dokeos.com>
+ * @author Christian Fasanando
  * @author Julio Montoya <gugli100@gmail.com> Lots of bug fixes/improvements
  *
  * @package chamilo.backup

main/coursecopy/copy_course_session_selected.php
@@ -9,7 +9,7 @@ use ChamiloSession as Session;
 /**
  * Copy resources from one course in a session to another one.
  *
- * @author Christian Fasanando <christian.fasanando@dokeos.com>
+ * @author Christian Fasanando
  * @author Julio Montoya <gugli100@gmail.com> Lots of bug fixes/improvements
  * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com> Code conventions
  *

main/document/create_audio.php
@@ -113,7 +113,6 @@ if (!($is_allowed_to_edit || $groupRights ||
     api_not_allowed(true);
 }
 
-/*	Header */
 Event::event_access_tool(TOOL_DOCUMENT);
 
 $display_dir = $dir;

main/document/create_document.php
@@ -63,10 +63,9 @@ if (isset($_REQUEST['certificate']) && $_REQUEST['certificate'] == 'true') {
     $is_certificate_mode = true;
 }
 
+$nameTools = get_lang('CreateDocument');
 if ($is_certificate_mode) {
     $nameTools = get_lang('CreateCertificate');
-} else {
-    $nameTools = get_lang('CreateDocument');
 }
 
 /* Constants and variables */

main/exercise/exercise_show.php
@@ -1210,3 +1210,4 @@ unset($questionList);
 
 Session::erase('exerciseResult');
 unset($exerciseResult);
+Session::erase('calculatedAnswerId');

main/exercise/hotspot_actionscript.as.php
@@ -18,6 +18,7 @@ require_once __DIR__.'/../inc/global.inc.php';
 
 api_protect_course_script(true);
 
+$_course = api_get_course_info();
 require api_get_path(LIBRARY_PATH).'geometry.lib.php';
 
 // set vars

main/exercise/hotspot_actionscript_admin.as.php
@@ -21,10 +21,9 @@ if (!$isAllowedToEdit) {
     exit;
 }
 
-// set vars
-$questionId = intval($_GET['modifyAnswers']);
-$objQuestion = Question::read($questionId);
 $_course = api_get_course_info();
+$questionId = isset($_GET['modifyAnswers']) ? (int) $_GET['modifyAnswers'] : 0;
+$objQuestion = Question::read($questionId);
 $documentPath = api_get_path(SYS_COURSE_PATH).$_course['path'].'/document';
 $picturePath = $documentPath.'/images';
 $pictureName = $objQuestion->getPictureFilename();

commit 23bad83a744482179b36bc14365ed0c1bef45a2c

Quiz: Fix regression in hotspot image upload introduced in d8fdd1f410cdcc8fcce7580d677f4045475ec267

main/exercise/hotspot.class.php
@@ -81,7 +81,7 @@ class HotSpot extends Question
         parent::processCreation($form, $exercise);
 
         if (!empty($fileInfo['tmp_name'])) {
-            $result = $this->uploadPicture($fileInfo);
+            $result = $this->uploadPicture($fileInfo['tmp_name']);
             if ($result) {
                 $this->save($exercise);
 
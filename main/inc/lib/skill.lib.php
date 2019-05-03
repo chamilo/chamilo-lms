0dbb8508c34244d860562a5e6c20af2527a0ce2c
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-05-03 11:31:42 -0500

    Minor - Fix html tag - refs BT#15617

--- main/inc/lib/skill.lib.php
+++ main/inc/lib/skill.lib.php
@@ -1437,7 +1437,7 @@ class Skill extends Model
         }
 
         if ($addTitle) {
-            $tableResult .= '<h3 class="section-title">'.get_lang('AchievedSkills').'</div>
+            $tableResult .= '<h3 class="section-title">'.get_lang('AchievedSkills').'</h3>
                     <div class="skills-badges">
                    ';
         }

fe183552d7daafe0d6d79a5ea7dee9e43a48c8d4
Alex Aragón <aragcar@gmail.com>
2019-05-08 15:51:58 -0500

    add VueJS in Chamilo

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,11 +17,6 @@ const legacyIndex = Routing.generate('legacy_index');
 const mainUrl = Routing.generate('web.main');
 const webAjax = Routing.generate('web.ajax');
 
-/*console.log(homePublicUrl);
-console.log(legacyIndex);
-console.log(mainUrl);
-console.log(webAjax);*/
-
 var ajax_url = webAjax + 'chat.ajax.php';
 var online_button = '<img src="' + homePublicUrl + 'img/statusonline.png">';
 var offline_button = '<img src="' + homePublicUrl + 'img/statusoffline.png">';
@@ -31,7 +26,36 @@ var disconnect_lang = '{{ "ChatDisconnected"|get_lang }}';*/
 var connect_lang = 'ChatConnected';
 var disconnect_lang = 'ChatDisconnected';
 
+// Import Vue and Components
+import Vue from 'vue';
+import {VueConfig} from './VueConfig';
+import Example from './components/Example'
+
+
 $(function() {
+    // Create App Aplication Vue
+    new Vue(
+        Object.assign(VueConfig, {
+            el: '#app_header',
+            components: {Example},
+            data() {
+                return {
+                    chamilo: ''
+                }
+            },
+            methods: {
+                getConsole() {
+                    console.log("Hello Vuejs");
+                }
+            }, created() {
+                this.chamilo = "Hello Chamilo";
+                this.getConsole();
+                console.log(this.chamilo);
+            }
+        })
+    );
+
+
     var webCidReq = '&cidReq=' + $('body').attr('data-course-code');
     window.webCidReq = webCidReq;
 

ec5893c15a74d1c860cd081ecca44fba57107232
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-05-09 12:44:42 -0500

    Add Embed Registry plugin - refs BT#15390
    Add a course tool to get a registry of embed content and track the students access to it.

---
+++ plugin/embedregistry/lang/spanish.php
@@ -0,0 +1,17 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = 'Registro de contenido Embed';
+$strings['plugin_comment'] = 'Agrega una herramienta de curso para llevar un registro del contenido Embed y registrar el acceso de los estudiantes a este.';
+
+$strings['tool_enabled'] = 'Herramienta habilitada';
+$strings['tool_title'] = 'Título de la herramienta';
+$strings['external_url'] = 'URL externa';
+
+$strings['YouNeedCreateContent'] = 'Primero necesita crear el contenido externo. Luego, puede agregar el contenido Embed a su curso.';
+$strings['CreateContent'] = 'Crear contenido externo';
+$strings['CreateEmbeddable'] = 'Agregar Embed';
+$strings['EditEmbeddable'] = 'Editar Embed';
+$strings['ContentNotFound'] = 'Contenido no encontrado';
+$strings['HtmlCode'] = 'Código HTML';
+$strings['LaunchContent'] = 'Lanzar contenido';
---
+++ plugin/embedregistry/plugin.php
@@ -0,0 +1,4 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+$plugin_info = EmbedRegistryPlugin::create()->get_info();

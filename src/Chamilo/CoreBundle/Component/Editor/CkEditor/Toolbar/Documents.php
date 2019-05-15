8281f4a3e9b25bf0fee04498662c85cccd7b29d5
Julio Montoya <gugli100@gmail.com>
2019-05-15 15:09:50 +0200

    Fix toolbar see BT#15641

--- src/Chamilo/CoreBundle/Component/Editor/CkEditor/Toolbar/Documents.php
+++ src/Chamilo/CoreBundle/Component/Editor/CkEditor/Toolbar/Documents.php
@@ -25,6 +25,7 @@ class Documents extends Basic
             $config['toolbar'] = $this->getNormalToolbar();
         } else {
             $config['toolbar_minToolbar'] = $this->getMinimizedToolbar();
+            $config['toolbar_maxToolbar'] = $this->getMaximizedToolbar();
         }
 
         $config['extraPlugins'] = $this->getPluginsToString();
@@ -84,9 +85,58 @@ class Documents extends Basic
             '/',
             ['Styles', 'Format', 'Font', 'FontSize'],
             ['Bold', 'Italic', 'Underline'],
-            ['JustifyLeft', 'JustifyCenter', 'JustifyRight'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
             api_get_setting('enabled_wiris') === 'true' ? ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_CAS'] : [''],
             ['Source'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getMaximizedToolbar()
+    {
+        return [
+            $this->getNewPageBlock(),
+            ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', 'inserthtml'],
+            ['Undo', 'Redo', '-', 'SelectAll', 'Find', '-', 'RemoveFormat'],
+            ['Link', 'Unlink', 'Anchor', 'Glossary'],
+            [
+                'Image',
+                'Mapping',
+                'Video',
+                'Oembed',
+                'Flash',
+                'Youtube',
+                'Audio',
+                'leaflet',
+                'Smiley',
+                'SpecialChar',
+                'Asciimath',
+                'Asciisvg',
+            ],
+            '/',
+            ['Table', '-', 'CreateDiv'],
+            ['BulletedList', 'NumberedList', 'HorizontalRule', '-', 'Outdent', 'Indent', 'Blockquote'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            [
+                'Bold',
+                'Italic',
+                'Underline',
+                'Strike',
+                '-',
+                'Subscript',
+                'Superscript',
+                '-',
+                'TextColor',
+                'BGColor',
+                api_get_configuration_value('translate_html') ? 'Language' : '',
+            ],
+            [api_get_setting('allow_spellcheck') == 'true' ? 'Scayt' : ''],
+            ['Styles', 'Format', 'Font', 'FontSize'],
+            ['PageBreak', 'ShowBlocks'],
+            api_get_setting('enabled_wiris') == 'true' ? ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_CAS'] : [''],
+            ['Toolbarswitch', 'Source'],
         ];
     }
 
@@ -112,7 +162,7 @@ class Documents extends Basic
                 'Asciisvg',
             ],
             ['BulletedList', 'NumberedList', 'HorizontalRule'],
-            ['JustifyLeft', 'JustifyCenter', 'JustifyBlock'],
+            ['JustifyLeft', 'JustifyCenter', 'JustifyRight'],
             ['Styles',
                 'Format',
                 'Font',
@@ -124,12 +174,11 @@ class Documents extends Basic
                 'BGColor',
             ],
             [
-                'Language',
+                api_get_configuration_value('translate_html') ? 'Language' : '',
                 'ShowBlocks',
-                'Source',
             ],
             api_get_setting('enabled_wiris') === 'true' ? ['ckeditor_wiris_formulaEditor', 'ckeditor_wiris_CAS'] : [''],
-            ['Toolbarswitch'],
+            ['Toolbarswitch', 'Source'],
         ];
     }
 }

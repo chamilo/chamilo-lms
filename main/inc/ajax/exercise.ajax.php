bcaf23593ec87f134bc950bc683359eca6488251
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-06-10 17:40:31 -0500

    Quiz: Convert date in live resutls - refs BT#15765

--- main/inc/ajax/exercise.ajax.php
+++ main/inc/ajax/exercise.ajax.php
@@ -274,12 +274,12 @@ switch ($action) {
                     $h = floor($remaining / 3600);
                     $m = floor(($remaining - ($h * 3600)) / 60);
                     $s = ($remaining - ($h * 3600) - ($m * 60));
-                    $timeInfo = api_format_date(
+                    $timeInfo = api_convert_and_format_date(
                             $row['start_date'],
                             DATE_TIME_FORMAT_LONG
                         ).' ['.($h > 0 ? $h.':' : '').sprintf("%02d", $m).':'.sprintf("%02d", $s).']';
                 } else {
-                    $timeInfo = api_format_date(
+                    $timeInfo = api_convert_and_format_date(
                         $row['start_date'],
                         DATE_TIME_FORMAT_LONG
                     );

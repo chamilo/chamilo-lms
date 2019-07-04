9a8c96938416b9237bb8abd3b507ce53b0db9184
Julio <gugli100@gmail.com>
2019-07-04 10:18:01 +0200

    Import csv: fix announcement email subject, fix language loading
    BT#15628

--- main/cron/import_csv.php
+++ main/cron/import_csv.php
@@ -1426,8 +1426,9 @@ class ImportCsv
                     }
 
                     $sessionName = '';
-                    if (!empty($event['session_id'])) {
-                        $sessionName = ' ('.api_get_session_name($event['session_id']).')';
+                    $sessionId = isset($event['session_id']) && !empty($event['session_id']) ? $event['session_id'] : 0;
+                    if (!empty($sessionId)) {
+                        $sessionName = api_get_session_name($sessionId);
                     }
 
                     $courseTitle = $courseInfo['title'];
@@ -1459,6 +1460,18 @@ class ImportCsv
                     $tpl->assign('first_lesson', $date);
                     $tpl->assign('location', $eventComment);
                     $tpl->assign('session_name', $sessionName);
+
+                    if (empty($sessionId)) {
+                        $teachersToString = CourseManager::getTeacherListFromCourseCodeToString($courseInfo['code'], ',');
+                    } else {
+                        $teachersToString = SessionManager::getCoachesByCourseSessionToString(
+                            $sessionId,
+                            $courseInfo['real_id'],
+                            ','
+                        );
+                    }
+
+                    $tpl->assign('teachers', $teachersToString);
 
                     $templateName = $tpl->get_template('mail/custom_calendar_welcome.tpl');
                     $emailBody = $tpl->fetch($templateName);

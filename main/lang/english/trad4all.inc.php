9e281e564ad78f26ef1e0e780301c9ea89bed0ba
Yannick Warnier <ywarnier@beeznest.org>
2019-07-17 21:22:57 +0200

    Update language terms

--- main/lang/english/trad4all.inc.php
+++ main/lang/english/trad4all.inc.php
@@ -8410,4 +8410,8 @@ $AllowHtaccessScormImport = "Allow htaccess in the SCORM import";
 $ExerciseAutoEvaluationAndRankingMode = "Auto-evaluation mode and ranking";
 $YouAreReceivingACopyBecauseYouAreACourseCoach = "You're receiving a copy because, you're a course coach";
 $GenerateReport = "Generate report";
+$VersionFromVersionFile = "Version from the version file";
+$VersionFromConfigFile = "Version from the config file";
+$TheVersionFromTheVersionFileIsUpdatedWithEachVersionIfMainInstallDirectoryIsPresent = "The version from the version.php file is updated with each version but only available if the main/install/ directory is present.";
+$TheVersionFromTheConfigurationFileShowsOnTheAdminPageButHasToBeChangedManuallyOnUpgrade = "The version from the main configuration file shows on the main administration page, but has to be changed manually on upgrade.";
 ?>

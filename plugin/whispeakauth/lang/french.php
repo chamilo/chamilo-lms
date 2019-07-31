fdcf31ca7a1f97c064572c97b1bdba3391b0c96b
Yannick Warnier <ywarnier@beeznest.org>
2019-07-31 21:59:16 +0200

    Add French translations to Whispeak plugin

--- plugin/whispeakauth/lang/french.php
+++ plugin/whispeakauth/lang/french.php
@@ -2,52 +2,52 @@
 /* For licensing terms, see /license.txt */
 
 $strings['plugin_title'] = 'Authentification vocale avec Whispeak';
-$strings['plugin_comment'] = 'Allow speech authentication in Chamilo.';
+$strings['plugin_comment'] = 'Autoriser l\'authentification de la voix dans Chamilo.';
 
-$strings['enable'] = 'Enable';
-$strings['enable_help'] = '<p>Add <code>$_configuration[\'whispeak_auth_enabled\'] = true;</code> in the <code>configuration.php</code> file</p>';
-$strings['api_url'] = 'API URL';
-$strings['api_url_help'] = 'http://api.whispeak.io:8080/v1/';
-$strings['token'] = 'API key';
-$strings['max_attempts'] = 'Max attempts';
-$strings['max_attempts_help'] = '(Optional) If the Whispeak authentication is failed x times, then ask and verify the password of the user';
-$strings['2fa'] = 'Two-Factor Authentication';
-$strings['2fa_help'] = 'Allows extend the login page with a Two-Factor Authentication process. After the classic login, the user must authenticate through Whispeak.';
+$strings['enable'] = 'Activer';
+$strings['enable_help'] = '<p>Ajoutez <code>$_configuration[\'whispeak_auth_enabled\'] = true;</code> dans le fichier <code>configuration.php</code></p>';
+$strings['api_url'] = 'URL de l\'API';
+$strings['api_url_help'] = 'http://api.whispeak.io:8080/v1.1/';
+$strings['token'] = 'Clef API';
+$strings['max_attempts'] = 'Tentatives maximum';
+$strings['max_attempts_help'] = '(Optionnel) Si l\'authentification Whispeak échoue x fois, alors abandonner et demander le mot de passe de l\'utilisateur';
+$strings['2fa'] = 'Authentification à 2 facteurs (2FA)';
+$strings['2fa_help'] = 'Autoriser l\'extension du formulaire de login par une page d\'authentification forte. Après le login classique, l\'utilisateur/trice devra aussi s\'authentifier au travers de Whispeak.';
 
 $strings['EnrollmentSampleText'] = 'Le fameux chef-d\'oeuvre Mona Lisa a été peint par Léonardo da Vinci.';
-$strings['AuthentifySampleText1'] = 'Dropping Like Flies.';
-$strings['AuthentifySampleText2'] = 'Keep Your Eyes Peeled.';
-$strings['AuthentifySampleText3'] = 'The fox screams at midnight.';
-$strings['AuthentifySampleText4'] = 'Go Out On a Limb.';
-$strings['AuthentifySampleText5'] = 'Under the Water.';
-$strings['AuthentifySampleText6'] = 'Barking Up The Wrong Tree.';
+$strings['AuthentifySampleText1'] = 'Tomber comme des mouches.';
+$strings['AuthentifySampleText2'] = 'Restez vigilants.';
+$strings['AuthentifySampleText3'] = 'Le renard hurle à minuit.';
+$strings['AuthentifySampleText4'] = 'Errer dans la campagne.';
+$strings['AuthentifySampleText5'] = 'Sous l\'océan.';
+$strings['AuthentifySampleText6'] = 'Prendre la mouche.';
 
 $strings['RepeatThisPhrase'] = 'Autorisez l\'enregistrement audio puis répétez cette phrase trois fois:';
-$strings['EnrollmentSignature0'] = 'Signature non viable, nécessite un nouvel enrôlement';
-$strings['EnrollmentSignature1'] = 'Signature passable, conseil de faire un nouvel enrôlement.';
+$strings['EnrollmentSignature0'] = 'Signature non utilisable, nécessite un nouvel enregistrement';
+$strings['EnrollmentSignature1'] = 'Signature passable, il est conseillé de faire un nouvel enregistrement.';
 $strings['EnrollmentSignature2'] = 'Signature correcte.';
-$strings['EnrollmentSignature3'] = 'Signature bonne.';
-$strings['SpeechAuthAlreadyEnrolled'] = 'Speech authentication already enrolled previously.';
-$strings['SpeechAuthNotEnrolled'] = 'Speech authentication not enrolled previously.';
-$strings['SpeechAuthentication'] = 'Authentification de voix';
+$strings['EnrollmentSignature3'] = 'Signature satisfaisante.';
+$strings['SpeechAuthAlreadyEnrolled'] = 'L\'authentification de voix a déjà réussi précédemment.';
+$strings['SpeechAuthNotEnrolled'] = 'L\'authentification de voix n\'a pas encore été enregistrée.';
+$strings['SpeechAuthentication'] = 'Authentification par la voix';
 $strings['EnrollmentFailed'] = 'Échec à l\'inscription.';
 $strings['EnrollmentSuccess'] = 'Inscription réussie.';
 $strings['AuthentifyFailed'] = 'Échec de l\'authentification.';
 $strings['AuthentifySuccess'] = 'Authentification réussie!';
 $strings['TryAgain'] = 'Essayez encore';
-$strings['MaxAttemptsReached'] = 'You reached the maximum number of attempts allowed.';
-$strings['LoginWithUsernameAndPassword'] = 'You should login using the username and password.';
-$strings['YouNeedToIdentifyYourselfToAnswerThisQuestion'] = 'You need to identify yourself to answer this question.';
-$strings['IdentifyMe'] = 'Identify me';
-$strings['PleaseWaitWhileLoading'] = "Please wait while loading...";
+$strings['MaxAttemptsReached'] = 'Vous avez atteint le nombre maximum de tentatives autorisées.';
+$strings['LoginWithUsernameAndPassword'] = 'Authentifiez-vous en utilisant votr mot de passe.';
+$strings['YouNeedToIdentifyYourselfToAnswerThisQuestion'] = 'Vous devez vous authentifier pour répondre à cette question.';
+$strings['IdentifyMe'] = 'M\'identifier';
+$strings['PleaseWaitWhileLoading'] = 'Connexion au serveur d\'authentification. Veuillez patienter...';
 
-$strings['AudioQualityShort'] = 'Too short audio';
-$strings['AudioQualityQuiet'] = 'Too quiet audio';
-$strings['AudioQualityLoud'] = 'Too loud audio';
-$strings['AudioQualityNoisy'] = 'Too noisy audio';
-$strings['AudioQualityFrequency'] = 'Missing some audio frequencies';
-$strings['AudioQualityPoorness'] = 'Too poor general audio quality';
+$strings['AudioQualityShort'] = 'Audio trop court';
+$strings['AudioQualityQuiet'] = 'Audio trop peu audible';
+$strings['AudioQualityLoud'] = 'Audio trop fort';
+$strings['AudioQualityNoisy'] = 'Audio trop bruyant';
+$strings['AudioQualityFrequency'] = 'Certaines fréquences audio sont manquantes';
+$strings['AudioQualityPoorness'] = 'Qualité générale de l\'enregistrement trop faible';
 
-$strings['AgreeAllowResearch'] = 'I agree to allow the use of data for research (no commercial usage).';
+$strings['AgreeAllowResearch'] = 'J\'accepte l\'utilisation de mes données à des fins de recherche (non commerciales).';
 
-$strings['MarkForSpeechAuthentication'] = 'Mark it for speech authentication';
+$strings['MarkForSpeechAuthentication'] = 'Cocher pour l\'authentification par la voix';

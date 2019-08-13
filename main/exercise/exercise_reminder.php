41b3c065a0d17819e2aac1359ebeb38aa1664962
Julio Montoya <gugli100@gmail.com>
2019-08-13 08:34:21 +0200

    Minor - UI change add arrow see BT#15518

--- main/exercise/exercise_reminder.php
+++ main/exercise/exercise_reminder.php
@@ -239,14 +239,15 @@ foreach ($question_list as $questionId) {
 
     $checkbox = Display::input('checkbox', 'remind_list['.$questionId.']', '', $attributes);
 
-    $checkbox = '<div class="pretty p-default">
+    $checkbox = '<div class="pretty p-svg p-curve">
         '.$checkbox.'
         <div class="state p-primary ">
-            <label>&nbsp;</label>
+         <svg class="svg svg-icon" viewBox="0 0 20 20">
+                <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+         </svg>
+         <label>&nbsp;</label>
         </div>
     </div>';
-
-    $url = 'exercise_submit.php?exerciseId='.$objExercise->id.'&num='.$counter.'&reminder=1&'.api_get_cidreq();
 
     $counter++;
     $questionTitle = $counter.'. '.strip_tags($objQuestionTmp->selectTitle());

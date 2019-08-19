0cfe2f2e05e423d3d8484452a91bfcfd70df927e
Julio Montoya <gugli100@gmail.com>
2019-08-19 09:01:09 +0200

    Fix symfony deprecations

--- src/ThemeBundle/Event/ThemeEvent.php
+++ src/ThemeBundle/Event/ThemeEvent.php
@@ -1,13 +1,9 @@
 <?php
-/**
- * ThemeEvent.php
- * avanzu-admin
- * Date: 23.02.14.
- */
+/* For licensing terms, see /license.txt */
 
 namespace Chamilo\ThemeBundle\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Base event class to make theme related events easier to detect.

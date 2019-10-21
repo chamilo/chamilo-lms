cccadff0626eefc57fa203533e70b87e1b137563
Julio Montoya <gugli100@gmail.com>
2019-10-21 11:03:23 +0200

    Minor - add from 1.11.x

---
+++ main/admin/periodic_export.php
@@ -0,0 +1,96 @@
+<?php
+/* For licensing terms, see /license.txt */
+exit;
+$cidReset = true;
+require_once __DIR__.'/../inc/global.inc.php';
+
+api_protect_admin_script(true);
+api_set_more_memory_and_time_limits();
+
+$this_section = SECTION_PLATFORM_ADMIN;
+$interbreadcrumb[] = ['url' => 'index.php', 'name' => get_lang('PlatformAdmin')];
+
+$nameTools = get_lang('PeriodicExports');
+$export = '';
+
+Display::display_header($nameTools);
+
+echo Display::page_header($nameTools);
+
+$form = new FormValidator('special_exports', 'post');
+$form->addDateRangePicker('date', get_lang('Dates'));
+$form->addButtonSearch(get_lang('Search'));
+
+$form->display();
+
+if ($form->validate()) {
+    $values = $form->getSubmitValues();
+    $urlId = api_get_current_access_url_id();
+    $startDate = $values['date_start'];
+    $endDate = $values['date_end'];
+
+    // Count active users in the platform
+    $countUsers = UserManager::get_number_of_users(null, $urlId, 1);
+    // Count user connected in those dates
+    $connectedUsers = Statistics::getLoginCount($startDate, $endDate);
+
+    $activeCourses = CourseManager::countActiveCourses($urlId);
+    $totalCourses = CourseManager::count_courses($urlId);
+
+    $total = Tracking::getTotalTimeSpentOnThePlatform();
+
+    $now = api_get_utc_datetime();
+
+    $beforeDateStart = new DateTime('-90 days', new DateTimeZone('UTC'));
+    $end = $beforeDateStart->format('Y-m-d H:i:s');
+
+    $thisTrimester = Tracking::getTotalTimeSpentOnThePlatform($end, $now);
+
+    $beforeDateEnd = new DateTime('-180 days', new DateTimeZone('UTC'));
+    $start = $beforeDateEnd->format('Y-m-d H:i:s');
+
+    $lastTrimester = Tracking::getTotalTimeSpentOnThePlatform($start, $end);
+
+    //var_dump($countUsers, $connectedUsers, $activeCourses, $totalCourses, $total, $thisTrimester, $lastTrimester);
+
+    $courses = Statistics::getCoursesWithActivity($startDate, $endDate);
+
+    $totalUsers = 0;
+    $totalCertificates = 0;
+    foreach ($courses as $courseId) {
+        $courseInfo = api_get_course_info_by_id($courseId);
+
+        $countUsers = CourseManager::get_user_list_from_course_code(
+            $courseInfo['code'],
+            0,
+            null,
+            null,
+            null,
+            true
+        );
+
+        $totalUsers += $countUsers;
+
+        $categories = Category::load(
+            null,
+            null,
+            $courseInfo['code'],
+            null,
+            false,
+            0
+        );
+
+        $category = null;
+        $certificateCount = 0;
+        if (!empty($categories)) {
+            $category = current($categories);
+            // @todo use count
+            $certificateCount = count(GradebookUtils::get_list_users_certificates($categoryId));
+            $totalCertificates += $certificateCount;
+        }
+    }
+
+    $totalUsersCourses = CourseManager::totalSubscribedUsersInCourses($urlId);
+}
+
+Display::display_footer();
---
+++ main/user/career_diagram.php
@@ -0,0 +1,99 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+/*
+ *
+ * Requires extra_field_values.value to be longtext to save diagram:
+ *
+UPDATE extra_field_values SET created_at = NULL WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00';
+UPDATE extra_field_values SET updated_at = NULL WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00';
+ALTER TABLE extra_field_values modify column value longtext null;
+*/
+
+require_once __DIR__.'/../inc/global.inc.php';
+
+if (api_get_configuration_value('allow_career_diagram') == false) {
+    api_not_allowed(true);
+}
+
+$this_section = SECTION_COURSES;
+
+$careerId = isset($_GET['career_id']) ? $_GET['career_id'] : 0;
+
+if (empty($careerId)) {
+    api_not_allowed(true);
+}
+
+$career = new Career();
+$careerInfo = $career->get($careerId);
+if (empty($careerInfo)) {
+    api_not_allowed(true);
+}
+
+$userId = api_get_user_id();
+$allow = UserManager::userHasCareer($userId, $careerId) || api_is_platform_admin();
+
+if ($allow === false) {
+    api_not_allowed(true);
+}
+
+$htmlHeadXtra[] = api_get_js('jsplumb2.js');
+$htmlHeadXtra[] = api_get_asset('qtip2/jquery.qtip.min.js');
+$htmlHeadXtra[] = api_get_css_asset('qtip2/jquery.qtip.min.css');
+
+// setting breadcrumbs
+$interbreadcrumb[] = [
+    'url' => api_get_path(WEB_CODE_PATH).'auth/my_progress.php',
+    'name' => get_lang('Progress'),
+];
+
+$interbreadcrumb[] = [
+    'url' => '#',
+    'name' => get_lang('Careers'),
+];
+
+$extraFieldValue = new ExtraFieldValue('career');
+
+// Check urls
+$itemUrls = $extraFieldValue->get_values_by_handler_and_field_variable(
+    $careerId,
+    'career_urls',
+    false,
+    false,
+    false
+);
+
+$urlToString = '';
+if (!empty($itemUrls) && !empty($itemUrls['value'])) {
+    $urls = explode(',', $itemUrls['value']);
+    $urlToString = '&nbsp;&nbsp;';
+    if (!empty($urls)) {
+        foreach ($urls as $urlData) {
+            $urlData = explode('@', $urlData);
+            if (isset($urlData[1])) {
+                $urlToString .= Display::url($urlData[0], $urlData[1]).'&nbsp;';
+            } else {
+                $urlToString .= $urlData[0].'&nbsp;';
+            }
+        }
+    }
+}
+
+$tpl = new Template(get_lang('Diagram'));
+$html = Display::page_subheader2($careerInfo['name'].$urlToString);
+$diagram = Career::renderDiagramByColumn($careerInfo, $tpl, $userId);
+
+if (!empty($diagram)) {
+    $html .= $diagram;
+} else {
+    Display::addFlash(
+        Display::return_message(
+            sprintf(get_lang('CareerXDoesntHaveADiagram'), $careerInfo['name']),
+            'warning'
+        )
+    );
+}
+
+$tpl->assign('content', $html);
+$layout = $tpl->get_template('career/diagram.tpl');
+$tpl->display($layout);

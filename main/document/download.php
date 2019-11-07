352c9a51c15669ccfeac3465ae57d3a3d677e10f
Yannick Warnier <ywarnier@users.noreply.github.com>
2019-11-07 10:38:46 +0100

    Fix issue in survey media publishing for students - refs BT#16334

--- main/document/download.php
+++ main/document/download.php
@@ -52,6 +52,10 @@ $refer_script = isset($_SERVER["HTTP_REFERER"]) ? strrchr($_SERVER["HTTP_REFERER
 $sys_course_path = api_get_path(SYS_COURSE_PATH).$_course['path'].'/document';
 
 if (substr($refer_script, 0, 15) == '/fillsurvey.php') {
+    list($part1, $part2) = preg_split('/invitationcode=/', $refer_script);
+    list($invitation, $part1) = preg_split('/&/', $part2);
+    unset($part1);
+    unset($part2);
     $invitation = substr(strstr($refer_script, 'invitationcode='), 15);
     $course = strstr($refer_script, 'course=');
     $course = substr($course, 7, strpos($course, '&') - 7);

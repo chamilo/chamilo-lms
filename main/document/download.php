6a7ed64e9794b500e24829f9b62a04ff72dfce85
Yannick Warnier <ywarnier@users.noreply.github.com>
2019-11-13 00:26:56 +0100

    Optimized global (#3024)
    * Document: Use optimized global-min.inc.php for document downloads

--- main/document/download.php
+++ main/document/download.php
@@ -8,7 +8,7 @@
  */
 session_cache_limiter('none');
 
-require_once __DIR__.'/../inc/global.inc.php';
+require_once __DIR__.'/../inc/global-min.inc.php';
 $this_section = SECTION_COURSES;
 
 // Protection

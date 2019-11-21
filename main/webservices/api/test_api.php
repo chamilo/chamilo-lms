c5f0981f721544c5e204a5599efaf5d617cb9f6a
Sébastien Ducoulombier <seb@ldd.fr>
2019-11-21 19:42:56 +0100

    Convertion of API test to PHPUnit test BT#16388

--- main/webservices/api/test_api.php
+++ main/webservices/api/test_api.php
@@ -2,116 +2,113 @@
 /* For licensing terms, see /license.txt */
 
 /**
- * Test example to user API v2.php
+ * Test case for v2.php
  *
  * Using Guzzle' HTTP client to call the API endpoint and make requests.
- * Change URL on the first lines of createUser() below to suit your needs.
  */
-
 use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
 
-require_once '../../../vendor/autoload.php';
+require_once __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__.'/../../inc/global.inc.php';
 
-// set your URL, username and password here to use it for all webservices in this test file.
-$webserviceURL='http://MYCHAMILO.loc/main/webservices/api/';
-$webserviceUsername='admin';
-$webservicePassword='admin';
 
-/**
- * Make a request to get the API key for admin user.
- *
- * @throws Exception
- *
- * @return string
- */
-function authenticate() {
-    global $webserviceURL;
-    global $webserviceUsername;
-    global $webservicePassword;
-    $client = new Client([
-        'base_uri' => $webserviceURL,
-    ]);
-
-    $response = $client->post('v2.php', [
-        'form_params' => [
-            'action' => 'authenticate',
-            'username' => $webserviceUsername,
-            'password' => $webservicePassword,
-        ],
-    ]);
-
-    if ($response->getStatusCode() !== 200) {
-        throw new Exception('Entry denied with code : ' . $response->getStatusCode());
-    }
-
-    $jsonResponse = json_decode($response->getBody()->getContents());
-
-    if ($jsonResponse->error) {
-        throw new Exception('Authentication failed because : ' . $jsonResponse->message);
-    }
-
-    return $jsonResponse->data->apiKey;
-}
-
-/**
- * @param $apiKey
- *
- * @throws Exception
- *
- * @return int
- */
-function createUser($apiKey)
+class V2Test extends TestCase
 {
-    global $webserviceURL;
-    global $webserviceUsername;
-    $client = new Client([
-        'base_uri' => $webserviceURL,
-    ]);
+    const WEBSERVICE_USERNAME = 'admin';
+    const WEBSERVICE_PASSWORD = 'admin';
+    public $client;
 
-    $response = $client->post(
-        'v2.php',
-        [
+    protected function setUp(): void
+    {
+        $this->client = new Client([
+            'base_uri' => api_get_path(WEB_CODE_PATH).'webservices/api/v2.php',
+        ]);
+    }
+
+    /**
+     * Make a request to get the API key for admin user.
+     *
+     * @return string
+     * @throws Exception
+     *
+     */
+    public function testAuthenticate()
+    {
+        $response = $this->client->post('v2.php', [
             'form_params' => [
-                // data for the user who makes the request
-                'action' => 'save_user',
-                'username' => $webserviceUsername,
-                'api_key' => $apiKey,
-                // data for new user
-                'firstname' => 'Test User',
-                'lastname' => 'Chamilo',
-                'status' => 5, // student
-                'email' => 'testuser@example.com',
-                'loginname' => 'restuser',
-                'password' => 'restuser',
-                'original_user_id_name' => 'myplatform_user_id', // field to identify the user in the external system
-                'original_user_id_value' => '1234', // ID for the user in the external system
-                'extra' => [
-                    [
-                        'field_name' => 'age',
-                        'field_value' => 29,
-                    ],
-                ],
-                'language' => 'english',
-                //'phone' => '',
-                //'expiration_date' => '',
+                'action' => 'authenticate',
+                'username' => self::WEBSERVICE_USERNAME,
+                'password' => self::WEBSERVICE_PASSWORD,
             ],
-        ]
-    );
+        ]);
 
-    if ($response->getStatusCode() !== 200) {
-        throw new Exception('Entry denied with code : ' . $response->getStatusCode());
+        $this->assertSame(200, $response->getStatusCode(), 'Entry denied with code : ' . $response->getStatusCode());
+
+        $jsonResponse = json_decode($response->getBody()->getContents());
+
+        $this->assertSame(False, $jsonResponse->error, 'Authentication failed because : ' . $jsonResponse->message);
+        $this->assertNotNull($jsonResponse->data);
+        $this->assertIsObject($jsonResponse->data);
+        $this->assertNotNull($jsonResponse->data->apiKey);
+        $this->assertIsString($jsonResponse->data->apiKey);
+
+        return $jsonResponse->data->apiKey;
     }
 
-    $jsonResponse = json_decode($response->getBody()->getContents());
+    /**
+     * @param $apiKey
+     *
+     * @return int
+     *
+     * @depends testAuthenticate
+     * @throws Exception
+     *
+     */
+    public function testCreateUser($apiKey)
+    {
+        $response = $this->client->post(
+            'v2.php',
+            [
+                'form_params' => [
+                    // data for the user who makes the request
+                    'action' => 'save_user',
+                    'username' => self::WEBSERVICE_USERNAME,
+                    'api_key' => $apiKey,
+                    // data for new user
+                    'firstname' => 'Test User ',
+                    'lastname' => 'Chamilo',
+                    'status' => 5, // student
+                    'email' => 'testuser@example.com',
+                    'loginname' => 'restuser',
+                    'password' => 'restuser',
+                    'original_user_id_name' => 'myplatform_user_id', // field to identify the user in the external system
+                    'original_user_id_value' => '1234', // ID for the user in the external system
+                    'extra' => [
+                        [
+                            'field_name' => 'age',
+                            'field_value' => 29,
+                        ],
+                    ],
+                    'language' => 'english',
+                    //'phone' => '',
+                    //'expiration_date' => '',
+                ],
+            ]
+        );
 
-    if ($jsonResponse->error) {
-        throw new Exception('User not created because : ' . $jsonResponse->message);
+        $this->assertSame(200, $response->getStatusCode(), 'Entry denied with code : ' . $response->getStatusCode());
+
+        $jsonResponse = json_decode($response->getBody()->getContents());
+
+        $this->assertFalse($jsonResponse->error, 'User not created because : ' . $jsonResponse->message);
+        $this->assertNotNull($jsonResponse->data);
+        $this->assertIsArray($jsonResponse->data);
+        $this->assertArrayHasKey(0, $jsonResponse->data);
+        $this->assertIsInt($jsonResponse->data[0]);
+        $userId = $jsonResponse->data[0];
+
+        UserManager::delete_user($userId);
     }
-    return $jsonResponse->data[0];
+
 }
-
-$apiKey = authenticate();
-
-//Creating a new user restuser
-$userId = createUser($apiKey);
-echo 'ID of new user: '.$userId;

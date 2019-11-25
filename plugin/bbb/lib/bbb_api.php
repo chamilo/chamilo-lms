dd6e862a3a56ca18da8fbbdcb2afdc535dc846a1
Ghazi Triki <ghazi.triki@riadvice.tn>
2019-11-25 01:51:23 +0100

    Add follow redirect in bigbluebutton plugin. (#3009)
    * Add follow redirect in bigbluebutton plugin.
    * Fix indentation in bbb_api.php
    * Second code style fix.
    * Put max redirections to bbb api calls.

--- plugin/bbb/lib/bbb_api.php
+++ plugin/bbb/lib/bbb_api.php
@@ -64,6 +64,8 @@ class BigBlueButtonBN
 			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt( $ch, CURLOPT_URL, $url );
 			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+			curl_setopt( $ch, CURLOPT_MAXREDIRS, 5 )
 			curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 			$data = curl_exec( $ch );
 			curl_close( $ch );

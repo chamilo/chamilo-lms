c6aec47c9c75ae2323d91e4537a020eb1c8e07a6
Julio Montoya <gugli100@gmail.com>
2019-11-26 17:02:26 +0100

    Move main folder to public/main

--- public/legacy.php
+++ public/legacy.php
@@ -3,13 +3,13 @@
 
 $charset = 'UTF-8';
 
-require_once __DIR__.'/../main/inc/lib/api.lib.php';
-require_once __DIR__.'/../main/inc/lib/array.lib.php';
-require_once __DIR__.'/../main/inc/lib/database.constants.inc.php';
-require_once __DIR__.'/../main/inc/lib/internationalization.lib.php';
-require_once __DIR__.'/../main/inc/lib/text.lib.php';
-require_once __DIR__.'/../main/inc/lib/banner.lib.php';
-require_once __DIR__.'/../main/inc/lib/online.inc.php';
-require_once __DIR__.'/../main/inc/lib/fileManage.lib.php';
-require_once __DIR__.'/../main/inc/lib/fileUpload.lib.php';
-require_once __DIR__.'/../main/inc/lib/fileDisplay.lib.php';
+require_once __DIR__.'/main/inc/lib/api.lib.php';
+require_once __DIR__.'/main/inc/lib/array.lib.php';
+require_once __DIR__.'/main/inc/lib/database.constants.inc.php';
+require_once __DIR__.'/main/inc/lib/internationalization.lib.php';
+require_once __DIR__.'/main/inc/lib/text.lib.php';
+require_once __DIR__.'/main/inc/lib/banner.lib.php';
+require_once __DIR__.'/main/inc/lib/online.inc.php';
+require_once __DIR__.'/main/inc/lib/fileManage.lib.php';
+require_once __DIR__.'/main/inc/lib/fileUpload.lib.php';
+require_once __DIR__.'/main/inc/lib/fileDisplay.lib.php';

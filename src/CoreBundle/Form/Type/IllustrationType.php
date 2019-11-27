4c22629f804acc9b4bbc53be3e1d688ba02a90b7
Julio Montoya <gugli100@gmail.com>
2019-11-27 08:15:05 +0100

    WIP: Add illustration form type

---
+++ src/CoreBundle/Form/Type/IllustrationType.php
@@ -0,0 +1,43 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class IllustrationType.
+ */
+class IllustrationType extends AbstractType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        /*$resolver->setDefaults(
+            [
+                'choices' => [
+                    'Yes' => 'true',
+                    'No' => 'false',
+                ],
+            ]
+        );*/
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return FileType::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'illustration';
+    }
+}

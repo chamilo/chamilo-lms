10c0b1f3d82d0fda9f8b4f033aa033805d6a83a8
Julio Montoya <gugli100@gmail.com>
2019-11-27 08:21:14 +0100

    Fix main path

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -3,6 +3,6 @@ import 'jsplumb';
 import 'jquery-ui-touch-punch';
 import 'xcolor/jquery.xcolor.js';
 
-import '../../main/inc/lib/javascript/epiclock/javascript/jquery.dateformat.min.js';
-import '../../main/inc/lib/javascript/epiclock/javascript/jquery.epiclock.js';
-import '../../main/inc/lib/javascript/epiclock/renderers/minute/epiclock.minute.js';
+import '../../public/main/inc/lib/javascript/epiclock/javascript/jquery.dateformat.min.js';
+import '../../public/main/inc/lib/javascript/epiclock/javascript/jquery.epiclock.js';
+import '../../public/main/inc/lib/javascript/epiclock/renderers/minute/epiclock.minute.js';

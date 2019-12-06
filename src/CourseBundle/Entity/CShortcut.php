e363930424a9ae6f8202b23aa7f1cda138dbd9ef
Julio <gugli100@gmail.com>
2019-12-06 17:02:39 +0100

    Add CShortcut entity/repository

---
+++ src/CourseBundle/Entity/CShortcut.php
@@ -0,0 +1,73 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CourseBundle\Entity;
+
+use APY\DataGridBundle\Grid\Mapping as GRID;
+
+use Chamilo\CoreBundle\Entity\Resource\AbstractResource;
+use Chamilo\CoreBundle\Entity\Resource\ResourceInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Table(name="c_shortcut")
+ * @ORM\Entity
+ * @GRID\Source(columns="id, name, resourceNode.createdAt", filterable=false, groups={"resource"})
+ */
+class CShortcut extends AbstractResource implements ResourceInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @Assert\NotBlank
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return CShortcut
+     */
+    public function setName(string $name): CShortcut
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Resource identifier.
+     */
+    public function getResourceIdentifier(): int
+    {
+        return $this->id;
+    }
+
+    public function getResourceName(): string
+    {
+        return $this->getName();
+    }
+
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+}
---
+++ src/CourseBundle/Repository/CShortcutRepository.php
@@ -0,0 +1,67 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CourseBundle\Repository;
+
+use APY\DataGridBundle\Grid\Column\Column;
+use APY\DataGridBundle\Grid\Grid;
+use Chamilo\CoreBundle\Component\Utils\ResourceSettings;
+use Chamilo\CoreBundle\Entity\Course;
+use Chamilo\CoreBundle\Entity\Resource\ResourceLink;
+use Chamilo\CoreBundle\Entity\Resource\ResourceNode;
+use Chamilo\CoreBundle\Entity\Session;
+use Chamilo\CoreBundle\Repository\ResourceRepository;
+use Chamilo\CoreBundle\Repository\ResourceRepositoryInterface;
+use Chamilo\CourseBundle\Entity\CDocument;
+use Chamilo\CourseBundle\Entity\CGroupInfo;
+use Chamilo\UserBundle\Entity\User;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * Class CDocumentRepository.
+ */
+final class CShortcutRepository extends ResourceRepository implements ResourceRepositoryInterface
+{
+    public function getResources(User $user, ResourceNode $parentNode, Course $course = null, Session $session = null, CGroupInfo $group = null)
+    {
+        return $this->getResourcesByCourse($course, $session, $group, $parentNode);
+    }
+
+    public function getResourceSettings(): ResourceSettings
+    {
+        $settings = parent::getResourceSettings();
+
+        $settings
+            ->setAllowNodeCreation(false)
+            ->setAllowResourceCreation(true)
+            ->setAllowResourceUpload(false)
+        ;
+
+        return $settings;
+    }
+
+    public function saveUpload(UploadedFile $file)
+    {
+        return false;
+    }
+
+    public function saveResource(FormInterface $form, $course, $session, $fileType)
+    {
+        $newResource = $form->getData();
+        $newResource
+            ->setCourse($course)
+            ->setSession($session)
+            ->setFiletype($fileType)
+            //->setTitle($title) // already added in $form->getData()
+            ->setReadonly(false)
+        ;
+
+        return $newResource;
+    }
+
+    public function getTitleColumn(Grid $grid): Column
+    {
+        return $grid->getColumn('name');
+    }
+}

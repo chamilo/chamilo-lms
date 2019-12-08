607ee0723dcf7fea9c31b0a5a9995cf0c8e05c61
Julio <gugli100@gmail.com>
2019-12-08 22:01:07 +0100

    Remove unused code.

--- public/main/upload/index.php
+++ public/main/upload/index.php
@@ -58,23 +58,97 @@ if (isset($_REQUEST['tool'])) {
     Session::write('my_tool', $my_tool);
 }
 
-/**
- * Process.
- */
 Event::event_access_tool(TOOL_UPLOAD);
 
-/**
- * Now call the corresponding display script, the current script acting like a controller.
- */
-switch ($my_tool) {
-    case TOOL_LEARNPATH:
-        require 'form.scorm.php';
-        break;
-    //the following cases need to be distinguished later on
-    case TOOL_DROPBOX:
-    case TOOL_STUDENTPUBLICATION:
-    case TOOL_DOCUMENT:
-    default:
-        require 'form.document.php';
-        break;
+function get_zip_files_in_garbage()
+{
+    $list = [];
+    $dh = opendir(api_get_path(SYS_ARCHIVE_PATH));
+    if ($dh === false) {
+        //ignore
+    } else {
+        while ($entry = readdir($dh)) {
+            if (substr($entry, 0, 1) == '.') {
+                /* ignore files starting with . */
+            } else {
+                if (preg_match('/^.*\.zip$/i', $entry)) {
+                    $list[] = $entry;
+                }
+            }
+        }
+        natcasesort($list);
+        closedir($dh);
+    }
+
+    return $list;
 }
+
+/**
+ * Just display the form needed to upload a SCORM and give its settings.
+ */
+$nameTools = get_lang('File upload');
+$interbreadcrumb[] = [
+    'url' => api_get_path(WEB_CODE_PATH).'lp/lp_controller.php?action=list&'.api_get_cidreq(),
+    'name' => get_lang('Learning path'),
+];
+
+Display::display_header($nameTools, 'Path');
+
+require_once '../lp/content_makers.inc.php';
+require_once api_get_path(LIBRARY_PATH).'specific_fields_manager.lib.php';
+
+echo '<div class="actions">';
+echo '<a href="'.api_get_path(WEB_CODE_PATH).'lp/lp_controller.php?'.api_get_cidreq().'">'.
+    Display::return_icon('back.png', get_lang('Back to learning paths'), '', ICON_SIZE_MEDIUM).'</a>';
+echo '</div>';
+
+$form = new FormValidator(
+    '',
+    'POST',
+    api_get_path(WEB_CODE_PATH).'upload/upload.php?'.api_get_cidreq(),
+    '',
+    [
+        'id' => 'upload_form',
+        'enctype' => 'multipart/form-data',
+    ]
+);
+$form->addHeader($nameTools);
+$form->addLabel(null, Display::return_icon('scorm_logo.jpg', null, ['style' => 'width:230px;height:100px']));
+$form->addElement('hidden', 'curdirpath', $path);
+$form->addElement('hidden', 'tool', $my_tool);
+$form->addElement('file', 'user_file', get_lang('SCORM or AICC file to upload'));
+$form->addProgress();
+$form->addRule('user_file', get_lang('Required field'), 'required');
+
+unset($content_origins[0]);
+unset($content_origins[1]);
+
+if (api_get_setting('search_enabled') == 'true') {
+    $form->addElement('checkbox', 'index_document', '', get_lang('Index document text?'));
+    $specific_fields = get_specific_field_list();
+    foreach ($specific_fields as $specific_field) {
+        $form->addElement('text', $specific_field['code'], $specific_field['name'].' : ');
+    }
+}
+
+if (api_is_platform_admin()) {
+    $form->addElement('checkbox', 'use_max_score', null, get_lang('Use default maximum score of 100'));
+}
+
+if (api_get_configuration_value('allow_htaccess_import_from_scorm')) {
+    $form->addElement('checkbox', 'allow_htaccess', null, get_lang('Allow htaccess in the SCORM import'));
+}
+
+$form->addButtonUpload(get_lang('Upload'));
+
+// the default values for the form
+$defaults = ['index_document' => 'checked="checked"', 'use_max_score' => 1];
+$form->setDefaults($defaults);
+echo Display::return_message(
+    Display::tag('strong', get_lang('SCORM Authoring tools supported')).': '.implode(', ', $content_origins),
+    'normal',
+    false
+);
+$form->display();
+
+Display::display_footer();
--- public/main/upload/upload.php
+++ public/main/upload/upload.php
@@ -13,33 +13,8 @@ use ChamiloSession as Session;
  */
 require_once __DIR__.'/../inc/global.inc.php';
 
-$toolFromSession = Session::read('my_tool');
-
-// return to index if no tool is set
-if (empty($toolFromSession)) {
-    header('location:index.php');
-    exit;
-}
-
-// check access permissions (edit permission is needed to add a document or a LP)
-$is_allowed_to_edit = api_is_allowed_to_edit();
-
-if (!$is_allowed_to_edit) {
+if (!api_is_allowed_to_edit()) {
     api_not_allowed(true);
 }
 
-/**
- * Redirect to the correct script to handle this type of upload.
- */
-switch ($toolFromSession) {
-    case TOOL_LEARNPATH:
-        require 'upload.scorm.php';
-        break;
-    //the following cases need to be distinguished later on
-    case TOOL_DROPBOX:
-    case TOOL_STUDENTPUBLICATION:
-    case TOOL_DOCUMENT:
-    default:
-        require 'upload.document.php';
-        break;
-}
+require 'upload.scorm.php';

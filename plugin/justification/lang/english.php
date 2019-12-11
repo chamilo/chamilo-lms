c4f198c4527936c5e81c52ee70ffd6348e897f29
Julio <gugli100@gmail.com>
2019-12-11 13:10:42 +0100

    Justification plugin: add course link in index BT#16387

--- plugin/justification/lang/english.php
+++ plugin/justification/lang/english.php
@@ -12,12 +12,11 @@ $strings['NoJustificationFound'] = "No justification found";
 $strings['ValidityDate'] = "Validity date";
 $strings['Justification'] = "Justification";
 $strings['MyJustifications'] = "My justifications";
-
 $strings['JustificationSaved'] = "Justification saved";
-
-
 $strings['default_course_id'] = "Course id";
 $strings['SetCourse'] = "Set course";
+$strings['SubscribeToASession'] = "Subscribe to a session";
+
 
 
 

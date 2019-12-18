05bf53a1836773013c338747d57b4c1dfee72084
Julio <gugli100@gmail.com>
2019-12-18 08:41:14 +0100

    Remove "if (api_is_platform_admin()) {" already using api_protect_admin_script()

--- plugin/justification/list.php
+++ plugin/justification/list.php
@@ -16,48 +16,46 @@ $list = $plugin->getList();
 $tpl->assign('list', $list);
 
 $content = $tpl->fetch('justification/view/list.tpl');
-
 $actionLinks = '';
-if (api_is_platform_admin()) {
-    $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : '';
-    $id = isset($_REQUEST['id']) ? (int) $_REQUEST['id'] : 0;
+$action = isset($_REQUEST['a']) ? $_REQUEST['a'] : '';
+$id = isset($_REQUEST['id']) ? (int) $_REQUEST['id'] : 0;
 
-    switch ($action) {
-        case 'delete':
-            $sql = "DELETE FROM justification_document WHERE id = $id";
-            Database::query($sql);
+switch ($action) {
+    case 'delete':
+        $sql = "DELETE FROM justification_document WHERE id = $id";
+        Database::query($sql);
 
-            Display::addFlash(Display::return_message(get_lang('Deleted')));
-            header('Location: '.api_get_self());
-            exit;
-            break;
-    }
-
-    $actionLinks .= Display::toolbarButton(
-        $plugin->get_lang('Add'),
-        api_get_path(WEB_PLUGIN_PATH).'justification/add.php',
-        'plus',
-        'primary'
-    );
-    $actionLinks .= Display::toolbarButton(
-        $plugin->get_lang('Users'),
-        api_get_path(WEB_PLUGIN_PATH).'justification/justification_by_user.php',
-        'user',
-        'primary'
-    );
-
-    $actionLinks .= Display::toolbarButton(
-        $plugin->get_lang('SetNewCourse'),
-        api_get_path(WEB_PLUGIN_PATH).'justification/set_course.php',
-        'book',
-        'primary'
-    );
-
-    $tpl->assign(
-        'actions',
-        Display::toolbarAction('toolbar', [$actionLinks])
-    );
+        Display::addFlash(Display::return_message(get_lang('Deleted')));
+        header('Location: '.api_get_self());
+        exit;
+        break;
 }
+
+$actionLinks .= Display::toolbarButton(
+    $plugin->get_lang('Add'),
+    api_get_path(WEB_PLUGIN_PATH).'justification/add.php',
+    'plus',
+    'primary'
+);
+$actionLinks .= Display::toolbarButton(
+    $plugin->get_lang('Users'),
+    api_get_path(WEB_PLUGIN_PATH).'justification/justification_by_user.php',
+    'user',
+    'primary'
+);
+
+$actionLinks .= Display::toolbarButton(
+    $plugin->get_lang('SetNewCourse'),
+    api_get_path(WEB_PLUGIN_PATH).'justification/set_course.php',
+    'book',
+    'primary'
+);
+
+$tpl->assign(
+    'actions',
+    Display::toolbarAction('toolbar', [$actionLinks])
+);
+
 
 $tpl->assign('content', $content);
 $tpl->display_one_col_template();

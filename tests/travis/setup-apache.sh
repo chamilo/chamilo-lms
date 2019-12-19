18f9f154fb5b5d44333ee9751665e5766c0982f6
Julio <gugli100@gmail.com>
2019-12-19 15:05:37 +0100

    Travis fix host

--- tests/travis/setup-apache.sh
+++ tests/travis/setup-apache.sh
@@ -7,6 +7,7 @@ sudo a2enmod rewrite actions fastcgi alias
 # Use default config
 sudo cp -f tests/travis/travis-apache /etc/apache2/sites-available/000-default.conf
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
+sudo sed -e "s?%VHOST_URL%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
 sudo chmod 777 -R $HOME
 
 cat /etc/apache2/sites-available/000-default.conf

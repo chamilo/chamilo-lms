996feff3edc6daa2b993487e40c04b92a1b330c6
Julio <gugli100@gmail.com>
2019-12-26 09:22:42 +0100

    Remove unused code

--- src/CourseBundle/Repository/CExerciseCategoryRepository.php
+++ src/CourseBundle/Repository/CExerciseCategoryRepository.php
@@ -12,24 +12,6 @@ use Chamilo\CoreBundle\Repository\ResourceRepository;
 final class CExerciseCategoryRepository extends ResourceRepository
 {
     /**
-     * Gets the number of values stored in the table (all fields together)
-     * for this type of resource.
-     *
-     * @param int $courseId
-     *
-     * @return int Number of rows in the table
-     */
-    public function getCourseCount($courseId)
-    {
-        $query = $this->getRepository()->createQueryBuilder('e');
-        $query->select('count(e.id)');
-        $query->where('e.course = :cId');
-        $query->setParameter('cId', $courseId);
-
-        return $query->getQuery()->getSingleScalarResult();
-    }
-
-    /**
      * @param int $courseId
      *
      * @return array

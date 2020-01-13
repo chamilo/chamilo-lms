1e579cb46f2d4350b85e3905eb4aa755c49d866b
Julio Montoya <gugli100@gmail.com>
2020-01-13 11:07:01 +0100

    Minor - add lang var

--- plugin/justification/lang/french.php
+++ plugin/justification/lang/french.php
@@ -3,5 +3,5 @@
 $strings['plugin_title'] = "Justification Plugin";
 $strings['plugin_comment'] = "";
 $strings['tool_enable'] = "Activer le plugin";
-
 $strings['SubscribeToASession'] = "S'inscrire à une session d'examen";
+$strings['DocumentListForUserX'] = "Liste des documents pour %s";

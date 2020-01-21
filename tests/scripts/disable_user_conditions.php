f071dba4b1e823985ca5bd3e8ff434d0d68434a6
Julio Montoya <gugli100@gmail.com>
2020-01-21 11:47:08 +0100

    Only take active user, first check case 3 then case 2 BT#16345

--- tests/scripts/disable_user_conditions.php
+++ tests/scripts/disable_user_conditions.php
@@ -1,5 +1,7 @@
 <?php
 
+/* For licensing terms, see /license.txt */
+
 require_once __DIR__.'/../../main/inc/global.inc.php';
 
 $test = true;
@@ -19,10 +21,10 @@ $date6Months = $date->format('Y-m-d H:i:s');
 
 // 1. Not contract not connected in 3 months
 $sql = "SELECT u.id
-                FROM user u
-                LEFT JOIN extra_field_values ev
-                ON u.id = ev.item_id AND field_id = $fieldId
-                WHERE (ev.value IS NULL OR ev.value = '') ";
+        FROM user u
+        LEFT JOIN extra_field_values ev
+        ON u.id = ev.item_id AND field_id = $fieldId
+        WHERE (ev.value IS NULL OR ev.value = '') AND u.active = 1 ";
 
 /*
 SELECT distinct u.id, access_date                 FROM user u                 LEFT JOIN extra_field_values ev                 ON u.id = ev.item_id AND field_id = 44                 INNER JOIN track_e_lastaccess a                 ON (a.access_user_id = u.id)                 WHERE (ev.value IS NULL OR ev.value = '') AND access_date > '2019-11-01';
@@ -42,12 +44,41 @@ foreach ($students as $student) {
     }
 }
 
+// 3. Certificate completed not connected 6 months.
+$sql = "SELECT user_id, MAX(login_date) latest_login_date
+        FROM gradebook_certificate c
+        INNER JOIN track_e_login l
+        ON (l.login_user_id = c.user_id)
+        GROUP BY user_id  ";
+
+$result = Database::query($sql);
+$students = Database::store_result($result);
+foreach ($students as $student) {
+    $studentId = $student['user_id'];
+    $lastDate = $student['latest_login_date'];
+
+    if ($date6Months > $lastDate) {
+        $disabledUser = api_get_user_info($studentId);
+        if ($disabledUser['active'] != 1) {
+            continue;
+        }
+
+        $userReportList[$studentId]['message'] = "User# $studentId to be disabled. Case 3. Last connection: $lastDate - 6 months: $date6Months ";
+        if ($test === false) {
+            UserManager::disable($studentId);
+            $subject = 'AccountDisabled: '.$disabledUser['complete_name'];
+            $content = 'AccountDisabled: '.$disabledUser['complete_name'];
+            MessageManager::send_message($studentId, $subject, $content);
+        }
+    }
+}
+
 // 2. Validated contract, not connected + 6 months
 $sql = "SELECT u.id
         FROM user u
         INNER JOIN extra_field_values ev
         ON u.id = ev.item_id AND field_id = $fieldId
-        WHERE (ev.value = 1) ";
+        WHERE ev.value = 1 AND u.active = 1  ";
 
 $result = Database::query($sql);
 $students = Database::store_result($result);
@@ -75,31 +106,6 @@ foreach ($students as $student) {
     }
 }
 
-// 3. Certificate completed not connected 6 months.
-$sql = "SELECT user_id, MAX(login_date) latest_login_date
-                FROM gradebook_certificate c
-                INNER JOIN track_e_login l
-                ON (l.login_user_id = c.user_id)
-                GROUP BY user_id  ";
-
-$result = Database::query($sql);
-$students = Database::store_result($result);
-foreach ($students as $student) {
-    $studentId = $student['user_id'];
-    $lastDate = $student['latest_login_date'];
-
-    if ($date6Months > $lastDate) {
-        $disabledUser = api_get_user_info($studentId);
-
-        $userReportList[$studentId]['message'] = "User# $studentId to be disabled. Case 3. Last connection: $lastDate - 6 months: $date6Months ";
-        if ($test === false) {
-            UserManager::disable($studentId);
-            $subject = 'AccountDisabled: '.$disabledUser['complete_name'];
-            $content = 'AccountDisabled: '.$disabledUser['complete_name'];
-            MessageManager::send_message($studentId, $subject, $content);
-        }
-    }
-}
 
 //$newLine = PHP_EOL;
 

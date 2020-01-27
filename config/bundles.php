757a15b5fa97cce96412f3bff5057065671269c1
Julio Montoya <gugli100@gmail.com>
2020-01-27 13:15:51 +0100

    Add missing bundle

--- config/bundles.php
+++ config/bundles.php
@@ -73,4 +73,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle::class => ['all' => true],
 ];

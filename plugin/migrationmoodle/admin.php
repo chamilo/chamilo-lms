2213485865f6aba5872912c151f278ab1cba5d04
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-02-07 01:16:51 -0500

    MigrationMoodle: Don't list unused tasks - refs BT#15992

--- plugin/migrationmoodle/admin.php
+++ plugin/migrationmoodle/admin.php
@@ -23,7 +23,7 @@ $menu = [
         'course_categories',
         //'courses',
         'efc_courses',
-        'role_assignments',
+        //'role_assignments',
         'efc_user_sessions',
     ],
     'efc_courses' => [
@@ -36,7 +36,7 @@ $menu = [
         'course_modules_lesson',
         'course_modules_quiz',
         'course_modules_url',
-        'c_quiz',
+        //'c_quiz',
     ],
     'course_modules_lesson' => [
         'lesson_pages',

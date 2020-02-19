fe0d9d45319bfdadc5223badd695e1bbb9eceb01
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-02-19 02:07:31 -0500

    MigrationMoodle: Fix session id on UserLearnPaths loader - refs BT#15992

--- plugin/migrationmoodle/src/Loader/UserLearnPathsLoader.php
+++ plugin/migrationmoodle/src/Loader/UserLearnPathsLoader.php
@@ -3,6 +3,7 @@
 
 namespace Chamilo\PluginBundle\MigrationMoodle\Loader;
 
+use Chamilo\CoreBundle\Entity\SessionRelCourseRelUser;
 use Chamilo\PluginBundle\MigrationMoodle\Interfaces\LoaderInterface;
 
 /**
@@ -21,22 +22,51 @@ class UserLearnPathsLoader implements LoaderInterface
 
         $subscriptionsRepo = $em->getRepository('ChamiloCoreBundle:SessionRelCourseRelUser');
         $learnPathsRepo = $em->getRepository('ChamiloCourseBundle:CLp');
+        $lpItemRepo = $em->getRepository('ChamiloCourseBundle:CLpItem');
+
+        $tblLpView = \Database::get_course_table(TABLE_LP_VIEW);
+        $tblLpItemView = \Database::get_course_table(TABLE_LP_ITEM_VIEW);
 
         $userId = $incomingData['user_id'];
 
         $subscriptions = $subscriptionsRepo->findBy(['user' => $userId]);
 
+        /** @var SessionRelCourseRelUser $subscription */
         foreach ($subscriptions as $subscription) {
             $course = $subscription->getCourse();
+            $session = $subscription->getSession();
 
             $lps = $learnPathsRepo->findBy(['cId' => $course->getId(), 'lpType' => 1]);
 
             foreach ($lps as $lp) {
-                new \learnpath(
-                    $course->getCode(),
-                    $lp->getId(),
-                    $userId
-                );
+                $lpItems = $lpItemRepo->findBy(['lpId' => $lp->getId()]);
+
+                $params = [
+                    'c_id' => $course->getId(),
+                    'lp_id' => $lp->getId(),
+                    'user_id' => $userId,
+                    'view_count' => 1,
+                    'session_id' => $session->getId(),
+                    'last_item' => 0,
+                ];
+                $lpViewId = \Database::insert($tblLpView, $params);
+                \Database::query("UPDATE $tblLpView SET id = iid WHERE iid = $lpViewId");
+
+                foreach ($lpItems as $lpItem) {
+                    $params = [
+                        'c_id' => $course->getId(),
+                        'lp_item_id' => $lpItem->getId(),
+                        'lp_view_id' => $lpViewId,
+                        'view_count' => 1,
+                        'status' => 'not attempted',
+                        'start_time' => time(),
+                        'total_time' => 0,
+                        'score' => 0,
+                        'max_score' => $lpItem->getMaxScore(),
+                    ];
+                    $lpItemViewId = \Database::insert($tblLpItemView, $params);
+                    \Database::query("UPDATE $tblLpItemView SET id = iid WHERE iid = $lpItemViewId");
+                }
             }
         }
 

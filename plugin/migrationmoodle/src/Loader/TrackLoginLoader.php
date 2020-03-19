2ababbd68d37b316d9c312bca193501efa005137
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-03-18 19:42:28 -0500

    MigrationMoodle: Fix TrackLoginLoader - refs BT#15992

--- plugin/migrationmoodle/src/Loader/TrackLoginLoader.php
+++ plugin/migrationmoodle/src/Loader/TrackLoginLoader.php
@@ -23,11 +23,35 @@ class TrackLoginLoader implements LoaderInterface
             $incomingData['logout_date'] = $incomingData['logout_date']->format('Y-m-d H:i:s');
         }
 
+        $tblTrackELogin = \Database::get_main_table(TABLE_STATISTIC_TRACK_E_LOGIN);
+
+        $firstId = \Database::insert(
+            $tblTrackELogin,
+            [
+                'login_user_id' => $incomingData['login_user_id'],
+                'login_date' => $incomingData['login_date'],
+                'user_ip' => '',
+            ]
+        );
+
+        \Database::update(
+            $tblTrackELogin,
+            ['logout_date' => $incomingData['logout_date']],
+            ['login_id = ?' => [$firstId]]
+        );
+
         $incomingData['user_ip'] = '';
 
-        return \Database::insert(
-            \Database::get_main_table(TABLE_STATISTIC_TRACK_E_LOGIN),
-            $incomingData
+        \Database::insert(
+            $tblTrackELogin,
+            [
+                'login_user_id' => $incomingData['login_user_id'],
+                'login_date' => $incomingData['logout_date'],
+                'logout_date' => $incomingData['logout_date'],
+                'user_ip' => '',
+            ]
         );
+
+        return $incomingData['login_user_id'];
     }
 }

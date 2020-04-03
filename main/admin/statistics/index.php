492de5c1e8ce166ed0f2d76a849b5fffc5797b54
Julio Montoya <gugli100@gmail.com>
2020-04-03 09:18:23 +0200

    Validate date BT#16339

--- main/admin/statistics/index.php
+++ main/admin/statistics/index.php
@@ -1128,18 +1128,22 @@ switch ($report) {
                 while ($row = Database::fetch_array($query)) {
                     ++$usersFound;
                     if (!empty($row['value'])) {
-                        $date1 = new DateTime($row['value']);
-                        $interval = $now->diff($date1);
-                        $years = (int) $interval->y;
+                        $validDate = DateTime::createFromFormat('Y-m-d', $row['value']);
+                        $validDate = $validDate && $validDate->format('Y-m-d') === $row['value'];
+                        if ($validDate) {
+                            $date1 = new DateTime($row['value']);
+                            $interval = $now->diff($date1);
+                            $years = (int) $interval->y;
 
-                        if ($years >= 16 && $years <= 17) {
-                            ++$all['16-17'];
-                        }
-                        if ($years >= 18 && $years <= 25) {
-                            ++$all['18-25'];
-                        }
-                        if ($years >= 26 && $years <= 30) {
-                            ++$all['26-30'];
+                            if ($years >= 16 && $years <= 17) {
+                                ++$all['16-17'];
+                            }
+                            if ($years >= 18 && $years <= 25) {
+                                ++$all['18-25'];
+                            }
+                            if ($years >= 26 && $years <= 30) {
+                                ++$all['26-30'];
+                            }
                         }
                     }
                 }

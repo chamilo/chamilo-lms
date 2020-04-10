87365da4b859445bf9f594d6179e4a2a80bd185f
Yannick Warnier <ywarnier@beeznest.org>
2020-04-11 00:58:02 +0200

    Fix missing import of $platform_email in Notification lib - loosely refs #3133

--- main/inc/lib/notification.lib.php
+++ main/inc/lib/notification.lib.php
@@ -61,6 +61,8 @@ class Notification extends Model
      */
     public function __construct()
     {
+        // Get config from mail.conf.php, as loaded by global.inc.php
+        $platform_email = $GLOBALS['platform_email'];
         $this->table = Database::get_main_table(TABLE_NOTIFICATION);
         if (!empty($platform_email['SMTP_FROM_EMAIL'])) {
             $this->adminEmail = $platform_email['SMTP_FROM_EMAIL'];

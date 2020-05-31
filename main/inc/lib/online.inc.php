chamilo/chamilo-lms

commit a8eb3b61176f3166f6986cc571221ce5df50d2f5

Global: Optimize query tracking user connexion in track_e_login - refs BT#17137

main/inc/lib/online.inc.php
@@ -15,8 +15,9 @@ use ChamiloSession as Session;
  */
 
 /**
- * Insert a login reference for the current user into the track_e_online stats table.
- * This table keeps trace of the last login. Nothing else matters (we don't keep traces of anything older).
+ * Insert a login reference for the current user into the track_e_online stats 
+ * table. This table keeps trace of the last login. Nothing else matters (we 
+ * don't keep traces of anything older).
  *
  * @param int user id
  */
@@ -37,16 +38,22 @@ function LoginCheck($uid)
             $access_url_id = api_get_current_access_url_id();
         }
         $session_id = api_get_session_id();
-        // if the $_course array exists this means we are in a course and we have to store this in the who's online table also
-        // to have the x users in this course feature working
-        if (is_array($_course) && count($_course) > 0 && !empty($_course['id'])) {
-            $query = "REPLACE INTO ".$online_table." (login_id,login_user_id,login_date,user_ip, c_id, session_id, access_url_id)
-                      VALUES ($uid,$uid,'$login_date','$user_ip', '".$_course['real_id']."' , '$session_id' , '$access_url_id' )";
-        } else {
-            $query = "REPLACE INTO ".$online_table." (login_id,login_user_id,login_date,user_ip, c_id, session_id, access_url_id)
-                      VALUES ($uid,$uid,'$login_date','$user_ip', 0, '$session_id', '$access_url_id')";
+        $cid = 0;
+        if (is_array($_course) && count($_course) > 0 && !empty($_course['real_id'])) {
+            $cid = intval($_course['real_id']);
         }
-        Database::query($query);
+        $query = "SELECT login_id FROM $online_table WHERE login_id = $uid";
+        $resLogin = Database::query($query);
+        if (Database::num_rows($resLogin) > 0) {
+            $query = "UPDATE $online_table SET
+                      login_date = '$login_date', 
+                      user_ip = '$user_ip', 
+                      c_id = $cid,  
+                      session_id = $session_id, 
+                      access_url_id = $access_url_id 
+                      WHERE login_id = $uid";
+            Database::query($query);
+        }
     }
 }
 

commit 073254eee868addf74a4d042c17f7d138d9d60d3

Global: Fix additional issue introduced in optimization in a8eb3b61176f3166f6986cc571221ce5df50d2f5 whereby no *new* records would be inserted with a 0'd out user id in track_e_online (only previously connected users)

main/inc/lib/online.inc.php
@@ -42,20 +42,20 @@ function LoginCheck($uid)
         if (is_array($_course) && count($_course) > 0 && !empty($_course['real_id'])) {
             $cid = intval($_course['real_id']);
         }
-        $query = "SELECT login_id FROM $online_table WHERE login_id = $uid";
+        $query = "SELECT login_id FROM $online_table WHERE login_user_id = $uid";
         $resLogin = Database::query($query);
         if (Database::num_rows($resLogin) > 0) {
             $query = "UPDATE $online_table SET
-                      login_date = '$login_date', 
-                      user_ip = '$user_ip', 
-                      c_id = $cid,  
-                      session_id = $session_id, 
-                      access_url_id = $access_url_id 
-                      WHERE login_id = $uid";
+                      login_date = '$login_date',
+                      user_ip = '$user_ip',
+                      c_id = $cid,
+                      session_id = $session_id,
+                      access_url_id = $access_url_id
+                      WHERE login_user_id = $uid";
             Database::query($query);
         } else {
             $query = "INSERT $online_table (
-                login_id,
+                login_user_id,
                 login_date,
                 user_ip,
                 c_id,
@@ -86,7 +86,7 @@ function preventMultipleLogin($userId)
             $isFirstLogin = Session::read('first_user_login');
             if (empty($isFirstLogin)) {
                 $sql = "SELECT login_id FROM $table
-                        WHERE login_user_id = $userId 
+                        WHERE login_user_id = $userId
                         LIMIT 1";
 
                 $result = Database::query($sql);
@@ -244,7 +244,7 @@ function user_is_online($user_id)
 
     $query = " SELECT login_user_id, login_date
                FROM $track_online_table track
-               INNER JOIN $table_user u 
+               INNER JOIN $table_user u
                ON (u.id=track.login_user_id)
                WHERE
                     track.access_url_id =  $access_url_id AND
@@ -494,13 +494,13 @@ function who_is_online_in_this_course($from, $number_of_items, $uid, $time_limit
     }
 
     $query = "SELECT o.login_user_id, o.login_date
-              FROM $track_online_table o 
+              FROM $track_online_table o
               INNER JOIN $tableUser u
               ON (o.login_user_id = u.id)
               $urlJoin
               WHERE
-                u.status <> '".ANONYMOUS."' AND 
-                o.c_id = $courseId AND 
+                u.status <> '".ANONYMOUS."' AND
+                o.c_id = $courseId AND
                 o.login_date >= '$current_date'
                 $urlCondition
               LIMIT $from, $number_of_items ";
@@ -551,13 +551,13 @@ function who_is_online_in_this_course_count(
     }
 
     $query = "SELECT count(login_user_id) as count
-              FROM $track_online_table o 
-              INNER JOIN $tableUser u              
+              FROM $track_online_table o
+              INNER JOIN $tableUser u
               ON (login_user_id = u.id)
               $urlJoin
-              WHERE 
+              WHERE
                 u.status <> '".ANONYMOUS."' AND
-                c_id = $courseId AND 
+                c_id = $courseId AND
                 login_date >= '$current_date'
                 $urlCondition
                 ";
@@ -600,14 +600,14 @@ function whoIsOnlineInThisSessionCount($timeLimit, $sessionId)
     }
 
     $query = "SELECT count(login_user_id) as count
-              FROM $tblTrackOnline o 
-              INNER JOIN $tableUser u 
+              FROM $tblTrackOnline o
+              INNER JOIN $tableUser u
               ON (login_user_id = u.id)
               $urlJoin
-              WHERE 
-                    u.status <> '".ANONYMOUS."' AND 
-                    session_id = $sessionId AND 
-                    login_date >= '$current_date' 
+              WHERE
+                    u.status <> '".ANONYMOUS."' AND
+                    session_id = $sessionId AND
+                    login_date >= '$current_date'
                     $urlCondition
             ";
     $result = Database::query($query);
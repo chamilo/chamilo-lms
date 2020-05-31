chamilo/chamilo-lms

commit a8eb3b61176f3166f6986cc571221ce5df50d2f5

Global: Optimize query tracking user connexion in track_e_login - refs BT#17137

main/inc/lib/online.inc.php
@@ -15,8 +15,9 @@ use ChamiloSession as Session;
  */
 
 /**
- * Insert a login reference for the current user into the track_e_online stats table.
- * This table keeps trace of the last login. Nothing else matters (we don't keep traces of anything older).
+ * Insert a login reference for the current user into the track_e_online stats 
+ * table. This table keeps trace of the last login. Nothing else matters (we 
+ * don't keep traces of anything older).
  *
  * @param int user id
  */
@@ -37,16 +38,22 @@ function LoginCheck($uid)
             $access_url_id = api_get_current_access_url_id();
         }
         $session_id = api_get_session_id();
-        // if the $_course array exists this means we are in a course and we have to store this in the who's online table also
-        // to have the x users in this course feature working
-        if (is_array($_course) && count($_course) > 0 && !empty($_course['id'])) {
-            $query = "REPLACE INTO ".$online_table." (login_id,login_user_id,login_date,user_ip, c_id, session_id, access_url_id)
-                      VALUES ($uid,$uid,'$login_date','$user_ip', '".$_course['real_id']."' , '$session_id' , '$access_url_id' )";
-        } else {
-            $query = "REPLACE INTO ".$online_table." (login_id,login_user_id,login_date,user_ip, c_id, session_id, access_url_id)
-                      VALUES ($uid,$uid,'$login_date','$user_ip', 0, '$session_id', '$access_url_id')";
+        $cid = 0;
+        if (is_array($_course) && count($_course) > 0 && !empty($_course['real_id'])) {
+            $cid = intval($_course['real_id']);
         }
-        Database::query($query);
+        $query = "SELECT login_id FROM $online_table WHERE login_id = $uid";
+        $resLogin = Database::query($query);
+        if (Database::num_rows($resLogin) > 0) {
+            $query = "UPDATE $online_table SET
+                      login_date = '$login_date', 
+                      user_ip = '$user_ip', 
+                      c_id = $cid,  
+                      session_id = $session_id, 
+                      access_url_id = $access_url_id 
+                      WHERE login_id = $uid";
+            Database::query($query);
+        }
     }
 }
 

commit 4597ecd7a715f126c5d53bfbd75781958ae7561c

Global: Fix issue introduced in optimization in a8eb3b61176f3166f6986cc571221ce5df50d2f5 whereby no *new* records would be inserted in track_e_login (only previously connected users)

main/inc/lib/online.inc.php
@@ -53,6 +53,23 @@ function LoginCheck($uid)
                       access_url_id = $access_url_id 
                       WHERE login_id = $uid";
             Database::query($query);
+        } else {
+            $query = "INSERT $online_table (
+                login_id,
+                login_date,
+                user_ip,
+                c_id,
+                session_id,
+                access_url_id
+            ) values (
+                $uid,
+                '$login_date',
+                '$user_ip',
+                $cid,
+                $session_id,
+                $access_url_id
+            )";
+            Database::query($query);
         }
     }
 }
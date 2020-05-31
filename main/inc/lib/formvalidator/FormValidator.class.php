1163a41781b414150431ff73f4a0e683d2ac8088
Yannick Warnier <ywarnier@beeznest.org>
2020-05-31 07:35:38 +0200

    Minor - Update code documentation for FormValidator::addSelect()

--- main/inc/lib/formvalidator/FormValidator.class.php
+++ main/inc/lib/formvalidator/FormValidator.class.php
@@ -873,7 +873,7 @@ EOT;
 
     /**
      * @param string $name
-     * @param string $label
+     * @param mixed $label String, or array if form element with a comment
      * @param array  $options
      * @param array  $attributes
      *

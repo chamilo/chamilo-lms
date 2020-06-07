ca89b67eb6c9c7da0a3f8ed9369a29fb042d69a2
SpaceCabbie <ikke@spacecabbie.nl>
2020-06-07 10:17:40 +0200

    Add dutch lang
    quick addition for dutch

---
+++ plugin/static/lang/dutch.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @copyright (c) 2012 University of Geneva
+ * @license GNU General Public License - http://www.gnu.org/copyleft/gpl.html
+ * @author Laurent Opprecht <laurent@opprecht.info>
+ * @translation Hans Haufe <hans@cappon.org>
+ */
+$strings['plugin_title'] = "Statisch";
+$strings['plugin_comment'] = "Statische html inhoud weergeven.;
+$strings['block_title'] = "Blok Titel";
+
+//$strings['title'] = "Titel";

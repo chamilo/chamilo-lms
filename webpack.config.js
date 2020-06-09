d5b0102945260f59c702d69e3cbac1af38a263e1
Julio Montoya <gugli100@gmail.com>
2020-06-09 09:28:55 +0200

    webpack disable versioning

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ Encore
 
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    //.enableVersioning(Encore.isProduction())
 
     // enables @babel/preset-env polyfills
     .configureBabel((babelConfig) => {

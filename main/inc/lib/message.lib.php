7de1eddca2b9200e860545134c9971f3d732adf8
Yannick Warnier <ywarnier@beeznest.org>
2020-07-15 01:10:05 +0200

    Minor - Code style and icon naming fix - refs #3125

--- main/inc/lib/message.lib.php
+++ main/inc/lib/message.lib.php
@@ -1477,7 +1477,6 @@ class MessageManager
 
             switch ($type) {
                 case self::MESSAGE_TYPE_INBOX:
-                    //$message_content .= api_strtolower(get_lang('To')).'&nbsp;<b>-</b></li>';
                     $message_content .= '&nbsp;'.api_strtolower(get_lang('To')).'&nbsp;'.get_lang('Me');
                     break;
                 case self::MESSAGE_TYPE_OUTBOX:
@@ -1525,33 +1524,29 @@ class MessageManager
 
         switch ($type) {
             case self::MESSAGE_TYPE_OUTBOX:
-                    $message_content .= '<a href="outbox.php?'.$social_link.'">'.
-                        Display::return_icon('back.png', get_lang('ReturnToOutbox')).'</a> &nbsp';
-                    $message_content .= '<a href="outbox.php?action=deleteone&id='.$messageId.'&'.$social_link.'" >'.
-                        Display::return_icon('delete.png', get_lang('DeleteMessage')).'</a>&nbsp';
-                    break;
+                $message_content .= '<a href="outbox.php?'.$social_link.'">'.
+                    Display::return_icon('back.png', get_lang('ReturnToOutbox')).'</a> &nbsp';
+                $message_content .= '<a href="outbox.php?action=deleteone&id='.$messageId.'&'.$social_link.'" >'.
+                    Display::return_icon('delete.png', get_lang('DeleteMessage')).'</a>&nbsp';
+                break;
             case self::MESSAGE_TYPE_INBOX:
-
-                    $message_content .= '<a href="inbox.php?'.$social_link.'">'.
-                        Display::return_icon('icons/22/arrow_up_1.png', get_lang('ReturnToInbox')).'</a> &nbsp';
-
-                    $message_content .= '<a href="new_message.php?re_id='.$messageId.'&'.$social_link.'">'.
-                        Display::return_icon('message_reply.png', get_lang('ReplyToMessage')).'</a> &nbsp';
-                    $message_content .= '<a href="inbox.php?action=deleteone&id='.$messageId.'&'.$social_link.'" >'.
-                        Display::return_icon('delete.png', get_lang('DeleteMessage')).'</a>&nbsp';
-                if($idPrevMessage != 0) {
-                    $message_content .= '<a title="' . get_lang('ScormPrevious') . '" href="view_message.php?type=' . $type . '&id=' . $idPrevMessage . '" ">' . Display::return_icon('icons/22/back.png', get_lang('ScormPrevious')) . '</a> &nbsp';
+                $message_content .= '<a href="inbox.php?'.$social_link.'">'.
+                    Display::return_icon('icons/22/arrow_up.png', get_lang('ReturnToInbox')).'</a>&nbsp;';
+                $message_content .= '<a href="new_message.php?re_id='.$messageId.'&'.$social_link.'">'.
+                    Display::return_icon('message_reply.png', get_lang('ReplyToMessage')).'</a>&nbsp;';
+                $message_content .= '<a href="inbox.php?action=deleteone&id='.$messageId.'&'.$social_link.'" >'.
+                    Display::return_icon('delete.png', get_lang('DeleteMessage')).'</a>&nbsp;';
+                if ($idPrevMessage != 0) {
+                    $message_content .= '<a title="' . get_lang('PrevMessage') . '" href="view_message.php?type=' . $type . '&id=' . $idPrevMessage . '" ">' . Display::return_icon('icons/22/back.png', get_lang('ScormPrevious')) . '</a> &nbsp';
                 }
-                if($idNextMessage != 0) {
-                    //Display::return_icon('back.png', get_lang('ReturnToInbox')) . '</a> &nbsp';
-                    $message_content .= '<a title="' . get_lang('ScormNext') . '" href="view_message.php?type=' . $type . '&id=' . $idNextMessage . '">' . Display::return_icon('icons/22/move.png', get_lang('ScormNext')) . '</a> &nbsp';
+                if ($idNextMessage != 0) {
+                    $message_content .= '<a title="' . get_lang('NextMessage') . '" href="view_message.php?type=' . $type . '&id=' . $idNextMessage . '">' . Display::return_icon('icons/22/move.png', get_lang('ScormNext')) . '</a> &nbsp';
                 }
-
-                    break;
+                break;
         }
 
         $message_content .= '</div></td>
-		      <td width=10></td>
+		      <td width="10"></td>
 		    </tr>
 		</table>';
 

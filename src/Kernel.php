58e3550d9ca7c5c0fb550e6aa6f290ef5d62bccf
Julio Montoya <gugli100@gmail.com>
2020-07-15 15:51:06 +0200

    Fix PHP warning

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    //private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     public function registerBundles(): iterable
     {
@@ -72,7 +72,6 @@ class Kernel extends BaseKernel
         $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
         $container->import('../config/{services}.yaml');
         $container->import('../config/{services}_'.$this->environment.'.yaml');
-
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void

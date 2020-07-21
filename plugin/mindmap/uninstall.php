09e0b42bef36d57ac342167f6666b61ad0b788f9
Yannick Warnier <ywarnier@beeznest.org>
2020-07-21 13:26:41 +0200

    Plugin: Mindmap: Add access check to uninstall script

--- plugin/mindmap/uninstall.php
+++ plugin/mindmap/uninstall.php
@@ -1,4 +1,7 @@
 <?php
 /* For license terms, see /license.txt */
 require_once 'mindmap_plugin.class.php';
+if(!api_is_platform_admin()){
+    die('You must have admin permissions to install plugins');
+}
 MindmapPlugin::create()->uninstall();

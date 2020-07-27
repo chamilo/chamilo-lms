6b402e1cf794f908dea60d7e8767e2055416e0c8
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-07-27 18:15:50 -0500

    Plugin: Whispeak: Remove unused lang variables - refs BT#17415

--- plugin/whispeakauth/lang/spanish.php
+++ plugin/whispeakauth/lang/spanish.php
@@ -21,12 +21,7 @@ $strings['AuthentifySampleText3'] = 'El zorro grita a medianoche.';
 $strings['AuthentifySampleText4'] = 'Ir por las ramas.';
 $strings['AuthentifySampleText5'] = 'Debajo del agua.';
 $strings['AuthentifySampleText6'] = 'Ladrando al árbol equivocado.';
-
 $strings['RepeatThisPhrase'] = 'Permita la grabación de audio y luego lea esta oración en voz alta:';
-$strings['EnrollmentSignature0'] = 'Firma insostenible, requiere una nueva inscripción.';
-$strings['EnrollmentSignature1'] = 'Firma aceptable, pero se aconseja hacer una nueva inscripción.';
-$strings['EnrollmentSignature2'] = 'Firma correcta.';
-$strings['EnrollmentSignature3'] = 'Buena firma.';
 $strings['SpeechAuthAlreadyEnrolled'] = 'Autenticación de voz registrada anteriormente.';
 $strings['SpeechAuthNotEnrolled'] = 'Autenticación de voz no registrada previamente.';
 $strings['SpeechAuthentication'] = 'Atenticación con voz';
@@ -44,12 +39,4 @@ $strings['Quality'] = 'Calidad';
 $strings['Failed'] = "Fallido";
 $strings['ActivityId'] = "Identificador de actividad";
 $strings['Success'] = "Satisfactotio";
-
-$strings['AudioQualityShort'] = 'Audio demasiado corto';
-$strings['AudioQualityQuiet'] = 'Audio demasiado silencioso';
-$strings['AudioQualityLoud'] = 'Audio demasiado alto';
-$strings['AudioQualityNoisy'] = 'Audio demasiado ruidoso';
-$strings['AudioQualityFrequency'] = 'Falta algunas frecuencias de audio';
-$strings['AudioQualityPoorness'] = 'Calidad de audio general demasiado pobre';
-
 $strings['MarkForSpeechAuthentication'] = 'Marcarlo para autenticación con voz';

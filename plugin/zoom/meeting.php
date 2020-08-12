e3babb6f4611bf9f1f4e02ee694974b428dc5ce3
Julio Montoya <gugli100@gmail.com>
2020-08-12 13:07:58 +0200

    Minor - Fix condition

--- plugin/zoom/meeting.php
+++ plugin/zoom/meeting.php
@@ -66,7 +66,7 @@ if ($plugin->userIsConferenceManager($meeting)) {
     }
 
     if ('true' === $plugin->get('enableCloudRecording') && $meeting->hasCloudAutoRecordingEnabled()) {
-        $tpl->assign('fileForm', $plugin->getFileForm($meeting)->returnForm(), $returnURL);
+        $tpl->assign('fileForm', $plugin->getFileForm($meeting, $returnURL)->returnForm());
         $tpl->assign('recordings', $meeting->getRecordings());
     }
 } elseif ($meeting->requiresRegistration()) {

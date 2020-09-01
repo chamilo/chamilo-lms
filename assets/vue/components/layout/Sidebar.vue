3de9bb8ac0c7e7141d6a2f3280bac6912c909e8b
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-09-01 01:41:39 -0500

    Customize sidebar

--- assets/vue/components/layout/Sidebar.vue
+++ assets/vue/components/layout/Sidebar.vue
@@ -1,56 +1,88 @@
 <template>
-  <b-list-group>
-    <b-list-group-item :to="{ name: 'Index' }">
-      Home
-    </b-list-group-item>
-
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="{ name: 'MyCourses' }"
+  <b-sidebar
+    id="sidebar-1"
+    title="Chamilo"
+    backdrop
+  >
+    <template
+      v-slot:footer
     >
-      Courses
-    </b-list-group-item>
+      <b-nav
+        v-if="!isAuthenticated"
+        class="d-sm-none bg-dark text-light"
+      >
+        <b-nav-item to="/login">
+          Login
+        </b-nav-item>
+        <b-nav-item to="/register">
+          Register
+        </b-nav-item>
+      </b-nav>
 
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="{ name: 'MySessions' }"
-    >
-      Sessions
-    </b-list-group-item>
+      <p
+        v-if="isAuthenticated"
+        class="d-sm-none px-3 py-2 mb-0 bg-dark text-light"
+      >
+        {{ username }}
+      </p>
+      <b-nav
+        v-if="isAuthenticated"
+        class="d-sm-none bg-dark text-light"
+      >
+        <b-nav-item href="/main/messages/inbox.php">
+          Inbox
+        </b-nav-item>
+        <b-nav-item href="/account/home">
+          Profile
+        </b-nav-item>
+        <b-nav-item href="/logout">
+          Logout
+        </b-nav-item>
+      </b-nav>
+    </template>
 
-    <b-list-group-item
-      v-if="isAuthenticated"
-    />
-
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="'/main/admin/user_list.php'"
-    >
-      Users
-    </b-list-group-item>
-
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="'/main/admin/course_list.php'"
-    >
-      Courses
-    </b-list-group-item>
-
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="'/main/session/session_list.php'"
-    >
-      Sessions
-    </b-list-group-item>
-
-
-    <b-list-group-item
-      v-if="isAuthenticated"
-      :to="'/main/admin/index.php'"
-    >
-      Settings
-    </b-list-group-item>
-  </b-list-group>
+    <b-nav vertical>
+      <b-nav-item :to="{ name: 'Index' }">
+        Home
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="{ name: 'MyCourses' }"
+      >
+        Courses
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="{ name: 'MySessions' }"
+      >
+        Sessions
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="'/main/admin/user_list.php'"
+      >
+        Users
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="'/main/admin/course_list.php'"
+      >
+        Courses
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="'/main/session/session_list.php'"
+      >
+        Sessions
+      </b-nav-item>
+      <b-nav-item
+        v-if="isAuthenticated"
+        :to="'/main/admin/index.php'"
+      >
+        Settings
+      </b-nav-item>
+    </b-nav>
+  </b-sidebar>
 </template>
 
 <script>
@@ -59,6 +91,9 @@ export default {
     isAuthenticated() {
       return this.$store.getters['security/isAuthenticated']
     },
+    username() {
+      return this.$store.getters['security/getUser'].username
+    }
   },
 };
 

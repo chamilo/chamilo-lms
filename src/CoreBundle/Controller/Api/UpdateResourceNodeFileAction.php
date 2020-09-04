1a3eadeafa3e2c9a2986ee2ba3cbe335bb4b2a44
Julio Montoya <gugli100@gmail.com>
2020-09-04 15:33:03 +0200

    Internal: Fix update error

--- src/CoreBundle/Controller/Api/UpdateResourceNodeFileAction.php
+++ src/CoreBundle/Controller/Api/UpdateResourceNodeFileAction.php
@@ -16,7 +16,7 @@ class UpdateResourceNodeFileAction
         $fileType = $document->getFileType();
         $contentData = $request->getContent();
         error_log('UpdateResourceNodeFileAction __invoke');
-
+        $resourceLinkList = [];
         if (!empty($contentData)) {
             error_log('contentData');
             $contentData = json_decode($contentData, true);

4f3f17a7d20d322e83a7b4d251bc54687ba1dd0e
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-10-19 12:56:41 -0500

    XAPI: Add hooks to send statments to LRS - refs BT#16742

---
+++ plugin/xapi/src/Hook/XApiQuizEndHook.php
@@ -0,0 +1,146 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+use Xabbuh\XApi\Model\Activity;
+use Xabbuh\XApi\Model\Definition;
+use Xabbuh\XApi\Model\IRI;
+use Xabbuh\XApi\Model\LanguageMap;
+use Xabbuh\XApi\Model\Result as ActivityResult;
+use Xabbuh\XApi\Model\Score;
+
+/**
+ * Class XApiQuizEndHook.
+ */
+class XApiQuizEndHook extends XApiActivityHookObserver implements HookQuizEndObserverInterface
+{
+    use XApiStatementTrait;
+
+    /**
+     * @var \Chamilo\CoreBundle\Entity\TrackEExercises
+     */
+    private $exe;
+    /**
+     * @var \Chamilo\CourseBundle\Entity\CQuiz
+     */
+    private $quiz;
+    /**
+     * @var \Chamilo\CoreBundle\Entity\Course
+     */
+    private $course;
+    /**
+     * @var \Chamilo\CoreBundle\Entity\Session|null
+     */
+    private $session;
+
+    /**
+     * @inheritDoc
+     */
+    public function hookQuizEnd(HookQuizEndEventInterface $hookEvent)
+    {
+        $data = $hookEvent->getEventData();
+        $em = Database::getManager();
+
+        $this->exe = $em->find('ChamiloCoreBundle:TrackEExercises', $data['exe_id']);
+        $this->quiz = $em->find('ChamiloCourseBundle:CQuiz', $this->exe->getExeExoId());
+        $this->user = api_get_user_entity($this->exe->getExeUserId());
+        $this->course = api_get_course_entity($this->exe->getCId());
+        $this->session = api_get_session_entity($this->exe->getSessionId());
+
+        $statement = $this->createStatement();
+
+        try {
+            $statement = $this->sendStatementToLrs($statement);
+
+            $this->saveSharedStatement(
+                $statement->getId(),
+                XApiPlugin::DATA_TYPE_EXERCISE,
+                $this->exe->getExeId()
+            );
+        } catch (Exception $e) {
+            return;
+        }
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActor()
+    {
+        return $this->generateActor(
+            $this->user
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getVerb()
+    {
+        return $this->generateVerb(
+            'terminated',
+            XApiPlugin::VERB_TERMINATED
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActivity()
+    {
+        $title = strip_tags($this->quiz->getTitle());
+        $title = trim($title);
+        $description = strip_tags($this->quiz->getDescription());
+        $description = trim($description);
+
+        $languageIso = api_get_language_isocode($this->course->getCourseLanguage());
+
+        $titleMap = LanguageMap::create([$languageIso => $title]);
+        $descriptionMap = $description ? LanguageMap::create([$languageIso => $description]) : null;
+
+        $activityIdIri = $this->plugin->generateIri(
+            $this->quiz->getId(),
+            'quiz'
+        );
+
+        return new Activity(
+            IRI::fromString($activityIdIri),
+            new Definition(
+                $titleMap,
+                $descriptionMap,
+                IRI::fromString(XApiPlugin::IRI_QUIZ)
+            )
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActivityResult()
+    {
+        $raw = $this->exe->getExeResult();
+        $max = $this->exe->getExeWeighting();
+        $scaled = $raw / $max;
+
+        $duration = $this->exe->getExeDuration();
+
+        return new ActivityResult(
+            new Score($scaled, $raw, 0, $max),
+            null,
+            true,
+            null,
+            $duration ? "PT{$duration}S" : null
+        );
+    }
+
+    /**
+     * @return \Xabbuh\XApi\Model\StatementId
+     */
+    protected function getId()
+    {
+        return $this->generateId(
+            XApiPlugin::DATA_TYPE_EXERCISE,
+            $this->exe->getExeId()
+        );
+    }
+}
---
+++ plugin/xapi/src/Hook/XApiQuizQuestionAnsweredHook.php
@@ -0,0 +1,177 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+use Xabbuh\XApi\Model\Activity;
+use Xabbuh\XApi\Model\ContextActivities;
+use Xabbuh\XApi\Model\Definition;
+use Xabbuh\XApi\Model\IRI;
+use Xabbuh\XApi\Model\LanguageMap;
+use Xabbuh\XApi\Model\Result as ActivityResult;
+use Xabbuh\XApi\Model\Score;
+
+/**
+ * Class XApiQuizQuestionAnsweredHook.
+ */
+class XApiQuizQuestionAnsweredHook
+    extends XApiActivityHookObserver
+    implements HookQuizQuestionAnsweredObserverInterface
+{
+    use XApiStatementTrait;
+
+    /**
+     * @var \Chamilo\CoreBundle\Entity\TrackEExercises
+     */
+    private $exe;
+    /**
+     * @var \Chamilo\CoreBundle\Entity\TrackEAttempt
+     */
+    private $attempt;
+    /**
+     * @var \Chamilo\CourseBundle\Entity\CQuizQuestion
+     */
+    private $question;
+    /**
+     * @var \Chamilo\CoreBundle\Entity\Course
+     */
+    private $course;
+    /**
+     * @var \Chamilo\CoreBundle\Entity\Session|null
+     */
+    private $session;
+
+    /**
+     * @inheritDoc
+     */
+    public function hookQuizQuestionAnswered(HookQuizQuestionAnsweredEventInterface $event)
+    {
+        $data = $event->getEventData();
+
+        $em = Database::getManager();
+
+        $this->exe = $em->find('ChamiloCoreBundle:TrackEExercises', $data['exe_id']);
+        $this->question = $em->find('ChamiloCourseBundle:CQuizQuestion', $data['question']['id']);
+        $this->attempt = $em
+            ->getRepository('ChamiloCoreBundle:TrackEAttempt')
+            ->findOneBy(['exeId' => $this->exe->getExeId(), 'questionId' => $this->question->getId()]);
+        $this->user = api_get_user_entity($this->exe->getExeUserId());
+        $this->course = api_get_course_entity($this->exe->getCId());
+        $this->session = api_get_session_entity($this->exe->getSessionId());
+
+        $statement = $this
+            ->createStatement()
+            ->withCreated(
+                $this->attempt->getTms()
+            );
+
+        try {
+            $statement = $this->sendStatementToLrs($statement);
+
+            $this->saveSharedStatement(
+                $statement->getId(),
+                XApiPlugin::DATA_TYPE_ATTEMPT,
+                $this->attempt->getId()
+            );
+        } catch (Exception $e) {
+            return;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActor()
+    {
+        return $this->generateActor(
+            $this->user
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getVerb()
+    {
+        return $this->generateVerb(
+            'answered',
+            XApiPlugin::VERB_ANSWERED
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActivity()
+    {
+        $questionTitle = strip_tags($this->question->getQuestion());
+        $questionTitle = trim($questionTitle);
+        $questionDescription = strip_tags($this->question->getDescription());
+        $questionDescription = trim($questionDescription);
+
+        $languageIso = api_get_language_isocode($this->course->getCourseLanguage());
+
+        $titleMap = LanguageMap::create([$languageIso => $questionTitle]);
+        $descriptionMap = $questionDescription ? LanguageMap::create([$languageIso => $questionDescription]) : null;
+
+        $activityIdIri = $this->plugin->generateIri(
+            $this->question->getId(),
+            'quiz_question'
+        );
+
+        return new Activity(
+            IRI::fromString($activityIdIri),
+            new Definition(
+                $titleMap,
+                $descriptionMap,
+                IRI::fromString(XApiPlugin::IRI_QUIZ_QUESTION)
+            )
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getActivityResult()
+    {
+        $raw = $this->attempt->getMarks();
+        $max = $this->question->getPonderation();
+        $scaled = $raw / $max;
+
+        return new ActivityResult(
+            new Score($scaled, $raw, null, $max),
+            null,
+            true
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getContext()
+    {
+        $quizIri = $this->plugin->generateIri(
+            $this->exe->getExeExoId(),
+            XApiPlugin::TYPE_QUIZ
+        );
+
+        $quizActivity = new Activity(
+            IRI::fromString($quizIri)
+        );
+
+        $activities = new ContextActivities(
+            [$quizActivity]
+        );
+
+        return parent::getContext()->withContextActivities($activities);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getId()
+    {
+        return $this->generateId(
+            XApiPlugin::DATA_TYPE_ATTEMPT,
+            $this->attempt->getId()
+        );
+    }
+}

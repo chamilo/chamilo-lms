1d2487ec9f1221fe3e55a6aff3c7f0a4185fe547
Carlos Alvarado <alvaradocarlo@gmail.com>
2020-10-21 11:30:08 -0500

    Plugin: BuyCourses: Added the ability to filter by session category in the session purchase catalog. Corrected a word in Spanish - refs GH#3572

--- plugin/buycourses/src/buy_course_plugin.class.php
+++ plugin/buycourses/src/buy_course_plugin.class.php
@@ -584,9 +584,9 @@ class BuyCoursesPlugin extends Plugin
      *
      * @return array|int
      */
-    public function getCatalogSessionList($start, $end, $name = null, $min = 0, $max = 0, $typeResult = 'all')
+    public function getCatalogSessionList($start, $end, $name = null, $min = 0, $max = 0, $typeResult = 'all', $sessionCategory= 0)
     {
-        $sessions = $this->filterSessionList($start, $end, $name, $min, $max, $typeResult);
+        $sessions = $this->filterSessionList($start, $end, $name, $min, $max, $typeResult, $sessionCategory);
 
         if ($typeResult === 'count') {
             return $sessions;
@@ -3003,21 +3003,24 @@ class BuyCoursesPlugin extends Plugin
     /**
      * Search filtered sessions by name, and range of price.
      *
-     * @param int    $start
-     * @param int    $end
-     * @param string $name  Optional. The name filter
-     * @param int    $min   Optional. The minimun price filter
-     * @param int    $max   Optional. The maximum price filter
+     * @param int        $start
+     * @param int        $end
+     * @param string     $name              Optional. The name filter
+     * @param int        $min               Optional. The minimun price filter
+     * @param int        $max               Optional. The maximum price filter
+     * @param string     $max               Optional. all and count
+     * @param int        $sessionCategory   Optional. Session category id
      *
      * @return array
      */
-    private function filterSessionList($start, $end, $name = null, $min = 0, $max = 0, $typeResult = 'all')
+    private function filterSessionList($start, $end, $name = null, $min = 0, $max = 0, $typeResult = 'all', $sessionCategory= 0)
     {
         $itemTable = Database::get_main_table(self::TABLE_ITEM);
         $sessionTable = Database::get_main_table(TABLE_MAIN_SESSION);
 
         $min = floatval($min);
         $max = floatval($max);
+        $sessionCategory = (int)$sessionCategory;
 
         $innerJoin = "$itemTable i ON s.id = i.product_id";
         $whereConditions = [
@@ -3036,8 +3039,12 @@ class BuyCoursesPlugin extends Plugin
             $whereConditions['AND i.price <= ?'] = $max;
         }
 
-        $start = (int) $start;
-        $end = (int) $end;
+        $start = (int)$start;
+        $end = (int)$end;
+
+        if ($sessionCategory != 0) {
+            $whereConditions['AND s.session_category_id = ?'] = $sessionCategory;
+        }
 
         $sessionIds = Database::select(
             's.id',

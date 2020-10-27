3d3de794c16cd8de4766dbec869a8bf995a31ac0
Julio Montoya <gugli100@gmail.com>
2020-10-27 10:29:20 +0100

    Documents: Fix wrong get_lang variable + add verbose in travis
    Fixes behat test not finding the correct link.

--- main/document/document.php
+++ main/document/document.php
@@ -1808,7 +1808,7 @@ if ($isAllowedToEdit ||
         );
     } else {
         $actionsLeft .= Display::url(
-            Display::return_icon('upload_file.png', get_lang('FileToUpload'), '', ICON_SIZE_MEDIUM),
+            Display::return_icon('upload_file.png', get_lang('UplUploadDocument'), '', ICON_SIZE_MEDIUM),
             api_get_path(WEB_CODE_PATH).'document/upload.php?'.api_get_cidreq().'&id='.$current_folder_id
         );
     }

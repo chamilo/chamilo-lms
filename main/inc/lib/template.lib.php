b8908513658c81c9b57006ac7de24c3f55c4c649
Yannick Warnier <ywarnier@beeznest.org>
2020-11-17 23:36:32 +0100

    Security: Add filter to new code using GET variables - refs #3564

--- main/inc/lib/template.lib.php
+++ main/inc/lib/template.lib.php
@@ -1346,11 +1346,11 @@ class Template
 
             if (api_get_configuration_value('ticket_add_quiz_and_lp')) {
                 if (isset($_GET['exerciseId']) && !empty($_GET['exerciseId'])) {
-                    $extraParams = '&exerciseId=' . $_GET['exerciseId'];
+                    $extraParams = '&exerciseId=' . (int) $_GET['exerciseId'];
                 }
 
                 if (isset($_GET['lp_id']) && !empty($_GET['lp_id'])) {
-                    $extraParams .= '&lpId=' . $_GET['lp_id'];
+                    $extraParams .= '&lpId=' . (int) $_GET['lp_id'];
                 }
             }
             $url = api_get_path(WEB_CODE_PATH).'ticket/tickets.php?project_id='.$defaultProjectId.'&'.$courseParams.$extraParams;

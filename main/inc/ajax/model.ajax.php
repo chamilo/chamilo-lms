679303ec268518097c5ddf1eb95f02ef786ebcdb
Carlos Alvarado <alvaradocarlo@gmail.com>
2020-11-18 15:50:45 -0500

    Session: - Changed $_SESSION['id_session'] and $_GET['id_session'] by api_get_session_id - refs BT#17984

--- main/inc/ajax/model.ajax.php
+++ main/inc/ajax/model.ajax.php
@@ -880,7 +880,7 @@ switch ($action) {
         $course_id = api_get_course_int_id();
         $options = [];
         $options['course_id'] = $course_id;
-        $options['session_id'] = $_SESSION['id_session'];
+        $options['session_id'] = api_get_session_id();
 
         switch ($type) {
             case 'not_registered':
@@ -2359,10 +2359,9 @@ switch ($action) {
         $columns = ['name', 'users', 'status', 'group_type', 'actions'];
         $options['order'] = "name $sord";
         $options['limit'] = "$start , $limit";
-        $options['session_id'] = $_SESSION['id_session'];
+        $options['session_id'] = api_get_session_id();
         switch ($type) {
             case 'not_registered':
-
             $result = $obj->getUserGroupNotInCourse($options, $groupFilter);
                 break;
             case 'registered':
@@ -2394,9 +2393,9 @@ switch ($action) {
                 if ($obj->usergroup_was_added_in_course(
                     $group['id'],
                     $course_id,
-                    $_SESSION['id_session']
+                     api_get_session_id()
                 )) {
-                    $url = 'class.php?action=remove_class_from_course&id='.$group['id'].'&'.api_get_cidreq().'&id_session='.$_SESSION['id_session'];
+                    $url = 'class.php?action=remove_class_from_course&id='.$group['id'].'&'.api_get_cidreq().'&id_session='. api_get_session_id();
                     $icon = Display::return_icon('delete.png', get_lang('Remove'));
                 } else {
                     $url = 'class.php?action=add_class_to_course&id='.$group['id'].'&'.api_get_cidreq().'&type=not_registered';

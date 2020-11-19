b661dbe42cbc8af5a91e5bd62ca02e9179cbe9df
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-11-19 03:36:29 -0500

    XAPI: Start statements api - refs BT#16742

---
+++ plugin/xapi/src/Lrs/LrsRequest.php
@@ -0,0 +1,103 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\PluginBundle\XApi\Lrs;
+
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * Class LrsRequest.
+ *
+ * @package Chamilo\PluginBundle\XApi\Lrs
+ */
+class LrsRequest
+{
+    /**
+     * @var HttpRequest
+     */
+    private $request;
+
+    /**
+     * LrsRequest constructor.
+     */
+    public function __construct()
+    {
+        $this->request = HttpRequest::createFromGlobals();
+    }
+
+    public function send()
+    {
+        $version = $this->request->headers->get('X-Experience-API-Version');
+
+        if (null === $version) {
+            throw new BadRequestHttpException('The "X-Experience-API-Version" header is required.');
+        }
+
+        if (!$this->isValidVersion($version)) {
+            throw new BadRequestHttpException("The xAPI version \"$version\" is not supported.");
+        }
+
+        $controllerName = $this->getControllerName();
+        $methodName = $this->getMethodName();
+
+        if ($controllerName
+            && class_exists($controllerName)
+            && method_exists($controllerName, $methodName)
+        ) {
+            /** @var HttpResponse $response */
+            $response = call_user_func([new $controllerName, $methodName]);
+        } else {
+            $response = HttpResponse::create('Not Found', HttpResponse::HTTP_NOT_FOUND);
+        }
+
+        $response->headers->set('X-Experience-API-Version', '1.0.3');
+
+        $response->send();
+    }
+
+    /**
+     * @return string|null
+     */
+    private function getControllerName()
+    {
+        $segments = explode('/', $this->request->getPathInfo());
+
+        if (empty($segments[1])) {
+            return null;
+        }
+
+        $controllerName = ucfirst($segments[1]).'Controller';
+
+        return "Chamilo\\PluginBundle\\XApi\Lrs\\$controllerName";
+    }
+
+    /**
+     * @return string
+     */
+    private function getMethodName()
+    {
+        $method = $this->request->getMethod();
+
+        return strtolower($method);
+    }
+
+    /**
+     * @param string $version
+     *
+     * @return bool
+     */
+    private function isValidVersion($version)
+    {
+        if (preg_match('/^1\.0(?:\.\d+)?$/', $version)) {
+            if ('1.0' === $version) {
+                $this->request->headers->set('X-Experience-API-Version', '1.0.0');
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}

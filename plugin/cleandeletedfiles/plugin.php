138854be251f58f3dd128b6ef22514d371bf27b1
Julio Montoya <gugli100@gmail.com>
2020-11-20 08:59:40 +0100

    Minor - format code
    https://github.com/chamilo/chamilo-lms/commit/fc4ee68c2b088b8f89001e52329f86e2a8ce99b3

--- plugin/cleandeletedfiles/plugin.php
+++ plugin/cleandeletedfiles/plugin.php
@@ -6,10 +6,7 @@
  * Plugin.
  *
  * @author Jose Angel Ruiz
- *
- * @package chamilo.plugin.CleanDeletedFiles
  */
 
-/* Plugin config */
 require_once __DIR__.'/config.php';
 $plugin_info = CleanDeletedFilesPlugin::create()->get_info();

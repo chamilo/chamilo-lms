7dc53933d293b03a40c8f7feee0b94440cab89b8
Julio Montoya <gugli100@gmail.com>
2020-12-01 14:32:22 +0100

    Internal: Update logout handler to logout listener

--- src/CoreBundle/EventListener/LogoutListener.php
+++ src/CoreBundle/EventListener/LogoutListener.php
@@ -5,73 +5,80 @@
 namespace Chamilo\CoreBundle\EventListener;
 
 use Database;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
-/**
- * Class LogoutSuccessHandler.
- */
-class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
+class LogoutListener
 {
     protected $router;
     protected $checker;
     protected $storage;
+    protected $em;
 
     public function __construct(
         UrlGeneratorInterface $urlGenerator,
         AuthorizationCheckerInterface $checker,
-        TokenStorageInterface $storage
+        TokenStorageInterface $storage,
+        EntityManagerInterface $em
     ) {
         $this->router = $urlGenerator;
         $this->checker = $checker;
         $this->storage = $storage;
+        $this->em = $em;
     }
 
     /**
      * @return RedirectResponse|null
      */
-    public function onLogoutSuccess(Request $request)
+    public function onSymfonyComponentSecurityHttpEventLogoutEvent(LogoutEvent $event)
     {
+        $request = $event->getRequest();
+
         // Chamilo logout
         $request->getSession()->remove('_locale');
         $request->getSession()->remove('_locale_user');
 
-        if (api_is_global_chat_enabled()) {
+        /*if (api_is_global_chat_enabled()) {
             $chat = new \Chat();
             $chat->setUserStatus(0);
-        }
+        }*/
 
         $userId = $this->storage->getToken()->getUser()->getId();
 
-        $tbl_track_login = Database :: get_main_table(TABLE_STATISTIC_TRACK_E_LOGIN);
+        $table = Database :: get_main_table(TABLE_STATISTIC_TRACK_E_LOGIN);
 
         $sql = "SELECT login_id, login_date
-                FROM $tbl_track_login
+                FROM $table
                 WHERE login_user_id = $userId
                 ORDER BY login_date DESC
                 LIMIT 0,1";
-        $row = Database::query($sql);
+        //$row = Database::query($sql);
         $loginId = null;
-        if (Database::num_rows($row) > 0) {
-            $loginId = Database::result($row, 0, 'login_id');
+        $connection = $this->em->getConnection();
+        $result = $connection->executeQuery($sql);
+        if ($result->rowCount() > 0) {
+            $row = $result->fetchAssociative();
+            $loginId = $row['login_id'];
         }
 
         $loginAs = $this->checker->isGranted('ROLE_PREVIOUS_ADMIN');
         if (!$loginAs) {
             $current_date = api_get_utc_datetime();
-            $sql = "UPDATE $tbl_track_login
+            $sql = "UPDATE $table
                     SET logout_date='".$current_date."'
         		    WHERE login_id='$loginId'";
-            Database::query($sql);
+            $connection->executeQuery($sql);
         }
 
         $online_table = Database::get_main_table(TABLE_STATISTIC_TRACK_E_ONLINE);
-        $query = 'DELETE FROM '.$online_table." WHERE login_user_id = $userId";
-        Database::query($query);
+        $sql = 'DELETE FROM '.$online_table." WHERE login_user_id = $userId";
+        $connection->executeQuery($sql);
 
         $login = $this->router->generate('home');
 

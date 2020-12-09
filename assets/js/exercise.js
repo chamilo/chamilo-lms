45c9ff3977627e470e6b8ff64223d809e01c4975
Julio Montoya <gugli100@gmail.com>
2020-12-09 10:21:42 +0100

    Yarn: add signature_pad js for exercises

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -1,8 +1,10 @@
-
+// Script to be added in the exercises tool.
 import 'jsplumb';
 import 'jquery-ui-touch-punch';
 import 'xcolor/jquery.xcolor.js';
-
+import 'xcolor/jquery.xcolor.js';
+import 'signature_pad';
 import '../../public/main/inc/lib/javascript/epiclock/javascript/jquery.dateformat.min.js';
 import '../../public/main/inc/lib/javascript/epiclock/javascript/jquery.epiclock.js';
 import '../../public/main/inc/lib/javascript/epiclock/renderers/minute/epiclock.minute.js';
+import './annotation'

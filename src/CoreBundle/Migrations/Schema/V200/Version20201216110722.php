535881736e7806ac3997b8f7001e003bc9375527
Julio Montoya <gugli100@gmail.com>
2020-12-16 13:48:34 +0100

    Migrations: Add c_attendance migration (WIP)

---
+++ src/CoreBundle/Migrations/Schema/V200/Version20201216110722.php
@@ -0,0 +1,84 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Migrations\Schema\V200;
+
+use Chamilo\CoreBundle\Entity\Course;
+use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
+use Chamilo\CoreBundle\Repository\Node\CourseRepository;
+use Chamilo\CoreBundle\Repository\Node\UserRepository;
+use Chamilo\CoreBundle\Repository\SessionRepository;
+use Chamilo\CourseBundle\Entity\CAttendance;
+use Chamilo\CourseBundle\Entity\CAttendanceCalendar;
+use Chamilo\CourseBundle\Repository\CAttendanceRepository;
+use Chamilo\CourseBundle\Repository\CGroupRepository;
+use Chamilo\Kernel;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20201216110722 extends AbstractMigrationChamilo
+{
+    public function getDescription(): string
+    {
+        return 'Migrate c_attendance';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $container = $this->getContainer();
+        $doctrine = $container->get('doctrine');
+        $em = $doctrine->getManager();
+        /** @var Connection $connection */
+        $connection = $em->getConnection();
+
+        $attendanceRepo = $container->get(CAttendanceRepository::class);
+        //$attendanceRepo = $container->get(CAttendanceCalendar::class);
+        $courseRepo = $container->get(CourseRepository::class);
+        $sessionRepo = $container->get(SessionRepository::class);
+        $groupRepo = $container->get(CGroupRepository::class);
+        $userRepo = $container->get(UserRepository::class);
+
+        /** @var Kernel $kernel */
+        $kernel = $container->get('kernel');
+        $rootPath = $kernel->getProjectDir();
+        $admin = $this->getAdmin();
+
+        $q = $em->createQuery('SELECT c FROM Chamilo\CoreBundle\Entity\Course c');
+        /** @var Course $course */
+        foreach ($q->toIterable() as $course) {
+            $courseId = $course->getId();
+            $course = $courseRepo->find($courseId);
+
+            // c_thematic.
+            $sql = "SELECT * FROM c_attendance WHERE c_id = $courseId
+                    ORDER BY iid";
+            $result = $connection->executeQuery($sql);
+            $items = $result->fetchAllAssociative();
+            foreach ($items as $itemData) {
+                $id = $itemData['iid'];
+                /** @var CAttendance $resource */
+                $resource = $attendanceRepo->find($id);
+                if ($resource->hasResourceNode()) {
+                    continue;
+                }
+
+                $result = $this->fixItemProperty(
+                    'attendance',
+                    $attendanceRepo,
+                    $course,
+                    $admin,
+                    $resource,
+                    $course
+                );
+
+                if (false === $result) {
+                    continue;
+                }
+
+                $em->persist($resource);
+                $em->flush();
+            }
+        }
+    }
+}

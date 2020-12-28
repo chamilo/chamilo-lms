11c79b6107411626edf75fb8274e0fd7fd697730
Julio Montoya <gugli100@gmail.com>
2020-12-28 09:09:10 +0100

    Minor - fix tracking URL

--- main/lp/lp_report.php
+++ main/lp/lp_report.php
@@ -342,7 +342,7 @@ if (!empty($users)) {
                 }
             }
         }
-        $trackingUrl = api_get_path(WEB_CODE_PATH).'mySpace/myStudents.php?details=true'.
+        $trackingUrl = api_get_path(WEB_CODE_PATH).'mySpace/myStudents.php?details=true&'.
         api_get_cidreq().'&course='.$courseCode.'&origin=tracking_course&student='.$userId;
         $row = [];
         $row[] = Display::url($userInfo['firstname'], $trackingUrl);

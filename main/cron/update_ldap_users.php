a918e2fd202151e3ba3c3f9555526b88de4c8263
Julio Montoya <gugli100@gmail.com>
2020-12-31 08:19:17 +0100

    LDAP: Add cron to update users - WIP - BT#18222

---
+++ main/cron/update_ldap_users.php
@@ -0,0 +1,42 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+exit;
+
+require_once __DIR__.'../../app/config/auth.conf.php';
+require_once __DIR__.'../auth/external_login/ldap.inc.php';
+require_once __DIR__.'../auth/external_login/functions.inc.php';
+
+global $extldap_config;
+
+if (empty($extldap_config)) {
+    echo "$extldap_config not found";
+    exit;
+}
+
+$ds = extldap_connect();
+if (!$ds) {
+    echo 'ldap not connected';
+    exit;
+}
+
+$table = Database::get_main_table(TABLE_MAIN_USER);
+$sql = "SELECT * FROM $table WHERE auth_source = 'ldap' ";
+$result = Database::query($sql);
+while ($user = Database::fetch_array($result, 'ASSOC')) {
+    echo "Loading user #".$user['id'].PHP_EOL;
+    $login = $user['username'];
+
+    //$ldapbind = @ldap_bind($ds, $extldap_config['admin_dn'], $extldap_config['admin_password']);
+    $sr = ldap_search($ds, $extldap_config['base_dn'], 'uid='.$login);
+    if (!$sr) {
+        echo "Username not found in LDAP: ".$login.PHP_EOL;
+        continue;
+    }
+    $users = ldap_get_entries($ds, $sr);
+    for ($key = 0; $key < $users['count']; $key++) {
+        $ldapUser = $users[$key];
+        print_r($ldapUser).PHP_EOL;
+    }
+}

1f492e0e0e9a4a5845f7bc81f9976ad76b82949d
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-01-11 18:47:04 -0500

    Portfolio: Add option to teacher copy items/comments - refs BT#18201

--- main/portfolio/index.php
+++ main/portfolio/index.php
@@ -128,6 +128,7 @@ switch ($action) {
 
         return;
     case 'copy':
+    case 'teacher_copy':
         $type = $httpRequest->query->getAlpha('copy');
         $id = $httpRequest->query->getInt('id');
 
@@ -138,7 +139,11 @@ switch ($action) {
                 break;
             }
 
-            $controller->copyItem($item);
+            if ('copy' === $action) {
+                $controller->copyItem($item);
+            } elseif ('teacher_copy' === $action) {
+                   $controller->teacherCopyItem($item);
+            }
         } elseif ('comment' === $type) {
             $comment = $em->find(PortfolioComment::class, $id);
 
@@ -146,7 +151,11 @@ switch ($action) {
                 break;
             }
 
-            $controller->copyComment($comment);
+            if ('copy' === $action) {
+                $controller->copyComment($comment);
+            } elseif ('teacher_copy' === $action) {
+                $controller->teacherCopyComment($comment);
+            }
         }
 
         break;

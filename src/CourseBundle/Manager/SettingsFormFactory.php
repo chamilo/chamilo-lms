292ce82fd969f6c156abc78d6549c463c8e40868
Julio <gugli100@gmail.com>
2021-01-24 10:43:34 +0100

    Minor - format code

--- src/CourseBundle/Manager/SettingsFormFactory.php
+++ src/CourseBundle/Manager/SettingsFormFactory.php
@@ -3,9 +3,9 @@
 namespace Chamilo\CourseBundle\Manager;
 
 use Sylius\Bundle\SettingsBundle\Form\Factory\SettingsFormFactoryInterface;
+use Sylius\Bundle\SettingsBundle\Registry\ServiceRegistryInterface;
 use Sylius\Bundle\SettingsBundle\Schema\SchemaFormOptionsInterface;
 use Sylius\Bundle\SettingsBundle\Schema\SchemaInterface;
-use Sylius\Bundle\SettingsBundle\Registry\ServiceRegistryInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormFactoryInterface;
 

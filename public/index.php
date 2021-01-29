b15ace85651305758c8a66e86d3349415fbb7bab
Julio Montoya <gugli100@gmail.com>
2021-01-29 17:47:51 +0100

    Update from 5.2

--- public/index.php
+++ public/index.php
@@ -25,16 +25,7 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();

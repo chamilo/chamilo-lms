8020862cd7a7e29c38c5a13c197740ea4fd00274
Julio Montoya <gugli100@gmail.com>
2021-01-31 12:12:22 +0100

    Add forum, work specific_fields_manager and libs in legacy.php

--- public/legacy.php
+++ public/legacy.php
@@ -1,4 +1,5 @@
 <?php
+
 /* For licensing terms, see /license.txt */
 
 $charset = 'UTF-8';
@@ -13,3 +14,7 @@ require_once __DIR__.'/main/inc/lib/online.inc.php';
 require_once __DIR__.'/main/inc/lib/fileManage.lib.php';
 require_once __DIR__.'/main/inc/lib/fileUpload.lib.php';
 require_once __DIR__.'/main/inc/lib/fileDisplay.lib.php';
+require_once __DIR__.'/main/inc/lib/specific_fields_manager.lib.php';
+require_once __DIR__.'/main/forum/forumfunction.inc.php';
+require_once __DIR__.'/main/work/work.lib.php';
+

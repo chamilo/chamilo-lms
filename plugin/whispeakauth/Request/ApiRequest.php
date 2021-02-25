3d4bafbb79281785995a8fb824eb0288300646c2
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-02-25 13:04:04 -0500

    Whispeak: Show invalid assert messages in request - refs BT#18476

--- plugin/whispeakauth/Request/ApiRequest.php
+++ plugin/whispeakauth/Request/ApiRequest.php
@@ -178,11 +178,9 @@ class ApiRequest
 
             if (isset($json['asserts'])) {
                 foreach ($json['asserts'] as $assert) {
-                    // Removed message filter because Whispeak indicates that all message should be presented to student.
-                    // Leaving the code and only commenting because if it's not modified on their side it should come back
-//                    if (in_array($assert['value'], ['valid_audio', 'invalid_audio'])) {
-                    $message .= $assert['message'].PHP_EOL;
-//                    }
+                    if ('invalid_' === substr($assert['value'], 0, 8)) {
+                        $message .= $assert['message'].PHP_EOL;
+                    }
                 }
             } elseif (empty($json['message'])) {
                 $message = $requestException->getMessage();

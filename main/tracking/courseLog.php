7a0c258e08c8c48c15688fe86e4135de79e20df0
Yannick Warnier <ywarnier@beeznest.org>
2021-03-15 10:36:54 +0100

    Minor - Display: Improve search form in course log with a slight space

--- main/tracking/courseLog.php
+++ main/tracking/courseLog.php
@@ -225,6 +225,8 @@ $form_search = new FormValidator(
     [],
     FormValidator::LAYOUT_INLINE
 );
+$renderer = $form_search->defaultRenderer();
+$renderer->setCustomElementTemplate('<span>{element}</span>');
 $form_search->addHidden('from', Security::remove_XSS($from));
 $form_search->addHidden('session_id', $sessionId);
 $form_search->addHidden('id_session', $sessionId);

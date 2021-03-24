49e3bd00c563cae29a0b8a06a1dd90a3b182633c
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-03-24 17:08:44 -0500

    TopLinks: Allow add tools when creating course - refs BT#18474

--- plugin/toplinks/src/TopLinksPlugin.php
+++ plugin/toplinks/src/TopLinksPlugin.php
@@ -2,10 +2,13 @@
 
 /* For license terms, see /license.txt */
 
+use Chamilo\PluginBundle\Entity\TopLinks\TopLink;
+use Doctrine\ORM\Tools\SchemaTool;
+
 /**
  * Class TopLinksPlugin.
  */
-class TopLinksPlugin extends Plugin
+class TopLinksPlugin extends Plugin implements HookPluginInterface
 {
     /**
      * TopLinksPlugin constructor.
@@ -41,5 +44,66 @@ class TopLinksPlugin extends Plugin
         $webPath = api_get_path(WEB_PLUGIN_PATH).$this->get_name();
 
         return "$webPath/admin.php";
+    }
+
+    public function addToolInCourse(int $courseId, TopLink $link)
+    {
+        $this->createLinkToCourseTool(
+            $link->getTitle(),
+            $courseId,
+            null,
+            'toplinks/start.php?'.http_build_query(['link' => $link->getId()])
+        );
+    }
+
+    public function install()
+    {
+        $em = Database::getManager();
+        $schemaManager = $em->getConnection()->getSchemaManager();
+
+        $tableReferences = [
+            'toplinks_link' => $em->getClassMetadata(TopLink::class),
+        ];
+
+        $tablesExists = $schemaManager->tablesExist(array_keys($tableReferences));
+
+        if ($tablesExists) {
+            return;
+        }
+
+        $schemaTool = new SchemaTool($em);
+        $schemaTool->createSchema(array_values($tableReferences));
+
+        $this->installHook();
+    }
+
+    public function installHook(): int
+    {
+        $createCourseObserver = TopLinksCreateCourseHookObserver::create();
+        HookCreateCourse::create()->attach($createCourseObserver);
+
+        return 1;
+    }
+
+    public function uninstall()
+    {
+        $em = Database::getManager();
+
+        $tableReferences = [
+            'toplinks_link' => $em->getClassMetadata(TopLink::class),
+        ];
+
+        $schemaTool = new SchemaTool($em);
+        $schemaTool->dropSchema(array_values($tableReferences));
+
+        $this->uninstallHook();
+    }
+
+    public function uninstallHook(): int
+    {
+        $createCourseObserver = TopLinksCreateCourseHookObserver::create();
+        HookCreateCourse::create()->detach($createCourseObserver);
+
+        return 1;
     }
 }

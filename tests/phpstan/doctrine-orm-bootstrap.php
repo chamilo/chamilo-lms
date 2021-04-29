27df30b11b9e21c928a56019ba548efbc9ccaf9f
Julio Montoya <gugli100@gmail.com>
2021-04-29 15:54:53 +0200

    Fix PHPStan

--- tests/phpstan/doctrine-orm-bootstrap.php
+++ tests/phpstan/doctrine-orm-bootstrap.php
@@ -1,10 +1,13 @@
 <?php
 
 use Chamilo\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-$kernel = new Kernel('dev', true);
+(new Dotenv())->bootEnv(__DIR__ . '/../../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 
 return $kernel->getContainer()->get('doctrine')->getManager();

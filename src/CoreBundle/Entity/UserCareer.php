6c23c3b553e2002d27e93924103494b8bc02c813
Julio Montoya <gugli100@gmail.com>
2021-05-11 10:01:18 +0200

    Migrations: Add UserCareer.php

---
+++ src/CoreBundle/Entity/UserCareer.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Table(name="user_career")
+ * @ORM\Entity
+ */
+class UserCareer
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    protected int $id;
+
+    /**
+     * @ORM\Column(name="user_id", type="integer", nullable=false)
+     */
+    protected User $user;
+
+    /**
+     * @ORM\Column(name="career_id", type="integer", nullable=false)
+     */
+    protected Career $career;
+
+    /**
+     * @ORM\Column(name="extra_data", type="text", nullable=true)
+     */
+    protected string $extraData;
+}

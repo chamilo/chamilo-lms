ce024c90d9cfcaa336a2a77ee35049fb535c190a
Julio Montoya <gugli100@gmail.com>
2021-05-19 10:06:07 +0200

    Minor - format code

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -9628,6 +9628,46 @@ class SessionManager
         }
     }
 
+    public static function getCareerDiagramPerSession($sessionId, $userId): string
+    {
+        $extraFieldValueSession = new ExtraFieldValue('session');
+        $extraFieldValueCareer = new ExtraFieldValue('career');
+
+        $visibility = api_get_session_visibility($sessionId, null, false, $userId);
+
+        $content = '';
+        if (SESSION_AVAILABLE === $visibility) {
+            $value = $extraFieldValueSession->get_values_by_handler_and_field_variable($sessionId, 'careerid');
+            if (isset($value['value']) && !empty($value['value'])) {
+                $careerList = str_replace(['[', ']'], '', $value['value']);
+                $careerList = explode(',', $careerList);
+
+                foreach ($careerList as $career) {
+                    $careerIdValue = $extraFieldValueCareer->get_item_id_from_field_variable_and_field_value(
+                        'external_career_id',
+                        $career
+                    );
+                    if (isset($careerIdValue['item_id']) && !empty($careerIdValue['item_id'])) {
+                        $finalCareerId = $careerIdValue['item_id'];
+                        $career = new Career();
+                        $careerInfo = $career->get($finalCareerId);
+                        if (!empty($careerInfo)) {
+                            $careerUrl = api_get_path(WEB_CODE_PATH).
+                                'user/career_diagram.php?iframe=1&career_id='.$finalCareerId;
+                            $content .= '<iframe
+                                style="width:100%; height:500px"
+                                border="0"
+                                frameborder="0"
+                                src="'.$careerUrl.'"></iframe>';
+                        }
+                    }
+                }
+            }
+        }
+
+        return $content;
+    }
+
     /**
      * @param int $id
      *
@@ -9764,45 +9804,5 @@ class SessionManager
         } else {
             return -1;
         }
-    }
-
-    public static function getCareerDiagramPerSession($sessionId, $userId): string
-    {
-        $extraFieldValueSession = new ExtraFieldValue('session');
-        $extraFieldValueCareer = new ExtraFieldValue('career');
-
-        $visibility = api_get_session_visibility($sessionId, null, false, $userId);
-
-        $content = '';
-        if (SESSION_AVAILABLE === $visibility) {
-            $value = $extraFieldValueSession->get_values_by_handler_and_field_variable($sessionId, 'careerid');
-            if (isset($value['value']) && !empty($value['value'])) {
-                $careerList = str_replace(['[', ']'], '', $value['value']);
-                $careerList = explode(',', $careerList);
-
-                foreach ($careerList as $career) {
-                    $careerIdValue = $extraFieldValueCareer->get_item_id_from_field_variable_and_field_value(
-                        'external_career_id',
-                        $career
-                    );
-                    if (isset($careerIdValue['item_id']) && !empty($careerIdValue['item_id'])) {
-                        $finalCareerId = $careerIdValue['item_id'];
-                        $career = new Career();
-                        $careerInfo = $career->get($finalCareerId);
-                        if (!empty($careerInfo)) {
-                            $careerUrl = api_get_path(WEB_CODE_PATH).
-                                'user/career_diagram.php?iframe=1&career_id='.$finalCareerId;
-                            $content .= '<iframe
-                                style="width:100%; height:500px"
-                                border="0"
-                                frameborder="0"
-                                src="'.$careerUrl.'"></iframe>';
-                        }
-                    }
-                }
-            }
-        }
-
-        return $content;
     }
 }

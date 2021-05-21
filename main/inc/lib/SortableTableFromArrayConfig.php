c6cdfc680445c9d072fdabdf77480e5e204d5018
Christian <christian1827@gmail.com>
2021-05-21 05:54:51 -0500

    Document: Remove extra space in code - refs #3748

--- main/inc/lib/SortableTableFromArrayConfig.php
+++ main/inc/lib/SortableTableFromArrayConfig.php
@@ -66,7 +66,7 @@ class SortableTableFromArrayConfig extends SortableTable
             $itemsPerPage,
             $direction
         );
-        $this->table_data = $data;        
+        $this->table_data = $data;
     }
 
     /**

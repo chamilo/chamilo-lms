23268575b0fd80d07477749e335c1c58f8c661b7
Julio Montoya <gugli100@gmail.com>
2021-05-31 21:05:01 +0200

    Update files to Symfony 5.3 + add phpunit/phpunit

--- public/index.php
+++ public/index.php
@@ -3,30 +3,17 @@
 /* For licensing terms, see /license.txt */
 
 use Chamilo\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+return function (array $context) {
+    // Chamilo
+    $isInstalled = $context['APP_INSTALLED'] ?? null;
+    if (1 !== (int) $isInstalled) {
+        // Does not support subdirectories for now
+        header('Location: /main/install/index.php');
+        exit;
+    }
 
-// Redirects to the installation page.
-$isInstalled = $_ENV['APP_INSTALLED'] ?? null;
-if (1 !== (int) $isInstalled) {
-    // Does not support subdirectories for now
-    header('Location: /main/install/index.php');
-    exit;
-}
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};

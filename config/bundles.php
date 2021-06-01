61e70cbcbe24530f23107e3848a34152cf38c14a
Julio Montoya <gugli100@gmail.com>
2021-06-01 11:56:34 +0200

    Composer: Update libs + add dama/doctrine-test-bundle (phpunit tests)

--- config/bundles.php
+++ config/bundles.php
@@ -33,4 +33,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];

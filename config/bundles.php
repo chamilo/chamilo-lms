fd189ce4d39a2842136117d1a5919c435778fd65
Julio <gugli100@gmail.com>
2021-08-13 12:38:22 +0200

    Update bundle

--- config/bundles.php
+++ config/bundles.php
@@ -37,4 +37,5 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterSymfonyBundle::class => ['all' => true],
 ];

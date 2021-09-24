a6e986656346969d5e29e214925fc79b528f1d5a
Julio <gugli100@gmail.com>
2021-09-24 10:51:00 +0200

    Tools: Add bbb  + mobidico tools

---
+++ src/CoreBundle/Tool/VideoConference.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Tool;
+
+class VideoConference extends AbstractTool implements ToolInterface
+{
+    public function getName(): string
+    {
+        return 'bbb';
+    }
+
+    public function getNameToShow(): string
+    {
+        return 'VideoConference';
+    }
+
+    public function getIcon(): string
+    {
+        return 'mdi-video';
+    }
+
+    public function getLink(): string
+    {
+        return '/plugin/bbb/start.php';
+    }
+
+    public function getCategory(): string
+    {
+        return 'plugin';
+    }
+
+    public function getResourceTypes(): ?array
+    {
+        return [
+        ];
+    }
+}

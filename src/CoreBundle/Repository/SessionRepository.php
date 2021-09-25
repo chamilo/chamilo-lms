bb3af8fe30a8e32f4db0f4735dfd0da4d12e6eab
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-09-25 13:55:04 -0500

    Change queries to find session coached by user

--- src/CoreBundle/Repository/SessionRepository.php
+++ src/CoreBundle/Repository/SessionRepository.php
@@ -10,6 +10,7 @@ use Chamilo\CoreBundle\Entity\AccessUrl;
 use Chamilo\CoreBundle\Entity\AccessUrlRelUser;
 use Chamilo\CoreBundle\Entity\Course;
 use Chamilo\CoreBundle\Entity\Session;
+use Chamilo\CoreBundle\Entity\SessionRelCourse;
 use Chamilo\CoreBundle\Entity\SessionRelCourseRelUser;
 use Chamilo\CoreBundle\Entity\SessionRelUser;
 use Chamilo\CoreBundle\Entity\User;
@@ -156,6 +157,102 @@ class SessionRepository extends ServiceEntityRepository
             default:
                 throw new Exception(sprintf('Cannot handle status %s', $status));
         }
+    }
+
+    /**
+     * @return array<SessionRelCourse>
+     */
+    public function getSessionCoursesByStatusInUserSubscription(User $user, Session $session, int $relationType, AccessUrl $url = null): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->select('src')
+            ->from(SessionRelCourse::class, 'src')
+            ->innerJoin(
+                SessionRelUser::class,
+                'sru',
+                Join::WITH,
+                'src.session = sru.session'
+            )
+            ->innerJoin('src.session', 'session')
+            ->where(
+                $qb->expr()->eq('session', ':session')
+            )
+            ->andWhere(
+                $qb->expr()->eq('sru.user', ':user')
+            )
+            ->andWhere(
+                $qb->expr()->eq('sru.relationType', ':relation_type')
+            )
+        ;
+
+        $parameters = [
+            'session' => $session,
+            'user' => $user,
+            'relation_type' => $relationType,
+        ];
+
+        if ($url) {
+            $qb->innerJoin('session.urls', 'urls')
+                ->andWhere(
+                    $qb->expr()->eq('urls.url', ':url')
+                )
+            ;
+
+            $parameters['url'] = $url;
+        }
+
+        $qb->setParameters($parameters);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @return array<SessionRelCourse>
+     */
+    public function getSessionCoursesByStatusInCourseSuscription(User $user, Session $session, int $status, AccessUrl $url = null): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->select('src')
+            ->from(SessionRelCourse::class, 'src')
+            ->innerJoin(
+                SessionRelCourseRelUser::class,
+                'srcru',
+                Join::WITH,
+                'src.session = srcru.session AND src.course = srcru.course'
+            )
+            ->innerJoin('srcru.session', 'session')
+            ->where(
+                $qb->expr()->eq('session', ':session')
+            )
+            ->andWhere(
+                $qb->expr()->eq('srcru.user', ':user')
+            )
+            ->andWhere(
+                $qb->expr()->eq('srcru.status', ':status')
+            )
+        ;
+
+        $parameters = [
+            'session' => $session,
+            'user' => $user,
+            'status' => $status,
+        ];
+
+        if ($url) {
+            $qb->innerJoin('session.urls', 'urls')
+                ->andWhere(
+                    $qb->expr()->eq('urls.url', ':url')
+                )
+            ;
+
+            $parameters['url'] = $url;
+        }
+
+        $qb->setParameters($parameters);
+
+        return $qb->getQuery()->getResult();
     }
 
     private function addSessionRelUserFilterByUrl(Session $session, AccessUrl $url): QueryBuilder

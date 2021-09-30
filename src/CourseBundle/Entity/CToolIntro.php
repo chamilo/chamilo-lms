9113bfbdc49ec2268e49761efb6f9da8361ad16b
Christian <christian1827@gmail.com>
2021-09-30 07:13:22 -0500

    Minor: Plugin - Implement Common Cartridge import and export - refs#2747

--- src/CourseBundle/Entity/CToolIntro.php
+++ src/CourseBundle/Entity/CToolIntro.php
@@ -6,7 +6,19 @@ declare(strict_types=1);
 
 namespace Chamilo\CourseBundle\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Chamilo\CoreBundle\Entity\AbstractResource;
+use Chamilo\CoreBundle\Entity\ResourceInterface;
+use Chamilo\CoreBundle\Traits\TimestampableTypedEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * CToolIntro.
@@ -17,9 +29,42 @@ use Doctrine\ORM\Mapping as ORM;
  *         @ORM\Index(name="course", columns={"c_id"})
  *     }
  * )
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Chamilo\CourseBundle\Repository\CToolIntroRepository")
  */
-class CToolIntro
+#[UniqueEntity(
+    fields: ['c_id', 'session_id'],
+    errorPath: 'session_id',
+    message: 'This user-tag relation is already used.',
+)]
+#[ApiResource(
+    collectionOperations: [
+        'get' => [
+            'security' => "is_granted('ROLE_USER')", // the get collection is also filtered by MessageTagExtension
+        ],
+        'post' => [
+            'security_post_denormalize' => "is_granted('CREATE', object)",
+        ],
+    ],
+    itemOperations: [
+        'get' => [
+            'security' => "is_granted('VIEW', object)",
+        ],
+        'put' => [
+            'security' => "is_granted('EDIT', object)",
+        ],
+        'delete' => [
+            'security' => "is_granted('DELETE', object)",
+        ],
+    ],
+    attributes: [
+        'security' => 'is_granted("ROLE_USER") or object.user == user',
+    ]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'c_id' => 'exact',
+    'session_id' => 'exact',
+])]
+class CToolIntro extends AbstractResource implements ResourceInterface
 {
     /**
      * @ORM\Column(name="iid", type="integer")
@@ -102,5 +147,26 @@ class CToolIntro
     public function getSessionId()
     {
         return $this->sessionId;
+    }
+
+    public function getResourceIdentifier(): int
+    {
+        return $this->iid;
+    }
+
+    public function getResourceName(): string
+    {
+        return 'ToolIntro';
+    }
+
+    public function setResourceName(string $name): self
+    {
+        //$this->introText = 'ToolIntro';
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return 'ToolIntro';
     }
 }

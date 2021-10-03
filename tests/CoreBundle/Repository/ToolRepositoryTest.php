3c8b181f1b780e935b5d12d02bdd0e59d17cd63b
Julio <gugli100@gmail.com>
2021-10-03 11:27:43 +0200

    Add ToolRepository.php + test

---
+++ tests/CoreBundle/Repository/ToolRepositoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\Tests\CoreBundle\Repository;
+
+use Chamilo\CoreBundle\Entity\ExtraField;
+use Chamilo\CoreBundle\Entity\Tag;
+use Chamilo\CoreBundle\Entity\Tool;
+use Chamilo\CoreBundle\Entity\UserRelTag;
+use Chamilo\CoreBundle\Repository\TagRepository;
+use Chamilo\CoreBundle\Repository\ToolRepository;
+use Chamilo\Tests\AbstractApiTest;
+use Chamilo\Tests\ChamiloTestTrait;
+
+class ToolRepositoryTest extends AbstractApiTest
+{
+    use ChamiloTestTrait;
+
+    public function testCreate(): void
+    {
+        self::bootKernel();
+
+        $em = $this->getEntityManager();
+        $repo = self::getContainer()->get(ToolRepository::class);
+        $defaultCount = $repo->count([]);
+
+        $tool = (new Tool())
+            ->setName('test')
+        ;
+        $em->persist($tool);
+        $em->flush();
+
+        $this->assertSame($defaultCount + 1, $repo->count([]));
+        $this->assertSame('test', $tool->getName());
+    }
+}

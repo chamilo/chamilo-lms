98b90f0f313b849571397a1ced4f39e88a5c6851
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-10-04 17:21:29 -0500

    Restore styles for Loading componente

--- assets/vue/components/Loading.vue
+++ assets/vue/components/Loading.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="text-center" v-if="visible">
-    <div class="spinner-border " role="status">
+  <div v-if="visible" class="w-full h-full fixed block top-0 left-0 bg-white opacity-60 text-center"
+       style="z-index: 9999;">
+    <div class="spinner-border text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0" role="status">
       <span class="sr-only">Loading</span>
     </div>
   </div>

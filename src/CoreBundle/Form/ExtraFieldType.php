e8af0b3e4b0e30c96101fe055f7503c509cbcfc7
Julio <gugli100@gmail.com>
2021-10-18 09:07:14 +0200

    Minor - format code

--- src/CoreBundle/Form/ExtraFieldType.php
+++ src/CoreBundle/Form/ExtraFieldType.php
@@ -18,7 +18,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
@@ -58,6 +57,7 @@ class ExtraFieldType extends AbstractType
         if (null === $item) {
             return;
         }
+
         $extraFieldType = ExtraField::USER_FIELD_TYPE; // user/course/session ?
         $extraFields = $this->extraFieldRepository->getExtraFields($extraFieldType);
         $values = $this->extraFieldValuesRepository->getExtraFieldValuesFromItem($item, $extraFieldType);
@@ -258,6 +258,7 @@ class ExtraFieldType extends AbstractType
                                 $newValue = $newValue['address'].'::'.$newValue['latitude'].','.$newValue['longitude'];
                             }
                             $this->extraFieldValuesRepository->updateItemData($extraField, $item, $newValue);
+
                             break;
                         case \ExtraField::FIELD_TYPE_TAG:
                             $formItem = $event->getForm()->get($extraField->getVariable());

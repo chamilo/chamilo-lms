ada9ad5e73bc37db2cd3c0890df93b6fb9b508e6
Julio <gugli100@gmail.com>
2021-10-19 15:49:17 +0200

    Fix sticky migrations

--- src/CoreBundle/Migrations/Schema/V200/Version20201212195011.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20201212195011.php
@@ -81,10 +81,11 @@ final class Version20201212195011 extends AbstractMigrationChamilo
         $sql = "SELECT id FROM extra_field
                 WHERE extra_field_type = $extraFieldType AND variable = 'special_course'";
         $result = $connection->executeQuery($sql);
-        $extraFieldId = $result->fetchOne();
+        $extraFieldRow = $result->fetchOne();
 
         $specialCourses = '';
-        if (!empty($extraFieldId)) {
+        if (!empty($extraFieldRow)) {
+            $extraFieldId = (int) $extraFieldRow['id'];
             $sql = 'SELECT DISTINCT(item_id)
                     FROM extra_field_values
                     WHERE field_id = '.$extraFieldId." AND value = '1'";

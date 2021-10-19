259b6e6e0c7102b9f2233896a1de64dceaf928b8
Julio <gugli100@gmail.com>
2021-10-19 16:01:24 +0200

    Fix sticky migrations

--- src/CoreBundle/Migrations/Schema/V200/Version20201212195011.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20201212195011.php
@@ -83,7 +83,7 @@ final class Version20201212195011 extends AbstractMigrationChamilo
         $result = $connection->executeQuery($sql);
         $extraFieldId = $result->fetchOne();
 
-        $specialCourses = '';
+        $specialCourses = [];
         if (!empty($extraFieldId)) {
             $extraFieldId = (int) $extraFieldId;
             $sql = "SELECT DISTINCT(item_id)
@@ -92,7 +92,7 @@ final class Version20201212195011 extends AbstractMigrationChamilo
             $result = $connection->executeQuery($sql);
             $specialCourses = $result->fetchAllAssociative();
             if (!empty($specialCourses)) {
-                $specialCourses = array_column($specialCourses, 'item_id');
+                $specialCourses = array_map('intval', array_column($specialCourses, 'item_id'));
             }
         }
 
@@ -102,7 +102,6 @@ final class Version20201212195011 extends AbstractMigrationChamilo
         foreach ($q->toIterable() as $course) {
             $counter = 1;
             $courseId = $course->getId();
-
             if (!empty($specialCourses) && \in_array($courseId, $specialCourses, true)) {
                 $this->addSql("UPDATE course SET sticky = 1 WHERE id = $courseId ");
             }

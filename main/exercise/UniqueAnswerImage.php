4d7d5c72c3749e1d7f24e5bfd34299551b746ab3
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-10-20 17:48:45 -0500

    UniqueAnswerImage header shows UniqueAnswer header

--- main/exercise/UniqueAnswerImage.php
+++ main/exercise/UniqueAnswerImage.php
@@ -425,29 +425,4 @@ class UniqueAnswerImage extends UniqueAnswer
         $this->updateWeighting($questionWeighting);
         $this->save($exercise);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function return_header(Exercise $exercise, $counter = null, $score = [])
-    {
-        if ($exercise->showExpectedChoice()) {
-            $header = '<table class="'.$this->question_table_class.'">
-			<tr>
-				<th>'.get_lang('Choice').'</th>';
-            if ($exercise->showExpectedChoiceColumn()) {
-                $header .= '<th>'.get_lang('ExpectedChoice').'</th>';
-            }
-            $header .= '<th>'.get_lang('Answer').'</th>';
-            $header .= '<th>'.get_lang('Status').'</th>';
-            if (false === $exercise->hideComment) {
-                $header .= '<th>'.get_lang('Comment').'</th>';
-            }
-            $header .= '</tr>';
-        } else {
-            $header = parent::return_header($exercise, $counter, $score);
-        }
-
-        return $header;
-    }
 }

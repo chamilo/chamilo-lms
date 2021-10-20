3c36a2c266d0a5f942403d03df9fffb56ab6440c
Julio <gugli100@gmail.com>
2021-10-20 13:54:38 +0200

    Sessions: Add session category list BT#19044

---
+++ assets/vue/components/session/SessionListNoCategoryWrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-xl">
+      <v-icon icon="mdi-folder"/>
+      {{ $t('No category') }}
+    </div>
+    <SessionListCategoryWrapper :sessions="sessions"/>
+</template>
+<script>
+
+import SessionListCategoryWrapper from '../../components/session/SessionListCategoryWrapper';
+
+export default {
+  name: 'SessionListNoCategoryWrapper',
+  components: {
+    SessionListCategoryWrapper
+  },
+  props: {
+    sessions: Array,
+  },
+  setup() {
+  }
+}
+
+</script>
--- assets/vue/views/user/sessions/Past.vue
+++ assets/vue/views/user/sessions/Past.vue
@@ -1,6 +1,9 @@
 <template>
   <SessionTabs/>
-  <SessionListWrapper :sessions="sessions"/>
+
+<!--  <SessionListWrapper :sessions="sessions"/>-->
+
+  <SessionCategoryView :result-sessions="resultSessions"/>
 </template>
 
 <script>
@@ -9,13 +12,13 @@ import {useStore} from 'vuex';
 import {useQuery, useResult} from '@vue/apollo-composable'
 import {GET_SESSION_REL_USER} from "../../../graphql/queries/SessionRelUser.js";
 import {DateTime} from 'luxon'
-import SessionListWrapper from '../../../components/session/SessionListWrapper';
+import SessionCategoryView from '../../../components/session/SessionCategoryView';
 import SessionTabs from '../../../components/session/Tabs';
 
 export default {
   name: 'SessionListPast',
   components: {
-    SessionListWrapper,
+    SessionCategoryView,
     SessionTabs
   },
   setup() {
@@ -27,29 +30,14 @@ export default {
       let start = DateTime.local().minus({days: 360}).toISO();
       let end = DateTime.local().toISO();
 
-      const {result, loading} = useQuery(GET_SESSION_REL_USER, {
+      const {result: resultSessions, loading} = useQuery(GET_SESSION_REL_USER, {
         user: "/api/users/" + userId,
         afterStartDate: start,
         beforeEndDate: end,
       });
 
-      const sessions = useResult(result, [], ({sessionRelUsers}) => {
-        let sessionList = [];
-        sessionRelUsers.edges.map(({node}) => {
-          const sessionExists = sessionList.findIndex(suSession => suSession._id === node.session._id) >= 0;
-          if (!sessionExists) {
-            sessionList.push(node.session);
-          }
-
-          return sessionExists ? null : node.session;
-        });
-
-        return sessionList;
-      });
-
-      console.log(sessions);
       return {
-        sessions,
+        resultSessions,
         loading
       }
     }

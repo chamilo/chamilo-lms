aa07f86ec1e659317d57be02d8b7f37a3c5c53f7
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-11-08 10:55:19 -0500

    Exercise: avoid duplicates in attempts list - refs BT#19342

--- main/inc/lib/exercise.lib.php
+++ main/inc/lib/exercise.lib.php
@@ -2440,6 +2440,8 @@ HOTSPOT;
                 $courseCondition
                 $exerciseFilter
                 $sessionCondition
+            ORDER BY tr.insert_date DESC
+            LIMIT 1
         )";
 
         if ($is_allowedToEdit) {

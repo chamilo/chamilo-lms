1e15dac49b6739be0212d076a149d5960a4897bb
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-12-06 10:03:40 -0500

    Calendar: fix date comparaison when personal_calendar_show_sessions_occupation is enabled - refs BT#19393

--- main/inc/lib/agenda.lib.php
+++ main/inc/lib/agenda.lib.php
@@ -4130,6 +4130,8 @@ class Agenda
             return;
         }
 
+        $start = api_get_utc_datetime($start, false, true);
+        $end = api_get_utc_datetime($end, false, true);
         $userInfo = api_get_user_info();
         $sessionList = SessionManager::getSessionsFollowedByUser($userInfo['id'], $userInfo['status']);
 

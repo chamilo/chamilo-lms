e4adbec5992ca6372be8f92ad855462f9a2b8054
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2021-12-17 11:28:31 -0500

    LP: Fix delete c_tool for category as course tool - refs BT#19453

--- main/lp/learnpath.class.php
+++ main/lp/learnpath.class.php
@@ -12834,10 +12834,10 @@ EOD;
 
             // Delete link tool
             $tbl_tool = Database::get_course_table(TABLE_TOOL_LIST);
-            $link = 'lp/lp_controller.php?cidReq='.$courseInfo['code'].'&id_session='.$sessionId.'&gidReq=0&gradebook=0&origin=&action=view_category&id='.$id;
+            $linkLikeValue = "lp/lp_controller.php?%&action=view_category&id=$id%";
             // Delete tools
             $sql = "DELETE FROM $tbl_tool
-                    WHERE c_id = ".$courseId." AND (link LIKE '$link%' AND image='lp_category.gif')";
+                    WHERE c_id = ".$courseId." AND (link LIKE '$linkLikeValue' AND image='lp_category.gif')";
             Database::query($sql);
 
             return true;

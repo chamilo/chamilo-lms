282f38c7b328d50f1c008b68210dc9b2c69d0150
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2022-03-03 06:51:07 -0500

    Update JS libs

--- assets/vue/store/index.js
+++ assets/vue/store/index.js
@@ -1,11 +1,9 @@
 import { createStore, createLogger } from "vuex";
 import notifications from './modules/notifications';
 import SecurityModule from "./security";
-import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
     plugins: [
-        createPersistedState(),
         //createLogger(),
     ],
     modules: {

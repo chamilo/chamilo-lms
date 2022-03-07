b33e86e155ddb9615b60a3964b1fd8613a2833a2
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2022-03-07 00:16:41 -0500

    Return RedirectResponse when it isn't installed

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 /* For licensing terms, see /license.txt */
 
 use Chamilo\Kernel;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
@@ -10,9 +11,7 @@ return function (array $context) {
     // Chamilo
     $isInstalled = $context['APP_INSTALLED'] ?? null;
     if (1 !== (int) $isInstalled) {
-        // Does not support subdirectories for now
-        header('Location: /main/install/index.php');
-        exit;
+        return new HttpRedirectResponse('./main/install/index.php');
     }
 
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);

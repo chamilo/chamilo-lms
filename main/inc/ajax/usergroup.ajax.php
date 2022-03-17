1679732e436f1bfcd65c6e112475e2efc87a0c6f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2022-03-17 15:45:01 -0500

    Group: Don't use $_REQUEST directly

--- main/inc/ajax/usergroup.ajax.php
+++ main/inc/ajax/usergroup.ajax.php
@@ -5,14 +5,20 @@
 /**
  * Responses to AJAX calls.
  */
+
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+
 require_once __DIR__.'/../global.inc.php';
 
-$action = isset($_REQUEST['a']) ? $_REQUEST['a'] : '';
+$httpRequest = HttpRequest::createFromGlobals();
+
+$action = $httpRequest->query->has('a') ? $httpRequest->query->get('a') : $httpRequest->request->get('a');
+
 $isAllowedToEdit = api_is_allowed_to_edit();
 
 switch ($action) {
     case 'get_class_by_keyword':
-        $keyword = isset($_REQUEST['q']) ? $_REQUEST['q'] : '';
+        $keyword = $httpRequest->query->has('q') ? $httpRequest->query->get('q') : $httpRequest->request->get('q');
         $allow = api_is_platform_admin() || api_is_session_admin();
 
         if ($allow && !empty($keyword)) {
@@ -32,9 +38,13 @@ switch ($action) {
     case 'delete_user_in_usergroup':
         if ($isAllowedToEdit) {
             $userGroup = new UserGroup();
-            $userId = isset($_REQUEST['id']) ? $_REQUEST['id'] : 0;
+            $userId = $httpRequest->query->has('id')
+                ? $httpRequest->query->getInt('id')
+                : $httpRequest->request->getInt('id');
             $userIdList = explode(',', $userId);
-            $groupId = isset($_REQUEST['group_id']) ? $_REQUEST['group_id'] : 0;
+            $groupId = $httpRequest->query->has('group_id')
+                ? $httpRequest->query->getInt('group_id')
+                : $httpRequest->request->getInt('group_id');
             foreach ($userIdList as $userId) {
                 $userGroup->delete_user_rel_group($userId, $groupId);
             }

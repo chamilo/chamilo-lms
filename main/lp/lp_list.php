chamilo/chamilo-lms

commit 9ad315af4dc5ff7c60a0ed7394599a605398b439

Learnpath: Enable HTML presentation of title of LP in LP list -refs BT#19939

main/lp/lp_list.php
@@ -358,8 +358,12 @@ foreach ($categories as $item) {
                 $oddclass = 'row_odd';
             }
 
-            $url_start_lp = 'lp_controller.php?'.$cidReq.'&action=view&lp_id='.$id;
-            $name = trim(strip_tags(Security::remove_XSS($details['lp_name'])));
+	    $url_start_lp = 'lp_controller.php?'.$cidReq.'&action=view&lp_id='.$id;
+            if (api_get_configuration_value('save_titles_as_html')) {
+                $name = trim(Security::remove_XSS($details['lp_name']));
+            } else {
+                $name = trim(strip_tags(Security::remove_XSS($details['lp_name'])));
+            }
             $extra = null;
 
             if ($is_allowed_to_edit) {

main/install/configuration.dist.php
@@ -484,6 +484,7 @@ ALTER TABLE c_lp CHANGE name name LONGTEXT NOT NULL;
 ALTER TABLE c_lp_item CHANGE title title LONGTEXT NOT NULL;
 --
 */
+// This option will not remove tags when presenting LP list so it might be a source of security vulnerability.
 // $_configuration['save_titles_as_html'] = false;
 // Show the full toolbar set to all CKEditor
 //$_configuration['full_ckeditor_toolbar_set'] = false;

commit db479c12399c1b728a6fb229f63d4291cf01c53d

Learnpath: Fix delete LP javascript broken by commit 9ad315af Enable HTML presentation of title of LP in LP list -refs BT#19939

main/lp/lp_list.php
@@ -799,7 +799,7 @@ foreach ($categories as $item) {
                         ),
                         'lp_controller.php?'.$cidReq."&action=delete&lp_id=$id",
                         [
-                            'onclick' => "javascript: return confirmation('".addslashes($name)."');",
+                            'onclick' => "javascript: return confirmation('".addslashes($strip_tags(name))."');",
                         ]
                     );
                 } else {
fa58c537613cec7d601ef0e36cfadf7fa10255f7
Christian <christian1827@gmail.com>
2022-05-10 10:07:34 -0500

    Exercise - Fix conflict with file usermanager.lib.php - refs BT#19930

--- main/inc/lib/usermanager.lib.php
+++ main/inc/lib/usermanager.lib.php
@@ -3173,9 +3173,7 @@ class UserManager
 
                     $fval = $efv->get_values_by_handler_and_field_variable($user_id, $row['fvar']);
                     $fopt = $efo->get_field_option_by_field_and_option($row['id'], $fval['value']);
-                    if (is_array($fopt)) {
-                        $fopt = current($fopt);
-                    }
+                    $fopt = current($fopt);
                     $fOptText = $fopt ? $fopt['display_text'] : $fval['value'];
 
                     if ($prefix) {

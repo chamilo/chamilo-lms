chamilo/chamilo-lms

commit db479c12399c1b728a6fb229f63d4291cf01c53d

Learnpath: Fix delete LP javascript broken by commit 9ad315af Enable HTML presentation of title of LP in LP list -refs BT#19939

main/lp/lp_list.php
@@ -799,7 +799,7 @@ foreach ($categories as $item) {
                         ),
                         'lp_controller.php?'.$cidReq."&action=delete&lp_id=$id",
                         [
-                            'onclick' => "javascript: return confirmation('".addslashes($name)."');",
+                            'onclick' => "javascript: return confirmation('".addslashes($strip_tags(name))."');",
                         ]
                     );
                 } else {

commit a6be837f868846df75a061db5970e6b09ac76251

Learnpath: Fix delete LP javascript broken by commit db479c1 Enable HTML presentation of title of LP in LP list -refs BT#19939

main/lp/lp_list.php
@@ -799,7 +799,7 @@ foreach ($categories as $item) {
                         ),
                         'lp_controller.php?'.$cidReq."&action=delete&lp_id=$id",
                         [
-                            'onclick' => "javascript: return confirmation('".addslashes($strip_tags(name))."');",
+                            'onclick' => "javascript: return confirmation('".addslashes(strip_tags($name))."');",
                         ]
                     );
                 } else {
a01b1f1d8d796dc5a1203c9d37a79ae8332da6e6
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2022-06-22 13:25:33 -0500

    Plugin: OAuth2: check if user exists before trying to create it - refs BT#19734

--- plugin/oauth2/src/OAuth2.php
+++ plugin/oauth2/src/OAuth2.php
@@ -213,6 +213,21 @@ class OAuth2 extends Plugin
         );
         if (false === $result) {
             $this->log('user not found', "extrafield 'oauth2_id' with value '$resourceOwnerId'");
+
+            $username = $this->getValueByKey(
+                $response,
+                $this->get(self::SETTING_RESPONSE_RESOURCE_OWNER_USERNAME),
+                'oauth2user_'.$resourceOwnerId
+            );
+
+            $userInfo = api_get_user_info_from_username($username);
+
+            if (false !== $userInfo && !empty($userInfo['id']) && 'platform' === $userInfo['auth_source']) {
+                $this->log('platform user exists', print_r($userInfo, true));
+
+                return $userInfo;
+            }
+
             // authenticated user not found in internal database
             if ('true' !== $this->get(self::SETTING_CREATE_NEW_USERS)) {
                 $this->log('exception', 'create_new_users setting is disabled');
@@ -222,6 +237,8 @@ class OAuth2 extends Plugin
                 );
                 throw new RuntimeException($message);
             }
+
+            require_once __DIR__.'/../../../main/auth/external_login/functions.inc.php';
 
             $firstName = $this->getValueByKey(
                 $response,
@@ -243,35 +260,21 @@ class OAuth2 extends Plugin
                 $this->get(self::SETTING_RESPONSE_RESOURCE_OWNER_EMAIL),
                 'oauth2user_'.$resourceOwnerId.'@'.(gethostname() or 'localhost')
             );
-            $username = $this->getValueByKey(
-                $response,
-                $this->get(self::SETTING_RESPONSE_RESOURCE_OWNER_USERNAME),
-                'oauth2user_'.$resourceOwnerId
-            );
 
-            $userInfo = api_get_user_info_from_username($username);
-
-            if (false !== $userInfo && !empty($userInfo['id']) && 'platform' === $userInfo['auth_source']) {
-                $this->log('platform user exists', print_r($userInfo, true));
-                $userId = $userInfo['id'];
-            } else {
-                require_once __DIR__.'/../../../main/auth/external_login/functions.inc.php';
-
-                $userInfo = [
-                    'firstname' => $firstName,
-                    'lastname' => $lastName,
-                    'status' => $status,
-                    'email' => $email,
-                    'username' => $username,
-                    'auth_source' => 'oauth2',
-                ];
-                $userId = external_add_user($userInfo);
-                if (false === $userId) {
-                    $this->log('user not created', print_r($userInfo, true));
-                    throw new RuntimeException($this->get_lang('FailedUserCreation'));
-                }
-                $this->log('user created', (string) $userId);
+            $userInfo = [
+                'firstname' => $firstName,
+                'lastname' => $lastName,
+                'status' => $status,
+                'email' => $email,
+                'username' => $username,
+                'auth_source' => 'oauth2',
+            ];
+            $userId = external_add_user($userInfo);
+            if (false === $userId) {
+                $this->log('user not created', print_r($userInfo, true));
+                throw new RuntimeException($this->get_lang('FailedUserCreation'));
             }
+            $this->log('user created', (string) $userId);
             $this->updateUser($userId, $response);
             // Not checking function update_extra_field_value return value because not reliable
             UserManager::update_extra_field_value($userId, self::EXTRA_FIELD_OAUTH2_ID, $resourceOwnerId);

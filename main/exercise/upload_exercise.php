21f8a17bd2bccc684ed83b4aef376f1fd18ba78f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2022-08-10 13:54:58 -0500

    Quiz: Allow import multiple answer dropdown from excel - refs BT#19479

--- main/exercise/upload_exercise.php
+++ main/exercise/upload_exercise.php
@@ -85,6 +85,7 @@ function lp_upload_quiz_main()
     $tableList = [
         UNIQUE_ANSWER => get_lang('UniqueSelect'),
         MULTIPLE_ANSWER => get_lang('MultipleSelect'),
+        MULTIPLE_ANSWER_DROPDOWN => get_lang('MultipleAnswerDropdown'),
         FILL_IN_BLANKS => get_lang('FillBlanks'),
         FILL_IN_BLANKS_GLOBAL => get_lang('FillBlanksGlobal'),
         MATCHING => get_lang('Matching'),
@@ -329,6 +330,9 @@ function lp_upload_quiz_action_handling()
                 case MULTIPLE_ANSWER:
                     $answer = new MultipleAnswer();
                     break;
+                case MULTIPLE_ANSWER_DROPDOWN:
+                    $answer = new MultipleAnswerDropdown();
+                    break;
                 case FILL_IN_BLANKS:
                 case FILL_IN_BLANKS_GLOBAL:
                     $answer = new FillBlanks();
@@ -362,6 +366,7 @@ function lp_upload_quiz_action_handling()
             }
             switch ($detectQuestionType) {
                 case GLOBAL_MULTIPLE_ANSWER:
+                case MULTIPLE_ANSWER_DROPDOWN:
                 case MULTIPLE_ANSWER:
                 case UNIQUE_ANSWER:
                     $total = 0;
@@ -424,6 +429,9 @@ function lp_upload_quiz_action_handling()
                                         //$total = $total - $score;
                                     }
                                     break;
+                                case MULTIPLE_ANSWER_DROPDOWN:
+                                    $score = 0;
+                                    break;
                             }
 
                             $objAnswer->createAnswer(
@@ -450,6 +458,7 @@ function lp_upload_quiz_action_handling()
                         if ($questionObj) {
                             switch ($detectQuestionType) {
                                 case GLOBAL_MULTIPLE_ANSWER:
+                                case MULTIPLE_ANSWER_DROPDOWN:
                                     $questionObj->updateWeighting($globalScore);
                                     break;
                                 case UNIQUE_ANSWER:

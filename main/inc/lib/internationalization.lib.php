8d434ac9899e1eec164f19cd7e6719df2ee9ad9c
Borja Sánchez <borja.sanchez@beeznest.com>
2022-11-15 10:40:32 +0100

    Minor - Fix PHPDoc comment

--- main/inc/lib/internationalization.lib.php
+++ main/inc/lib/internationalization.lib.php
@@ -1047,6 +1047,7 @@ function api_sort_by_first_name($language = null)
  * If no DateTime is not sent, use the current date.
  *
  * @param string|null $date (optional) The date or null.
+ *
  * @return array E.G.: ['quarter_start' => '2022-10-11',
  *               'quarter_end' => '2022-12-31',
  *               'quarter_title' => 'Q4 2022']

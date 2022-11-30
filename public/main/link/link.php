a07e592f90db9d2ce93ceb75ab2a2aa56032bb39
Borja Sánchez <borja.sanchez@beeznest.com>
2022-11-30 19:53:39 +0100

    Link: Set nested view by default, fix enter to edit Link view and fix update parent node - refs GH#3665

--- public/main/link/link.php
+++ public/main/link/link.php
@@ -72,7 +72,7 @@ Event::event_access_tool(TOOL_LINK);
 /*	Action Handling */
 $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : null;
 $scope = isset($_REQUEST['scope']) ? $_REQUEST['scope'] : null;
-$show = isset($_REQUEST['show']) && in_array(trim($_REQUEST['show']), ['all', 'none']) ? $_REQUEST['show'] : '';
+$show = isset($_REQUEST['show']) && in_array(trim($_REQUEST['show']), ['all', 'none']) ? $_REQUEST['show'] : 'all';
 $categoryId = isset($_REQUEST['category_id']) ? (int) $_REQUEST['category_id'] : '';
 $linkListUrl = api_get_self().'?'.api_get_cidreq().'&category_id='.$categoryId.'&show='.$show;
 $content = '';

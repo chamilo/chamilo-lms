da084223c9ad2168575f22825c39004c74177c09
Borja Sánchez <borja.sanchez@beeznest.com>
2022-12-12 14:37:29 +0100

    Minor: Add braces to vars in SQL

--- public/main/gradebook/lib/be/result.class.php
+++ public/main/gradebook/lib/be/result.class.php
@@ -269,10 +269,10 @@ class Result
             $sql .= 'null';
         }
         if (isset($this->id)) {
-            $sql .= " WHERE id = $this->id";
+            $sql .= " WHERE id = {$this->id}";
         } else {
-            $sql .= " WHERE evaluation_id = $this->evaluation
-                AND user_id = $this->user_id
+            $sql .= " WHERE evaluation_id = {$this->evaluation}
+                AND user_id = {$this->user_id}
             ";
         }
 

2bd03b20fae3e7231e2afa60acd3710f9f8bf0b2
Yannick Warnier <ywarnier@users.noreply.github.com>
2023-01-17 14:45:59 +0100

    Minor: Plugin: ExtraMenuFromWebService: Translate to French - refs BT#20442

--- plugin/extramenufromwebservice/lang/french.php
+++ plugin/extramenufromwebservice/lang/french.php
@@ -1,15 +1,15 @@
 <?php
 
-$strings['plugin_title'] = "Extra menu from webservice";
-$strings['plugin_comment'] = "Add a menu obtained by webservice";
-$strings['authentication_url'] = 'Authentication url';
-$strings['authentication_email'] = 'Email for authentication.';
-$strings['authentication_password'] = 'Password for authentication';
-$strings['normal_menu_url'] = 'Url for normal menu webservice';
-$strings['mobile_menu_url'] = 'Url for mobile menu webservice';
-$strings['tool_enable'] = 'ExtraMenuFromWebservice plugin enabled';
-$strings['tool_enable_help'] = "Choose whether you want to enable the ExtraMenuFromWebservice plugin.";
-$strings['session_timeout'] = "Token in session timeout, in seconds";
-$strings['session_timeout_help'] = "If time is not specified, it will be 86400 seconds.";
-$strings['list_css_imports'] = "List of CSS urls to import, separated by ';'";
-$strings['list_fonts_imports'] = "List of fonts urls to import, separated by ';'";
+$strings['plugin_title'] = "Menu extra depuis un service web";
+$strings['plugin_comment'] = "Ajouter un menu flottant fourni par un service web sur base de l'e-mail utilisateur";
+$strings['authentication_url'] = "URL d'authentication";
+$strings['authentication_email'] = "Email pour l'authentification.";
+$strings['authentication_password'] = "Mot de passe pour l'authentification";
+$strings['normal_menu_url'] = "URL pour le web service du menu standard";
+$strings['mobile_menu_url'] = "URL pour le web service du menu mobile";
+$strings['tool_enable'] = "Plugin ExtraMenuFromWebservice activé";
+$strings['tool_enable_help'] = "Activer ou désactiver ce plugin.";
+$strings['session_timeout'] = "Durée de vie du token, en secondes";
+$strings['session_timeout_help'] = "Évite de recharger le menu de manière répétée. Si aucun temps n'est défini, le plugin utilisera la valeur de 86400 secondes (24h).";
+$strings['list_css_imports'] = "Liste des URLs CSS à importer, séparées par des points-virgule (';')";
+$strings['list_fonts_imports'] = "Liste des fontes de caractères à importer, séparées par des points-virgule (';')";

34262c1674511b14a63cf208d821c4fd1e611a5c
Yannick Warnier <ywarnier@beeznest.org>
2023-01-18 12:35:49 +0100

    Plugin: Dictionary: Improve documentation

--- plugin/dictionary/lang/english.php
+++ plugin/dictionary/lang/english.php
@@ -1,7 +1,6 @@
 <?php
 $strings['plugin_title'] = "Extensible Dictionary";
-$strings['plugin_comment'] = "This plugin allows the admins to add terms and definitions.";
+$strings['plugin_comment'] = "This plugin allows the admins to add terms and definitions to a global dictionary. Only admins have access to it (through /plugin/dictionary/terms.php) as it is mainly useful to extend upon through other plugins, at this point in time.";
 $strings['enable_plugin_dictionary'] = "Enable plugin";
-
 $strings['Term'] = "Term";
 $strings['Definition'] = "Definition";

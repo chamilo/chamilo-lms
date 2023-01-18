6f6be22df1928ffeb4017818788be6688271e76c
Yannick Warnier <ywarnier@beeznest.org>
2023-01-18 17:48:18 +0100

    Plugin: Exercise Signature: Improve documentation

--- plugin/exercise_signature/lang/english.php
+++ plugin/exercise_signature/lang/english.php
@@ -4,7 +4,7 @@
 
 // Needed in order to show the plugin title
 $strings['plugin_title'] = "Exercise signature";
-$strings['plugin_comment'] = "";
+$strings['plugin_comment'] = "Add the possibility for teachers to ask for a signature at the end of any exam attempt. The signature request is made through a modal window with a whiteboard-style area that works in touch mode, and teachers can later check (manually) the signature attached to the attempt.";
 $strings['tool_enable'] = 'Tool enabled';
 $strings['ProvideASignatureFirst'] = 'Please provide a signature first';
 $strings['Sign'] = 'Sign';

54d9590102c19f983de3aa487d12edb87435cd7d
Francis Gonzales <fgonzalestello91@gmail.com>
2023-02-28 17:57:43 -0500

    Fixing PHP-CS

--- plugin/text2speech/src/mozillatts/MozillaTTS.php
+++ plugin/text2speech/src/mozillatts/MozillaTTS.php
@@ -26,7 +26,7 @@ class MozillaTTS implements IProvider
         $filePath = $this->filePath.$filename;
         file_put_contents($filePath, file_get_contents(
             $this->url.'?api_key='.urlencode($this->apiKey).
-            '&text='.str_replace('%0A','+', urlencode($data))
+            '&text='.str_replace('%0A', '+', urlencode($data))
         ));
 
         return $filename;

0d190c7c17b130370b300e8b0ab4c20fe5c7af1d
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-03-13 16:57:21 -0500

    Plugin: OAuth2: Add message about redirect to provider when user's auth_source has been changed - refs BT#20611

--- plugin/oauth2/src/OAuth2.php
+++ plugin/oauth2/src/OAuth2.php
@@ -1,6 +1,8 @@
 <?php
 /* For license terms, see /license.txt */
 
+use Chamilo\CoreBundle\Entity\ExtraFieldValues;
+use Chamilo\CoreBundle\Entity\TrackELogin;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GenericProvider;
@@ -469,5 +471,38 @@ class OAuth2 extends Plugin
         if (self::DEBUG) {
             error_log("OAuth2 plugin: $key: $content");
         }
+    }
+
+    public static function isFirstLoginAfterAuthSource(int $userId): bool
+    {
+        $em = Database::getManager();
+
+        $lastLogin = $em
+            ->getRepository(TrackELogin::class)
+            ->findOneBy(
+                ['loginUserId' => $userId],
+                ['loginDate' => 'DESC']
+            )
+        ;
+
+        if (!$lastLogin) {
+            return false;
+        }
+
+        $objExtraField = new ExtraField('user');
+        $field = $objExtraField->getHandlerEntityByFieldVariable(self::EXTRA_FIELD_OAUTH2_ID);
+
+        $fieldValue = $em
+            ->getRepository(ExtraFieldValues::class)
+            ->findOneBy(
+                ['itemId' => $userId, 'field' => $field]
+            )
+        ;
+
+        if (!$fieldValue) {
+            return false;
+        }
+
+        return $fieldValue->getCreatedAt() >= $lastLogin->getLoginDate();
     }
 }

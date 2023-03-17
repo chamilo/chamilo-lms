628ef5e669adac6db016521d206362c197a70d87
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-03-17 17:30:26 -0500

    Minor: Format code

--- assets/vue/components/resource_links/ShowLinks.vue
+++ assets/vue/components/resource_links/ShowLinks.vue
@@ -2,21 +2,21 @@
   <div v-if="item && item['resourceLinkListFromEntity']">
     <v-card>
       <v-list-item
-          v-for="link in item['resourceLinkListFromEntity']"
+        v-for="link in item['resourceLinkListFromEntity']"
       >
         <v-list-item-content>
           <div v-if="link['course']">
-            <v-icon icon="mdi-book"/>
+            <v-icon icon="mdi-book" />
             {{ $t('Course') }}: {{ link.course.resourceNode.title }}
           </div>
 
           <div v-if="link['session']">
-            <v-icon icon="mdi-book-open"/>
+            <v-icon icon="mdi-book-open" />
             {{ $t('Session') }}: {{ link.session.name }}
           </div>
 
           <div v-if="link['group']">
-            <v-icon icon="mdi-people"/>
+            <v-icon icon="mdi-people" />
             {{ $t('Group') }}: {{ link.group.resourceNode.title }}
           </div>
 
@@ -25,7 +25,7 @@
           </div>
 
           <div v-if="link['user']">
-            <v-icon icon="mdi-account"/>
+            <v-icon icon="mdi-account" />
             <!--  @todo add avatar        -->
             <!--  <q-avatar size="32px">-->
             <!--            <img :src="link.user.illustrationUrl + '?w=80&h=80&fit=crop'" />-->
@@ -38,37 +38,25 @@
           </div>
 
           <q-select
-              v-if="editStatus"
-              v-model="link.visibility"
-              :options="visibilityList"
-              emit-value
-              filled
-              label="Status"
-              map-options
+            v-if="editStatus"
+            v-model="link.visibility"
+            :options="visibilityList"
+            emit-value
+            filled
+            label="Status"
+            map-options
           />
-
         </v-list-item-content>
       </v-list-item>
     </v-card>
   </div>
 </template>
-<script>
 
-import {toRefs} from "vue";
+<script>
 import {RESOURCE_LINK_PUBLISHED, RESOURCE_LINK_DRAFT} from "./visibility";
 
 export default {
   name: 'ShowLinks',
-  setup(props) {
-    const visibilityList = [
-      {value: RESOURCE_LINK_PUBLISHED, label: 'Published'},
-      {value: RESOURCE_LINK_DRAFT, label: 'Draft'},
-    ];
-
-    return {
-      visibilityList,
-    };
-  },
   props: {
     item: {
       type: Object,
@@ -84,6 +72,16 @@ export default {
       required: false,
       default: false
     }
+  },
+  setup() {
+    const visibilityList = [
+      {value: RESOURCE_LINK_PUBLISHED, label: 'Published'},
+      {value: RESOURCE_LINK_DRAFT, label: 'Draft'},
+    ];
+
+    return {
+      visibilityList,
+    };
   }
 };
 </script>

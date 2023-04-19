cbc9d605497a77a9d615bcaec2e5215c564634c7
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-04-19 16:34:30 -0500

    Minor: Format code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,61 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-      './assets/**/*.{js,vue}',
-      './public/main/**/*.{php,twig}',
-      './src/CoreBudnle/Resources/views/**/*.html.twig',
+    "./assets/**/*.{js,vue}",
+    "./public/main/**/*.{php,twig}",
+    "./src/CoreBudnle/Resources/views/**/*.html.twig",
   ],
   theme: {
     colors: {
-      'primary': {
-          DEFAULT: '#2e75a3',
-          'gradient': '#9CC2DA',
+      primary: {
+        DEFAULT: "#2e75a3",
+        gradient: "#9CC2DA",
       },
-      'secondary': '#fd6600',
-      'gray': {
-        5: '#fcfcfc',
-        10: '#fafafa',
-        15: '#f7f8f9',
-        20: '#edf0f2',
-        25: '#e4e9ed',
-        30: 'rgba(0, 0, 0, 0.12)',
-        50: '#a2a6b0',
-        90: '#333333',
+      secondary: "#fd6600",
+      gray: {
+        5: "#fcfcfc",
+        10: "#fafafa",
+        15: "#f7f8f9",
+        20: "#edf0f2",
+        25: "#e4e9ed",
+        30: "rgba(0, 0, 0, 0.12)",
+        50: "#a2a6b0",
+        90: "#333333",
       },
-      'support': {
-        1: 'rgba(46, 117, 163, 0.08)',
-        2: '#f5f8fa',
-        3: 'rgba(46, 117, 163, 0.5)',
-        4: '#244d67',
-        5: '#e06410',
-        6: '#faf7f5',
+      support: {
+        1: "rgba(46, 117, 163, 0.08)",
+        2: "#f5f8fa",
+        3: "rgba(46, 117, 163, 0.5)",
+        4: "#244d67",
+        5: "#e06410",
+        6: "#faf7f5",
       },
-      'warning': '#f5ce01',
-      'success': '#77aa0c',
-      'error': '#df3b3b',
-      'info': '#0d7bfd',
+      warning: "#f5ce01",
+      success: "#77aa0c",
+      error: "#df3b3b",
+      info: "#0d7bfd",
 
-      'white': colors.white,
-      'black': colors.black,
-      'transparent': colors.transparent,
-      'current': colors.current,
+      white: colors.white,
+      black: colors.black,
+      transparent: colors.transparent,
+      current: colors.current,
     },
     extend: {
       fontFamily: {
-        sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif']
+        sans: ["Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
       },
       gridTemplateColumns: {
-        'course-tools': 'repeat(auto-fit, 120px)',
-      }
+        "course-tools": "repeat(auto-fit, 120px)",
+      },
     },
   },
   corePlugins: {
     aspectRatio: true,
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/forms'),
-    require("@tailwindcss/typography")
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
   ],
-}
+};

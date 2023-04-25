9e27d8b52300b564b3b36ce75d75f1710a533721
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-04-25 13:24:16 -0500

    UI: Include Pinia state in main_installer.js

--- assets/vue/main_installer.js
+++ assets/vue/main_installer.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import AppInstaller from './AppInstaller';
 import PrimeVue from 'primevue/config';
 import i18n from './i18n';
 
 const app = createApp(AppInstaller);
 
+const pinia = createPinia();
+
 app
     .use(PrimeVue, { ripple: false })
     .use(i18n)
+    .use(pinia)
 
     .mount('#app');

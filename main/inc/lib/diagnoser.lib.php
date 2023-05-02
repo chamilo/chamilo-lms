9ffdbf8844c0a9ddda57d00ceb8d13e33da01b2f
NicoDucou <nicolas@ldd.fr>
2023-05-02 17:38:39 +0200

    Admin: limit course listing to specifi URLs in case of multiURL context -refs BT#20721 #4696

--- main/inc/lib/diagnoser.lib.php
+++ main/inc/lib/diagnoser.lib.php
@@ -834,8 +834,13 @@ class Diagnoser
         $array = [];
 
         $em = Database::getManager();
-        $connection = $em->getConnection();
-        $res = $connection->query('SELECT id, code, directory, disk_quota, last_visit FROM course ORDER BY last_visit DESC, code LIMIT 500');
+	$connection = $em->getConnection();
+        $multiUrlQueryExtra = "";
+        if (api_is_multiple_url_enabled()) {
+            $access_url_id = api_get_current_access_url_id();
+            $multiUrlQueryExtra = " WHERE id in (select c_id from access_url_rel_course where access_url_id = " . $access_url_id . ")";
+        }
+        $res = $connection->query('SELECT id, code, directory, disk_quota, last_visit FROM course' . $multiUrlQueryExtra . ' ORDER BY last_visit DESC, code LIMIT 500');
         $systemPath = api_get_path(SYS_COURSE_PATH);
         $webPath = api_get_path(WEB_COURSE_PATH);
         $courseHomeIcon = Display::return_icon('home.png', get_lang('CourseHome'));

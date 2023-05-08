b4cb71de5bf52548ef1743266b6f849be0c12a69
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-05-08 16:18:38 -0500

    Minor: Format code

--- assets/vue/pages/Home.vue
+++ assets/vue/pages/Home.vue
@@ -1,56 +1,46 @@
 <template>
-  <div
-    v-if="announcements.length"
-  >
-    <SystemAnnouncementCardList
-      :announcements="announcements"
-    />
+  <div v-if="announcements.length">
+    <SystemAnnouncementCardList :announcements="announcements" />
   </div>
 
-  <div
-    v-if="pages.length"
-    class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 mt-2"
-  >
-    <PageCardList
-      :pages="pages"
-    />
+  <div v-if="pages.length" class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 mt-2">
+    <PageCardList :pages="pages" />
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import {reactive, toRefs} from 'vue'
-import {useStore} from "vuex";
-import {useI18n} from "vue-i18n";
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useI18n } from "vue-i18n";
 import PageCardList from "../components/page/PageCardList";
 import SystemAnnouncementCardList from "../components/systemannouncement/SystemAnnouncementCardList";
 
 const store = useStore();
-const state = reactive({
-  announcements: [],
-  pages: [],
-});
 
-axios.get('/news/list').then(response => {
-  if (Array.isArray(response.data)) {
-    state.announcements = response.data;
-  }
-}).catch(function (error) {
-  console.log(error);
-});
+const announcements = ref([]);
+const pages = ref([]);
 
-const {locale} = useI18n();
+axios
+  .get("/news/list")
+  .then((response) => {
+    if (Array.isArray(response.data)) {
+      announcements.value = response.data;
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-store.dispatch(
-  'page/findAll',
-  {
-    'category.title': 'home',
-    'enabled': '1',
-    'locale': locale.value
-  }
-).then((response) => {
-  state.pages = response;
-});
+const { locale } = useI18n();
 
-const {announcements, pages} = toRefs(state);
+store
+  .dispatch("page/findAll", {
+    "category.title": "home",
+    enabled: "1",
+    locale: locale.value,
+  })
+  .then((response) => {
+    pages.value = response;
+  });
 </script>

7a00a99921abd291bc2f8e08e06e591a0b2acbde
Daniel Gayoso González <danigayosog@gmail.com>
2023-06-02 13:32:08 +0200

    Show recording time on AudioRecorder
    * Add properties to show interface for audio recorded in order to customize appearance when needed
    * Expose record and stop methods to use outside component

--- assets/vue/components/AudioRecorder.vue
+++ assets/vue/components/AudioRecorder.vue
@@ -1,21 +1,46 @@
 <template>
   <div v-if="isMicrophoneSupported">
-    <BaseButton v-if="recorderState.isRecording" :label="t('Stop recording')" icon="stop" type="danger" @click="stop"/>
-    <BaseButton v-else :label="t('Start recording')" icon="microphone" type="primary" @click="record"/>
-
-    <div v-for="(audio, index) in recorderState.audioList" :key="index" class="py-2">
-      <audio class="max-w-full" controls>
-        <source :src="window.URL.createObjectURL(audio)"/>
-      </audio>
+    <div class="flex flex-row">
+      <div v-if="recorderState.isRecording" class="flex rounded-md mr-2 mb-2 py-2 px-3 border border-error">
+        <BaseIcon class="self-center mr-2 text-error motion-safe:animate-pulse" icon="microphone"/>
+        <p class="self-center font-semibold text-error">
+          {{ recordedTime }}
+        </p>
+      </div>
 
       <BaseButton
-        :label="$t('Attach')"
-        class="my-1"
-        icon="attachment"
-        size="small"
-        type="success"
-        @click="attachAudio(audio)"
+        v-if="showButtons && recorderState.isRecording"
+        :label="t('Stop recording')"
+        class="mr-2 mb-2"
+        icon="stop"
+        type="danger"
+        @click="stop"
       />
+      <BaseButton
+        v-else-if="showButtons"
+        :label="t('Start recording')"
+        class="mr-2 mb-2"
+        icon="microphone"
+        type="primary"
+        @click="record"
+      />
+    </div>
+
+    <div v-if="showRecordedAudios">
+      <div v-for="(audio, index) in recorderState.audioList" :key="index" class="py-2">
+        <audio class="max-w-full" controls>
+          <source :src="window.URL.createObjectURL(audio)"/>
+        </audio>
+
+        <BaseButton
+          :label="$t('Attach')"
+          class="my-1"
+          icon="attachment"
+          size="small"
+          type="success"
+          @click="attachAudio(audio)"
+        />
+      </div>
     </div>
   </div>
   <div v-else>
@@ -30,6 +55,7 @@ import BaseButton from "./basecomponents/BaseButton.vue";
 import { computed, reactive } from "vue";
 import { RecordRTCPromisesHandler, StereoAudioRecorder } from "recordrtc";
 import { useI18n } from "vue-i18n";
+import BaseIcon from "./basecomponents/BaseIcon.vue";
 
 const { t } = useI18n();
 
@@ -39,15 +65,37 @@ const props = defineProps({
     required: false,
     default: true,
   },
+  showButtons: {
+    type: Boolean,
+    default: true,
+  },
+  showRecordedAudios: {
+    type: Boolean,
+    default: true,
+  }
 });
 
 const emit = defineEmits(["attach-audio"]);
 
+defineExpose({
+  record,
+  stop,
+});
+
 const recorderState = reactive({
   isRecording: false,
   audioList: [],
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
 });
 
+const recordedTime = computed(() => {
+  let hours = timeComponentToString(recorderState.hours)
+  let minutes = timeComponentToString(recorderState.minutes)
+  let seconds = timeComponentToString(recorderState.seconds)
+  return `${hours} : ${minutes} : ${seconds}`
+})
 
 const isMicrophoneSupported = computed(() => {
   let isMediaDevicesSupported = navigator.mediaDevices && navigator.mediaDevices.getUserMedia
@@ -73,6 +121,7 @@ async function record() {
     numberOfAudioChannels: 2,
   });
   recorder.startRecording();
+  startTimer()
 
   recorderState.isRecording = true;
 }
@@ -93,6 +142,7 @@ async function stop() {
   recorderState.audioList.push(audioBlob);
 
   recorderState.isRecording = false;
+  stopTimer()
 }
 
 function attachAudio(audio) {
@@ -103,5 +153,37 @@ function attachAudio(audio) {
   if (index >= 0) {
     recorderState.audioList.splice(index, 1);
   }
+}
+
+let timer = null;
+function startTimer() {
+  recorderState.seconds = 0
+  recorderState.minutes = 0
+  recorderState.hours = 0
+
+  timer = setInterval(() => {
+    recorderState.seconds = recorderState.seconds + 1
+    if (recorderState.seconds > 59) {
+      recorderState.minutes = recorderState.minutes + 1
+      recorderState.seconds = 0
+    }
+    if (recorderState.minutes > 59) {
+      recorderState.hours = recorderState.hours + 1
+      recorderState.minutes = 0
+    }
+  }, 1000)
+}
+
+function stopTimer() {
+  recorderState.seconds = 0
+  recorderState.minutes = 0
+  recorderState.hours = 0
+
+  clearInterval(timer)
+  timer = null
+}
+
+function timeComponentToString(value) {
+  return value.toString().padStart(2, '0')
 }
 </script>

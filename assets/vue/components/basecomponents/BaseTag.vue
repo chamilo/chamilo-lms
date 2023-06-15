057e7234203bbd298f0aed6cbfc46770bbea0e02
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-06-15 08:18:45 -0500

    Internal: Add BaseTag and BaseUserAvatar components

---
+++ assets/vue/components/basecomponents/BaseTag.vue
@@ -0,0 +1,18 @@
+<template>
+  <Tag :severity="type" :value="label" />
+</template>
+
+<script setup>
+import Tag from 'primevue/tag';
+
+defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>

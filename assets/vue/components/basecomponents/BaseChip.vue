74e7ac64f2658845800d6eea92448eea5845d102
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-06-21 01:46:09 -0500

    UI: Add BaseChip component

---
+++ assets/vue/components/basecomponents/BaseChip.vue
@@ -0,0 +1,35 @@
+<template>
+  <Chip
+    :image="value[imageField]"
+    :label="value[labelField]"
+    :removable="isRemovable"
+    @remove="$emit('remove', value)"
+  />
+</template>
+
+<script setup>
+import Chip from "primevue/chip";
+
+defineProps({
+  value: {
+    type: Object,
+    required: true,
+  },
+  labelField: {
+    type: String,
+    required: true,
+  },
+  imageField: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  isRemovable: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+defineEmits(["remove"]);
+</script>

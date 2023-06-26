a90f599a2c39ca3266bab604e02c35a4142558e5
Daniel Gayoso González <danigayosog@gmail.com>
2023-06-26 10:10:40 +0200

    Minor: migrate social comment form to script setup

--- assets/vue/components/social/SocialWallCommentForm.vue
+++ assets/vue/components/social/SocialWallCommentForm.vue
@@ -16,56 +16,45 @@
   </q-form>
 </template>
 
-<script>
-import {ref} from "vue";
-import {useStore} from "vuex";
-import axios from "axios";
-import {ENTRYPOINT} from "../../config/entrypoint";
-import {SOCIAL_TYPE_WALL_COMMENT} from "./constants";
+<script setup>
+import {ref} from "vue"
+import {useStore} from "vuex"
+import axios from "axios"
+import {ENTRYPOINT} from "../../config/entrypoint"
+import {SOCIAL_TYPE_WALL_COMMENT} from "./constants"
 
-export default {
-  name: "WallCommentForm",
-  props: {
-    post: {
-      type: Object,
-      required: true,
-    }
-  },
-  emits: ["comment-posted"],
-  setup(props, {emit}) {
-    const store = useStore();
+const store = useStore()
 
-    const currentUser = store.getters['security/getUser'];
-
-    const comment = ref('');
-    const isLoading = ref(false);
-
-    function sendComment() {
-      isLoading.value = true;
-
-      axios
-        .post(ENTRYPOINT + 'social_posts', {
-          content: comment.value,
-          type: SOCIAL_TYPE_WALL_COMMENT,
-          sender: currentUser['@id'],
-          parent: props.post['@id'],
-        })
-        .then(response => {
-          emit('comment-posted', response.data);
-
-          comment.value = '';
-        })
-        .finally(() => {
-          isLoading.value = false
-        })
-      ;
-    }
-
-    return {
-      sendComment,
-      comment,
-      isLoading,
-    };
+const props = defineProps({
+  post: {
+    type: Object,
+    required: true,
   }
+})
+
+const emit = defineEmits(['comment-posted'])
+
+const currentUser = store.getters['security/getUser']
+const comment = ref('')
+const isLoading = ref(false)
+
+function sendComment() {
+  isLoading.value = true;
+
+  axios
+    .post(ENTRYPOINT + 'social_posts', {
+      content: comment.value,
+      type: SOCIAL_TYPE_WALL_COMMENT,
+      sender: currentUser['@id'],
+      parent: props.post['@id'],
+    })
+    .then(response => {
+      emit('comment-posted', response.data);
+
+      comment.value = '';
+    })
+    .finally(() => {
+      isLoading.value = false
+    })
 }
 </script>

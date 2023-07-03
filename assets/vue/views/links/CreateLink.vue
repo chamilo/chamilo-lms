2a1342241ef6ce82d55fce94e0825049669b74e8
Daniel Gayoso González <danigayosog@gmail.com>
2023-07-03 09:45:10 +0200

    Minor: migrate create link to script setup

--- assets/vue/views/links/CreateLink.vue
+++ assets/vue/views/links/CreateLink.vue
@@ -3,33 +3,23 @@
     <div class="mb-4">
       <button class="btn btn--secondary" @click="goBack">Back</button>
     </div>
-    <h1 class="text-h3 font-small text-gray-800 mb-4">Add a link<hr /></h1>
+    <h1 class="text-h3 font-small text-gray-800 mb-4">Add a link</h1>
+    <hr/>
     <LinkForm />
   </div>
 </template>
 
-<script>
-import LinkForm from "../../components/links/LinkForm.vue";
-import { useRouter, useRoute } from 'vue-router';
+<script setup>
+import LinkForm from "../../components/links/LinkForm.vue"
+import { useRouter, useRoute } from "vue-router"
 
-export default {
-  components: {
-    LinkForm,
-  },
-  setup() {
-    const router = useRouter();
-    const route = useRoute();
+const router = useRouter()
+const route = useRoute()
 
-    const goBack = () => {
-      router.push({
-        name: "LinksList",
-        query: route.query,
-      });
-    };
-
-    return {
-      goBack,
-    };
-  },
-};
+const goBack = () => {
+  router.push({
+    name: "LinksList",
+    query: route.query,
+  })
+}
 </script>

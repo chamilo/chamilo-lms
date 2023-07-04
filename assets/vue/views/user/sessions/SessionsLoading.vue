c01e7f33cf4120c16d2fad67f0c134f8c8e10cab
Daniel Gayoso González <danigayosog@gmail.com>
2023-07-04 12:54:45 +0200

    Delete no category title for sessions

--- assets/vue/views/user/sessions/SessionsLoading.vue
+++ assets/vue/views/user/sessions/SessionsLoading.vue
@@ -3,8 +3,6 @@
     v-if="isLoading"
     class="t-3 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
   >
-    <Skeleton class="h-8 w-44"/>
-    <div class="hidden md:block md:col-span-1 lg:col-span-3 "></div>
     <Skeleton v-for="el in 4" :key="el" class="h-44 xl:h-60"/>
   </div>
 </template>

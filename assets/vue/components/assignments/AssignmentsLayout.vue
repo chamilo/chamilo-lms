78850283593e0bcc671e7e3c6910f9864fe1bec6
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-07-05 02:29:01 -0500

    Internal: Work: Add link to new UI to add assignment #4769

--- assets/vue/components/assignments/AssignmentsLayout.vue
+++ assets/vue/components/assignments/AssignmentsLayout.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="flex">
+  <div class="flex gap-2">
     <h2 v-t="'Assignments'" class="mr-auto" />
+
+    <StudentViewButton />
+
+    <BaseButton
+      v-if="isCurrentTeacher && menuItems.length > 0"
+      icon="cog"
+      only-icon
+      popup-identifier="course-tmenu"
+      type="black"
+      @click="toggleCourseTMenu"
+    />
+
+    <BaseMenu v-if="isCurrentTeacher && menuItems.length > 0" id="course-tmenu" ref="menu" :model="menuItems" />
   </div>
 
   <hr />
@@ -9,4 +22,44 @@
 </template>
 
 <script setup>
+import StudentViewButton from "../StudentViewButton.vue";
+import BaseButton from "../basecomponents/BaseButton.vue";
+import BaseMenu from "../basecomponents/BaseMenu.vue";
+import { computed, ref, watchEffect } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { useI18n } from "vue-i18n";
+import { useCidReq } from "../../composables/cidReq";
+
+const route = useRoute();
+const store = useStore();
+const { t } = useI18n();
+
+const { cid, sid, gid } = useCidReq();
+
+const isCurrentTeacher = computed(() => store.getters["security/isCurrentTeacher"]);
+
+const menu = ref(null);
+
+const menuItems = ref([]);
+
+const toggleCourseTMenu = (event) => {
+  menu.value.toggle(event);
+};
+
+watchEffect(() => {
+  menuItems.value = [];
+
+  if ("AssigmnentsList" === route.name) {
+    menuItems.value = [
+      {
+        label: t("Create assignment"),
+        to: {
+          name: "AssigmnentsCreate",
+          query: { cid, sid, gid },
+        },
+      },
+    ];
+  }
+});
 </script>

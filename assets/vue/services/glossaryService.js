36059307ece5a57be519314e47ab2ded195a3601
Daniel Gayoso González <danigayosog@gmail.com>
2023-07-06 12:08:34 +0200

    Adapt glossary list to new components
    * Split table and list term visualization in 2 different components
    * Extract api code to glossaryService

---
+++ assets/vue/services/glossaryService.js
@@ -0,0 +1,30 @@
+import { ENTRYPOINT } from "../config/entrypoint"
+import axios from "axios"
+
+export default {
+  /**
+   * @param {Object} params
+   */
+  getGlossaryTerms: async (params) => {
+    const response = await axios.get(ENTRYPOINT + "glossaries", { params })
+    return response.data
+  },
+
+  /**
+   * @param {Object} data
+   */
+  exportToDocuments: async (data) => {
+    const endpoint = `${ENTRYPOINT}glossaries/export_to_documents`
+    const response = await axios.post(endpoint, data)
+    return response.data
+  },
+
+  /**
+   * @param {String|Number} termId
+   */
+  deleteTerm: async (termId) => {
+    const endpoint = `${ENTRYPOINT}glossaries /${termId}`
+    const response = await axios.delete(endpoint)
+    return response.data
+  },
+}
--- assets/vue/views/glossary/GlossaryList.vue
+++ assets/vue/views/glossary/GlossaryList.vue
@@ -1,68 +1,80 @@
 <template>
   <div>
-    <input type="text" v-model="searchTerm" placeholder="Search term..." />
     <ButtonToolbar v-if="isAuthenticated && isCurrentTeacher">
       <BaseButton
-        label="Add new glossary term"
-        icon="new_glossary_term"
+        :label="t('Add new glossary term')"
+        icon="plus"
         class="mb-2 mr-2"
         type="black"
         @click="addNewTerm"
       />
-      <BaseButton label="Import glossary" icon="import" class="mb-2 mr-2" type="black" @click="importGlossary" />
-      <BaseButton label="Export" icon="save" class="mb-2 mr-2" type="black" @click="exportGlossary" />
       <BaseButton
-        :label="view === 'table' ? 'List view' : 'Table view'"
-        icon="view_text"
+        :label="t('Import glossary')"
+        icon="import"
+        class="mb-2 mr-2"
+        type="black"
+        @click="importGlossary"
+      />
+      <BaseButton
+        :label="t('Export')"
+        icon="file-export"
+        class="mb-2 mr-2"
+        type="black"
+        @click="exportGlossary"
+      />
+      <BaseButton
+        :label="view === 'table' ? t('List view') : t('Table view')"
+        :icon="view === 'table' ? 'list' : 'table'"
         class="mb-2 mr-2"
         type="black"
         @click="changeView(view)"
       />
       <BaseButton
-        label="Export to Documents"
-        icon="export_to_documents"
+        :label="t('Export to Documents')"
+        icon="export"
         class="mb-2 mr-2"
         type="black"
         @click="exportToDocuments"
       />
     </ButtonToolbar>
 
-    <div v-if="glossaries.length === 0">
+    <BaseInputText
+      v-model="searchTerm"
+      class="mb-4"
+      :label="t('Search term...')"
+    />
+
+    <div v-if="glossaries.length === 0 && searchTerm === ''">
       <!-- Render the image and create button -->
-      <EmptyState icon="mdi mdi-alphabetical" summary="Add your first term glossary to this course">
-        <BaseButton label="Add Glossary" class="mt-4" icon="plus" type="primary" @click="addNewTerm" />
+      <EmptyState
+        icon="glossary"
+        summary="Add your first term glossary to this course"
+      >
+        <BaseButton
+          :label="t('Add new glossary term')"
+          class="mt-4"
+          icon="plus"
+          type="primary"
+          @click="addNewTerm"
+        />
       </EmptyState>
     </div>
 
-    <div v-if="glossaries">
-      <div v-if="view === 'list'">
-        <ul>
-          <li v-for="glossary in glossaries" :key="glossary.id">
-            <span>{{ glossary.name }} - {{ glossary.description }}</span>
-            <BaseButton label="Edit" class="mr-2" icon="edit" type="black" @click="editTerm(glossary.iid)" />
-            <BaseButton label="Delete" class="mr-2" icon="delete" type="black" @click="deleteTerm(glossary.iid)" />
-          </li>
-        </ul>
-      </div>
-      <table v-else>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="glossary in glossaries" :key="glossary.id">
-            <td>{{ glossary.name }}</td>
-            <td>{{ glossary.description }}</td>
-            <td>
-              <BaseButton label="Edit" class="mr-2" icon="edit" type="black" @click="editTerm(glossary.iid)" />
-              <BaseButton label="Delete" class="mr-2" icon="delete" type="black" @click="deleteTerm(glossary.iid)" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <GlossaryTermList
+        v-if="view === 'list'"
+        :glossaries="glossaries"
+        :search-term="searchTerm"
+        @edit="editTerm($event)"
+        @delete="deleteTerm($event)"
+      />
+      <GlossaryTermTable
+        v-else
+        :glossaries="glossaries"
+        :search-term="searchTerm"
+        @edit="editTerm($event)"
+        @delete="deleteTerm($event)"
+      />
     </div>
   </div>
 </template>
@@ -75,13 +87,18 @@ import { computed, onMounted, ref, watch } from "vue"
 import { useStore } from "vuex"
 import { useRoute, useRouter } from "vue-router"
 import { useI18n } from "vue-i18n"
-import axios from "axios"
-import { ENTRYPOINT } from "../../config/entrypoint"
 import { RESOURCE_LINK_PUBLISHED } from "../../components/resource_links/visibility"
+import BaseInputText from "../../components/basecomponents/BaseInputText.vue"
+import GlossaryTermList from "../../components/glossary/GlossaryTermList.vue"
+import GlossaryTermTable from "../../components/glossary/GlossaryTermTable.vue"
+import { useCidReq } from "../../composables/cidReq"
+import glossaryService from "../../services/glossaryService"
+import { useNotification } from "../../composables/notification"
 
 const store = useStore()
 const route = useRoute()
 const router = useRouter()
+const notifications = useNotification()
 
 const { t } = useI18n()
 
@@ -104,6 +121,14 @@ const isCurrentTeacher = computed(() => store.getters["security/isCurrentTeacher
 const glossaries = ref([])
 const view = ref("list")
 
+watch(searchTerm, () => {
+  fetchGlossaries()
+})
+
+onMounted(() => {
+  fetchGlossaries()
+})
+
 function addNewTerm() {
   router.push({
     name: "CreateTerm",
@@ -111,27 +136,23 @@ function addNewTerm() {
   })
 }
 
-function editTerm(termId) {
-  console.log("termId ", termId)
-
+function editTerm(term) {
   router.push({
     name: "UpdateTerm",
-    params: { id: termId },
+    params: { id: term.iid },
     query: route.query,
   })
 }
 
-function deleteTerm(termId) {
+async function deleteTerm(term) {
   if (confirm("¿Delete?")) {
-    axios
-      .delete(`${ENTRYPOINT}glossaries/${termId}`)
-      .then((response) => {
-        console.log("Term deleted:", response.data)
-        fetchGlossaries()
-      })
-      .catch((error) => {
-        console.error("Error deleting term:", error)
-      })
+    try {
+      await glossaryService.deleteTerm(term.iid)
+      notifications.showSuccessNotification(t("Term deleted"))
+    } catch (error) {
+      console.error("Error deleting term:", error)
+      notifications.showErrorNotification(t("Could not delete term"))
+    }
   }
 }
 
@@ -154,50 +175,36 @@ function changeView(newView) {
   view.value = newView === "table" ? "list" : "table"
 }
 
-function exportToDocuments() {
+async function exportToDocuments() {
   const postData = {
     parentResourceNodeId: parentResourceNodeId.value,
     resourceLinkList: resourceLinkList.value,
   }
 
-  const endpoint = `${ENTRYPOINT}glossaries/export_to_documents`
-
-  axios
-    .post(endpoint, postData)
-    .then((response) => {
-      console.log(response.data)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+  try {
+    await glossaryService.exportToDocuments(postData)
+    notifications.showSuccessNotification(t("Exported to documents"))
+  } catch (error) {
+    console.error("Error fetching links:", error)
+    notifications.showErrorNotification(t("Could not export to documents"))
+  }
 }
 
-function fetchGlossaries() {
+const { cid, sid } = useCidReq()
+
+async function fetchGlossaries() {
   const params = {
     "resourceNode.parent": route.query.parent || null,
-    cid: route.query.cid || null,
-    sid: route.query.sid || null,
+    cid: cid || null,
+    sid: sid || null,
     q: searchTerm.value,
   }
 
-  axios
-    .get(ENTRYPOINT + "glossaries", { params })
-    .then((response) => {
-      console.log("responsedata:", response.data)
-      glossaries.value = response.data
-
-      console.log("en fetch glossaries.value", glossaries.value)
-    })
-    .catch((error) => {
-      console.error("Error fetching links:", error)
-    })
+  try {
+    glossaries.value = await glossaryService.getGlossaryTerms(params)
+  } catch (error) {
+    console.error("Error fetching links:", error)
+    notifications.showErrorNotification(t("Could not fetch glossary terms"))
+  }
 }
-
-watch(searchTerm, () => {
-  fetchGlossaries()
-})
-
-onMounted(() => {
-  fetchGlossaries()
-})
 </script>

074d208d8171eacbf15060dea7c30a7d2640a929
christian <christian.fasanando@beeznest.com>
2023-08-08 18:22:47 -0500

    Migrations: Fix introduction course home with session - refs BT#20846

--- assets/vue/views/ctoolintro/Update.vue
+++ assets/vue/views/ctoolintro/Update.vue
@@ -18,9 +18,15 @@ import ToolIntroForm from '../../components/ctoolintro/Form.vue';
 import Loading from '../../components/Loading.vue';
 import Toolbar from '../../components/Toolbar.vue';
 import UpdateMixin from '../../mixins/UpdateMixin';
+import {computed, onMounted, reactive, ref, toRefs} from "vue";
 import {useI18n} from "vue-i18n";
 import {useRoute, useRouter} from "vue-router";
 import toInteger from "lodash/toInteger";
+import useVuelidate from '@vuelidate/core'
+import axios from 'axios'
+import { ENTRYPOINT } from '../../config/entrypoint'
+import { RESOURCE_LINK_PUBLISHED } from '../../components/resource_links/visibility'
+import useNotification from '../../components/Notification'
 
 const servicePrefix = 'ctoolintro';
 
@@ -35,19 +41,42 @@ export default {
   },
   setup() {
     const route = useRoute();
+    const router = useRouter();
+    const {showNotification} = useNotification();
     const store = useStore();
+    const item = ref({});
     const cid = toInteger(route.query.cid);
     if (cid) {
       let courseIri = '/api/courses/' + cid;
       store.dispatch('course/findCourse', { id: courseIri });
     }
 
-    /*function updated(val) {
+    let toolId = route.query.ctoolId;
+    let ctoolintroId = route.query.ctoolintroIid;
+
+    // Get the current intro text.
+    axios.get(ENTRYPOINT + 'c_tool_intros/'+ctoolintroId).then(response => {
+      let data = response.data;
+      item.value['introText'] = data.introText;
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+    item.value['parentResourceNodeId'] = Number(route.query.parentResourceNodeId);
+    item.value['courseTool'] = '/api/c_tools/'+toolId;
+
+    item.value['resourceLinkList'] = [{
+      sid: route.query.sid,
+      cid: route.query.cid,
+      visibility: RESOURCE_LINK_PUBLISHED, // visible by default
+    }];
+
+    function onUpdated(val) {
       showNotification(t('Updated'));
       router.go(-1);
-    }*/
-    //return {updated};
-    return;
+    }
+
+    return {v$: useVuelidate(), item, onUpdated};
   },
   computed: {
     ...mapFields('ctoolintro', {

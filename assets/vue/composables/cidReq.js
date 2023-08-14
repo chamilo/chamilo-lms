eef0cef92421f73299e2747b60b0594e48687f30
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-08-14 02:38:49 -0500

    Internal: Check if route has query params in cidReq composable

--- assets/vue/composables/cidReq.js
+++ assets/vue/composables/cidReq.js
@@ -4,8 +4,8 @@ export function useCidReq() {
   const route = useRoute()
 
   return {
-    cid: parseInt(route.query?.cid ?? 0),
-    sid: parseInt(route.query?.sid ?? 0),
-    gid: parseInt(route.query?.gid ?? 0),
+    cid: parseInt(route?.query?.cid ?? 0),
+    sid: parseInt(route?.query?.sid ?? 0),
+    gid: parseInt(route?.query?.gid ?? 0),
   }
 }

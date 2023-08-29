4c755d387ba022db10974ebe8e28db5f223af5ed
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-08-28 19:39:22 -0500

    Minor: Format code

--- assets/vue/views/ctoolintro/Create.vue
+++ assets/vue/views/ctoolintro/Create.vue
@@ -1,113 +1,120 @@
 <template>
-  <Toolbar
-      :handle-submit="onSendForm"
-  />
+  <Toolbar :handle-submit="onSendForm" />
 
   <ToolIntroForm
-      ref="createForm"
-      :values="item"
-      :errors="violations"
+    ref="createForm"
+    :errors="violations"
+    :values="item"
   />
   <Loading :visible="isLoading" />
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
-import { createHelpers } from 'vuex-map-fields';
-import ToolIntroForm from '../../components/ctoolintro/Form.vue';
-import Loading from '../../components/Loading.vue';
-import Toolbar from '../../components/Toolbar.vue';
-import CreateMixin from '../../mixins/CreateMixin';
-import {computed, onMounted, reactive, ref, toRefs} from "vue";
-import useVuelidate from "@vuelidate/core";
-import {useRoute, useRouter} from "vue-router";
-import isEmpty from "lodash/isEmpty";
-import {RESOURCE_LINK_PUBLISHED} from "../../components/resource_links/visibility.js";
-import axios from 'axios'
-import { ENTRYPOINT } from '../../config/entrypoint'
-import useNotification from "../../components/Notification";
-import {useI18n} from "vue-i18n";
+import { mapActions, mapGetters } from "vuex"
+import { createHelpers } from "vuex-map-fields"
+import ToolIntroForm from "../../components/ctoolintro/Form.vue"
+import Loading from "../../components/Loading.vue"
+import Toolbar from "../../components/Toolbar.vue"
+import CreateMixin from "../../mixins/CreateMixin"
+import { ref } from "vue"
+import useVuelidate from "@vuelidate/core"
+import { useRoute, useRouter } from "vue-router"
+import isEmpty from "lodash/isEmpty"
+import { RESOURCE_LINK_PUBLISHED } from "../../components/resource_links/visibility.js"
+import axios from "axios"
+import { ENTRYPOINT } from "../../config/entrypoint"
+import useNotification from "../../components/Notification"
+import { useI18n } from "vue-i18n"
 import { useCidReq } from "../../composables/cidReq"
 import { useCidReqStore } from "../../store/cidReq"
-const servicePrefix = 'ctoolintro';
+
+const servicePrefix = "ctoolintro"
 
 const { mapFields } = createHelpers({
-  getterType: 'ctoolintro/getField',
-  mutationType: 'ctoolintro/updateField'
-});
+  getterType: "ctoolintro/getField",
+  mutationType: "ctoolintro/updateField",
+})
 
 export default {
-  name: 'ToolIntroCreate',
+  name: "ToolIntroCreate",
   servicePrefix,
   mixins: [CreateMixin],
   components: {
     Loading,
     Toolbar,
-    ToolIntroForm
+    ToolIntroForm,
   },
   setup() {
-    const users = ref([]);
-    const isLoadingSelect = ref(false);
-    const item = ref({});
-    const route = useRoute();
-    const router = useRouter();
-    const {showNotification} = useNotification();
-    const { t } = useI18n();
+    const users = ref([])
+    const isLoadingSelect = ref(false)
+    const item = ref({})
+    const route = useRoute()
+    const router = useRouter()
+    const { showNotification } = useNotification()
+    const { t } = useI18n()
     const cidReqStore = useCidReqStore()
 
-    let id = route.params.id;
+    let id = route.params.id
     if (isEmpty(id)) {
-      id = route.query.id;
+      id = route.query.id
     }
 
     const { cid } = useCidReq()
 
     cidReqStore.setCourseAndSessionById(cid)
 
-    let toolId = route.params.courseTool;
-    let ctoolId = route.query.ctoolId;
+    let toolId = route.params.courseTool
+    let ctoolId = route.query.ctoolId
 
     // Get the current intro text.
-    axios.get(ENTRYPOINT + 'c_tool_intros/' + toolId).then(response => {
-      let data = response.data;
-      item.value['introText'] = data.introText;
-    }).catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .get(ENTRYPOINT + "c_tool_intros/" + toolId)
+      .then((response) => {
+        let data = response.data
+        item.value["introText"] = data.introText
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
 
-    item.value['parentResourceNodeId'] = Number(route.query.parentResourceNodeId);
-    item.value['courseTool'] = '/api/c_tools/' + ctoolId;
+    item.value["parentResourceNodeId"] = Number(route.query.parentResourceNodeId)
+    item.value["courseTool"] = "/api/c_tools/" + ctoolId
 
-    item.value['resourceLinkList'] = [{
-      sid: route.query.sid,
-      cid: route.query.cid,
-      visibility: RESOURCE_LINK_PUBLISHED, // visible by default
-    }];
+    item.value["resourceLinkList"] = [
+      {
+        sid: route.query.sid,
+        cid: route.query.cid,
+        visibility: RESOURCE_LINK_PUBLISHED, // visible by default
+      },
+    ]
 
     function onCreated(item) {
-      showNotification(t('Updated'));
-      axios.post('/course/'+cid+'/addToolIntro', {
-        iid: item.iid,
-        cid: route.query.cid,
-        sid: route.query.sid
-      }).then(response => {
-        router.go(-1);
-      }).catch(function (error) {
-        console.log(error);
-      });
+      showNotification(t("Updated"))
+      axios
+        .post("/course/" + cid + "/addToolIntro", {
+          iid: item.iid,
+          cid: route.query.cid,
+          sid: route.query.sid,
+        })
+        .then((response) => {
+          router.go(-1)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     }
 
-    return {v$: useVuelidate(), users, isLoadingSelect, item, onCreated};
+    return { v$: useVuelidate(), users, isLoadingSelect, item, onCreated }
   },
   computed: {
-    ...mapFields(['error', 'isLoading', 'created', 'violations']),
+    ...mapFields(["error", "isLoading", "created", "violations"]),
     ...mapGetters({
-      'isAuthenticated': 'security/isAuthenticated',
-      'currentUser': 'security/getUser',
+      isAuthenticated: "security/isAuthenticated",
+      currentUser: "security/getUser",
     }),
   },
   methods: {
-    ...mapActions('ctoolintro', ['create', 'createWithFormData'])
-  }
-};
+    ...mapActions("ctoolintro", ["create", "createWithFormData"]),
+  },
+}
 </script>
--- assets/vue/views/ctoolintro/Update.vue
+++ assets/vue/views/ctoolintro/Update.vue
@@ -1,106 +1,109 @@
 <template>
-  <Toolbar
-      :handle-submit="onSendForm"
-  />
+  <Toolbar :handle-submit="onSendForm" />
   <ToolIntroForm
-      ref="updateForm"
-      v-if="item"
-      :values="item"
-      :errors="violations"
+    v-if="item"
+    ref="updateForm"
+    :errors="violations"
+    :values="item"
   />
-  <Loading :visible="isLoading || deleteLoading"/>
+  <Loading :visible="isLoading || deleteLoading" />
 </template>
 
 <script>
-import {mapActions, mapGetters} from 'vuex'
-import { mapFields } from 'vuex-map-fields';
-import ToolIntroForm from '../../components/ctoolintro/Form.vue';
-import Loading from '../../components/Loading.vue';
-import Toolbar from '../../components/Toolbar.vue';
-import UpdateMixin from '../../mixins/UpdateMixin';
-import {computed, onMounted, reactive, ref, toRefs} from "vue";
-import {useI18n} from "vue-i18n";
-import {useRoute, useRouter} from "vue-router";
-import useVuelidate from '@vuelidate/core'
-import axios from 'axios'
-import { ENTRYPOINT } from '../../config/entrypoint'
-import { RESOURCE_LINK_PUBLISHED } from '../../components/resource_links/visibility'
-import useNotification from '../../components/Notification'
+import { mapActions, mapGetters } from "vuex"
+import { mapFields } from "vuex-map-fields"
+import ToolIntroForm from "../../components/ctoolintro/Form.vue"
+import Loading from "../../components/Loading.vue"
+import Toolbar from "../../components/Toolbar.vue"
+import UpdateMixin from "../../mixins/UpdateMixin"
+import { ref } from "vue"
+import { useI18n } from "vue-i18n"
+import { useRoute, useRouter } from "vue-router"
+import useVuelidate from "@vuelidate/core"
+import axios from "axios"
+import { ENTRYPOINT } from "../../config/entrypoint"
+import { RESOURCE_LINK_PUBLISHED } from "../../components/resource_links/visibility"
+import useNotification from "../../components/Notification"
 import { useCidReqStore } from "../../store/cidReq"
 import { useCidReq } from "../../composables/cidReq"
 
-const servicePrefix = 'ctoolintro';
+const servicePrefix = "ctoolintro"
 
 export default {
-  name: 'ToolIntroUpdate',
+  name: "ToolIntroUpdate",
   servicePrefix,
   mixins: [UpdateMixin],
   components: {
     Loading,
     Toolbar,
-    ToolIntroForm
+    ToolIntroForm,
   },
   setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const {showNotification} = useNotification();
+    const route = useRoute()
+    const router = useRouter()
+    const { showNotification } = useNotification()
     const cidReqStore = useCidReqStore()
-    const item = ref({});
+    const item = ref({})
     const { cid, sid } = useCidReq()
     const { t } = useI18n()
 
     cidReqStore.setCourseAndSessionById(cid)
 
-    let toolId = route.query.ctoolId;
-    let ctoolintroId = route.query.ctoolintroIid;
+    let toolId = route.query.ctoolId
+    let ctoolintroId = route.query.ctoolintroIid
 
     // Get the current intro text.
-    axios.get(ENTRYPOINT + 'c_tool_intros/'+ctoolintroId).then(response => {
-      let data = response.data;
-      item.value['introText'] = data.introText;
-    }).catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .get(ENTRYPOINT + "c_tool_intros/" + ctoolintroId)
+      .then((response) => {
+        let data = response.data
+        item.value["introText"] = data.introText
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
 
-    item.value['parentResourceNodeId'] = Number(route.query.parentResourceNodeId);
-    item.value['courseTool'] = '/api/c_tools/'+toolId;
+    item.value["parentResourceNodeId"] = Number(route.query.parentResourceNodeId)
+    item.value["courseTool"] = "/api/c_tools/" + toolId
 
-    item.value['resourceLinkList'] = [{
-      sid,
-      cid,
-      visibility: RESOURCE_LINK_PUBLISHED, // visible by default
-    }];
+    item.value["resourceLinkList"] = [
+      {
+        sid,
+        cid,
+        visibility: RESOURCE_LINK_PUBLISHED, // visible by default
+      },
+    ]
 
     function onUpdated(val) {
-      showNotification(t('Updated'));
-      router.go(-1);
+      showNotification(t("Updated"))
+      router.go(-1)
     }
 
-    return {v$: useVuelidate(), item, onUpdated};
+    return { v$: useVuelidate(), item, onUpdated }
   },
   computed: {
-    ...mapFields('ctoolintro', {
-      deleteLoading: 'isLoading',
-      isLoading: 'isLoading',
-      error: 'error',
-      updated: 'updated',
-      violations: 'violations'
+    ...mapFields("ctoolintro", {
+      deleteLoading: "isLoading",
+      isLoading: "isLoading",
+      error: "error",
+      updated: "updated",
+      violations: "violations",
     }),
-    ...mapGetters('ctoolintro', ['find']),
+    ...mapGetters("ctoolintro", ["find"]),
     ...mapGetters({
-      'isCurrentTeacher': 'security/isCurrentTeacher',
+      isCurrentTeacher: "security/isCurrentTeacher",
     }),
   },
   methods: {
-    ...mapActions('ctoolintro', {
-      createReset: 'resetCreate',
-      deleteItem: 'del',
-      delReset: 'resetDelete',
-      retrieve: 'load',
-      update: 'update',
-      updateWithFormData: 'updateWithFormData',
-      updateReset: 'resetUpdate'
-    })
-  }
-};
+    ...mapActions("ctoolintro", {
+      createReset: "resetCreate",
+      deleteItem: "del",
+      delReset: "resetDelete",
+      retrieve: "load",
+      update: "update",
+      updateWithFormData: "updateWithFormData",
+      updateReset: "resetUpdate",
+    }),
+  },
+}
 </script>

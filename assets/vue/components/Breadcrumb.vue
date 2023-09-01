9b26d5e5988e9b28aacacd74cfa6e0cabacec079
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-09-01 11:06:16 -0500

    Minor: Format code

--- assets/vue/components/Breadcrumb.vue
+++ assets/vue/components/Breadcrumb.vue
@@ -7,11 +7,11 @@
 </template>
 
 <script setup>
-import {useStore} from "vuex";
-import {computed} from "vue";
-import {useRoute} from "vue-router";
-import {useI18n} from "vue-i18n";
-import Breadcrumb from 'primevue/breadcrumb';
+import { useStore } from "vuex"
+import { computed } from "vue"
+import { useRoute } from "vue-router"
+import { useI18n } from "vue-i18n"
+import Breadcrumb from "primevue/breadcrumb"
 import { useCidReqStore } from "../store/cidReq"
 import { storeToRefs } from "pinia"
 
@@ -24,139 +24,139 @@ const props = defineProps({
   legacy: {
     type: Array,
     default: () => [],
-  }
-});
+  },
+})
 
-const store = useStore();
+const store = useStore()
 const cidReqStore = useCidReqStore()
-const route = useRoute();
-const {t} = useI18n();
+const route = useRoute()
+const { t } = useI18n()
 
 const { course, session } = storeToRefs(cidReqStore)
 
-const resourceNode = computed(() => store.getters['resourcenode/getResourceNode']);
+const resourceNode = computed(() => store.getters["resourcenode/getResourceNode"])
 
 const home = {
-  icon: 'pi pi-home',
-  to: '/'
-};
+  icon: "pi pi-home",
+  to: "/",
+}
 
 const foo = computed(() => {
   const list = [
-    'CourseHome',
-    'MyCourses',
-    'MySessions',
-    'MySessionsUpcoming',
-    'MySessionsPast',
-    'Home',
-    'MessageList',
-    'MessageNew',
-    'MessageShow',
-    'MessageCreate',
-  ];
+    "CourseHome",
+    "MyCourses",
+    "MySessions",
+    "MySessionsUpcoming",
+    "MySessionsPast",
+    "Home",
+    "MessageList",
+    "MessageNew",
+    "MessageShow",
+    "MessageCreate",
+  ]
 
-  const items = [];
+  const items = []
 
-  if (route.name && route.name.includes('Page')) {
+  if (route.name && route.name.includes("Page")) {
     items.push({
-      label: t('Pages'),
-      to: '/resources/pages',
-    });
+      label: t("Pages"),
+      to: "/resources/pages",
+    })
   }
 
-  if (route.name && route.name.includes('Message')) {
+  if (route.name && route.name.includes("Message")) {
     items.push({
-      label: t('Messages'),
+      label: t("Messages"),
       //disabled: route.path === path || lastItem.path === route.path,
-      to: '/resources/messages',
-    });
+      to: "/resources/messages",
+    })
   }
 
   if (list.includes(route.name)) {
-    return items;
+    return items
   }
 
   if (0 < props.legacy.length) {
-    const mainUrl = window.location.href;
-    const mainPath = mainUrl.indexOf("main/");
+    const mainUrl = window.location.href
+    const mainPath = mainUrl.indexOf("main/")
 
-    props.legacy.forEach(item => {
-      let url = item.url.toString();
-      let newUrl = url;
+    props.legacy.forEach((item) => {
+      let url = item.url.toString()
+      let newUrl = url
 
-      if (url.indexOf('main/') > 0) {
-        newUrl = '/' + url.substring(mainPath, url.length);
+      if (url.indexOf("main/") > 0) {
+        newUrl = "/" + url.substring(mainPath, url.length)
       }
 
-      if (newUrl === '/') {
-        newUrl = '#';
+      if (newUrl === "/") {
+        newUrl = "#"
       }
 
       items.push({
-        label: item['name'],
-        href: newUrl
-      });
-    });
+        label: item["name"],
+        href: newUrl,
+      })
+    })
   }
 
-  let queryParams = '';
+  let queryParams = ""
 
   Object.keys(route.query)
-    .filter(key => !!key)
-    .forEach(key => {
-      if ('' !== queryParams) {
-        queryParams += "&";
+    .filter((key) => !!key)
+    .forEach((key) => {
+      if ("" !== queryParams) {
+        queryParams += "&"
       }
 
-      queryParams += key + '=' + encodeURIComponent(route.query[key].toString());
-    });
+      queryParams += key + "=" + encodeURIComponent(route.query[key].toString())
+    })
 
   if (course.value) {
-    let sessionTitle = '';
+    let sessionTitle = ""
 
     if (session.value) {
-      sessionTitle = ' (' + session.value.name + ') ';
+      sessionTitle = " (" + session.value.name + ") "
     }
 
     items.push({
       label: course.value.title + sessionTitle,
-      to: '/course/' + course.value.id + '/home?' + queryParams
-    });
+      to: "/course/" + course.value.id + "/home?" + queryParams,
+    })
   }
 
-  const {path, matched} = route;
-  const lastItem = matched[matched.length - 1];
+  const { path, matched } = route
+  const lastItem = matched[matched.length - 1]
 
   if (resourceNode.value) {
-    resourceNode.value.path.split('/').forEach((pathItem, i, pathItems) => {
-      let itemParts = pathItem.split('-');
+    resourceNode.value.path.split("/").forEach((pathItem, i, pathItems) => {
+      let itemParts = pathItem.split("-")
 
       if (0 === i) {
-        let firstParts = pathItems[i + 1].split('-');
+        let firstParts = pathItems[i + 1].split("-")
 
         items.push({
           label: matched[0].name,
-          to: '/resources/document/' + firstParts[1] + '/?' + queryParams
-        });
+          to: "/resources/document/" + firstParts[1] + "/?" + queryParams,
+        })
       } else if (itemParts[0]) {
         items.push({
           label: itemParts[0],
-          to: '/resources/document/' + itemParts[1] + '/?' + queryParams
-        });
+          to: "/resources/document/" + itemParts[1] + "/?" + queryParams,
+        })
       }
-    });
+    })
   }
 
-  matched.forEach(pathItem => {
+  matched.forEach((pathItem) => {
     if (pathItem.path) {
       items.push({
         label: pathItem.name,
         disabled: route.path === path || lastItem.path === route.path,
         href: pathItem.path,
-      });
+      })
     }
-  });
+  })
 
-  return items;
-});
+  return items
+})
 </script>
--- assets/vue/router/index.js
+++ assets/vue/router/index.js
@@ -1,161 +1,164 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import adminRoutes from './admin';
-import courseRoutes from './course';
-import accountRoutes from './account';
-import personalFileRoutes from './personalfile';
-import messageRoutes from './message';
-import userRoutes from './user';
-import userGroupRoutes from './usergroup';
-import userRelUserRoutes from './userreluser';
-import calendarEventRoutes from './ccalendarevent';
-import toolIntroRoutes from './ctoolintro';
-import pageRoutes from './page';
-import socialNetworkRoutes from './social';
+import { createRouter, createWebHistory } from "vue-router"
+import adminRoutes from "./admin"
+import courseRoutes from "./course"
+import accountRoutes from "./account"
+import personalFileRoutes from "./personalfile"
+import messageRoutes from "./message"
+import userRoutes from "./user"
+import userGroupRoutes from "./usergroup"
+import userRelUserRoutes from "./userreluser"
+import calendarEventRoutes from "./ccalendarevent"
+import toolIntroRoutes from "./ctoolintro"
+import pageRoutes from "./page"
+import socialNetworkRoutes from "./social"
 
 //import courseCategoryRoutes from './coursecategory';
-import documents from './documents';
-import assignments from './assignments';
-import links from './links';
-import glossary from './glossary';
-import store from '../store';
-import MyCourseList from '../views/user/courses/List.vue';
-import MySessionList from '../views/user/sessions/SessionsCurrent.vue';
-import MySessionListPast from '../views/user/sessions/SessionsPast.vue';
-import MySessionListUpcoming from '../views/user/sessions/SessionsUpcoming.vue';
+import documents from "./documents"
+import assignments from "./assignments"
+import links from "./links"
+import glossary from "./glossary"
+import store from "../store"
+import MyCourseList from "../views/user/courses/List.vue"
+import MySessionList from "../views/user/sessions/SessionsCurrent.vue"
+import MySessionListPast from "../views/user/sessions/SessionsPast.vue"
+import MySessionListUpcoming from "../views/user/sessions/SessionsUpcoming.vue"
 
-import CatalogLayout from '../layouts/Catalog.vue';
-import MyCoursesLayout from '../layouts/MyCourses.vue';
+import CatalogLayout from "../layouts/Catalog.vue"
+import MyCoursesLayout from "../layouts/MyCourses.vue"
 
-import CourseCatalog from '../views/course/Catalog.vue';
-import SessionCatalog from '../views/course/CatalogSession.vue';
-import CourseHome from '../views/course/CourseHome.vue';
+import CourseCatalog from "../views/course/Catalog.vue"
+import SessionCatalog from "../views/course/CatalogSession.vue"
+import CourseHome from "../views/course/CourseHome.vue"
 
-import Index from '../pages/Index.vue';
-import Home from '../pages/Home.vue';
-import Login from '../pages/Login.vue';
+import Index from "../pages/Index.vue"
+import Home from "../pages/Home.vue"
+import Login from "../pages/Login.vue"
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Index",
+      component: Index,
+      meta: {
+        requiresAuth: false,
+        showBreadcrumb: false,
+      },
+    },
+    {
+      path: "/home",
+      name: "Home",
+      component: Home,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      meta: {
+        layout: "Empty",
+        showBreadcrumb: false,
+      },
+    },
+    {
+      path: "/course/:id/home",
+      name: "CourseHome",
+      component: CourseHome,
+    },
+    {
+      path: "/courses",
+      component: MyCoursesLayout,
+      children: [
         {
-            path: '/',
-            name: 'Index',
-            component: Index,
-            meta: {
-                requiresAuth: false,
-                showBreadcrumb: false,
-            }
+          path: "",
+          name: "MyCourses",
+          component: MyCourseList,
+          meta: { requiresAuth: true },
+        },
+      ],
+    },
+    {
+      path: "/sessions",
+      //redirect: '/sessions/now',
+      component: MySessionList,
+      children: [
+        {
+          path: "/sessions",
+          name: "MySessions",
+          component: MySessionList,
+          meta: { requiresAuth: true },
+        },
+      ],
+    },
+    {
+      path: "/sessions/past",
+      name: "MySessionsPast",
+      component: MySessionListPast,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/sessions/upcoming",
+      name: "MySessionsUpcoming",
+      component: MySessionListUpcoming,
+      meta: { requiresAuth: true },
+    },
+    socialNetworkRoutes,
+    {
+      path: "/catalog",
+      redirect: "/catalog/course",
+      name: "Catalog",
+      component: CatalogLayout,
+      children: [
+        {
+          path: "course",
+          component: CourseCatalog,
         },
         {
-            path: '/home',
-            name: 'Home',
-            component: Home,
-            meta: {
-                requiresAuth: true
-            }
+          path: "session",
+          component: SessionCatalog,
         },
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login,
-            meta: {
-                layout: 'Empty',
-                showBreadcrumb: false
-            }
-        },
-        {
-            path: '/course/:id/home',
-            name: 'CourseHome',
-            component: CourseHome
-        },
-        {
-            path: '/courses',
-            component: MyCoursesLayout,
-            children: [
-                {
-                    path: '',
-                    name: 'MyCourses',
-                    component: MyCourseList,
-                    meta: {requiresAuth: true},
-                },
-            ]
-        },
-        {
-            path: '/sessions',
-            //redirect: '/sessions/now',
-            component: MySessionList,
-            children: [
-                {
-                    path: '/sessions', name: 'MySessions',
-                    component: MySessionList,
-                    meta: {requiresAuth: true},
-                },
-            ],
-        },
-        {
-            path: '/sessions/past', name: 'MySessionsPast',
-            component: MySessionListPast,
-            meta: {requiresAuth: true},
-        },
-        {
-            path: '/sessions/upcoming', name: 'MySessionsUpcoming',
-            component: MySessionListUpcoming,
-            meta: {requiresAuth: true},
-        },
-        socialNetworkRoutes,
-        {
-            path: '/catalog',
-            redirect: '/catalog/course',
-            name: 'Catalog',
-            component: CatalogLayout,
-            children: [
-                {
-                    path: 'course',
-                    component: CourseCatalog
-                },
-                {
-                    path: 'session',
-                    component: SessionCatalog
-                },
-            ],
-            meta: {requiresAuth: true},
-        },
-        adminRoutes,
-        courseRoutes,
-        //courseCategoryRoutes,
-        documents,
-        assignments,
-        links,
-        glossary,
-        accountRoutes,
-        personalFileRoutes,
-        messageRoutes,
-        userRoutes,
-        userGroupRoutes,
-        userRelUserRoutes,
-        calendarEventRoutes,
-        toolIntroRoutes,
-        pageRoutes
-    ]
-});
+      ],
+      meta: { requiresAuth: true },
+    },
+    adminRoutes,
+    courseRoutes,
+    //courseCategoryRoutes,
+    documents,
+    assignments,
+    links,
+    glossary,
+    accountRoutes,
+    personalFileRoutes,
+    messageRoutes,
+    userRoutes,
+    userGroupRoutes,
+    userRelUserRoutes,
+    calendarEventRoutes,
+    toolIntroRoutes,
+    pageRoutes,
+  ],
+})
 
 router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        //console.log('requiresAuth');
-        // this route requires auth, check if logged in
-        // if not, redirect to login page.
-        if (store.getters['security/isAuthenticated']) {
-            next();
-        } else {
-            next({
-                path: '/login',
-                query: {redirect: to.fullPath},
-            });
-        }
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    //console.log('requiresAuth');
+    // this route requires auth, check if logged in
+    // if it is not, redirect to login page.
+    if (store.getters["security/isAuthenticated"]) {
+      next()
     } else {
-        //console.log('next');
-        next(); // make sure to always call next()!
+      next({
+        path: "/login",
+        query: { redirect: to.fullPath },
+      })
     }
-});
+  } else {
+    //console.log('next');
+    next() // make sure to always call next()!
+  }
+})
 
-export default router;
+export default router

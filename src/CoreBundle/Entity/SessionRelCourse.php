9e46f8e9470767a25dcc949acdf6c8f5e8549905
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-09-26 19:03:28 -0500

    Minor: Format code - refs BT#21055

--- src/CoreBundle/Entity/SessionRelCourse.php
+++ src/CoreBundle/Entity/SessionRelCourse.php
@@ -18,9 +18,22 @@ use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * Course subscriptions to a session.
  */
-#[ApiResource(operations: [new Get(security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'VIEW\', object)'), new Put(security: 'is_granted(\'ROLE_ADMIN\')'), new GetCollection(security: 'is_granted(\'ROLE_USER\')'), new Post(security: 'is_granted(\'ROLE_ADMIN\')')], denormalizationContext: ['groups' => ['session_rel_course:write']], normalizationContext: ['groups' => ['session_rel_course:read']])]
+#[ApiResource(
+    operations: [
+        new Get(security: "is_granted('ROLE_ADMIN') or is_granted('VIEW', object)"),
+        new Put(security: "is_granted('ROLE_ADMIN')"),
+        new GetCollection(security: "is_granted('ROLE_USER')"),
+        new Post(security: "is_granted('ROLE_ADMIN')"),
+    ],
+    normalizationContext: [
+        'groups' => ['session_rel_course:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['session_rel_course:write'],
+    ]
+)]
 #[ORM\Table(name: 'session_rel_course')]
-#[ORM\Index(name: 'idx_session_rel_course_course_id', columns: ['c_id'])]
+#[ORM\Index(columns: ['c_id'], name: 'idx_session_rel_course_course_id')]
 #[ORM\UniqueConstraint(name: 'course_session_unique', columns: ['session_id', 'c_id'])]
 #[ORM\Entity]
 #[UniqueEntity(fields: ['course', 'session'], message: 'The course is already registered in this session.')]
@@ -30,66 +43,78 @@ class SessionRelCourse
     #[ORM\Id]
     #[ORM\GeneratedValue]
     protected ?int $id = null;
+
     #[Groups(['session_rel_course:read', 'session_rel_course:write'])]
-    #[ORM\ManyToOne(targetEntity: \Chamilo\CoreBundle\Entity\Session::class, inversedBy: 'courses', cascade: ['persist'])]
+    #[ORM\ManyToOne(targetEntity: Session::class, cascade: ['persist'], inversedBy: 'courses')]
     #[ORM\JoinColumn(name: 'session_id', referencedColumnName: 'id', nullable: false)]
     protected ?Session $session = null;
+
     #[Groups(['session_rel_course:read', 'session_rel_course:write', 'session:read'])]
-    #[ORM\ManyToOne(targetEntity: \Chamilo\CoreBundle\Entity\Course::class, inversedBy: 'sessions', cascade: ['persist'])]
+    #[ORM\ManyToOne(targetEntity: Course::class, cascade: ['persist'], inversedBy: 'sessions')]
     #[ORM\JoinColumn(name: 'c_id', referencedColumnName: 'id', nullable: false)]
     protected ?Course $course = null;
+
     #[ORM\Column(name: 'position', type: 'integer', nullable: false)]
     protected int $position;
+
     #[ORM\Column(name: 'nbr_users', type: 'integer')]
     protected int $nbrUsers;
+
     public function __construct()
     {
         $this->nbrUsers = 0;
         $this->position = 0;
     }
+
     /**
      * Get id.
-     *
-     * @return int
      */
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
-    public function setSession(Session $session): self
-    {
-        $this->session = $session;
 
-        return $this;
-    }
     public function getCourse(): Course
     {
         return $this->course;
     }
+
     public function setCourse(Course $course): self
     {
         $this->course = $course;
 
         return $this;
     }
+
     public function getSession(): Session
     {
         return $this->session;
     }
+
+    public function setSession(Session $session): self
+    {
+        $this->session = $session;
+
+        return $this;
+    }
+
+    public function getNbrUsers(): int
+    {
+        return $this->nbrUsers;
+    }
+
     public function setNbrUsers(int $nbrUsers): self
     {
         $this->nbrUsers = $nbrUsers;
 
         return $this;
     }
-    public function getNbrUsers(): int
-    {
-        return $this->nbrUsers;
-    }
+
     public function getPosition(): int
     {
         return $this->position;
     }
+
     public function setPosition(int $position): void
     {
         $this->position = $position;

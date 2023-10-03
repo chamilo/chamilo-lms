244d330409818128f84786da5b48dbf0d7d9ae62
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-10-03 16:03:50 -0500

    Plugin: ExerciseMonitoring: Add instructions to take snapshots - refs BT#20901

--- plugin/exercisemonitoring/lang/english.php
+++ plugin/exercisemonitoring/lang/english.php
@@ -9,5 +9,8 @@ $strings['tool_enable'] = "Enable tool";
 
 $strings['ExerciseMonitored'] = "Exercise monitored";
 $strings['Retry'] = "Retry";
-$strings['IdDocumentSnapshot'] = "Snapshot of the ID document";
+$strings['IdDocumentSnapshot'] = "Snapshot of the ID or identification document";
 $strings['LearnerSnapshot'] = "Snapshot of the student";
+
+$strings['ImageIdDocumentCameraInstructions'] = "Show / Place / Situate / Put / Accommodate / Approximate your ID or identification document in front of the camera. Once you see the photo inside the screen box, press the Snapshot button or space bar on your keyboard";
+$strings['ImageLearnerCameraInstructions'] = 'Show / Place / Situate / Put / Accommodate / Approximate your face in front of the camera. Once you see the photo inside the screen box, press the Snapshot button or space bar on your keyboard';

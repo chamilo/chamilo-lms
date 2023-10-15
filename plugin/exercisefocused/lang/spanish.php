7309a4b7639aac54674f2e0696768e53fe833729
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-10-14 21:31:31 -0500

    Plugin: ExerciseFocused: Add spanish language - refs BT#20901

---
+++ plugin/exercisefocused/lang/spanish.php
@@ -0,0 +1,35 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = "Enfoque en el Ejercicio";
+$strings['plugin_comment'] = "Mostrar un mensaje para regresar al ejercicio cuando el usuario sale de la ventana/pestaña de Chamilo.";
+
+$strings['tool_enable'] = "Habilitar herramienta";
+$strings['enable_time_limit'] = 'Habilitar límite de tiempo';
+$strings['time_limit'] = "Límite de tiempo";
+$strings['time_limit_help'] = "Límite el tiempo (en segundos) para regresar al ejercicio. Pasado este tiempo, el ejercicio se cerrará.";
+$strings['enable_outfocused_limit'] = "Habilitar el máximo de desenfoque";
+$strings['outfocused_limit'] = "Número máximo de desenfoques permitidos";
+$strings['outfocused_limit_help'] = "Número de desenfoques permitidos. Después de este límite, el ejercicio se cerrará.";
+$strings['session_field_filters'] = "Campo de sesión como filtro";
+$strings['session_field_filters_help'] = "Nombres de campos adicionales separados por comas.";
+$strings['percentage_sampling'] = "Porcentaje de intentos de muestreo";
+$strings['percentage_sampling_help'] = "Se seleccionará un porcentaje de intentos para una revisión aleatoria";
+
+$strings['ReportByAttempts'] = "Enfoque en el Ejercicio: Informe por intentos";
+$strings['YouHaveLeftTheExercise'] = "¡Cuidado! Detectamos que has abandonado la pantalla.<br>Por favor, mantente dentro del examen";
+$strings['YouHaveXTimeToReturn'] = "Tienes <span class=\"h3 text-danger\" id=\"time-limit-target\">%s</span> segundos para regresar";
+$strings['YouAreAllowedXOutfocused'] = "Se te permite <span class=\"h3 text-danger\" id=\"outfocused-limit-target\">%d</span> desenfoques";
+$strings['OutfocusedLimitExceeded'] = "Has excedido el límite permitido de desenfoques";
+$strings['SelectExercise'] = "Seleccionar ejercicio";
+$strings['UnselectExercise'] = "Deseleccionar ejercicio";
+$strings['Returns'] = "Regresos";
+$strings['MaxOutfocusedReached'] = "Se ha alcanzado el máximo de desenfoques";
+$strings['TimeLimitReached'] = "Se ha alcanzado el límite de tiempo";
+$strings['Outfocused'] = "Desenfoques";
+$strings['Return'] = "Regresar";
+$strings['Motive'] = "Motivo";
+$strings['AlertBeforeLeaving'] = "Por favor, mantente dentro del examen.";
+$strings['RandomSampling'] = "Muestreo Aleatorio";
+$strings['WindowTitleOutfocused'] = '¡Mantente dentro del examen!';

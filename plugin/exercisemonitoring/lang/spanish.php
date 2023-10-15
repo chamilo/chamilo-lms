c901a9b15f2706b1980bed336fae3df148dd3f1c
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-10-14 21:31:46 -0500

    Plugin: ExerciseMonitoring: Add spanish language - refs BT#20900

---
+++ plugin/exercisemonitoring/lang/spanish.php
@@ -0,0 +1,16 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+$strings['plugin_title'] = "Exercise monitoring";
+$strings['plugin_comment'] = "Random photo taking mechanism during an exercise";
+
+$strings['tool_enable'] = "Enable tool";
+
+$strings['ExerciseMonitored'] = "Ejercicio monitoreado";
+$strings['Retry'] = "Reintentar";
+$strings['IdDocumentSnapshot'] = "Instantánea del DNI o documento identificativo";
+$strings['LearnerSnapshot'] = "Foto de la estudiante";
+
+$strings['ImageIdDocumentCameraInstructions'] = "Ubica tu DNI o documento de identidad frente / delante a la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón OK o barra de espacio de tu teclado";
+$strings['ImageLearnerCameraInstructions'] = 'Ubica tu rostro delante de la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón OK o barra de espacio de tu teclado';

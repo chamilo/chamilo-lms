1e88b1088a7f195c62000e036ed10541d983beed
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2023-10-17 20:14:34 -0500

    Plugin: ExerciseMonitoring: Fix lang var - refs BT#20901

--- plugin/exercisemonitoring/lang/spanish.php
+++ plugin/exercisemonitoring/lang/spanish.php
@@ -9,8 +9,8 @@ $strings['tool_enable'] = "Enable tool";
 
 $strings['ExerciseMonitored'] = "Ejercicio monitoreado";
 $strings['Retry'] = "Reintentar";
-$strings['IdDocumentSnapshot'] = "Instantánea del DNI o documento identificativo";
-$strings['LearnerSnapshot'] = "Foto de la estudiante";
+$strings['IdDocumentSnapshot'] = "Instantánea del DNI o documento de identidad";
+$strings['LearnerSnapshot'] = "Foto del estudiante";
 
-$strings['ImageIdDocumentCameraInstructions'] = "Ubica tu DNI o documento de identidad frente / delante a la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón OK o barra de espacio de tu teclado";
-$strings['ImageLearnerCameraInstructions'] = 'Ubica tu rostro delante de la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón OK o barra de espacio de tu teclado';
+$strings['ImageIdDocumentCameraInstructions'] = "Ubica tu DNI o documento de identidad frente / delante a la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón <i>Capturar</i> o barra de espacio de tu teclado";
+$strings['ImageLearnerCameraInstructions'] = 'Ubica tu rostro delante de la cámara. Una vez que veas la foto dentro del cuadro de la pantalla, presiona el botón <i>Capturar</i> o barra de espacio de tu teclado';

48bb9a20f36d1e1d81f7d006f5e2803656a2536a
Yannick Warnier <ywarnier@beeznest.org>
2023-10-26 17:21:02 +0200

    Plugin: vChamilo: Update 'archive' (pre-1.10 term) to 'cache' - refs BT#21163

--- plugin/vchamilo/lang/english.php
+++ plugin/vchamilo/lang/english.php
@@ -115,7 +115,7 @@ $strings['CoursePath'] = 'Path to courses directory';
 $strings['HomePath'] = 'Path to home directory';
 $strings['UploadPath'] = 'Path to upload directory';
 
-$strings['ArchiveUrl'] = 'Archive URL';
+$strings['ArchiveUrl'] = 'Cache URL';
 $strings['HomeUrl'] = 'Home URL';
 $strings['UploadUrl'] = 'Upload URL';
 $strings['CourseUrl'] = 'Course URL';

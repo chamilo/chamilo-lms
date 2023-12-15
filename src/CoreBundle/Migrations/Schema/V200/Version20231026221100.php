fd504ab57eb0d34d8b4b0b7b57e8bbdc1a13233e
NicoDucou <nicolas@ldd.fr>
2023-12-15 12:06:47 +0100

    Migration: Social: fix migration to have the minimum function to be able to be instanciated -refs BT#21270

--- src/CoreBundle/Migrations/Schema/V200/Version20231026221100.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20231026221100.php
@@ -11,4 +11,12 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20231026221100 extends AbstractMigrationChamilo
 {
+    public function getDescription(): string
+    {
+        return 'Migration of Add table social_post_attachments moved to Version20230720222140';
+    }
+
+    public function up(Schema $schema): void
+    {
+    }
 }

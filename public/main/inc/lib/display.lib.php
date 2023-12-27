6841a43937608204b686aa4ab8187649e83a31dc
Yannick Warnier <ywarnier@beeznest.org>
2023-12-27 17:08:05 +0100

    Display: Add support for ObjectIcon and StateIcon first param in Display::getMdiIcon() (2)

--- public/main/inc/lib/display.lib.php
+++ public/main/inc/lib/display.lib.php
@@ -2234,7 +2234,7 @@ class Display
 
         $additionalAttributes['class'] = 'mdi mdi-';
 
-        if ($name instanceof ActionIcon or $name instanceof ToolIcon) {
+        if ($name instanceof ActionIcon or $name instanceof ToolIcon or $name instanceof ObjectIcon or $name instanceof StateIcon) {
             $additionalAttributes['class'] .= $name->value;
         } else {
             $additionalAttributes['class'] .= $name;

3d52f288cc2d5ffb1fd7938d7eda78ccf6ab83cf
Yannick Warnier <ywarnier@beeznest.org>
2024-01-14 01:43:52 +0100

    Calendar: Add DB structure to (later) support colors in system calendar events - refs #4206

---
+++ src/CoreBundle/Migrations/Schema/V200/Version20240112193100.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Migrations\Schema\V200;
+
+use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
+use Exception;
+
+final class Version20240112193100 extends AbstractMigrationChamilo
+{
+    public function getDescription(): string
+    {
+        return 'Migrate tool display_order as resource order';
+    }
+
+    public function up(Schema $schema): void
+    {
+        error_log('Changes fields type bigint by integer');
+
+        if ($schema->hasTable('sys_calendar')) {
+            error_log('Perform the changes in the sys_calendar table');
+            $this->addSql('ALTER TABLE sys_calendar ADD COLUMN color varchar(20) NULL;');
+        }
+    }
+    public function down(Schema $schema): void
+    {
+        error_log('Changes fields type bigint by integer');
+
+        if ($schema->hasTable('sys_calendar')) {
+            error_log('Perform the changes in the sys_calendar table');
+            $this->addSql('ALTER TABLE sys_calendar DROP COLUMN color;');
+        }
+    }
+}

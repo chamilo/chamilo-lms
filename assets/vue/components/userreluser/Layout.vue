11b8a066dff1363c38a09c390543533b3de0dbca
christianbeeznest <84335353+christianbeeznest@users.noreply.github.com>
2024-01-15 00:34:18 -0500

    Social: Fix pages in social interface (partial fix) - refs BT#21101
    Co-authored-by: Yannick Warnier <ywarnier@users.noreply.github.com>

--- assets/vue/components/userreluser/Layout.vue
+++ assets/vue/components/userreluser/Layout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col md:flex-row gap-4">
     <div class="md:basis-1/3 lg:basis-1/4 2xl:basis-1/6 flex flex-col">
+      <UserProfileCard />
       <SocialSideMenu />
     </div>
     <div class="md:basis-2/3 lg:basis-3/4 2xl:basis-5/6">
@@ -13,9 +14,10 @@
 
 <script>
   import SocialSideMenu from "../social/SocialSideMenu.vue";
+  import UserProfileCard from "./UserProfileCard.vue";
 
   export default {
       name: 'UserRelUserLayout',
-      components: {SocialSideMenu}
+      components: {UserProfileCard, SocialSideMenu}
   }
 </script>
--- assets/vue/views/social/SocialLayout.vue
+++ assets/vue/views/social/SocialLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col md:flex-row gap-4">
     <div class="md:basis-1/3 lg:basis-1/4 2xl:basis-1/6 flex flex-col">
+      <UserProfileCard />
       <BaseCard plain>
         <img
           :src="user.illustrationUrl"
@@ -13,8 +14,13 @@
       </BaseCard>
       <SocialSideMenu />
     </div>
-    <div class="md:basis-2/3 lg:basis-3/4 2xl:basis-5/6">
+
+    <div class="md:basis-2/3 lg:basis-1/2 2xl:basis-4/6">
       <SocialNetworkWall />
+    </div>
+
+    <div class="md:basis-1/3 lg:basis-1/4 2xl:basis-1/6">
+      <AddFriendSidebar />
     </div>
   </div>
 </template>
@@ -24,8 +30,9 @@ import { useStore } from "vuex"
 import { onMounted, provide, readonly, ref, watch } from "vue"
 import SocialNetworkWall from "./SocialWall.vue"
 import { useRoute } from "vue-router"
-import BaseCard from "../../components/basecomponents/BaseCard.vue"
 import SocialSideMenu from "../../components/social/SocialSideMenu.vue"
+import AddFriendSidebar from "../../components/userreluser/AddFriendSidebar.vue";
+import UserProfileCard from "../../components/userreluser/UserProfileCard.vue";
 
 const store = useStore()
 const route = useRoute()

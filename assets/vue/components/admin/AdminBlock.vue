0be3fe7a75b5b01f1e23b364888422c67149d868
NicoDucou <nicolas@ldd.fr>
2024-01-15 12:56:47 +0100

    Display: Fix unclosed div and double item list

--- assets/vue/components/admin/AdminBlock.vue
+++ assets/vue/components/admin/AdminBlock.vue
@@ -10,7 +10,7 @@
 
       <div class="space-y-4">
         <p v-if="props.description" class="text-body-4" v-text="props.description" />
-
+      
         <form v-if="props.searchUrl" :action="props.searchUrl" method="get">
           <BaseInputGroup
             :button-label="t('Search')"
@@ -21,24 +21,6 @@
         </form>
       </div>
 
-    <div class="p-menu p-component p-ripple-disabled">
-      <ul class="p-menu-list p-reset" role="menu">
-        <li
-          v-for="(item, index) in visibleItems"
-          :key="index"
-          :aria-label="t(item.label)"
-          :class="item.className"
-          class="p-menuitem"
-          role="menuitem"
-        >
-          <div class="p-menuitem-content">
-            <a :href="item.url" class="p-menuitem-link">
-              <span class="p-menuitem-text" v-text="item.label" />
-            </a>
-          </div>
-        </li>
-        <slot></slot>
-      </ul>
       <div class="p-menu p-component p-ripple-disabled">
         <ul class="p-menu-list p-reset" role="menu">
           <li
@@ -55,6 +37,7 @@
               </a>
             </div>
           </li>
+          <slot></slot>
         </ul>
       </div>
     </div>

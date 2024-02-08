d97fe52473cabdeb7b2eb2886ec133f51a69f1ee
Yannick Warnier <ywarnier@beeznest.org>
2024-02-08 09:52:50 +0100

    Internal: Use SonataExporterBundle to replace deprecated SonataExporterSymfonyBundle

--- config/bundles.php
+++ config/bundles.php
@@ -36,6 +36,6 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
-    Sonata\Exporter\Bridge\Symfony\SonataExporterSymfonyBundle::class => ['all' => true],
+    Sonata\Exporter\Bridge\Symfony\SonataExporterBundle::class => ['all' => true],
     Oh\GoogleMapFormTypeBundle\OhGoogleMapFormTypeBundle::class => ['all' => true],
 ];

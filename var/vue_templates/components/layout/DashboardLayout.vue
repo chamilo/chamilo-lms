6d9548a1170770fbb2ac1a9a11f0563579b108bf
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-02-10 15:09:18 -0500

    Display: Add breadcrumb to custom template - refs BT#20919

--- var/vue_templates/components/layout/DashboardLayout.vue
+++ var/vue_templates/components/layout/DashboardLayout.vue
@@ -3,8 +3,26 @@ import { useSecurityStore } from "../../../../assets/vue/store/securityStore"
 import Topbar from "./Topbar.vue"
 import Sidebar from "../../../../assets/vue/components/layout/Sidebar.vue"
 import SidebarNotLoggedIn from "./SidebarNotLoggedIn.vue"
+import Breadcrumb from "../../../../assets/vue/components/Breadcrumb.vue"
+
+defineProps({
+  showBreadcrumb: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 const securityStore = useSecurityStore()
+
+let breadcrumb = [];
+
+try {
+  if (window.breadcrumb) {
+    breadcrumb = window.breadcrumb;
+  }
+} catch (e) {
+  console.log(e.message);
+}
 </script>
 
 <template>
@@ -19,6 +37,10 @@ const securityStore = useSecurityStore()
     class="app-main"
     :class="{ 'app-main--no-sidebar': !securityStore.isAuthenticated, 'app-main--no-loggedin': !securityStore.isAuthenticated }"
   >
+    <Breadcrumb
+      v-if="showBreadcrumb"
+      :legacy="breadcrumb"
+    />
     <slot />
     <router-view />
   </div>

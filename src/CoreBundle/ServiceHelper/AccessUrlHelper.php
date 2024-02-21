7f4f4f52a3243be7d378a5d7f4fdd0df6a234390
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-02-21 00:40:37 -0500

    Move code from api_get_current_access_url_id to AccessUrlHelper

---
+++ src/CoreBundle/ServiceHelper/AccessUrlHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
+
+namespace Chamilo\CoreBundle\ServiceHelper;
+
+use Chamilo\CoreBundle\Entity\AccessUrl;
+use Chamilo\CoreBundle\Repository\Node\AccessUrlRepository;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class AccessUrlHelper
+{
+    public function __construct(
+        private readonly AccessUrlRepository $accessUrlRepository,
+        private readonly ParameterBagInterface $parameterBag,
+        private readonly RouterInterface $router,
+    ) {}
+
+    public function isMultipleEnabled(): bool
+    {
+        return 1 === (int) $this->parameterBag->get('multiple_access_url');
+    }
+
+    public function getCurrent(): AccessUrl
+    {
+        static $accessUrl;
+
+        if (!empty($accessUrl)) {
+            return $accessUrl;
+        }
+
+        $urlId = $this->accessUrlRepository->getFirstId();
+        $accessUrl = $this->accessUrlRepository->find($urlId);
+
+        if ($this->isMultipleEnabled()) {
+            $url = $this->router->generate('index', [], UrlGeneratorInterface::ABSOLUTE_URL);
+
+            /** @var AccessUrl $accessUrl */
+            $accessUrl = $this->accessUrlRepository->findOneBy(['url' => $url]);
+        }
+
+        return $accessUrl;
+    }
+}

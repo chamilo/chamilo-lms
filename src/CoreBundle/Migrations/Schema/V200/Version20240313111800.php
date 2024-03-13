b18808b563d13b34bc1afa7537f4d06c61ecd4bf
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-03-13 17:53:20 -0500

    Internal: Refactoring migration

--- src/CoreBundle/Migrations/Schema/V200/Version20240313111800.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20240313111800.php
@@ -11,18 +11,31 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20240313111800 extends AbstractMigrationChamilo
 {
+    private array $changes = [
+        'c_lp' => 'display_order',
+        'c_lp_category' => 'position',
+        'c_forum_category' => 'cat_order',
+        'c_forum_forum' => 'forum_order',
+        'c_thematic' => 'display_order',
+        'c_announcement' => 'display_order',
+    ];
+
     public function getDescription(): string
     {
-        return 'Removing display_order, position, cat_order, forum_order columns from c_lp, c_lp_category, c_forum_category tables, c_forum_forum, c_thematic, c_announcement';
+        $tables = array_keys($this->changes);
+        $columns = array_values($this->changes);
+
+        return sprintf(
+            'Removing %s columns from % tables',
+            implode(', ', $columns),
+            implode(', ', $tables)
+        );
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE c_lp DROP display_order');
-        $this->addSql('ALTER TABLE c_lp_category DROP position');
-        $this->addSql('ALTER TABLE c_forum_category DROP cat_order');
-        $this->addSql('ALTER TABLE c_forum_forum DROP forum_order');
-        $this->addSql('ALTER TABLE c_thematic DROP display_order');
-        $this->addSql('ALTER TABLE c_announcement DROP display_order');
+        foreach ($this->changes as $table => $column) {
+            $this->addSql("ALTER TABLE $table DROP $column");
+        }
     }
 }

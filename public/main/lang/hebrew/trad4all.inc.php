3f1029043e437b5551fd84c4bde6818c4eef5d05
Yannick Warnier <ywarnier@beeznest.org>
2024-03-19 16:30:44 +0100

    Language: Remove deprecated translations directory public/main/lang/ - refs #5251

--- public/main/lang/hebrew/trad4all.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-for more information: see languages.txt in the lang folder.
-*/
-$ShowAllEvents = "הראה את כל פריטי לוח הזמנים";
-$AdminBy = "מנוהל על ידי";
-?>
--- public/main/lang/tibetan/trad4all.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-for more information: see languages.txt in the lang folder.
-*/
-$Title = "ཁ་བྱང་";
-?>

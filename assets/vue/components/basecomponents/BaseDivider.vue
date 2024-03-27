b836253e7cd66041c1f39e5fb79fa3e73e48fb3a
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-03-27 03:13:58 -0500

    Display: Add BaseDivider component #2642

---
+++ assets/vue/components/basecomponents/BaseDivider.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Divider from "primevue/divider"
+
+defineProps({
+  align: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  layout: {
+    type: String,
+    required: false,
+    default: "horizontal",
+  },
+  title: {
+    type: [String, undefined],
+    required: false,
+    default: null,
+  },
+})
+</script>
+
+<template>
+  <Divider
+    :align="align"
+    :layout="layout"
+    class="divider"
+    unstyled
+  >
+    {{ title }}
+  </Divider>
+</template>

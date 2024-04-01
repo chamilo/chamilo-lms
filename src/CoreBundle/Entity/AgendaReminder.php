d1c4e0c7539dfffbb74f267ec4504d9a95afdf64
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-04-01 13:06:02 -0500

    Revert "Calendar: Refactor Agenda Reminders Script - refs #5145" (#5321)

--- src/CoreBundle/Entity/AgendaReminder.php
+++ src/CoreBundle/Entity/AgendaReminder.php
@@ -6,37 +6,12 @@ declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\Entity;
 
-use ApiPlatform\Core\Annotation\ApiFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
-use ApiPlatform\Metadata\GetCollection;
-use Chamilo\CoreBundle\Controller\Api\DeleteRemindersByEventAction;
-use Chamilo\CoreBundle\State\AgendaReminderProcessor;
 use Chamilo\CoreBundle\Traits\TimestampableTypedEntity;
 use DateInterval;
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Post;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'agenda_reminder')]
-#[ApiResource(
-    operations: [
-        new GetCollection(),
-        new Post(processor: AgendaReminderProcessor::class),
-        new Post(
-            uriTemplate: '/agenda_reminders/delete_by_event',
-            controller: DeleteRemindersByEventAction::class,
-            openapiContext: ['summary' => 'Deletes all reminders for a specific event'],
-            denormalizationContext: ['groups' => ['agenda_reminder:delete_by_event']],
-            security: "is_granted('ROLE_USER')"
-        ),
-    ],
-    normalizationContext: ['groups' => ['agenda_reminder:read']],
-    denormalizationContext: ['groups' => ['agenda_reminder:write']],
-    security: "is_granted('ROLE_USER')"
-)]
-#[ApiFilter(SearchFilter::class, properties: ['eventId' => 'exact'])]
 class AgendaReminder
 {
     use TimestampableTypedEntity;
@@ -47,25 +22,20 @@ class AgendaReminder
     protected ?int $id = null;
 
     #[ORM\Column(name: 'type', type: 'string')]
-    #[Groups(['agenda_reminder:write', 'agenda_reminder:read'])]
     protected string $type;
 
     #[ORM\Column(name: 'event_id', type: 'integer')]
-    #[Groups(['agenda_reminder:write', 'agenda_reminder:read'])]
     protected int $eventId;
 
     #[ORM\Column(name: 'date_interval', type: 'dateinterval')]
-    #[Groups(['agenda_reminder:write', 'agenda_reminder:read'])]
     protected DateInterval $dateInterval;
 
     #[ORM\Column(name: 'sent', type: 'boolean')]
-    #[Groups(['agenda_reminder:write', 'agenda_reminder:read'])]
     protected bool $sent;
 
     public function __construct()
     {
         $this->sent = false;
-        $this->type = 'personal';
     }
 
     public function getId(): ?int

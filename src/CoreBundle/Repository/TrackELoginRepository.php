c4e687e37c9bae6b3695c8d369cf4251c3c0a0cb
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-04-08 18:27:00 -0500

    Fix update last login/logout date when there is no result

--- src/CoreBundle/Repository/TrackELoginRepository.php
+++ src/CoreBundle/Repository/TrackELoginRepository.php
@@ -10,6 +10,8 @@ use Chamilo\CoreBundle\Entity\TrackELogin;
 use Chamilo\CoreBundle\Entity\User;
 use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\Persistence\ManagerRegistry;
 
 class TrackELoginRepository extends ServiceEntityRepository
@@ -34,7 +36,7 @@ class TrackELoginRepository extends ServiceEntityRepository
 
     public function updateLastLoginLogoutDate(int $userId, DateTime $logoutDate): void
     {
-        $lastLoginId = $this->createQueryBuilder('t')
+        $query = $this->createQueryBuilder('t')
             ->select('t.loginId')
             ->where('t.user = :userId')
             ->andWhere('t.logoutDate IS NULL')
@@ -42,8 +44,13 @@ class TrackELoginRepository extends ServiceEntityRepository
             ->orderBy('t.loginDate', 'DESC')
             ->setMaxResults(1)
             ->getQuery()
-            ->getSingleScalarResult()
         ;
+
+        try {
+            $lastLoginId = $query->getSingleScalarResult();
+        } catch (NoResultException|NonUniqueResultException) {
+            $lastLoginId = 0;
+        }
 
         if ($lastLoginId) {
             $qb = $this->createQueryBuilder('t')

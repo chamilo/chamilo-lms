2b89590ae784665c07e99be69bb055d7541edb55
NicoDucou <nicolas@ldd.fr>
2024-04-10 17:57:30 +0200

    Migration: fix name to title change, renaming in the table creation as well

--- src/CoreBundle/Migrations/Schema/V200/Version20240102161200.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20240102161200.php
@@ -18,6 +18,6 @@ final class Version20240102161200 extends AbstractMigrationChamilo
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE contact_form_contact_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ROW_FORMAT = DYNAMIC');
+        $this->addSql('CREATE TABLE contact_form_contact_category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ROW_FORMAT = DYNAMIC');
     }
 }

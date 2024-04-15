317b685c55fc18ccf2d190a46f26259ea8a0ef0d
NicoDucou <nicolas@ldd.fr>
2024-04-15 14:19:37 +0200

    Extra Fields: remove unneeded translation select name reverting commit af263a9df14f44429dea0acdac2b067b6b4e8eb3

--- public/main/inc/lib/extra_field.lib.php
+++ public/main/inc/lib/extra_field.lib.php
@@ -3359,7 +3359,7 @@ JAVASCRIPT;
 
         $select = $form->addSelect(
             'extra_'.$fieldDetails['variable'],
-            get_lang($fieldDetails['display_text']),
+            $fieldDetails['display_text'],
             [],
             ['id' => 'extra_'.$fieldDetails['variable']]
         );

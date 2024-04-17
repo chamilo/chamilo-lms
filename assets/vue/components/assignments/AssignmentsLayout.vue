2cfbc1856ba4beedf6eb13f1f7d8287b7f0df3b8
daniboygg <danigayosog@gmail.com>
2024-04-17 09:35:54 +0200

    Internal: Remove unused vuex code

--- assets/vue/components/assignments/AssignmentsLayout.vue
+++ assets/vue/components/assignments/AssignmentsLayout.vue
@@ -5,14 +5,12 @@
 <script setup>
 import { computed, ref, watchEffect } from "vue";
 import { useRoute, useRouter } from "vue-router"
-import { useStore } from "vuex";
 import { useI18n } from "vue-i18n";
 import { useCidReq } from "../../composables/cidReq";
 import { useSecurityStore } from "../../store/securityStore"
 
 const route = useRoute();
 const router = useRouter()
-const store = useStore();
 const { t } = useI18n();
 
 const { cid, sid, gid } = useCidReq();

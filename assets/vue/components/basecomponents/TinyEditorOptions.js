e40eca3c0f2f12bdafc4203aae9ee0bc5200ff48
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-04-22 16:19:06 -0500

    Show file manager when selecting files from tinymce editor

--- assets/vue/components/basecomponents/TinyEditorOptions.js
+++
@@ -1,6 +0,0 @@
-// option to show dialog to upload to personal files in Chamilo
-export const TINYEDITOR_MODE_PERSONAL_FILES = "personal_files"
-// ??
-export const TINYEDITOR_MODE_DOCUMENTS = "documents"
-
-export const TINYEDITOR_MODES = [TINYEDITOR_MODE_PERSONAL_FILES, TINYEDITOR_MODE_DOCUMENTS]

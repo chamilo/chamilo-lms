a96b386e8d7f99ee401ba9ee82eb9691c76e47aa
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-04-29 09:52:45 -0500

    Minor: Format code - refs BT#21600

--- src/CoreBundle/Entity/Page.php
+++ src/CoreBundle/Entity/Page.php
@@ -53,94 +53,115 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Page
 {
     use TimestampableTypedEntity;
+
     #[ORM\Column(name: 'id', type: 'integer')]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     protected ?int $id = null;
+
     #[Assert\NotBlank]
     #[Groups(['page:read', 'page:write'])]
     #[ORM\Column(name: 'title', type: 'string', length: 255)]
     protected string $title;
+
     #[Groups(['page:read', 'page:write', 'adminblock:read'])]
     #[Assert\NotBlank]
     #[ORM\Column(name: 'content', type: 'text')]
     protected string $content;
+
     #[Gedmo\Slug(fields: ['title'], updatable: true, unique: true)]
     #[ORM\Column(name: 'slug', type: 'string', length: 255)]
     protected string $slug;
+
     #[Groups(['page:read', 'page:write'])]
     #[ORM\Column(name: 'enabled', type: 'boolean', nullable: false)]
     protected bool $enabled;
+
     #[Groups(['page:read', 'page:write'])]
     #[Gedmo\SortablePosition]
     #[ORM\Column(name: 'position', type: 'integer')]
     protected int $position;
+
     #[Groups(['page:read', 'page:write'])]
     #[ORM\Column(name: 'locale', type: 'string', length: 10)]
     protected string $locale;
+
     #[Assert\NotNull]
     #[Groups(['page:read', 'page:write'])]
     #[ORM\ManyToOne(targetEntity: AccessUrl::class, cascade: ['persist'])]
     #[ORM\JoinColumn(name: 'access_url_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     protected AccessUrl $url;
+
     #[Assert\NotNull]
     #[Groups(['page:read', 'page:write'])]
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(name: 'creator_id', referencedColumnName: 'id', nullable: true, onDelete: 'CASCADE')]
     protected User $creator;
+
     #[Groups(['page:read', 'page:write', 'adminblock:read'])]
     #[Gedmo\SortableGroup]
     #[ORM\ManyToOne(targetEntity: PageCategory::class, inversedBy: 'pages')]
     #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id', onDelete: 'SET NULL')]
     protected ?PageCategory $category = null;
+
     public function __construct()
     {
         $this->enabled = false;
     }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
     public function getTitle(): string
     {
         return $this->title;
     }
+
     public function setTitle(string $title): self
     {
         $this->title = $title;
 
         return $this;
     }
+
     public function getContent(): string
     {
         return $this->content;
     }
+
     public function setContent(string $content): self
     {
         $this->content = $content;
 
         return $this;
     }
+
     public function getSlug(): string
     {
         return $this->slug;
     }
+
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
 
         return $this;
     }
+
     public function isEnabled(): bool
     {
         return $this->enabled;
     }
+
     public function setEnabled(bool $enabled): self
     {
         $this->enabled = $enabled;
 
         return $this;
     }
+
     public function getLocale(): string
     {
         return $this->locale;
@@ -151,40 +172,48 @@ class Page
 
         return $this;
     }
+
     public function getUrl(): AccessUrl
     {
         return $this->url;
     }
+
     public function setUrl(AccessUrl $url): self
     {
         $this->url = $url;
 
         return $this;
     }
+
     public function getCreator(): User
     {
         return $this->creator;
     }
+
     public function setCreator(User $creator): self
     {
         $this->creator = $creator;
 
         return $this;
     }
+
     public function getPosition(): int
     {
         return $this->position;
     }
+
     public function setPosition(int $position): self
     {
         $this->position = $position;
 
         return $this;
     }
+
     public function getCategory(): ?PageCategory
     {
         return $this->category;
     }
+
     public function setCategory(PageCategory $category): self
     {
         $this->category = $category;

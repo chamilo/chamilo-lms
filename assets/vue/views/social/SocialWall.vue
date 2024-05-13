b09d91d45a17e589f3f56c0f2beb771ecde58cd2
christianbeeznst <christian.fasanando@beeznest.com>
2024-05-13 00:30:03 -0500

    Social: Enhancements and Fixes for Social Network Features - refs BT#21101

--- assets/vue/views/social/SocialWall.vue
+++ assets/vue/views/social/SocialWall.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <SocialWallPostForm @post-created="refreshPosts" class="mb-6" />
+    <SocialWallPostForm v-if="!hidePostForm"  @post-created="refreshPosts" class="mb-6" />
     <SocialWallPostList ref="postListRef" class="mb-6" />
   </div>
 </template>
@@ -9,6 +9,13 @@
 import { ref } from 'vue';
 import SocialWallPostForm from "../../components/social/SocialWallPostForm.vue"
 import SocialWallPostList from "../../components/social/SocialWallPostList.vue"
+
+const props = defineProps({
+  hidePostForm: {
+    type: Boolean,
+    default: false
+  }
+});
 
 const postListRef = ref(null);
 

8ad3ba5e87a4cb2cfbe83a490d1a75722ef0d06f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2024-05-15 11:59:35 -0500

    Move code to function - refs BT#21633

--- assets/vue/components/session/SessionCategoryView.vue
+++ assets/vue/components/session/SessionCategoryView.vue
@@ -1,53 +1,27 @@
 <template>
-  <SessionListCategoryWrapper :sessions="sessionList" />
+  <SessionListCategoryWrapper :sessions="uncategorizedSessions" />
   <SessionCategoryListWrapper
     :categories="categories"
-    :category-with-sessions="categoryWithSessions"
+    :category-with-sessions="categoriesWithSessions"
   />
 </template>
 
 <script setup>
 import SessionCategoryListWrapper from "../../components/session/SessionCategoryListWrapper"
-import isEmpty from "lodash/isEmpty"
-import { computed } from "vue"
 import SessionListCategoryWrapper from "./SessionListCategoryWrapper.vue"
 
-const props = defineProps({
-  resultSessions: {
+defineProps({
+  uncategorizedSessions: {
     type: Array,
     required: true,
   },
-})
-
-let categories = computed(() => {
-  let categoryList = []
-  props.resultSessions.forEach((session) => {
-    if (session.category) {
-      const alreadyAdded = categoryList.findIndex((cat) => cat["@id"] === session.category["@id"]) >= 0
-
-      if (!alreadyAdded) {
-        categoryList.push(session.category)
-      }
-    }
-  })
-
-  return categoryList
-})
-
-const sessionList = computed(() => props.resultSessions.filter((session) => isEmpty(session.category)))
-
-let categoryWithSessions = computed(() => {
-  let categoriesIn = []
-  props.resultSessions.forEach(function (session) {
-    if (!isEmpty(session.category)) {
-      if (categoriesIn[session.category["@id"]] === undefined) {
-        categoriesIn[session.category["@id"]] = []
-        categoriesIn[session.category["@id"]]["sessions"] = []
-      }
-      categoriesIn[session.category["@id"]]["sessions"].push(session)
-    }
-  })
-
-  return categoriesIn
+  categories: {
+    type: Array,
+    required: true,
+  },
+  categoriesWithSessions: {
+    type: Array,
+    required: true,
+  },
 })
 </script>
--- assets/vue/views/user/sessions/session.js
+++ assets/vue/views/user/sessions/session.js
@@ -1,24 +1,70 @@
 import { ref } from "vue"
 import { useSecurityStore } from "../../../store/securityStore"
 import sessionService from "../../../services/sessionService"
+import isEmpty from "lodash/isEmpty"
 
 export function useSession(type) {
   const securityStore = useSecurityStore()
 
-  const sessions = ref([])
   const isLoading = ref(false)
+
+  const uncategorizedSessions = ref([])
+  const categories = ref([])
+  const categoriesWithSessions = ref([])
+
+  function getUncategorizedSessions(sessions) {
+    return sessions.filter((session) => isEmpty(session.category))
+  }
+
+  function getCategories(sessions) {
+    let categoryList = []
+
+    sessions.forEach((session) => {
+      if (session.category) {
+        const alreadyAdded = categoryList.findIndex((cat) => cat["@id"] === session.category["@id"]) >= 0
+
+        if (!alreadyAdded) {
+          categoryList.push(session.category)
+        }
+      }
+    })
+
+    return categoryList
+  }
+
+  function getCategoriesWithSessions(sessions) {
+    let categoriesIn = []
+
+    sessions.forEach(function (session) {
+      if (!isEmpty(session.category)) {
+        if (categoriesIn[session.category["@id"]] === undefined) {
+          categoriesIn[session.category["@id"]] = []
+          categoriesIn[session.category["@id"]]["sessions"] = []
+        }
+        categoriesIn[session.category["@id"]]["sessions"].push(session)
+      }
+    })
+
+    return categoriesIn
+  }
 
   if (securityStore.isAuthenticated) {
     isLoading.value = true
 
     sessionService
       .findUserSubscriptions(securityStore.user["@id"], type)
-      .then(({ items }) => (sessions.value = items))
+      .then(({ items }) => {
+        uncategorizedSessions.value = getUncategorizedSessions(items)
+        categories.value = getCategories(items)
+        categoriesWithSessions.value = getCategoriesWithSessions(items)
+      })
       .finally(() => (isLoading.value = false))
   }
 
   return {
-    sessions,
     isLoading,
+    uncategorizedSessions,
+    categories,
+    categoriesWithSessions,
   }
 }

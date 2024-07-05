a185b2085b5988d9eaf8a805348dccc215385689
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2024-07-05 11:57:09 -0500

    Display: Add link to top bar logo + load logo svg first

--- assets/vue/components/layout/PlatformLogo.vue
+++ assets/vue/components/layout/PlatformLogo.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { usePlatformConfig } from "../../store/platformConfig"
 import { useVisualTheme } from "../../composables/theme"
+import BaseAppLink from "../basecomponents/BaseAppLink.vue"
 
 const platformConfigStore = usePlatformConfig()
 const { getThemeAssetUrl } = useVisualTheme()
@@ -9,9 +10,19 @@ const siteName = platformConfigStore.getSetting("platform.site_name")
 </script>
 
 <template>
-  <img
-    :alt="siteName"
-    :title="siteName"
-    :src="getThemeAssetUrl('images/header-logo.png')"
-  />
+  <div class="platform-logo">
+    <BaseAppLink :to="{ name: 'Index' }">
+      <picture>
+        <source
+          :srcset="getThemeAssetUrl('images/header-logo.svg')"
+          type="image/svg+xml"
+        />
+        <img
+          :alt="siteName"
+          :title="siteName"
+          :src="getThemeAssetUrl('images/header-logo.png')"
+        />
+      </picture>
+    </BaseAppLink>
+  </div>
 </template>

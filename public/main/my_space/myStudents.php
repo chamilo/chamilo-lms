ed9d3144be84f5d97ed289073cc6bc8c259229ec
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2024-07-09 01:59:30 -0500

    Minor: Fix type of value - refs BT#21621

--- public/main/my_space/myStudents.php
+++ public/main/my_space/myStudents.php
@@ -27,7 +27,7 @@ $export = $_GET['export'] ?? false;
 $sessionId = isset($_GET['sid']) ? (int) $_GET['sid'] : 0;
 $action = $_GET['action'] ?? '';
 $origin = api_get_origin();
-$courseId = isset($_GET['cid']) ? (int) $_GET['cid'] : '';
+$courseId = isset($_GET['cid']) ? (int) $_GET['cid'] : 0;
 $course = api_get_course_entity($courseId);
 $courseCode = '';
 if (null !== $course) {

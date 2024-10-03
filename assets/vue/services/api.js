025ea4ff48e91bd568220438b8e9583cc85d68a0
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2024-10-03 00:52:53 -0500

    Minor: Format code

--- assets/vue/services/api.js
+++ assets/vue/services/api.js
@@ -1,4 +1,4 @@
-import fetch from '../utils/fetch';
+import fetch from "../utils/fetch"
 
 // As stated here https://github.com/chamilo/chamilo-lms/pull/5386#discussion_r1578471409
 // this service should not be used and instead the assets/bue/config/api.js should be used instead
@@ -6,66 +6,66 @@ import fetch from '../utils/fetch';
 export default function makeService(endpoint, extensions = {}) {
   const baseService = {
     find(id, params) {
-      console.log('api.js find');
-      const currentParams = new URLSearchParams(window.location.search);
+      console.log("api.js find")
+      const currentParams = new URLSearchParams(window.location.search)
       const combinedParams = {
         ...Object.fromEntries(currentParams),
         ...params,
-        getFile: true
-      };
+        getFile: true,
+      }
 
-      const searchParams = new URLSearchParams(combinedParams);
+      const searchParams = new URLSearchParams(combinedParams)
 
-      return fetch(id, { params: Object.fromEntries(searchParams) });
+      return fetch(id, { params: Object.fromEntries(searchParams) })
     },
     findAll(params) {
-      console.log('api.js findAll');
-      console.log(params);
-      return fetch(endpoint, params);
+      console.log("api.js findAll")
+      console.log(params)
+      return fetch(endpoint, params)
     },
     async createWithFormData(payload) {
-      console.log('api.js createWithFormData');
+      console.log("api.js createWithFormData")
 
-      let formData = new FormData();
-      console.log('body');
-      console.log(payload);
+      let formData = new FormData()
+      console.log("body")
+      console.log(payload)
       if (payload) {
         Object.keys(payload).forEach(function (key) {
           // key: the name of the object key
           // index: the ordinal position of the key within the object
-          formData.append(key, payload[key]);
-          console.log('options.key', key);
-        });
-        payload = formData;
+          formData.append(key, payload[key])
+          console.log("options.key", key)
+        })
+        payload = formData
       }
 
-      return fetch(endpoint, { method: 'POST', body: payload});
+      return fetch(endpoint, { method: "POST", body: payload })
     },
     async create(payload) {
-      console.log('api.js create');
-      console.log(payload);
-      return fetch(endpoint, { method: 'POST', body: JSON.stringify(payload) });
+      console.log("api.js create")
+      console.log(payload)
+      return fetch(endpoint, { method: "POST", body: JSON.stringify(payload) })
     },
     del(item) {
-      console.log('api.js del');
-      console.log(item['@id']);
-      return fetch(item['@id'], { method: 'DELETE' });
+      console.log("api.js del")
+      console.log(item["@id"])
+      return fetch(item["@id"], { method: "DELETE" })
     },
     updateWithFormData(payload) {
-      console.log('api.js - update');
+      console.log("api.js - update")
 
-      return fetch(payload['@id'], {
-        method: 'PUT',
-        body: JSON.stringify(payload)
-      });
+      return fetch(payload["@id"], {
+        method: "PUT",
+        body: JSON.stringify(payload),
+      })
     },
     update(payload) {
-      console.log('api.js - update');
+      console.log("api.js - update")
 
-      return fetch(payload['@id'], {
-        method: 'PUT',
-        body: JSON.stringify(payload)
-      });
+      return fetch(payload["@id"], {
+        method: "PUT",
+        body: JSON.stringify(payload),
+      })
     },
     handleError(error, errorsRef, violationsRef) {
       if (error instanceof SubmissionError) {
@@ -74,8 +74,8 @@ export default function makeService(endpoint, extensions = {}) {
         return
       }
       errorsRef.value = error.message
-    }
-  };
+    },
+  }
 
-  return { ...baseService, ...extensions };
+  return { ...baseService, ...extensions }
 }
--- assets/vue/utils/fetch.js
+++ assets/vue/utils/fetch.js
@@ -1,141 +1,132 @@
-import { isArray, isObject, isUndefined, forEach } from 'lodash';
-import { ENTRYPOINT } from '../config/entrypoint';
-import SubmissionError from '../error/SubmissionError';
-import { normalize } from './hydra';
+import { isObject } from "lodash"
+import { ENTRYPOINT } from "../config/entrypoint"
+import SubmissionError from "../error/SubmissionError"
+import { normalize } from "./hydra"
 
-const MIME_TYPE = 'application/ld+json';
+const MIME_TYPE = "application/ld+json"
 
-const makeParamArray = (key, arr) =>
-  arr.map(val => `${key}[]=${val}`).join('&');
+const makeParamArray = (key, arr) => arr.map((val) => `${key}[]=${val}`).join("&")
 
-export default function(id, options = {}) {
-    console.log('fetch');
-    console.log(options);
+export default function (id, options = {}) {
+  console.log("fetch")
+  console.log(options)
 
-    if ("undefined" === typeof options.headers) {
-        options.headers = {};
-    }
+  if ("undefined" === typeof options.headers) {
+    options.headers = {}
+  }
 
-    if (!options.headers.hasOwnProperty("Accept")) {
-        options.headers = { ...options.headers, Accept: MIME_TYPE };
-    }
+  if (!options.headers.hasOwnProperty("Accept")) {
+    options.headers = { ...options.headers, Accept: MIME_TYPE }
+  }
 
-    if (
-        undefined !== options.body &&
-        !(options.body instanceof FormData) &&
-        !options.headers.hasOwnProperty("Content-Type")
-    ) {
-        options.headers = { ...options.headers, "Content-Type": MIME_TYPE };
-    }
+  if (
+    undefined !== options.body &&
+    !(options.body instanceof FormData) &&
+    !options.headers.hasOwnProperty("Content-Type")
+  ) {
+    options.headers = { ...options.headers, "Content-Type": MIME_TYPE }
+  }
 
-    if (options.params) {
-        const params = normalize(options.params);
-        let queryString = Object.keys(params)
-            .map(key =>
-                Array.isArray(params[key])
-                    ? makeParamArray(key, params[key])
-                    : `${key}=${params[key]}`
-            )
-            .join('&');
-        id = `${id}?${queryString}`;
-        console.log('URL', id);
-    }
+  if (options.params) {
+    const params = normalize(options.params)
+    let queryString = Object.keys(params)
+      .map((key) => (Array.isArray(params[key]) ? makeParamArray(key, params[key]) : `${key}=${params[key]}`))
+      .join("&")
+    id = `${id}?${queryString}`
+    console.log("URL", id)
+  }
 
-    const entryPoint = ENTRYPOINT + (ENTRYPOINT.endsWith('/') ? '' : '/');
+  const entryPoint = ENTRYPOINT + (ENTRYPOINT.endsWith("/") ? "" : "/")
 
-    if ('PUT' === options.method) {
-        const payload = options.body && JSON.parse(options.body);
-        if (isObject(payload) && payload['@id']) {
-            options.body = JSON.stringify(normalize(payload));
-        }
-    }
-
-    /*const payload = options.body && JSON.parse(options.body);
+  if ("PUT" === options.method) {
+    const payload = options.body && JSON.parse(options.body)
     if (isObject(payload) && payload["@id"]) {
-        options.body = JSON.stringify(normalize(payload));
-    }*/
+      options.body = JSON.stringify(normalize(payload))
+    }
+  }
 
-    /*if (useAxios) {
-        console.log('axios');
-        let url = new URL(id, entryPoint);
-        console.log(formData);
-        return axios({
-            url: url.toString(),
-            method: 'POST',
-            //headers: options.headers,
-            data: formData,
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            },
-            onUploadProgress: function (progressEvent) {
-                console.log('progress');
-                //console.log(progressEvent);
-                console.log(options.body);
+  /*const payload = options.body && JSON.parse(options.body);
+  if (isObject(payload) && payload["@id"]) {
+      options.body = JSON.stringify(normalize(payload));
+  }*/
 
-                let uploadPercentage = parseInt(Math.round((progressEvent.loaded / progressEvent.total) * 100));
-                options.body['__progress'] = uploadPercentage;
-                options.body['__progressLabel'] = uploadPercentage;
-                this.uploadPercentage = uploadPercentage;
-                console.log(options.body);
-                //options.body.set('__progressLabel', uploadPercentage);
-            }.bind(this)
-        }
-        ).then(response => {
-            options.body['__uploaded'] = 1;
-            options.body['uploadFile']['__uploaded'] = 1
+  /*if (useAxios) {
+      console.log('axios');
+      let url = new URL(id, entryPoint);
+      console.log(formData);
+      return axios({
+          url: url.toString(),
+          method: 'POST',
+          //headers: options.headers,
+          data: formData,
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          },
+          onUploadProgress: function (progressEvent) {
+              console.log('progress');
+              //console.log(progressEvent);
+              console.log(options.body);
 
-            console.log(response);
-            console.log('SUCCESS!!');
+              let uploadPercentage = parseInt(Math.round((progressEvent.loaded / progressEvent.total) * 100));
+              options.body['__progress'] = uploadPercentage;
+              options.body['__progressLabel'] = uploadPercentage;
+              this.uploadPercentage = uploadPercentage;
+              console.log(options.body);
+              //options.body.set('__progressLabel', uploadPercentage);
+          }.bind(this)
+      }
+      ).then(response => {
+          options.body['__uploaded'] = 1;
+          options.body['uploadFile']['__uploaded'] = 1
 
-            return response.data;
-        })
-            .catch(function (response) {
-                console.log(response);
-                console.log('FAILURE!!');
-            });
-    }*/
+          console.log(response);
+          console.log('SUCCESS!!');
 
-  console.log('ready to fetch');
+          return response.data;
+      })
+          .catch(function (response) {
+              console.log(response);
+              console.log('FAILURE!!');
+          });
+  }*/
 
-  return global.fetch(new URL(id, entryPoint), options).then(response => {
-    console.log(response, 'global.fetch');
+  console.log("ready to fetch")
+
+  return global.fetch(new URL(id, entryPoint), options).then((response) => {
+    console.log(response, "global.fetch")
 
     if (response.ok) {
-        return response;
+      return response
     }
 
-    return response.json().then(json => {
-        let error =
-          json['hydra:description'] ||
-          json['hydra:title'] ||
-          'An error occurred.';
+    return response.json().then(
+      (json) => {
+        let error = json["hydra:description"] || json["hydra:title"] || "An error occurred."
 
-        if (json['code'] && 401 === json['code']) {
-            error = 'Not allowed';
+        if (json["code"] && 401 === json["code"]) {
+          error = "Not allowed"
         }
 
-        if (json['error']) {
-            error = json['error'];
+        if (json["error"]) {
+          error = json["error"]
         }
 
-        console.log(error, 'fetch error');
+        console.log(error, "fetch error")
 
         if (!json.violations) {
-            console.log('violations');
-            throw Error(error);
+          console.log("violations")
+          throw Error(error)
         }
 
-        let errors = { _error: error };
-        json.violations.map(
-            violation => (errors[violation.propertyPath] = violation.message)
-        );
+        let errors = { _error: error }
+        json.violations.map((violation) => (errors[violation.propertyPath] = violation.message))
 
-        throw new SubmissionError(errors);
+        throw new SubmissionError(errors)
       },
       () => {
-        console.log('error3');
-        throw new Error(response.statusText || 'An error occurred.');
-      }
-    );
-  });
+        console.log("error3")
+        throw new Error(response.statusText || "An error occurred.")
+      },
+    )
+  })
 }

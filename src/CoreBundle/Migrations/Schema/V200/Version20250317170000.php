76d9ff81f16e37f985b1ff61befcaac6c740ed13
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-03-18 17:56:54 -0500

    Internal: Add migration to drop hook_* tables #2675

---
+++ src/CoreBundle/Migrations/Schema/V200/Version20250317170000.php
@@ -0,0 +1,30 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
+
+namespace Chamilo\CoreBundle\Migrations\Schema\V200;
+
+use Chamilo\CoreBundle\Entity\HookEvent;
+use Chamilo\CoreBundle\Event\Events;
+use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20250317170000 extends AbstractMigrationChamilo
+{
+    public function getDescription(): string
+    {
+        return 'Drop tables relatesd to hooks management.';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function up(Schema $schema): void
+    {
+        $this->addSql("DROP TABLE hook_call");
+        $this->addSql("DROP TABLE hook_event");
+        $this->addSql("DROP TABLE hook_observer");
+    }
+}

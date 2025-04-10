414686a9480f272e3bd580c1105eb9179362ad75
Nosolored SL <nosolored@gmail.com>
2025-04-10 11:12:28 +0200

    Add send_subscription_notification field when copying sessions

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -4623,7 +4623,9 @@ class SessionManager
             $s['duration'],
             $s['description'],
             $s['show_description'],
-            $extraFieldsValuesToCopy
+            $extraFieldsValuesToCopy,
+            0,
+            $s['send_subscription_notification']
         );
 
         if (!is_numeric($sid) || empty($sid)) {

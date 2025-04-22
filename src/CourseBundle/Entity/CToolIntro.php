8eb317a6e5ea8feb8ea32936748d34525a2af008
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-04-22 17:57:08 -0500

    Internal: Refactor API Resource definitions for CTool and CToolIntro

--- src/CourseBundle/Entity/CToolIntro.php
+++ src/CourseBundle/Entity/CToolIntro.php
@@ -26,7 +26,22 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ApiResource(operations: [new Get(security: 'is_granted(\'VIEW\', object)'), new Put(security: 'is_granted(\'EDIT\', object)'), new Delete(security: 'is_granted(\'DELETE\', object)'), new GetCollection(security: 'is_granted(\'ROLE_USER\')'), new Post(securityPostDenormalize: 'is_granted(\'CREATE\', object)')], security: 'is_granted(\'ROLE_ADMIN\') or is_granted(\'ROLE_CURRENT_COURSE_TEACHER\')', denormalizationContext: ['groups' => ['c_tool_intro:write']], normalizationContext: ['groups' => ['c_tool_intro:read']])]
+#[ApiResource(
+    operations: [
+        new Get(security: "is_granted('VIEW', object)"),
+        new Put(security: "is_granted('EDIT', object)"),
+        new Delete(security: "is_granted('DELETE', object)"),
+        new GetCollection(security: "is_granted('ROLE_USER')"),
+        new Post(securityPostDenormalize: "is_granted('CREATE', object)"),
+    ],
+    normalizationContext: [
+        'groups' => ['c_tool_intro:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['c_tool_intro:write'],
+    ],
+    security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_CURRENT_COURSE_TEACHER')",
+)]
 #[ORM\Table(name: 'c_tool_intro')]
 #[ORM\Entity(repositoryClass: CToolIntroRepository::class)]
 #[ApiFilter(filterClass: SearchFilter::class, properties: ['courseTool' => 'exact'])]
@@ -39,51 +54,62 @@ class CToolIntro extends AbstractResource implements ResourceInterface, Resource
     #[ORM\Id]
     #[ORM\GeneratedValue]
     protected ?int $iid = null;
+
     #[Assert\NotNull]
     #[Groups(['c_tool_intro:read', 'c_tool_intro:write'])]
     #[ORM\Column(name: 'intro_text', type: 'text', nullable: false)]
     protected string $introText;
+
     #[Assert\NotNull]
     #[Groups(['c_tool_intro:read', 'c_tool_intro:write'])]
     #[ORM\ManyToOne(targetEntity: CTool::class)]
     #[ORM\JoinColumn(name: 'c_tool_id', referencedColumnName: 'iid', nullable: false, onDelete: 'CASCADE')]
     protected CTool $courseTool;
+
     public function __toString(): string
     {
         return $this->getIntroText();
     }
+
     public function getIid(): ?int
     {
         return $this->iid;
     }
+
     public function getCourseTool(): CTool
     {
         return $this->courseTool;
     }
+
     public function setCourseTool(CTool $courseTool): self
     {
         $this->courseTool = $courseTool;
 
         return $this;
     }
+
     public function setIntroText(string $introText): self
     {
         $this->introText = $introText;
 
         return $this;
     }
+
     public function getIntroText(): string
     {
         return $this->introText;
     }
+
     public function getResourceIdentifier(): int|Uuid
     {
         return $this->getIid();
     }
+
     public function getResourceName(): string
     {
         return $this->getCourseTool()->getTitle();
     }
+
     public function setResourceName(string $name): self
     {
         return $this;

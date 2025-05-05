d581cf84287be77d72e72498a145653ee572e0fe
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-05-05 16:36:52 -0500

    Import primevue/toggleswitch instead primevue/inputswitch

--- assets/vue/components/Login.vue
+++ assets/vue/components/Login.vue
@@ -37,7 +37,7 @@
       </div>
 
       <div class="field login-section__remember-me">
-        <InputSwitch
+        <ToggleSwitch
           v-model="remember"
           input-id="binary"
           name="remember_me"
@@ -85,7 +85,7 @@ import { computed, ref } from "vue"
 import Button from "primevue/button"
 import InputText from "primevue/inputtext"
 import Password from "primevue/password"
-import InputSwitch from "primevue/inputswitch"
+import ToggleSwitch from "primevue/toggleswitch"
 import { useI18n } from "vue-i18n"
 import { useLogin } from "../composables/auth/login"
 import LoginOAuth2Buttons from "./login/LoginOAuth2Buttons.vue"

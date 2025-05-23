f95c44d956bc454012451b127eb75489cd34f048
Yannick Warnier <ywarnier@beeznest.org>
2025-05-23 15:03:58 +0200

    Quiz: Fix SQL error with remaining c_quiz.id field instead of iid - refs #3320

--- public/main/exercise/exercise.class.php
+++ public/main/exercise/exercise.class.php
@@ -8073,21 +8073,21 @@ class Exercise
         if (0 != $sessionId) {
             $sql = "SELECT * FROM $track_exercises te
               INNER JOIN c_quiz cq
-              ON cq.id = te.exe_exo_id AND te.c_id = cq.c_id
+              ON cq.iid = te.exe_exo_id AND te.c_id = cq.c_id
               WHERE
               te.c_id = %d AND
               te.session_id = %s AND
-              cq.id IN (%s)
-              ORDER BY cq.id";
+              cq.iid IN (%s)
+              ORDER BY cq.iid";
 
             $sql = sprintf($sql, $courseId, $sessionId, $ids);
         } else {
             $sql = "SELECT * FROM $track_exercises te
-              INNER JOIN c_quiz cq ON cq.id = te.exe_exo_id AND te.c_id = cq.c_id
+              INNER JOIN c_quiz cq ON cq.iid = te.exe_exo_id AND te.c_id = cq.c_id
               WHERE
               te.c_id = %d AND
-              cq.id IN (%s)
-              ORDER BY cq.id";
+              cq.iid IN (%s)
+              ORDER BY cq.iid";
             $sql = sprintf($sql, $courseId, $ids);
         }
         $result = Database::query($sql);

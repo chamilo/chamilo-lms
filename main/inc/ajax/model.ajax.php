c3afce072806198c39e36de7b0b72be01ca7c6c2
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-05-26 18:31:20 -0500

    Security: Initialize variable to avoid wrong result by old value
    See GHSA-qv5j-rq2p-q5w5

--- main/inc/ajax/model.ajax.php
+++ main/inc/ajax/model.ajax.php
@@ -143,6 +143,8 @@ $accessStartDate = '';
 $accessEndDate = '';
 $overwriteColumnHeaderExport = [];
 
+$result = [];
+
 if (!empty($search)) {
     $search = 'true';
 }
@@ -215,7 +217,7 @@ if (($search || $forceSearch) && ($search !== 'false')) {
                     }
                 }
 
-                $result = $extraField->getExtraFieldRules($filters, 'extra_');
+                $result = []; $extraField->getExtraFieldRules($filters, 'extra_');
 
                 $extra_fields = $result['extra_fields'];
                 $condition_array = $result['condition_array'];

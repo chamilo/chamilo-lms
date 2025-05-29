26b2691f7b5aa352f340ecd6fef22d8394069484
Christian Beeznest <christian.fasanando@beeznest.com>
2025-05-29 13:02:16 -0500

    Exercise: Add parent_media_id field to CQuizQuestion - refs #2676

---
+++ src/CoreBundle/Migrations/Schema/V200/Version20250529165710.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/* For licensing terms, see /license.txt */
+
+namespace Chamilo\CoreBundle\Migrations\Schema\V200;
+
+use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20250529165710 extends AbstractMigrationChamilo
+{
+    public function getDescription(): string
+    {
+        return 'Add parent_media_id column to c_quiz_question for grouping media-related questions.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE c_quiz_question ADD parent_media_id INT DEFAULT NULL;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE c_quiz_question DROP COLUMN parent_media_id;');
+    }
+}

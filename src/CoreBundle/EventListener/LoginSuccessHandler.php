6ba5eb07623ac6ac044c14ccf48a49d06b1b176a
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-06-12 17:10:48 -0500

    Internal: Rename variable properly + clear code - refs BT#22704

--- src/CoreBundle/EventListener/LoginSuccessHandler.php
+++ src/CoreBundle/EventListener/LoginSuccessHandler.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 /* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\EventListener;
 
@@ -14,6 +14,7 @@ use Chamilo\CoreBundle\Repository\TrackELoginRecordRepository;
 use Chamilo\CoreBundle\Repository\TrackELoginRepository;
 use Chamilo\CoreBundle\Repository\TrackEOnlineRepository;
 use Chamilo\CoreBundle\ServiceHelper\LoginAttemptLogger;
+use Chamilo\CoreBundle\ServiceHelper\UserHelper;
 use Chamilo\CoreBundle\Settings\SettingsManager;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,7 +24,6 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use UserManager;
 
-// class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 class LoginSuccessHandler
 {
     public function __construct(
@@ -31,18 +31,19 @@ class LoginSuccessHandler
         private readonly AuthorizationCheckerInterface $checker,
         private readonly SettingsManager $settingsManager,
         private readonly EntityManagerInterface $entityManager,
-        private readonly LoginAttemptLogger $loginAttemptLogger
+        private readonly LoginAttemptLogger $loginAttemptLogger,
+        private readonly UserHelper $userHelper,
     ) {}
 
-    public function __invoke(InteractiveLoginEvent $event): ?RedirectResponse
+    /**
+     * @throws \Exception
+     */
+    public function __invoke(InteractiveLoginEvent $event): RedirectResponse
     {
         $request = $event->getRequest();
-        $session = $request->getSession();
+        $requestSession = $request->getSession();
 
-        /** @var User $user */
-        $user = $event->getAuthenticationToken()->getUser();
-        $userId = $user->getId();
-        $session = $request->getSession();
+        $user = $this->userHelper->getCurrent();
 
         // $userInfo = api_get_user_info($user->getId());
         // $userInfo['is_anonymous'] = false;
@@ -51,20 +52,20 @@ class LoginSuccessHandler
         // $ip = $request->getClientIp();
 
         // Setting user info.
-        // $session->set('_user', $user);
+        // $requestSession->set('_user', $user);
 
         // Setting admin permissions for.
         if ($this->checker->isGranted('ROLE_ADMIN')) {
-            $session->set('is_platformAdmin', true);
+            $requestSession->set('is_platformAdmin', true);
         }
 
         // Setting teachers permissions.
         if ($this->checker->isGranted('ROLE_TEACHER')) {
-            $session->set('is_allowedCreateCourse', true);
+            $requestSession->set('is_allowedCreateCourse', true);
         }
 
         // Setting last login datetime
-        $session->set('user_last_login_datetime', api_get_utc_datetime());
+        $requestSession->set('user_last_login_datetime', api_get_utc_datetime());
 
         $response = null;
         /* Possible values: index.php, user_portal.php, main/auth/courses.php */
@@ -96,14 +97,14 @@ class LoginSuccessHandler
 
         $goToCourse = $this->settingsManager->getSetting('course.go_to_course_after_login');
 
-        $session->set('_uid', $user->getId());
-        // $session->set('_user', $userInfo);
-        // $session->set('is_platformAdmin', \UserManager::is_admin($userId));
-        // $session->set('is_allowedCreateCourse', $userInfo['status'] === 1);
+        $requestSession->set('_uid', $user->getId());
+        // $requestSession->set('_user', $userInfo);
+        // $requestSession->set('is_platformAdmin', \UserManager::is_admin($userId));
+        // $requestSession->set('is_allowedCreateCourse', $userInfo['status'] === 1);
         // Redirecting to a course or a session.
         if ('true' === $goToCourse) {
-            // Get the courses list
-            $personal_course_list = UserManager::get_personal_session_course_list($userId);
+            // Get the course list
+            $personal_course_list = UserManager::get_personal_session_course_list($user->getId());
             $my_session_list = [];
             $count_of_courses_no_sessions = 0;
             foreach ($personal_course_list as $course) {
@@ -118,7 +119,7 @@ class LoginSuccessHandler
             if (1 === $count_of_sessions && 0 === $count_of_courses_no_sessions) {
                 $key = array_keys($personal_course_list);
                 $course_info = $personal_course_list[$key[0]]['course_info'];
-                $sessionId = isset($course_info['session_id']) ? $course_info['session_id'] : 0;
+                $sessionId = $course_info['session_id'] ?? 0;
                 $url = api_get_path(WEB_COURSE_PATH).$course_info['directory'].'/index.php?sid='.$sessionId;
             }
 
@@ -129,7 +130,7 @@ class LoginSuccessHandler
             }
         }
 
-        if (!$session->get('login_records_created')) {
+        if (!$requestSession->get('login_records_created')) {
             $userIp = $request->getClientIp();
 
             /** @var TrackEOnlineRepository $trackEOnlineRepository */
@@ -152,7 +153,7 @@ class LoginSuccessHandler
             $trackELoginRecordRepository->addTrackLogin($user->getUsername(), $userIp, true);
             $this->loginAttemptLogger->logAttempt(true, $user->getUsername(), $userIp);
 
-            $session->set('login_records_created', true);
+            $requestSession->set('login_records_created', true);
         }
 
         if (!empty($url)) {

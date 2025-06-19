9495a0ddcabacdd1efa811bffaa782d592c6eea4
Christian Beeznest <christian.fasanando@beeznest.com>
2025-06-19 12:43:40 -0500

    Internal: Split multiple classes from scorm_classes.php into separate files - refs #1179

--- public/main/exercise/export/scorm/ScormAnswerHotspot.php
+++ public/main/exercise/export/scorm/ScormAnswerHotspot.php
@@ -3,43 +3,6 @@
 /* For licensing terms, see /license.txt */
 
 /**
- * This class handles the SCORM export of free-answer questions.
- */
-class ScormAnswerFree extends Answer
-{
-    /**
-     * Export the text with missing words.
-     *
-     * As a side effect, it stores two lists in the class :
-     * the missing words and their respective weightings.
-     */
-    public function export()
-    {
-        $js = '';
-        $identifier = 'question_'.$this->questionJSId.'_free';
-        // currently the free answers cannot be displayed, so ignore the textarea
-        $html = '<tr><td colspan="2">';
-        $type = $this->getQuestionType();
-
-        if (ORAL_EXPRESSION == $type) {
-            $html = '<tr><td colspan="2">'.get_lang('This learning object or activity is not SCORM compliant. That\'s why it is not exportable.').'</td></tr>';
-
-            return [$js, $html];
-        }
-
-        $html .= '<textarea minlength="20" name="'.$identifier.'" id="'.$identifier.'" ></textarea>';
-        $html .= '</td></tr>';
-        $js .= 'questions_answers['.$this->questionJSId.'] = new Array();';
-        $js .= 'questions_answers_correct['.$this->questionJSId.'] = "";';
-        $js .= 'questions_types['.$this->questionJSId.'] = \'free\';';
-        $jstmpw = 'questions_answers_ponderation['.$this->questionJSId.'] = "0";';
-        $js .= $jstmpw;
-
-        return [$js, $html];
-    }
-}
-
-/**
  * This class handles the SCORM export of hotpot questions.
  */
 class ScormAnswerHotspot extends Answer
@@ -95,11 +58,11 @@ class ScormAnswerHotspot extends Answer
         // Get the answers, make a list
         $nbrAnswers = $this->selectNbrAnswers();
 
-        $answerList = '<div 
-            style="padding: 10px; 
-            margin-left: -8px; 
-            border: 1px solid #4271b5; 
-            height: 448px; 
+        $answerList = '<div
+            style="padding: 10px;
+            margin-left: -8px;
+            border: 1px solid #4271b5;
+            height: 448px;
             width: 200px;"><b>'.get_lang('Image zones').'</b><ol>';
         for ($answerId = 1; $answerId <= $nbrAnswers; $answerId++) {
             $answerList .= '<li>'.$this->selectAnswer($answerId).'</li>';

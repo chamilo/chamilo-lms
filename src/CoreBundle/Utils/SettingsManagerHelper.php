14f05002fb53de3cdc658293ca0d67ded33e3d6c
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-06-24 18:30:46 -0500

    Internal: Move SettingsManagerHelper directory

--- src/CoreBundle/Utils/SettingsManagerHelper.php
+++ src/CoreBundle/Utils/SettingsManagerHelper.php
@@ -4,10 +4,9 @@
 
 declare(strict_types=1);
 
-namespace Chamilo\CoreBundle\ServiceHelper;
+namespace Chamilo\CoreBundle\Utils;
 
 use Chamilo\CoreBundle\Entity\AccessUrl;
-use Chamilo\CoreBundle\Utils\AccessUrlUtil;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 readonly class SettingsManagerHelper

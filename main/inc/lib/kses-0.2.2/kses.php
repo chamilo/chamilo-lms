be94e40ff58eed62e05f369298c950fa7d1297e3
Yannick Warnier <ywarnier@beeznest.org>
2025-06-25 00:21:37 +0200

    Internal: Kses: Fix issue preventing loading $kses_allowedentitynames from global scope in PHP 8+ - refs #5021

--- main/inc/lib/kses-0.2.2/kses.php
+++ main/inc/lib/kses-0.2.2/kses.php
@@ -34,50 +34,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$kses_allowedentitynames = array(
-    'nbsp',    'iexcl',  'cent',    'pound',  'curren', 'yen',
-    'brvbar',  'sect',   'uml',     'copy',   'ordf',   'laquo',
-    'not',     'shy',    'reg',     'macr',   'deg',    'plusmn',
-    'acute',   'micro',  'para',    'middot', 'cedil',  'ordm',
-    'raquo',   'iquest', 'Agrave',  'Aacute', 'Acirc',  'Atilde',
-    'Auml',    'Aring',  'AElig',   'Ccedil', 'Egrave', 'Eacute',
-    'Ecirc',   'Euml',   'Igrave',  'Iacute', 'Icirc',  'Iuml',
-    'ETH',     'Ntilde', 'Ograve',  'Oacute', 'Ocirc',  'Otilde',
-    'Ouml',    'times',  'Oslash',  'Ugrave', 'Uacute', 'Ucirc',
-    'Uuml',    'Yacute', 'THORN',   'szlig',  'agrave', 'aacute',
-    'acirc',   'atilde', 'auml',    'aring',  'aelig',  'ccedil',
-    'egrave',  'eacute', 'ecirc',   'euml',   'igrave', 'iacute',
-    'icirc',   'iuml',   'eth',     'ntilde', 'ograve', 'oacute',
-    'ocirc',   'otilde', 'ouml',    'divide', 'oslash', 'ugrave',
-    'uacute',  'ucirc',  'uuml',    'yacute', 'thorn',  'yuml',
-    'quot',    'amp',    'lt',      'gt',     'apos',   'OElig',
-    'oelig',   'Scaron', 'scaron',  'Yuml',   'circ',   'tilde',
-    'ensp',    'emsp',   'thinsp',  'zwnj',   'zwj',    'lrm',
-    'rlm',     'ndash',  'mdash',   'lsquo',  'rsquo',  'sbquo',
-    'ldquo',   'rdquo',  'bdquo',   'dagger', 'Dagger', 'permil',
-    'lsaquo',  'rsaquo', 'euro',    'fnof',   'Alpha',  'Beta',
-    'Gamma',   'Delta',  'Epsilon', 'Zeta',   'Eta',    'Theta',
-    'Iota',    'Kappa',  'Lambda',  'Mu',     'Nu',     'Xi',
-    'Omicron', 'Pi',     'Rho',     'Sigma',  'Tau',    'Upsilon',
-    'Phi',     'Chi',    'Psi',     'Omega',  'alpha',  'beta',
-    'gamma',   'delta',  'epsilon', 'zeta',   'eta',    'theta',
-    'iota',    'kappa',  'lambda',  'mu',     'nu',     'xi',
-    'omicron', 'pi',     'rho',     'sigmaf', 'sigma',  'tau',
-    'upsilon', 'phi',    'chi',     'psi',    'omega',  'thetasym',
-    'upsih',   'piv',    'bull',    'hellip', 'prime',  'Prime',
-    'oline',   'frasl',  'weierp',  'image',  'real',   'trade',
-    'alefsym', 'larr',   'uarr',    'rarr',   'darr',   'harr',
-    'crarr',   'lArr',   'uArr',    'rArr',   'dArr',   'hArr',
-    'forall',  'part',   'exist',   'empty',  'nabla',  'isin',
-    'notin',   'ni',     'prod',    'sum',    'minus',  'lowast',
-    'radic',   'prop',   'infin',   'ang',    'and',    'or',
-    'cap',     'cup',    'int',     'sim',    'cong',   'asymp',
-    'ne',      'equiv',  'le',      'ge',     'sub',    'sup',
-    'nsub',    'sube',   'supe',    'oplus',  'otimes', 'perp',
-    'sdot',    'lceil',  'rceil',   'lfloor', 'rfloor', 'lang',
-    'rang',    'loz',    'spades',  'clubs',  'hearts', 'diams',
-);
-
 /**
  * Filters content and keeps only allowable HTML elements.
  *
@@ -709,7 +665,49 @@ function kses_normalize_entities($string)
  * @return string Correctly encoded entity
  */
 function kses_named_entities($matches) {
-    global $kses_allowedentitynames;
+    $kses_allowedentitynames = [
+        'nbsp',    'iexcl',  'cent',    'pound',  'curren', 'yen',
+        'brvbar',  'sect',   'uml',     'copy',   'ordf',   'laquo',
+        'not',     'shy',    'reg',     'macr',   'deg',    'plusmn',
+        'acute',   'micro',  'para',    'middot', 'cedil',  'ordm',
+        'raquo',   'iquest', 'Agrave',  'Aacute', 'Acirc',  'Atilde',
+        'Auml',    'Aring',  'AElig',   'Ccedil', 'Egrave', 'Eacute',
+        'Ecirc',   'Euml',   'Igrave',  'Iacute', 'Icirc',  'Iuml',
+        'ETH',     'Ntilde', 'Ograve',  'Oacute', 'Ocirc',  'Otilde',
+        'Ouml',    'times',  'Oslash',  'Ugrave', 'Uacute', 'Ucirc',
+        'Uuml',    'Yacute', 'THORN',   'szlig',  'agrave', 'aacute',
+        'acirc',   'atilde', 'auml',    'aring',  'aelig',  'ccedil',
+        'egrave',  'eacute', 'ecirc',   'euml',   'igrave', 'iacute',
+        'icirc',   'iuml',   'eth',     'ntilde', 'ograve', 'oacute',
+        'ocirc',   'otilde', 'ouml',    'divide', 'oslash', 'ugrave',
+        'uacute',  'ucirc',  'uuml',    'yacute', 'thorn',  'yuml',
+        'quot',    'amp',    'lt',      'gt',     'apos',   'OElig',
+        'oelig',   'Scaron', 'scaron',  'Yuml',   'circ',   'tilde',
+        'ensp',    'emsp',   'thinsp',  'zwnj',   'zwj',    'lrm',
+        'rlm',     'ndash',  'mdash',   'lsquo',  'rsquo',  'sbquo',
+        'ldquo',   'rdquo',  'bdquo',   'dagger', 'Dagger', 'permil',
+        'lsaquo',  'rsaquo', 'euro',    'fnof',   'Alpha',  'Beta',
+        'Gamma',   'Delta',  'Epsilon', 'Zeta',   'Eta',    'Theta',
+        'Iota',    'Kappa',  'Lambda',  'Mu',     'Nu',     'Xi',
+        'Omicron', 'Pi',     'Rho',     'Sigma',  'Tau',    'Upsilon',
+        'Phi',     'Chi',    'Psi',     'Omega',  'alpha',  'beta',
+        'gamma',   'delta',  'epsilon', 'zeta',   'eta',    'theta',
+        'iota',    'kappa',  'lambda',  'mu',     'nu',     'xi',
+        'omicron', 'pi',     'rho',     'sigmaf', 'sigma',  'tau',
+        'upsilon', 'phi',    'chi',     'psi',    'omega',  'thetasym',
+        'upsih',   'piv',    'bull',    'hellip', 'prime',  'Prime',
+        'oline',   'frasl',  'weierp',  'image',  'real',   'trade',
+        'alefsym', 'larr',   'uarr',    'rarr',   'darr',   'harr',
+        'crarr',   'lArr',   'uArr',    'rArr',   'dArr',   'hArr',
+        'forall',  'part',   'exist',   'empty',  'nabla',  'isin',
+        'notin',   'ni',     'prod',    'sum',    'minus',  'lowast',
+        'radic',   'prop',   'infin',   'ang',    'and',    'or',
+        'cap',     'cup',    'int',     'sim',    'cong',   'asymp',
+        'ne',      'equiv',  'le',      'ge',     'sub',    'sup',
+        'nsub',    'sube',   'supe',    'oplus',  'otimes', 'perp',
+        'sdot',    'lceil',  'rceil',   'lfloor', 'rfloor', 'lang',
+        'rang',    'loz',    'spades',  'clubs',  'hearts', 'diams',
+    ];
 
     if ( empty($matches[1]) )
         return '';

dce1054408a787de80c1adf46e4d148283892aff
Christian Beeznest <christian.fasanando@beeznest.com>
2025-06-30 15:01:55 -0500

    Learnpath: Add automatic switch to next step in learning path, only if video - refs #2512

---
+++ public/main/lp/lp_video_view.php
@@ -0,0 +1,46 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+use Chamilo\CourseBundle\Entity\CDocument;
+use ChamiloSession as Session;
+require_once __DIR__.'/../inc/global.inc.php';
+
+api_protect_course_script();
+
+$lpId = (int) ($_GET['lp_id'] ?? 0);
+$lpItemId = (int) ($_GET['lp_item_id'] ?? 0);
+$autostart = 'true';
+
+if (empty($lpId) || empty($lpItemId)) {
+    api_not_allowed();
+}
+
+/** @var learnpath $oLP */
+$oLP = Session::read('oLP');
+
+$lpItem = $oLP->items[$lpItemId] ?? null;
+
+if (!$lpItem) {
+    echo get_lang('Invalid LP Item');
+    exit;
+}
+
+$documentIid = (int) $lpItem->get_path();
+
+if (empty($documentIid)) {
+    echo get_lang('Document not found');
+    exit;
+}
+
+$em = Database::getManager();
+$document = $em->getRepository(CDocument::class)
+    ->find($documentIid);
+
+if (!$document instanceof CDocument) {
+    echo get_lang('Document not found');
+    exit;
+}
+
+$html = $oLP->getVideoPlayer($document, $autostart);
+
+echo $html;

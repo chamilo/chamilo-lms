d988266d6538280e5d83ab9b11dc05d65fef2f21
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-07-03 09:28:16 -0500

    Internal: Add AzureSyncState entity
    Allows saving the last state of synchronization by commands

---
+++ src/CoreBundle/Repository/AzureSyncStateRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Chamilo\CoreBundle\Repository;
+
+use Chamilo\CoreBundle\Entity\AzureSyncState;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<AzureSyncState>
+ */
+class AzureSyncStateRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AzureSyncState::class);
+    }
+
+    public function save(string $title, string $value): void
+    {
+        $em = $this->getEntityManager();
+
+        $state = $this->findOneBy(['title' => $title]);
+
+        if (!$state) {
+            $state = new AzureSyncState();
+            $state->setTitle($title);
+
+            $em->persist($state);
+        }
+
+        $state->setValue($value);
+
+        $em->flush();
+    }
+}

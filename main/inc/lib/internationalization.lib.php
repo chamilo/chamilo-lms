aff71f39059fad28b1c1852e1e62244701d8b2f3
Nicolas Ducoulombier <nicolas@ldd.fr>
2025-07-08 23:11:55 +0200

    Internal: fix encoding error with ' in language variables - refs BT#22763

--- main/inc/lib/internationalization.lib.php
+++ main/inc/lib/internationalization.lib.php
@@ -1207,7 +1207,7 @@ function api_htmlentities($string, $quote_style = ENT_COMPAT, $encoding = 'UTF-8
             break;
     }
 
-    return htmlentities($string, ENT_SUBSTITUTE, 'UTF-8');
+    return mb_convert_encoding($string, 'HTML-ENTITIES', 'UTF-8');
 }
 
 /**

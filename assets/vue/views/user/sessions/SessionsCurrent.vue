7bcc30e98623e472592a2e81e4efa678359cc54e
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-07-09 14:15:35 -0500

    Session: Display: Add load pagination for the session list page - refs BT#22668

--- assets/vue/views/user/sessions/SessionsCurrent.vue
+++ assets/vue/views/user/sessions/SessionsCurrent.vue
@@ -2,30 +2,27 @@
   <StickyCourses />
   <SessionTabs class="mb-4" />
 
-  <div class="relative min-h-[300px]">
+  <div class="relative grow">
     <Loading :visible="!isFullyLoaded" />
 
-    <SessionsLoading :is-loading="isLoading" />
-
     <SessionCategoryView
-      v-if="!isLoading"
       :categories="categories"
       :categories-with-sessions="categoriesWithSessions"
       :uncategorized-sessions="uncategorizedSessions"
     />
   </div>
+  <div ref="sentinel"></div>
 </template>
 
 <script setup>
-import { ref, watch, nextTick } from "vue"
+import { ref, watch, nextTick, onMounted, onUnmounted } from "vue"
 import SessionTabs from "../../../components/session/SessionTabs.vue"
 import StickyCourses from "../../../views/user/courses/StickyCourses.vue"
 import SessionCategoryView from "../../../components/session/SessionCategoryView"
 import { useSession } from "./session"
-import SessionsLoading from "./SessionsLoading.vue"
 import Loading from "../../../components/Loading.vue"
 
-const { isLoading, uncategorizedSessions, categories, categoriesWithSessions } = useSession("current")
+const { isLoading, uncategorizedSessions, categories, categoriesWithSessions, reload } = useSession("current")
 const isFullyLoaded = ref(false)
 
 watch(isLoading, async (newVal) => {
@@ -33,6 +30,37 @@ watch(isLoading, async (newVal) => {
     await new Promise((resolve) => setTimeout(resolve, 500))
     await nextTick()
     isFullyLoaded.value = true
+  }
+})
+
+let observer = null
+const sentinel = ref(null)
+
+const initObserver = () => {
+  observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting && !isLoading.value) {
+        reload()
+      }
+    },
+    {
+      rootMargin: "0px",
+      threshold: 1.0,
+    },
+  )
+
+  if (sentinel.value) {
+    observer.observe(sentinel.value)
+  }
+}
+
+onMounted(() => {
+  initObserver()
+})
+
+onUnmounted(() => {
+  if (observer && sentinel.value) {
+    observer.unobserve(sentinel.value)
   }
 })
 </script>
--- assets/vue/views/user/sessions/session.js
+++ assets/vue/views/user/sessions/session.js
@@ -10,12 +10,21 @@ export function useSession(type) {
 
   const uncategorizedSessions = ref([])
   const categories = ref([])
-  const categoriesWithSessions = ref([])
+  const categoriesWithSessions = ref(new Map())
+
+  let paginationParams = {
+    page: 1,
+    itemsPerPage: 1,
+  }
 
   function getUncategorizedSessions(sessions) {
     return sessions.filter((session) => isEmpty(session.category))
   }
 
+  /**
+   * @param {Object[]} sessions
+   * @returns {Object[]}
+   */
   function getCategories(sessions) {
     let categoryList = []
 
@@ -58,21 +67,37 @@ export function useSession(type) {
     return categoriesIn
   }
 
-  if (securityStore.isAuthenticated) {
-    isLoading.value = true
+  async function getSessions() {
+    if (securityStore.isAuthenticated) {
+      isLoading.value = true
 
-    sessionService
-      .findUserSubscriptions(securityStore.user["@id"], type)
-      .then(({ items }) => {
-        uncategorizedSessions.value = getUncategorizedSessions(items)
-        categories.value = getCategories(items)
+      try {
+        const { items, nextPageParams } = await sessionService.findUserSubscriptions(securityStore.user["@id"], type, {
+          ...paginationParams,
+        })
+
+        paginationParams = nextPageParams ? { ...nextPageParams } : null
+
+        uncategorizedSessions.value.push(...getUncategorizedSessions(items))
+        categories.value.push(...getCategories(items))
         categoriesWithSessions.value = getCategoriesWithSessions(items)
-      })
-      .finally(() => (isLoading.value = false))
+      } finally {
+        isLoading.value = false
+      }
+    }
   }
+
+  async function reload() {
+    if (null !== paginationParams) {
+      await getSessions()
+    }
+  }
+
+  getSessions().then(() => {})
 
   return {
     isLoading,
+    reload,
     uncategorizedSessions,
     categories,
     categoriesWithSessions,

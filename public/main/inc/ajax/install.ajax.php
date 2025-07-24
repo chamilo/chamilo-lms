1f25b2a8be8d848df572db9e04ca1e2dd5f7e8da
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-07-24 18:13:05 -0500

    Refactor contact information handling in install.ajax.php

--- public/main/inc/ajax/install.ajax.php
+++ public/main/inc/ajax/install.ajax.php
@@ -21,71 +21,69 @@ $action = $request->query->getString('a');
 
 switch ($action) {
     case 'send_contact_information':
-        if ($request->isXmlHttpRequest()) {
-            // get params from contact form
-            $person_name = $request->request->get('person_name');
-            $person_email = $request->request->get('person_email');
-            $person_role = $request->request->get('person_role');
-            $financial_decision = $request->request->get('financial_decision');
-            $contact_language = $request->request->get('language');
-            $company_name = $request->request->get('company_name');
-            $company_activity = $request->request->get('company_activity');
-            $company_country = $request->request->get('company_country');
-            $company_city = $request->request->get('company_city');
+        // get params from contact form
+        $person_name = $request->request->get('person_name');
+        $person_email = $request->request->get('person_email');
+        $person_role = $request->request->get('person_role');
+        $financial_decision = $request->request->get('financial_decision');
+        $contact_language = $request->request->get('language');
+        $company_name = $request->request->get('company_name');
+        $company_activity = $request->request->get('company_activity');
+        $company_country = $request->request->get('company_country');
+        $company_city = $request->request->get('company_city');
 
-            // validating required fields
-            $a_required_fields = [$person_name, $person_role, $company_name, $company_activity, $company_country];
-            $required_field_error = false;
-            foreach ($a_required_fields as $required_file) {
-                if ('' === trim($required_file)) {
-                    $required_field_error = true;
-                    break;
+        // validating required fields
+        $a_required_fields = [$person_name, $person_role, $company_name, $company_activity, $company_country];
+        $required_field_error = false;
+        foreach ($a_required_fields as $required_file) {
+            if ('' === trim($required_file)) {
+                $required_field_error = true;
+                break;
+            }
+        }
+
+        // Return error if any of the required fields is empty
+        if ($required_field_error) {
+            echo 'required_field_error';
+            break;
+        } else {
+            // save contact information with web service
+            // create a client
+
+            $url = 'https://version.chamilo.org/contactv2.php';
+            $options = [
+                'verify' => false,
+            ];
+
+            $urlValidated = false;
+            try {
+                $client = new Client();
+                $res = $client->request('GET', $url, $options);
+                if ('200' == $res->getStatusCode() || '301' == $res->getStatusCode()) {
+                    $urlValidated = true;
                 }
+            } catch (Exception|GuzzleException $e) {
+                error_log("Could not check $url from ".__FILE__);
+                break;
             }
 
-            // Return error if any of the required fields is empty
-            if ($required_field_error) {
-                echo 'required_field_error';
-                break;
-            } else {
-                // save contact information with web service
-                // create a client
+            $data = [
+                'person_name' => $person_name,
+                'person_email' => $person_email,
+                'person_role' => $person_role,
+                'financial_decision' => $financial_decision,
+                'contact_language' => $contact_language,
+                'company_name' => $company_name,
+                'company_activity' => $company_activity,
+                'company_country' => $company_country,
+                'company_city' => $company_city,
+            ];
 
-                $url = 'https://version.chamilo.org/contactv2.php';
-                $options = [
-                    'verify' => false,
-                ];
-
-                $urlValidated = false;
-                try {
-                    $client = new Client();
-                    $res = $client->request('GET', $url, $options);
-                    if ('200' == $res->getStatusCode() || '301' == $res->getStatusCode()) {
-                        $urlValidated = true;
-                    }
-                } catch (Exception|GuzzleException $e) {
-                    error_log("Could not check $url from ".__FILE__);
-                    break;
-                }
-
-                $data = [
-                    'person_name' => $person_name,
-                    'person_email' => $person_email,
-                    'person_role' => $person_role,
-                    'financial_decision' => $financial_decision,
-                    'contact_language' => $contact_language,
-                    'company_name' => $company_name,
-                    'company_activity' => $company_activity,
-                    'company_country' => $company_country,
-                    'company_city' => $company_city,
-                ];
-
-                $client = new Client();
-                $options['query'] = $data;
-                $res = $client->request('GET', $url, $options);
-                if ('200' == $res->getStatusCode()) {
-                    echo '1';
-                }
+            $client = new Client();
+            $options['query'] = $data;
+            $res = $client->request('GET', $url, $options);
+            if ('200' == $res->getStatusCode()) {
+                echo '1';
             }
         }
         break;

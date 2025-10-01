a98292e5a48aef8d74ab0870ad619e4576538a9e
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-10-01 13:35:41 -0500

    Portfolio: Fix migration priorities

--- src/CoreBundle/Migrations/Schema/V200/Version20250927180001.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20250927180001.php
@@ -14,6 +14,7 @@ use Chamilo\CoreBundle\Repository\Node\PortfolioRepository;
 use Chamilo\CoreBundle\Repository\Node\UserRepository;
 use Chamilo\CoreBundle\Repository\ResourceRepository;
 use DateTime;
+use DateTimeZone;
 use Doctrine\DBAL\Schema\Schema;
 use Exception;
 
@@ -39,17 +40,14 @@ class Version20250927180001 extends AbstractMigrationChamilo
 
         foreach ($portfolioRows as $portfolioRow) {
             $portfolioId = $portfolioRow['id'];
-            $creatorId = $portfolioRow['user_id'];
-            $courseId = $portfolioRow['c_id'];
-            $sessionId = $portfolioRow['session_id'];
             $creationDate = new DateTime($portfolioRow['creation_date']);
             $updateDate = new DateTime($portfolioRow['update_date']);
 
             /** @var Portfolio $portfolio */
             $portfolio = $portfolioRepo->find($portfolioId);
-            $creator = $userRepo->find($creatorId);
-            $course = $courseId ? $this->findCourse($courseId) : null;
-            $session = $sessionId ? $this->findSession($sessionId) : null;
+            $creator = $userRepo->find($portfolioRow['user_id']);
+            $course = $portfolioRow['c_id'] ? $this->findCourse($portfolioRow['c_id']) : null;
+            $session = $portfolioRow['session_id'] ? $this->findSession($portfolioRow['session_id']) : null;
 
             $portfolio->setParent($creator);
 
@@ -68,13 +66,20 @@ class Version20250927180001 extends AbstractMigrationChamilo
 
             $this->entityManager->flush();
 
-            if ($course) {
-                $resourceVisibility = match ($portfolio->getVisibility()) {
-                    Portfolio::VISIBILITY_HIDDEN => ResourceLink::VISIBILITY_DRAFT,
-                    Portfolio::VISIBILITY_VISIBLE => ResourceLink::VISIBILITY_PUBLISHED,
-                    default => ResourceLink::VISIBILITY_PENDING,
-                };
+            $resourceVisibility = match ($portfolio->getVisibility()) {
+                Portfolio::VISIBILITY_HIDDEN => ResourceLink::VISIBILITY_DRAFT,
+                Portfolio::VISIBILITY_VISIBLE => ResourceLink::VISIBILITY_PUBLISHED,
+                default => ResourceLink::VISIBILITY_PENDING,
+            };
 
+            $itemsProperty = $this->connection
+                ->executeQuery(
+                    "SELECT * FROM c_item_property
+                        WHERE tool = 'portfolio' AND ref = $portfolioId"
+                )
+                ->fetchAllAssociative();
+
+            if (empty($itemsProperty) && $course) {
                 $courseLink = $portfolio->addCourseLink(
                     $course,
                     $session,
@@ -85,44 +90,43 @@ class Version20250927180001 extends AbstractMigrationChamilo
                 );
 
                 $this->entityManager->persist($courseLink);
-                $this->entityManager->flush();
-
-
-                $itemsProperty = $this->connection
-                    ->executeQuery(
-                        "SELECT * FROM c_item_property
-                        WHERE tool = 'portfolio' AND ref = $portfolioId"
-                    )
-                    ->fetchAllAssociative();
-
+            } else {
                 foreach ($itemsProperty as $itemProperty) {
-                    if (empty($itemProperty['to_user_id'])) {
-                        continue;
+                    $course = $itemProperty['c_id'] ? $this->findCourse($itemProperty['c_id']) : null;
+                    $session = $itemProperty['session_id'] ? $this->findSession($itemProperty['session_id']) : null;
+                    $toUser = $itemProperty['to_user_id'] ? $userRepo->find($itemProperty['to_user_id']) : null;
+                    $insertDate = new DateTime($itemProperty['insert_date'], new DateTimeZone('UTC'));
+                    $editDate = new DateTime($itemProperty['lastedit_date'], new DateTimeZone('UTC'));
+
+                    $resourceNode->setUpdatedAt($editDate);
+
+                    if ($toUser) {
+                        $userLink = $portfolio->addUserLink(
+                            $toUser,
+                            $course,
+                            $session,
+                            null,
+                            $insertDate,
+                            $editDate
+                        );
+
+                        $this->entityManager->persist($userLink);
+                    } else {
+                        $courseLink = $portfolio->addCourseLink(
+                            $course,
+                            $session,
+                            null,
+                            $resourceVisibility,
+                            $creationDate,
+                            $updateDate
+                        );
+
+                        $this->entityManager->persist($courseLink);
                     }
-
-                    /** @var User|null $toUser */
-                    $toUser = $userRepo->find($itemProperty['to_user_id']);
-
-                    if (!$toUser) {
-                        continue;
-                    }
-
-                    $insertDate = new DateTime($itemProperty['insert_date']);
-                    $lastEditDate = new DateTime($itemProperty['lastedit_date']);
-
-                    $userLink = $portfolio->addUserLink(
-                        $toUser,
-                        $course,
-                        $session,
-                        null,
-                        $insertDate,
-                        $lastEditDate
-                    );
-
-                    $this->entityManager->persist($userLink);
-                    $this->entityManager->flush();
                 }
             }
+
+            $this->entityManager->flush();
         }
     }
 }

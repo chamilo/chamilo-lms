8bc08cced7bd558414a605ee544084eec5729143
Yannick Warnier <ywarnier@beeznest.org>
2025-10-03 09:53:49 +0200

    Minor: Internal: Fix coding standard

--- src/CoreBundle/Migrations/Schema/V200/Version20251001120000.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20251001120000.php
@@ -24,19 +24,19 @@ class Version20251001120000 extends AbstractMigrationChamilo
         $tblExternalTool = $schema->getTable('lti_external_tool');
 
         if (!$tblExternalTool->hasForeignKey('FK_DB0E04E41BAD783F')) {
-            $this->addSql("ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E41BAD783F FOREIGN KEY (resource_node_id) REFERENCES resource_node (id) ON DELETE CASCADE");
+            $this->addSql('ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E41BAD783F FOREIGN KEY (resource_node_id) REFERENCES resource_node (id) ON DELETE CASCADE');
         }
 
         if (!$tblExternalTool->hasForeignKey('FK_DB0E04E491D79BD3')) {
-            $this->addSql("ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E491D79BD3 FOREIGN KEY (c_id) REFERENCES course (id)");
+            $this->addSql('ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E491D79BD3 FOREIGN KEY (c_id) REFERENCES course (id)');
         }
 
         if (!$tblExternalTool->hasForeignKey('FK_DB0E04E482F80D8B')) {
-            $this->addSql("ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E482F80D8B FOREIGN KEY (gradebook_eval_id) REFERENCES gradebook_evaluation (id) ON DELETE SET NULL");
+            $this->addSql('ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E482F80D8B FOREIGN KEY (gradebook_eval_id) REFERENCES gradebook_evaluation (id) ON DELETE SET NULL');
         }
 
         if (!$tblExternalTool->hasForeignKey('FK_DB0E04E4727ACA70')) {
-            $this->addSql("ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E4727ACA70 FOREIGN KEY (parent_id) REFERENCES lti_external_tool (id)");
+            $this->addSql('ALTER TABLE lti_external_tool ADD CONSTRAINT FK_DB0E04E4727ACA70 FOREIGN KEY (parent_id) REFERENCES lti_external_tool (id)');
         }
     }
 }

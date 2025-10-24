c1b659b04aeb40c0374481014977ba5849ab7297
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-10-24 10:31:39 -0500

    Auth: Filter user access by their access url #4328

---
+++ src/CoreBundle/EventListener/UserAccessUrlListener.php
@@ -0,0 +1,46 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
+
+namespace Chamilo\CoreBundle\EventListener;
+
+use Chamilo\CoreBundle\Entity\User;
+use Chamilo\CoreBundle\Helpers\AccessUrlHelper;
+use Chamilo\CoreBundle\Repository\Node\AccessUrlRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+
+final readonly class UserAccessUrlListener
+{
+    public function __construct(
+        private AccessUrlHelper $accessUrlHelper,
+        private AccessUrlRepository $accessUrlRepo,
+    ) {}
+
+    public function __invoke(CheckPassportEvent $event): void
+    {
+        $passport = $event->getPassport();
+        $user = $passport->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $currentAccessUrl = $this->accessUrlHelper->getCurrent();
+
+        try {
+            $isAllowed = $this->accessUrlRepo->isUrlActiveForUser($currentAccessUrl, $user);
+        } catch (NoResultException|NonUniqueResultException $e) {
+            throw new CustomUserMessageAuthenticationException('Unable to verify user authentication');
+        }
+
+        if (!$isAllowed) {
+            throw new CustomUserMessageAuthenticationException('Account inactive for this URL');
+        }
+    }
+}

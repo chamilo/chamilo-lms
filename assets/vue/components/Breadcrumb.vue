dd6457d7236528fc9f4755550f0aa9eeaad35a43
Yannick Warnier <ywarnier@users.noreply.github.com>
2025-10-28 23:32:48 +0100

    Minor: Fix spacing typo

--- assets/vue/components/Breadcrumb.vue
+++ assets/vue/components/Breadcrumb.vue
@@ -6,7 +6,8 @@
     <Breadcrumb :model="itemList">
       <template #item="{ item, props }">
         <BaseAppLink
-          v-if="(item.route || item.url) && item !== itemList[itemList.length - 1]"          :to="item.route"
+          v-if="(item.route || item.url) && item !== itemList[itemList.length - 1]"
+          :to="item.route"
           :url="item.url"
           v-bind="props.action"
           @click="handleBreadcrumbClick(item)"

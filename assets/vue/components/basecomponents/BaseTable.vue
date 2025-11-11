93a4bb80d1b7b3c12d4f1754a2bd46fbe31bed0f
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-11-11 18:11:47 -0500

    Admin: Allow filtering users when assigning auth sources - refs BT#22986

--- assets/vue/components/basecomponents/BaseTable.vue
+++ assets/vue/components/basecomponents/BaseTable.vue
@@ -129,7 +129,7 @@ if (rows.value == null) {
   rows.value = initialRows()
 }
 
-defineEmits(["page", "sort"])
+defineEmits(["filter", "page", "sort"])
 
 const slots = useSlots()
 
@@ -165,6 +165,7 @@ defineExpose({
     responsive-layout="scroll"
     size="small"
     striped-rows
+    @filter="$emit('filter', $event)"
     @page="$emit('page', $event)"
     @sort="$emit('sort', $event)"
   >

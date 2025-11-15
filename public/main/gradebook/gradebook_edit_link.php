fee8c87f119cf736f9523aa31296357b4aad293f
Christian Beeznest <christian.fasanando@beeznest.com>
2025-11-15 16:11:04 -0500

    Gradebook: Fix flat view totals with hidden zero-weight categories - refs #3504

--- public/main/gradebook/gradebook_edit_link.php
+++ public/main/gradebook/gradebook_edit_link.php
@@ -126,7 +126,7 @@ $(function() {
        $("#hide_category_id option:selected").each(function () {
            var cat_id = $(this).val();
             $.ajax({
-                url: "'.api_get_path(WEB_AJAX_PATH).'gradebook.ajax.php?a=get_gradebook_weight",
+                url: "'.api_get_path(WEB_AJAX_PATH).'gradebook.ajax.php?'.api_get_cidreq().'&a=get_gradebook_weight",
                 data: "cat_id="+cat_id,
                 success: function(return_value) {
                     if (return_value != 0 ) {

29e21dab4b0fa1142a1be4217ae08a04a76a5ee6
Christian Beeznest <christian.fasanando@beeznest.com>
2025-11-24 13:25:31 -0500

    Forum: Enable thread grading settings - refs #7039

--- public/main/forum/forumfunction.inc.php
+++ public/main/forum/forumfunction.inc.php
@@ -2405,25 +2405,18 @@ function newThread(CForum $forum, $form_values = '', $showPreview = true)
         [] // extra data);
     );
 
-    if (Gradebook::is_active() &&
-        (api_is_course_admin() || api_is_session_general_coach() || api_is_course_tutor())
-    ) {
+    if ((api_is_course_admin() || api_is_session_general_coach() || api_is_course_tutor())) {
         $form->addElement('advanced_settings', 'advanced_params', get_lang('Advanced settings'));
         $form->addElement('html', '<div id="advanced_params_options" style="display:none">');
 
         // Thread qualify
-        if (Gradebook::is_active()) {
-            //Loading gradebook select
-            GradebookUtils::load_gradebook_select_in_tool($form);
-            $form->addCheckBox(
-                'thread_qualify_gradebook',
-                '',
-                get_lang('Grade this thread'),
-                ['onclick' => 'javascript:if(this.checked==true){document.getElementById(\'options_field\').style.display = \'block\';}else{document.getElementById(\'options_field\').style.display = \'none\';}']
-            );
-        } else {
-            $form->addElement('hidden', 'thread_qualify_gradebook', false);
-        }
+        GradebookUtils::load_gradebook_select_in_tool($form);
+        $form->addCheckBox(
+            'thread_qualify_gradebook',
+            '',
+            get_lang('Grade this thread'),
+            ['onclick' => 'javascript:if(this.checked==true){document.getElementById(\'options_field\').style.display = \'block\';}else{document.getElementById(\'options_field\').style.display = \'none\';}']
+        );
 
         $form->addElement('html', '<div id="options_field" style="display:none">');
         $form->addElement('text', 'numeric_calification', get_lang('Maximum score'));

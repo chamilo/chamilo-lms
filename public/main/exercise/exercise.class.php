e3347e709aee8ae4176424a32921fd04c3539b41
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2025-12-10 22:46:17 -0500

    Exercise: Fix CSS class for exercise buttons wrapper - refs BT#22293
    And fix events from record_audio.js to block buttons

--- public/main/exercise/exercise.class.php
+++ public/main/exercise/exercise.class.php
@@ -8340,7 +8340,7 @@ class Exercise
             }
 
             echo '<div class="row"><div class="pull-right">'.$paginationCounter.'</div></div>';
-            echo Display::div($exercise_actions, ['class' => 'form-actions']);
+            echo Display::div($exercise_actions, ['class' => 'exercise_actions']);
             echo '</div>';
         }
     }

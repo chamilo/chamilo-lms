b22c7b6f6a9c3e7d703a2454a6b3ecda82ef5f85
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-01-12 13:46:33 -0500

    Display: Update `LAYOUT_BOX` to use `flex-col` for improved vertical alignment

--- public/main/inc/lib/formvalidator/FormValidator.class.php
+++ public/main/inc/lib/formvalidator/FormValidator.class.php
@@ -56,7 +56,7 @@ class FormValidator extends HTML_QuickForm
                 $attributes['class'] = 'flex flex-row gap-3 items-center ';
                 break;
             case self::LAYOUT_BOX:
-                $attributes['class'] = 'ch flex gap-1 ';
+                $attributes['class'] = 'ch flex flex-col gap-1 ';
                 break;
             case self::LAYOUT_GRID:
                 $attributes['class'] = 'ch form-grid';

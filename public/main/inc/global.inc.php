ae1dd2f8af7bcc01355dabf998c405763ab8d28a
Yannick Warnier <ywarnier@beeznest.org>
2026-01-16 12:34:27 +0100

    Internal: Add global code for legacy to close session if AJAX request and no KEEP_SESSION_OPEN flag - refs #6858

--- public/main/inc/global.inc.php
+++ public/main/inc/global.inc.php
@@ -199,6 +199,13 @@ if ($isCli) {
         }
     }
 
+    if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
+        // Check if the script has explicitly asked to keep the session open
+        if (!defined('KEEP_SESSION_OPEN')) {
+            session_write_close();
+        }
+    }
+
     $charset = 'UTF-8';
     ini_set('log_errors', '1');
     $this_section = SECTION_GLOBAL;

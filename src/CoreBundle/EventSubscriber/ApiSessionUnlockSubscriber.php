af7e511f97267dcc5b9fa65b0907cfa8a6b77778
Yannick Warnier <ywarnier@beeznest.org>
2026-01-17 17:30:41 +0100

    Internal: Add ApiSessionUnlockSubscriber to reduce the time holding the lock on a session file in API calls - refs #6858

---
+++ src/CoreBundle/EventSubscriber/ApiSessionUnlockSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Chamilo\CoreBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * This subscriber unlocks the session after an API call,
+ * to reduce the likeliness of a session lock that would slow down or block other API calls.
+ * See GH#6858.
+ */
+class ApiSessionUnlockSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // We listen on KernelEvents::REQUEST
+            // Priority 4 is usually after Security but before API Platform starts its logic
+            KernelEvents::REQUEST => [['unlockSession', 4]],
+        ];
+    }
+
+    public function unlockSession(RequestEvent $event): void
+    {
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        // Target only API calls
+        if (!str_starts_with($request->getPathInfo(), '/api/')) {
+            return;
+        }
+
+        // Target only GET requests (safe to read-only)
+        if ($request->isMethod('GET')) {
+            if ($request->hasSession() && $request->getSession()->isStarted()) {
+                // This writes and closes the session file handle
+                $request->getSession()->save();
+            }
+        }
+    }
+}

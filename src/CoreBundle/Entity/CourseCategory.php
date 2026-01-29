e871e55c3a2b0519531367b605af377c46108b76
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-01-29 15:44:58 -0500

    Internal: API: Define granular CRUD operations for CourseCategory API resource with role-based security

--- src/CoreBundle/Entity/CourseCategory.php
+++ src/CoreBundle/Entity/CourseCategory.php
@@ -10,8 +10,12 @@ use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Link;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\Serializer\Filter\PropertyFilter;
 use Chamilo\CoreBundle\Repository\CourseCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -23,13 +27,19 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(security: "is_granted('ROLE_ADMIN')"),
+        new Delete(security: "is_granted('ROLE_ADMIN')"),
+        new Patch(security: "is_granted('ROLE_ADMIN')"),
+    ],
     normalizationContext: [
         'groups' => ['course_category:read', 'course:read'],
     ],
     denormalizationContext: [
         'groups' => ['course_category:write', 'course:write'],
     ],
-    security: "is_granted('ROLE_ADMIN')"
 )]
 #[ORM\Table(name: 'course_category')]
 #[ORM\Index(columns: ['parent_id'], name: 'parent_id')]

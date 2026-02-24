1ce0dd8a5da33f6bead65451b423c4185060cec4
Yannick Warnier <ywarnier@beeznest.org>
2026-02-24 01:09:27 +0100

    Learnpath: Remove max score checking and fix other code syntax issues - refs #6745

--- main/lp/learnpathItem.class.php
+++ main/lp/learnpathItem.class.php
@@ -2386,12 +2386,17 @@ class learnpathItem
                                                     /** @var learnpathItem $myItemToCheck */
                                                     $myItemToCheck = $items[$refs_list[$this->get_id()]];
                                                     $minScore = $myItemToCheck->getPrerequisiteMinScore();
-                                                    $maxScore = $myItemToCheck->getPrerequisiteMaxScore();
+                                                    if (empty($minScore)) {
+                                                        // Try with mastery_score
+                                                        $masteryScoreAsMin = $myItemToCheck->get_mastery_score();
+                                                        if (!empty($masteryScoreAsMin)) {
+                                                            $minScore = $masteryScoreAsMin;
+                                                        }
+                                                    }
 
-                                                    if (isset($minScore) && isset($minScore)) {
+                                                    if (!empty($minScore)) {
                                                         // Taking min/max prerequisites values see BT#5776
-                                                        if ($quiz['exe_result'] >= $minScore
-                                                        ) {
+                                                        if ($quiz['exe_result'] >= $minScore) {
                                                             $returnstatus = true;
                                                         } else {
                                                             $explanation = sprintf(
@@ -2439,7 +2444,6 @@ class learnpathItem
                                                     /** @var learnpathItem $myItemToCheck */
                                                     $myItemToCheck = $items[$refs_list[$this->get_id()]];
                                                     $minScore = $myItemToCheck->getPrerequisiteMinScore();
-                                                    $maxScore = $myItemToCheck->getPrerequisiteMaxScore();
 
                                                     if (empty($minScore)) {
                                                         // Try with mastery_score
@@ -2448,11 +2452,9 @@ class learnpathItem
                                                             $minScore = $masteryScoreAsMin;
                                                         }
                                                     }
-                                                    if (isset($minScore) && isset($minScore)) {
+                                                    if (!empty($minScore)) {
                                                         // Taking min/max prerequisites values see BT#5776
-                                                        if ($quiz['exe_result'] >= $minScore &&
-                                                            $quiz['exe_result'] <= $maxScore
-                                                        ) {
+                                                        if ($quiz['exe_result'] >= $minScore) {
                                                             $returnstatus = true;
                                                             break;
                                                         } else {

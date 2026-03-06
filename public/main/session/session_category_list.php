b24e71895b589db0d8625242fd866a75c14131e8
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-06 12:23:48 -0500

    Security: Fix authenticated XSS in session categories
    See advisory GHSA-qg5f-gq95-9vhq reported by @elliSzAt

--- public/main/session/session_category_list.php
+++ public/main/session/session_category_list.php
@@ -226,27 +226,30 @@ if (isset($_GET['search']) && 'advanced' === $_GET['search']) {
                 <?php
                 if ($num > $limit) {
                     if ($page) {
-                        ?>
-                        <a href="<?php echo api_get_self(); ?>?page=<?php echo $page
-                            - 1; ?>&sort=<?php echo $sort; ?>&order=<?php echo Security::remove_XSS(
-                            $_REQUEST['order']
-                        ); ?>&keyword=<?php echo Security::remove_XSS($_REQUEST['keyword'] ?? ''); ?><?php echo @$cond_url; ?>">
-                            <?php echo get_lang('Previous'); ?></a>
-                        <?php
+                        echo Display::url(
+                            get_lang('Previous'),
+                            api_get_self().'?'.http_build_query([
+                                'page' => $page - 1,
+                                'sort' => $sort,
+                                'order' => $order,
+                                'keyword' => $keyword,
+                            ]),
+                        );
                     } else {
                         echo get_lang('Previous');
                     } ?>
                     |
                     <?php
                     if ($nbr_results > $limit) {
-                        ?>
-                        <a href="<?php echo api_get_self(); ?>?page=<?php echo $page
-                            + 1; ?>&sort=<?php echo $sort; ?>&order=<?php echo Security::remove_XSS(
-                            $_REQUEST['order']
-                        ); ?>&keyword=<?php echo Security::remove_XSS($_REQUEST['keyword'] ?? ''); ?><?php echo @$cond_url; ?>">
-                            <?php echo get_lang('Next'); ?></a>
-
-                        <?php
+                        echo Display::url(
+                            get_lang('Next'),
+                            api_get_self().'?'.http_build_query([
+                                'page' => $page + 1,
+                                'sort' => $sort,
+                                'order' => $order,
+                                'keyword' => $keyword,
+                            ])
+                        );
                     } else {
                         echo get_lang('Next');
                     }

cf9bd04750a780f0eed0566d9571642c40d0587c
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-11 17:32:37 -0500

    Plugin: ExerciseMonitoring: Fix load of JS #6996

--- public/plugin/ExerciseMonitoring/index.php
+++ public/plugin/ExerciseMonitoring/index.php
@@ -5,20 +5,21 @@
 use Chamilo\CoreBundle\Framework\Container;
 
 $plugin = ExerciseMonitoringPlugin::create();
-$em = Database::getManager();
+$em = Container::getEntityManager();
 
 $request = Container::getRequest();
+$requestRoute = $request->query->get('_route');
 
 $isEnabled = $plugin->isEnabled(true);
-$showOverviewRegion = $isEnabled && str_contains($request->getScriptName(), '/main/exercise/overview.php');
-$showSubmitRegion = $isEnabled && str_contains($request->getScriptName(), '/main/exercise/exercise_submit.php');
+$showOverviewRegion = $isEnabled && '/main/exercise/overview.php' === $requestRoute;
+$showSubmitRegion = $isEnabled && '/main/exercise/exercise_submit.php' === $requestRoute;
 
 $_template['enabled'] = false;
 $_template['show_overview_region'] = $showOverviewRegion;
 $_template['show_submit_region'] = $showSubmitRegion;
 
 if ($showOverviewRegion || $showSubmitRegion) {
-    $exerciseId = (int) $_GET['exerciseId'];
+    $exerciseId = $request->query->getInt('exerciseId');
 
     $objFieldValue = new ExtraFieldValue('exercise');
     $values = $objFieldValue->get_values_by_handler_and_field_variable(
@@ -54,7 +55,7 @@ if ($showSubmitRegion && $_template['enabled']) {
     $exercise = new Exercise(api_get_course_int_id());
 
     if ($exercise->read($_template['exercise_id'])) {
-        $_template['exercise_type'] = (int) $exercise->selectType();
+        $_template['exercise_type'] = $exercise->selectType();
 
         if ('true' === $plugin->get(ExerciseMonitoringPlugin::SETTING_INSTRUCTION_AGE_DISTINCTION_ENABLE)
             && !$isAdult

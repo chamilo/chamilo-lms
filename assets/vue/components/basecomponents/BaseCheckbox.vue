746e7c4656c8cbae80c283bee5f9f5b16ed9c0f8
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-13 17:50:01 -0500

    Display: Simplify `BaseCheckbox` by replacing `proxy` with `v-model` binding and cleaning up props usage #5652

--- assets/vue/components/basecomponents/BaseCheckbox.vue
+++ assets/vue/components/basecomponents/BaseCheckbox.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="field-checkbox">
     <Checkbox
+      v-model="modelValue"
       :binary="true"
       :inputId="id"
       :name="name"
-      v-model="proxy"
     />
-    <label :for="id" class="ml-2 cursor-pointer">{{ label }}</label>
+    <label
+      :for="id"
+      class="ml-2 cursor-pointer"
+      >{{ label }}</label
+    >
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import Checkbox from 'primevue/checkbox'
+import Checkbox from "primevue/checkbox"
 
-const props = defineProps({
-  id: { type: String, required: true },
-  modelValue: { type: Boolean, required: true },
-  name: { type: String, required: true },
-  label: { type: String, required: true },
+const modelValue = defineModel({
+  type: Boolean,
+  required: true,
 })
 
-const emit = defineEmits(['update:modelValue'])
-const proxy = computed({
-  get: () => !!props.modelValue,
-  set: (val) => emit('update:modelValue', !!val),
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
 })
 </script>
-<style scoped>
-.p-checkbox .p-checkbox-box {
-  width: 1.25rem;
-  height: 1.25rem;
-}
-</style>

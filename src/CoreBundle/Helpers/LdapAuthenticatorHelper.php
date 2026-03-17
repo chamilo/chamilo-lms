e0628f27e1cd705a896e26817c1125cf845dbd80
Yannick Warnier <ywarnier@beeznest.org>
2026-03-17 11:00:08 +0100

    Security: Filter LDAP injection possibility in query strings

--- src/CoreBundle/Helpers/LdapAuthenticatorHelper.php
+++ src/CoreBundle/Helpers/LdapAuthenticatorHelper.php
@@ -48,19 +48,19 @@ readonly class LdapAuthenticatorHelper
         ];
 
         if ($keywordUsername) {
-            $ldapQuery[] = "(uid=$keywordUsername)";
+            $ldapQuery[] = '(uid='.ldap_escape($keywordUsername, '', LDAP_ESCAPE_FILTER).')';
         }
 
         if ($keywordLastname) {
-            $ldapQuery[] = "({$dataCorrespondence['lastname']}=$keywordLastname*)";
+            $ldapQuery[] = '('.$dataCorrespondence['lastname'].'='.ldap_escape($keywordLastname, '', LDAP_ESCAPE_FILTER).'*)';
         }
 
         if ($keywordFirstname) {
-            $ldapQuery[] = "({$dataCorrespondence['firstname']}=$keywordFirstname*)";
+            $ldapQuery[] = '('.$dataCorrespondence['firstname'].'='.ldap_escape($keywordFirstname, '', LDAP_ESCAPE_FILTER).'*)';
         }
 
         if ($keywordType && 'all' !== $keywordType) {
-            $ldapQuery[] = "(employeeType=$keywordType)";
+            $ldapQuery[] = '(employeeType='.ldap_escape($keywordType, '', LDAP_ESCAPE_FILTER).')';
         }
 
         $query = \count($ldapQuery) > 1 ? '(& '.implode(' ', $ldapQuery).' )' : $ldapQuery[0];
@@ -85,7 +85,7 @@ readonly class LdapAuthenticatorHelper
 
         return $this->ldap
             ->query(
-                "ou=$ou,".$this->ldapConfig['base_dn'],
+                'ou='.ldap_escape($ou, '', LDAP_ESCAPE_DN).','.$this->ldapConfig['base_dn'],
                 "(objectClass={$this->ldapConfig['object_class']})"
             )
             ->execute()

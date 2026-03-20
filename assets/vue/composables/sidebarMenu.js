1d973800295edd18bd271df9fc1ec613c62e9ee8
Yannick Warnier <ywarnier@beeznest.org>
2026-03-20 11:51:09 +0100

    Display: Maintain highligh on Courses, Admin and Session administrator menu items

--- assets/vue/composables/sidebarMenu.js
+++ assets/vue/composables/sidebarMenu.js
@@ -289,6 +289,7 @@ export function useSidebarMenu() {
           items: courseItems.length > 1 ? courseItems : undefined,
           route: 1 === courseItems.length ? courseItems[0].route : undefined,
           class: courseItems.length > 0 ? courseItems[0].class : "",
+          expanded: courseItems.length > 1 ? isActive({ items: courseItems }) : undefined,
         })
       }
     }
@@ -480,6 +481,7 @@ export function useSidebarMenu() {
         icon: "mdi mdi-account-cog",
         label: t("Session administrator"),
         items: sessionAdminItems,
+        expanded: isActive({ items: sessionAdminItems }),
       })
     }
 
@@ -500,6 +502,7 @@ export function useSidebarMenu() {
           icon: "mdi mdi-cog",
           items: adminItems,
           label: t("Administration"),
+          expanded: isActive({ items: adminItems }),
         })
       }
     }

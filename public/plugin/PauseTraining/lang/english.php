77d2aac24e65aebfb1c4883d9a61dd9092528962
Christian Beeznest <christian.fasanando@beeznest.com>
2026-03-24 09:37:03 -0500

    Plugin: Adapt PauseTraining plugin for C2

--- public/plugin/PauseTraining/lang/english.php
+++ public/plugin/PauseTraining/lang/english.php
@@ -2,17 +2,19 @@
 
 /* For licensing terms, see /license.txt */
 
-$strings['plugin_title'] = "Pause training";
-$strings['plugin_comment'] = "";
-$strings['PauseTraining'] = "Pause training";
-$strings['allow_users_to_edit_pause_formation'] = 'Allow users to edit pause formation';
-$strings['cron_alert_users_if_inactive_days'] = 'Alert users if inactive days (via cron)';
-$strings['PauseFormation'] = 'Pause my formation';
-$strings['StartPauseDateTime'] = 'Start pause date';
-$strings['EndPauseDateTime'] = 'End pause date';
-$strings['DisableEmails'] = 'Disable email notifications';
+$strings['plugin_title'] = 'Pause training';
+$strings['plugin_comment'] = "Adds a user pause period and inactivity reminders based on a cron task.";
+$strings['tool_enable'] = 'Enable plugin';
+$strings['tool_enable_help'] = '';
+$strings['PauseTraining'] = 'Pause training';
+$strings['allow_users_to_edit_pause_formation'] = 'Allow users to edit pause training';
+$strings['cron_alert_users_if_inactive_days'] = 'Alert users after these inactivity days (cron)';
+$strings['PauseFormation'] = 'Pause my training';
+$strings['StartPauseDateTime'] = 'Pause start date';
+$strings['EndPauseDateTime'] = 'Pause end date';
+$strings['DisableEmails'] = 'Disable inactivity notifications';
 $strings['InactivityXDays'] = 'Inactivity for %s days';
 $strings['YouAreConnectedInPlatformXLinkXSinceXDays'] = '
-We have noticed that you have not connected to the platform %s (%s) for %s days.
-This is an automatic message to remind you of your activity.';
-$strings['sender_id'] = 'User that will send the cron emails';
+We noticed that you have not connected to the platform %s (%s) for %s days.
+This automatic message is a reminder of your activity.';
+$strings['sender_id'] = 'User that will send cron notifications';

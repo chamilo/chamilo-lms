a49228d515670116f1f971627602d70f42a0aa77
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-26 12:29:19 -0500

    Move page background logic into reusable `usePageBackground` composable

--- assets/vue/components/layout/DashboardLayout.vue
+++ assets/vue/components/layout/DashboardLayout.vue
@@ -17,9 +17,7 @@ import Breadcrumb from "../../components/Breadcrumb.vue"
 import Topbar from "../../components/layout/Topbar.vue"
 import Sidebar from "../../components/layout/Sidebar.vue"
 import { useSecurityStore } from "../../store/securityStore"
-import { useVisualTheme } from "../../composables/theme"
-import { ref, watch, onMounted } from "vue"
-import { useRoute } from "vue-router"
+import { usePageBackground } from "../../composables/pageBackground"
 
 defineProps({
   showBreadcrumb: {
@@ -29,50 +27,5 @@ defineProps({
 })
 
 const securityStore = useSecurityStore()
-const { getThemeAssetUrl } = useVisualTheme()
-const route = useRoute()
-const appMainRef = ref(null)
-
-const pageBackgrounds = {
-  "page-administration-platform": "images/bg-cityscape.png",
-  "page-administration-session": "images/bg-fieldscape.png",
-  "page-my-courses": "images/bg-landscape.png",
-  "page-sessions": "images/bg-landscape.png",
-  "page-social": "images/bg-seascape.png",
-}
-
-function applyBackground() {
-  const el = appMainRef.value
-  if (!el) return
-
-  const bodyClasses = document.body.classList
-  let bgImage = null
-
-  for (const [pageClass, image] of Object.entries(pageBackgrounds)) {
-    if (bodyClasses.contains(pageClass)) {
-      bgImage = image
-      break
-    }
-  }
-
-  if (bgImage) {
-    el.style.backgroundImage = `url('${getThemeAssetUrl(bgImage)}')`
-    el.style.backgroundRepeat = "repeat-x"
-    el.style.backgroundPosition = "bottom center"
-    el.style.backgroundSize = "auto 180px"
-    el.style.paddingBottom = "200px"
-  } else {
-    el.style.backgroundImage = ""
-    el.style.backgroundRepeat = ""
-    el.style.backgroundPosition = ""
-    el.style.backgroundSize = ""
-    el.style.paddingBottom = ""
-  }
-}
-
-watch(() => route.path, () => {
-  setTimeout(applyBackground, 0)
-})
-
-onMounted(applyBackground)
+const { appMainRef } = usePageBackground()
 </script>

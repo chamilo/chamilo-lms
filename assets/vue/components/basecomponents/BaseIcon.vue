e1cc2e49bc8b330e845bfc5d5a2df76233779dc1
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-27 17:52:17 -0500

    Display: Add BaseIcon styles #5652

--- assets/vue/components/basecomponents/BaseIcon.vue
+++ assets/vue/components/basecomponents/BaseIcon.vue
@@ -1,19 +1,19 @@
 <template>
   <span
-    class="relative inline-flex cursor-pointer"
-    :class="{ group: tooltip }"
-    v-bind="{ ...$attrs, class: undefined }"
+    class="base-icon"
+    :class="[size !== 'custom' && `base-icon--${size}`, { 'base-icon--has-tooltip': tooltip }]"
+    v-bind="attrs"
   >
     <i
-      :class="[iconClass, $attrs.class]"
+      :class="iconClass"
       aria-hidden="true"
     />
 
     <!-- Optional badge: supports text or a chamilo icon -->
     <span
       v-if="badge || badgeIcon"
-      class="absolute text-base font-bold leading-none"
-      :class="[badgePositionClass, badgeClass]"
+      class="base-icon__badge"
+      :class="[`base-icon__badge--${badgePosition}`, badgeClass]"
     >
       <i
         v-if="badgeIconClass"
@@ -26,7 +26,7 @@
     <!-- Optional tooltip shown below on hover -->
     <span
       v-if="tooltip"
-      class="absolute top-full left-1/2 -translate-x-1/2 mt-1 px-2 py-1 text-xs text-white bg-gray-90 rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none z-10"
+      class="base-icon__tooltip"
     >
       {{ tooltip }}
     </span>
@@ -67,7 +67,7 @@ const props = defineProps({
   },
   badgeClass: {
     type: String,
-    default: "text-success text-base",
+    default: "",
   },
   badgePosition: {
     type: String,
@@ -80,34 +80,13 @@ const props = defineProps({
   },
 })
 
-const iconClass = computed(() => {
-  let cls = chamiloIconToClass[props.icon] + " "
-  switch (props.size) {
-    case "big":
-      cls += "text-3xl/4 "
-      break
-    case "normal":
-      cls += "text-xl/4 "
-      break
-    case "small":
-      cls += "text-base/4 "
-      break
-  }
-  return cls
-})
+const iconClass = computed(() => chamiloIconToClass[props.icon])
 
 const badgeIconClass = computed(() => {
-  if (!props.badgeIcon) return ""
-  return chamiloIconToClass[props.badgeIcon] || ""
-})
-
-const badgePositionClass = computed(() => {
-  const positions = {
-    "top-left": "-top-2 -left-4",
-    "top-right": "-top-2 -right-4",
-    "bottom-left": "-bottom-2 -left-4",
-    "bottom-right": "-bottom-2 -right-4",
+  if (!props.badgeIcon) {
+    return ""
   }
-  return positions[props.badgePosition]
+
+  return chamiloIconToClass[props.badgeIcon] || ""
 })
 </script>

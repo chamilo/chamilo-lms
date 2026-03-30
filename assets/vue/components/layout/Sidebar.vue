fdfea6eb55a7a3ac2b6c56c266363606a9bd5d86
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-03-30 18:26:05 -0500

    Display: Adjust sidebar behavior for mobile view

--- assets/vue/components/layout/Sidebar.vue
+++ assets/vue/components/layout/Sidebar.vue
@@ -92,9 +92,13 @@ const enrolledStore = useEnrolledStore()
 const { menuItemsBeforeMyCourse, menuItemMyCourse, menuItemsAfterMyCourse, hasOnlyOneItem, initialize } =
   useSidebarMenu()
 
-const stored = window.localStorage.getItem("sidebarIsOpen")
-const sidebarIsOpen = ref(stored === null ? true : stored === "true")
-if (stored === null) {
+const isMobile = () => window.innerWidth < 640
+
+const storedSidebarState = window.localStorage.getItem("sidebarIsOpen")
+
+const sidebarIsOpen = ref(isMobile() ? false : storedSidebarState === null ? true : storedSidebarState === "true")
+
+if (!isMobile() && storedSidebarState === null) {
   window.localStorage.setItem("sidebarIsOpen", "true")
 }
 const expandingDueToPanelClick = ref(false)
@@ -114,7 +118,9 @@ watch(
   sidebarIsOpen,
   (newValue) => {
     const appEl = document.querySelector("#app")
-    window.localStorage.setItem("sidebarIsOpen", newValue.toString())
+    if (!isMobile()) {
+      window.localStorage.setItem("sidebarIsOpen", newValue.toString())
+    }
     appEl.classList.toggle("app--sidebar-inactive", !newValue)
 
     if (!newValue) {
@@ -122,7 +128,9 @@ watch(
         const expandedHeaders = document.querySelectorAll(".p-panelmenu-header.p-highlight")
         expandedHeaders.forEach((header) => header.click())
         sidebarIsOpen.value = false
-        window.localStorage.setItem("sidebarIsOpen", "false")
+        if (!isMobile()) {
+          window.localStorage.setItem("sidebarIsOpen", "false")
+        }
       }
     }
     expandingDueToPanelClick.value = false

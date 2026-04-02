27deab040002c5d700ff327d66c4a53e4640be36
Yannick Warnier <ywarnier@beeznest.org>
2026-04-02 22:46:26 +0200

    Plugin: CustomFooter: Update language files structure

---
+++ public/plugin/CustomFooter/lang/en_US.php
@@ -0,0 +1,7 @@
+<?php
+
+$strings['plugin_title'] = 'Custom Footer';
+$strings['plugin_comment'] = 'Add custom text in the footer left and right areas';
+$strings['footerleft'] = 'Footer left';
+$strings['footerright'] = 'Footer right';
+$strings['Save'] = 'Save';
---
+++ public/plugin/CustomFooter/lang/fr_FR.php
@@ -0,0 +1,7 @@
+<?php
+
+$strings['plugin_title'] = 'Pied de page personnalisé';
+$strings['plugin_comment'] = 'Ajouter du texte personnalisé dans les zones gauche et droite du pied de page';
+$strings['footerleft'] = 'Pied de page gauche';
+$strings['footerright'] = 'Pied de page droite';
+$strings['Save'] = 'Enregistrer';

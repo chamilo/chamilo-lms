35300e8258e1ec1674f61adaa740b6f7a7692c84
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-04-27 17:56:54 -0500

    Internal: Remove user profile template, controller, and related test for deprecated public profile feature

--- src/CoreBundle/Controller/UserController.php
+++ src/CoreBundle/Controller/UserController.php
@@ -6,14 +6,11 @@ declare(strict_types=1);
 
 namespace Chamilo\CoreBundle\Controller;
 
-use Chamilo\CoreBundle\Repository\Node\IllustrationRepository;
-use Chamilo\CoreBundle\Repository\Node\UserRepository;
 use CourseManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 use UserGroupModel;
 
 /**
@@ -65,32 +62,6 @@ class UserController extends AbstractController
             'usersNotSubscribedToCourse' => $data['usersNotSubscribedToCourse'],
             'error' => $data['error'],
             'warning' => $data['warning'],
-        ]);
-    }
-
-    /**
-     * Public profile.
-     */
-    #[Route(path: '/{username}', name: 'chamilo_core_user_profile', methods: ['GET'])]
-    public function profile(string $username, UserRepository $userRepository, IllustrationRepository $illustrationRepository): Response
-    {
-        $user = $userRepository->findByUsername($username);
-
-        if (!\is_object($user) || !$user instanceof UserInterface) {
-            throw $this->createAccessDeniedException('This user does not have access to this section');
-        }
-
-        $url = $illustrationRepository->getIllustrationUrl($user);
-
-        $sanitize = static fn (?string $v): string => '' !== ($v ?? '') ? \Security::remove_XSS($v, STUDENT) : '';
-
-        return $this->render('@ChamiloCore/User/profile.html.twig', [
-            'user' => $user,
-            'illustration_url' => $url,
-            'competences' => $sanitize($user->getCompetences()),
-            'openarea' => $sanitize($user->getOpenarea()),
-            'teach' => $sanitize($user->getTeach()),
-            'diplomas' => $sanitize($user->getDiplomas()),
         ]);
     }
 }

04ca7fa3e80efb637290bea10d0baaa5dd8bc2a3
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-06-02 03:33:34 -0500

    Security: Sanitize `SocialPost` content to prevent XSS vulnerabilities in SocialWall posts
    See advisory GHSA-ppw9-gf4r-5c39

--- assets/vue/components/social/SocialWallPost.vue
+++ assets/vue/components/social/SocialWallPost.vue
@@ -80,7 +80,7 @@
           </video>
         </div>
 
-        <div v-html="post.content" />
+        <div v-html="sanitizeHtml(post.content)" />
 
         <LinkPreviewCard
           v-for="previewUrl in extractedUrls"
@@ -149,6 +149,7 @@ import { SOCIAL_TYPE_PROMOTED_MESSAGE } from "./constants"
 import { useFormatDate } from "../../composables/formatDate"
 import { useSecurityStore } from "../../store/securityStore"
 import { useI18n } from "vue-i18n"
+import { sanitizeHtml } from "../../utils/sanitizeHtml"
 
 const props = defineProps({
   post: {

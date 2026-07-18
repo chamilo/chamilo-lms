a6d53406493e03325a19054c479b2a5d7e18ca2c
Christian Beeznest <christian.fasanando@beeznest.com>
2026-07-18 09:32:13 -0500

    Learning paths: Report SCORM upload limit errors

---
+++ tests/CoreBundle/Service/LearningPath/ScormPackageImporterTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/* For licensing terms, see /license.txt */
+
+declare(strict_types=1);
+
+namespace Chamilo\Tests\CoreBundle\Service\LearningPath;
+
+use Chamilo\CoreBundle\Entity\Course;
+use Chamilo\CoreBundle\Service\LearningPath\ScormPackageImporter;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+final class ScormPackageImporterTest extends TestCase
+{
+    public function testItReportsThePhpUploadLimitForOversizedPackages(): void
+    {
+        $package = new UploadedFile(
+            '',
+            'minerias.zip',
+            'application/zip',
+            \UPLOAD_ERR_INI_SIZE,
+        );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage($package->getErrorMessage());
+
+        $this->createImporterWithoutDependencies()->import(
+            $package,
+            $this->createCourseWithoutConstructor(),
+            null,
+            null,
+            true,
+            'local',
+            'Scorm',
+            false,
+        );
+    }
+
+    public function testItKeepsTheGenericMessageForOtherUploadFailures(): void
+    {
+        $package = new UploadedFile(
+            '',
+            'minerias.zip',
+            'application/zip',
+            \UPLOAD_ERR_PARTIAL,
+        );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('The uploaded package is not valid.');
+
+        $this->createImporterWithoutDependencies()->import(
+            $package,
+            $this->createCourseWithoutConstructor(),
+            null,
+            null,
+            true,
+            'local',
+            'Scorm',
+            false,
+        );
+    }
+
+    private function createImporterWithoutDependencies(): ScormPackageImporter
+    {
+        $reflection = new ReflectionClass(ScormPackageImporter::class);
+
+        return $reflection->newInstanceWithoutConstructor();
+    }
+
+    private function createCourseWithoutConstructor(): Course
+    {
+        $reflection = new ReflectionClass(Course::class);
+
+        return $reflection->newInstanceWithoutConstructor();
+    }
+}

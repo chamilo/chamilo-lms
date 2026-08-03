chamilo/chamilo-lms

commit 68912b3d1e1d29e1318125bedc4e4a29415499ad

Auth: manage case with ldap_attribute is null BT #22093

src/CoreBundle/Security/Authenticator/Ldap/LdapAuthenticator.php
@@ -72,6 +72,12 @@ class LdapAuthenticator extends AbstractAuthenticator implements InteractiveAuth
 
         $this->dataCorrespondence = array_filter($ldapConfig['data_correspondence']) ?: [];
 
+        if (null !== $ldapConfig['password_attribute']) {
+            $dataCorrespondence = array_values($this->dataCorrespondence + [$ldapConfig['password_attribute']]);
+        } else {
+            $dataCorrespondence = $this->dataCorrespondence;
+        }
+
         $this->ldapBadge = new LdapBadge(Ldap::class, $dnString, $searchDn, $searchPassword, $queryString);
 
         $this->userProvider = new LdapUserProvider(
@@ -83,7 +89,7 @@ class LdapAuthenticator extends AbstractAuthenticator implements InteractiveAuth
             $ldapConfig['uid_key'] ?? null,
             $ldapConfig['filter'] ?? null,
             $ldapConfig['password_attribute'] ?? null,
-            array_values($this->dataCorrespondence + [$ldapConfig['password_attribute']]),
+            $dataCorrespondence,
         );
     }
 

commit e11e78d29b8a30b82a258c1b6067695a03f7b323

Auth: #fix error with data correspondance introduced with commit 68912b3d1e1d2 to enable null value for password_attibute sent by @madmoe00 - refs GH #8528

src/CoreBundle/Security/Authenticator/Ldap/LdapAuthenticator.php
@@ -86,7 +86,7 @@ class LdapAuthenticator extends AbstractAuthenticator implements InteractiveAuth
         if (null !== $ldapConfig['password_attribute']) {
             $dataCorrespondence = array_values($this->dataCorrespondence + [$ldapConfig['password_attribute']]);
         } else {
-            $dataCorrespondence = $this->dataCorrespondence;
+            $dataCorrespondence = array_values($this->dataCorrespondence);
         }
 
         // Always use the queryString approach (search for actual DN, then bind) unless the admin
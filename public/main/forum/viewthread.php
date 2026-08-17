25f52f947a34f9604b64f2a583fe8941b23445b0
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2026-08-17 17:59:58 -0500

    Security: sanitize forum post body in legacy thread view (#8905)
    See advisory GHSA-gjcf-95hx-382f
    
    Related to 63bf4ad8481e9d53c10bb1ed5f5e45837a98145b

--- public/main/forum/viewthread.php
+++ public/main/forum/viewthread.php
@@ -693,7 +693,11 @@ foreach ($posts as $post) {
     $post['post_title'] .= Display::tag('div', $titlePost, ['class' => 'post-header']);
 
     // Body
-    $post['post_data'] = Display::tag('div', $post['post_text'], ['class' => 'post-body']);
+    $post['post_data'] = Display::tag(
+        'div',
+        Security::remove_XSS($post['post_text'], STUDENT),
+        ['class' => 'post-body']
+    );
 
     // Attachments
     $post['post_attachments'] = '';

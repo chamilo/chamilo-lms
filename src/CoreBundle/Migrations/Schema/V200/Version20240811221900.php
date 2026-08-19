f7b179c4202700242f6aa8f5e1f2c9773dacf391
NicoDucou <nicolas@ldd.fr>
2026-08-19 22:18:59 +0200

    Install: fix to check all element in migration to be able to rerun the migration without error - refs BT#23576

--- src/CoreBundle/Migrations/Schema/V200/Version20240811221900.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20240811221900.php
@@ -18,50 +18,97 @@ final class Version20240811221900 extends AbstractMigrationChamilo
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE block ADD user_id INT DEFAULT NULL;');
-        $this->addSql('ALTER TABLE block ADD CONSTRAINT FK_831B9722A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_831B9722A76ED395 ON block (user_id);');
+        $blockTable = $schema->getTable('block');
+        if (!$blockTable->hasColumn('user_id')) {
+            $this->addSql('ALTER TABLE block ADD user_id INT DEFAULT NULL;');
+        }
+        if (!$blockTable->hasForeignKey('FK_831B9722A76ED395')) {
+            $this->addSql('ALTER TABLE block ADD CONSTRAINT FK_831B9722A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
+        if (!$blockTable->hasIndex('UNIQ_831B9722A76ED395')) {
+            $this->addSql('CREATE UNIQUE INDEX UNIQ_831B9722A76ED395 ON block (user_id);');
+        }
 
-        if ($schema->getTable('c_blog_attachment')->hasIndex('course')) {
+        $blogAttachmentTable = $schema->getTable('c_blog_attachment');
+        if ($blogAttachmentTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_attachment;');
         }
-        $this->addSql('ALTER TABLE c_blog_attachment DROP id, DROP c_id, DROP post_id, DROP comment_id, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        if ($blogAttachmentTable->hasColumn('id')) {
+            $this->addSql('ALTER TABLE c_blog_attachment DROP id, DROP c_id, DROP post_id, DROP comment_id, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_attachment WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_attachment ADD CONSTRAINT FK_E769AADCDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_E769AADCDAE07E97 ON c_blog_attachment (blog_id);');
+        if (!$blogAttachmentTable->hasForeignKey('FK_E769AADCDAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_attachment ADD CONSTRAINT FK_E769AADCDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
+        if (!$blogAttachmentTable->hasIndex('IDX_E769AADCDAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_E769AADCDAE07E97 ON c_blog_attachment (blog_id);');
+        }
 
-        if ($schema->getTable('c_blog_rating')->hasIndex('course')) {
+        $blogRatingTable = $schema->getTable('c_blog_rating');
+        if ($blogRatingTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_rating;');
         }
-        $this->addSql('ALTER TABLE c_blog_rating DROP rating_id, DROP c_id, DROP item_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL;');
+        if ($blogRatingTable->hasColumn('rating_id')) {
+            $this->addSql('ALTER TABLE c_blog_rating DROP rating_id, DROP c_id, DROP item_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_rating WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_rating ADD CONSTRAINT FK_D4E30760DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        if (!$blogRatingTable->hasForeignKey('FK_D4E30760DAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_rating ADD CONSTRAINT FK_D4E30760DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_rating WHERE user_id NOT IN (SELECT id FROM user);');
-        $this->addSql('ALTER TABLE c_blog_rating ADD CONSTRAINT FK_D4E30760A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_D4E30760DAE07E97 ON c_blog_rating (blog_id);');
-        $this->addSql('CREATE INDEX IDX_D4E30760A76ED395 ON c_blog_rating (user_id);');
+        if (!$blogRatingTable->hasForeignKey('FK_D4E30760A76ED395')) {
+            $this->addSql('ALTER TABLE c_blog_rating ADD CONSTRAINT FK_D4E30760A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
+        if (!$blogRatingTable->hasIndex('IDX_D4E30760DAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_D4E30760DAE07E97 ON c_blog_rating (blog_id);');
+        }
+        if (!$blogRatingTable->hasIndex('IDX_D4E30760A76ED395')) {
+            $this->addSql('CREATE INDEX IDX_D4E30760A76ED395 ON c_blog_rating (user_id);');
+        }
 
-        if ($schema->getTable('c_blog_post')->hasIndex('course')) {
+        $blogPostTable = $schema->getTable('c_blog_post');
+        if ($blogPostTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_post;');
         }
-        $this->addSql('ALTER TABLE c_blog_post DROP c_id, DROP post_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE author_id author_id INT DEFAULT NULL;');
+        if ($blogPostTable->hasColumn('post_id')) {
+            $this->addSql('ALTER TABLE c_blog_post DROP c_id, DROP post_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE author_id author_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_post WHERE author_id NOT IN (SELECT id FROM user);');
-        $this->addSql('ALTER TABLE c_blog_post ADD CONSTRAINT FK_B6FD68A3F675F31B FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE CASCADE;');
+        if (!$blogPostTable->hasForeignKey('FK_B6FD68A3F675F31B')) {
+            $this->addSql('ALTER TABLE c_blog_post ADD CONSTRAINT FK_B6FD68A3F675F31B FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_post WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_post ADD CONSTRAINT FK_B6FD68A3DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_B6FD68A3F675F31B ON c_blog_post (author_id);');
-        $this->addSql('CREATE INDEX IDX_B6FD68A3DAE07E97 ON c_blog_post (blog_id);');
+        if (!$blogPostTable->hasForeignKey('FK_B6FD68A3DAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_post ADD CONSTRAINT FK_B6FD68A3DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
+        if (!$blogPostTable->hasIndex('IDX_B6FD68A3F675F31B')) {
+            $this->addSql('CREATE INDEX IDX_B6FD68A3F675F31B ON c_blog_post (author_id);');
+        }
+        if (!$blogPostTable->hasIndex('IDX_B6FD68A3DAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_B6FD68A3DAE07E97 ON c_blog_post (blog_id);');
+        }
 
-        if ($schema->getTable('c_blog_rel_user')->hasIndex('course')) {
+        $blogRelUserTable = $schema->getTable('c_blog_rel_user');
+        if ($blogRelUserTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_rel_user;');
         }
-        $this->addSql('ALTER TABLE c_blog_rel_user DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL;');
+        if ($blogRelUserTable->hasColumn('c_id')) {
+            $this->addSql('ALTER TABLE c_blog_rel_user DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_rel_user WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_rel_user ADD CONSTRAINT FK_B55D851BDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        if (!$blogRelUserTable->hasForeignKey('FK_B55D851BDAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_rel_user ADD CONSTRAINT FK_B55D851BDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_rel_user WHERE user_id NOT IN (SELECT id FROM user);');
-        $this->addSql('ALTER TABLE c_blog_rel_user ADD CONSTRAINT FK_B55D851BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_B55D851BDAE07E97 ON c_blog_rel_user (blog_id);');
-        $this->addSql('CREATE INDEX IDX_B55D851BA76ED395 ON c_blog_rel_user (user_id);');
+        if (!$blogRelUserTable->hasForeignKey('FK_B55D851BA76ED395')) {
+            $this->addSql('ALTER TABLE c_blog_rel_user ADD CONSTRAINT FK_B55D851BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
+        if (!$blogRelUserTable->hasIndex('IDX_B55D851BDAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_B55D851BDAE07E97 ON c_blog_rel_user (blog_id);');
+        }
+        if (!$blogRelUserTable->hasIndex('IDX_B55D851BA76ED395')) {
+            $this->addSql('CREATE INDEX IDX_B55D851BA76ED395 ON c_blog_rel_user (user_id);');
+        }
 
         $blogTaskRelUserTable = $schema->getTable('c_blog_task_rel_user');
         if ($blogTaskRelUserTable->hasIndex('course')) {
@@ -73,16 +120,30 @@ final class Version20240811221900 extends AbstractMigrationChamilo
         if ($blogTaskRelUserTable->hasIndex('task')) {
             $this->addSql('DROP INDEX task ON c_blog_task_rel_user;');
         }
-        $this->addSql('ALTER TABLE c_blog_task_rel_user DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL, CHANGE task_id task_id INT DEFAULT NULL;');
+        if ($blogTaskRelUserTable->hasColumn('c_id')) {
+            $this->addSql('ALTER TABLE c_blog_task_rel_user DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL, CHANGE task_id task_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_task_rel_user WHERE task_id NOT IN (SELECT iid FROM c_blog_task);');
-        $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C738DB60186 FOREIGN KEY (task_id) REFERENCES c_blog_task (iid) ON DELETE CASCADE;');
+        if (!$blogTaskRelUserTable->hasForeignKey('FK_FD8B3C738DB60186')) {
+            $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C738DB60186 FOREIGN KEY (task_id) REFERENCES c_blog_task (iid) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_task_rel_user WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C73DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        if (!$blogTaskRelUserTable->hasForeignKey('FK_FD8B3C73DAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C73DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_task_rel_user WHERE user_id NOT IN (SELECT id FROM user);');
-        $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C73A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_FD8B3C73DAE07E97 ON c_blog_task_rel_user (blog_id);');
-        $this->addSql('CREATE INDEX IDX_FD8B3C738DB60186 ON c_blog_task_rel_user (task_id);');
-        $this->addSql('CREATE INDEX IDX_FD8B3C73A76ED395 ON c_blog_task_rel_user (user_id);');
+        if (!$blogTaskRelUserTable->hasForeignKey('FK_FD8B3C73A76ED395')) {
+            $this->addSql('ALTER TABLE c_blog_task_rel_user ADD CONSTRAINT FK_FD8B3C73A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
+        if (!$blogTaskRelUserTable->hasIndex('IDX_FD8B3C73DAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_FD8B3C73DAE07E97 ON c_blog_task_rel_user (blog_id);');
+        }
+        if (!$blogTaskRelUserTable->hasIndex('IDX_FD8B3C738DB60186')) {
+            $this->addSql('CREATE INDEX IDX_FD8B3C738DB60186 ON c_blog_task_rel_user (task_id);');
+        }
+        if (!$blogTaskRelUserTable->hasIndex('IDX_FD8B3C73A76ED395')) {
+            $this->addSql('CREATE INDEX IDX_FD8B3C73A76ED395 ON c_blog_task_rel_user (user_id);');
+        }
 
         $blogTable = $schema->getTable('c_blog');
         if ($blogTable->hasIndex('course')) {
@@ -91,29 +152,53 @@ final class Version20240811221900 extends AbstractMigrationChamilo
         if ($blogTable->hasIndex('session_id')) {
             $this->addSql('DROP INDEX session_id ON c_blog;');
         }
-        $this->addSql('ALTER TABLE c_blog DROP c_id, DROP blog_id, DROP visibility, DROP session_id;');
-        $this->addSql('ALTER TABLE c_blog ADD CONSTRAINT FK_64B00A121BAD783F FOREIGN KEY (resource_node_id) REFERENCES resource_node (id) ON DELETE CASCADE;');
+        if ($blogTable->hasColumn('visibility')) {
+            $this->addSql('ALTER TABLE c_blog DROP c_id, DROP blog_id, DROP visibility, DROP session_id;');
+        }
+        if (!$blogTable->hasForeignKey('FK_64B00A121BAD783F')) {
+            $this->addSql('ALTER TABLE c_blog ADD CONSTRAINT FK_64B00A121BAD783F FOREIGN KEY (resource_node_id) REFERENCES resource_node (id) ON DELETE CASCADE;');
+        }
 
-        $this->addSql('ALTER TABLE c_lp_category_rel_user DROP FOREIGN KEY FK_61F0427A76ED395;');
+        if ($schema->getTable('c_lp_category_rel_user')->hasForeignKey('FK_61F0427A76ED395')) {
+            $this->addSql('ALTER TABLE c_lp_category_rel_user DROP FOREIGN KEY FK_61F0427A76ED395;');
+        }
 
-        if ($schema->getTable('c_blog_task')->hasIndex('course')) {
+        $blogTaskTable = $schema->getTable('c_blog_task');
+        if ($blogTaskTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_task;');
         }
-        $this->addSql('ALTER TABLE c_blog_task DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        if ($blogTaskTable->hasColumn('c_id')) {
+            $this->addSql('ALTER TABLE c_blog_task DROP c_id, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_task WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_task ADD CONSTRAINT FK_BE09DF0BDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_BE09DF0BDAE07E97 ON c_blog_task (blog_id);');
+        if (!$blogTaskTable->hasForeignKey('FK_BE09DF0BDAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_task ADD CONSTRAINT FK_BE09DF0BDAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
+        if (!$blogTaskTable->hasIndex('IDX_BE09DF0BDAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_BE09DF0BDAE07E97 ON c_blog_task (blog_id);');
+        }
 
-        if ($schema->getTable('c_blog_comment')->hasIndex('course')) {
+        $blogCommentTable = $schema->getTable('c_blog_comment');
+        if ($blogCommentTable->hasIndex('course')) {
             $this->addSql('DROP INDEX course ON c_blog_comment;');
         }
-        $this->addSql('ALTER TABLE c_blog_comment DROP c_id, DROP post_id, DROP task_id, DROP parent_comment_id, CHANGE author_id author_id INT DEFAULT NULL, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        if ($blogCommentTable->hasColumn('post_id')) {
+            $this->addSql('ALTER TABLE c_blog_comment DROP c_id, DROP post_id, DROP task_id, DROP parent_comment_id, CHANGE author_id author_id INT DEFAULT NULL, CHANGE blog_id blog_id INT DEFAULT NULL;');
+        }
         $this->addSql('DELETE FROM c_blog_comment WHERE author_id NOT IN (SELECT id FROM user);');
-        $this->addSql('ALTER TABLE c_blog_comment ADD CONSTRAINT FK_CAA18F1F675F31B FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE CASCADE;');
+        if (!$blogCommentTable->hasForeignKey('FK_CAA18F1F675F31B')) {
+            $this->addSql('ALTER TABLE c_blog_comment ADD CONSTRAINT FK_CAA18F1F675F31B FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE CASCADE;');
+        }
         $this->addSql('DELETE FROM c_blog_comment WHERE blog_id NOT IN (SELECT iid FROM c_blog);');
-        $this->addSql('ALTER TABLE c_blog_comment ADD CONSTRAINT FK_CAA18F1DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
-        $this->addSql('CREATE INDEX IDX_CAA18F1F675F31B ON c_blog_comment (author_id);');
-        $this->addSql('CREATE INDEX IDX_CAA18F1DAE07E97 ON c_blog_comment (blog_id);');
+        if (!$blogCommentTable->hasForeignKey('FK_CAA18F1DAE07E97')) {
+            $this->addSql('ALTER TABLE c_blog_comment ADD CONSTRAINT FK_CAA18F1DAE07E97 FOREIGN KEY (blog_id) REFERENCES c_blog (iid) ON DELETE CASCADE;');
+        }
+        if (!$blogCommentTable->hasIndex('IDX_CAA18F1F675F31B')) {
+            $this->addSql('CREATE INDEX IDX_CAA18F1F675F31B ON c_blog_comment (author_id);');
+        }
+        if (!$blogCommentTable->hasIndex('IDX_CAA18F1DAE07E97')) {
+            $this->addSql('CREATE INDEX IDX_CAA18F1DAE07E97 ON c_blog_comment (blog_id);');
+        }
     }
 
     public function down(Schema $schema): void

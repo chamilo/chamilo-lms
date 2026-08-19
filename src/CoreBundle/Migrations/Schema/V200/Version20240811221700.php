3a3b610cf18fdffd9d185c5dec13f0cceda28b4b
NicoDucou <nicolas@ldd.fr>
2026-08-19 22:07:53 +0200

    Install: check if foreign key exist before creating it to avoid error - refs BT#23246

--- src/CoreBundle/Migrations/Schema/V200/Version20240811221700.php
+++ src/CoreBundle/Migrations/Schema/V200/Version20240811221700.php
@@ -89,47 +89,67 @@ final class Version20240811221700 extends AbstractMigrationChamilo
             ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ROW_FORMAT = DYNAMIC;
         ");
 
-        $this->addSql('
-            ALTER TABLE lti_token
-            ADD CONSTRAINT FK_EA71C468F7B22CC FOREIGN KEY (tool_id)
-            REFERENCES lti_external_tool (id) ON DELETE CASCADE;
-        ');
+        $ltiToken = $schema->getTable('lti_token');
+        $ltiExternalTool = $schema->getTable('lti_external_tool');
+        $ltiLineitem = $schema->getTable('lti_lineitem');
 
-        $this->addSql('
-            ALTER TABLE lti_external_tool
-            ADD CONSTRAINT FK_DB0E04E41BAD783F FOREIGN KEY (resource_node_id)
-            REFERENCES resource_node (id) ON DELETE CASCADE;
-        ');
+        if (!$ltiToken->hasForeignKey('FK_EA71C468F7B22CC')) {
+            $this->addSql('
+                ALTER TABLE lti_token
+                ADD CONSTRAINT FK_EA71C468F7B22CC FOREIGN KEY (tool_id)
+                REFERENCES lti_external_tool (id) ON DELETE CASCADE;
+            ');
+        }
 
-        $this->addSql('
-            ALTER TABLE lti_external_tool
-            ADD CONSTRAINT FK_DB0E04E491D79BD3 FOREIGN KEY (c_id)
-            REFERENCES course (id);
-        ');
+        if (!$ltiExternalTool->hasForeignKey('FK_DB0E04E41BAD783F')) {
+            $this->addSql('
+                ALTER TABLE lti_external_tool
+                ADD CONSTRAINT FK_DB0E04E41BAD783F FOREIGN KEY (resource_node_id)
+                REFERENCES resource_node (id) ON DELETE CASCADE;
+            ');
+        }
 
-        $this->addSql('
-            ALTER TABLE lti_external_tool
-            ADD CONSTRAINT FK_DB0E04E482F80D8B FOREIGN KEY (gradebook_eval_id)
-            REFERENCES gradebook_evaluation (id) ON DELETE SET NULL;
-        ');
+        if (!$ltiExternalTool->hasForeignKey('FK_DB0E04E491D79BD3')) {
+            $this->addSql('
+                ALTER TABLE lti_external_tool
+                ADD CONSTRAINT FK_DB0E04E491D79BD3 FOREIGN KEY (c_id)
+                REFERENCES course (id);
+            ');
+        }
 
-        $this->addSql('
-            ALTER TABLE lti_external_tool
-            ADD CONSTRAINT FK_DB0E04E4727ACA70 FOREIGN KEY (parent_id)
-            REFERENCES lti_external_tool (id);
-        ');
+        if (!$ltiExternalTool->hasForeignKey('FK_DB0E04E482F80D8B')) {
+            $this->addSql("ALTER TABLE lti_external_tool CHANGE gradebook_eval_id gradebook_eval_id {$gradebookEvaluationIdType} DEFAULT NULL");
+            $this->addSql('
+                ALTER TABLE lti_external_tool
+                ADD CONSTRAINT FK_DB0E04E482F80D8B FOREIGN KEY (gradebook_eval_id)
+                REFERENCES gradebook_evaluation (id) ON DELETE SET NULL;
+            ');
+        }
 
-        $this->addSql('
-            ALTER TABLE lti_lineitem
-            ADD CONSTRAINT FK_5C76B75D8F7B22CC FOREIGN KEY (tool_id)
-            REFERENCES lti_external_tool (id) ON DELETE CASCADE;
-        ');
+        if (!$ltiExternalTool->hasForeignKey('FK_DB0E04E4727ACA70')) {
+            $this->addSql('
+                ALTER TABLE lti_external_tool
+                ADD CONSTRAINT FK_DB0E04E4727ACA70 FOREIGN KEY (parent_id)
+                REFERENCES lti_external_tool (id);
+            ');
+        }
 
-        $this->addSql('
-            ALTER TABLE lti_lineitem
-            ADD CONSTRAINT FK_5C76B75D1323A575 FOREIGN KEY (evaluation)
-            REFERENCES gradebook_evaluation (id) ON DELETE CASCADE;
-        ');
+        if (!$ltiLineitem->hasForeignKey('FK_5C76B75D8F7B22CC')) {
+            $this->addSql('
+                ALTER TABLE lti_lineitem
+                ADD CONSTRAINT FK_5C76B75D8F7B22CC FOREIGN KEY (tool_id)
+                REFERENCES lti_external_tool (id) ON DELETE CASCADE;
+            ');
+        }
+
+        if (!$ltiLineitem->hasForeignKey('FK_5C76B75D1323A575')) {
+            $this->addSql("ALTER TABLE lti_lineitem CHANGE evaluation evaluation {$gradebookEvaluationIdType} NOT NULL");
+            $this->addSql('
+                ALTER TABLE lti_lineitem
+                ADD CONSTRAINT FK_5C76B75D1323A575 FOREIGN KEY (evaluation)
+                REFERENCES gradebook_evaluation (id) ON DELETE CASCADE;
+            ');
+        }
     }
 
     public function down(Schema $schema): void

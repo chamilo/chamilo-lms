37909c55d1e78be67ccee448002be40a6bce79ba
Christian Beeznest <christian.fasanando@beeznest.com>
2026-09-01 00:00:58 -0500

    Migration: fix legacy completion exercise mappings

--- src/CoreBundle/Command/MigrateCompletionRulesCommand.php
+++ src/CoreBundle/Command/MigrateCompletionRulesCommand.php
@@ -45,14 +45,14 @@ final class MigrateCompletionRulesCommand extends Command
             'exercises' => [
                 [
                     'legacy_id' => 17,
-                    'current_id' => 332,
+                    'current_id' => 337,
                     'weight' => 20.0,
                     'mapping_status' => 'verified_from_migrated_course_metadata',
                     'mapping_confidence' => 'high',
                 ],
                 [
                     'legacy_id' => 22,
-                    'current_id' => 337,
+                    'current_id' => 342,
                     'weight' => 30.0,
                     'mapping_status' => 'verified_from_migrated_course_metadata',
                     'mapping_confidence' => 'high',
@@ -581,7 +581,7 @@ final class MigrateCompletionRulesCommand extends Command
             'exercises' => [
                 [
                     'legacy_id' => 25,
-                    'current_id' => 482,
+                    'current_id' => 483,
                     'weight' => 20.0,
                     'mapping_status' => 'verified_from_migrated_course_metadata',
                     'mapping_confidence' => 'high',

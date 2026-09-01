61c5c07f045e06376959e316385e5a5c6e42d94e
Christian Beeznest <christian.fasanando@beeznest.com>
2026-09-01 12:51:30 -0500

    Migration: correct 2120 completion exercise mappings

--- src/CoreBundle/Command/MigrateCompletionRulesCommand.php
+++ src/CoreBundle/Command/MigrateCompletionRulesCommand.php
@@ -332,14 +332,14 @@ final class MigrateCompletionRulesCommand extends Command
             'exercises' => [
                 [
                     'legacy_id' => 5,
-                    'current_id' => 5,
+                    'current_id' => 6,
                     'weight' => 20.0,
-                    'mapping_status' => 'resolved_from_history',
+                    'mapping_status' => 'verified_from_migrated_course_metadata',
                     'mapping_confidence' => 'high',
                 ],
                 [
                     'legacy_id' => 7,
-                    'current_id' => 17,
+                    'current_id' => 8,
                     'weight' => 20.0,
                     'mapping_status' => 'resolved_from_final_exam_rule',
                     'mapping_confidence' => 'high',

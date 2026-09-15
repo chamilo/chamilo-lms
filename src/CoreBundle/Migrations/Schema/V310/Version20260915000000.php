98a4d233288566969125c0131a09aafc651bd181
Angel Fernando Quiroz Campos <1697880+AngelFQC@users.noreply.github.com>
2026-09-15 18:34:40 -0500

    Migration: Move the chamilo_database_version removal from V400 to V310
    A database that already ran it under the V400 name reports it as executed but
    unavailable; re-running it is harmless, since it deletes a row that is gone.

--- src/CoreBundle/Migrations/Schema/V310/Version20260915000000.php
+++ src/CoreBundle/Migrations/Schema/V310/Version20260915000000.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /* For licensing terms, see /license.txt */
 
-namespace Chamilo\CoreBundle\Migrations\Schema\V400;
+namespace Chamilo\CoreBundle\Migrations\Schema\V310;
 
 use Chamilo\CoreBundle\Migrations\AbstractMigrationChamilo;
 use Doctrine\DBAL\Schema\Schema;

0eb080de432234b88b6216eb7c6763dbfa976afa
Yannick Warnier <ywarnier@beeznest.org>
2026-09-17 02:40:42 +0200

    Internal: Bump version to 3.0.1

--- version.php
+++ version.php
@@ -3,11 +3,11 @@
 /* For licensing terms, see /license.txt */
 
 return [
-    'new_version' => '3.0.0',
+    'new_version' => '3.0.1',
     'new_version_status' => 'stable',
     'new_version_last_id' => '1',
     'new_version_stable' => true,
-    'new_version_major' => true,
+    'new_version_major' => false,
     'software_name' => 'Chamilo',
     'software_url' => 'https://chamilo.org/',
 ];

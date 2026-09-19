eae2d17cecc7f9a395fdc77368ab4a9c238b0f53
Yannick Warnier <ywarnier@beeznest.org>
2026-09-19 03:02:20 +0200

    Internal: Bump version number to 3.0.1

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 /* For licensing terms, see /license.txt */
 
 return [
-    'new_version' => '3.0.0',
+    'new_version' => '3.0.1',
     'new_version_status' => 'stable',
     'new_version_last_id' => '1',
     'new_version_stable' => true,
